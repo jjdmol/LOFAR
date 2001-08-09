3440cf24500528fa37bbdbb5c9c0344f0d33bf52
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2001-08-09 15:48:48 +0000

    Implemented first version of TH_Corba and test program

---
+++ CEP/Tools/PerfTest/P2Perf/src/DH_IntArray.h
@@ -0,0 +1,94 @@
+//  DH_IntArray.h: Example DataHolder
+//
+//  Copyright (C) 2000, 2001
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//  $Log$
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef BASESIM_DH_INTARRAY_H
+#define BASESIM_DH_INTARRAY_H
+
+#include "DataHolder.h"
+#include <complex>
+
+/**
+   This class is an integer array DataHolder.
+*/
+
+class DH_IntArray: public DataHolder
+{
+public:
+  typedef int BufferType;
+
+  DH_IntArray (const string& name, unsigned int nbuffer = 10);
+
+  virtual ~DH_IntArray();
+
+  /// Set the Counter attribute in the DataPacket.
+  void setCounter (int counter);
+  /// Get the Counter attribute in the DataPacket.
+  int getCounter() const;
+
+  /// Get write access to the Buffer in the DataPacket.
+  BufferType* getBuffer();
+  /// Get read access to the Buffer in the DataPacket.
+  const BufferType* getBuffer() const;
+
+protected:
+  // Definition of the DataPacket type.
+  class DataPacket: public DataHolder::DataPacket
+  {
+  public:
+    DataPacket() : itsCounter(0) {};
+
+    int itsCounter;
+#if 1
+    BufferType itsFill;         // to ensure alignment
+#endif
+  };
+
+private:
+  /// Forbid copy constructor.
+  DH_IntArray (const DH_IntArray&);
+  /// Forbid assignment.
+  DH_IntArray& operator= (const DH_IntArray&);
+
+
+  DataPacket* itsDataPacket;
+  BufferType* itsBuffer;
+};
+
+
+inline void DH_IntArray::setCounter (int counter)
+  { itsDataPacket->itsCounter = counter; }
+
+inline int DH_IntArray::getCounter() const
+  { return itsDataPacket->itsCounter; }
+
+inline DH_IntArray::BufferType* DH_IntArray::getBuffer()
+  { return itsBuffer; }
+
+inline const DH_IntArray::BufferType* DH_IntArray::getBuffer() const
+  { return itsBuffer; }
+
+
+#endif 
---
+++ CEP/Tools/PerfTest/P2Perf/src/WH_Square.h
@@ -0,0 +1,88 @@
+//  WH_Square.h: This is an example of a WorkHolder class.
+//
+//  Copyright (C) 2000, 2001
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//  $Log$
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef WH_SQUARE_H
+#define WH_SQUARE_H
+
+#include "WorkHolder.h"
+#include "DH_IntArray.h"
+
+
+/**
+   This is an example of a WorkHolder class.
+   It has one input and one output DH_IntArray object as DataHolders.
+
+   It shows which functions have to be implemented
+*/
+
+class WH_Square: public WorkHolder
+{
+public:
+  /// Construct the work holder and give it a name.
+  /// It is possible to specify how many input and output data holders
+  /// are created and how many elements there are in the buffer.
+  /// The argument first tells if this is the first WorkHolder in
+  /// the simulation chain.
+  WH_Square (const string& name="aWH_Square",
+	      bool first = false,
+	      unsigned int nin=1, unsigned int nout=1,
+	      unsigned int nbuffer=10);
+
+  virtual ~WH_Square();
+
+  /// Do a process step.
+  virtual void process();
+
+  /// Show the work holder on stdout.
+  virtual void dump() const;
+
+  /// Get a pointer to the i-th input DataHolder.
+  virtual DH_IntArray* getInHolder (int channel);
+
+  /// Get a pointer to the i-th output DataHolder.
+  virtual DH_IntArray* getOutHolder (int channel);
+
+private:
+  /// Forbid copy constructor.
+  WH_Square (const WH_Square&);
+
+  /// Forbid assignment.
+  WH_Square& operator= (const WH_Square&);
+
+
+  /// Pointer to the array of input DataHolders.
+  DH_IntArray** itsInHolders;
+  /// Pointer to the array of output DataHolders.
+  DH_IntArray** itsOutHolders;
+
+  /// Length of DH_IntArray buffers.
+  int itsBufLength;
+  /// Is this the first WorkHolder in the simulation chain?
+  bool itsFirst;
+};
+
+
+#endif
