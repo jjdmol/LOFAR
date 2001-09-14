3f0f56f4af4aa738669f3de380674a201a7e68d3
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2001-09-14 11:56:34 +0000

    initial

---
+++ trunk/CEP/Demo/Platform/RadioDemo/src/sound-recorder/src/play-sample.h
@@ -0,0 +1,26 @@
+// Sound recorder 0.06 (Build on Aug 06 2000), GPL 2 (see COPYRIGHTS)
+// 1997, 1998  B. Warmerdam
+// $Id$
+
+#ifndef _playsample_h
+#define _playsample_h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <sys/soundcard.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include "dsp.h"
+#include "soundrec.h"
+#include "waveriff.h"
+#include "pcm.h"
+#include "cdr.h"
+#include "filefmt.h"
+#include "rcfile.h"
+
+#endif
