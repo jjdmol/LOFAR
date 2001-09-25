4aefa951013eff20d3890d8f86d7dc1536862866
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2001-09-25 13:43:21 +0000

    get multiple samples from the same wav file

--- CEP/Demo/Platform/RadioDemo/src/RadioSim/WH_WAV.cc
+++ CEP/Demo/Platform/RadioDemo/src/RadioSim/WH_WAV.cc
@@ -45,28 +45,35 @@ WH_WAV::WH_WAV (int inputs,int outputs, int channel):
 
 WH_WAV::~WH_WAV () 
 {
+  fclose(itsInfile);
 }
 
 void WH_WAV::takeSamples(){
+    cout << "WH_WAV::takeSamples;  channel = " << itsChannel << endl;
+    unsigned short in;
 
-  char ptr[10];
-  int i, sample=0;
-  unsigned short currentvalue, in , left, right;
-  char ready_name[256];
-  struct stat ready_stat;
-
-  if (itsChannel == 0) { 
+    system ("rm -f ./WAVE/antenna1.wav");
+    //system ("rm -f ./WAVE/antenna1.wav.ready");
+    if (itsChannel == 0) { 
       // record 1 second of sound into WAV file
       system("rsh astron5 ~/RadioDemo/record_sample.csh antenna1");
   }
   
-  cout << "wait for ready file" << endl;
+  // try to close file
+  if (itsInfile != NULL) {
+      fclose(itsInfile);
+  }
+
+  struct stat stat_result;
+    
+  cout << "wait for wav file" << endl;
   do {
       errno = 0;
-      (void)stat("./WAVE/antenna1.wav.ready", &ready_stat);
-      (void)stat("./WAVE/antenna1.wav", &ready_stat);
+      //(void)stat("./WAVE/antenna1.wav.ready", &stat_result);
+      (void)stat("./WAVE/antenna1.wav", &stat_result);
   } while (errno == ENOENT);
-  
+  system("touch ./WAVE/antenna1.wav.ready");
+
   cout << "open WAV file " << endl;
   itsInfile = fopen("./WAVE/antenna1.wav","r");
   if (itsInfile == NULL) {
@@ -74,61 +81,73 @@ void WH_WAV::takeSamples(){
     return;
   }
   /* skip header (or about that size...)*/
-  for (i=0; i< 0x2c; i++) {
+  for (int i=0; i< 0x2c; i++) {
     fread(&in,sizeof(in),1,itsInfile);
   }
 
-  // skip one sample; i.e. go to the second inpu channel of the stereo WAV file
+  // skip one sample; i.e. go to the second input channel of the stereo WAV file
   if (itsChannel == 0) fread(&in,sizeof(in),1,itsInfile);
-
-  while (   fread(&in,sizeof(in),1,itsInfile)
-	 && sample++ < 2*ANTSAMPLES) {
-    float currentvalue=0.;
-      
-#ifdef SWAPWAVVALUES
-      /* swap first 2 bytes with last 2 bytes in the word */
-      left = in << 8;
-      right= in >> 8;
-      currentvalue = left | right;
-#endif
-      currentvalue = in;
-      itsSampleBuffer[sample] = currentvalue;
-    // skip the next sample since that is the other channel;
-    fread(&in,sizeof(in),1,itsInfile);
-  }
-
-//    for (int i=0; i<10; i++) {
-//      cout << "Output [" << i << "]"
-//  	 << " = " << itsSampleBuffer[i] 
-//  	 << endl;
-//    }
-//    cout << "*************************************" << endl;
-
-  itsOffset = 0;
-  fclose(itsInfile);
+  
 }
 
+
 void WH_WAV::process () {
-  if (WorkHolder::getProcMode() == Process) {
 
-//      if (itsLags == 0) {
-//        itsOffset++;      
-//      }
 
-    for (int i=0; i<ANTSAMPLES; i++) {
-      itsOutDataHolders[0]->getBuffer()[i] = itsSampleBuffer[i+itsOffset];
+    char ptr[10];
+    int i, sample=0;
+    unsigned short in, tmp , left, right;
+    
+    if (WorkHolder::getProcMode() == Process) {
+	
+	
+	
+	bool readSamples = false;
+	while (!readSamples) {
+	    if (itsInfile == NULL) {
+		takeSamples();
+	    }
+	    int i=0;
+	    while (   i<ANTSAMPLES 
+		      && fread(&in,sizeof(in),1,itsInfile)) {
+		i++;
+#ifdef SWAPWAVVALUES
+		/* swap first 2 bytes with last 2 bytes in the word */
+		left = in << 8;
+		right= in >> 8;
+		tmp  = left | right;
+		in = tmp;
+#endif
+		itsOutDataHolders[0]->getBuffer()[i] = in;
+		
+		// skip the next sample since that is the other channel;
+		if (!fread(&in,sizeof(in),1,itsInfile)){
+		    cout << "Could not skip channel" << endl;
+		}
+	    }
+	    if (i < ANTSAMPLES) {
+		readSamples = false;
+		takeSamples();
+	    } else {
+		readSamples = true;
+	    }
+	}
+      
+//  	    for (int i=0; i<10; i++) {
+//  	     cout << "Output [" << i << "]"
+//  	  	 << " = " << itsOutDataHolders[0]->getBuffer()[i] 
+//  	  	 << endl;
+//  	    }
+//  	    cout << "*************************************" << endl;
+      
     }
-
-
-    //ANTSAMPLES
-   
     //cout << "WH_WAV::Process " << endl;
     itsTimeStamp++;
     for (int output = 0; output < getOutputs (); output++)
-      {
-	itsOutDataHolders[output]->getPacket()->timeStamp = itsTimeStamp;
-      }
-  }   
+	{
+	    itsOutDataHolders[output]->getPacket()->timeStamp = itsTimeStamp;
+	}   
+    if (itsChannel == 0) system("touch ./WAVE/antenna1.wav.ready");
 }
 
 void WH_WAV::dump () const {
