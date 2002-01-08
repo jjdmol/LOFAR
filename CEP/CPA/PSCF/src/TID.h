221874565462ff0a3a7a930709cbf38be110fe87
Oleg Smirnov <o-smirnov@users.noreply.github.com>
2002-01-08 11:25:06 +0000

    *** empty log message ***

---
+++ CEP/CPA/PSCF/src/TID.h
@@ -0,0 +1,22 @@
+#ifndef TID_h
+#define TID_h 1
+
+// This file is generated automatically -- do not edit
+#include "AtomicID.h"
+
+const AtomicID Tpchar(10);
+const AtomicID Tpdouble(19);
+const AtomicID Tpfloat(18);
+const AtomicID Tpint(14);
+const AtomicID Tpldouble(20);
+const AtomicID Tplong(16);
+const AtomicID Tpmytype(22);
+const AtomicID Tpshort(12);
+const AtomicID Tpstring(21);
+const AtomicID Tpuchar(11);
+const AtomicID Tpuint(15);
+const AtomicID Tpulong(17);
+const AtomicID Tpushort(13);
+
+
+#endif
