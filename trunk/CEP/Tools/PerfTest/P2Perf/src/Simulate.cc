280e43e9ed75d0a76944c64cfcd9ffaf9cc8a671
wierenga <wierenga@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-05-13 11:04:29 +0000

    Initialize MPI before trying to look at the command line options. In MPICH for processes with rank != 0, argc and argv are received from the process with rank 0 and so are not set properly until after a call to MPI_Init.

--- trunk/CEP/Tools/PerfTest/P2Perf/src/Simulate.cc
+++ trunk/CEP/Tools/PerfTest/P2Perf/src/Simulate.cc
@@ -1,8 +1,15 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "BaseSim/SimulatorParseClass.h"
 #include "BaseSim/ShMem/shmem_alloc.h"
 #include <Common/lofar_iostream.h>
 #include <Common/Debug.h>
 #include "P2Perf/P2Perf.h"
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
 
 #ifdef HAVE_CORBA
 int atexit(void (*function)(void))
@@ -14,6 +21,11 @@ int atexit(void (*function)(void))
 int main (int argc, char** argv)
 {
   // Set trace level.
+
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
   Debug::initLevels (argc, (const char* [])argv);
 
   try {
@@ -39,7 +51,6 @@ int main (int argc, char** argv)
 	cout << "It was a pleasure working with you!" << endl << endl;
 
 #else
-	shmem_debug();
 	simulator.baseDefine();
 	simulator.baseRun(3651);
 	simulator.baseDump();
