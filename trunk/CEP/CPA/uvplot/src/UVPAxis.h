d3dce4e4a5ec3fcad17f46afa1f4304b5762380c
brentjens <brentjens@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-05-24 14:31:33 +0000

    %[BugId: 15]%
    UVPAxis.cc: UVPAxis::calcTransferFunction: added assertion: worldMin !=
    worldMax.
    
    UVPAxis.h: Added comments for class UVPAxis, constructor,
    setTransferFunction, calcTransferFunction, worldToAxis, axisToWorld.

--- trunk/CEP/CPA/uvplot/src/UVPAxis.h
+++ trunk/CEP/CPA/uvplot/src/UVPAxis.h
@@ -10,35 +10,45 @@
 
 
 
-// Manages the properties of a coordinate axis of a dataset.
+//! Manages the properties of a coordinate axis of a dataset.
+/*!  The UVPAxis class is used to scale and shift coordinates. One of
+  its applications is the mapping of a value in the real world to a
+  value in the pixel or intensity domain. For example for making graphs
+  of data.
+ */
 class UVPAxis
 {
  public:
 
-  // Constructor. scale must be > 0.0, offset may be any value, type
-  // indicates what is plotted along the axis and unit the units
-  // used.
-              UVPAxis(double             scale  = 1.0,
-                      double             offset = 0.0,
-                      const std::string& type="",
-                      const std::string& unit="");
+  //! Constructor.
+  /*! \param scale may not be 0.0, \param offset may have any value,
+    \param type indicates what is plotted along the axis and \param
+    unit the units used. */
 
-  // scale may not be 0.0
+   UVPAxis(double scale = 1.0, double offset = 0.0, const std::string&
+           type="", const std::string& unit="");
+
+  //! \param scale may not be 0.0, \param offset may have any value.
   void        setTransferFunction(double scale,
                                   double offset);
 
-  // axisMin and axisMax may be pixel coordinates on a screen. This
-  // method calculates itsScale and itsOffset from the provided
-  // ranges.
+
+  //! Calculates scale and offset of the transfer function from the
+  //! provided world- and axis-ranges.
+
+  /*! \param axisMin and \paramn axisMax may be pixel coordinates on a
+      screen. This method calculates itsScale and itsOffset from the
+      provided ranges. worldMin may not be equal to worldMax. axisMin
+      may not be equal to axisMax.*/
   void        calcTransferFunction(double worldMin, 
                                    double worldMax,
                                    double axisMin,
                                    double axisMax);
 
-  // axis = (world - itsOffset)/itsScale
+  //! axis = (world - itsOffset)/itsScale
   inline double      worldToAxis(double world) const;
 
-  // world = itsOffset + itsScale*axis;
+  //! world = itsOffset + itsScale*axis;
   inline double      axisToWorld(double axis) const;
 
   
