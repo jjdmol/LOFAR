13a6ae5889f6c14ca6198c55402e805dbc970dba
Michiel Brentjens <brentjens@users.noreply.github.com>
2002-05-27 08:22:21 +0000

    %[BugId: 16]%
    Finally started implementation of UVPDataAtom class. Added and
    implemented the methods:
    
                    UVPDataAtom(int    numberOfChannels = 0,
                                double time            = 0);
    
    void            setChannels(unsigned int numberOfChannels);
    
    void            setData(unsigned int     channel,
                            const double_complex& data);
    
    void            setData(const std::vector<double_complex>& data);
    
    unsigned int    getNumberOfChannels() const;
    
    const double_complex *getData(unsigned int channel) const;

--- CEP/CPA/uvplot/src/UVPDataAtom.h
+++ CEP/CPA/uvplot/src/UVPDataAtom.h
@@ -5,13 +5,35 @@
 #if !defined(UVPDATAATOM_H)
 #define UVPDATAATOM_H
 
+
+#include <vector>
+#include <complex>
+
 class UVPDataAtom
 {
  public:
   
-  UVPDataAtom();
+  UVPDataAtom(int    numberOfChannels = 0,
+              double time            = 0);
+
+  // Sets a new number of channels. Destroys previous contents of
+  // itsData.
+  void            setChannels(unsigned int numberOfChannels);
+  
+  void            setData(unsigned int     channel,
+                          const double_complex& data);
+  
+  void            setData(const std::vector<double_complex>& data);
+
+  unsigned int getNumberOfChannels() const;
+
+  const double_complex *getData(unsigned int channel) const;
+
  protected:
  private:
+  
+  std::vector<double_complex> itsData;
+  double                   itsTime;
 };
 
 #endif // UVPDATAATOM_H
