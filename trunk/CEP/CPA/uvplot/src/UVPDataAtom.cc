9ca5e50d3f91700d30ee47e3af2da88f93341304
brentjens <brentjens@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-05-27 08:22:21 +0000

    %[BugId: 16]%
    Finally started implementation of UVPDataAtom class. Added and
    implemented the methods:
    
                    UVPDataAtom(int    numberOfChannels = 0,
                                double time            = 0);
    
    void            setChannels(unsigned int numberOfChannels);
    
    void            setData(unsigned int     channel,
                            const double_complex& data);
    
    void            setData(const std::vector<double_complex>& data);
    
    unsigned int    getNumberOfChannels() const;
    
    const double_complex *getData(unsigned int channel) const;

--- trunk/CEP/CPA/uvplot/src/UVPDataAtom.cc
+++ trunk/CEP/CPA/uvplot/src/UVPDataAtom.cc
@@ -5,8 +5,81 @@
 #include <UVPDataAtom.h>
 
 
+#if(DEBUG_MODE)
+#include <cassert>
+#endif
+
+
+
 //====================>>>  UVPDataAtom::UVPDataAtom  <<<====================
 
-UVPDataAtom::UVPDataAtom()
+UVPDataAtom::UVPDataAtom(int    numberOfChannels,
+                         double time)
+  : itsData(numberOfChannels),
+    itsTime(time)    
 {
+}
+
+
+
+
+
+//====================>>>  UVPDataAtom::setChannels  <<<====================
+
+void UVPDataAtom::setChannels(unsigned int numberOfChannels)
+{
+  itsData = std::vector<double_complex>(numberOfChannels);
+}
+
+
+
+
+
+//====================>>>  UVPDataAtom::setData  <<<====================
+
+void UVPDataAtom::setData(unsigned int channel,
+                          const double_complex& data)
+{
+  itsData[channel] = data;
+}
+
+
+
+
+
+
+//====================>>>  UVPDataAtom::setData  <<<====================
+
+void UVPDataAtom::setData(const std::vector<double_complex>& data)
+{
+#if(DEBUG_MODE)
+  assert(data.size() == itsData.size());
+#endif
+
+  itsData = data;
+}
+
+
+
+
+
+//=================>>>  UVPDataAtom::getNumberOfChannels  <<<=================
+
+unsigned int  UVPDataAtom::getNumberOfChannels() const
+{
+  return itsData.size();
+}
+
+
+
+
+
+//====================>>>  UVPDataAtom::getData  <<<====================
+
+const double_complex * UVPDataAtom::getData(unsigned int channel) const
+{
+#if(DEBUG_MODE)
+  assert(channel < itsData.size());
+#endif
+  return &(itsData[channel]);
 }
