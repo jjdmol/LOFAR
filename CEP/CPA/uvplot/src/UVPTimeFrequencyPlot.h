ef4351bfd93ada708bc9df7b875afc865690d32a
Michiel Brentjens <brentjens@users.noreply.github.com>
2002-06-07 14:18:07 +0000

    %[BugId: 15]%
    Added doxygen documentation.

--- CEP/CPA/uvplot/src/UVPTimeFrequencyPlot.h
+++ CEP/CPA/uvplot/src/UVPTimeFrequencyPlot.h
@@ -15,6 +15,10 @@
 #include <Common/Debug.h>
 #endif
 
+//! Plots a 2D image representation of spectrum-time data.
+/*! Time runs from top to bottom, frequency from left to right
+ */
+
 class UVPTimeFrequencyPlot: public UVPDisplayArea
 {
   Q_OBJECT
@@ -24,18 +28,33 @@ class UVPTimeFrequencyPlot: public UVPDisplayArea
 #endif
     
  public:
-  
+  //! Constructor
                  UVPTimeFrequencyPlot(QWidget *parent);
   
 
+  //! Performs the actual drawing.
   virtual void drawView();
-  
+
+  //! Sets the number of frequency channels.
+  /*! setChannels destroys any already present data. Also resets
+      itsValueAxis to the default, UVPAxis().
+
+      \param numberOfFrequencyChannels is the new number of frequency
+      channels. 
+   */
   void         setChannels(unsigned int numberOfChannels);
 
  public slots:
   
-  // Add a spectrum to itsSpectrum.
+   //! Add a spectrum to itsSpectrum.
+   /*! Adds spectrum to itsSpectrum. The transferfunction of
+       itsValueAxis is recalculated.
+       \param spectrum must have the same number of channels as defined
+       by setChannels().
+   */
   void slot_addSpectrum(const UVPSpectrum &spectrum);
+ 
+ //! Redraws the image. Simply calls drawView().
   void slot_paletteChanged();
   
   
