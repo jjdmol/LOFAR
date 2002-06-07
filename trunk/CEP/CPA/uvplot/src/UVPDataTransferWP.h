33113f64f84aa62f08af6bfd9751d5dbcb154f8c
brentjens <brentjens@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-06-07 13:58:46 +0000

    %[BugId: 15]%
    Converted documentation in UVPDataTransferWP.h
    
    set HAVE_DOT to YES in doxygen.conf

--- trunk/CEP/CPA/uvplot/src/UVPDataTransferWP.h
+++ trunk/CEP/CPA/uvplot/src/UVPDataTransferWP.h
@@ -27,7 +27,7 @@ static int AID_DUMMY_INITIALISATION = aidRegistry_uvplot();
 //*********** End of AID mechanism declarations
 
 
-
+//! The WorkProcess that receives visibility data and stores it locally
 class UVPDataTransferWP: public WorkProcess
 {
 #if(DEBUG_MODE)
@@ -35,28 +35,45 @@ class UVPDataTransferWP: public WorkProcess
 #endif
  public:
 
-  // Default constructor.
+  //! Constructor.
+  /*!
+    \param correlation The correlationtype that must be stored
+    \param baseline    The IFR number that must be stored. IFR =
+    ant1*(ant1+1)/2 + ant2.
+    \param patchID     Number of the patch.
+   */
   UVPDataTransferWP(int correlation,
                     int baseline,
                     int patchID);
 
-  // Overridden from WPInterface, the parent class of WorkProcess. These
-  // functions are called by the base class.
-  // In init, one has to subscribe to the messages one wants to receive
+  //!Initializes communication. Sets up the subscriptions. 
+  /*!Overridden from WPInterface, the parent class of WorkProcess. These
+     functions are called by the base class.
+     In init, one has to subscribe to the messages one wants to receive
+  */
   virtual void init();
   
-  // I have no idea what would be a could implementation of start. For
-  // the time being it just calls its baseclass::start() function.
+  //! Calls WorkProcess::start()
+  /*! I have no idea what would be a could implementation of start. For
+      the time being it just calls its baseclass::start() function.
+  */
   virtual bool start();
   
-  // receive actually receives the messages, translates them to
-  // UVPDataAtom objects and stores them in itsCachedData.
+
+  //! Actually receives the messages.
+  /*! It translates them to UVPDataAtom objects and stores them in
+      itsCachedData.
+  */
   virtual int receive(MessageRef &messageRef);
 
-  // Returns the number of rows received up to now.
+  //! Returns the number of rows received up to now.
   unsigned int size() const;
 
-  // Returns a const pointer to row rowIndex. rowIndex is zero based
+  //! Returns a const pointer to a row.
+  /*! \param rowIndex is the index of the row that you are interested
+    in. One should make sure yourself that rowIndex < size(). rowIndex
+    is zero based.
+  */
   const UVPDataAtom *getRow(unsigned int rowIndex) const;
 
 
