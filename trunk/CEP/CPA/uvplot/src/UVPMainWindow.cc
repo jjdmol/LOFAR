8b87006652bfc5fed90ad38eedcfc7e1a926d97c
brentjens <brentjens@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-06-20 14:55:37 +0000

    %[BugId: 32]%
    Wrote input method for reading measurementset
    
    Adedd slots slot_openFile and slot_readMeasurementSet
    
    New datamembers UVPDataSet itsDataSet and std::vector<
    std::vector<UVPDataAtom> > itsTestMS. The former is meant to test IO
    via OCTOPUSSY, the latter to test MS reading.
    
    Added itsGraphSettingWidget, a GUI element to modify graph parameters.
    itsCanvas and itsGraphSettingWidget are managed by a horizontal layout
    manager.
    
    Practically rewrote slot_plotTimeFrequencyImage().

--- trunk/CEP/CPA/uvplot/src/UVPMainWindow.cc
+++ trunk/CEP/CPA/uvplot/src/UVPMainWindow.cc
@@ -10,8 +10,20 @@
 
 #include <qapplication.h>       // qApp
 #include <qmessagebox.h>
+#include <qfiledialog.h>
+#include <qlayout.h>
 
 #include <sstream>              // std::ostringstream
+
+#include <aips/aips.h>
+#include <aips/MeasurementSets/MeasurementSet.h>
+#include <aips/MeasurementSets/MSMainColumns.h>
+#include <aips/MeasurementSets/MSPolColumns.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Tables/ExprNode.h>
+#include <aips/Tables/TableColumn.h>
+#include <trial/MeasurementEquations/VisibilityIterator.h>
+#include <trial/MeasurementEquations/VisBuffer.h>
 
 
 #if(DEBUG_MODE)
@@ -23,8 +35,10 @@ InitDebugContext(UVPMainWindow, "DEBUG_CONTEXT");
 UVPMainWindow::UVPMainWindow()
   : QMainWindow()
 {
+  //  itsDataSet = new UVPDataSet;
+
   itsFileMenu = new QPopupMenu;
-  itsFileMenu->insertItem("&Open", mc_file_open);
+  itsFileMenu->insertItem("&Open", this, SLOT(slot_openFile()));
   itsFileMenu->insertItem("&Quit", qApp, SLOT(quit()));
 
   itsPlotMenu = new QPopupMenu;
@@ -58,15 +72,24 @@ UVPMainWindow::UVPMainWindow()
   itsStatusBar->addWidget(itsProgressBar, 5, true);
   
   itsStatusBar->show();
-
   itsBusyPlotting = false;
 
 #if(DEBUG_MODE)
   TRACER1("itsCanvas = new UVPTimeFrequencyPlot(this, Channels);");
 #endif
-  itsCanvas = new UVPTimeFrequencyPlot(this);
+  QWidget  *CentralWidget = new QWidget(this);
+  setCentralWidget(CentralWidget);
+
+  QHBoxLayout *hLayout = new QHBoxLayout(CentralWidget);
+
+  itsCanvas              = new UVPTimeFrequencyPlot(CentralWidget);
   itsCanvas->drawView();
-  
+  itsGraphSettingsWidget = new UVPGraphSettingsWidget(CentralWidget);
+
+  hLayout->addWidget(itsCanvas, 10);
+  hLayout->addWidget(itsGraphSettingsWidget, 1);
+
+  CentralWidget->show();
   connect(itsCanvas, SIGNAL(signal_mouseWorldPosChanged(double, double)),
           this, SLOT(slot_mouse_world_pos(double, double)));
 
@@ -82,6 +105,7 @@ UVPMainWindow::UVPMainWindow()
 
 UVPMainWindow::~UVPMainWindow()
 {
+  //  delete itsDataSet;
 }
 
 
@@ -92,7 +116,7 @@ UVPMainWindow::~UVPMainWindow()
 
 void UVPMainWindow::resizeEvent(QResizeEvent */*event*/)
 {
-  itsCanvas->setGeometry(0, itsMenuBar->height(), width(), height()-itsMenuBar->height()-itsStatusBar->height());
+  //  itsCanvas->setGeometry(0, 0/*itsMenuBar->height()*/, width(), height() /*-itsMenuBar->height()*/-itsStatusBar->height());
 }
 
 
@@ -166,12 +190,10 @@ void UVPMainWindow::slot_plotTimeFrequencyImage()
     Dispatcher    dispatcher;     // Octopussy Message Dispatcher
 
     // Octopussy
-    const int corr     = 5;
-    const int baseline = 6;
-    const int patch    = 0;
-    
+    unsigned int patch = 0;
+
     // Anonymous counted reference. No need to delete.
-    UVPDataTransferWP *transfer = new UVPDataTransferWP(corr, baseline, patch);
+    UVPDataTransferWP *transfer = new UVPDataTransferWP(patch, &itsDataSet);
     dispatcher.attach(transfer, DMI::ANON);
     //    dispatcher.attach(new MSIntegratorWP("test.ms", 2, 1, 1), DMI::ANON);
     initGateways(dispatcher);     // Octopussy     ^^^ Channels to average
@@ -182,35 +204,64 @@ void UVPMainWindow::slot_plotTimeFrequencyImage()
     
     itsBusyPlotting = true;
 
-    itsCanvas->setChannels(128); // Number of channels
-    
     unsigned int spectraAdded(0);
+    unsigned int previousSize = 0;
+
+    UVPDataAtomHeader FromHeader;
+    UVPDataAtomHeader ToHeader;
+
+    FromHeader.itsAntenna1 = 0;
+    FromHeader.itsAntenna2 = 6;
+    
+    ToHeader.itsAntenna1 = 0;
+    ToHeader.itsAntenna2 = 7;
+      
     while(itsBusyPlotting) {
-      dispatcher.poll();
+      dispatcher.poll(50);
       qApp->processEvents();
 
       bool doDraw(false);
       
       // First experiment to see how fast I can paint...
 
-      if(transfer->size() > spectraAdded) {doDraw = true;};
-
-      for(unsigned int i = spectraAdded; i < transfer->size(); i++) {
-        const   UVPDataAtom *dataAtom = transfer->getRow(i);
-        double *Values = new double[dataAtom->getNumberOfChannels()];
-        for(unsigned int j = 0; j < dataAtom->getNumberOfChannels(); j++) {
-          Values[j] = sqrt(dataAtom->getData(j)->real()*dataAtom->getData(j)->real() +
-                           dataAtom->getData(j)->imag()*dataAtom->getData(j)->imag());
-        }
-        UVPSpectrum   Spectrum(dataAtom->getNumberOfChannels(), i, Values);
-        itsCanvas->slot_addSpectrum(Spectrum);
-        spectraAdded++;
-        delete[] Values;
+      if(itsDataSet.size() > previousSize) {
+#if(DEBUG_MODE)
+        TRACER1("ItsDataSet.size()" << itsDataSet.size());
+#endif
+        doDraw = true;
+        previousSize = itsDataSet.size();
       }
+
       if(doDraw) {
+        itsCanvas->setChannels(itsDataSet.begin()->second.getNumberOfChannels()); // Number of channels. Clears buffer.
+        spectraAdded = 0;
+        for(UVPDataSet::iterator p = itsDataSet.upper_bound(FromHeader);
+            p != itsDataSet.upper_bound(ToHeader) && p != itsDataSet.end();
+            p++) {
+          const   UVPDataAtom *dataAtom = &(p->second);
+#if(DEBUG_MODE)
+          TRACER2( "Ant1: " <<  dataAtom->getHeader().itsAntenna1);
+          TRACER2( "Ant2: " <<  dataAtom->getHeader().itsAntenna2);
+          cout.precision(10);
+          TRACER2( "Time: " <<  dataAtom->getHeader().itsTime); 
+          cout.precision(6);
+          TRACER2( "Corr: " <<  dataAtom->getHeader().itsCorrelationType);
+#endif
+          double *Values = new double[dataAtom->getNumberOfChannels()];
+          for(unsigned int j = 0; j < dataAtom->getNumberOfChannels(); j++) {
+            Values[j] = std::abs(*dataAtom->getData(j));
+          }
+          UVPSpectrum   Spectrum(dataAtom->getNumberOfChannels(), spectraAdded, Values);
+          if(dataAtom->getHeader().itsCorrelationType == UVPDataAtomHeader::RR) {
+            itsCanvas->slot_addSpectrum(Spectrum);
+            spectraAdded++;
+          }
+          delete[] Values;
+        }        
         itsCanvas->drawView();
       }
-    }
+      
+    } // while
     
 #if(ARTIFICIAL_IMAGE_ASDJKNF)
     for(unsigned int t = 0; t < 600; t++) {
@@ -242,4 +293,116 @@ void UVPMainWindow::slot_plotTimeFrequencyImage()
 void UVPMainWindow::slot_quitPlotting()
 {
   itsBusyPlotting = false;
+}
+
+
+
+
+//==================>>>  UVPMainWindow::slot_openFile  <<<==================
+
+void UVPMainWindow::slot_openFile()
+{
+  QString filename = QFileDialog::getExistingDirectory(".",
+                                                       this, 
+                                                       "", 
+                                                       "Open Measurement Set");
+  if(!filename.isNull()) {
+    slot_readMeasurementSet(filename.latin1());
+  }
+}
+
+
+
+
+//==============>>>  UVPMainWindow::slot_readMeasurementSet  <<<==============
+
+void UVPMainWindow::slot_readMeasurementSet(const std::string& msName)
+{
+  MeasurementSet ms(msName);
+  MSAntenna      AntennaTable(ms.antenna());
+  MSField        FieldTable(ms.field());
+  
+  std::cout << "=========>>> Table thing  <<<=========" << std::endl;
+  Table        msTable(msName);
+  Table        Selection(msTable(msTable.col("ANTENNA1") == 0 && msTable.col("ANTENNA2") == 6));
+
+  ROArrayColumn<Complex> DataColumn(Selection, "DATA");
+  //  ROScalarColumn<Float>  TimeColumn(Selection, "TIME");
+
+  unsigned int NumRows          = ms.nrow();
+  unsigned int NumAntennae      = AntennaTable.nrow();
+  unsigned int NumBaselines     = NumAntennae*(NumAntennae-1)/2;
+  unsigned int NumPolarizations = DataColumn(0).shape()[0];
+  unsigned int NumChannels      = DataColumn(0).shape()[1];
+  unsigned int NumTimeslots     = Selection.nrow();
+
+  itsCanvas->setChannels(NumChannels);
+  slot_setProgressTotalSteps(NumTimeslots);
+  for(unsigned int i = 0; i < NumTimeslots; i++) {
+    double*     Values = new double[NumChannels];
+    IPosition   Pos(2, 0);
+    for(unsigned int j = 0; j < NumChannels; j++) {
+      Pos[0] = 0;
+      Pos[1] = j;
+      Complex     Visibility0 = DataColumn(i)(Pos);
+      Pos[0] = 1;
+      Complex     Visibility1 = DataColumn(i)(Pos);
+      Values[j] = fabs(Visibility0-Visibility1);
+    }
+    UVPSpectrum   Spectrum(NumChannels, i, Values);
+    itsCanvas->slot_addSpectrum(Spectrum);
+    delete[] Values;
+    slot_setProgress(i+1);
+    if(i % 3 == 0) {
+      itsCanvas->drawView();
+    }
+  }
+  itsCanvas->drawView();
+  
+
+  std::cout << DataColumn(0).shape() << std::endl;
+  std::cout << "=========>>> Table thing  <<<=========" << std::endl << std::flush;
+
+  
+#if(DEBUG_MODE)
+  TRACER1("Selection.nrow();" <<   Selection.nrow());
+  TRACER1("NumRows     : " << NumRows);
+  TRACER1("NumBaselines: " << NumBaselines);
+  TRACER1("NumTimeslots: " << NumTimeslots << std::flush);
+#endif
+
+  /*  slot_setProgressTotalSteps(NumRows);
+  unsigned int progress = 0;
+  //  Just the first 30 Baselines for now...
+  itsTestMS = std::vector<std::vector<UVPDataAtom> >(30);
+  for(Iterator.originChunks(); Iterator.moreChunks() && Continue; Iterator.nextChunk()) {
+    qApp->processEvents();
+    for(Iterator.origin(); Iterator.more(); Iterator++, progress++) { 
+      Vector<Int>    Ant1(VisibilityBuffer.antenna1());
+      Vector<Int>    Ant2(VisibilityBuffer.antenna2());
+      unsigned int   NRows(VisibilityBuffer.nRow());
+      Vector<Double> Time(VisibilityBuffer.time());
+      
+      for(unsigned int i = 0; i < NRows; i++, progress++) {
+        if(progress%3000 == 0) {
+          slot_setProgress(progress);
+        }
+        unsigned int A1       = Ant1[i];
+        unsigned int A2       = Ant2[i];
+        unsigned int Baseline = A2 + NumAntennae*A1 - A1*(A1+1)/2;
+        if(Baseline < itsTestMS.size() ) {
+          UVPDataAtom  Atom(NumChannels, Time[i], std::vector<double>(3, 0));
+          
+          itsTestMS[Baseline].push_back(Atom);
+        } else {
+          //0          Continue = false;
+        }
+
+      }
+
+     }
+  }
+  std::cout << "Number of Timeslots: " << itsTestMS[6].size() <<std::endl;
+  */
+  
 }
