891d7d65462ed72e38bf88548e170a69f03fa8a9
gerdes <gerdes@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-07-17 15:12:26 +0000

    %[BugId: 71]%
    Fixed the order of the convolution. Also made the code cleaner.

--- StationSim/src/WH_BandSep.cc
+++ StationSim/src/WH_BandSep.cc
@@ -46,36 +46,46 @@ WH_BandSep::WH_BandSep (const string& name,
   itsSubPos    (0),
   itsFiltPos   (0)
 {
-  if (nout > 0) {
+  if (nout > 0) 
+  {
     itsOutHolders = new DH_SampleC* [nout];
   }
+
   char str[8];
+  
   // Create the output DH-s.
-  for (unsigned int i=0; i<nout; i++) {
+  for (unsigned int i=0; i<nout; i++) 
+  {
     sprintf (str, "%d", i);
-    itsOutHolders[i] = new DH_SampleC (string("out_") + str, nrcu, nsubband);
+    itsOutHolders[i] = new DH_SampleC(string("out_") + str, nrcu, nsubband);
   }
+  osDebugInput.open ("debuginput.dat", ios::out);
+  iCount = 0;
 }
 
 WH_BandSep::~WH_BandSep()
 {
-  for (int i=0; i<getOutputs(); i++) {
+  // DEBUG
+  osDebugInput.close();
+
+  for (int i = 0; i < getOutputs(); i++) 
+  {
     delete itsOutHolders[i];
   }
   delete [] itsOutHolders;
 }
 
-WorkHolder* WH_BandSep::construct (const string& name, int ninput, int noutput,
+WorkHolder* WH_BandSep::construct(const string& name, int ninput, int noutput,
 				   const ParamBlock& params)
 {
-  Assert (ninput==1);
-  return new WH_BandSep (name, noutput,
-			 params.getInt ("nrcu", 10),
-			 params.getInt ("nsubband", 10),
-			 params.getString ("coeffname", "filter.coeff"));
+  Assert (ninput == 1);
+
+  return new WH_BandSep(name, noutput, params.getInt("nrcu", 10),
+			params.getInt("nsubband", 10), params.getString("coeffname", 
+			"filter.coeff"));
 }
 
-WH_BandSep* WH_BandSep::make (const string& name) const
+WH_BandSep* WH_BandSep::make(const string& name) const
 {
   return new WH_BandSep (name, getOutputs(), itsNrcu, itsNsubband,
 			 itsCoeffName);
@@ -84,42 +94,50 @@ WH_BandSep* WH_BandSep::make (const string& name) const
 void WH_BandSep::preprocess()
 {
   // If first time, read the filter coefficients and allocate the buffers.
-  if (itsConv.nelements() == 0) {
-    ifstream coeffFile (itsCoeffName.c_str());
-    AssertStr (coeffFile, "Coeff.file " << itsCoeffName << " not found");
-    RegularFile file(itsCoeffName);
-    RegularFileIO fio(file);
-    int size = fio.length();
-    int ncoeff = size / sizeof(double);
-    vector<double> coeff(ncoeff);
-    fio.read (size, &coeff[0]);
-    // Form and normalize the subfilter coefficient matrix.
-    AssertStr (ncoeff > 0  &&  ncoeff % itsNsubband == 0,
-	       "Incorrect number of filter coefficients given");
-    itsFilterLength = ncoeff / itsNsubband;
-    Matrix<double> filter(IPosition(2, itsFilterLength, itsNsubband),
+  if (itsConv.nelements() == 0) 
+  {
+     ifstream coeffFile(itsCoeffName.c_str());
+     AssertStr (coeffFile, "Coeff.file " << itsCoeffName << " not found");
+     RegularFile file(itsCoeffName);
+     RegularFileIO fio(file);
+     int size = fio.length();
+     int ncoeff = size / sizeof(double);
+     vector<double> coeff(ncoeff);
+     fio.read(size, &coeff[0]);
+
+     AssertStr (ncoeff > 0  &&  ncoeff % itsNsubband == 0,
+     	       "Incorrect number of filter coefficients given");
+    Matrix<double> filter(IPosition(2, itsNsubband, itsFilterLength),
 			  &coeff[0], SHARE);
-    filter /= max(filter);
-    cout << "Filter coefficients: " << endl;
-    cout << filter << endl;
+    //filter /= max(filter);
+    
+    itsFilterLength = ncoeff / itsNsubband;
+
     // Create for each subband the filter vector from the matrix.
-    itsFilters.resize (itsNsubband);
-    for (int i=0; i<itsNsubband; i++) {
+    itsFilters.resize(itsNsubband);
+
+    for (int i = 0; i < itsNsubband; i++) 
+    {
       vector<double>& filt = itsFilters[i];
-      filt.resize (itsFilterLength, 0);
-      for (int j=0; j<itsFilterLength; j++) {
-	filt[j] = filter(j,i);
+      filt.resize(itsFilterLength, 0);
+
+      for (int j = 0; j < itsFilterLength; j++) 
+      {
+	filt[j] = filter(i, j);
       }
     }
+
     // Size and initialize the buffers.
-    for (int i=0; i<itsNrcu; i++) {
-      itsBuffers.resize (itsNsubband);
-      itsBuffers[i].resize (itsFilterLength*itsNsubband, 0);
+    for (int i = 0; i < itsNrcu; i++) 
+    {
+      itsBuffers.resize(itsNsubband);
+      itsBuffers[i].resize(itsFilterLength * itsNsubband, 0);
     }
+
     // Create convolution and FFT buffers.
-    itsConv.resize (itsNsubband);
-    itsFFTBuf.resize ((itsNsubband+2)/2);
-    itsFFTserver.resize (IPosition(1, itsNsubband));
+    itsConv.resize(itsNsubband);
+    itsFFTBuf.resize((itsNsubband + 2) / 2);
+    itsFFTserver.resize(IPosition(1, itsNsubband));
   }
 }
 
@@ -128,60 +146,111 @@ void WH_BandSep::process()
   // Process receives each time a buffer of nrcu samples.
   // Every nsubband times it convolutes and ffts the buffered data
   // and outputs it.
-  if (getOutputs() > 0) {
+  if (getOutputs() > 0) 
+  {
     DH_SampleR::BufferType* bufin = itsInHolder.getBuffer();
+
     // Keep the received data per RCU in a buffer.
-    Assert (itsSubPos < itsNsubband);
-    Assert (itsFiltPos < itsFilterLength);
-    int pos = itsSubPos*itsFilterLength + itsFiltPos;
-    for (int i=0; i<itsNrcu; i++) {
+    Assert(itsSubPos < itsNsubband);
+    Assert(itsFiltPos < itsFilterLength);
+
+    int pos = itsSubPos * itsFilterLength + itsFiltPos;
+
+    for (int i = 0; i < itsNrcu; i++) 
+    {
       itsBuffers[i][pos] = bufin[i];
     }
     itsSubPos++;
+    
     // Calculate the filter output. This only needs to be done if in the
     // process of downsampling it is time to process a bunch of samples
     // (which is controlled by the rate of the output dataholder).
-    if (itsOutHolders[0]->doHandle()) {
-      Assert (itsSubPos == itsNsubband);
+    if (itsOutHolders[0]->doHandle()) 
+    {
+      Assert(itsSubPos == itsNsubband);
       DH_SampleC::BufferType* bufout = itsOutHolders[0]->getBuffer();
-      for (int i=0; i<itsNrcu; i++) {
-	for (int j=0; j<itsNsubband; j++) {
+      
+      osDebugInput << " Multiplication " << iCount++ << " :" << endl;
+      for (int i = 0; i < itsNrcu; i++) 
+      {
+	osDebugInput << endl << "next antenna: " <<  endl;
+	for (int j = 0; j < itsNsubband; j++) 
+	{
 	  double& conv = itsConv(j);
 	  conv = 0;
 	  int fp = 0;
-	  int pos = j*itsFilterLength + itsFiltPos;
+	  int pos = j * itsFilterLength + itsFiltPos;
+
+// 	  // Go from begin to end the data.
+// 	  for (int k = pos; k >= j * itsFilterLength; k--) 
+// 	  {
+// 	    conv += itsFilters[j][fp++] * itsBuffers[i][k];
+// 	    osDebugInput << itsFilters[j][fp - 1] << ' ' << itsBuffers[i][k] << ' ' << conv << endl;
+// 	  }
+// 	  for (int k = (j + 1) * itsFilterLength - 1; k > pos; k--) 
+// 	  {
+// 	    conv += itsFilters[j][fp++] * itsBuffers[i][k];
+// 	    osDebugInput << itsFilters[j][fp - 1] << ' ' << itsBuffers[i][k] << ' ' << conv << endl;
+// 	  }
+
 	  // Go from end to begin through the data.
-	  for (int k=pos; k>=j*itsFilterLength; k--) {
+	  for (int k = pos + 1; k < (j + 1) * itsFilterLength; k++) 
+	  {
 	    conv += itsFilters[j][fp++] * itsBuffers[i][k];
+	    osDebugInput << itsFilters[j][fp - 1] << ' ' << itsBuffers[i][k] << ' ' << conv << endl;
 	  }
-	  for (int k=(j+1)*itsFilterLength-1; k>pos; k--) {
+	  for (int k = j * itsFilterLength; k <= pos; k++) 
+	  {
 	    conv += itsFilters[j][fp++] * itsBuffers[i][k];
+	    osDebugInput << itsFilters[j][fp - 1] << ' ' << itsBuffers[i][k] << ' ' << conv << endl;
 	  }
+
+	  osDebugInput << endl;
 	  conv /= itsFilterLength;
 	}
-	itsFFTserver.fft0 (itsFFTBuf, itsConv, False);
+		
+	itsFFTserver.fft0(itsFFTBuf, itsConv, False);
 	itsFFTBuf *= DH_SampleC::BufferType(1./itsNsubband);
+
 	// The FFT output has (n+2)/2 points, where first point is the
 	// DC component and last point is nyquist sample (if n is even).
 	// Matlab returns all points, so put complex conjugate in remainder.
 	// Store the output with rcu as most rapidly varying axis.
+
 	DH_SampleC::BufferType* out = bufout + i;
-	for (int j=0; j<itsNsubband/2; j++) {
-	  out[j*itsNrcu] = itsFFTBuf(j);
-	}
-	for (int j=1; j<(itsNsubband-1)/2; j++) {
-	  out[(itsNsubband-j)*itsNrcu] = conj(itsFFTBuf(j));
-	}
+
+
+
+	// With FFT do:
+  	for (int j = 0; j < itsNsubband / 2; j++)
+  	{
+  	  out[j * itsNrcu] = itsFFTBuf(j);
+  	}
+
+  	for (int j = 1; j < (itsNsubband - 1) / 2; j++) 
+  	{
+  	  out[(itsNsubband - j) * itsNrcu] = conj(itsFFTBuf(j));
+  	}
+
+	// Just convolution do:
+//   	for (int j = 0; j < itsNsubband; j++)
+//    	{
+//    	  out[j * itsNrcu] =  itsConv(j);
+//    	}
       }
+
       // Copy to other output buffers.
-      for (int i=1; i<getOutputs(); i++) {
+      for (int i = 1; i < getOutputs(); i++) 
+      {
 	memcpy (getOutHolder(i)->getBuffer(), bufout,
-		itsNrcu *itsNsubband * sizeof(DH_SampleC::BufferType));
+		itsNrcu * itsNsubband * sizeof(DH_SampleC::BufferType));
       }
+
       // Move to next buffer position.
       // The buffer is circular, to restart if it is full.
       itsFiltPos++;
-      if (itsFiltPos == itsFilterLength) {
+      if (itsFiltPos == itsFilterLength) 
+      {
 	itsFiltPos = 0;
       }
       itsSubPos = 0;
@@ -192,27 +261,28 @@ void WH_BandSep::process()
 void WH_BandSep::dump() const
 {
   cout << "WH_BandSep " << getName() << " Buffer:" << endl;
-  if (getOutputs() > 0) {
+
+  if (getOutputs() > 0) 
+  {
     cout << itsOutHolders[0]->getBuffer()[0] << ','
-	 << itsOutHolders[0]->getBuffer()[itsNrcu*itsNsubband-1] << endl;
+	 << itsOutHolders[0]->getBuffer()[itsNrcu * itsNsubband - 1] << endl;
   }
 }
 
 
-DH_SampleR* WH_BandSep::getInHolder (int channel)
+DH_SampleR* WH_BandSep::getInHolder(int channel)
 {
-  AssertStr (channel == 0,
-	     "input channel too high");
+  AssertStr (channel == 0, "input channel too high");
+
   return &itsInHolder;
 }
-DH_SampleC* WH_BandSep::getOutHolder (int channel)
+
+DH_SampleC* WH_BandSep::getOutHolder(int channel)
 {
-  AssertStr (channel < getOutputs(),
-	     "output channel too high");
+  AssertStr (channel < getOutputs(), "output channel too high");
+
   return itsOutHolders[channel];
 }
-
-
 
 //# Instantiate the FFTServer.
 #include <aips/Mathematics/FFTServer.cc>
