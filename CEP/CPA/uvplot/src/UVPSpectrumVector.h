3caa6bc5e6fae5dea80aa68b29354bd10287fe59
Michiel Brentjens <brentjens@users.noreply.github.com>
2002-08-06 12:38:20 +0000

    %[BugId: 15]%
    Added documentation...

--- CEP/CPA/uvplot/src/UVPSpectrumVector.h
+++ CEP/CPA/uvplot/src/UVPSpectrumVector.h
@@ -12,6 +12,10 @@
 #include <Common/Debug.h>
 #endif
 
+//! List of UVPSpectrum objects that maintains min/max values
+/*! It is used in UVPTimeFrequencyPlot to represent a time-frequency
+  plane with real values.
+ */
 class UVPSpectrumVector: public std::vector<UVPSpectrum>
 {
 #if(DEBUG_MODE)
@@ -20,14 +24,26 @@ class UVPSpectrumVector: public std::vector<UVPSpectrum>
   
  public:
 
+  //! Constructor
+  /*! \param numberOfChannels specifies the number of channels that
+    every spectrum that is added should have.
+   */
   UVPSpectrumVector(unsigned int numberOfChannels=0);
   
   
+  //! Add a spectrum to the vector. Calculates min/max values.
+  /*! Make sure that spectrum has the same number of channels as was
+    set in the constructor. In order to use the min/max feature, one
+    is required to use this method instead of push_back.*/
   void add(const UVPSpectrum &spectrum);
 
+  //! \returns the minimum value of all data points.
   double min() const;
+  
+  //! \returns the maximum value of all data points.
   double max() const;
   
+  //! \returns the number of channels that was set in the constructor.
   unsigned int getNumberOfChannels() const;
   
 
@@ -35,9 +51,9 @@ class UVPSpectrumVector: public std::vector<UVPSpectrum>
  protected:
  private:
 
-  unsigned int     itsNumberOfChannels;
-  double itsMinValue;
-  double itsMaxValue;
+  unsigned int itsNumberOfChannels;
+  double       itsMinValue;
+  double       itsMaxValue;
 
 };
 
