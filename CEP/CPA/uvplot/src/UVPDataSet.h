420d2a2fa98c8697b260d67cdc8c632f1082c095
Michiel Brentjens <brentjens@users.noreply.github.com>
2002-08-23 09:54:40 +0000

    %[BugId: 32]%
    Removed unused typedefs.

--- CEP/CPA/uvplot/src/UVPDataSet.h
+++ CEP/CPA/uvplot/src/UVPDataSet.h
@@ -13,21 +13,6 @@
 //! Atom = TimeCache[timeslot]
 typedef std::vector<UVPDataAtom>          UVPTimeCache;
 
-//! Atom = BaselineCache[baseline][timeslot]
-typedef std::vector<UVPTimeCache>  UVPBaselineCache;
-
-
-//! Atom = CorrelationCache[correlation][baseline][timeslot]
-typedef std::vector<UVPBaselineCache>  UVPCorrelationCache;
-
-
-//! Atom = BaselineCache[field][correlation][baseline][timeslot]
-typedef std::vector<UVPCorrelationCache>  UVPFieldCache;
-
-
-//! Atom = BaselineCache[field][correlation][baseline][timeslot]
-//typedef UVPFieldCache     UVPDataSet;
-
 typedef std::map<UVPDataAtomHeader, UVPDataAtom> UVPDataSet;
 
 #endif // UVPDATASET_H
