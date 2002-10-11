07025d6eeacefa290b92c15cb66c1f2c9b31de8e
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-10-11 14:25:11 +0000

    %[BugId: 95]% Implemented quite some real functionality

--- trunk/CEP/CPA/PSS3/CAL/src/MeqCalibraterImpl.h
+++ trunk/CEP/CPA/PSS3/CAL/src/MeqCalibraterImpl.h
@@ -23,25 +23,24 @@
 #ifndef CAL_MEQCALIBRATER_H
 #define CAL_MEQCALIBRATER_H
 
+#include <aips/Arrays/Matrix.h>
+#include <aips/MeasurementSets/MSMainColumns.h>
 #include <aips/MeasurementSets/MeasurementSet.h>
+#include <aips/Quanta/MVBaseline.h>
 #include <aips/Tables/Table.h>
 #include <aips/Tables/TableIter.h>
 #include <aips/Utilities/String.h>
 #include <aips/aips.h>
-#include <trial/MeasurementEquations/VisBuffer.h>
-#include <trial/MeasurementEquations/VisibilityIterator.h>
 #include <trial/Tasking/ApplicationObject.h>
 
-#include <MNS/ParmTable.h>
 #include <MNS/MeqDomain.h>
-#include <MNS/MeqParm.h>
-#include <MNS/MeqStoredParmPolc.h>
+#include <MNS/MeqJonesExpr.h>
 #include <MNS/MeqMatrix.h>
+#include <MNS/MeqParm.h>
 #include <MNS/MeqRequest.h>
 #include <MNS/MeqStation.h>
-#include <MNS/MeqJonesExpr.h>
-#include <MNS/MeqUVW.h>
 #include <MNS/MeqUVWPolc.h>
+#include <MNS/ParmTable.h>
 #include <GSM/SkyModel.h>
 
 //
@@ -51,7 +50,7 @@
 class MeqCalibrater : public ApplicationObject
 {
 
- public:
+public:
   //
   // Create MeqCalibrater object for a specific
   // MeaurementSet, MEQ model (with associated MEP database) and skymodel
@@ -65,53 +64,51 @@ class MeqCalibrater : public ApplicationObject
   // destructor
   ~MeqCalibrater();
 
-  // set the time interval for which to solve
-  void setTimeIntervalSize(uInt secInterval);
+  // Set the time interval for which to solve.
+  void setTimeInterval (double secInterval);
 
-  // reset the time interval iterator
-  void resetTimeIterator();
+  // Reset the iterator.
+  void resetIterator();
 
-  // advance the time interval iterator
-  void nextTimeInterval();
+  // Advance the iterator.
+  bool nextInterval();
 
-  // make all parameters non-solvable
+  // Make all parameters non-solvable
   void clearSolvableParms();
 
-  // make specific parameters solvable (isSolvable = True) or non-solvable (False)
-  void setSolvableParms(Vector<String>& parmPatterns, Bool isSolvable);
+  // Make specific parameters solvable (isSolvable = True) or
+  // non-solvable (False)
+  void setSolvableParms (Vector<String>& parmPatterns, Bool isSolvable);
 
   // predict visibilities for the current domain
-  void predict(const String& modelColName);
+  void predict();
   
   // Solve for the current domain.
   // Return fit value to indicate fitness of the solution and updates the
   // parameters for which to solve.
   Double solve();
 
-  // Save modified parameters to the MEP database.
+  // Save solved parameters to the MEP database.
   void saveParms();
 
   // Save the predicted data to the named column of the MeasurementSet.
-  void savePredictedData(const String& dataColName);
+  void savePredictedData (const String& dataColName);
 
   // Save residual data in the named column (residualColName) by substracting
   // data in the first named column (colAName) from data in the second named
   // column (colBName).
-  void saveResidualData(const String& colAName, const String& colBName,
-			const String& residualColName);
+  void saveResidualData (const String& colAName, const String& colBName,
+			 const String& residualColName);
 
   // Get info about the parameters whose name matches one of the parameter
   // patterns in a GlishRecord, exclude parameters matching one of the
   // exclude pattterns.
-  GlishRecord getParms(Vector<String>& parmPatterns,
-		       Vector<String>& excludePatterns);
+  GlishRecord getParms (Vector<String>& parmPatterns,
+			Vector<String>& excludePatterns);
 
   // Get a description of the current solve domain, which changes
   // after each call to nextTimeIteration.
   GlishRecord getSolveDomain();
-
-  // Return True if iterator over time intervals is past the end
-  Bool timeIteratorPastEnd();
 
   // standard DO methods
   virtual String         className() const;
@@ -122,7 +119,7 @@ class MeqCalibrater : public ApplicationObject
 				 ParameterSet& inputRecord,
 				 Bool runMethod);
 
- private:
+private:
 
   // calculate the UVW polcs for all frequency domains per hour wide time domain
   void calcUVWPolc();
@@ -130,29 +127,53 @@ class MeqCalibrater : public ApplicationObject
   MeqCalibrater& operator=(const MeqCalibrater& other); // no assignment operator
 
   // initialize all parameters in the MeqExpr tree for the current domain
-  void initParms(MeqDomain& domain);
+  void initParms    (const MeqDomain& domain);
+
+  // Get the phase reference position of the first field.
+  void getPhaseRef  ();
+
+  // Get the frequncy info of the given data desc (spectral window).
+  void getFreq      (int ddid);
+
+  // Get the station info (position and name).
+  void fillStations ();
+
+  // Get all baseline info.
+  void fillBaselines(const Vector<int>& ant1, const Vector<int>& ant2);
+
+  // Create the expressions for each baseline.
+  void makeWSRTExpr ();
+
+  // Calculate the UVW polynomial coefficients.
+  void calcUVWPolc  (const Table& ms);
 
   //
   // variables
   //
-  MeasurementSet     itsMS;
-  ParmTable          itsMEP;
-  Table              itsGSMTable;
-  GSM::SkyModel      itsGSM;
+  MeasurementSet        itsMS;
+  ROMSMainColumns       itsMSCol;
+  ParmTable             itsMEP;
+  Table                 itsGSMTable;
+  GSM::SkyModel         itsGSM;
 
-  TableIterator      itsIter;
+  Vector<uInt>          itsCurRows;     //# Rows in the current iter step
+  TableIterator         itsIter;
 
-  MeqDomain          itsDomain;
-  MeqRequest         itsRequest;
+  MDirection            itsPhaseRef;    //# Phase reference position in J2000
+  MeqDomain             itsSolveDomain;
+  int                   itsNrScid;      //# nr of solvable coefficients
 
-  vector<MVBaseline> itsBaselines;
-  vector<MeqStation> itsStations;
-  MeqUVWPolc         itsUVWPolc;
-  MeqJonesExpr*      itsExprTree;
+  Matrix<int>           itsBLIndex;     //# baseline index of antenna pair
+  vector<MeqStation>    itsStations;
+  vector<MeqJonesExpr*> itsStatExpr;    //# Expression per station
+  vector<MVBaseline>    itsBaselines;
+  vector<MeqUVWPolc*>   itsUVWPolc;     //# UVW polynomial per baseline
+  vector<MeqJonesExpr*> itsExpr;        //# expression tree per baseline
 
-  double itsStartTime;
+  double itsTimeInterval;
 
-  double itsStartFreq, itsEndFreq;
+  double itsStartFreq;
+  double itsEndFreq;
   int    itsNrChan;
 
   Bool itsDataRead;
