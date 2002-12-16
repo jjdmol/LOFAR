4508a741eea6ef5d16d0d13aa97c89880d3b7ad5
smirnov <smirnov@b3281354-443f-0410-9bd6-bf5212d75c11>
2002-12-16 16:17:08 +0000

    %[BugId: 136]% First working revision of the AppAgent/VisAgent/MSVisInputAgent chain.

---
+++ trunk/CEP/CPA/AppAgent/MSVisAgent/src/MSVisAgentDebugContext.h
@@ -0,0 +1,10 @@
+#ifndef MSVISAGENT_MSVISAGENTDEBUGCONTEXT_h 
+#define MSVISAGENT_MSVISAGENTDEBUGCONTEXT_h 1
+
+#include "Common/Debug.h"
+class MSVisAgentDebugContext 
+{
+  LocalDebugContext;
+};
+
+#endif
