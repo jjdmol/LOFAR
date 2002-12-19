343515e29858176695ed7f60319d425bcd3bfaa0
Chris Broekema <broekema@users.noreply.github.com>
2002-12-19 11:50:47 +0000

    %[BugId: 149]% Moved some files from the Common tree. These are also adapted to use the new LCSMath tools.

---
+++ StationSim/src/MDL.h
@@ -0,0 +1,31 @@
+//  lofar_mdl.h
+//
+//  Copyright (C) 2002
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+// Chris Broekema, november 2002.
+
+#ifndef STATIONSIM_MDL_H
+#define STATIONSIM_MDL_H
+#include <Math/LCSMath.h>
+#include <Common/Lorrays.h>
+
+unsigned int mdl (LoMat_double d, unsigned int nantl, unsigned int nsnsh) ;
+unsigned int mdl (LoVec_double d, unsigned int nantl, unsigned int nsnsh) ;
+#endif
