147a19f690f019d0dcc4f57b9986882909d6311e
Ger van Diepen <gervandiepen@users.noreply.github.com>
2003-02-12 07:04:32 +0000

    %[BugId: 76]% Made argv const and added header

--- CEP/Tools/PerfTest/P2Perf/src/Simulate.cc
+++ CEP/Tools/PerfTest/P2Perf/src/Simulate.cc
@@ -1,3 +1,26 @@
+//  Simulate.cc:
+//
+//  Copyright (C) 2000-2002
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -17,7 +40,7 @@ int atexit(void (*function)(void))
 }
 #endif
 
-int main (int argc, char** argv)
+int main (int argc, const char** argv)
 {
   // Set trace level.
 
@@ -35,7 +58,7 @@ int main (int argc, char** argv)
 	  P2Perf simulator;
 	  simulator.setarg (argc, argv);
 	  
-	  Debug::initLevels (argc, (const char* [])argv);
+	  Debug::initLevels (argc, argv);
 	  try {
 	    SimulatorParse::parse (simulator);
 	  } catch (SimulatorParseError x) {
