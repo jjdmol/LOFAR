40a8249a4b829b7a12c233de4c97f7f9a8517be9
Oleg Smirnov <o-smirnov@users.noreply.github.com>
2003-04-02 13:06:18 +0000

    Removed VisDM package. Classes moved to VisCube.

--- CEP/CPA/VisDM/src/VisDM.cc
+++
@@ -1,8 +0,0 @@
-#include "VisDM/VisDM.h"
-#include "VisDM/TID-VisDM.h"
-    
-// make sure registry is puled in
-static int dum = aidRegistry_VisDM();    
-    
-// init the debug context
-InitDebugContext(VisDMDebugContext,"VisDM");
