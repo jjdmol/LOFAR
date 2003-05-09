d97cc724f4bf96ac57ae3f60781de40613883a0a
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-05-09 07:58:26 +0000

    First real test of DummySolver

--- trunk/CEP/CPA/AppAgent/Solver/test/test_solver.cc
+++ trunk/CEP/CPA/AppAgent/Solver/test/test_solver.cc
@@ -1,4 +1,4 @@
-//  test_input.cc: tests the MSVisInputAgent class
+//  test_solver.cc: tests the DummySolver class
 //
 //  Copyright (C) 2002
 //  ASTRON (Netherlands Foundation for Research in Astronomy)
@@ -20,17 +20,24 @@
 //
 //  $Id$
 
+
 #include <MSVisAgent/MSInputAgent.h>
 #include <MSVisAgent/MSOutputAgent.h>
-#include "../src/BatchAgent.h"
-#include "../src/DummySolver.h"
+#include <Solver/BatchAgent.h>
+#include <Solver/DummySolver.h>
+#include <DMI/DataArray.h>
 
-int main (int argc,const char *argv[])
+#include <aips/Arrays/ArrayUtil.h>
+#include <aips/Exceptions/Error.h>
+
+#include <stdexcept>
+
+
+int main (int argc, const char *argv[])
 {
   using namespace MSVisAgent;
   using namespace SolverControl;
-  MeasurementSet ms("test.ms");
-  MeasurementSet ms1("test.ms",Table::Update);
+  //  using namespace VisVocabulary;
   
   try 
   {
@@ -39,27 +46,154 @@ int main (int argc,const char *argv[])
     Debug::setLevel("Solver",2);
     Debug::initLevels(argc,argv);
 
+    if (argc < 2) {
+      cerr << "Run as:  tCAL msname mepname gsmname scenario loopcnt "
+	   << "stchan endchan selection calcuvw" << endl;
+      cerr << " msname:    name of MS without .MS extension" << endl;
+      cerr << " mepname:   name of MEP table   (default is msname)" << endl;
+      cerr << " gsmname:   name of GSM table   (default is msname)" << endl;
+      cerr << " modeltype: WSRT or LOFAR       (default is LOFAR)" << endl;
+      cerr << " scenario:  scenario to execute (default is predict)" << endl;
+      cerr << " solvparms: solvable parms pattern ({RA,DEC,StokesI}.*)"
+	   << endl;
+      cerr << " loopcnt:   number of scenario loops (default is 1)" << endl;
+      cerr << " stchan:    first channel       (default is -1 (first channel)"
+	   << endl;
+      cerr << " endchan:   last channel        (default is stchan)" << endl;
+      cerr << " selection: TaQL selection string (default is empty)" << endl;
+      cerr << " peel:      source nrs to peel as 2,4,1 (default is all)"
+	   << endl;
+      cerr << " calcuvw:   calculate UVW       (default is 0)" << endl;
+      return 0;
+    }
+    string msname (argv[1]);
+
+    string mepname;
+    if (argc > 2) {
+      mepname = argv[2];
+    }
+    if ("0" == mepname  ||  "" == mepname) {
+      mepname = msname;
+    }
+
+    string gsmname;
+    if (argc > 3) {
+      gsmname = argv[3];
+    }
+    if ("0" == gsmname  ||  "" == gsmname) {
+      gsmname = mepname;
+    }
+
+    string modelType;
+    if (argc > 4) {
+      modelType = argv[4];
+    }
+    if ("0" == modelType  ||  "" == modelType) {
+      modelType = "LOFAR";
+    }
+
+    string scenario;
+    if (argc > 5) {
+      scenario = argv[5];
+    }
+    if ("0" == scenario  ||  "" == scenario) {
+      scenario = "predict";
+    }
+
+    string solvparms;
+    if (argc > 6) {
+      solvparms = argv[6];
+    }
+    if ("0" == solvparms  ||  "" == solvparms) {
+      solvparms = "{RA,DEC,StokesI}.*";
+    }
+    //LoVec_string solvvec (1);
+    //  solvvec(0) = solvparms;
+    vector<string> solvvec (1, solvparms);
+    LoVec_bool solvflag (1);
+    solvflag(0) = true;
+
+    int loopcnt = 1;
+    if (argc > 7) {
+      loopcnt = atoi(argv[7]);
+      if (0 == loopcnt) loopcnt = 1;
+    }
+
+    int stchan = -1;
+    if (argc > 8) {
+      istringstream iss(argv[8]);
+      iss >> stchan;
+    }
+    int endchan = stchan;
+    if (argc > 9) {
+      istringstream iss(argv[9]);
+      iss >> endchan;
+    }
+
+    string selstr;
+    if (argc > 10) {
+      selstr = argv[10];
+    }
+
+    string peelstr;
+    if (argc > 11) {
+      peelstr = argv[11];
+    }
+    vector<int> peelVec;
+    if (!peelstr.empty()) {
+      Vector<String> peels = stringToVector (peelstr);
+      peelVec.resize (peels.nelements());
+      for (unsigned int i=0; i<peels.nelements(); i++) {
+	istringstream iss(peels(i));
+	iss >> peelVec[i];
+      }
+    }
+
+    bool calcuvw=false;
+    if (argc > 12) {
+      istringstream iss(argv[12]);
+      iss >> calcuvw;
+    }
+
+    cout << "tCAL parameters:" << endl;
+    cout << " msname:    " << msname << endl;
+    cout << " mepname:   " << mepname << endl;
+    cout << " gsmname:   " << gsmname << endl;
+    cout << " modeltype: " << modelType << endl;
+    cout << " scenario:  " << scenario << endl;
+    cout << " solvparms: " << solvparms << endl;
+    cout << " loopcnt:   " << loopcnt << endl;
+    cout << " stchan:    " << stchan << endl;
+    cout << " endchan:   " << endchan << endl;
+    cout << " selection: " << selstr << endl;
+    cout << " peel:      " << peelstr << endl;
+    cout << " calcuvw  : " << calcuvw << endl;
+
     // initialize parameter record
     DataRecord::Ref recref;
     DataRecord &rec = recref <<= new DataRecord;
     // init errors will be thrown as exceptions
     rec[FThrowError] = True;
     
-    rec[SolverVocabulary::FDomainSize] = 600; // 10-minute domains
+    rec[FDomainSize] = 3600;               // 1-hr domains
+    rec[FMEPName] = mepname + ".MEP";
+    rec[FGSMName] = gsmname + ".MEP";
+    rec[FCalcUVW] = calcuvw;
+    rec[FModelType] = modelType;
         
     // setup input agent parameters 
     DataRecord &inpargs = rec[AidInput] <<= new DataRecord;
     
-      inpargs[FMSName] = "test.ms";
-      inpargs[FDataColumnName] = "DATA";
-      inpargs[FTileSize] = 10;
+      inpargs[FMSName] = msname + ".MS";
+      inpargs[FDataColumnName] = "MODEL_DATA";
+      inpargs[FTileSize] = 1;
       // setup selection
       DataRecord &select = inpargs[FSelection] <<= new DataRecord;
         select[FDDID] = 0;
-        select[FFieldIndex] = 1;
-        select[FChannelStartIndex] = 10;
-        select[FChannelEndIndex]   = 20;
-        select[FSelectionString] = "ANTENNA1=1 && ANTENNA2=2";
+        select[FFieldIndex] = 0;
+        if (stchan >= 0) select[FChannelStartIndex] = stchan;
+	if (endchan >= 0) select[FChannelEndIndex] = endchan;
+        select[FSelectionString] = selstr;
     // setup output agent parameters 
     //   ... none for now
         
@@ -69,27 +203,30 @@ int main (int argc,const char *argv[])
     
       solveargs[FAutoExit] = True;
       solveargs[FStopWhenEnd] = True;
-      solveargs[FBatchJobs] <<= new DataField(TpDataRecord,3);
+      solveargs[FBatchJobs] <<= new DataField(TpDataRecord,1);
         solveargs[FBatchJobs][0][FConvergence] = 0;
-        solveargs[FBatchJobs][0][FMaxIterations] = 10;
-
-        solveargs[FBatchJobs][1][FConvergence] = 0.1;
-        solveargs[FBatchJobs][1][FMaxIterations] = 100;
-
-        solveargs[FBatchJobs][2][FConvergence] = 0.005;
-        solveargs[FBatchJobs][2][FMaxIterations] = 10;
+        solveargs[FBatchJobs][0][Niter] = loopcnt;
+        solveargs[FBatchJobs][0][SolvableParm] = solvvec;
+        solveargs[FBatchJobs][0][SolvableFlag] = solvflag;
+        solveargs[FBatchJobs][0][PeelNrs] = peelVec;
+        solveargs[FBatchJobs][0][PredNrs] = LoVec_int();
+        solveargs[FBatchJobs][0][PredNrs] = vector<int>();
 
     cout<<"=================== creating agents ===========================\n";
-    MSVisAgent::MSInputAgent in(AidInput);
-    MSVisAgent::MSOutputAgent out(AidOutput);
-    SolverControl::BatchAgent control(AidControl);
-    AppEventFlag evflag;
+    AppAgent::Ref inref,outref,ctrlref;
+    MSVisAgent::MSInputAgent &in = *new MSVisAgent::MSInputAgent(AidInput);
+    inref <<= &in;
+    MSVisAgent::MSOutputAgent &out = *new MSVisAgent::MSOutputAgent(AidOutput);
+    outref <<= &out;
+    SolverControl::BatchAgent &control = *new SolverControl::BatchAgent(AidControl);
+    ctrlref <<= &control;
+    AppEventFlag::Ref evflag(DMI::ANONWR);
     in.attach(evflag);
     out.attach(evflag);
     control.attach(evflag);
     cout<<"=================== creating solver ===========================\n";
     DummySolver solver;
-    solver<<in<<out<<control;
+    solver<<&in<<&out<<&control;
     cout<<"=================== initializing solver =======================\n";
     control.preinit(recref);
     cout<<"=================== running solver ============================\n";
@@ -98,7 +235,17 @@ int main (int argc,const char *argv[])
   }
   catch ( std::exception &exc ) 
   {
-    cout<<"Exiting with exception: "<<exc.what()<<endl;  
+    cout<<"Exiting with std exception: "<<exc.what()<<endl;  
+    return 1;
+  }
+  catch ( AipsError& exc )
+  {
+    cout<<"Exiting with AIPS++ exception: "<<exc.getMesg()<<endl;  
+    return 1;
+  }
+  catch ( ... )
+  {
+    cout<<"Exiting with unknown exception" <<endl;  
     return 1;
   }
   
