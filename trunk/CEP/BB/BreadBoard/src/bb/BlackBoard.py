1dd54d250b2be804a90e99668ae7e7d8ac13553a
daan <daan@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-05-12 15:54:46 +0000

    daily snapshot

--- trunk/CEP/BB/BreadBoard/src/bb/BlackBoard.py
+++ trunk/CEP/BB/BreadBoard/src/bb/BlackBoard.py
@@ -10,18 +10,47 @@ $Id$
 import pg;
 db = pg.DB(dbname="bb");
 
+import datetime;
+
+from BlackBoardController import BlackBoardController;
+
 class BlackBoard:
   """something containing information about a problem to be solved."""
 
-  def __init__(self):
+  def __init__(self, copy = None):
     self.threads = {};
     self.workload = {};
     self.knowledgeSources = {};
-    self.id = db.query("INSERT INTO blackboards DEFAULT VALUES");
-    print self.id;
+    self.controller = BlackBoardController(self);
+    if copy == None:
+      self.id = db.query("INSERT INTO blackboards DEFAULT VALUES");
+    else:
+      self.id = db.insert("blackboards",copy.record);
+    self.refresh_data();
+
+  def refresh_data(self):
+    self.record = db.get("blackboards", self.id, "oid");
 
   def time(self, start, end):
-    db.query("UPDATE blackboards set starttime = '"+ str(start) +"' , endtime = '" + str(end) + "' WHERE oid = " + str(self.id) );
+    self.record["starttime"] = start;
+    self.record["endtime"] = end;
+    data = db.query("UPDATE blackboards SET starttime = '"+ str(start) +"' , endtime = '" + str(end) + "' WHERE oid = " + str(self.id) );
+    print "time updated: " + str(data);
+
+  def split_over_time(self, early, late):
+    early = BlackBoard(self);
+    late = BlackBoard(self);
+    start = datetime.datetime.fromtimestamp(self.record["starttime"]);
+    end = datetime.datetime.fromtimestamp(self.record["endtime"]);
+    print self.record;
+    early.time(start, start + (end - start) /2);
+    late.time(start + (end - start) /2, end);
+
+  def parent(self, parent_id):
+    db.query("UPDATE blackboards SET parent_id = " + str(parent_id) + " WHERE oid = " + str(self.id) );
+
+  def frequency(self, low, high):
+    db.query("UPDATE blackboards set low = '"+ str(low) +"' , high = '" + str(high) + "' WHERE oid = " + str(self.id) );
 
   def range(self, low, high):
     db.query("UPDATE blackboards set low = '"+ str(low) +"' , high = '" + str(high) + "' WHERE oid = " + str(self.id) );
@@ -35,6 +64,7 @@ class BlackBoard:
   def __repr__(self):
     representation = "";
     representation += "<" + self.__class__.__name__ + " instance at " + repr(id(self))  + ">\n";
+    representation += str(self.record) + "\n";
     for kskey in self.knowledgeSources.keys():
       representation += "\tks: " + repr(self.knowledgeSources[kskey]) + "\n";
     return representation;
--- trunk/CEP/BB/BreadBoard/src/bbdb.py
+++ trunk/CEP/BB/BreadBoard/src/bbdb.py
@@ -6,13 +6,17 @@ import pg
 
 db = pg.DB(dbname="bb");
 
-try:
- db.query("DROP TABLE blackboards")
-except:
- print "blackboards didn't exsist"
+tables = ["blackboards", "watchers", "controllers", "threads", "engines", "comparisons", "forks", "workloads"];
+
+for table in tables:
+  try:
+   db.query("DROP TABLE " + table)
+  except:
+   print table + " didn't exsist"
+
 db.query("CREATE TABLE blackboards ( children oid[] default '{}', \
-                                     starttime DATE, \
-                                     endtime DATE, \
+                                     starttime TIMESTAMP DEFAULT now() - interval '6 hours', \
+                                     endtime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
                                      low FLOAT default '10', \
                                      high FLOAT default '100', \
                                      baseline INTEGER[2] default '{1, 4950}', \
@@ -41,54 +45,29 @@ db.query("CREATE TABLE blackboards ( children oid[] default '{}', \
 
 ## db.query("CREATE TRIGGER no_child BEFORE INSERT ON blackboards FOR EACH ROW EXECUTE PROCEDURE no_child();");
 
-try:
- db.query("DROP TABLE watchers")
-except:
- print "watchers didn't exsist"
 db.query("CREATE TABLE watchers ( ) WITH OIDS")
 
-try:
- db.query("DROP TABLE controllers")
-except:
- print "controllers didn't exsist"
 db.query("CREATE TABLE controllers ( strategy text ) WITH OIDS")
 
-try:
- db.query("DROP TABLE threads")
-except:
- print "threads didn't exsist"
 db.query("CREATE TABLE threads ( ) WITH OIDS")
 
-try:
- db.query("DROP TABLE engines")
-except:
- print "engines didn't exsist"
 db.query("CREATE TABLE engines (  ) WITH OIDS")
 
-try:
- db.query("DROP TABLE comparisons")
-except:
- print "comparisons didn't exsist"
-db.query("CREATE TABLE comparisons ( watcher_id oid , controller_id oid) WITH OIDS")
+db.query("CREATE TABLE comparisons ( watcher_id OID , controller_id OID) WITH OIDS")
 
-try:
- db.query("DROP TABLE forks")
-except:
- print "forks didn't exsist"
-db.query("CREATE TABLE forks ( controller_id oid, child_id oid ) WITH OIDS")
+db.query("CREATE TABLE forks ( controller_id OID, child_id OID ) WITH OIDS")
 
-try:
- db.query("DROP TABLE workloads")
-except:
- print "workloads didn't exsist"
-db.query("CREATE TABLE workloads ( controller_id oid,\
-                                   engine_id oid,\
-                                   parent_id oid,\
-                                   start_time timestamp,\
-                                   end_time timestamp,\
-                                   start_freq float,\
-                                   end_freq float\
+db.query("CREATE TABLE workloads ( controller_id OID,\
+                                   engine_id OID,\
+                                   parent_id OID,\
+                                   start_time TIMESTAMP,\
+                                   end_time TIMESTAMP,\
+                                   start_freq FLOAT,\
+                                   end_freq FLOAT\
                                   ) WITH OIDS")
+
+
+# block of test code:
 
 p = {}
 p["watcher_id"] = db.query("INSERT INTO watchers DEFAULT VALUES");
@@ -97,3 +76,14 @@ result = db.insert("comparisons",p)
 
 print result;
 print result["oid_comparisons"];
+
+# end of test code
+
+db.query("TRUNCATE TABLE blackboards");
+db.query("TRUNCATE TABLE watchers");
+db.query("TRUNCATE TABLE controllers");
+db.query("TRUNCATE TABLE threads");
+db.query("TRUNCATE TABLE engines");
+db.query("TRUNCATE TABLE comparisons");
+db.query("TRUNCATE TABLE forks");
+db.query("TRUNCATE TABLE workloads");
