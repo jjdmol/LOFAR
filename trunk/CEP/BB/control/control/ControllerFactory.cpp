07ce6086ffeda98e438b53dc83b5eea70fadaec1
daan <daan@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-09-22 10:04:34 +0000

    %[ER: 25]% factories

---
+++ trunk/CEP/BB/control/control/ControllerFactory.cpp
@@ -0,0 +1,17 @@
+#include "ControllerFactory.h"
+
+//##ModelId=3F6EC713008C
+Control ControllerFactory::newController(DirectiveData data)
+{
+}
+
+//##ModelId=3F6EC77A02EE
+IntermediateLevelStrategy ControllerFactory::newStrategy()
+{
+}
+
+//##ModelId=3F6EC7B2032C
+LowerStrategy ControllerFactory::newTactic(DirectiveData data)
+{
+}
+
---
+++ trunk/CEP/BB/control/directives/DirectiveFactory.h
@@ -0,0 +1,20 @@
+#ifndef DIRECTIVEFACTORY_H_HEADER_INCLUDED_C09168FB
+#define DIRECTIVEFACTORY_H_HEADER_INCLUDED_C09168FB
+#include "Directive.h"
+#include "DirectiveData.h"
+
+//##ModelId=3F6EC3780186
+class DirectiveFactory
+{
+  public:
+    //##ModelId=3F6EC39F02EE
+    static Directive makeDirective(
+        //##Documentation
+        //## The data from the database (or file).
+        DirectiveData data);
+
+};
+
+
+
+#endif /* DIRECTIVEFACTORY_H_HEADER_INCLUDED_C09168FB */
