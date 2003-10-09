604721aa2448a438f824acec56b6e0ca949ae8ac
Ger van Diepen <gervandiepen@users.noreply.github.com>
2003-10-09 08:21:44 +0000

    %[ER: 37]% First version

---
+++ CEP/CPA/PSS4/MEQ/src/Cos.cc
@@ -0,0 +1,50 @@
+//# Cos.cc: Take cosine of a node
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <MEQ/Cos.h>
+
+namespace MEQ {    
+
+
+Cos::Cos()
+{}
+
+Cos::Cos (Function* ch)
+{
+  children().push_back (ch);
+}
+
+Cos::~Cos()
+{}
+
+void Cos::evaluate (Vells& result, const Request&,
+		    const vector<Vells*>& values)
+{
+  if (result.isReal()) {
+    result.getRealArray() = cos(values[0]->getRealArray());
+  } else {
+    result.getComplexArray() = cos(values[0]->getComplexArray());
+  }
+}
+
+
+} // namespace MEQ
---
+++ CEP/CPA/PSS4/MEQ/src/Cos.h
@@ -0,0 +1,51 @@
+//# Cos.h: Take cosine of a node
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef MEQ_COS_H
+#define MEQ_COS_H
+    
+#include <MEQ/Function.h>
+
+#pragma aidgroup MEQ
+#pragma types #MEQ::Cos
+
+namespace MEQ {    
+
+
+class Cos : public Function
+{
+public:
+  Cos();
+
+  Cos (Function* ch1);
+
+  virtual ~Cos();
+
+  // Evaluate the value for the given request.
+  virtual void evaluate (Vells& result, const Request&,
+			 const vector<Vells*>& values);
+};
+
+
+} // namespace MEQ
+
+#endif
