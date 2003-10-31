d9458a8d6cf53d26397488313956d9fdafa2ebfd
Oleg Smirnov <o-smirnov@users.noreply.github.com>
2003-10-31 13:13:32 +0000

    %[ER: 37]%

---
+++ CEP/CPA/PSS4/MeqGen/src/compare.cc
@@ -0,0 +1,33 @@
+//# compare.cc: 
+//# MeqExpr node class, automatically generated from Glish
+//#
+//# Copyright  (C)  2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2,  7990 AA Dwingeloo, The Netherlands,  seg@astron.nl
+//# 
+//# $Id$: 
+
+#include "compare.h"
+#include <MEQ/VellsTmp.h>
+#include <Common/Debug.h>
+
+#define   copy              *(values[0])
+#define   MeqSpigot         *(values[1])
+
+namespace Meq {
+
+  compare::compare()
+  {}
+
+  compare::~compare()
+  {}
+
+  void compare::evaluateVells (Vells& result, const Request&, const vector<Vells*>& values)
+  {
+    result = ( copy - MeqSpigot );
+  }
+
+
+} // namespace MEQ
+
+
