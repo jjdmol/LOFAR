794cecf2b072fdfdeb8bc573beaca273e4f24c42
Ger van Diepen <gervandiepen@users.noreply.github.com>
2003-10-31 14:52:05 +0000

    %[ER: 37]% Added UVW

---
+++ CEP/CPA/PSS4/MEQ/src/UVW.h
@@ -0,0 +1,120 @@
+//# UVW.h: Calculate station UVW from station position and phase center
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef MEQ_UVW_H
+#define MEQ_UVW_H
+    
+#include <MEQ/Function.h>
+
+#pragma aidgroup Meq
+#pragma types #Meq::UVW #Meq::U
+
+namespace Meq {    
+
+
+class UVW : public Function
+{
+public:
+  UVW();
+
+  virtual ~UVW();
+
+  // Get the result for the given request.
+  virtual int getResultImpl (Result::Ref &resref, const Request&, bool newReq);
+
+  // Check and convert the children.
+  void checkChildren();
+
+  Result::Ref& getU()
+    { return itsRefU; }
+  Result::Ref& getV()
+    { return itsRefV; }
+  Result::Ref& getW()
+    { return itsRefW; }
+
+private:
+  Result itsU;
+  Result itsV;
+  Result itsW;
+  Result::Ref itsRefU;
+  Result::Ref itsRefV;
+  Result::Ref itsRefW;
+};
+
+
+class U : public Function
+{
+public:
+  U();
+
+  virtual ~U();
+
+  // Get the result for the given request.
+  virtual int getResultImpl (Result::Ref &resref, const Request&, bool newReq);
+
+  // Check and convert the children.
+  void checkChildren();
+
+private:
+  UVW* itsUVW;
+};
+
+
+class V : public Function
+{
+public:
+  V();
+
+  virtual ~V();
+
+  // Get the result for the given request.
+  virtual int getResultImpl (Result::Ref &resref, const Request&, bool newReq);
+
+  // Check and convert the children.
+  void checkChildren();
+
+private:
+  UVW* itsUVW;
+};
+
+
+class W : public Function
+{
+public:
+  W();
+
+  virtual ~W();
+
+  // Get the result for the given request.
+  virtual int getResultImpl (Result::Ref &resref, const Request&, bool newReq);
+
+  // Check and convert the children.
+  void checkChildren();
+
+private:
+  UVW* itsUVW;
+};
+
+
+} // namespace Meq
+
+#endif
