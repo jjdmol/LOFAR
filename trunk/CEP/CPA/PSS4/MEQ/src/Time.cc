cee8e90b6fff3c61e3d3dbbda52e992dc0053277
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-11-03 14:44:44 +0000

    %[ER: 37]% Added class to expand Freq and Time

---
+++ trunk/CEP/CPA/PSS4/MEQ/src/Time.cc
@@ -0,0 +1,63 @@
+//# Time.cc: Give the times
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <MEQ/Time.h>
+#include <MEQ/Request.h>
+#include <MEQ/Result.h>
+#include <MEQ/Cells.h>
+
+namespace Meq {    
+
+
+Time::Time()
+{}
+
+Time::~Time()
+{}
+
+int Time::getResultImpl (Result::Ref &resref, const Request& request, bool)
+{
+  // Get cells.
+  const Cells& cells = request.cells();
+  int nfreq = cells.nfreq();
+  int ntime = cells.ntime();
+  // Create result object and attach to the ref that was passed in.
+  Result& result = resref <<= new Result();
+  LoMat_double& arr = result.setReal (nfreq,ntime);
+  // Evaluate the main value.
+  for (int i=0; i<ntime; i++) {
+    double time = cells.time(i);
+    for (int j=0; j<nfreq; j++) {
+      arr(j,i) = time;
+    }
+  }
+  // There are no perturbations.
+  return 0;
+}
+
+void Time::checkChildren()
+{
+  convertChildren(0);
+}
+
+
+} // namespace Meq
