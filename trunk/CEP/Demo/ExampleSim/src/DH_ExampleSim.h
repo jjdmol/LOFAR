1dc2129a2d1b8ba817a251ca20e9f7337aac4730
ellen <ellen@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-11-18 11:04:58 +0000

    This commit was generated by cvs2svn to compensate for changes in r1426, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/CEP/Demo/ExampleSim/src/DH_ExampleSim.h
@@ -0,0 +1,88 @@
+//  DH_ExampleSim.h: Example DataHolder class
+//
+//  Copyright (C) 2000, 2001
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef EXAMPLESIM_DH_EXAMPLESIM_H
+#define EXAMPLESIM_DH_EXAMPLESIM_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <Common/lofar_complex.h>
+#include "CEPFrame/DataHolder.h"
+#include "CEPFrame/BaseSim.h"
+
+/**
+   This class is a DataHolder example class used in ExampleSim.
+*/
+
+class DH_ExampleSim: public DataHolder
+{
+public:
+  
+  explicit DH_ExampleSim (const string& name);
+  DH_ExampleSim (const DH_ExampleSim&);
+  virtual ~DH_ExampleSim();
+
+  DataHolder* clone() const;
+
+  void setCounter (int counter);
+  int getCounter() const;
+
+  int* getBuffer();
+  const int* getBuffer() const;
+
+protected:
+  class DataPacket: public DataHolder::DataPacket
+  {
+  public:
+    DataPacket() : itsCounter(0) {};
+    int itsBuffer[10];
+    int itsCounter;
+  };
+
+private:
+  /// Forbid assignment.
+  DH_ExampleSim& operator= (const DH_ExampleSim&);
+
+
+  DataPacket itsDataPacket;
+};
+
+
+inline void DH_ExampleSim::setCounter (int counter)
+  { itsDataPacket.itsCounter = counter; }
+
+inline int DH_ExampleSim::getCounter() const
+  { return itsDataPacket.itsCounter; }
+
+inline int* DH_ExampleSim::getBuffer()
+  { return itsDataPacket.itsBuffer; }
+
+inline const int* DH_ExampleSim::getBuffer() const
+  { return itsDataPacket.itsBuffer; }
+
+
+#endif 
---
+++ trunk/CEP/Demo/ExampleSim/src/WH_Add.h
@@ -0,0 +1,74 @@
+//  WH_Add.h: WorkHolder for the Tester test program
+//
+//  Copyright (C) 2000, 2001
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef EXAMPLESIM_WH_ADD_H
+#define EXAMPLESIM_WH_ADD_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "CEPFrame/WorkHolder.h"
+#include "ExampleSim/DH_ExampleSim.h"
+
+
+/**
+   This WorkHolder adds a constant to the input.
+*/
+
+class WH_Add: public WorkHolder
+{
+public:
+  /// Construct the work holder.
+  explicit WH_Add (const string& name = "WH_Add", int factor=0);
+
+  virtual ~WH_Add();
+
+  /// Static function to create an object.
+  static WorkHolder* construct (const string& name, int ninput, int noutput,
+				const ParamBlock&);
+
+   /// Make a fresh copy of the WH object.
+  virtual WH_Add* make (const string& name);
+
+  /// Do a process step.
+  virtual void process();
+
+  /// Show the work holder on stdout.
+  virtual void dump();
+
+private:
+  /// Forbid copy constructor.
+  WH_Add (const WH_Add&);
+
+  /// Forbid assignment.
+  WH_Add& operator= (const WH_Add&);
+
+  int itsFactor;
+
+};
+
+
+#endif
