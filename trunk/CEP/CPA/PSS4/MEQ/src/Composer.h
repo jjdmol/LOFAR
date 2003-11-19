7648ffe0357ac61c72e8a6b14465fa989ce45e29
smirnov <smirnov@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-11-19 09:25:06 +0000

    %[ER: 37]% Added selector and composer nodes

---
+++ trunk/CEP/CPA/PSS4/MEQ/src/Composer.h
@@ -0,0 +1,50 @@
+//# Composer.h: Selects result planes from a result set
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef MEQ_COMPOSER_H
+#define MEQ_COMPOSER_H
+    
+#include <MEQ/Node.h>
+
+#pragma aidgroup Meq
+#pragma types #Meq::Composer
+
+namespace Meq {    
+
+
+class Composer : public Node
+{
+  public:
+    Composer ();
+    virtual ~Composer ();
+    
+    virtual TypeId objectType() const
+    { return TpMeqComposer; }
+    
+  protected:
+    int getResultImpl (ResultSet::Ref &resref, const Request& request, bool newReq);
+};
+
+
+} // namespace Meq
+
+#endif
