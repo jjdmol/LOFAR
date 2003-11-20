06c6b9b039f0135ab88b6e5d2d0bfd4b87a4a63c
Oleg Smirnov <o-smirnov@users.noreply.github.com>
2003-11-20 14:31:08 +0000

    %[ER: 37]% Added auto-generation of glish defrecs

--- CEP/CPA/AppAgent/MeqServer/src/Spigot.h
+++ CEP/CPA/AppAgent/MeqServer/src/Spigot.h
@@ -8,6 +8,28 @@
 #pragma aid Input Col Next Request Id
 #pragma types #Meq::Spigot
     
+// The comments below are used to automatically generate a default
+// init-record for the class 
+
+//defrec begin MeqSpigot
+//  A MeqSpigot is attached to a VisAgent data source, and represents
+//  one interferometer. For every matching VisTile at the input of the 
+//  source, it caches the visibility data. If a matching request is then
+//  received, it returns that data as the result set (with one plane per
+//  correlation.) A MeqSpigot usually works in concert with a MeqSink,
+//  in that a sink is placed at the base of the tree, and generates 
+//  results matching the input data. Note that for this to work properly,
+//  a spigot must be created BEFORE its corresponding sink.
+//  A MeqSpigot can have no children.
+//field: station_1_index 0
+//  Index (1-based) of first station composing the interferometer
+//field: station_2_index 0
+//  Index (1-based) of second station composing the interferometer
+//field: input_col ''
+//  tile column to get result from: DATA, PREDICT or RESIDUALS. Default
+//  is DATA.
+//defrec end
+
 namespace Meq {
 
 //##ModelId=3F98DAE503C9
