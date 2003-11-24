bf708ab7f6b10a7d98b32c230f07e72fcfadd2d1
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-11-24 12:46:23 +0000

    %[ER: 19]% Turned MetaData into a "real" class with accessor methods for its members. These members are now shared pointers to the actual meta data. The methods erase(), insert(), save() and update() are now non-virtual. They call the associated do*() methods which are now pure virtual.

---
+++ trunk/LCS/PL/src/PersistentObject.cc
@@ -0,0 +1,114 @@
+//#  PersistentObject.cc: implementation of the persistent object base class.
+//#
+//#  Copyright (C) 2002-2003
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <PL/PersistentObject.h>
+#include <PL/Exception.h>
+
+namespace LCS
+{
+  namespace PL
+  {
+
+//     PersistentObject::MetaData* PersistentObject::MetaData::clone() const
+//     {
+//       MetaData* md = new MetaData();
+//       md->oid().set(oid().get());
+//       md->ownerOid().set(ownerOid().get());
+//       md->versionNr() = versionNr();
+//       return md;
+//     }
+
+    void PersistentObject::MetaData::reset() const
+    {
+      itsOid->reset();
+      // We do not want to reset the owner-id. Owner-ids don't need to be
+      // (re)generated; they are simply attached to an exisiting object-id, 
+      // according to the relationship between owning and owned TPOs. This
+      // is done in the init() method.
+      // itsOwnerOid->set(0); 
+      *itsVersionNr = 0;
+    }
+
+    void PersistentObject::erase() const
+    {
+      doErase();
+      POContainer::const_iterator it;
+      for(it = itsOwnedPOs.begin(); it != itsOwnedPOs.end(); ++it) {
+	(*it)->erase();
+      }
+    }
+
+    void PersistentObject::insert() const
+    {
+//       // Make a copy of the meta data first. We might need it to restore
+//       // the meta data when the insert actions fails.
+//       boost::shared_ptr<MetaData> md(metaData().clone());
+
+      metaData().reset();
+      doInsert();
+      POContainer::const_iterator it;
+      for(it = itsOwnedPOs.begin(); it != itsOwnedPOs.end(); ++it) {
+	(*it)->insert();
+      }
+    }
+
+//     void PersistentObject::retrieve(const ObjectId& oid)
+//     {
+//       // Implementation could be something like:
+//       doRetrieve(oid, isOwned());
+//       POContainer::const_iterator it;
+//       for(it = itsOwnedPOs.begin(); it != itsOwnedPOs.end(); ++it) {
+// 	(*it)->retrieve(oid);
+//       }
+//       // where doRetrieve is defined pure virtual and must be
+//       // implemented in TPersistentObject<T>.
+//       // Or maybe, we'd have to loop over all elements in POContainer,
+//       // exactly the way that e.g. insert() is implemented.
+//       // We can do this, because this is a non-static member function.
+//       // In that case, however, the result of the query should be stored
+//       // in "DTL format", in order to avoid multiple queries to the
+//       // database. I think it is possible to do this "caching" in the
+//       // static retrieve() method in TPersistentObject<T>.
+//     }
+
+    void PersistentObject::save() const
+    {
+      if (isPersistent()) {
+        update();
+      } 
+      else {
+        insert();
+      }
+    }
+
+    void PersistentObject::update() const
+    {
+      doUpdate();
+      POContainer::const_iterator it;
+      for(it = itsOwnedPOs.begin(); it != itsOwnedPOs.end(); ++it) {
+	(*it)->update();
+      }
+    }
+
+  } // namespace PL
+
+} // namespace LCS
