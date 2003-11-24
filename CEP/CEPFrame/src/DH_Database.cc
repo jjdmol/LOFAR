0d28189ef842c5611fef744f7615dc8b2aaff710
tanaka <tanaka@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-11-24 16:01:37 +0000

    %[ER: 56]%
    Initial creation.

---
+++ CEP/CEPFrame/src/DH_Database.cc
@@ -0,0 +1,76 @@
+//  DH_Database.cc: Implementation Database DataHolder
+//
+//  Copyright (C) 2000, 2002
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include <DH_Database.h>
+#include <PO_DH_Database.h>
+#include <Common/lofar_iostream.h>
+
+bool DH_Database::StoreInDatabase (int, int tag, char * buf, int size) {
+
+  cout << ">>> DH_Database: StoreInDatabase () called." << endl; 
+
+  PO_DH_Database po_dh_db;
+
+  po_dh_db.setMessageTag (tag);
+  po_dh_db.setReservedData1 (1111);
+  po_dh_db.setReservedData2 (2222);
+  po_dh_db.setReservedData3 (3333);
+  po_dh_db.setReservedData4 (4444);
+
+  po_dh_db.setByteStringLength (getDataPacketSize ());
+  po_dh_db.CopyToByteString (buf, size);
+
+  po_dh_db.setTimeStamp (5555);
+
+  po_dh_db.setType ("PODBRecUnkType");
+  po_dh_db.setName ("PODBRecUnkName");
+
+  po_dh_db.setHumanReadableForm ("PODBRecUnkHRF");
+
+  po_dh_db.Store ();
+
+  return true; 
+}
+
+
+bool DH_Database::RetrieveFromDatabase (int, int tag, char * buf, int size) { 
+
+  cout << "<<< DH_Database: RetrieveFromDatabase () called." << endl; 
+
+  PO_DH_Database po_dh_db;
+
+  po_dh_db.setMessageTag (tag);
+  po_dh_db.setByteStringLength (getDataPacketSize ());
+
+  po_dh_db.Retrieve ();
+
+  po_dh_db.CopyFromByteString (buf, size);
+
+  return true;
+}
+
+
+
+
+
---
+++ CEP/CEPFrame/src/DH_Database.h
@@ -0,0 +1,50 @@
+//  DH_Database.h: Definition Database DataHolder
+//
+//  Copyright (C) 2000, 2002
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef CEPFRAME_DH_DATABASE_H
+#define CEPFRAME_DH_DATABASE_H
+
+#include <CEPFrame/DataHolder.h>
+
+class DH_Database : public DataHolder
+{
+public:
+  explicit DH_Database (const string& name, const string& type)
+    : DataHolder (name, type) {}
+
+  bool StoreInDatabase (int appId, int tag, char * buf, int size);
+  bool RetrieveFromDatabase (int appId, int tag, char * buf, int size);
+
+protected:
+  class DataPacket:
+    public DataHolder::DataPacket
+  {
+  public:
+    DataPacket () {}
+  };
+
+};
+
+#endif
