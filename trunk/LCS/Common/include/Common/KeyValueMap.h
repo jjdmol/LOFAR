89136afba3b069cc2bbfc5f9a92912804ecef086
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-12-03 12:51:15 +0000

    %[ER: 38]% Updated comments

--- trunk/LCS/Common/include/Common/KeyValueMap.h
+++ trunk/LCS/Common/include/Common/KeyValueMap.h
@@ -1,24 +1,24 @@
-// KeyValueMap.h: Class to hold a collection of parameter name/value pairs.
-//
-//  Copyright (C) 2001
-//  ASTRON (Netherlands Foundation for Research in Astronomy)
-//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//  $Id$
+//# KeyValueMap.h: Class to hold a collection of key name/value pairs.
+//#
+//#  Copyright (C) 2001
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
 
 #ifndef COMMON_KEYVALUEMAP_H
 #define COMMON_KEYVALUEMAP_H
@@ -31,9 +31,11 @@
 #include <Common/lofar_map.h>
 #include <Common/lofar_iosfwd.h>
 
+// <summary> Class to hold a collection of key name/value pairs </summary>
+
 /**
-   The KeyValueMap class holds a collection of parameter name/value pairs.
-   It uses the STL map class to hold the parameters.
+   The KeyValueMap class holds a collection of key name/value pairs.
+   It uses the STL map class to hold the pairs.
 */
 
 class KeyValueMap: public map<string, KeyValue>
@@ -52,11 +54,12 @@ public:
   /// Assignment (copy semantics)
   KeyValueMap& operator= (const KeyValueMap& that);
 
-  /// Is a parameter defined?
+  /// Is a key defined?
   bool isDefined (const string& name) const
     { return find(name) != end(); }
 
-  /// Get the value of a parameter. Use the default if not existing.
+  // Get the value of a key. Use the default if not existing.
+  // <group>
   bool getBool (const string& name, bool defVal) const;
   int getInt (const string& name, int defVal) const;
   float getFloat (const string& name, float defVal) const;
@@ -64,13 +67,17 @@ public:
   fcomplex getFComplex (const string& name, const fcomplex& defVal) const;
   dcomplex getDComplex (const string& name, const dcomplex& defVal) const;
   const string& getString (const string& name, const string& defVal) const;
+  // </group>
 
-  /// Show the contents of the object.
+  // Show the contents of the object.
+  // <group>
   void show (ostream&) const;
-
   friend ostream& operator<< (ostream&, const KeyValueMap&);
+  // </group>
 
+  // Turn the object into a blob.
   friend BlobOStream& operator<< (BlobOStream&, const KeyValueMap&);
+  // Fill the object from a blob. It destroys the current contents.
   friend BlobIStream& operator>> (BlobIStream&, KeyValueMap&);
 };
 
