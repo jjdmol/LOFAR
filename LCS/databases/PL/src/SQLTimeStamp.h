aec6407900fc4a4249cd4fc37608d28739ca634c
Marcel Loose <gmloose@users.noreply.github.com>
2003-12-08 13:44:32 +0000

    %[ER: 61]% Moved SQLTimeStamp into namespace LOFAR::PL.

--- LCS/databases/PL/src/SQLTimeStamp.h
+++ LCS/databases/PL/src/SQLTimeStamp.h
@@ -36,98 +36,99 @@
 #include <Common/LofarTypes.h>
 #include <Common/Debug.h>
 
-//
-// SQLTimeStamp is a thin wrapper around the \c TIMESTAMP_STRUCT which is
-// defined in the ODBC layer. The main purpose of this wrapper class is to
-// provide easy comparison of time stamps. \c TIMESTAMP_STRUCT is transformed 
-// into an internal format which guarantees a strict weak ordering. In other
-// words, SQLTimeStamp is LessThanComparable.
-//
-class SQLTimeStamp
+namespace LOFAR
 {
-public:
-
-  // \c TIMESTAMP_STRUCT is the type that is used by ODBC to represent an SQL
-  // time stamp.
-  typedef TIMESTAMP_STRUCT sql_timestamp_t;
-
-  // Default constructor.
-  SQLTimeStamp() : itsTimeStampRep(0) {}
-
-  // Construct an SQLTimeStamp.
-  explicit SQLTimeStamp(const sql_timestamp_t& ts) : 
-    itsTimeStampRep(toTimeStampRep(ts.year, ts.month, ts.day, ts.hour, 
-				   ts.minute, ts.second, ts.fraction/1000)) {}
-
-  // Compare two SQL TimeStamps.
-  inline friend bool operator<(const SQLTimeStamp& lhs, 
-			       const SQLTimeStamp& rhs);
-
-private:
-  //
-  // TimeStampRep is the internal representation of an SQL time stamp. 
-  // Year, month, day, hour, min, sec, and usec are all separately stored 
-  // into one 64 bits integer, each unit using the minimally required number
-  // of bits, according to the table below.
-  // \verbatim
-  //        year month day hour min sec usec
-  // #bits   18    4    5    5   6   6   20
-  // \endverbatim
-  //
-  typedef int64  TimeStampRep;
-
-  //
-  // Offsets (in number of bits) of the date and time units in TimeStampRep.
-  //
-  struct Offset
+  namespace PL
   {
-    static const uint sec   = 20;
-    static const uint min   =  6 + sec;
-    static const uint hour  =  6 + min;
-    static const uint day   =  5 + hour;
-    static const uint month =  5 + day;
-    static const uint year  =  4 + month;
-  };
 
-  //
-  // Convert year, month, day, hour, min, sec, usec into a TimeStampRep.
-  //
-  // \warning No check is done on validity of input data. Furthermore,
-  // \c year should fit into an 18-bits signed integer!
-  //
-  inline TimeStampRep toTimeStampRep(short year, ushort month, ushort day, 
-                                     ushort hour, ushort min, ushort sec,
-				     ulong usec);
+    // SQLTimeStamp is a thin wrapper around the \c TIMESTAMP_STRUCT which is
+    // defined in the ODBC layer. The main purpose of this wrapper class is to
+    // provide easy comparison of time stamps. \c TIMESTAMP_STRUCT is
+    // transformed into an internal format which guarantees a strict weak
+    // ordering. In other words, SQLTimeStamp is LessThanComparable.
+    class SQLTimeStamp
+    {
+    public:
 
-  //
-  // Internal representation of the SQL TimeStamp.
-  //
-  TimeStampRep itsTimeStampRep;
+      // \c TIMESTAMP_STRUCT is the type that is used by ODBC to represent an
+      // SQL time stamp.
+      typedef TIMESTAMP_STRUCT sql_timestamp_t;
 
-};
+      // Default constructor.
+      SQLTimeStamp() : itsTimeStampRep(0) {}
+
+      // Construct an SQLTimeStamp.
+      explicit SQLTimeStamp(const sql_timestamp_t& ts) : 
+        itsTimeStampRep(toTimeStampRep(ts.year, ts.month, ts.day, ts.hour, 
+                                       ts.minute, ts.second, ts.fraction/1000))
+      {}
+
+      // Compare two SQL TimeStamps.
+      inline friend bool operator<(const SQLTimeStamp& lhs, 
+                                   const SQLTimeStamp& rhs);
+
+    private:
+      // TimeStampRep is the internal representation of an SQL time
+      // stamp. Year, month, day, hour, min, sec, and usec are all separately
+      // stored into one 64 bits integer, each unit using the minimally
+      // required number of bits, according to the table below.
+      // \verbatim
+      //        year month day hour min sec usec
+      // #bits   18    4    5    5   6   6   20
+      // \endverbatim
+      typedef int64  TimeStampRep;
+
+      // Offsets (in number of bits) of the date and time units in
+      // TimeStampRep.
+      struct Offset
+      {
+        static const uint sec   = 20;
+        static const uint min   =  6 + sec;
+        static const uint hour  =  6 + min;
+        static const uint day   =  5 + hour;
+        static const uint month =  5 + day;
+        static const uint year  =  4 + month;
+      };
+
+      // Convert year, month, day, hour, min, sec, usec into a TimeStampRep.
+      //
+      // \warning No check is done on validity of input data. Furthermore,
+      // \c year should fit into an 18-bits signed integer!
+      inline TimeStampRep toTimeStampRep(short year, ushort month, ushort day, 
+                                         ushort hour, ushort min, ushort sec,
+                                         ulong usec);
+
+      // Internal representation of the SQL TimeStamp.
+      TimeStampRep itsTimeStampRep;
+
+    };
 
 
-inline bool operator<(const SQLTimeStamp& lhs, const SQLTimeStamp& rhs)
-{
-  return lhs.itsTimeStampRep < rhs.itsTimeStampRep;
-}
+    inline bool operator<(const SQLTimeStamp& lhs, const SQLTimeStamp& rhs)
+    {
+      return lhs.itsTimeStampRep < rhs.itsTimeStampRep;
+    }
 
 
-inline SQLTimeStamp::TimeStampRep 
-SQLTimeStamp::toTimeStampRep(short year, ushort month, ushort day, 
-                             ushort hour, ushort min, ushort sec, 
-                             ulong usec)
-{
-  Assert( month < 13 && day < 32 && hour < 24 && min < 60 && 
-	  sec < 60 && usec < 1000000);
-  return 
-    (static_cast<int64>(year)  << Offset::year)  + 
-    (static_cast<int64>(month) << Offset::month) +
-    (static_cast<int64>(day)   << Offset::day)   + 
-    (static_cast<int64>(hour)  << Offset::hour)  + 
-    (static_cast<int64>(min)   << Offset::min)   + 
-    (static_cast<int64>(sec)   << Offset::sec)   + 
-    (static_cast<int64>(usec));
-}
+    inline SQLTimeStamp::TimeStampRep 
+    SQLTimeStamp::toTimeStampRep(short year, ushort month, ushort day, 
+                                 ushort hour, ushort min, ushort sec, 
+                                 ulong usec)
+    {
+      Assert( month < 13 && day < 32 && hour < 24 && min < 60 && 
+              sec < 60 && usec < 1000000);
+      return 
+        (static_cast<int64>(year)  << Offset::year)  + 
+        (static_cast<int64>(month) << Offset::month) +
+        (static_cast<int64>(day)   << Offset::day)   + 
+        (static_cast<int64>(hour)  << Offset::hour)  + 
+        (static_cast<int64>(min)   << Offset::min)   + 
+        (static_cast<int64>(sec)   << Offset::sec)   + 
+        (static_cast<int64>(usec));
+    }
+
+  } // namespace PL
+
+} // namespace LOFAR
 
 #endif
