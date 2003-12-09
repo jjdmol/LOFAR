c0c169ff747779d2bcdbbf2b6e2d6cf31f2d2c1f
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2003-12-09 16:25:50 +0000

    %[ER: 65]% First rudimentary implementation of the Query classes. These classes support the building of a tree of expression nodes which together form the WHERE clause of a query.

---
+++ trunk/LCS/databases/PL/include/PL/Query/ConstExprNode.h
@@ -0,0 +1,92 @@
+//#  ConstExprNode.h: one line description
+//#
+//#  Copyright (C) 2002-2003
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LOFAR_PL_QUERY_CONSTEXPRNODE_H
+#define LOFAR_PL_QUERY_CONSTEXPRNODE_H
+
+#if defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif
+
+//# Includes
+#include <PL/Query/ExprNode.h>
+#include <string>
+
+namespace LOFAR
+{
+  namespace PL
+  {
+    namespace Query
+    {
+      //# Forward Declarations
+
+      // @defgroup ConstExprNode
+      // @ingroup ExprNode
+      // These classes represent constant expression nodes. A constant
+      // expression is an expression that can be evaluated compile-time and
+      // has a primitive type like int, double or string.
+
+      //@{
+
+      // Expression node class for an int.
+      class IntExprNode : public ExprNode
+      {
+      public:
+        IntExprNode(int i);
+        virtual ~IntExprNode();
+        virtual void print(std::ostream& os);
+      private:
+        const int itsOperand;
+      };
+
+      // Expression node class for a double.
+      class DoubleExprNode : public ExprNode
+      {
+      public:
+        DoubleExprNode(double d);
+        virtual ~DoubleExprNode();
+        virtual void print(std::ostream& os);
+      private:
+        const double itsOperand;
+      };
+      
+      // Expression node class for a string.
+      class StringExprNode : public ExprNode
+      {
+      public:
+        StringExprNode(const std::string& s);
+//         StringExprNode(const char* const cp);
+        virtual ~StringExprNode();
+        virtual void print(std::ostream& os);
+      private:
+        const std::string itsOperand;
+      };
+
+      //@}
+
+    } // namespace Query
+
+  } // namespace PL
+
+} // namespace LOFAR
+
+#endif
