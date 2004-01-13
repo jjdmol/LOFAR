4a81fa4319925e011e48b517d9ab64f2cf2135b0
meijeren <meijeren@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-01-13 13:34:59 +0000

    %[ER:81]% Initial version of Blackboard demo using CEPFrame and PSS3.

---
+++ CEP/BB/PSS3/build/gnu3_debug/reinit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf demo_gsm.MEP
+cp -R /data/PSS3/CAL/demo_start_gsm.MEP demo_gsm.MEP
+rm -rf demo.MS
+cp -R /data/PSS3/CAL/demo_start.MS demo.MS
+rm -rf demo.MEP
+cp -R /data/PSS3/CAL/demo_start.MEP demo.MEP
---
+++ CEP/BB/PSS3/build/gnu3_debug/run.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+src/BlackBoardDemo < run.in 
+
+
+ 
