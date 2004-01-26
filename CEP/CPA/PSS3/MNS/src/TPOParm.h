596b6fc009f46ff6aae32976fdee6ddae1542026
Ger van Diepen <gervandiepen@users.noreply.github.com>
2004-01-26 08:34:55 +0000

    %[ER: 64]% Use new PL code

--- CEP/CPA/PSS3/MNS/src/TPOParm.h
+++ CEP/CPA/PSS3/MNS/src/TPOParm.h
@@ -27,127 +27,53 @@
 #ifdef HAVE_DTL
 
 #include <PL/TPersistentObject.h>
-#include <PL/DTLBase.h>
+#include <PL/DBRep.h>
 #include <MNS/MeqParmHolder.h>
 
 namespace LOFAR {
   namespace PL {
 
-    class MetaDataRep
-    {
-    public:
-      MetaDataRep()
-        {}
-      void metaBindCols (dtl::BoundIOs& cols);
-      void metaToRep (const PersistentObject&);
-      void metaFromRep (PersistentObject&) const;
-
-      LOFAR::PL::ObjectId::oid_t  itsOid;
-      LOFAR::PL::ObjectId::oid_t  itsOwnerOid;
-      unsigned int     itsVersionNr;
-    };
-
     // The DBRep<MeqParmHolder> structure is a compilation of the fields
     // of the MeqParmHolder class and the persistency layer. It contains
     // all fields that should be stored to the database
-    class DBRep<MeqParmDefHolder>
+    class DBRep<MeqParmDefHolder> : public DBRepMeta
     {
     public:
-      struct Rep : public MetaDataRep
-      {
-	Rep()
-	  {}
-	void bindCols (dtl::BoundIOs& cols);
-	void toRep (const MeqParmDefHolder&);
-	void fromRep (MeqParmDefHolder&) const;
-
-	std::string itsName;
-	int         itsSrcNr;
-	int         itsStatNr;
-	double      itsCoeff00;
-	dtl::blob   itsCoeff;
-	dtl::blob   itsSimCoeff;
-	dtl::blob   itsPertSimCoeff;
-	double      itsTime0;
-	double      itsFreq0;
-	bool        itsNormalized;
-	dtl::blob   itsMask;
-	double      itsDiff;
-	bool        itsDiffRel;
-      };
-
-      DBRep()
-        {}
-
-      Rep& rep()
-        { return itsRep; }
-
-      const Rep& rep() const
-        { return itsRep; }
-
-      LOFAR::PL::ObjectId::oid_t getOid() const
-        { return itsRep.itsOid; }
+      void bindCols (dtl::BoundIOs& cols);
+      void toDBRep (const MeqParmDefHolder&);
+      void fromDBRep (MeqParmDefHolder&) const;
 
     private:
-      Rep itsRep;
+      std::string itsName;
+      int         itsSrcNr;
+      int         itsStatNr;
+      double      itsCoeff00;
+      dtl::blob   itsCoeff;
+      dtl::blob   itsSimCoeff;
+      dtl::blob   itsPertSimCoeff;
+      double      itsTime0;
+      double      itsFreq0;
+      bool        itsNormalized;
+      dtl::blob   itsMask;
+      double      itsDiff;
+      bool        itsDiffRel;
     };
 
 
-    class DBRep<MeqParmHolder>
+    class DBRep<MeqParmHolder> : public DBRep<MeqParmDefHolder>
     {
     public:
-      struct Rep : public DBRep<MeqParmDefHolder>::Rep
-      {
-	Rep()
-	  {}
-	void bindCols (dtl::BoundIOs& cols);
-	void toRep (const MeqParmHolder&);
-	void fromRep (MeqParmHolder&) const;
-
-	double           itsStartTime;
-	double           itsEndTime;
-	double           itsStartFreq;
-	double           itsEndFreq;
-      };
-
-      DBRep()
-        {}
-
-      Rep& rep()
-        { return itsRep; }
-
-      const Rep& rep() const
-        { return itsRep; }
-
-      LOFAR::PL::ObjectId::oid_t getOid() const
-        { return itsRep.itsOid; }
+      void bindCols (dtl::BoundIOs& cols);
+      void toDBRep (const MeqParmHolder&);
+      void fromDBRep (MeqParmHolder&) const;
 
     private:
-      Rep itsRep;
+      double           itsStartTime;
+      double           itsEndTime;
+      double           itsStartFreq;
+      double           itsEndFreq;
     };
 
-
-    // The BCA<MeqParmDefHolder> structure 'binds' the database columns
-    // to the members of the DBRep<MeqParmDefHolder> class.
-    template<>
-    inline void BCA<MeqParmDefHolder>::operator() (dtl::BoundIOs& cols,
-						   DataObj& rowbuf)
-      { rowbuf.rep().metaBindCols(cols); rowbuf.rep().bindCols(cols); }
-
-    // toDatabaseRep copies the fields of the persistency layer
-    // and of the MeqParmDefHolder class to the given DBRep<MeqParmDefHolder>
-    // structure
-    template<>
-    inline void TPersistentObject<MeqParmDefHolder>::toDatabaseRep
-                                         (DBRep<MeqParmDefHolder>& dest) const
-      { dest.rep().metaToRep(*this); dest.rep().toRep(*itsObjectPtr); }
-
-    // fromDatabaseRep copies the fields of the DBRep<MeqParmDefHolder> struct
-    // to the persistency layer and the MeqParmDefHolder class.
-    template<>
-    inline void TPersistentObject<MeqParmDefHolder>::fromDatabaseRep
-                                         (const DBRep<MeqParmDefHolder>& org)
-      { org.rep().metaFromRep(*this); org.rep().fromRep(*itsObjectPtr); }
 
 
     // Initialize the internals of TPersistentObject<MeqParmDefHolder>
@@ -157,28 +83,6 @@ namespace LOFAR {
     // Initialize the attribute map of TPersistentObject<MeqParmDefHolder>
     template<>
     void TPersistentObject<MeqParmDefHolder>::initAttribMap();
-
-    // The BCA<MeqParmHolder> structure 'binds' the database columns
-    // to the members of the DBRep<MeqParmHolder> class.
-    template<>
-    inline void BCA<MeqParmHolder>::operator() (dtl::BoundIOs& cols,
-						DataObj& rowbuf)
-      { rowbuf.rep().metaBindCols(cols); rowbuf.rep().bindCols(cols); }
-
-    // toDatabaseRep copies the fields of the persistency layer
-    // and of the MeqParmHolder class to the given DBRep<MeqParmHolder>
-    // structure
-    template<>
-    inline void TPersistentObject<MeqParmHolder>::toDatabaseRep
-                                         (DBRep<MeqParmHolder>& dest) const
-      { dest.rep().metaToRep(*this); dest.rep().toRep(*itsObjectPtr); }
-
-    // fromDatabaseRep copies the fields of the DBRep<MeqParmHolder> struct
-    // to the persistency layer and the MeqParmHolder class.
-    template<>
-    inline void TPersistentObject<MeqParmHolder>::fromDatabaseRep
-                                         (const DBRep<MeqParmHolder>& org)
-      { org.rep().metaFromRep(*this); org.rep().fromRep(*itsObjectPtr); }
 
 
     // Initialize the internals of TPersistentObject<MeqParmHolder>
