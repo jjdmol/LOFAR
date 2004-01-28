93960c06b8c31fe41c69e034c18a86548acd6232
smirnov <smirnov@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-01-28 15:44:44 +0000

    %[ER: 16]% Misc cleanups in test scripts

--- trunk/CEP/CPA/AppAgent/MeqServer/test/solver_test.sh
+++ trunk/CEP/CPA/AppAgent/MeqServer/test/solver_test.sh
@@ -2,4 +2,8 @@
 if [ ! -L meqserver ]; then
   ln -s ../src/meqserver .
 fi
-glish -l ${srcdir}/solver_test.g -runtest 2>&1 | tee solver_test.log
+glish -l ${srcdir}/solver_test.g -runtest &>solver_test.log
+retval=$?
+cat solver_test.log
+echo "Glish exited with status $retval"
+exit $retval
