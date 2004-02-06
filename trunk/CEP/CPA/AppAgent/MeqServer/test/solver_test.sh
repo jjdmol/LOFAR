ddf7eb51311233c4ea20d1a6275bec339f70b372
smirnov <smirnov@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-02-06 11:11:28 +0000

    %[ER: 16]% Fixes to tests; solver test now has three phases

--- trunk/CEP/CPA/AppAgent/MeqServer/test/solver_test.sh
+++ trunk/CEP/CPA/AppAgent/MeqServer/test/solver_test.sh
@@ -5,6 +5,9 @@ fi
 if [ ! -L meqserver.g ]; then
   ln -s ${srcdir}/../src/meqserver.g .
 fi
+if [ ! -L parmtable.g ]; then
+  ln -s ${srcdir}/../../../PSS4/MEQ/src/parmtable.g .
+fi
 glish -l ${srcdir}/solver_test.g -runtest &>solver_test.log
 retval=$?
 cat solver_test.log
