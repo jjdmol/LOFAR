027ff6660d6b79de475b56ce09629c7c7f9e293f
Ger van Diepen <gervandiepen@users.noreply.github.com>
2004-02-23 07:50:40 +0000

    %[ER: 38]% Added a few more tests to handle unconverted data

--- LCS/Blob/test/tBlobField.cc
+++ LCS/Blob/test/tBlobField.cc
@@ -28,6 +28,7 @@
 #include <Common/BlobOBufStream.h>
 #include <Common/BlobIBufStream.h>
 #include <Common/BlobIBufVector.h>
+#include <Common/BlobHeader.h>
 #include <Common/DataConvert.h>
 #include <Common/Debug.h>
 #include <iostream>
@@ -68,13 +69,14 @@ template class BlobField<XX>;
 void doOut (BlobFieldSet& fset, BlobOBufChar& bb)
 {
   // Create the blob for the given field set.
-  BlobOStream bs(&bb);
   fset.createBlob (bb);
   // Check if fields are correct.
   Assert (fset[0].getNelem() == 1);
   Assert (fset[0].isScalar());
   Assert (fset[1].getNelem() == 20);
   Assert (! fset[1].isScalar());
+  Assert (fset[2].getNelem() == 2);
+  Assert (! fset[2].isScalar());
   // Get pointers to the data in the buffer.
   int* pint = fset[0].getData<int> (bb);
   fcomplex* pfcomplex = fset[1].getData<fcomplex> (bb);
@@ -91,14 +93,15 @@ void doOut (BlobFieldSet& fset, BlobOBufChar& bb)
 // Function to read back and check a blob.
 // The field set must have the offsets filled in, so the field set used
 // in doOut can be used.
-void doIn (BlobFieldSet& fset, BlobIBufChar bb)
+void doIn (BlobFieldSet& fset, BlobIBufChar& bb)
 {
-  BlobIStream bs(&bb);
   // Check if fields are correct.
   Assert (fset[0].getNelem() == 1);
   Assert (fset[0].isScalar());
   Assert (fset[1].getNelem() == 20);
   Assert (! fset[1].isScalar());
+  Assert (fset[2].getNelem() == 2);
+  Assert (! fset[2].isScalar());
   // Get pointers to the data.
   const int* pint = fset[0].getData<int> (bb);
   const fcomplex* pfcomplex = fset[1].getData<fcomplex> (bb);
@@ -112,6 +115,49 @@ void doIn (BlobFieldSet& fset, BlobIBufChar bb)
   Assert (px[1] == XX(2,1));
 }
 
+// Function to interpret and check a blob.
+void doIn2 (BlobFieldSet& fset, BlobIBufChar& bb)
+{
+  bool convert = BlobFieldSet::checkHeader (bb, 0, 0, bb.size());
+  fset.openBlob (bb);
+  // Check if fields are correct.
+  Assert (fset[0].getNelem() == 1);
+  Assert (fset[0].isScalar());
+  Assert (fset[1].getNelem() == 20);
+  Assert (! fset[1].isScalar());
+  Assert (fset[2].getNelem() == 2);
+  Assert (! fset[2].isScalar());
+  // Get pointers to the data.
+  const int* pint = fset[0].getData<int> (bb);
+  const fcomplex* pfcomplex = fset[1].getData<fcomplex> (bb);
+  const XX* px = fset[2].getData<XX> (bb);
+  // Check if data matches as put in doOut.
+  if (convert) {
+    LOFAR::DataFormat fmt =
+      ((BlobHeaderBase*)(bb.getBuffer()))->getDataFormat();
+    fcomplex valfc;
+    XX valxx(0,0);
+    Assert (LOFAR::dataConvert (fmt, *pint) == 3);
+    for (uint i=0; i<fset[1].getNelem(); i++) {
+      LOFAR::dataConvertFloat (fmt, &valfc, pfcomplex+i, 2);
+      Assert (valfc == fcomplex(i+1.,i+2.));
+    }
+    memcpy (&valxx, px, sizeof(XX));
+    LOFAR::dataConvert (fmt, &valxx, 1);
+    Assert (valxx == XX(1,2));
+    memcpy (&valxx, px+1, sizeof(XX));
+    LOFAR::dataConvert (fmt, &valxx, 1);
+    Assert (valxx == XX(2,1));
+  } else {
+    Assert (*pint == 3);
+    for (uint i=0; i<fset[1].getNelem(); i++) {
+      Assert (pfcomplex[i] == fcomplex(i+1.,i+2.));
+    }
+    Assert (px[0] == XX(1,2));
+    Assert (px[1] == XX(2,1));
+  }
+}
+
 // Read back and check a blob in a file.
 // Note that the blob as created in doOut is written in the file as a
 // byte array packed in another blob.
@@ -121,12 +167,16 @@ void readFile (BlobFieldSet& fset, BlobFieldSet& fset2,
   // Read the blob as a byte array from the file.
   std::ifstream is (fileName);
   BlobIBufStream bibs(is);
-  BlobIStream bis(&bibs);
+  BlobIStream bis(bibs);
   std::vector<char> buf;
   bis >> buf;
   // Use the byte array as a buffer and check its header.
   BlobIBufVector<char> bbuf(buf);
   bool cv = BlobFieldSet::checkHeader (bbuf, "test1", 1, 0);
+  // Interpret the data directly.
+  BlobFieldSet fsetc(fset);
+  doIn2 (fsetc, bbuf);
+  bbuf.setPos (0);
   // Convert the data if needed.
   if (cv) {
     fset.convertData (bbuf);
@@ -165,7 +215,7 @@ int main()
 	// Write the blob in a file (as an array of bytes).
 	std::ofstream os ("tBlobField_tmp.dat");
 	BlobOBufStream bobs(os);
-	BlobOStream bos(&bobs);
+	BlobOStream bos(bobs);
 	putBlobVector (bos, bstr.data(), bstr.size());
       }
       // Reinterpret the blob from the file.
