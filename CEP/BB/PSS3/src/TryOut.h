94d71d81b70631296ded5b80dfeb89d85a10adef
tanaka <tanaka@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-15 10:28:15 +0000

    %[ER: 72]%
    Initial creation.

---
+++ CEP/BB/PSS3/src/TryOut.h
@@ -0,0 +1,12 @@
+
+#ifndef __BB_TRYOUT_H__
+#define __BB_TRYOUT_H__
+
+// TryOut.cc contains many (serial) strategy try-outs. These were used
+// to investigate the different flows of the calibration algorithm and
+// check the suitability for BlackBoard.
+
+void TryOut (void);
+
+#endif
+
