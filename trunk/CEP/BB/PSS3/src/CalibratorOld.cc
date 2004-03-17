0ba9389b6bcfa164b265e4c629149430a7df2804
tanaka <tanaka@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-17 15:00:10 +0000

    %[ER: 72]%
    Addition showCurentOutputParm () method.

--- trunk/CEP/BB/PSS3/src/CalibratorOld.cc
+++ trunk/CEP/BB/PSS3/src/CalibratorOld.cc
@@ -162,6 +162,30 @@ void CalibratorOld::ShowSettings (void) {
 }
 
 
+void CalibratorOld::showCurrentParms (void) {
+  vector <double> vals;
+  getParmValues (itsSolvableParms, vals);
+  char str [20];
+  char str2 [20];
+
+  vector<string> :: iterator i;
+  for (i = itsSolvableParms.begin (); i != itsSolvableParms.end (); ++ i) {
+    strcpy (str2, i -> c_str ());
+    str2[5] = 0;
+    sprintf (str, "%4s ", str2);
+    cout << str;
+  }
+  cout << endl;
+
+  vector<double> :: iterator j;
+  for (j = vals.begin (); j != vals.end (); ++ j) {
+    sprintf (str, "%1.3f ", * j);
+    cout << str;
+  }
+  cout << endl;
+
+}
+
 void CalibratorOld::clearSolvableParms (void) {
   itsSolvableParms.erase (itsSolvableParms.begin (), itsSolvableParms.end ());
 
