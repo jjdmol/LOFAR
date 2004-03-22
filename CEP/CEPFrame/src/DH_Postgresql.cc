997e3876dbd7d4c4345eb6f57e66ea23106dda16
tanaka <tanaka@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-22 13:09:24 +0000

    %[ER: 59]% Addition of comments/

--- CEP/CEPFrame/src/DH_Postgresql.cc
+++ CEP/CEPFrame/src/DH_Postgresql.cc
@@ -1,4 +1,4 @@
- //  DH_Postgresql.cc: Database persistent DH_Database
+//  DH_Postgresql.cc: Database persistent DH_Database
 //
 //  Copyright (C) 2000, 2002
 //  ASTRON (Netherlands Foundation for Research in Astronomy)
@@ -22,23 +22,21 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
-#include <DH_Postgresql.h>			// for class definition
-#include <Common/lofar_iostream.h>		// for cout, cerr
-#include <Common/Stopwatch.h>			// for Stopwatch class
-#include <Common/Debug.h>			// for AssertStr
-
-
 #include <sstream>				// for ostringstream
 #include <sys/time.h>				// for ctime (), time ()
 #include <unistd.h>				// for gethostname ()
 #include <stdlib.h>				// for strtoul
+#include <Common/lofar_iostream.h>		// for cout, cerr
+#include <Common/Stopwatch.h>			// for Stopwatch class
+#include <Common/Debug.h>			// for AssertStr
+#include <DH_Postgresql.h>			// for class definition
 
 using namespace std;
 
 namespace LOFAR{
 
-
 ulong    DH_Postgresql::theirInstanceCount = 0L;
+
 #ifdef HAVE_PSQL
 PGconn * DH_Postgresql::theirConnection = NULL;
 #endif
@@ -48,31 +46,29 @@ string DH_Postgresql::theirDBName = "TransportHolder";
 string DH_Postgresql::theirUserName = "tanaka";
 
 
-
 DH_Postgresql::DH_Postgresql (const string& name, const string& type)
   : DH_Database (name, type) {
 
+  // If this is the first DH_Postgresql object being created in the current
+  // process, establish a connection with the Postgresql database.
   if (DH_Postgresql::theirInstanceCount == 0L) {
     ConnectDatabase ();
   }
 
   DH_Postgresql::theirInstanceCount ++;
-
-  //  cerr << DH_Postgresql::theirInstanceCount 
-  //    << ") DH_Postgresql constructed." << endl;
 } 
 
 
 DH_Postgresql::~DH_Postgresql () {
-  //  cerr << DH_Postgresql::theirInstanceCount
-  //    << ") DH_Postgresql destroyed." << endl;
-
   DH_Postgresql::theirInstanceCount --;
 
+  // If this is the last DH_Postgresql object being destroyed in the current
+  // process, destroy the connection with the Postgresql database.
   if (DH_Postgresql::theirInstanceCount == 0L) {
     DisconnectDatabase ();
   }
 }
+
 
 void DH_Postgresql::UseDatabase 
   (char * dbHost, char * dbName, char * userName) {
@@ -80,7 +76,6 @@ void DH_Postgresql::UseDatabase
   DH_Postgresql::theirDBName   = dbName;
   DH_Postgresql::theirUserName = userName;
 }
-
 
 
 void DH_Postgresql::ConnectDatabase (void) {
@@ -122,11 +117,12 @@ void DH_Postgresql::DisconnectDatabase (void) {
 #endif
 }
 
+// Maximum size of Postgresql blob, which is used for communication.
 #define MAX_BYTEA_SIZE (5*4096)
 
 bool DH_Postgresql::StoreInDatabase (int, int tag, char * buf, int size) {
 #ifdef HAVE_PSQL
-  // First create blob:
+  // First create a string based blob and fill it with the content of buf:
   int i;
   ostringstream ostr;
 
@@ -136,7 +132,7 @@ bool DH_Postgresql::StoreInDatabase (int, int tag, char * buf, int size) {
     ostr << hexrep;
   }
 
-  // Create a postgres byte string
+  // Now create a postgres blob and fill it with the content of the buf.
   ostringstream ByteString;
 
 #if defined (__GNUC__) && (__GNUC__) > 2
@@ -145,8 +141,6 @@ bool DH_Postgresql::StoreInDatabase (int, int tag, char * buf, int size) {
   	       << PQescapeBytea ((unsigned char *)
   		    buf, size, &len)
   	       << "'::bytea";
-    // TODO check len with MAX_BYTEA_SIZE
-    // TODO Maybe must free buffer returned from fuction.
 #else
     ByteString << "'byteaNotSupported'::bytea";
 #endif
@@ -167,6 +161,12 @@ bool DH_Postgresql::StoreInDatabase (int, int tag, char * buf, int size) {
     << ByteString.str () << ");";
 
   ExecuteSQLCommand (q);
+
+  // Both the string based blob as the postgresql blob are stored in the
+  // database due to incompatibility issues on lofar3. If the receiving
+  // end is lofar3, the string based blob is used to read out the message
+  // content. If the receiving end is not lofar3, the postgresql blob is
+  // used to read out the message content.
 
   itsWriteSeqNo ++;
 
@@ -196,13 +196,14 @@ bool DH_Postgresql::RetrieveFromDatabase (int,int tag, char * buf, int size) {
 
   PGresult * res;
 
-  // Block until a packet appears in the table
+  // The following do-while loop implements the blocking behaviour of
+  // DH_Postgresql. It waits until a records appears in the database with
+  // the correct readSeqNo.
   do {
     res = PQexec (DH_Postgresql::theirConnection, (q.str ()).c_str ());
   
     AssertStr (PQresultStatus (res) == PGRES_TUPLES_OK,
 	       "DH_Postgressql::Retrieve (); Select query failed.")
-      // TODO: Do a PQclear here?
   } while (PQntuples (res) == 0);
 
   int nRows;
@@ -210,24 +211,29 @@ bool DH_Postgresql::RetrieveFromDatabase (int,int tag, char * buf, int size) {
   AssertStr (nRows == 1, "DH_Postgresql::Retrieve ();"
     << "ERROR: Message table may not have been cleaned up before starting program.")
   
-    // TODO: What to do with these?:
-  // Read the found tuple
-    //  setMessageTag       (atoi (PQgetvalue (res, 0, 1)));
-    //  setByteStringLength (atoi (PQgetvalue (res, 0, 4)));
-    //  setTimeStamp        (atoi (PQgetvalue (res, 0, 5)));
-    //  setType             (PQgetvalue (res, 0, 6));
-    //  setName             (PQgetvalue (res, 0, 7));
+  // Communication of the following fields are (yet) optional:
+  // setMessageTag       (atoi (PQgetvalue (res, 0, 1)));
+  // setByteStringLength (atoi (PQgetvalue (res, 0, 4)));
+  // setTimeStamp        (atoi (PQgetvalue (res, 0, 5)));
+  // setType             (PQgetvalue (res, 0, 6));
+  // setName             (PQgetvalue (res, 0, 7));
 
   // Copy the data packet
+
 #if defined (__GNUC__) && (__GNUC__) > 2
+  // If the receiving end is not lofar3, the postgresql blob is used to
+  // read out the message content.
+
   unsigned char * ByteString;
   size_t sizeRes;
   ByteString = PQunescapeBytea 
     ((unsigned char*)PQgetvalue (res, 0, 9), & sizeRes);
   
   memcpy (buf, ByteString, size);
-  // TODO: Must free ByteString here?
 #else
+  // If the receiving end is lofar3, the string based blob is used to read
+  // out the message content. 
+
   unsigned int k;
   char token[40];
   istringstream istr (PQgetvalue (res, 0, 8));
