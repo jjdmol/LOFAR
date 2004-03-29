c37c16638cdfaede2a05c20f3621801f000307f4
Chris Broekema <broekema@users.noreply.github.com>
2004-03-29 07:25:22 +0000

    Entering first stab at tinyFrame code into CVS. It compiles, that's all I'm willing to say about it.

---
+++ CEP/CEPFrame/tinyCF/src/WorkBlock.cc
@@ -0,0 +1,59 @@
+//
+//  WorkBlock.cc: 
+//
+//  Copyright (C) 2000-2004
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+
+#include "WorkBlock.h"
+
+namespace LOFAR
+{
+
+WorkBlock::WorkBlock (int inputs, int outputs, 
+		      const string& name)
+  : itsName     (name),
+    itsNinputs  (inputs),
+    itsNoutputs (outputs) {
+}
+
+
+WorkBlock::WorkBlock (const WorkBlock& that) 
+  : itsName     (that.itsName),
+    itsNinputs  (that.itsNinputs),
+    itsNoutputs (that.itsNoutputs) {
+}
+
+WorkBlock::~WorkBlock() {
+}
+
+void WorkBlock::preprocess() {
+}
+
+void WorkBlock::process() {
+}
+
+void WorkBlock::postprocess() {
+}
+
+void WorkBlock::dump() {
+}
+
+} // namespace
