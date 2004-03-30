b1572f4c0180c9b8406a5c73518b499db074c069
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-30 07:13:45 +0000

    %[ER: 105]% Inserted the database access handling from DH_PL

--- trunk/CEP/Transport/src/TH_PL.cc
+++ trunk/CEP/Transport/src/TH_PL.cc
@@ -27,101 +27,125 @@
 #include <BaseTransport.h>
 #include <Transportable.h>
 #include <Common/lofar_iostream.h>
+#include <PL/Query.h>
 
 namespace LOFAR
 {
 
+// Default data source and account names for PL. You should override them
+// with a call to UseDatabase () in main ().
+string TH_PL::theirDSN = "tanaka";
+string TH_PL::theirUserName = "postgres";
+
+int TH_PL::theirInstanceCount=0;   
+   
 // The PersistenceBroker is a management object from PL. One static
 // PersistenceBroker is used to optimally use resources.
-
 PersistenceBroker TH_PL::theirPersistenceBroker;
 
 
 TH_PL TH_PL::proto;
 
-TH_PL::TH_Database() {}
+TH_PL::TH_PL() :
+   itsWriteSeqNo(0),
+   itsReadSeqNo(0)
+{
+  // If this is the first instance of TH_PL in the (current) process,
+  // connect to the database.
+  if (TH_PL::theirInstanceCount == 0L) {
+    ConnectDatabase ();
+  }
+  TH_PL::theirInstanceCount ++;
+}
 
-TH_PL::~TH_Database() {}
+TH_PL::~TH_PL() {
+   TH_PL::theirInstanceCount --;
+
+  // If this was the last instance of DH_PL, disconnect from the database. 
+
+  if (TH_PL::theirInstanceCount == 0L) {
+    DisconnectDatabase ();
+  }
+}
 
 TH_PL* TH_PL::make() const
-  { return new TH_Database(); }
+  { return new TH_PL(); }
 
 void TH_PL::UseDatabase (char * dbDSN, char * userName) {
-  DH_PL::theirDSN   = dbDSN;
-  DH_PL::theirUserName = userName;
+  TH_PL::theirDSN   = dbDSN;
+  TH_PL::theirUserName = userName;
 }
 
 void TH_PL::ConnectDatabase (void) {
-  if (DH_PL::theirDSN == "tanaka") {
-    cerr << "***WARNING***: DH_PL::ConnectDatabase (); DH_PL "
+  if (TH_PL::theirDSN == "tanaka") {
+    cerr << "***WARNING***: TH_PL::ConnectDatabase (); TH_PL "
 	 << "is trying to connect to a test database residing on "
-	 << "dop50. You probably have forgotten to call the DH_PL "
-	 << "method UseDatabase (). See the comments in DH_PL.h " 
+	 << "dop50. You probably have forgotten to call the TH_PL "
+	 << "method UseDatabase (). See the comments in TH_PL.h " 
 	 << "for more detaills. Continuing execution... " << endl;
   }
 
-  theirPersistenceBroker.connect (DH_PL::theirDSN, DH_PL::theirUserName);
+  theirPersistenceBroker.connect (TH_PL::theirDSN, TH_PL::theirUserName);
 }
 
 
 void TH_PL::DisconnectDatabase (void) {
 }
 
-string TH_Database::getType() const
-  { return "TH_Database"; }
+string TH_PL::getType() const
+  { return "TH_PL"; }
 
-bool TH_Database::connectionPossible(int srcRank, int dstRank) const
+bool TH_PL::connectionPossible(int srcRank, int dstRank) const
   { return srcRank == dstRank; }
 
-bool TH_Database::send(void* buf, int nbytes, int, int tag)
+bool TH_PL::send(void* buf, int nbytes, int, int tag)
 {
   if (getTransporter() -> getBaseDataHolder() != 0) {
         
-    DH_PL* DHPLptr = getBaseTransport () -> getTransportable ();
+    DH_PL* DHPLptr = (DH_PL*)getTransporter () -> getBaseDataHolder ();
     // ToDo: DbgAssertStr(DHPLptr->getType = "", "Wrong DH type;")
 
-    DHPLptr->setSeqNo(itsWriteSeqNo ++);
+    //ToDo:  DHPLptr->setSeqNo(itsWriteSeqNo ++);
     theirPersistenceBroker.save(DHPLptr -> getPO());
 
   } else {
-    cerr << "TH_Database::send():Transportable not found." << endl;
+    cerr << "TH_PL::send():Transportable not found." << endl;
     return false;
   }
 
   return true;
 }
 
-bool TH_Database::recv(void* buf, int nbytes, int, int tag)
+bool TH_PL::recv(void* buf, int nbytes, int, int tag)
 { 
   DbgAssertStr( (getTransporter() -> getBaseDataHolder () != 0),
-		"TH_Database::recv():Transportable not found.");
+		"TH_PL::recv():Transportable not found.");
   
   // get a refernece to the DHPL object
-  DH_PL* DHPLptr = getTransporter() -> getBaseDataHolder();
+  DH_PL* DHPLptr = (DH_PL*)getTransporter() -> getBaseDataHolder();
   
-  // get the massage record form the DH_PL
-  DH_PL_MessageRecord *mr = DHPLptr->itsMR;
-  
+ 
   // PL is based on query objects to identify records in database
   // tables. A query object is now prepared to retrieve the record with
   // the correct tag and sequence number.
   char q [200]; 
   sprintf (q, "WHERE tag='%d' AND SeqNo='%ld'",
-	   tag, itsReadSeqNo);
-  Query qGetRecord (q);
+	   tag, 
+	   itsReadSeqNo);
+   PL::QueryObject qGetRecord(q);
     
   // Perform the actual query and obtain the data collection.
-  Results = theirPersistenceBroker.retrieve<DH_PL_MessageRecord> (qGetRecord);
+   Results = theirPersistenceBroker.retrieve<DH_PL::DataPacket> (qGetRecord);
   
   // ToDo: DbgAssertStr(itsReadSeqnNo == ... ,"");
   itsReadSeqNo ++;
 
-  // Take the size of the first DH_PL_MessageRecord in the collection.
+  // Take the size of the first TH_PL_MessageRecord in the collection.
   int resSize = Results.begin () -> data().Size;
   
   // Check its size.
   DbgAssertStr((resSize == size),
-	       "DH_PL Warning: Not matching size, continuing anyway.");
+	       "TH_PL Warning: Not matching size, continuing anyway.");
   
   // Unpack the blob
   unsigned int k;
@@ -134,14 +158,14 @@ bool TH_Database::recv(void* buf, int nbytes, int, int tag)
   return true;
 }
 
-void TH_Database::waitForBroadCast () {}
-void TH_Database::waitForBroadCast (unsigned long&) {}
-void TH_Database::sendBroadCast (unsigned long) {}
-int  TH_Database::getCurrentRank () { return -1; }
-int  TH_Database::getNumberOfNodes () { return 1; }
-void TH_Database::init (int, const char * []) {}
-void TH_Database::finalize () {}
-void TH_Database::synchroniseAllProcesses () {}
+void TH_PL::waitForBroadCast () {}
+void TH_PL::waitForBroadCast (unsigned long&) {}
+void TH_PL::sendBroadCast (unsigned long) {}
+int  TH_PL::getCurrentRank () { return -1; }
+int  TH_PL::getNumberOfNodes () { return 1; }
+void TH_PL::init (int, const char * []) {}
+void TH_PL::finalize () {}
+void TH_PL::synchroniseAllProcesses () {}
 
 }
 
