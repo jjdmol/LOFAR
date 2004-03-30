8d10831abc7588b5f4f0c118b89b8a53bb6d0b35
Chris Broekema <broekema@users.noreply.github.com>
2004-03-30 14:55:14 +0000

    %[ER: 105]% Add a connection class to facilitate connections between Transporters.

---
+++ CEP/Transport/src/Connection.h
@@ -0,0 +1,52 @@
+//#  Connection.h: 
+//#
+//#  Copyright (C) 2002-2003
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LIBTRANSPORT_CONNECTION_H
+#define LIBTRANSPORT_CONNECTION_H
+
+#include <lofar_config.h>
+#include <Transporter.h>
+
+// I should not have to include this, right? -- CB
+#include <Common/Debug.h>
+
+namespace LOFAR
+{
+  // Description of class.
+  class Connection: public Transporter
+  {
+  public:
+    Connection(Transporter&);
+    virtual ~Connection();
+
+    bool connectRep(const TransportHolder& prototype,
+		    Transporter& sourceTP,
+		    Transporter& targetTP);
+
+    bool connectData(const TransportHolder& prototype,
+		     Transporter& sourceTP, 
+		     Transporter& targetTP);
+  };
+
+} // namespace LOFAR
+
+#endif
