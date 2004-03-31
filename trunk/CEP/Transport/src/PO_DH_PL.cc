468a77003f0fadcba827290ed81720818b73d6ed
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-31 13:07:05 +0000

    %[ER: 105]% TPO functions for DH_PL

---
+++ trunk/CEP/Transport/src/PO_DH_PL.cc
@@ -0,0 +1,74 @@
+//# DH_PL_PO,cc.cc: Persistency object for DH_PL
+//#
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <libTransport/PO_DH_PL.h>
+#include <PL/TPersistentObject.h>
+#include <Common/BlobOBufString.h>
+#include <Common/BlobIBufString.h>
+#include <Common/BlobOStream.h>
+#include <Common/BlobIStream.h>
+#include <Common/BlobArray.h>
+#include <string>
+
+
+namespace LOFAR {
+  namespace PL {
+
+
+void DBRep<DH_PL>::bindCols (dtl::BoundIOs& cols)
+{
+  cols["TIMESTAMP"]  == itsTimeStamp;
+  cols["SEQNR"]      == itsSeqNr;
+  cols["TAG"]        == itsTag;
+  cols["DATA"]       == itsData;
+}
+
+void DBRep<DH_PL>::toDBRep (const DH_PL& obj)
+{
+  // Copy the info from DH_PL
+  itsTimeStamp = obj.getName();
+  itsSeqNr     = obj.getSourceNr();
+  itsTag       = obj.getStation();
+  itsData      = obj.getPolc().getCoeff().getDouble();
+}
+
+// fromDatabaseRep copies the fields of the DBRep<MeqParmHolder> structure
+// to the persistency layer and the MeqParmHolder class.
+void DBRep<DH_PL>::fromDBRep (DH_PL& obj) const
+{
+  // Finally copy the info to DH_PL
+  obj.setData     (itsName);
+  obj.setSourceNr (itsSrcNr);
+  obj.setStation  (itsStatNr);
+}
+
+
+
+//# Force the instantiation of the templates.
+template class TPersistentObject<DH_PL>;
+template class DBRep<DH_PL>;
+
+
+  }  // end namespace PL
+}    // end namespace LOFAR
+
+#endif
