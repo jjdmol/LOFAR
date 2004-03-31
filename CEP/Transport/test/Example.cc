c71cdb26aed08a356e0e53905be83d2a7dd0cea3
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-31 15:58:55 +0000

    %[ER: 105]%
    many typos etc. in order to get these sources to compile

--- CEP/Transport/test/Example.cc
+++ CEP/Transport/test/Example.cc
@@ -1,51 +1,53 @@
 #include <iostream>
 
-#include <libTransporter/Transporter.h>
-#include "DH_Example.cc"
+#include <libTransport/Transporter.h>
+#include <libTransport/TH_Mem.h>
+#include <DH_Example.h>
 
 
-namespace LOFAR
+namespace LOFAR {
 
-
-void main() {
-
-  cout << "libTransport Example test program" << endl;
-
-  DH_Example DH1();
-  DH_Example DH2();
-
-  // Create the Transporter objects containing the DataHolders
-  Transporter TR1(DH1);
-  Transporter TR2(DH2);
-
-  // connect DH1 to DH2
-  TR2.ConnectTo(TR1, TH_Mem, nonblocking);
   
-  //initiate the DataHolders
-  DH1.preproces();
-  DH2.preproces();
-
-  // fill the DataHolders with some initial data
-  DH1.getBuffer() = 17;
-  DH2.getBuffer() = 0;
-
-  cout << "Before transport : " 
-       << DH1.getBuffer() 
-       << " -- " 
-       << DH2.getBuffer() 
-       << endl;
-
-  // do the data transport
-  DH1.write();
-  DH2.read();
-
-  cout << "After transport : " 
-       << DH1.getBuffer() 
-       << " -- " 
-       << DH2.getBuffer() 
-       << endl;
-}
-
+  void main() {
+    
+    cout << "libTransport Example test program" << endl;
+    
+    DH_Example DH1();
+    DH_Example DH2();
+    
+    // Create the Transporter objects containing the DataHolders
+    Transporter TR1((BaseDataHolder*)&DH1);
+    Transporter TR2((BaseDataHolder*)&DH2);
+    
+    // connect DH1 to DH2
+    TH_Mem proto;
+    TR2.ConnectTo(&TR1, (TransportHolder)proto);
+    
+    //initiate the DataHolders
+    DH1.preproces();
+    DH2.preproces();
+    
+    // fill the DataHolders with some initial data
+    DH1.getBuffer() = 17;
+    DH2.getBuffer() = 0;
+    
+    cout << "Before transport : " 
+	 << DH1.getBuffer() 
+	 << " -- " 
+	 << DH2.getBuffer() 
+	 << endl;
+    
+    // do the data transport
+    DH1.write();
+    DH2.read();
+    
+    cout << "After transport : " 
+	 << DH1.getBuffer() 
+	 << " -- " 
+	 << DH2.getBuffer() 
+	 << endl;
+  }
+  
 
 
 } //namespace LOFAR
