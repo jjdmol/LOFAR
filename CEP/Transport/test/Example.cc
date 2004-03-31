4a973cf05e0e3a96b76f1974477ab8620ffb3914
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-03-31 07:21:36 +0000

    %[ER: 105]%
    Pseudo code outline of the test program

--- CEP/Transport/test/Example.cc
+++ CEP/Transport/test/Example.cc
@@ -13,11 +13,38 @@ void main() {
   DH_Example DH1();
   DH_Example DH2();
 
+  // Create the Transporter objects containing the DataHolders
   Transporter TR1(DH1);
   Transporter TR2(DH2);
 
-  TR2.ConnectTo(TR1);
+  // connect DH1 to DH2
+  TR2.ConnectTo(TR1, TH_Mem, nonblocking);
+  
+  //initiate the DataHolders
+  DH1.preproces();
+  DH2.preproces();
 
+  // fill the DataHolders with some initial data
+  DH1.getBuffer() = 17;
+  DH2.getBuffer() = 0;
+
+  cout << "Before transport : " 
+       << DH1.getBuffer() 
+       << " -- " 
+       << DH2.getBuffer() 
+       << endl;
+
+  // do the data transport
+  DH1.write();
+  DH2.read();
+
+  cout << "After transport : " 
+       << DH1.getBuffer() 
+       << " -- " 
+       << DH2.getBuffer() 
+       << endl;
 }
+
+
 
 } //namespace LOFAR
