ead7d50023d3cd1349eefb31f8c522821fda1533
Chris Broekema <broekema@users.noreply.github.com>
2004-04-14 11:04:16 +0000

    Removed duplicate files.

--- CEP/tinyCEP/src/AppController.cc
+++ CEP/tinyCEP/src/AppController.cc
@@ -24,7 +24,7 @@
 
 #include <tinyCEP/AppController.h>
 #include <libTransport/BaseSim.h>
-#include <tinyCEP/SimulatorParseClass.h>
+#include <tinyCEP/AppControllerParseClass.h>
 #include TRANSPORTERINCLUDE
 
 namespace LOFAR
