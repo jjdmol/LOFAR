38ace60364152b135537727dd89bbd42d5053116
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2004-04-23 21:36:22 +0000

    %[ER:109 ]%
    Updated to compile SWIG binding correctly.

---
+++ MAC/Test/PROTO/EventExt/test_pybind.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from pybind import *
+
+if __name__ == "__main__":
+
+    ba = ABSBeamAllocEvent()
+    print "length=", ba.length
+    bae = ABSBeamAllocEventExt(ba)
+    print "length=", ba.length
+
+    ba.param1 = 10
+    ba.param2 = 100
+
+    print ba.param1
+    print ba.param2
+    print ba.length
+    print ba.signal
+
+    bae.ext1Dim = 100
+    ext1 = int_array(bae.ext1Dim)
+    for i in range(bae.ext1Dim):
+        ext1[i] = i
+
+    bae.ext1 = ext1
+    del ext1
+
+    bae.ext2 = "test"
+    bae.ext2Dim = len(bae.ext2)
+
+    print bae
+
+    
