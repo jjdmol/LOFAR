9b6a57278f090591cecd2e3cb76c23fe99378573
mueller <mueller@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-04-28 10:35:35 +0000

    %[ER: 107]%

--- trunk/MAC/Test/PROTO/EventExt/ExampleUse.cc
+++ trunk/MAC/Test/PROTO/EventExt/ExampleUse.cc
@@ -4,57 +4,12 @@
 
 using namespace std;
 
-class TransObject : public GCFTransportable
-{
-  public:
-    TransObject(int aValue1, float aValue2, string aValue3) :
-      value1(aValue1),
-      value2(aValue2),
-      value3(aValue3)
-    {
-    }
-    TransObject() :
-      value1(0),
-      value2(0.0),
-      value3("")
-    {
-    }
-    virtual ~TransObject() {};
-    unsigned int pack(char* buffer)
-    {
-      unsigned int offset = 0;
-      memcpy(buffer + offset, &value1, sizeof(value1));
-      offset += sizeof(value1);
-      memcpy(buffer + offset, &value2, sizeof(value2));
-      offset += sizeof(value2);
-      offset += packString(buffer + offset, value3);
-      return offset;
-    }
-    unsigned int unpack(char* buffer)
-    {
-      unsigned int offset = 0;
-      memcpy(&value1, buffer + offset, sizeof(value1));
-      offset += sizeof(value1);
-      memcpy(&value2, buffer + offset, sizeof(value2));
-      offset += sizeof(value2);
-      offset += unpackString(value3, buffer + offset);
-      return offset;
-    }
-    unsigned int getSize()
-    {
-      return sizeof(value1) + sizeof(value2) + sizeof(unsigned int) + value3.length();
-    }
-  
-    int value1;
-    float value2;
-    string value3;  
-}; 
 #include "MY_Protocol.ph"
 
 void recv(GCFEvent& e)
 {
     ABSBeamAllocEvent* ba = (ABSBeamAllocEvent*)(&e);
-    ABSBeamAllocEventExt bae(*ba, true);
+    ABSBeamAllocEventExt bae(*ba, GCFEventExt::RECEIVED);
 
     bae.base.param1;
     bae.base.param2;
