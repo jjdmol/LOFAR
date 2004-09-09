fae1994d4164f004dafe600e95a0edad025cfff4
nobody <nobody@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-09-09 15:23:33 +0000

    This commit was manufactured by cvs2svn to create tag 'MAC-ABS-2_0'.

---
+++ tags/MAC-ABS-2_0/MAC/APL/PAC/BeamServer/src/ABSBeamlet.cc
@@ -0,0 +1,211 @@
+//#  ABSBeamlet.h: implementation of the Beamlet class
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <ABSBeamlet.h>
+#include <ABSBeam.h>
+
+#include <iostream>
+#include <queue>
+
+#include <blitz/array.h>
+using namespace blitz;
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+using namespace boost::gregorian;
+using namespace boost::posix_time;
+
+#undef PACKAGE
+#undef VERSION
+#include <lofar_config.h>
+#include <Common/LofarLogger.h>
+using namespace LOFAR;
+
+using namespace ABS;
+using namespace std;
+
+int      Beamlet::m_ninstances = 0;
+Beamlet* Beamlet::m_beamlets   = 0;
+
+const W_TYPE SPEED_OF_LIGHT_MS = 299792458.0; // speed of light in meters/sec
+const complex<W_TYPE> I_COMPLEX = complex<W_TYPE>(0.0,1.0);
+
+Beamlet::Beamlet() :
+    m_spw(0), m_subband(0), m_index(-1), m_beam(0)
+{}
+
+Beamlet::~Beamlet()
+{
+  if (m_beamlets) delete [] m_beamlets;
+  m_ninstances = 0;
+}
+
+Beamlet* Beamlet::getInstance()
+{
+  // if not yet initialised, just return 0
+  if (!m_beamlets) return 0;
+
+  for (int i = 0; i < m_ninstances; i++)
+  {
+      if (!m_beamlets[i].allocated())
+	  return &m_beamlets[i];
+  }
+
+  // otherwise return 0
+  return 0;
+}
+
+int Beamlet::init(int ninstances)
+{
+  // if already initialised just return
+  // only one initialisation is allowed
+  if (m_beamlets) return -1;
+
+  m_beamlets = new Beamlet[ninstances];
+  if (!m_beamlets) return -1;
+  m_ninstances = ninstances;
+
+  for (int i = 0; i < m_ninstances; i++)
+  {
+      m_beamlets[i].m_index = i;
+  }
+
+  return (m_beamlets ? 0 : -1);
+}
+
+int Beamlet::allocate(const Beam& beam,
+		      SpectralWindow const& spw, int subband)
+{
+  // don't allow second allocation
+  if (m_beam) return -1;
+
+  // check that the subband is within the spectral window
+  if ((subband >= spw.nsubbands()) 
+      || (subband < 0) ) return -1;
+
+  m_spw     = &spw;
+  m_subband = subband;
+  m_beam = &beam;
+
+  return 0;
+}
+
+int Beamlet::deallocate()
+{
+  if (!m_beam) return -1;
+
+  m_spw     = 0;
+  m_subband = -1;
+  m_beam    = 0;
+
+  return 0;
+}
+
+const Beam* Beamlet::getBeam() const
+{
+  return m_beam;
+}
+
+void Beamlet::calculate_weights(const Array<W_TYPE, 3>&          pos,
+				      Array<complex<W_TYPE>, 4>& weights)
+{
+  const Array<W_TYPE,2>* lmn = 0;
+  int compute_interval = weights.extent(firstDim);
+  int nelements        = weights.extent(secondDim);
+  int npolarizations   = weights.extent(fourthDim);
+  Range all = Range::all();
+
+  if ((weights.extent(thirdDim) != m_ninstances)
+      || (pos.extent(firstDim) != nelements)
+      || (pos.extent(secondDim) != npolarizations))
+  {
+      LOG_ERROR("\nmismatching pos and weight array shapes\n");
+      return;
+  }
+
+  for (int bi = 0; bi < m_ninstances; bi++)
+  {
+      Beamlet* beamlet = &m_beamlets[bi];
+      if (beamlet && beamlet->allocated())
+      {
+	  const Beam* beam = beamlet->getBeam();
+
+	  // get coordinates from beam
+	  if (!beam)
+	  {
+	      LOG_ERROR(formatString("\nno beam for beamlet %d?\n", beamlet->m_index));
+	      continue;
+	  }
+	  lmn = &beam->getLMNCoordinates();
+
+	  if (!lmn)
+	  {
+	      LOG_ERROR(formatString("\ninvalid (l,m,n) vector for beamlet %d\n", beamlet->m_index));
+	      continue;
+	  }
+
+	  LOG_DEBUG_STR("lmn(t=0)=" << (*lmn)(0,all));
+
+	  if (compute_interval != lmn->extent(firstDim))
+	  {
+	      LOG_ERROR(formatString("\nlmn vector length (%d) != compute_interval (%d)\n",
+				     lmn->extent(firstDim), compute_interval));
+	      continue;
+	  }
+
+	  W_TYPE freq = 0.0;
+	  if (beamlet->spw())
+	  {
+	      freq = beamlet->spw()->getFrequency(beamlet->subband());
+	      LOG_DEBUG_STR("freq = " << freq);
+	  }
+
+  	  for (int si = 0; si < nelements; si++)
+	  {
+	    //
+	    // calculate (xm - yl + zn) for both polarizations
+	    // of all elements
+	    //
+	    
+	    for (int pol = 0; pol < npolarizations; pol++)
+	    {
+	      weights(all, si, bi, pol) =
+		(pos(si, pol, 0) * (*lmn)(all, 1))
+		- (pos(si, pol, 1) * (*lmn)(all, 0))
+		+ (pos(si, pol, 2) * (*lmn)(all, 2));
+	    }
+	  }
+
+	  weights(all, all, bi, all) =
+	      exp((I_COMPLEX * ((W_TYPE)2.0) * ((W_TYPE)M_PI) * freq) * weights(all, all, bi, all) / SPEED_OF_LIGHT_MS);
+      }
+      else
+      {
+	  weights(all, all, bi, all) = complex<W_TYPE>(0.0, 0.0);
+      }
+  }
+
+  //cout << "weights(t=0) = " << weights(0,all,0,0) << endl;
+
+  LOG_DEBUG(formatString("sizeof weights() = %d bytes", weights.size()*sizeof(complex<W_TYPE>)));
+  LOG_DEBUG(formatString("contiguous storage? %s", (weights.isStorageContiguous()?"yes":"no")));
+}
+
---
+++ tags/MAC-ABS-2_0/MAC/APL/PAC/BeamServer/test/CheckWeightsTest2.cc
@@ -0,0 +1,228 @@
+//#
+//#  CheckWeightsTest2.cc: class definition for the Beam Server task.
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include "ABSSpectralWindow.h"
+#include "ABSBeam.h"
+#include "ABSBeamlet.h"
+
+#include <Suite/suite.h>
+
+#include <iostream>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <blitz/array.h>
+
+#include <stdio.h>
+#include <netinet/in.h>
+
+#undef PACKAGE
+#undef VERSION
+#include <lofar_config.h>
+#include <Common/LofarLogger.h>
+
+using namespace boost::posix_time;
+using namespace blitz;
+using namespace LOFAR;
+using namespace ABS;
+using namespace std;
+
+#define N_BEAMS          1 // must always be greater than 0!
+#define N_BEAMLETS       1
+#define UPDATE_INTERVAL  1
+#define COMPUTE_INTERVAL 3
+#define N_ELEMENTS       1
+#define N_POLARIZATIONS  2
+#define N_SUBBANDS       N_BEAMLETS
+
+#define SCALE (1<<(16-2))
+
+inline complex<int16_t> convert2complex_int16_t(complex<W_TYPE> cd)
+{
+#ifdef W_TYPE_DOUBLE
+  return complex<int16_t>(htons((int16_t)(round(cd.real()*SCALE))),
+			  htons((int16_t)(round(cd.imag()*SCALE))));
+#else
+  return complex<int16_t>(htons((int16_t)(roundf(cd.real()*SCALE))),
+			  htons((int16_t)(roundf(cd.imag()*SCALE))));
+#endif
+}
+
+class CheckWeightsTest : public Test
+{
+private:
+  Beam*          m_beam[N_BEAMS];
+  SpectralWindow m_spw;
+
+public:
+
+    /**
+     * create a spectral window from 10MHz to 90Mhz
+     * steps of 256kHz
+     * SpectralWindow spw(1.5e6, 256*1e3, 80*(1000/250));
+     */
+    CheckWeightsTest() :
+	Test("CheckWeightsTest"),
+	m_spw(1.5e6, 256*1e3, 80*(1000/256))
+	{
+	  //cerr << "c";
+	  Beam::init(N_BEAMS, UPDATE_INTERVAL, COMPUTE_INTERVAL);
+	  Beamlet::init(N_BEAMLETS);
+	}
+
+    void run()
+	{
+	  check_weights();
+	}
+
+    void check_weights()
+	{
+	  set<int> subbands;
+	  Range all = Range::all();
+
+	  //
+	  // Compare result of this test with Octave/Matlab generated reference
+	  //
+
+	  subbands.clear();
+	  for (int i = 0; i < N_SUBBANDS; i++) subbands.insert(i);
+	  TESTC(0 != (m_beam[0] = Beam::allocate(m_spw, subbands)));
+
+	  ptime now = from_time_t(time(0));
+
+	  // add a few pointings
+	  TESTC(m_beam[0]->addPointing(Pointing(Direction(
+			    0.0, 0.0,
+			    Direction::LOFAR_LMN), now + seconds(0))) == 0);
+	  TESTC(m_beam[0]->addPointing(Pointing(Direction(
+			    0.0, 1.0,
+			    Direction::LOFAR_LMN), now + seconds(1))) == 0);
+	  TESTC(m_beam[0]->addPointing(Pointing(Direction(
+			    0.0, -1.0,
+			    Direction::LOFAR_LMN), now + seconds(2))) == 0);
+
+	  struct timeval start, delay;
+	  gettimeofday(&start, 0);
+	  // iterate over all beams
+	  time_period period(now, seconds(COMPUTE_INTERVAL));
+
+	  TESTC(0 == m_beam[0]->convertPointings(period));
+
+	  Array<W_TYPE, 3>           pos(N_ELEMENTS, N_POLARIZATIONS, 3);
+	  Array<complex<W_TYPE>, 4>  weights(COMPUTE_INTERVAL, N_ELEMENTS, N_SUBBANDS, N_POLARIZATIONS);
+	  Array<complex<int16_t>, 4> weights16(COMPUTE_INTERVAL, N_ELEMENTS, N_SUBBANDS, N_POLARIZATIONS);
+
+	  weights   = complex<W_TYPE>(0,0);
+	  weights16 = complex<int16_t>(0,0);
+
+	  pos(0, 0, all) = 0.0, 0.0, 0.0;
+	  pos(0, 1, all) = 100.0, 0.0, 0.0;
+
+	  cout << "pos = " << pos << endl;
+
+	  cout << "lmn = " << m_beam[0]->getLMNCoordinates() << endl;
+
+	  Beamlet::calculate_weights(pos, weights);
+	  gettimeofday(&delay, 0);
+
+	  cout << "weights = " << weights << endl;
+
+	  //weights16 = convert2complex_int16_t(conj(weights));
+
+	  for (int i = 0; i < COMPUTE_INTERVAL; i++)
+	    for (int j = 0; j < N_ELEMENTS; j++)
+	      for (int k = 0; k < N_SUBBANDS; k++)
+		for (int l = 0; l < N_POLARIZATIONS; l++)
+		  {
+		    //weights16(i,j,k,l) = conj(weights16(i,j,k,l));
+		    weights16(i,j,k,l) = complex<int16_t>((int16_t)round(weights(i,j,k,l).real()*SCALE),
+							  -1*(int16_t)round(weights(i,j,k,l).imag()*SCALE));
+		  }	  
+	  cout << "weights16 = " << weights16 << endl;
+	  
+	  imag(weights16(all, all, all, 1)) *= -1;
+
+	  cout << "weights16_imag_neg = " << weights16 << endl;
+
+	  weights16 *= complex<int16_t>(0,1);
+
+	  cout << "weights16_times_i = " << weights16 << endl;
+
+#if 0
+	  Array<complex<W_TYPE>, 2> weights_ref(COMPUTE_INTERVAL, N_ELEMENTS);
+	  Array<complex<W_TYPE>, 2> error(COMPUTE_INTERVAL, N_ELEMENTS);
+
+	  //
+	  // read in reference data
+	  //
+	  FILE* wfile = fopen("../../../test/weights2.dat", "r");
+	  TESTC(wfile);
+	  if (!wfile) return;
+
+	  for (int bl = 0; bl < N_BEAMLETS; bl++)
+	  {
+	    TESTC((size_t)weights_ref.size()
+		  == fread(weights_ref.data(), sizeof(complex<W_TYPE>), weights_ref.size(), wfile));
+
+	    error = weights_ref - weights(all, all, bl, 0);
+	    cout << "error = abs(sum(error)) = " << abs(sum(error)) << endl;
+	    TESTC(abs(sum(error)) < 1e-8);
+
+	    error = weights_ref - weights(all, all, bl, 1);
+	    cout << "error = abs(sum(error)) = " << abs(sum(error)) << endl;
+	    TESTC(abs(sum(error)) < 1e-8);
+	  }
+	  fclose(wfile);
+
+	  delay.tv_sec -= start.tv_sec;
+	  delay.tv_usec -= start.tv_usec;
+	  if (delay.tv_usec < 0)
+	  {
+	      delay.tv_sec -= 1;
+	      delay.tv_usec = 1000000 + delay.tv_usec;
+	  }
+	  LOG_INFO(formatString("calctime = %d sec %d msec", delay.tv_sec, delay.tv_usec/1000));
+#endif
+	  TESTC(m_beam[0]->deallocate() == 0);
+	}
+};
+
+int main(int /*argc*/, char** /*argv*/)
+{
+  Pointing p;
+  p = p;
+
+  char prop_path[PATH_MAX];
+  const char* mac_config = getenv("MAC_CONFIG");
+
+  snprintf(prop_path, PATH_MAX-1,
+	   "%s/%s", (mac_config?mac_config:"."),
+	   "log4cplus.properties");
+  INIT_LOGGER(prop_path);
+
+  Suite s("Check Weights Test Suite", &cout);
+
+  s.addTest(new CheckWeightsTest);
+  s.run();
+  long nFail = s.report();
+  s.free();
+  return nFail;
+}
