9aa378ae6f5786bc78498b8986f7732f5cd016e4
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-09-16 07:27:10 +0000

    First development version of RTCorrelator, a collelator for BG/L that makes use of the hard realtime nature of the BG/L system to make a simple round robin communication system work.

---
+++ trunk/CEP/Demo/RTCorrelator/src/DH_CorrCube.h
@@ -0,0 +1,98 @@
+//# DH_CorrCube.h: CorrCube DataHolder
+//#
+//# Copyright (C) 2000, 2001
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef ONLINEPROTO_DH_ARRAYTFPLANE_H
+#define ONLINEPROTO_DH_ARRAYTFPLANE_H
+
+
+#include <lofar_config.h>
+
+#include <Transport/DataHolder.h>
+#include <Common/lofar_complex.h>
+
+namespace LOFAR
+{
+
+
+/**
+   TBW
+*/
+class DH_CorrCube: public DataHolder
+{
+public:
+  typedef complex<float> BufferType;
+
+  explicit DH_CorrCube (const string& name);
+
+  DH_CorrCube(const DH_CorrCube&);
+
+  virtual ~DH_CorrCube();
+
+  DataHolder* clone() const;
+
+
+  /// Allocate the buffers.
+  virtual void preprocess();
+
+  /// Deallocate the buffers.
+  virtual void postprocess();
+
+  /// Get write access to the Buffer in the DataPacket.
+  BufferType* getBuffer();
+  /// Get access to the Buffer in the DataPacket.
+  const BufferType* getBuffer() const;
+  BufferType* getBufferElement(int station, int channel, int sample);
+  void setBufferElement(int station, int channel, int sample, BufferType* value); 
+   
+   const int         getFBW() const;
+
+private:
+  /// Forbid assignment.
+  DH_CorrCube& operator= (const DH_CorrCube&);
+
+  BufferType*  itsBuffer;    // array containing frequency spectrum.
+  unsigned int itsBufSize;
+  
+  int          itsFBW; // number of frequency channels within this beamlet
+
+  int nstations;
+  int nsamples ;
+  int nchannels;
+
+  void fillDataPointers();
+};
+
+inline DH_CorrCube::BufferType* DH_CorrCube::getBuffer()
+  { return itsBuffer; }
+
+inline const DH_CorrCube::BufferType* DH_CorrCube::getBuffer() const
+  { return itsBuffer; }
+
+   
+   
+
+inline const int DH_CorrCube::getFBW() const
+  { return itsFBW; }
+
+}
+
+#endif 
---
+++ trunk/CEP/Demo/RTCorrelator/src/WH_Correlator.cc
@@ -0,0 +1,84 @@
+//#  WH_Correlator.cc: Round robin BG/L correlator. Using hard real-time
+//#  property of BG/L so we don't have to synchronize.
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <stdio.h> 
+
+// Application Specific includes
+#include <WH_Correlator.h>
+
+using namespace LOFAR;
+
+
+WH_Correlator::WH_Correlator(const string& name, 
+			     unsigned int nin,
+			     unsigned int nout) 
+  : WorkHolder(nin, nout, name, "WH_Correlator") {
+  
+  char str[8];
+  for (unsigned int i = 0; i < nin; i++) {
+    sprintf(str, "%d", i);
+    getDataManager().addInDataHolder(i, new DH_CorrCube(string("in_")+str));
+  }
+
+  for (unsigned int i = 0; i < nout; i++) {
+    sprintf(str, "%d", i);
+    getDataManager().addOutDataHolder(i, new DH_CorrCube(string("out_")+str));
+  }
+  
+}
+
+WH_Correlator::~WH_Correlator() {
+}
+
+WorkHolder* WH_Correlator::construct(const string& name, 
+				     unsigned int nin, 
+				     unsigned int nout) {
+  return new WH_Correlator(name, nin, nout);
+}
+
+WH_Correlator* WH_Correlator::make(const string& name) {
+  return new WH_Correlator(name, 
+			   getDataManager().getInputs(), 
+			   getDataManager().getOutputs());
+}
+
+void WH_Correlator::preprocess() {
+}
+
+void WH_Correlator::process() {
+}
+
+void WH_Correlator::postprocess() {
+}
+
+void WH_Correlator::dump() {
+}
+
+void WH_Correlator::correlator_core(complex<float>* sig, complex<float>* cor) {
+}
+
+void WH_Correlator::master() {
+}
+
+void WH_Correlator::slave() {
+}
