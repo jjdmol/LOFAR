62febac3348822d2070a7ec48a9745215e8c0026
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-09-28 07:37:37 +0000

    %[ER: 142]% Added header renamed simulator to theApp

--- CEP/Demo/OnLineProto/src/MainProgram.cc
+++ CEP/Demo/OnLineProto/src/MainProgram.cc
@@ -1,3 +1,27 @@
+//  MainProgram.cc: Main loop for the OnLineProto ApplicationHolder 
+//
+//  Copyright (C) 2000-2002
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+/////////////////////////////////////////////////////////////////////////
+
 #include <OnLineProto/OnLineProto.h>
 #include <Common/lofar_iostream.h>
 #include <Common/Debug.h>
@@ -25,12 +49,12 @@ int main (int argc, const char** argv)
   INIT_LOGGER("OnLineProto.log_prop");
 
   try {
-    OnLineProto simulator;
-    simulator.setarg (argc, argv);
- 	simulator.baseDefine();
-  	simulator.baseRun(10); 
-  	simulator.baseDump();
-  	simulator.baseQuit();
+    OnLineProto theApp;
+    theApp.setarg (argc, argv);
+ 	theApp.baseDefine();
+  	theApp.baseRun(10); 
+  	theApp.baseDump();
+  	theApp.baseQuit();
   } catch (...) {
     cout << "Unexpected exception" << endl;
   }
