117abad945fa393c42b34476bb9ccbca826e174c
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-09-29 08:13:11 +0000

    %[ER: 146]%

---
+++ trunk/CEP/BB/DFTServer/src/WH_DFTServer.cc
@@ -0,0 +1,71 @@
+//  WH_DFTServer.cc:
+//
+//  Copyright (C) 2000, 2001
+//  ASTRON (Netherlands Foundation for Research in Astronomy)
+//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  $Id$
+//
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "CEPFrame/WH_DFTServer.h"
+
+namespace LOFAR
+{
+
+WH_DFTServer::WH_DFTServer (const string& name)
+: WorkHolder       (1, 1, name,"WH_DFTServer")
+{
+  getDataManager().addInDataHolder(0, new DH_DFTIn("DFTServer_in"));
+  getDataManager().addOutDataHolder(0, new DH_DFTOut("DFTServer_out"));
+}
+
+
+WH_DFTServer::~WH_DFTServer()
+{}
+
+WorkHolder* WH_DFTServer::construct (const string& name, int, int,
+				  const KeyValueMap&)
+{
+  return new WH_DFTServer (name);
+}
+
+WH_DFTServer* WH_DFTServer::make (const string& name)
+{
+  return new WH_DFTServer (name);
+}
+
+void WH_DFTServer::process()
+{
+}
+
+void WH_DFTServer::dump()
+{
+//   cout << "WH_DFTServer" << endl;
+//   cout << "Timestamp " << 0 << " = "
+//        << getDataManager().getOutHolder(0)->getTimeStamp() << endl;
+//   cout << "Counter   " << 0 << " = "
+//        << ((DH_DFTServer*)getDataManager().getOutHolder(0))->getCounter() << endl;
+//   cout << "Buffer    ";
+//   for (int ch=0; ch<10; ch++){
+//     cout << ((DH_DFTServer*)getDataManager().getInHolder(0))->getBuffer()[ch] << ' ';
+//   }
+//   cout << endl;
+}
+
+}
---
+++ trunk/CEP/BB/DFTServer/src/WH_DFTServer.h
@@ -0,0 +1,71 @@
+//# WH_DFTServer.h: WorkHolder for the DFTServer 
+//#
+//# Copyright (C) 2000, 2001
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef DFTSERVER_WH_DFTSERVER_H
+#define DFTSERVER_WH_DFTSERVER_H
+
+#include <lofar_config.h>
+
+#include "tinyCEP/WorkHolder.h"
+#include "BB/PSS3/DH_DFTRequest.h"
+#include "BB/PSS3/DH_DFTResult.h"
+
+namespace LOFAR
+{
+
+/**
+   This is the work holder for the DFTServer program.
+*/
+
+class WH_DFTServer: public WorkHolder
+{
+public:
+  /// Construct the work holder.
+  explicit WH_DFTServer (const string& name = "WH_DFTServer");
+
+  virtual ~WH_DFTServer();
+
+  /// Static function to create an object.
+  static WorkHolder* construct (const string& name, int ninput, int noutput,
+				const KeyValueMap&);
+
+   /// Make a fresh copy of the WH object.
+  virtual WH_DFTServer* make (const string& name);
+
+  /// Do a process step.
+  virtual void process();
+
+  /// Show the work holder on stdout.
+  virtual void dump();
+
+private:
+  /// Forbid copy constructor.
+  WH_DFTServer (const WH_DFTServer&);
+
+  /// Forbid assignment.
+  WH_DFTServer& operator= (const WH_DFTServer&);
+
+};
+
+}
+
+#endif
