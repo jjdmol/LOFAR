d789f3e733bfd78abd92b46bdbfce19781288325
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-09-30 09:07:16 +0000

    %[ER: 156]%

--- CEP/Tools/PerfTest/P2Perf/src/StopWatch.cc
+++ CEP/Tools/PerfTest/P2Perf/src/StopWatch.cc
@@ -3,10 +3,13 @@
 
 #include "P2Perf/StopWatch.h"
 
-StopWatch::StopWatch()
+StopWatch* StopWatch::theirGlobalStopWatch = 0;
+
+StopWatch::StopWatch() : pausedSecs(0), pauseduSecs(0), isPausing (false)
 {
   memset(&startTime, 0, sizeof(struct timeval));
   memset(&stopTime,  0, sizeof(struct timeval));
+  memset(&pauseStartTime,  0, sizeof(struct timeval));
 }
 
 StopWatch::~StopWatch()
@@ -15,31 +18,70 @@ StopWatch::~StopWatch()
 
 void StopWatch::start()
 {
+  memset(&pauseStartTime,  0, sizeof(struct timeval));
   timerclear(&stopTime);
+  pausedSecs = 0;
+  pauseduSecs = 0;
+  isPausing = false;
   gettimeofday(&startTime, NULL);
-
-  //cout << "start: " << startTime.tv_sec << ":" << startTime.tv_usec << endl;
 }
 
 void StopWatch::stop()
 {
   gettimeofday(&stopTime, NULL);
-
-  //cout << "stop: " << stopTime.tv_sec << ":" << stopTime.tv_usec << endl;
+  if (isPausing)
+  {
+    // if we were pausing overwrite the stoptime with the time the pause() was called
+    memcpy(&stopTime, &pauseStartTime, sizeof(struct timeval));
+    isPausing = false;
+  }
 }
 
-double StopWatch::elapsed()
+void StopWatch::pause()
 {
-  long   secsDiff  = stopTime.tv_sec  - startTime.tv_sec;
-  long   usecsDiff = stopTime.tv_usec - startTime.tv_usec;
-  double usecsElapsed = 0.0;
+  gettimeofday(&pauseStartTime, NULL);
+  isPausing = true;
+}
+
+void StopWatch::resume()
+{
+  if (isPausing==true)
+  {
+    isPausing = false;
+    pausedSecs -= pauseStartTime.tv_sec;
+    pauseduSecs -= pauseStartTime.tv_usec;
+
+    struct timeval pauseStopTime;
+    gettimeofday(&pauseStopTime, NULL);
+    // the following instructions are executed while the time 
+    // is running so this is probably not the best solution
+    pausedSecs += pauseStopTime.tv_sec;
+    pauseduSecs += pauseStopTime.tv_usec;
+  }    
+}
+
+double StopWatch::elapsed() const
+{
+  long secsDiff;
+  long usecsDiff;
+  secsDiff = stopTime.tv_sec  - startTime.tv_sec - pausedSecs;
+  usecsDiff = stopTime.tv_usec - startTime.tv_usec - pauseduSecs;
+  double secsElapsed(0);
 
   if (   timerisset(&startTime)
       && timerisset(&stopTime)
       && timercmp(&startTime, &stopTime, <=))
   {
-    usecsElapsed = (double)secsDiff + ((double)usecsDiff / 1000000.0);
+    secsElapsed = (double) secsDiff + ((double)usecsDiff / 1000000.0);
   }
 	
-  return usecsElapsed;
+  return secsElapsed;
+}
+
+//static
+StopWatch * StopWatch::getGlobalStopWatch()
+{
+  if (theirGlobalStopWatch == 0)
+    theirGlobalStopWatch = new StopWatch();
+  return theirGlobalStopWatch;    
 }
