20e1a4663040f0c3199058b099c72691d2708633
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-10-11 07:20:16 +0000

    %[ER: 146]% Class to do a DFT for a bunch of times/baselines/frequencies

---
+++ trunk/CEP/BB/DFTServer/src/DFTAll.cc
@@ -0,0 +1,60 @@
+//# DFTAll.cc: Class to process all DFTs
+//#
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <DFTServer/DFTAll.h>
+#include <Transport/TH_Socket.h>
+#include <Common/LofarLogger.h>
+
+namespace LOFAR
+{
+
+
+  DFTAll::DFTAll()
+  {
+    itsRequest.preprocess();
+    itsResult.preprocess();
+    itsRequest.setID (0);
+    itsResult.setID (1);
+    itsStub.connect (itsRequest, itsResult);
+  }
+
+  DFTAll::~DFTAll()
+  {}
+
+  // Set the values.
+  void DFTAll::set (double startFreq, double stepFreq, int nFreq,
+		    double startTime, double stepTime, int nTime,
+		    int nAnt, int nBaseline)
+  {
+    itsRequest.set (startFreq, stepFreq, nFreq, startTime, stepTime, nTime,
+		    nAnt, nBaseline);
+  }
+
+  void DFTAll::receive()
+  {
+    itsResult.read();
+    ASSERT (itsResult.getNFreq() == itsRequest.getNFreq());
+    ASSERT (itsResult.getNTime() == itsRequest.getNTime());
+    ASSERT (itsResult.getNBaseline() == itsRequest.getNBaseline());
+  }
+
+}
---
+++ trunk/CEP/BB/DFTServer/src/DFTAll.h
@@ -0,0 +1,108 @@
+//# DFTAll.h: Class to process all DFTs
+//#
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef PSS3_DFTALL_H
+#define PSS3_DFTALL_H
+
+
+#include <DFTServer/DH_DFTRequest.h>
+#include <DFTServer/DH_DFTResult.h>
+#include <DFTServer/DFTStub.h>
+#include <Common/LofarTypes.h>
+#include <Transport/DataHolder.h>
+
+namespace LOFAR
+{
+
+
+class DFTAll
+{
+public:
+  // The contructor connects to the DFTServer.
+  explicit DFTAll();
+
+  // The destructor disconnects from the DFTServer.
+  ~DFTAll();
+
+  // Set the values of LM.
+  void setLM (double L, double M);
+
+  // Set the values.
+  void set (double startFreq, double stepFreq, int nFreq,
+	    double startTime, double stepTime, int nTime,
+	    int nAnt, int nBaseline);
+
+  /// Give write access to the variable sized data fields.
+  // The set function should have been called before.
+  // <group>
+  double* accessUVW();
+  int32*  accessAnt();
+  int32*  accessAnt1();
+  int32*  accessAnt2();
+  // </group>
+
+  // Get access to the values in the result.
+  const double* getValues() const;
+
+  // Send the request to the DFTServer.
+  void send();
+
+  // Receive the result from the DFTServer.
+  void receive();
+
+private:
+  /// Forbid copy constructor.
+  DFTAll(const DFTAll&);
+
+  /// Forbid assignment.
+  DFTAll& operator= (const DFTAll&);
+
+  DH_DFTRequest itsRequest;
+  DH_DFTResult  itsResult;
+  DFTStub       itsStub;
+};
+
+
+inline void DFTAll::setLM (double L, double M)
+  { itsRequest.setLM (L, M); }
+
+inline double* DFTAll::accessUVW()
+  { return itsRequest.accessUVW(); }
+inline int32* DFTAll::accessAnt()
+  { return itsRequest.accessAnt(); }
+inline int32* DFTAll::accessAnt1()
+  { return itsRequest.accessAnt1(); }
+inline int32* DFTAll::accessAnt2()
+  { return itsRequest.accessAnt2(); }
+
+inline const double* DFTAll::getValues() const
+  { return itsResult.getValues(); }
+
+inline void DFTAll::send()
+  { itsRequest.write(); }   
+
+  //#inline void DFTAll::receive()
+  //#  { itsResult.read(); }
+
+}
+
+#endif 
