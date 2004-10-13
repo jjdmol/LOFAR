11bdfaf91e48ad4a51c426905144012ecf84423d
Ger van Diepen <gervandiepen@users.noreply.github.com>
2004-10-13 08:12:43 +0000

    %[ER: 70]% Made tLorrays run

--- LCS/Common/test/tLorrays.cc
+++ LCS/Common/test/tLorrays.cc
@@ -1,7 +1,33 @@
-#define HAVE_BLITZ
-#include "Common/Lorrays.h"
-#include "Common/Debug.h"
+//#  tLorrays.cc: test program for Lorray classes
+//#
+//#  Copyright (C) 2002
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
 
+#include <lofar_config.h>
+#include <Common/lofar_iostream.h>
+
+#ifdef HAVE_BLITZ
+
+#include <Common/Lorrays.h>
+
+using namespace LOFAR;
 
 int main ()
 {
@@ -26,4 +52,15 @@ int main ()
   
   LoMatPos pos(3,3);
   cout<<"mat3(3,3): "<<mat3(pos)<<endl;
+  exit(0);
 }
+
+#else
+
+int main()
+{
+  cout << "Not tested (Blitz not configured)" << endl;
+  exit(0);
+}
+
+#endif
