7eae5f00643fdbe3f6a3ab5437c568bf24377ab3
Marcel Loose <gmloose@users.noreply.github.com>
2004-10-14 10:20:20 +0000

    Added standard LOFAR header

--- LCS/Common/src/CheckConfig.h
+++ LCS/Common/src/CheckConfig.h
@@ -1,10 +1,32 @@
-#ifndef _CheckConfig_h
-#define _CheckConfig_h 1 
-    
+//#  CheckConfig.h: Macros for declaring a config-dependent data structure.
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LOFAR_COMMON_CHECKCONFIG_H
+#define LOFAR_COMMON_CHECKCONFIG_H
+
 // The CHECK_CONFIG macros should be used if and when you declare a
 // config-dependent data structure. These allow you to automatically insert
-// symbol references into your object files that will be checked for consistency
-// at link time.
+// symbol references into your object files that will be checked for
+// consistency at link time.
     
 
 // If you declare a config-dependent data structure, you should insert a 
