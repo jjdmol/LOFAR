f85632a5c0cd65cf7f537460ef19a98cfe4dfb91
Marcel Loose <gmloose@users.noreply.github.com>
2004-10-19 08:04:05 +0000

    %[ER: 161]%
    Changed header comment tokens.

--- LCS/Common/src/shmem/segbasemap.cc
+++ LCS/Common/src/shmem/segbasemap.cc
@@ -1,4 +1,4 @@
-//#  segbasemap.cc: 
+//#  segbasemap.cc
 //#
 //#  Copyright (C) 2002-2004
 //#  ASTRON (Netherlands Foundation for Research in Astronomy)
@@ -25,48 +25,44 @@
 #include <stdio.h>
 #include <unistd.h>
 
-namespace LOFAR
+/* sorted list of segment base addresses */
+static map<void*, void*> segbase;
+
+/**
+ * ~shm_segbasemap_find
+ *
+ * Find the base address of the segment in which this address lies
+ *
+ */
+void* shm_segbasemap_find(void* address)
 {
+  map<void*, void*>::const_iterator it;
+  void* prev_base_address = 0;
+  void* base_address = 0;
 
-  /* sorted list of segment base addresses */
-  static map<void*, void*> segbase;
-
-  /**
-   * ~shm_segbasemap_find
-   *
-   * Find the base address of the segment in which this address lies
-   *
-   */
-  void* shm_segbasemap_find(void* address)
+  if (segbase.begin() == segbase.end())
   {
-    map<void*, void*>::const_iterator it;
-    void* prev_base_address = 0;
-    void* base_address = 0;
-
-    if (segbase.begin() == segbase.end())
-    {
-      fprintf(stderr, "shm_segbasemap_find: map empty!");
-      exit(1);
-    }
-
-    for (it = segbase.begin(); it != segbase.end(); ++it)
-    {
-      if (address < it->first) break;
-      prev_base_address = it->first;
-      base_address = prev_base_address;
-    }
-
-    return base_address;
+    fprintf(stderr, "shm_segbasemap_find: map empty!");
+    exit(1);
   }
 
-  /**
-   * ~shm_segbasemap_add
-   *
-   * Add the segment base address to the segbase map.
-   */
-  void shm_segbasemap_add(void* base)
+  for (it = segbase.begin(); it != segbase.end(); ++it)
   {
-    segbase[base] = base;
+    if (address < it->first) break;
+    prev_base_address = it->first;
+    base_address = prev_base_address;
   }
 
-} // namespace LOFAR
+  return base_address;
+}
+
+/**
+ * ~shm_segbasemap_add
+ *
+ * Add the segment base address to the segbase map.
+ */
+void shm_segbasemap_add(void* base)
+{
+  segbase[base] = base;
+}
+
