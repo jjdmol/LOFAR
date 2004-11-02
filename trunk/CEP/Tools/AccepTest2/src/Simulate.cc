d4cadc3ca7dcf280902798f02bdb4508c7a356a9
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-11-02 15:56:23 +0000

    %[ER: 181]% Added TH_MPI::init() which calls MPI_Init(). Also changed the default values in AH_AcceptTest to something more sane.

--- trunk/CEP/Tools/AccepTest2/src/Simulate.cc
+++ trunk/CEP/Tools/AccepTest2/src/Simulate.cc
@@ -1,6 +1,7 @@
 #include <Common/LofarLogger.h>
 #include <tinyCEP/SimulatorParseClass.h>
 #include <AH_AccepTest.h>
+#include <Transport/TH_MPI.h>
 
 using namespace LOFAR;
 
@@ -9,6 +10,10 @@ int main (int argc, const char** argv)
   //INIT_LOGGER("AccepTest2.properties");
 
   try {
+#ifdef HAVE_MPI
+    TH_MPI::init(argc, argv);
+#endif
+
     AH_AccepTest simulator;
     simulator.setarg (argc, argv);
     simulator.baseDefine();
@@ -16,6 +21,8 @@ int main (int argc, const char** argv)
     simulator.baseRun(1);
     //simulator.baseDump();
     simulator.baseQuit();
+  } catch (LOFAR::Exception ex) {
+    cout << "found a known exception: " << ex.what() << endl;
 
   } catch (...) {
     cout << "Unexpected exception" << endl;
