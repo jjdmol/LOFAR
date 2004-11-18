b1d22d072ec4d2184997d9a11454711c7d5f9c11
overeem <overeem@astron.nl>
2004-11-18 12:26:45 +0000

    %[ER:116]% Update of the flavors of parameter collections to the new design.

--- LCS/ACC/APS/src/ParameterSet.cc
+++ LCS/ACC/APS/src/ParameterSet.cc
@@ -24,6 +24,8 @@
 #include <Common/LofarLogger.h>
 #include <Common/lofar_fstream.h>
 
+using namespace std;
+
 namespace LOFAR {
   namespace ACC {
 
@@ -35,18 +37,24 @@ ParameterSet::ParameterSet()
 {}
 
 //#
-//# Construction by reading a parameter file.
+//# ParameterSet(filename)
 //#
-ParameterSet::ParameterSet(const string&	theFilename)
-{
-	readFile(theFilename, false);
-}
+ParameterSet::ParameterSet(const string&	filename) :
+	ParameterCollection(filename)
+{}
+
+//#
+//# ParameterSet (ParameterCollection)
+//#
+ParameterSet::ParameterSet(const ParameterCollection& that) :
+	ParameterCollection(that)
+{}
 
 //#
 //# Copying is allowed.
 //#
 ParameterSet::ParameterSet(const ParameterSet& that)
-  : map<string, string> (that)
+  : ParameterCollection (that)
 {}
 
 //#
@@ -55,7 +63,7 @@ ParameterSet::ParameterSet(const ParameterSet& that)
 ParameterSet& ParameterSet::operator=(const ParameterSet& that)
 {
 	if (this != &that) {
-		map<string, string>::operator= (that);
+		ParameterCollection::operator= (that);
 	}
 	return (*this);
 }
@@ -71,254 +79,35 @@ ParameterSet::~ParameterSet()
 //#
 std::ostream&	operator<< (std::ostream& os, const ParameterSet &thePS)
 {
-	map<string,string>::const_iterator		iter = thePS.begin();
+	return os << static_cast<ParameterCollection> (thePS);
+}
 
-	while (iter != thePS.end()) {
-		os << "[" << iter->first << "],[" << iter->second << "]" << endl;
+//#
+//# check()
+//#
+//# 1) Versionnumber must be present
+//# 2) All keys must be filled.
+//#
+bool	ParameterSet::check(string&	errorReport) const
+{
+	bool	isOk = true;
+	errorReport  = "";
+
+	if (getVersionNr() == "") {
+		errorReport += "No versionnumber found\n";
+		isOk = false;
+	}
+
+	const_iterator		iter = begin();
+	while (iter != end()) {
+		if (iter->second == "") {
+			errorReport += "Key " + iter->first + " has no value\n";
+			isOk = false;
+		}
 		iter++;
 	}
 
-	return os;
-}
-
-//#-------------------------- merge and split --------------------------------
-//#
-//# makeSubset(baseKey)
-//#
-// Creates a Subset from the current ParameterSet containing all the parameters
-// that start with the given baseKey. The baseKey is cut off from the Keynames
-// in the created subset.
-//
-ParameterSet ParameterSet::makeSubset(const string& baseKey) const
-{
-	const_iterator		scanner    = begin();
-	int					baseKeyLen = strlen(baseKey.c_str());
-	ParameterSet		subSet;
-
-	//# Scan through whole ParameterSet
-	while (scanner != end()) {
-		//# starts with basekey?
-		if (!strncmp(baseKey.c_str(), scanner->first.c_str(), baseKeyLen)) {
-			//# cut off and copy to subset
-			subSet.insert(make_pair(scanner->first.c_str()+baseKeyLen,
-									scanner->second));
-		}
-		scanner++;
-	}
-
-	return (subSet);
-}
-
-//#
-//# adoptFile
-//#
-// Adds the parameters from the file to the current ParameterSet.
-//
-void ParameterSet::adoptFile(const string&	theFilename)
-{
-	readFile(theFilename, true);
-}
-
-//#
-//# adoptBuffer
-//#
-// Adds the parameters from the string to the current ParameterSet.
-//
-void ParameterSet::adoptBuffer(const string&	theBuffer)
-{
-	readBuffer(theBuffer, true);
-}
-
-//#
-//# readFile
-//# (private)
-//#
-// Disentangles the file and adds the Key-Values pair to the current ParameterSet.
-//
-void ParameterSet::readFile(const	string&	theFilename,
-							const	bool	merge)
-{
-	ifstream		paramFile;
-
-	//# Try to pen the file
-	paramFile.open(theFilename.c_str(), ifstream::in);
-	if (!paramFile) {
-		THROW (Exception, formatString("Unable to open file %s", theFilename.c_str()));
-	}
-
-	addStream(paramFile, merge);
-
-	paramFile.close();
-}
-
-//#
-//# readBuffer
-//# (private)
-//#
-// Disentangles the file and adds the Key-Values pair to the current ParameterSet.
-//
-void ParameterSet::readBuffer(const	string&	theBuffer,
-							  const	bool	merge)
-{
-	istringstream		iss(theBuffer, istringstream::in);
-
-	addStream(iss, merge);
-
-}
-
-//#
-//# addStream
-//# (private)
-//#
-// Disentangles the stream and adds the Key-Values pair to the current ParameterSet.
-//
-void ParameterSet::addStream(istream&	inputStream, bool	merge)
-{
-	char	paramLine[1024];
-	char*	separator;
-	//# Read the file line by line and convert it to Key Value pairs.
-	while (inputStream.getline (paramLine, 1024)) {
-		LOG_TRACE_VAR(formatString("data:>%s<", paramLine));
-	
-		if (!paramLine[0] || paramLine[0] == '#') {		//# skip empty lines
-			continue;									//# and comment lines
-		}
-		
-		//# TODO: allow multiline parametervalues
-		//# line must have an equal-character ofcourse.
-		separator = strstr(paramLine, "=");
-		if (!separator) {
-			THROW (Exception, formatString("No '=' found in %s", paramLine));
-		}
-		*separator= '\0';					//# terminate key string
-		++separator;						//# place on 1st char of value-part
-
-		//# check for quoted strings, cutoff quotes
-		int valueLen = strlen(separator)-1;
-		if ((valueLen > 0) && (*separator == '"' || *separator == '\'') &&
-			(*separator == separator[valueLen])) {
-			separator[valueLen] = '\0';
-			++separator;
-		}
-
-		LOG_TRACE_VAR(formatString("pair:[%s][%s]", paramLine, separator));
-
-		//# remove any existed value and insert this value
-		if ((erase(paramLine) > 0) && !merge) {
-			LOG_WARN (
-				formatString("Key %s is defined twice. Ignoring first value.", 
-																  paramLine));
-		}
-		pair< map<string, string>::iterator, bool>		result;
-		result = insert(std::make_pair(paramLine, separator));  //# add to map
-		if (!result.second) {
-			THROW (Exception, formatString("Key %s double defined?", paramLine));
-		}
-	}
-}
-
-//#-------------------------- retrieve functions -----------------------------
-//#
-//#	getInt(key)
-//#
-int ParameterSet::getInt(const string& theKey) const
-{
-	int				theInt;
-	const_iterator	iter = find (theKey);
-
-	if (iter == end()) {
-		THROW (Exception, formatString("Key %s unknown", theKey.c_str()));
-	}
-	if (sscanf (iter->second.c_str(), "%d", &theInt) != 1) {
-		THROW (Exception, formatString("%s is not an integer value", iter->second.c_str()));
-	}
-	return (theInt);
-}
-
-//#
-//# getDouble(key)
-//#
-double ParameterSet::getDouble(const string& theKey) const
-
-{
-	double			theDouble;
-	const_iterator	iter = find (theKey);
-
-	if (iter == end()) {
-		THROW (Exception, formatString("Key %s unknown", theKey.c_str()));
-	}
-	if (sscanf (iter->second.c_str(), "%lg", &theDouble) != 1) {
-		THROW (Exception, formatString("%s is not a double value", iter->second.c_str()));
-	}
-	return (theDouble);
-}
-
-//#
-//# getFloat(key)
-//#
-float ParameterSet::getFloat(const string& theKey) const
-
-{
-  float theFloat;
-  const_iterator iter = find (theKey);
-
-  if (iter == end()) {
-    THROW (Exception, formatString("Key %s unknown", theKey.c_str()));
-  }
-  if (sscanf (iter->second.c_str(), "%e", &theFloat) != 1) {
-    THROW (Exception, formatString("%s is not a float value", iter->second.c_str()));
-  }
-  return (theFloat);
-}
-
-//#
-//# getString(key)
-//#
-string ParameterSet::getString(const string& theKey) const
-{
-	const_iterator	iter = find (theKey);
-
-	if (iter == end()) {
-		THROW (Exception, formatString("Key %s unknown", theKey.c_str()));
-	}
-	return (iter->second);
-}
-
-//#---------------------------- save functions -------------------------------
-//#
-//# writeFile
-//#
-// Writes the Key-Values pair from the current ParameterSet to the given file
-// thereby overwritting any file contents.
-//
-void ParameterSet::writeFile(const string&	theFilename) const
-{
-	ofstream		paramFile;
-
-	//# Try to pen the file
-	paramFile.open(theFilename.c_str(), ofstream::out | ofstream::trunc);
-	if (!paramFile) {
-		THROW (Exception, formatString("Unable to open file %s", theFilename.c_str()));
-	}
-
-	//# Write all the pairs to the file
-	const_iterator		curPair = begin();
-	while (curPair != end()) {
-		//# Key can always be written.
-		paramFile << curPair->first << "=";
-
-		//* value may begin or end in a space: use quotes
-		if (*(curPair->second.begin()) == ' ' || *(curPair->second.rbegin()) == ' ') {
-			paramFile << "\"" << curPair->second << "\"" << endl;
-		}
-		else {
- 			paramFile << curPair->second << endl;
-		}
-
-		curPair++;
-	}
-
-	paramFile.close();
+	return (isOk);
 }
 
 } // namespace ACC
