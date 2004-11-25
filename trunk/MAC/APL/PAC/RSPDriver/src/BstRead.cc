5a48a122d0a5468710fd016335e058fb18d1506c
wierenga <wierenga@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-11-25 14:34:18 +0000

    %[ER:189 ]% Split StatsRead into BstRead and SstRead because subband stats deal with BLPs and beamlet stats deal with RSP/board.

--- trunk/MAC/APL/PAC/RSPDriver/src/BstRead.cc
+++ trunk/MAC/APL/PAC/RSPDriver/src/BstRead.cc
@@ -1,4 +1,4 @@
-//#  StatsRead.cc: implementation of the StatsRead class
+//#  BstRead.cc: implementation of the BstRead class
 //#
 //#  Copyright (C) 2002-2004
 //#  ASTRON (Netherlands Foundation for Research in Astronomy)
@@ -20,7 +20,7 @@
 //#
 //#  $Id$
 
-#include "StatsRead.h"
+#include "BstRead.h"
 #include "Statistics.h"
 #include "EPA_Protocol.ph"
 #include "Cache.h"
@@ -38,41 +38,31 @@ using namespace EPA_Protocol;
 using namespace RSP_Protocol;
 using namespace blitz;
 
-StatsRead::StatsRead(GCFPortInterface& board_port, int board_id, uint8 type, uint8 nblps, uint8 nfragments)
-  : SyncAction(board_port, board_id, nblps * nfragments), m_type(type), m_nblps(nblps), m_nfragments(nfragments)
+BstRead::BstRead(GCFPortInterface& board_port, int board_id, uint8 type)
+  : SyncAction(board_port, board_id, BST_N_FRAGMENTS), m_type(type)
 {
 }
 
-StatsRead::~StatsRead()
+BstRead::~BstRead()
 {
   /* TODO: delete event? */
 }
 
-void StatsRead::sendrequest()
+void BstRead::sendrequest()
 {
-  EPAReadEvent statsread;
+  EPAReadEvent bstread;
 
-  uint16 byteoffset = (getCurrentBLP() % m_nfragments) * MEPHeader::FRAGMENT_SIZE;
+  uint16 byteoffset = (getCurrentBLP() % BST_N_FRAGMENTS) * MEPHeader::FRAGMENT_SIZE;
 
   switch (m_type)
   {
-    case Statistics::SUBBAND_MEAN:
-      statsread.hdr.set(MEPHeader::SST_MEAN_HDR, getCurrentBLP() / m_nfragments,
-			MEPHeader::READ, N_STATS * sizeof(int32), byteoffset);
-      break;
-    
-    case Statistics::SUBBAND_POWER:
-      statsread.hdr.set(MEPHeader::SST_POWER_HDR, getCurrentBLP() / m_nfragments,
-			MEPHeader::READ, N_STATS * sizeof(uint32), byteoffset);
-      break;
-
     case Statistics::BEAMLET_MEAN:
-      statsread.hdr.set(MEPHeader::BST_MEAN_HDR, getCurrentBLP()/ m_nfragments,
+      bstread.hdr.set(MEPHeader::BST_MEAN_HDR, MEPHeader::DST_RSP,
 			MEPHeader::READ, N_STATS * sizeof(int32), byteoffset);
       break;
     
     case Statistics::BEAMLET_POWER:
-      statsread.hdr.set(MEPHeader::BST_POWER_HDR, getCurrentBLP() / m_nfragments,
+      bstread.hdr.set(MEPHeader::BST_POWER_HDR, MEPHeader::DST_RSP,
 			MEPHeader::READ, N_STATS * sizeof(uint32), byteoffset);
       break;
 
@@ -81,10 +71,10 @@ void StatsRead::sendrequest()
       break;
   }
 
-  getBoardPort().send(statsread);
+  getBoardPort().send(bstread);
 }
 
-void StatsRead::sendrequest_status()
+void BstRead::sendrequest_status()
 {
   // intentionally left empty
 }
@@ -106,16 +96,14 @@ inline complex<double> convert_uint32_to_double(complex<uint32> val)
 			 (double)val.imag());
 }
 
-GCFEvent::TResult StatsRead::handleack(GCFEvent& event, GCFPortInterface& /*port*/)
+GCFEvent::TResult BstRead::handleack(GCFEvent& event, GCFPortInterface& /*port*/)
 {
   EPAStatsEvent ack(event);
 
-  uint8 global_blp = (getBoardId() * m_nblps) + (getCurrentBLP() / m_nfragments);
-
   uint16 offset = ack.hdr.m_fields.offset / MEPHeader::N_PHASE / sizeof(int32);
   
-  LOG_DEBUG(formatString("StatsRead::handleack: global_blp=%d, offset=%d",
-			 global_blp, offset));
+  LOG_DEBUG(formatString("BstRead::handleack: boardid=%d, offset=%d",
+			 getBoardId(), offset));
 
   Range fragment_range(offset / MEPHeader::N_POL,
 		       (offset / MEPHeader::N_POL) + (N_STATS / MEPHeader::N_PHASEPOL) - 1);
@@ -124,52 +112,6 @@ GCFEvent::TResult StatsRead::handleack(GCFEvent& event, GCFPortInterface& /*port
   
   switch (m_type)
   {
-    case Statistics::SUBBAND_MEAN:
-    {
-      Array<complex<int32>, 2> stats((complex<int32>*)&ack.stat,
-				      shape(N_STATS / MEPHeader::N_PHASEPOL,
-					    MEPHeader::N_POL),
-				      neverDeleteData);
-
-      if (m_type != ack.hdr.m_fields.addr.regid)
-      {
-	LOG_ERROR("invalid stats ack");
-	return GCFEvent::HANDLED;
-      }
-
-      Array<complex<double>, 3>& cache(Cache::getInstance().getBack().getSubbandStats()());
-
-      // x-pol subband statistics: copy and convert to double
-      cache(m_type, global_blp * 2,     fragment_range) = convert_int32_to_double(stats(Range::all(), 0));
-
-      // y-pol subband statistics: copy and convert to double
-      cache(m_type, global_blp * 2 + 1, fragment_range) = convert_int32_to_double(stats(Range::all(), 1));
-    }
-    break;
-      
-    case Statistics::SUBBAND_POWER:
-    {
-      Array<complex<uint32>, 2> stats((complex<uint32>*)&ack.stat,
-				      shape(N_STATS / MEPHeader::N_PHASEPOL,
-					    MEPHeader::N_POL),
-				      neverDeleteData);
-
-      if (m_type != ack.hdr.m_fields.addr.regid)
-      {
-	LOG_ERROR("invalid stats ack");
-	return GCFEvent::HANDLED;
-      }
-
-      Array<complex<double>, 3>& cache(Cache::getInstance().getBack().getSubbandStats()());
-
-      // x-pol subband statistics: copy and convert to double
-      cache(m_type, global_blp * 2,     fragment_range) = convert_uint32_to_double(stats(Range::all(), 0));
-
-      // y-pol subband statistics: copy and convert to double
-      cache(m_type, global_blp * 2 + 1, fragment_range) = convert_uint32_to_double(stats(Range::all(), 1));
-    }
-    break;
-
     case Statistics::BEAMLET_MEAN:
     {
       Array<complex<int32>, 2> stats((complex<int32>*)&ack.stat,
@@ -179,18 +121,18 @@ GCFEvent::TResult StatsRead::handleack(GCFEvent& event, GCFPortInterface& /*port
 
       if ((m_type - Statistics::BEAMLET_MEAN) != ack.hdr.m_fields.addr.regid)
       {
-	LOG_ERROR("invalid stats ack");
+	LOG_ERROR("invalid bst ack");
 	return GCFEvent::HANDLED;
       }
 
       Array<complex<double>, 3>& cache(Cache::getInstance().getBack().getBeamletStats()());
 
       // x-pol beamlet statistics: copy and convert to double
-      cache(m_type - Statistics::BEAMLET_MEAN, global_blp * 2,     fragment_range) =
+      cache(m_type - Statistics::BEAMLET_MEAN, getBoardId() * 2,     fragment_range) =
 	convert_int32_to_double(stats(Range::all(), 0));
 
       // y-pol beamlet statistics: copy and convert to double
-      cache(m_type - Statistics::BEAMLET_MEAN, global_blp * 2 + 1, fragment_range) =
+      cache(m_type - Statistics::BEAMLET_MEAN, getBoardId() * 2 + 1, fragment_range) =
 	convert_int32_to_double(stats(Range::all(), 1));
     }
     break;
@@ -204,18 +146,18 @@ GCFEvent::TResult StatsRead::handleack(GCFEvent& event, GCFPortInterface& /*port
 
       if ((m_type - Statistics::BEAMLET_MEAN) != ack.hdr.m_fields.addr.regid)
       {
-	LOG_ERROR("invalid stats ack");
+	LOG_ERROR("invalid bst ack");
 	return GCFEvent::HANDLED;
       }
 
       Array<complex<double>, 3>& cache(Cache::getInstance().getBack().getBeamletStats()());
 
       // x-pol beamlet statistics: copy and convert to double
-      cache(m_type - Statistics::BEAMLET_MEAN, global_blp * 2,     fragment_range) =
+      cache(m_type - Statistics::BEAMLET_MEAN, getBoardId() * 2,     fragment_range) =
 	convert_uint32_to_double(stats(Range::all(), 0));
 
       // y-pol beamlet statistics: copy and convert to double
-      cache(m_type - Statistics::BEAMLET_MEAN, global_blp * 2 + 1, fragment_range) =
+      cache(m_type - Statistics::BEAMLET_MEAN, getBoardId() * 2 + 1, fragment_range) =
 	convert_uint32_to_double(stats(Range::all(), 1));
     }
     break;
