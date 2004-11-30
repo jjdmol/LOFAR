752c04b50e86a061a7cae6d84072a8b6252711a0
overeem <overeem@astron.nl>
2004-11-30 13:58:23 +0000

    %[ER:116]% Development update after split of ApplControl.

--- LCS/ACC/test/tACServer.cc
+++ LCS/ACC/test/tACServer.cc
@@ -8,57 +8,78 @@
 using namespace LOFAR;
 using namespace LOFAR::ACC;
 
-void ping ()
-{
-	cout << "Function 'ping' called" << endl;
-}
+class myACImpl : public ApplControl {
+public:
+//	myACImpl ();
+//	~myACImpl();
 
-bool noParam ()
-{
-	cout << "Function 'noparam' called" << endl;
-	return (true);
-}
+	bool	boot(const time_t	scheduleTime, const string& configID) const {
+		cout << "Boot function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	define(const time_t	scheduleTime) const {
+		cout << "Define function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	init(const time_t	scheduleTime) const {
+		cout << "Init function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	run(const time_t	scheduleTime) const {
+		cout << "Run function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	pause(const time_t	scheduleTime, const time_t waitTime, 
+											  const string& configID) const {
+		cout << "Pause function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	quit(const time_t	scheduleTime) const {
+		cout << "Quit function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	shutdown(const time_t	scheduleTime) const {
+		cout << "Shutdown function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	snapshot(const time_t	scheduleTime, const string& dest) const {
+		cout << "Snapshot function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	recover(const time_t	scheduleTime, const string& source) const {
+		cout << "Recover function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	reinit(const time_t	scheduleTime, const string& configID) const {
+		cout << "Reinit function called with:" << scheduleTime << endl;
+		return (true);
+	}
+	bool	replace(const time_t	scheduleTime, const string& processList,
+					const string&	nodeList,     const string& configID) const {
+		cout << "Replace function called with:" << scheduleTime << endl;
+		return (true);
+	}
 
-bool	singleParam(const time_t	scheduleTime) {
-	cout << "SingleParam function called with:" << scheduleTime << endl;
-	return (true);
-}
-
-bool	doubleParam(const time_t	scheduleTime, const string& options) {
-	cout << "DoubleParam function called with:" << scheduleTime 
-		 <<	"," << options << endl;
-	return (true);
-}
-
-string	supplyInfo(const string& keylist) {
-	return ("No information available yet");
-}
-
-void handleAckMessage()
-{
-	cout << "Ack message received!" << endl;
-}
+	string	askInfo(const string& keylist) const {
+		cout << "askInfo function called with:" << keylist << endl;
+		return ("Answer from ACimpl class");
+	}
+};	// class myACImpl
 
 int main (int argc, char *argv[]) {
 	INIT_LOGGER ("default.log_prop");
 
+	if (argc != 2) {
+		cout << "Syntax: tACServer portnr" << endl;
+		return (1);
+	}
+
 	try {
-		ApplCtrlFunctions		ACF(doubleParam,
-									doubleParam, 
-									singleParam,
-									singleParam,
-									doubleParam,
-									noParam,
-									doubleParam,
-									doubleParam,
-									doubleParam,
-									ping,
-									supplyInfo);
-//		ACF.handleAckMessage = handleAckMessage;
-
 		uint16	portNr = atoi(argv[1]);
-		ApplControlServer		ServerStub(portNr, ACF);
+		myACImpl				ACImpl;
+		ApplControlServer		ServerStub(portNr, &ACImpl);
 
+		LOG_INFO("Entering main 'while' loop");
 		while (ServerStub.processACmsgFromClient()) {
 			;
 		}
