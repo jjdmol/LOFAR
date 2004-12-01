0d3a9d76b5102167917d8340af0a6ff64af393e5
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-12-01 16:27:19 +0000

    %[ER: 182]% definied typical topology for test

--- CEP/Tools/AccepTest1/src/TestRange.h
+++ CEP/Tools/AccepTest1/src/TestRange.h
@@ -5,17 +5,17 @@
 #ifndef TESTRANGE_H
 #define TESTRANGE_H
 
-const int min_elements  = 50;
-const int max_elements  = 50;
+const int min_elements  = 100;   // equals the #stations
+const int max_elements  = 100;
 
-const int min_samples   = 50;
-const int max_samples   = 50;
+const int min_samples   = 200;   // integration tim of the correlator
+const int max_samples   = 200;
 
 const int port          = 1100;
-const int channels      = 1;
-const int runs          = 10;
+const int channels      = 10;    // frequency channels per correlator
+const int runs          = 10;    // length of the test run
 
-const int targets       = 2;       /* The number of compute nodes per front end */
+const int targets       = 8;       /* The number of compute nodes per front end */
 
 char* frontend_ip       = "192.168.1.117";
 
