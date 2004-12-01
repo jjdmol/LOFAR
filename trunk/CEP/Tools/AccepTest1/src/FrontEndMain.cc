b355929d6fbee41f99008f8e2c8c9835c56d93da
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-12-01 15:02:43 +0000

    %[ER: 182]% removed GPL renamed simulator to frontend

--- trunk/CEP/Tools/AccepTest1/src/FrontEndMain.cc
+++ trunk/CEP/Tools/AccepTest1/src/FrontEndMain.cc
@@ -2,21 +2,7 @@
 //#
 //#  Copyright (C) 2002-2004
 //#  ASTRON (Netherlands Foundation for Research in Astronomy)
-//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
-//#
-//#  This program is free software; you can redistribute it and/or modify
-//#  it under the terms of the GNU General Public License as published by
-//#  the Free Software Foundation; either version 2 of the License, or
-//#  (at your option) any later version.
-//#
-//#  This program is distributed in the hope that it will be useful,
-//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//#  GNU General Public License for more details.
-//#
-//#  You should have received a copy of the GNU General Public License
-//#  along with this program; if not, write to the Free Software
-//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, swe@astron.nl
 //#
 //#  $Id$
 
@@ -40,14 +26,14 @@ int main (int argc, const char** argv) {
       
       try {
 		
-	AH_FrontEnd simulator(port, elements, samples,channels, runs, targets);
+	AH_FrontEnd frontend(port, elements, samples,channels, runs, targets);
 	
-	simulator.setarg(argc, argv);
-	simulator.baseDefine();
-	simulator.basePrerun();
-	simulator.baseRun(runs);
- 	simulator.baseDump();
-	simulator.baseQuit();
+	frontend.setarg(argc, argv);
+	frontend.baseDefine();
+	frontend.basePrerun();
+	frontend.baseRun(runs);
+ 	frontend.baseDump();
+	frontend.baseQuit();
 
       } catch (LOFAR::Exception ex) {
 	cout << "Caught a known exception" << endl;
