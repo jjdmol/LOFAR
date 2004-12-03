b98993d18948056a41c808bbeda9dc4990481cc9
wierenga <wierenga@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-12-03 17:00:00 +0000

    %[ER:189 ]% Made setting RCUSettings by single value possible.

--- trunk/MAC/APL/PIC/RSPDriver/src/RCUSettings.h
+++ trunk/MAC/APL/PIC/RSPDriver/src/RCUSettings.h
@@ -33,47 +33,61 @@ namespace RSP_Protocol
 {
   class RCUSettings
   {
-  public:
-    /**
-     * Setting bitfield for an RCU.
-     */
-    typedef struct RCURegisterType
-    {
-      uint8 lba_enable:1;
-      uint8 hba_enable:1;
-      uint8 bandsel:1;
-      uint8 filsel_a:1;
-      uint8 filsel_b:1;
-      uint8 vl_enable:1;
-      uint8 vh_enable:1;
-      uint8 vddvcc_en:1;
-    };
+    public:
+      /**
+       * Setting bitfield for an RCU.
+       */
+      typedef union RCURegisterType
+      {
+	  struct
+	  {
+	      uint8 lba_enable:1; // bit 0
+	      uint8 hba_enable:1; // bit 1
+	      uint8 bandsel:1;    // bit 2
+	      uint8 filsel_a:1;   // bit 3
+	      uint8 filsel_b:1;   // bit 4
+	      uint8 vl_enable:1;  // bit 5
+	      uint8 vh_enable:1;  // bit 6
+	      uint8 vddvcc_en:1;  // bit 7
+	  };
+	  uint8 value;
+      };
 
-    /**
-     * Constructors for a RCUSettings object.
-     * Currently the tv_usec part is always set to 0 irrespective
-     * of the value passed in.
-     */
-    RCUSettings() { }
+      /**
+       * Predefined settings.
+       *                                    bit 7 6 5 4 3 2 1 0
+       */
+      static const uint8 LBL_10_40  = 0xB9; //  1 0 1 1 1 0 0 1
+      static const uint8 LBL_30_90  = 0xB9; //  1 0 1 1 1 0 0 1
+      static const uint8 HB_110_190 = 0xC6; //  1 1 0 0 0 1 1 0
+      static const uint8 HB_170_230 = 0xCE; //  1 1 0 0 1 1 1 0
+      static const uint8 HB_210_240 = 0xD6; //  1 1 0 1 0 1 1 0
+      
+      /**
+       * Constructors for a RCUSettings object.
+       * Currently the tv_usec part is always set to 0 irrespective
+       * of the value passed in.
+       */
+      RCUSettings() { }
 	  
-    /* Destructor for RCUSettings. */
-    virtual ~RCUSettings() {}
+      /* Destructor for RCUSettings. */
+      virtual ~RCUSettings() {}
 
-    /* get reference settings array */
-    blitz::Array<RCURegisterType, 1>& operator()();
+      /* get reference settings array */
+      blitz::Array<RCURegisterType, 1>& operator()();
 
-  public:
-    /*@{*/
-    /**
-     * marshalling methods
-     */
-    unsigned int getSize();
-    unsigned int pack  (void* buffer);
-    unsigned int unpack(void *buffer);
-    /*@}*/
+    public:
+      /*@{*/
+      /**
+       * marshalling methods
+       */
+      unsigned int getSize();
+      unsigned int pack  (void* buffer);
+      unsigned int unpack(void *buffer);
+      /*@}*/
 
-  private:
-    blitz::Array<RCURegisterType, 1> m_registers;
+    private:
+      blitz::Array<RCURegisterType, 1> m_registers;
   };
   
   inline blitz::Array<RCUSettings::RCURegisterType, 1>& RCUSettings::operator()() { return m_registers; }
