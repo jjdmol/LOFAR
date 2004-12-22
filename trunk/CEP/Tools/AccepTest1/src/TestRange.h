32a010852cd42f36508d8730b969c450eeeb6fb7
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2004-12-22 15:13:34 +0000

    %[ER: 182]% Increased sleep() time to 2 seconds and removed some typo's

--- trunk/CEP/Tools/AccepTest1/src/TestRange.h
+++ trunk/CEP/Tools/AccepTest1/src/TestRange.h
@@ -8,13 +8,13 @@
 const int min_elements  = 50;   // equals the #stations
 const int max_elements  = 110;
 
-const int min_samples   = 500;   // integration tim of the correlator
+const int min_samples   = 500;   // integration time of the correlator
 const int max_samples   = 1000;
 
-const int port          = 1100;  // startport; port-port+2*targets will be used
+const int port          = 1100;  // startport; [port,port+2*targets] will be used
 const int channels      = 10;    // frequency channels per correlator
 const int polarisations = 2;     // number of polarisations (usually two)
-const int runs          = 2;    // length of the test run
+const int runs          = 2;     // length of the test run
 
 const int targets       = 8;     // The number of compute nodes per front end
 
