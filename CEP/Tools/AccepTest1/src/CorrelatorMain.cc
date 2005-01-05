f53aa3b450c153700e2bbec1fec9f91875682db8
Chris Broekema <broekema@users.noreply.github.com>
2005-01-05 11:55:24 +0000

    %[ER: 182]% Improvements in LCS/Transport and CEP/TinyCEP made the sleep in CorrelatorMain.cc between closing a socket and opening a new one unnecessary.

--- CEP/Tools/AccepTest1/src/CorrelatorMain.cc
+++ CEP/Tools/AccepTest1/src/CorrelatorMain.cc
@@ -48,8 +48,6 @@ int main (int argc, const char** argv) {
 	  // 	correlator.baseDump();
 	  correlator.baseQuit();
 
-	  sleep(2);
-	  
 	} catch (LOFAR::Exception ex) {
 	  // catch known exceptions
 	  cout << "Caught a known exception" << endl;
