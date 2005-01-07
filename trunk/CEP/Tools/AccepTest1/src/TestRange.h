3cd4046985d70d2af40fb5ae000302c7e67c3070
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-01-07 08:41:37 +0000

    %[ER: 182]% Add the step values

--- trunk/CEP/Tools/AccepTest1/src/TestRange.h
+++ trunk/CEP/Tools/AccepTest1/src/TestRange.h
@@ -6,17 +6,19 @@
 #define TESTRANGE_H
 
 const int min_elements  = 50;   // equals the #stations
-const int max_elements  = 110;
+const int max_elements  = 60;
+const int stp_elements  = 5;
 
-const int min_samples   = 500;   // integration time of the correlator
+const int min_samples   = 1000;   // integration time of the correlator
 const int max_samples   = 1000;
+const int stp_samples   = 50;
 
 const int port          = 1100;  // startport; [port,port+2*targets] will be used
-const int channels      = 10;    // frequency channels per correlator
+const int channels      = 1;    // frequency channels per correlator
 const int polarisations = 2;     // number of polarisations (usually two)
-const int runs          = 2;     // length of the test run
+const int runs          = 3;     // length of the test run
 
-const int targets       = 8;     // The number of compute nodes per front end
+const int targets       = 7;     // The number of compute nodes per front end
 
 char* frontend_ip       = "192.168.100.254";
 
