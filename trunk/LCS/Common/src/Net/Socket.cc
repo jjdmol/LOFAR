cc2a6e0b75fc39a763e2a50e1e90c6db24b096d3
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-01-14 13:46:20 +0000

    %[ER: 182]% BG/L crashed on  the fcntl call. Remove for BG/L for now, but this should be investigasted.

--- trunk/LCS/Common/src/Net/Socket.cc
+++ trunk/LCS/Common/src/Net/Socket.cc
@@ -685,9 +685,11 @@ int32 Socket::setBlocking (bool block)
 	}
 
 	if (itsSocketID >= 0) {					// we must have a socket ofcourse
+#ifndef HAVE_BGL
 		if (fcntl (itsSocketID, F_SETFL, block ? 0 : O_NONBLOCK) < 0) {
 			return (setErrno(SOCKOPT));
 		}
+#endif
 		itsIsBlocking = block;					// register user wish
 		LOG_TRACE_COND(formatString("Socket(%d):setBlocking(%s)", 
 									itsSocketID, block ? "true" : "false"));
