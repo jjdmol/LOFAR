58e262ca538dc25e0512f9eabec0cdeeb01827a7
cvs <cvs@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-01-17 13:13:04 +0000

    Moved via cvsmv to LOFAR/Timba/VisCube/src

--- CEP/CPA/VisCube/src/VisCommon.cc
+++
@@ -1,8 +0,0 @@
-#include "VisCube/VisCommon.h"
-#include "VisCube/TID-VisCube.h"
-    
-// make sure registry is puled in
-static int dum = aidRegistry_VisCube();    
-    
-// init the debug context
-InitDebugContext(VisCubeDebugContext,"VisCube");
