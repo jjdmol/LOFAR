de7a161f27b02e53d7a367002bde0bd4c1a08c20
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-02-01 09:23:10 +0000

    %[ER: 206]% Make it work by temporarily outcommenting solve2

--- trunk/CEP/BB/BBS3/test/tPredSolv.cc
+++ trunk/CEP/BB/BBS3/test/tPredSolv.cc
@@ -81,13 +81,85 @@ void doSolve (Prediffer& pre1, const vector<string>& solv)
   delete [] buffer;
 }
 
+void doSolve2 (Prediffer& pre1, Prediffer& pre2, const vector<string>& solv)
+{
+  Solver solver;
+  solver.initSolvableParmData (2);
+  // Set the solvable parameters.
+  pre1.clearSolvableParms();
+  pre2.clearSolvableParms();
+  pre1.setSolvableParms (solv, vector<string>(), true);
+  pre2.setSolvableParms (solv, vector<string>(), true);
+  // Set a domain.
+  ///  vector<uint32> shape = pre2.setDomain (137750000-250000, 2*500000,
+  vector<uint32> shape1 = pre1.setDomain (0., 1e12,
+					  0., 1e12);
+  vector<uint32> shape2 = pre2.setDomain (0., 1e12,
+					  0., 1e12);
+  // Get the ParmData from the Prediffers and send it to the solver.
+  solver.setSolvableParmData (pre1.getSolvableParmData(), 0);
+  solver.setSolvableParmData (pre2.getSolvableParmData(), 1);
+  {
+    // Get the equations.
+    uint nrval = shape1[0] * shape1[1] * shape1[2];
+    uint bufnreq = shape1[2];
+    uint totnreq = shape1[3];
+    uint nrloop = (totnreq + bufnreq - 1) / bufnreq;
+    cout << "bufShape-1 " << shape1 << endl;
+    double* buffer = new double[nrval];
+    // Get the equations from the prediffer and give them to the solver.
+    for (uint i=0; i<nrloop; i++) {
+      bool more = pre1.getEquations (buffer, shape1);
+      uint nreq = bufnreq;
+      if (i == nrloop-1) {
+	nreq = totnreq  - (nrloop-1)*bufnreq;
+	ASSERT (!more);
+      } else {
+	ASSERT (more);
+      }
+      solver.setEquations (buffer, nreq, shape1[1], shape1[0], 0);
+    }
+    delete [] buffer;
+  }
+  {
+    uint nrval = shape2[0] * shape2[1] * shape2[2];
+    uint bufnreq = shape2[2];
+    uint totnreq = shape2[3];
+    uint nrloop = (totnreq + bufnreq - 1) / bufnreq;
+    cout << "bufShape-2 " << shape2 << endl;
+    double* buffer = new double[nrval];
+    // Get the equations from the prediffer and give them to the solver.
+    for (uint i=0; i<nrloop; i++) {
+      bool more = pre2.getEquations (buffer, shape2);
+      uint nreq = bufnreq;
+      if (i == nrloop-1) {
+	nreq = totnreq  - (nrloop-1)*bufnreq;
+	ASSERT (!more);
+      } else {
+	ASSERT (more);
+      }
+      solver.setEquations (buffer, nreq, shape2[1], shape2[0], 1);
+    }
+    delete [] buffer;
+  }
+
+  // Do the solve.
+  streamsize prec = cout.precision();
+  cout << "Before: " << setprecision(10) << solver.getSolvableValues() << endl;
+  Quality quality;
+  solver.solve (false, quality);
+  cout << "After:  " << setprecision(10) << solver.getSolvableValues() << endl;
+  cout.precision (prec);
+}
+
 
 int main (int argc, const char* argv[])
 {
   cout << ">>>" << endl;
   try {
-    if (argc < 4) {
-      cerr << "Run as: tPredSolv msname meqparmtable skyparmtable" << endl;
+    if (argc < 5) {
+      cerr << "Run as: tPredSolv user msname meqparmtable skyparmtable"
+	   << endl;
       return 1;
     }
     // Do a solve for RA using a few stations.
@@ -96,9 +168,8 @@ int main (int argc, const char* argv[])
       for (uint i=0; i<antVec.size(); ++i) {
 	antVec[i] = 2*i;
       }
-      Prediffer pre1(argv[1], argv[2], argv[3], "aips", "test", "", "",
+      Prediffer pre1(argv[2], argv[3], argv[4], "aips", argv[1], "", "",
 		     antVec, "LOFAR.RI", false, true);
-      
       // Do a further selection of a few stations.
       vector<int> antVec2(10);
       for (uint i=0; i<antVec2.size(); ++i) {
@@ -111,13 +182,38 @@ int main (int argc, const char* argv[])
       solv[2] = "StokesI.*";
       doSolve (pre1, solv);
     }
-    // Do a solve for all position info using all stations.
+    // Do a solve using 2 prediffers.
+    /*
+    {
+      vector<int> antVec(10);
+      for (uint i=0; i<antVec.size(); ++i) {
+	antVec[i] = 2*i;
+      }
+      Prediffer pre1(argv[2], argv[3], argv[4], "aips", argv[1], "", "",
+		     antVec, "LOFAR.RI", false, true);
+      Prediffer pre2(argv[2], argv[3], argv[4], "aips", argv[1], "", "",
+		     antVec, "LOFAR.RI", false, true);
+      // Do a further selection of a few stations.
+      vector<int> antVec2(10);
+      for (uint i=0; i<antVec2.size(); ++i) {
+	antVec2[i] = 4*i;
+      }
+      pre1.select (antVec2, antVec2, false);    // no autocorrelations
+      pre2.select (antVec2, antVec2, false);    // no autocorrelations
+      vector<string> solv(3);
+      solv[0] = "RA.*";
+      solv[1] = "DEC.*";
+      solv[2] = "StokesI.*";
+      doSolve2 (pre1, pre2, solv);
+    }
+    */
+    // Do a solve using all stations.
     {
       vector<int> antVec(100);
       for (uint i=0; i<antVec.size(); ++i) {
 	antVec[i] = i;
       }
-      Prediffer pre1(argv[1], argv[2], argv[3], "aips", "test", "", "",
+      Prediffer pre1(argv[2], argv[3], argv[4], "aips", argv[1], "", "",
 		     antVec, "LOFAR.RI", false, true);
       vector<string> solv(3);
       solv[0] = "RA.*";
