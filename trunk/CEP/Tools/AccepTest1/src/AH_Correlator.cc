daf637123fce0be1f43856185800f894bbe127cc
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-02-18 11:05:35 +0000

    %[ER: 182]% remove token based connect, since we have implemented it differently

--- trunk/CEP/Tools/AccepTest1/src/AH_Correlator.cc
+++ trunk/CEP/Tools/AccepTest1/src/AH_Correlator.cc
@@ -83,25 +83,11 @@ void AH_Correlator::undefine() {
 
 
 void AH_Correlator::init() {
-#ifdef HAVE_MPI
-  int  myrank = TH_MPI::getCurrentRank();
-  int maxrank = TH_MPI::getNumberOfNodes();
-  MPI_Status status;
-  int     buf = 0;
-//   cout << "Correlator " << myrank << " wait for token " << endl;
-  if (myrank > 0) MPI_Recv(&buf, 1, MPI_INT, myrank-1, 645327, MPI_COMM_WORLD, &status);
-  cout << "Correlator " << myrank << " has received token. Time =  " << MPI_Wtime() << endl;
-//   cout << "Correlator " << myrank << " start baseprocess " << endl;
-#endif
+
   itsWH->basePreprocess();
-#ifdef HAVE_MPI
-//   cout << "Correlator " << myrank << " send token " << endl;
-  cout << "Correlator " << myrank << " is going to send token. Time =  " << MPI_Wtime() << endl;
-  if (myrank < maxrank-1) MPI_Send(&buf, 1, MPI_INT, myrank+1, 645327, MPI_COMM_WORLD);
-//   cout << "Correlator " << myrank << " ended baseprocess " << endl;
+
   TH_MPI::synchroniseAllProcesses();
-//   if (myrank == maxrank) cout << "All connections initialised" << endl;
-#endif
+
 }
 
 void AH_Correlator::run(int nsteps) {
