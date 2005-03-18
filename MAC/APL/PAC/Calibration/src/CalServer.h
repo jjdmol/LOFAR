29f11e663364698515f26d252505f363a5de385c
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2005-03-18 15:51:00 +0000

    %[ER:246 ]% Initial check-in of CalServer code. This version will be expanded to become the standalone shell for calibration algorithm development.

---
+++ MAC/APL/PAC/Calibration/src/CalServer.h
@@ -0,0 +1,73 @@
+//#  -*- mode: c++ -*-
+//#  CalServer.h: class definition for the Beam Server task.
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef CALSERVER_H_
+#define CALSERVER_H_
+
+#include <GCF/TM/GCF_Control.h>
+
+namespace CAL
+{
+  class CalServer : public GCFTask
+  {
+    public:
+      /**
+       * The constructor of the CalServer task.
+       * @param name The name of the task. The name is used for looking
+       * up connection establishment information using the GTMNameService and
+       * GTMTopologyService classes.
+       */
+      CalServer(string name);
+      virtual ~CalServer();
+
+      /**
+       * Are all ports connected and are we ready to go to the
+       * enabled state?
+       */
+      bool isEnabled();
+
+      /**
+       * The initial state.
+       */
+      GCFEvent::TResult initial(GCFEvent& e, GCFPortInterface &p);
+
+      /**
+       * The undertaker method deletes dead clients on the m_dead_clients list.
+       */
+      void undertaker();
+
+      /**
+       * The enabled state.
+       */
+      GCFEvent::TResult enabled(GCFEvent& e, GCFPortInterface &p);
+
+    private:
+      GCFTCPPort                   m_acceptor;     // connect point for clients
+      std::list<GCFPortInterface*> m_clients;      // list of clients
+      std::list<GCFPortInterface*> m_dead_clients; // list of disconnected clients
+
+      GCFPort m_acmserver; // connection to the ACM server
+  };
+};
+     
+#endif /* CALSERVER_H_ */
