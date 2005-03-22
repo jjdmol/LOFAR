e5c3be6a693f0031faa6ddab83640d7d9b53ef5e
nobody <nobody@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-03-22 12:53:53 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.

---
+++ branches/unlabeled-1.4.2/Demo/CEP/StationCorrelator/src/DH_Vis.h
@@ -0,0 +1,107 @@
+//# DH_Vis.h: Vis DataHolder
+//#
+//# Copyright (C) 2000, 2001
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, swe@astron.nl
+//#
+//# $Id$
+
+#ifndef ONLINEPROTO_DH_VIS_H
+#define ONLINEPROTO_DH_VIS_H
+
+
+#include <lofar_config.h>
+
+#include <Transport/DataHolder.h>
+#include <complex>
+
+using std::complex;
+
+namespace LOFAR
+{
+class DH_Vis: public DataHolder
+{
+public:
+  typedef double  BufferPrimitive;
+  typedef complex<BufferPrimitive> BufferType;
+
+  explicit DH_Vis (const string& name, const int stations, const int channel, const int polarisations);
+
+  DH_Vis(const DH_Vis&);
+
+  virtual ~DH_Vis();
+
+  DataHolder* clone() const;
+
+
+  /// Allocate the buffers.
+  virtual void preprocess();
+
+  /// Deallocate the buffers.
+  virtual void postprocess();
+
+  /// Accessor funtion for the flagged inputs counter
+  int getFlagCounter() const;
+  void setFlagCounter(int); 
+
+  /// Get write access to the Buffer in the DataPacket.
+  BufferType* getBuffer();
+  /// Get read access to the Buffer in the DataPacket.
+  const BufferType* getBuffer() const;
+  BufferType*       getBufferElement(int station1, int station2, int channel, int polarisation);
+  void              addBufferElementVal(int station1, int station2, int channel, int polarisation, BufferType value);
+  void              setBufferElement(int station1, int station2, int channel, int polarisation, BufferType* valueptr);
+  const int         getFBW() const;
+  const unsigned int getBufSize() const;
+
+private:
+  /// Forbid assignment.
+  DH_Vis& operator= (const DH_Vis&);
+
+  BufferType*  itsBuffer;    // array containing frequency spectrum.
+  unsigned int itsBufSize;
+  int          itsFBW; // number of frequency channels within this beamlet
+
+  int nstations;
+  int nchannels;
+  int npolarisations;
+
+  void fillDataPointers();
+};
+
+inline DH_Vis::BufferType* DH_Vis::getBuffer()
+  { return itsBuffer; }
+
+inline const DH_Vis::BufferType* DH_Vis::getBuffer() const
+  { return itsBuffer; }
+
+#define VISADDRESS(station1, station2, channel, polarisation)          \
+                         (channel)*nstations*nstations*npolarisations*npolarisations \
+                       + (station1)*nstations*npolarisations*npolarisations           \
+                       + (station2)*npolarisations*npolarisations                      \
+                       + (polarisation) 
+
+inline DH_Vis::BufferType* DH_Vis::getBufferElement(int station1, int station2, int channel, int polarisation)
+  { 
+    return itsBuffer + VISADDRESS(station1, station2, channel, polarisation);
+  }
+ 
+inline void DH_Vis::setBufferElement(int station1, int station2, int channel, int polarisation, BufferType* valueptr)
+{
+  *( itsBuffer + VISADDRESS(station1, station2, channel, polarisation)  ) = *valueptr;
+};
+
+inline void DH_Vis::addBufferElementVal(int station1, int station2, int channel, int polarisation, BufferType value)
+{
+  *( itsBuffer + VISADDRESS(station1, station2, channel, polarisation) ) += value;
+};
+
+inline const int DH_Vis::getFBW() const
+  { return itsFBW; }
+
+inline const unsigned int DH_Vis::getBufSize() const 
+  { return itsBufSize; }  
+
+}
+
+#endif 
---
+++ branches/unlabeled-1.4.2/Demo/CEP/StationCorrelator/src/WH_Dump.h
@@ -0,0 +1,80 @@
+//# WH_Dump.h: 
+//#
+//# Copyright (C) 2000, 2001
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, swe@astron.nl
+//#
+//#
+//# $Id$
+
+#ifndef BG_CORRELATOR_WH_DUMP_H
+#define BG_CORRELATOR_WH_DUMP_H
+
+#include <sys/time.h>
+
+#include <lofar_config.h>
+#include <tinyCEP/WorkHolder.h>
+#include <Common/KeyValueMap.h>
+
+using namespace std;
+namespace LOFAR
+{
+
+class WH_Dump: public WorkHolder
+{
+public:
+  /// Construct the work holder and give it a name.
+  /// It is possible to specify how many input and output data holders
+  /// are created and how many elements there are in the buffer.
+  /// The first WorkHolder should have nin=0.
+  explicit WH_Dump (const string& name,
+		    const KeyValueMap& kvm,
+		    const string& fileName);
+
+  virtual ~WH_Dump();
+
+  /// Static function to create an object.
+  static WorkHolder* construct (const string& name, 
+				const KeyValueMap& kvm,
+				const string& fileName);
+
+  /// Make a fresh copy of the WH object.
+  virtual WH_Dump* make (const string& name);
+
+  virtual void preprocess();
+  /// Do a process step.
+  virtual void process();
+  virtual void postProcess();
+
+  /// Show the work holder on stdout.
+  virtual void dump();
+
+  float getBandwidth();
+
+private:
+  /// Forbid copy constructor.
+  WH_Dump (const WH_Dump&);
+
+  /// Forbid assignment.
+  WH_Dump& operator= (const WH_Dump&);
+  
+  int itsFBW; // frequency bandwidth of the DH_Beamlet 
+
+  string itsOutputFileName;
+  int itsOutputFile;
+  
+  int itsNelements;
+  int itsNchannels;
+  int itsNpolarisations;
+    
+  struct timeval itsLastTime;
+
+  float itsBandwidth;    // stores 'measured' bandwidth in bytes/sec
+
+  KeyValueMap itsKvm;
+};
+
+ inline float WH_Dump::getBandwidth() { return itsBandwidth; } 
+} // namespace LOFAR
+
+#endif
