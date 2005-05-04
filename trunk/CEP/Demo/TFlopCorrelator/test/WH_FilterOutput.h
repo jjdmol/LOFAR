62faa7b60ecf793d632ea08ba849d25632c9ea2b
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-05-04 19:16:05 +0000

    %[ER: 269]% First setup for test environment of the filter.

---
+++ trunk/CEP/Demo/TFlopCorrelator/test/WH_FilterOutput.h
@@ -0,0 +1,50 @@
+//#  WH_FilterOutput.h: output workholder for filter tester
+//#
+//#  Copyright (C) 2002-2005
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef TFLOP_CORRELATOR_FILTEROUTPUT_H
+#define TFLOP_CORRELATOR_FILTEROUTPUT_H
+
+#include <tinyCEP/WorkHolder.h>
+
+namespace LOFAR
+{
+  class WH_FilterOutput: public WorkHolder {
+
+  public:
+    explicit WH_FilterOutput (const string& name);
+    virtual ~WH_FilterOutput();
+
+    static WorkHolder* construct (const string& name);
+    virtual WH_FilterOutput* make (const string& name);
+
+    virtual void preprocess();
+    virtual void process();
+    virtual void dump();
+
+  private:
+    WH_FilterOutput (const WH_FilterOutput&);
+    WH_FilterOutput& operator= (const WH_FilterOutput&);
+
+  };
+} // namespace LOFAR
+
+#endif
---
+++ trunk/Demo/CEP/TFlopCorrelator/test/WH_FilterInput.h
@@ -0,0 +1,50 @@
+//#  WH_FilterInput.h: input workholder for filter tester
+//#
+//#  Copyright (C) 2002-2005
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef TFLOP_CORRELATOR_FILTERINPUT_H
+#define TFLOP_CORRELATOR_FILTERINPUT_H
+
+#include <tinyCEP/WorkHolder.h>
+
+namespace LOFAR
+{
+  class WH_FilterInput: public WorkHolder {
+
+  public:
+    explicit WH_FilterInput (const string& name);
+    virtual ~WH_FilterInput();
+
+    static WorkHolder* construct (const string& name);
+    virtual WH_FilterInput* make (const string& name);
+
+    virtual void preprocess();
+    virtual void process();
+    virtual void dump();
+
+  private:
+    WH_FilterInput (const WH_FilterInput&);
+    WH_FilterInput& operator= (const WH_FilterInput&);
+
+  };
+} // namespace LOFAR
+
+#endif
