df1271a4e791ce475a5155f861044e89640834ec
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-05-04 11:26:03 +0000

    %[ER: 269]% Correct compile errors; code now compiles, but linking not tested yet

--- trunk/CEP/Demo/TFlopCorrelator/src/SB_Stub.cc
+++ trunk/CEP/Demo/TFlopCorrelator/src/SB_Stub.cc
@@ -8,31 +8,28 @@
 
 #include <TFlopCorrelator/SB_Stub.h>
 #include <Transport/TH_Socket.h>
-#include <ACC/ParameterSet.h>
+
 
 using namespace LOFAR;
 using namespace LOFAR::ACC;
 
 namespace LOFAR { 
 
-  SB_Stub::SB_Stub (int nSBF,
-		    bool stubOnServer)
-    : itsStubOnServer (stubOnServer),
-      itsSB("noname",1), // todo: bring in correct SBID!!
-      itsNSBF (nSBF)
+  SB_Stub::SB_Stub (bool stubOnServer)
+    : itsStubOnServer (stubOnServer)
   {
     itsPS = new ACC::ParameterSet("TFlopCorrelator.cfg");
     itsNSBF     = itsPS->getInt("NSBF");  // number of SubBand filters in the application
     
-    for (int i=0; i<itsNSBF, i++) {
-      itsSB[i] = new DH_SubBand();
+    for (int i=0; i<itsNSBF; i++) {
+      itsSB.push_back(new DH_SubBand("noname",1)); //todo: get correct SubbandID
     }
   }
 
   SB_Stub::~SB_Stub()
   {}
 
-  void SB_Stub::connect (int SBF_nr,
+  void SB_Stub::connect (int& SBF_nr,
 			 DH_SubBand& sb)
   {
     const ParameterSet myPS("TFlopCorrelator.cfg");
@@ -41,11 +38,11 @@ namespace LOFAR {
 		   myPS.getInt("SBConnection.RequestPort"),   // port
 		   true
 		   );
-    itsSB.setID(myPS.getInt("SBConnection.ID"));
+    itsSB[SBF_nr]->setID(itsPS->getInt("SBConnection.ID"));
     if (itsStubOnServer) {
-      itsSB.connectTo (sb, thSB);
+      itsSB[SBF_nr]->connectTo (sb, thSB);
     } else {
-      sb.connectTo (itsSB, thSB);
+      sb.connectTo (*(itsSB[SBF_nr]), thSB);
     }
   };
 
