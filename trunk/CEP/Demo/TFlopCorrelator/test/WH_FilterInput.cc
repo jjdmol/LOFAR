dd335eea9cf127be042edc55a8824ae42d7f8ee0
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-05-10 15:15:24 +0000

    %[ER: 269]% Make config file paramter names more sane

--- trunk/CEP/Demo/TFlopCorrelator/test/WH_FilterInput.cc
+++ trunk/CEP/Demo/TFlopCorrelator/test/WH_FilterInput.cc
@@ -23,6 +23,8 @@
 //# Always #include <lofar_config.h> first!
 #include <lofar_config.h>
 
+#include <ACC/ParameterSet.h>
+
 #include <DH_SubBand.h>
 #include <WH_FilterInput.h>
 
@@ -32,6 +34,12 @@ WH_FilterInput::WH_FilterInput(const string& name):
   WorkHolder(0, 1, name, "WH_FilterInput"),
   itsSBID(0)
 {
+  ACC::ParameterSet  myPS("TFlopCorrelator.cfg");
+  //ParameterCollection	myPC(myPS);
+  itsNtaps     = myPS.getInt("WH_SubBand.taps");
+  itsNStations = myPS.getInt("WH_SubBand.stations");
+  itsCpF       = myPS.getInt("Corr_per_Filter");
+
   getDataManager().addOutDataHolder(0, new DH_SubBand("output",
 						      itsSBID));
 }
@@ -51,6 +59,15 @@ void WH_FilterInput::preprocess() {
 }
 
 void WH_FilterInput::process() {
+  DH_SubBand::BufferType * buffer;
+
+  buffer = ((DH_SubBand*)getDataManager().getOutHolder(0))->getBuffer();
+  memset(buffer, 0, ((DH_SubBand*)getDataManager().getOutHolder(0))->getBufSize() * sizeof(DH_SubBand::BufferType));
+
+  // an impuls signal
+  __real__ *buffer = 1;
+  
+
 }
 
 void WH_FilterInput::dump() {
