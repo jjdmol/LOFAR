9eea8491c774674becea57c076fb87b2ec866480
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-05-26 10:07:44 +0000

    %[ER:279 ]% Dayly update.

--- trunk/SAS/OTDB/sql/create_database.sh
+++ trunk/SAS/OTDB/sql/create_database.sh
@@ -1,3 +1,15 @@
 # create table and add plpgsql to curent database
 createdb test
 createlang plpgsql -d test
+
+create_base_tables.sql
+create_security.sql
+create_tree_table.sql
+create_types.sql
+
+create_log_system.sql
+
+security_func.sql
+
+getTreeList_func.sql
+
