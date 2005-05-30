bf6da42cfe6890e5f80087a0e647fea39e258551
Marcel Loose <gmloose@users.noreply.github.com>
2005-05-30 08:53:36 +0000

    %[ER: 252]%
    Now uses enhanced runtest.sh to limit execution time.

--- CEP/CEPFrame/test/CyclicBufferTest.sh
+++ CEP/CEPFrame/test/CyclicBufferTest.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 #
-# Limit the running time of this test to get out of possible deadlock situation
+# Limit the running time of this test to 90 seconds 
+# to get out of possible deadlock situation
 #
    echo 'call "../../../test/CyclicBufferTest.gnuplot" "CB.writers" 0 20' >  gnuplot.cmd \
 && echo 'call "../../../test/CyclicBufferTest.gnuplot" "CB.readers" 0 20' >> gnuplot.cmd \
-&& $common_top_builddir/src/LimitExec 90 ./CyclicBufferTest > CB.log 2> CB.err \
+&& $lofar_sharedir/runtest.sh ./CyclicBufferTest 90 > CB.log 2> CB.err \
 && cat CB.*.write > CB.writers \
 && cat CB.*.read  > CB.readers \
 && gnuplot < gnuplot.cmd \
