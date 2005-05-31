17ae45bf39a4c44bc80deca5e7746c1a2a2872bb
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-05-31 14:47:29 +0000

    %[ER:279]% Dayly update.

---
+++ trunk/SAS/OTDB/test/tPICadmin.cc
@@ -0,0 +1,91 @@
+//#  tPICadmin: test the PICadmin class
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+//# Includes
+#include <Common/LofarLogger.h>
+#include <Common/lofar_datetime.h>
+#include <OTDB/PICadmin.h>
+#include <OTDB/OTDBtypes.h>
+
+using namespace LOFAR;
+using namespace LOFAR::OTDB;
+
+//
+// show the result
+//
+void showList(const vector<treeInfo>&	trees) {
+
+
+	cout << "treeID|Classif|Creator   |Creationdate        |Type|Campaign|Starttime" << endl;
+	cout << "------+-------+----------+--------------------+----+--------+------------------" << endl;
+	for (uint32	i = 0; i < trees.size(); ++i) {
+		string row(formatString("%6d|%7d|%-10.10s|%-20.20s|%4d|%-8.8s|%s",
+			trees[i].ID,
+			trees[i].classification,
+			trees[i].creator.c_str(),
+			to_simple_string(trees[i].creationDate).c_str(),
+			trees[i].type,
+			trees[i].campaign.c_str(),
+			to_simple_string(trees[i].starttime).c_str()));
+		cout << row << endl;
+	}
+
+	cout << trees.size() << " records" << endl << endl;
+}
+
+int main (int	argc, char*	argv[]) {
+
+	INIT_LOGGER(basename(argv[0]));
+	LOG_INFO_STR("Starting " << argv[0]);
+
+	OTDBconnection conn("paulus", "boskabouter", "overeem");
+
+	try {
+
+		LOG_DEBUG("Trying to connect to the database");
+		ASSERTSTR(conn.connect(), "Connnection failed");
+		LOG_INFO_STR("Connection succesful: " << conn);
+
+		LOG_DEBUG("Trying to construct a PICadmin object");
+		PICadmin	pa(&conn);
+
+		LOG_DEBUG("Trying to load a master PIC file");
+		treeIDType	treeID = pa.loadMasterFile (argv[1]);
+
+		LOG_INFO_STR("New tree has ID: " << treeID);
+
+		LOG_DEBUG("Trying to classify the tree");
+		pa.classify(treeID, 23);
+
+	}
+	catch (std::exception&	ex) {
+		LOG_FATAL_STR("Unexpected exception: " << ex.what());
+		return (1);		// return !0 on failure
+	}
+
+	LOG_INFO_STR ("Terminated succesfully: " << argv[0]);
+
+	return (0);		// return 0 on succes
+}
---
+++ trunk/SAS/OTDB/test/tPICadmin_test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+$lofar_sharedir/runtest.sh tPICadmin PICmasterfile.txt 2>&1 > tPICadmin_test.log
