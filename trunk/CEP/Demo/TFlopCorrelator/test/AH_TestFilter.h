d88d244caecf9af6861729fa511f8fc84332f8c8
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-08 11:50:10 +0000

    Fixed a duplicate variable in method definition

--- trunk/CEP/Demo/TFlopCorrelator/test/AH_TestFilter.h
+++ trunk/CEP/Demo/TFlopCorrelator/test/AH_TestFilter.h
@@ -60,7 +60,7 @@ namespace LOFAR
     virtual void dump();
     virtual void postrun();
     virtual void quit();
-    void connectWHs(WorkHolder* srcWH, int dstDH, WorkHolder* dstWHs, int dstDH);
+    void connectWHs(WorkHolder* srcWH, int srcDH, WorkHolder* dstWHs, int dstDH);
 
   private:
     vector<WorkHolder*> itsWHs;
