d190fe7f066d5d0b600a4c372e4d5936c7ded93f
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-09 12:48:30 +0000

    %[ER: 269]%
    Created three executables

---
+++ CEP/Demo/TFlopCorrelator/src/TFC_BGLProcessing_main.cc
@@ -0,0 +1,44 @@
+//#  TFC_BGLProcessing_main.cc:
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+#include <Common/lofar_iostream.h> 
+#include <Common/LofarLogger.h>
+
+#include <TFlopCorrelator/AH_BGLProcessing.h>
+
+using namespace LOFAR;
+
+int main (int argc, const char** argv) {
+
+  INIT_LOGGER("TFC_BGLProcessing");
+
+  try {
+    AH_BGLProcessing myAH();
+    myAH.setarg(argc, argv);
+    myAH.baseDefine();
+    cout << "defined" << endl;
+    Profiler::init();
+    myAH.basePrerun();
+    cout << "init done" << endl;
+    Profiler::activate();
+    cout << "run" << endl;
+    myAH.baseRun(kvm.getInt("runsteps",1));
+    cout << "run complete" << endl;
+    myAH.baseDump();
+    myAH.baseQuit();
+    Profiler::deActivate();
+
+  } catch (std::exception& x) {
+    cout << "Unexpected exception" << endl;
+    cerr << x.what() << endl; 
+  }
+  return 0;
+}
