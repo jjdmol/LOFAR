073dce33b8d1a1dd05e241c584163ce3f9ca3f1c
Marcel Loose <gmloose@users.noreply.github.com>
2005-06-10 14:41:03 +0000

    %[ER: 231]%
    Removed some remnants of an unintentional merge with development code

--- LCS/Transport/test/ExampleShMem_test.sh
+++ LCS/Transport/test/ExampleShMem_test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-lofar_sharedir=//home/overeem/data/NEWTRANS/LOFAR/autoconf_share
 $lofar_sharedir/runtest.sh ExampleShMem > ExampleShMem_test.log 2>&1
--- LCS/Transport/test/ExampleVarBuf_test.sh
+++ LCS/Transport/test/ExampleVarBuf_test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-lofar_sharedir=//home/overeem/data/NEWTRANS/LOFAR/autoconf_share
 $lofar_sharedir/runtest.sh ExampleVarBuf > ExampleVarBuf_test.log 2>&1
