100306d0e482bf8793ef86cec35b425232e57bef
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-10 12:03:39 +0000

    %[ER: 269]%
    Changed main program
    Now they also work with ACC

--- trunk/CEP/Demo/TFlopCorrelator/src/TFC_Storage_main.cc
+++ trunk/CEP/Demo/TFlopCorrelator/src/TFC_Storage_main.cc
@@ -12,34 +12,49 @@
 #include <Common/lofar_iostream.h> 
 #include <Common/LofarLogger.h>
 
-#include <Common/RunOnNode.h>
+#include <tinyCEP/ApplicationHolderController.h>
 #include <TFlopCorrelator/AH_Storage.h>
 
 using namespace LOFAR;
 
 int main (int argc, const char** argv) {
-
   INIT_LOGGER("TFC_Storage");
 
+  // Check invocation syntax
   try {
-    AH_Storage myAH();
-    myAH.setarg(argc, argv);
-    myAH.baseDefine();
-    cout << "defined" << endl;
-    Profiler::init();
-    myAH.basePrerun();
-    cout << "init done" << endl;
-    Profiler::activate();
-    cout << "run" << endl;
-    myAH.baseRun(kvm.getInt("runsteps",1));
-    cout << "run complete" << endl;
-    myAH.baseDump();
-    myAH.baseQuit();
-    Profiler::deActivate();
+    if (argc < 2) {
+      LOG_TRACE_FLOW("Main program not started by ACC");
+      // there are no commandline arguments, so we were not called by ACC
 
-  } catch (std::exception& x) {
-    cout << "Unexpected exception" << endl;
-    cerr << x.what() << endl; 
-  }
-  return 0;
+      AH_Storage myAH();
+      myAH.setarg(argc, argv);
+      myAH.baseDefine();
+      cout << "defined" << endl;
+      Profiler::init();
+      myAH.basePrerun();
+      cout << "init done" << endl;
+      Profiler::activate();
+      cout << "run" << endl;
+      myAH.baseRun(1);
+      cout << "run complete" << endl;
+      myAH.baseDump();
+      myAH.baseQuit();
+      Profiler::deActivate();
+    } else {
+      LOG_TRACE_FLOW("Main program started by ACC");
+      // we were called by ACC so execute the ACCmain
+      AH_InputSection myAH;
+      ApplicationHolderController myAHController(myAH);
+      myAHController.main(argc, argv);
+    }
+  } catch (Exception& ex) {
+    LOG_FATAL_STR("Caught exception: " << ex << endl);
+    LOG_FATAL_STR(argv[0] << " terminated by exception!");
+    exit(1);
+  } catch (...) {
+    LOG_FATAL_STR("Caught unknown exception, exitting");
+    exit (1);
+  }  
+  LOG_INFO_STR(argv[0] << " terminated normally");
+  return (0);
 }
