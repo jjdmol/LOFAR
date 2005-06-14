b5478f3697458e342affccc807a4a23a91941425
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-14 12:12:38 +0000

    %[ER: 269]%
    last split off adds

---
+++ Demo/CEP/TFlopCorrelator/TFC_InputSection/src/Attic/BufferController.h
@@ -0,0 +1,207 @@
+//#  BufferController.h: template class to control a cyclic buffer.
+//#
+//#  Copyright (C) 2000, 2001
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+
+#ifndef TFLOPCORRELATOR_BUFFERCONTROLLER_H
+#define TFLOPCORRELATOR_BUFFERCONTROLLER_H
+
+#include <pthread.h>
+#include "CyclicBuffer.h"
+
+
+/* Main purpose of the BufferController class is to control a type independant
+   cyclic buffer */
+
+
+namespace LOFAR
+{
+
+template <class TYPE>
+class BufferController
+{
+public:
+
+  BufferController(int size);
+  ~BufferController();
+  
+  TYPE* getBufferReadPtr();
+  TYPE* getBufferWritePtr();
+
+  TYPE* getFirstReadPtr(int& ID);
+  TYPE* getUserReadPtr(uint offset);
+  TYPE* getUserWritePtr(uint ID);
+  
+  void readyReading();
+  void readyWriting();
+  void dump();
+  
+  int getBufferSize(); // maximum number of elements in Cyclic Buffer
+  int getBufferCount();// actual number of elements in Cyclic Buffer
+
+private:
+  
+  // current read info
+  TYPE* itsCurrentReadPtr;
+  int itsCurrentReadID;
+  
+  // current write info
+  TYPE* itsCurrentWritePtr;
+  int itsCurrentWriteID;
+
+  CyclicBuffer<TYPE*> itsBuffer;
+  int itsBufferSize;
+
+  TYPE* itsBufferItems;  
+};
+  
+template<class TYPE>
+BufferController<TYPE>::BufferController(int size)
+  : itsBufferSize(size)  
+{
+  LOG_TRACE_FLOW("BufferController constructor");
+
+  itsCurrentReadPtr = (TYPE*)0;
+  itsCurrentReadID = -1;
+
+  itsCurrentWritePtr = (TYPE*)0;
+  itsCurrentWriteID = -1;
+
+  // construct cyclic buffer
+  itsBufferItems = new TYPE[size];
+  for (int i = 0; i < itsBufferSize; i++) 
+  {  
+    itsBuffer.AddBufferItem(&itsBufferItems[i]);
+  }
+  itsBuffer.setWrittenBeforeReading(0); 
+}
+
+template<class TYPE>
+BufferController<TYPE>::~BufferController()
+{
+  LOG_TRACE_FLOW("BufferController destructor");
+
+  // clear the cyclic buffer
+  itsBuffer.RemoveItems();
+  delete[] itsBufferItems;
+}
+
+template<class TYPE>
+TYPE* BufferController<TYPE>::getBufferReadPtr()
+{
+  if (itsCurrentReadPtr == 0) {
+    itsCurrentReadPtr = itsBuffer.GetBufferReadPtr(itsCurrentReadID);
+  }
+  return itsCurrentReadPtr; 
+}
+
+template<class TYPE>
+TYPE*  BufferController<TYPE>::getBufferWritePtr()
+{
+  if (itsCurrentWritePtr == 0) {
+    itsCurrentWritePtr = itsBuffer.GetBufferWritePtr(itsCurrentWriteID);
+  }
+  return itsCurrentWritePtr; 
+}
+
+template<class TYPE>
+TYPE* BufferController<TYPE>::getFirstReadPtr(int& ID)
+{
+  if (itsCurrentReadPtr ==0) {
+    itsCurrentReadPtr = itsBuffer.GetFirstReadPtr(itsCurrentReadID);
+  }
+  ID = itsCurrentReadID; 
+  return itsCurrentReadPtr; 
+}
+
+template<class TYPE>
+TYPE* BufferController<TYPE>::getUserReadPtr(uint offset)
+{
+  if (itsCurrentReadPtr == 0) {
+    itsCurrentReadPtr = itsBuffer.GetUserReadPtr(offset, itsCurrentReadID);
+  }
+  return itsCurrentReadPtr; 
+}
+
+template<class TYPE>
+TYPE* BufferController<TYPE>::getUserWritePtr(uint ID)
+{
+  if (itsCurrentWritePtr == 0) {
+    itsCurrentWritePtr = itsBuffer.GetUserWritePtr(ID);
+    itsCurrentWriteID = ID;
+  }
+  return itsCurrentWritePtr; 
+}
+
+template<class TYPE>
+void BufferController<TYPE>::readyReading()
+{
+  if (itsCurrentReadID >= 0) 
+  {
+    itsBuffer.ReadUnlockItem(itsCurrentReadID); 
+  }
+  else
+  {
+    LOG_TRACE_RTTI("BufferController::readpointer not previously requested with getradPtr() function");
+  }
+  itsCurrentReadPtr = (TYPE*)0;
+  itsCurrentReadID = -1;
+}
+
+
+
+template<class TYPE>
+void BufferController<TYPE>::readyWriting()
+{
+  if (itsCurrentWriteID >= 0) 
+  {
+    itsBuffer.WriteUnlockItem(itsCurrentWriteID);
+  }
+  else {
+    LOG_TRACE_RTTI("BufferController::writepointer not previously requested with getWritePtr() function");
+  }
+  
+  itsCurrentWritePtr = (TYPE*)0;
+  itsCurrentWriteID = -1;
+}
+
+template<class TYPE>
+int BufferController<TYPE>::getBufferSize()
+{
+  return itsBuffer.getSize();
+}
+
+template<class TYPE>
+int BufferController<TYPE>::getBufferCount()
+{
+  return itsBuffer.getCount();
+}
+
+template<class TYPE>
+void BufferController<TYPE>::dump()
+{
+  itsBuffer.Dump();
+}
+
+
+}
+
+#endif
---
+++ Demo/CEP/TFlopCorrelator/TFC_Interface/src/RectMatrix.h
@@ -0,0 +1,195 @@
+//# RectMatrix.h: A class that holds a rectangular matrix that can be 
+//# addressed fast and easy to use (i.e. no pointers etc)
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#
+//# $Id$
+
+#ifndef TFLOP_CORRELATOR_MATRIX
+#define TFLOP_CORRELATOR_MATRIX
+
+#include<Common/LofarLogger.h>
+#include<Common/lofar_vector.h>
+#include<Common/lofar_map.h>
+#include<Common/lofar_string.h>
+
+using LOFAR::string;
+using LOFAR::vector;
+using LOFAR::map;
+
+class DimDef {
+ public:
+  DimDef(const string& newName, const int newSize):
+    name(newName),
+    size(newSize)
+    {};
+  string name;
+  int size;
+};
+
+class dimType{
+ public:
+  dimType(int ms = 0, int ne = 0) :
+    memSize(ms),
+    noElem(ne){total = ms*ne;};
+  int memSize; // how many values from this element to the next element in this dimension
+  int noElem;  // how many elements in this dimension
+  int total;   // memSize*noElem, used for for-loops
+  int operator*(int steps) {
+    return steps * memSize;};
+  friend int operator*(int steps, dimType& dim) {
+    return steps * dim.memSize;};
+};
+
+
+// this class holds a matrix
+// for example programs using this class see tRectMatrix.cc in the test directory
+
+template <typename valueType>
+class RectMatrix {
+ public:
+  RectMatrix(vector<DimDef>);
+  //  ~RectMatrix();
+  
+  typedef int cursorType; // is the offset of the datamember in itsData
+  
+  // get a dimension identifier by name
+  dimType& getDim(string dimName);
+  // get number of elements in this dimension
+  // don't use this function to much because it uses a map!
+  int getNElemInDim(const dimType& dim) const;
+  // get a cursor at a certain position in the matrix
+  // use this as 4*firstDim + 2* secondDim for element(4,2)
+  cursorType getCursor(const int pos) const; 
+  // get the value at the position of the cursor
+  valueType& getValue(const cursorType& cursor) const;
+  // set the value at the position of the cursor
+  void setValue(const cursorType& cursor, const valueType& value);
+  // move the cursor in a dimension
+  void moveCursor(cursorType* cursor, const dimType& dim) const;
+  // move the cursor in a dimension a certain number of steps
+  void moveCursorN(cursorType* cursor, const dimType& dim, const int& steps) const;
+  
+  // copy a subblock of the matrix
+  // note that the lower dimensions of both matrixes have to match
+  // it makes no sense to copy a block of information if it doesn't have the same
+  // meaning in the other matrix
+  void cpy2Matrix (cursorType srcCursor, 
+		   dimType& dim,
+		   RectMatrix& dstMatrix,
+		   cursorType dstCursor, 
+		   dimType& dim,
+		   int noBlocks);
+  void cpyFromBlock (valueType* srcPointer, 
+		     int blockSize,
+		     cursorType dstCursor, 
+		     dimType& dim,
+		     int noBlocks);
+  void cpy2Block (cursorType srcCursor, 
+		  dimType& dim,
+		  valueType* dstPointer, 
+		  int blockSize,
+		  int noBlocks);
+  // point the matrix to the memory it should use
+  void setBuffer(valueType* buffer, int size);
+    
+ private:
+  friend class DataHolder;
+
+  valueType* itsData;
+  map<string, dimType*> itsDimMap;
+  int itsTotalSize;
+};
+
+template <typename valueType>
+RectMatrix<valueType>::RectMatrix(vector<DimDef> dimdefv) :
+  itsData(0)
+{
+  vector<DimDef>::reverse_iterator it = dimdefv.rbegin();
+  int lastSize = 1;
+  for (; it<dimdefv.rend(); it++) {
+    itsDimMap[it->name] = new dimType(lastSize, it->size);
+    lastSize *= it->size;
+  }
+  itsTotalSize = lastSize;
+}
+
+template <typename valueType>
+inline dimType& RectMatrix<valueType>::getDim(string dimName) {
+  return *itsDimMap[dimName];};
+
+template <typename valueType>
+inline int RectMatrix<valueType>::getNElemInDim(const dimType& dim) const {
+  return dim.noElem;};
+
+template <typename valueType>
+inline typename RectMatrix<valueType>::cursorType RectMatrix<valueType>::getCursor(const int pos) const { 
+  return pos; };
+
+template <typename valueType>
+inline valueType& RectMatrix<valueType>::getValue(const cursorType& cursor) const {
+  DBGASSERTSTR(cursor<itsTotalSize, "Index out of range in RectMatrix::getValue()");
+  return itsData[cursor];};
+
+template <typename valueType>
+inline void RectMatrix<valueType>::setValue(const cursorType& cursor, const valueType& value) {
+  DBGASSERTSTR(cursor<itsTotalSize, "Index out of range in RectMatrix::setValue()");
+  itsData[cursor] = value; };
+
+template <typename valueType>
+inline void RectMatrix<valueType>::moveCursor(cursorType* cursorp, const dimType& dim) const {
+  *cursorp += dim.memSize;};
+
+template <typename valueType>
+inline void RectMatrix<valueType>::moveCursorN(cursorType* cursorp, const dimType& dim, const int& steps) const {
+  *cursorp += dim.memSize*steps; };
+
+template <typename valueType>
+inline void RectMatrix<valueType>::cpy2Matrix (cursorType srcCursor, 
+					       dimType& srcDim,
+					       RectMatrix& dstMatrix,
+					       cursorType dstCursor, 
+					       dimType& dstDim,
+					       int noBlocks) {
+  DBGASSERTSTR(srcDim.memSize == dstDim.memSize, "Dimension to be copied  are not compatible");
+  DBGASSERTSTR((dstCursor+noBlocks*dstDim.memSize)<=dstMatrix.itsTotalSize, "Index out of range in destination in RectMatrix::cpy2Matrix");
+  DBGASSERTSTR((srcCursor+noBlocks*srcDim.memSize)<=itsTotalSize, "Index out of range in source in RectMatrix::cpy2Matrix");
+  memcpy(dstMatrix.itsData[dstCursor], itsData[srcCursor], noBlocks * srcDim.memSize * sizeof(valueType));
+};
+template <typename valueType>
+inline void RectMatrix<valueType>::cpyFromBlock (valueType* srcPointer, 
+						 int blockSize,
+						 cursorType dstCursor, 
+						 dimType& dstDim,
+						 int noBlocks) {
+  DBGASSERTSTR(blockSize == dstDim.memSize, "Dimension to be copied  are not compatible");
+  cerr<<dstCursor<<" "<<dstDim.memSize<<" "<<itsTotalSize<<endl;
+  DBGASSERTSTR((dstCursor+noBlocks*dstDim.memSize)<=itsTotalSize, "Index out of range in destination in RectMatrix::cpyFromBlock");
+  memcpy(&(itsData[dstCursor]), srcPointer, noBlocks * blockSize * sizeof(valueType));
+}
+template <typename valueType>
+inline void RectMatrix<valueType>::cpy2Block (cursorType srcCursor, 
+					      dimType& srcDim,
+					      valueType* dstPointer, 
+					      int blockSize,
+					      int noBlocks) {
+  DBGASSERTSTR(srcDim.memSize == blockSize, "Dimension to be copied  are not compatible");
+  DBGASSERTSTR((srcCursor+noBlocks*srcDim.memSize)<=itsTotalSize, "Index out of range in source in RectMatrix::cpy2Block");
+  memcpy((void*)dstPointer, *(itsData[srcCursor]), noBlocks * blockSize * sizeof(valueType));
+}
+
+template <typename valueType>
+inline void RectMatrix<valueType>::setBuffer(valueType* buffer, int size){
+  DBGASSERTSTR(size == itsTotalSize, "cannot set buffer because of size mismatch");
+  itsData = buffer;
+};
+    
+#define MATRIX_FOR_LOOP(matrix, dim, cursor) \
+int cursorMax = cursor + dim.total;\
+    for (; cursor < cursorMax; matrix.moveCursor(&cursor, dim))
+#define MATRIX_FOR_LOOP_OLD(matrix, dim, cursor) \
+    for (int cursorIT = 0; cursorIT < matrix.getNElemInDim(dim); cursorIT ++ , matrix.moveCursor(&cursor, dim))
+
+#endif
