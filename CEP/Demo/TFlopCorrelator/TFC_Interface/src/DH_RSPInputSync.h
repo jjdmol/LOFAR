03cd62f2a415253ea27c25bc46011594bb8503f3
lang <lang@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-16 11:41:38 +0000

    %[ER:269]% initial version of DH_RSPInputSync

---
+++ CEP/Demo/TFlopCorrelator/TFC_Interface/src/DH_RSPInputSync.h
@@ -0,0 +1,61 @@
+//# DH_RSPInputSync.h: DataHolder used to synchronize incoming RSP data
+//#
+//# Copyright (C) 2000, 2001
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, swe@astron.nl
+//#
+//# $Id$
+
+#ifndef TFLOPCORRELATOR_DH_RSPINPUTSYNC_H
+#define TFLOPCORRELATOR_DH_RSPINPUTSYNC_H
+
+
+#include <lofar_config.h>
+
+#include <Transport/DataHolder.h>
+#include <RSPTimeStamp.h>
+
+namespace LOFAR
+{
+  class DH_RSPInputSync: public DataHolder
+{
+public:
+
+  explicit DH_RSPInputSync (const string& name);
+
+  DH_RSPInputSync(const DH_RSPInputSync&);
+
+  virtual ~DH_RSPInputSync();
+
+  DataHolder* clone() const;
+
+
+  /// Allocate the buffers.
+  virtual void preprocess();
+
+  /// Set the sync stamp
+  void setSyncStamp(const timestamp_t syncStamp);
+
+  /// Get the sync stamp
+  const timestamp_t getSyncStamp() const;
+  void incrementStamp(const int value);
+
+private:
+  /// Forbid assignment.
+  DH_RSPInputSync& operator= (const DH_RSPInputSync&);
+
+  timestamp_t*  itsSyncStamp;
+
+  void fillDataPointers();
+};
+
+inline void DH_RSPInputSync::setSyncStamp(const timestamp_t syncStamp)
+  { *itsSyncStamp = syncStamp; }
+ 
+inline const timestamp_t DH_RSPInputSync::getSyncStamp() const
+  { return *itsSyncStamp;}
+
+inline void DH_RSPInputSync::incrementStamp(const int value)
+  { *itsSyncStamp += value;}
+}
+#endif 
