b4424c35c04a157c6b37d18c9f995cb595963ac7
meijeren <meijeren@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-23 10:16:41 +0000

    %[ER: 269]% Created DelayCompensation package (skeleton).

---
+++ CEP/Demo/TFlopCorrelator/TFC_DelayCompensation/src/TFC_DelayCompensation_main.cc
@@ -0,0 +1,60 @@
+//#  TFC_DelayCompensation_main.cc:
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+#include <Common/lofar_iostream.h> 
+#include <Common/LofarLogger.h>
+
+#include <tinyCEP/ApplicationHolderController.h>
+#include <TFC_DelayCompensation/AH_DelayCompensation.h>
+
+using namespace LOFAR;
+
+int main (int argc, const char** argv) {
+  INIT_LOGGER("TFC_DelayCompensation");
+
+  // Check invocation syntax
+  try {
+    if (argc < 2) {
+      LOG_TRACE_FLOW("Main program not started by ACC");
+      // there are no commandline arguments, so we were not called by ACC
+
+      AH_DelayCompensation myAH;
+      myAH.setarg(argc, argv);
+      myAH.baseDefine();
+      cout << "defined" << endl;
+      Profiler::init();
+      myAH.basePrerun();
+      cout << "init done" << endl;
+      Profiler::activate();
+      cout << "run" << endl;
+      myAH.baseRun(1);
+      cout << "run complete" << endl;
+      myAH.baseDump();
+      myAH.baseQuit();
+      Profiler::deActivate();
+    } else {
+      LOG_TRACE_FLOW("Main program started by ACC");
+      // we were called by ACC so execute the ACCmain
+      AH_DelayCompensation myAH;
+      ApplicationHolderController myAHController(myAH);
+      myAHController.main(argc, argv);
+    }
+  } catch (Exception& ex) {
+    LOG_FATAL_STR("Caught exception: " << ex << endl);
+    LOG_FATAL_STR(argv[0] << " terminated by exception!");
+    exit(1);
+  } catch (...) {
+    LOG_FATAL_STR("Caught unknown exception, exitting");
+    exit (1);
+  }  
+  LOG_INFO_STR(argv[0] << " terminated normally");
+  return (0);
+}
