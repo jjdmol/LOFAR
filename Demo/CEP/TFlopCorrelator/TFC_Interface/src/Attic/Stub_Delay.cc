29781a97184d23b6a18b8a546b8acd9764ac3060
meijeren <meijeren@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-23 14:40:05 +0000

    %[ER: 269]% Added Stub_Delay

---
+++ Demo/CEP/TFlopCorrelator/TFC_Interface/src/Attic/Stub_Delay.cc
@@ -0,0 +1,85 @@
+//# Stub_Delay.cc: Stub for connection from 
+//#
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# $Id$
+//#
+////////////////////////////////////////////////////////////////////
+
+#include <TFC_Interface/Stub_Delay.h>
+#include <Transport/TH_Socket.h>
+#include <Transport/Connection.h>
+#include <ACC/ParameterSet.h>
+
+using namespace LOFAR;
+using namespace LOFAR::ACC;
+
+namespace LOFAR { 
+
+Stub_Delay::Stub_Delay (bool isInput)
+  : itsIsInput      (isInput),
+    itsTHs          (0),
+    itsConnections  (0)
+{
+  itsPS = new ACC::ParameterSet("TFlopCorrelator.cfg");
+  itsNRSP = itsPS->getInt("NRSP");
+  LOG_TRACE_FLOW_STR("Total number of RSPinputs in the Stub_Delay is " << itsNRSP);
+  ASSERTSTR(itsNRSP >= 0, "Number of RSPinputs in the Stub_Delay must be greater than 0");
+  
+  for (int i=0; i<itsNRSP; i++) {
+    itsTHs[i] = 0;
+    itsConnections[i] = 0;
+  }
+  
+}
+  
+Stub_Delay::~Stub_Delay()
+{
+  for (int i=0; i<itsNRSP; i++)
+  {
+    delete itsTHs[i];
+    delete itsConnections[i];
+  }
+  delete [] itsTHs;
+  delete [] itsConnections;
+}
+  
+void Stub_Delay ::connect (int RSP_nr,
+			   TinyDataManager& dm,
+			   int dhNr)
+{
+  DBGASSERTSTR(((RSP_nr >= 0) && (RSP_nr < itsNRSP)),
+	       "RSP_nr argument out of boundaries; " << RSP_nr << " / " << itsNRSP);
+
+  int port = itsPS->getInt("DelayConnection.RequestPort");
+  string service(formatString("%d", port));
+  
+  if (itsIsInput) // on the input side, start a client socket
+  {
+    DBGASSERTSTR(itsTHs[RSP_nr] == 0, "Stub output " << RSP_nr << 
+		 " has already been connected.");
+    // Create a client socket
+    itsTHs[RSP_nr] = new TH_Socket(itsPS->getString("DelayConnection.ServerHost"),
+				 service);
+    itsConnections[RSP_nr] = new Connection("fromInpSection", 0, 
+					  dm.getInHolder(dhNr), 
+					  itsTHs[RSP_nr], true);
+    dm.setInConnection(dhNr, itsConnections[RSP_nr]);
+  } 
+  else    // on DelayControl side, start client socket
+  {
+    DBGASSERTSTR(itsTHs[RSP_nr] == 0, "Stub input " << RSP_nr << 
+		 " has already been connected.");
+    // Create a server socket
+    itsTHs[RSP_nr] = new TH_Socket(service);
+    itsConnections[RSP_nr] = new Connection("toBG", dm.getOutHolder(dhNr), 
+					  0, itsTHs[RSP_nr], true);
+    dm.setOutConnection(dhNr, itsConnections[RSP_nr]);
+  }
+  
+};
+
+} //namespace
+
