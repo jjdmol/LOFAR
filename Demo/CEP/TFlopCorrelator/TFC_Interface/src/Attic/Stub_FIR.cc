31506005d9ffc9b91484b625fc2468d6d2950dcd
Chris Broekema <broekema@users.noreply.github.com>
2005-06-29 11:21:06 +0000

    %[ER: 269]% Rename Stub_SB to Stub_FIR

---
+++ Demo/CEP/TFlopCorrelator/TFC_Interface/src/Attic/Stub_FIR.cc
@@ -0,0 +1,83 @@
+//# Stub_FIR.cc: Stub for connection to DFTServer and DFTRequest
+//#
+//# Copyright (C) 2004
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# $Id$
+
+#include <TFC_Interface/Stub_FIR.h>
+#include <Transport/TH_Socket.h>
+#include <Transport/Connection.h>
+
+
+using namespace LOFAR;
+using namespace LOFAR::ACC;
+
+namespace LOFAR { 
+
+  Stub_FIR::Stub_FIR (bool stubOnServer)
+    : itsStubOnServer (stubOnServer),
+      itsTHs          (0),
+      itsConnections  (0)
+  {
+    itsPS = new ACC::ParameterSet("TFlopCorrelator.cfg");
+    itsNFIRF     = itsPS->getInt("NFIRF");  // number of SubBand filters in the application
+    ASSERTSTR(itsNFIRF >= 0, "Number of subband filters must be greater than 0");
+    itsTHs = new TH_Socket*[itsNFIRF];
+    itsConnections = new Connection*[itsNFIRF];
+
+    for (int i=0; i<itsNFIRF; i++) {
+      itsTHs[i] = 0;
+      itsConnections[i] = 0;
+    }
+
+  }
+
+  Stub_FIR::~Stub_FIR()
+  {
+    for (int i=0; i<itsNFIRF; i++)
+    {
+      delete itsTHs[i];
+      delete itsConnections[i];
+    }
+    delete [] itsTHs;
+    delete [] itsConnections;
+  }
+
+  void Stub_FIR::connect (int FIRF_nr,
+			 TinyDataManager& dm,
+			 int dhNr)
+  {
+    DBGASSERTSTR(FIRF_nr <= itsNFIRF, "Subband filter number too large");
+    const ParameterSet myPS("TFlopCorrelator.cfg");
+    int port = myPS.getInt("FIRConnection.RequestPort");
+    string service(formatString("%d", port));
+    if (itsStubOnServer)    // On the cluster side, so start a server socket
+    {
+      DBGASSERTSTR(itsTHs[FIRF_nr] == 0, "Stub input " << FIRF_nr << 
+		" has already been connected.");
+      // Create a server socket
+      itsTHs[FIRF_nr] = new TH_Socket(service);
+      itsConnections[FIRF_nr] = new Connection("toBG", dm.getOutHolder(dhNr), 
+					      0, itsTHs[FIRF_nr], true);
+      dm.setOutConnection(dhNr, itsConnections[FIRF_nr]);
+    }
+    else             // On BG side , so start a client socket
+    {
+      DBGASSERTSTR(itsTHs[FIRF_nr] == 0, "Stub output " << FIRF_nr << 
+		" has already been connected.");
+      // Create a client socket
+      itsTHs[FIRF_nr] = new TH_Socket(myPS.getString("FIRConnection.ServerHost"),
+				     service);
+      itsConnections[FIRF_nr] = new Connection("fromInpSection", 0, dm.getInHolder(dhNr), 
+					      itsTHs[FIRF_nr], true);
+      dm.setInConnection(dhNr, itsConnections[FIRF_nr]);
+    }
+
+  };
+
+  //todo: add connections for pre-correlation correction DH_?? 
+
+} //namespace
+
