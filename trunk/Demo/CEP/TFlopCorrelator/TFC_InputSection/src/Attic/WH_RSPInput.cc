7c3d4f52d21d2e9036ace51b3aa7797a8036c271
lang <lang@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-06-29 12:33:26 +0000

    %[ER:269]% WH_RSP obsolete. RSP issues all handled by WH_RSPInput now

--- trunk/Demo/CEP/TFlopCorrelator/TFC_InputSection/src/Attic/WH_RSPInput.cc
+++ trunk/Demo/CEP/TFlopCorrelator/TFC_InputSection/src/Attic/WH_RSPInput.cc
@@ -26,12 +26,12 @@
 // General includes
 #include <Common/LofarLogger.h>
 
-#include <CEPFrame/DataManager.h>
-
 // Application specific includes
 #include <WH_RSPInput.h>
 #include <TFC_Interface/DH_Sync.h>
 #include <TFC_Interface/DH_RSP.h>
+#include <TFC_Interface/DH_RSPSync.h>
+      
 
 using namespace LOFAR;
 
@@ -41,8 +41,10 @@ void* WriteToBufferThread(void* arguments)
   thread_args* args = (thread_args*)arguments;
   BufferController<dataType>* databuffer = args->databuffer;
   TH_Ethernet* connection = args->connection;
+  TinyDataManager* datamanager = args->datamanager;
   int framesize = args->framesize;
   int packetsinframe = args->packetsinframe;
+  bool syncmaster = args->syncmaster;
 
   char recvframe[9000];
   int seqid, blockid, itemid, offset;
@@ -68,42 +70,79 @@ void* WriteToBufferThread(void* arguments)
     blockid = ((int*)&recvframe[10])[0];
     actualstamp.setStamp(seqid ,blockid);
 
-    if (firstloop) {
-      // set nextstamp equal to actualstamp
-      nextstamp.setStamp(seqid, blockid);
-      firstloop = false;
-    }
+    if (!syncmaster) {
+    // we are a slave so read the syncstamp
+      if (firstloop) {
+        // this is the first time
+        // we need to force a read, because this inHolder has a lower data rate
+        // if the rate difference is 1000, the workholder will read for
+        // the first time in the 1000th runstep.
+        datamanager->getInHolder(1);
+        datamanager->readyWithInHolder(1);
+        firstloop = false;
+      }      
+      DH_RSPSync* dhp = (DH_RSPSync*)datamanager.getInHolder(1);
+      nextstamp = dhp->getSyncStamp(); 
+      // we need to increment the stamp because it is written only once per second or so
+      dhp->incrementStamp(itsNpackets);
+    } // (!itsSyncMaster)
+    else {
+      // we are the master, so increase the nextValue and send it to the slaves
+      if (firstloop) {
+        // set nextstamp equal to actualstamp
+        nextstamp.setStamp(seqid, blockid);
+        // build in a delay to let the slaves catch up
+        nextstamp += itsNPackets * 1500;
+        
+        // send the startstamp to the slaves
+        for (int i = 1; i < itsNRSPOutputs; i++) {
+	  ((DH_RSPSync*)datamanager->getOutHolder(i))->setSyncStamp(nextstamp);
+          // force a write 
+          datamanager->readyWithOutHolder(i);
+        }
+	firstloop = false;      
+      } // end (firstloop) 
+      else {
+        // increase the syncstamp
+        nextstamp += itsNpackets;
+        // send the syncstamp to the slaves
+        for (int i = 1; i < itsNRSPOutputs; i++) {
+	  ((DH_RSPSync*)datamanager().getOutHolder(i))->setSyncStamp(nextstamp);
+        }
+      }
+    } //end (itsIsSyncMaster)
+
+
     
-   // to do: bufferelements of EPApackets and not ethernet frames !!
+    // to do: bufferelements of EPApackets and not ethernet frames !!
     
     if (actualstamp < nextstamp) {
       // old packet received 
       // if exists, overwrite its previous created dummy
       
-      // read oldest item in cyclic buffer
-      dataptr = (dataType*)databuffer->getFirstReadPtr(itemid);
-      
-      // subtract timestamps to find offset
-      offset = actualstamp - dataptr->timestamp;
-      
-      // determine itemid to be written
-      itemid += offset;
-      
-      // release readlock
-      databuffer->readyReading();
+      // read oldest item in cyclic buffer if buffer is not empty
+      if (databuffer->getCount() > 0) {
+        
+        dataptr = (dataType*)databuffer->getFirstReadPtr(itemid);
+        // subtract timestamps to find offset
+        offset = actualstamp - dataptr->timestamp;
+        // determine itemid to be written
+        itemid += offset;
+        // release readlock
+        databuffer->readyReading();
    
-      // overwrite data and invalid flag
-      dataptr = (dataType*)databuffer->getUserWritePtr(itemid);
-      if (dataptr != 0) {
-        memcpy(dataptr->packet, recvframe, framesize);
-        dataptr->invalid = 0;
-      }      
-     
-      // release writelocks
-      databuffer->readyWriting();
+        // overwrite data and invalid flag
+        dataptr = (dataType*)databuffer->getUserWritePtr(itemid);
+        if (dataptr != 0) {
+          memcpy(dataptr->packet, recvframe, framesize);
+          dataptr->invalid = 0;
+        } 
+        // release writelocks
+        databuffer->readyWriting();
       
-      // read new frame in next loop
-      readnew = true;   
+        // read new frame in next loop
+        readnew = true;
+      }   
     }
     else if (nextstamp + (packetsinframe - 1) < actualstamp) {
       // missed a packet so set invalid flag and missing timestamp
@@ -138,13 +177,23 @@ WH_RSPInput::WH_RSPInput(const string& name,
                          const ACC::ParameterSet pset,
                          const string device,
                          const string srcMAC,
-                         const string destMAC)
-  : WorkHolder (1, 1, name, "WH_RSPInput"),
+                         const string destMAC
+                         const bool isSyncMaster)
+  : WorkHolder ((isSyncMaster ? 1 : 2), 
+                1 + (isSyncMaster ? pset.getInt("NoWH_RSP")-1 : 0), 
+                name, 
+                "WH_RSPInput"),
     itsPset (pset),
     itsDevice(device),
     itsSrcMAC(srcMAC),
-    itsDestMAC(destMAC)
+    itsDestMAC(destMAC),
+    itsSyncMaster(isSyncMaster)
 {
+  char str[32];
+  
+  // total amount of RSP-board interfaces
+  itsNRSPOutputs = pset.getInt("NoWH_RSP");
+
   // number of EPA packets per RSP frame
   itsNpackets = pset.getInt("NoPacketsInFrame");
  
@@ -162,13 +211,25 @@ WH_RSPInput::WH_RSPInput(const string& name,
   itsInputConnection = new TH_Ethernet(itsDevice, itsSrcMAC, itsDestMAC, 0x000 );
 
   // use  cyclic buffers to hold the rsp data and valid/invalid flag
-  itsDataBuffer = new BufferController<dataType>(1000); 
-  
-   // create incoming dataholder holding the delay information 
+  itsDataBuffer = new BufferController<dataType>(1000);
+ 
+  // create incoming dataholder holding the delay information 
   getDataManager().addInDataHolder(0, new DH_Sync("DH_Sync"));
 
   // create outgoing dataholder holding the delay controlled RSP data
   getDataManager().addOutDataHolder(0, new DH_RSP("DH_RSP_out", pset));
+
+  // create dataholders for RSPInput synchronization
+  if (itsIsSyncMaster) {
+    // if we are the sync master we need (NoWH_RSP-1) extra outputs
+    for (int i = 1; i < itsNRSPOutputs; i++) {
+      snprintf(str, 32, "DH_RSPInputSync_out_%d", i);
+      getDataManager().addOutDataHolder(i, new DH_RSPSync(str));
+    }
+  } else {
+    // if we are a sync slave we need 1 extra input
+    getDataManager().addInDataHolder(1, new DH_RSPSync("DH_RSPSync_in"));
+  }
  
 }
 
@@ -203,6 +264,8 @@ void WH_RSPInput::preprocess()
   writerinfo.connection = itsInputConnection;
   writerinfo.framesize = itsSzRSPframe;
   writerinfo.packetsinframe = itsNpackets;
+  writerinfo.datamanager = getDataManager();
+  writerinfo.syncmaster = itsSyncMaster;
   writerinfo.stopthread = false;
   
   if (pthread_create(&writerthread, NULL, WriteToBufferThread, &writerinfo) < 0)
@@ -214,20 +277,20 @@ void WH_RSPInput::preprocess()
 
 void WH_RSPInput::process() 
 { 
+   
+
   DH_Sync* inDHp;
   DH_RSP* outDHp;
   dataType* dataptr;
   timestamp_t syncstamp;
   int seqid, blockid;
 
-  // get delay information
+  // get delay offset
   inDHp = (DH_Sync*)getDataManager().getInHolder(0);
   inDHp->getNextMainBeat(seqid, blockid);
   syncstamp.setStamp(seqid, blockid);
   
-  // add delay control code here
- 
-  // get data from cycclic buffer
+  // get data from cyclic buffer
   dataptr = itsDataBuffer->getBufferReadPtr();
 
   // write flag to outgoing dataholder
