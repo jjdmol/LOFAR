760d01b502d66b4d11a72a88fc4e71104bfa6e37
blaakmeer <blaakmeer@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-07-06 12:07:14 +0000

    %[ER: 291]% First implementation of ArrayReceptorGroup

---
+++ MAC/APL/_VIC/ArrayReceptorGroup/include/ArrayReceptorGroup/ArrayReceptorGroupFactory.h
@@ -0,0 +1,65 @@
+//#  ArrayReceptorGroupFactory.h: factory class for ArrayReceptorGroups.
+//#
+//#  Copyright (C) 2002-2005
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef ArrayReceptorGroupFactory_H
+#define ArrayReceptorGroupFactory_H
+
+//# Includes
+#include <APLCommon/LogicalDeviceFactory.h>
+
+//# local includes
+#include "ArrayReceptorGroup.h"
+//# Common Includes
+
+// forward declaration
+
+namespace LOFAR
+{
+  
+namespace AAR
+{
+  class ArrayReceptorGroupFactory : public APLCommon::LogicalDeviceFactory
+  {
+    public:
+
+      ArrayReceptorGroupFactory() {}; 
+      virtual ~ArrayReceptorGroupFactory() {};
+      
+      virtual boost::shared_ptr<APLCommon::LogicalDevice> createLogicalDevice(const string& taskName, 
+                                                                              const string& parameterFile,
+                                                                              GCF::TM::GCFTask* pStartDaemon)
+      {
+        return boost::shared_ptr<APLCommon::LogicalDevice>(new ArrayReceptorGroup(taskName, parameterFile, pStartDaemon));
+      };
+
+    protected:
+      // protected copy constructor
+      ArrayReceptorGroupFactory(const ArrayReceptorGroupFactory&);
+      // protected assignment operator
+      ArrayReceptorGroupFactory& operator=(const ArrayReceptorGroupFactory&);
+
+    private:
+    
+  };
+};//AVT
+};//LOFAR
+#endif
