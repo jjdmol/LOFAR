6143a3e8e8dcf39d05a9b3cf31f3c75024a5427a
meijeren <meijeren@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-07-14 09:09:10 +0000

    %[ER: 269]% Fixed bug: added blobfield version in DH_RSP::init

--- Demo/CEP/TFlopCorrelator/TFC_Interface/src/DH_RSP.cc
+++ Demo/CEP/TFlopCorrelator/TFC_Interface/src/DH_RSP.cc
@@ -56,7 +56,7 @@ void DH_RSP::init()
   // Add the fields to the data definition.
   addField ("Buffer", BlobField<BufferType>(1,itsBufSize));
   addField ("Flag", BlobField<int>(1));
-  addField ("SyncedStamp", BlobField<char>(sizeof(timestamp_t)));
+  addField ("SyncedStamp", BlobField<char>(1, sizeof(timestamp_t)));
   // Create the data blob
   createDataBlock();
 
