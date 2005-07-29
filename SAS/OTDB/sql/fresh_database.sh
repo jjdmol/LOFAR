d30043dcb44bad2845e10b1018db687e511ec281
overeem <overeem@astron.nl>
2005-07-29 08:19:45 +0000

    BugID: 81 Weekly update.

--- SAS/OTDB/sql/fresh_database.sh
+++ SAS/OTDB/sql/fresh_database.sh
@@ -7,6 +7,6 @@ createdb -h dop50 -U postgres otdbtest
 echo "creating new tables"
 psql -f create_OTDB.sql -h dop50 -U postgres otdbtest 2>&1 | grep ERROR | grep -v "does not exist"
 
-echo "adding testset"
-psql -f fill_VIC_components.sql -h dop50 -U postgres otdbtest 2>&1 | grep ERROR
-psql -f fill_VIC_template.sql -h dop50 -U postgres otdbtest 2>&1 | grep ERROR
+#echo "adding testset"
+#psql -f fill_VIC_components.sql -h dop50 -U postgres otdbtest 2>&1 | grep ERROR
+#psql -f fill_VIC_template.sql -h dop50 -U postgres otdbtest 2>&1 | grep ERROR
