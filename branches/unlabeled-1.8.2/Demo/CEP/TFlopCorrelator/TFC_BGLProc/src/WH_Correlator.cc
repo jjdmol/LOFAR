9e6ab4c5ecf791f9e4f6807e48e9374932e91da3
schaaf <schaaf@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-07-31 20:26:01 +0000

    BugID: 226
    Modified input and output loops and addressing in WH_Correlator

--- branches/unlabeled-1.8.2/Demo/CEP/TFlopCorrelator/TFC_BGLProc/src/WH_Correlator.cc
+++ branches/unlabeled-1.8.2/Demo/CEP/TFlopCorrelator/TFC_BGLProc/src/WH_Correlator.cc
@@ -18,8 +18,6 @@
 #endif
 
 // Application specific includes
-#include <TFC_Interface/DH_Vis.h>
-#include <TFC_Interface/DH_CorrCube.h>
 #include <WH_Correlator.h>
 
 #ifdef HAVE_BGL
@@ -36,14 +34,19 @@ using namespace LOFAR;
 WH_Correlator::WH_Correlator(const string& name) : 
   WorkHolder( 1, 1, name, "WH_Correlator")
 {
-
   ACC::APS::ParameterSet  myPS("TFlopCorrelator.cfg");
-  itsNelements = myPS.getInt32("WH_Corr.stations");
-  itsNsamples  = myPS.getInt32("WH_Corr.samples");
-  itsNchannels = myPS.getInt32("WH_Corr.channels"); 
-  itsNtargets = 0; // not used?
+  itsNelements      = myPS.getInt32("NRSP");                                 //myPS.getInt32("WH_Corr.stations");
+  itsNsamples       = myPS.getInt32("WH_Corr.samples");
+  itsNpolarisations = myPS.getInt32("polarisations");
 
-  getDataManager().addInDataHolder(0, new DH_CorrCube("in", 1));
+  itsNinputs = itsNpolarisations*itsNelements;
+
+//   itsNchannels = myPS.getInt32("WH_Corr.channels"); 
+//   itsNtargets = 0; // not used?
+
+
+  getDataManager().addInDataHolder(0, new DH_FIR("in", 1, myPS));
+  //  getDataManager().addInDataHolder(0, new DH_CorrCube("in", 1));
   getDataManager().addOutDataHolder(0, new DH_Vis("out", 1, myPS));
 
   t_start.tv_sec = 0;
@@ -69,8 +72,11 @@ WH_Correlator* WH_Correlator::make (const string& name) {
 }
 
 void WH_Correlator::preprocess() {
+  ASSERTSTR(itsNinputs == ELEMENTS, "configuration from file does not match defined configuration");
+  ASSERTSTR(itsNsamples == SAMPLES, "configuration from file does not match defined configuration");
 
-  ASSERTSTR(static_cast<DH_CorrCube*>(getDataManager().getInHolder(0))->getBufSize() == ELEMENTS*SAMPLES, "InHolder size not equal to defined size");
+  ASSERTSTR(static_cast<DH_FIR*>(getDataManager().getInHolder(0))->getBufferSize() == ELEMENTS*SAMPLES, "InHolder size not equal to defined size");
+//   ASSERTSTR(static_cast<DH_CorrCube*>(getDataManager().getInHolder(0))->getBufSize() == ELEMENTS*SAMPLES, "InHolder size not equal to defined size");
   ASSERTSTR(static_cast<DH_Vis*>(getDataManager().getOutHolder(0))->getBufSize() == ELEMENTS*ELEMENTS, "OutHolder size not equal to defined size");
 
   // prevent stupid mistakes in the future by assuming we can easily change the unroll factor
@@ -79,9 +85,10 @@ void WH_Correlator::preprocess() {
 
 void WH_Correlator::process() {
   double starttime, stoptime, cmults;
-  const short ar_block = itsNelements / UNROLL_FACTOR;
+  const short ar_block = itsNinputs / UNROLL_FACTOR;
 
-  DH_CorrCube *inDH  = (DH_CorrCube*)(getDataManager().getInHolder(0));
+  DH_FIR *inDH  = (DH_FIR*)(getDataManager().getInHolder(0));
+//   DH_CorrCube *inDH  = (DH_CorrCube*)(getDataManager().getInHolder(0));
   DH_Vis      *outDH = (DH_Vis*)(getDataManager().getOutHolder(0));
 
   in_ptr = &(in_buffer[0][0]);
@@ -92,7 +99,8 @@ void WH_Correlator::process() {
 
   // fill the input buffer
   // could this be done without a memcpy?
-  memcpy(&in_buffer, inDH->getBuffer(), inDH->getBufSize()*sizeof(DH_CorrCube::BufferType));
+  memcpy(&in_buffer, inDH->getBuffer(), inDH->getBufferSize()*sizeof(DH_FIR::BufferType));
+//   memcpy(&in_buffer, inDH->getBuffer(), inDH->getBufSize()*sizeof(DH_CorrCube::BufferType));
 
 #ifdef DO_TIMING
   starttime = timer();
@@ -103,62 +111,85 @@ void WH_Correlator::process() {
   __alignx(8 , in_buffer);
 #endif
 
-  // The actual correlator loop
-  // Note that we don't make special considerations for polarisations X or Y
-  // We consider these completely seperate elements, thus removing an 
-  // extra dimension in the input and output data structure
- 
-  // Also note that this algorithm calculates slightly more correlation products than 
-  // strictly necessary. For now we assume that the extra overhead introduced by 
-  // exactly calculating half the matrix is more than what we calculate extra now.
-  // This may be optimized in a later version.
-
-  for (int i = 0; i < itsNsamples; i++) {
-    for (int y = 0; y < itsNelements; y+=4) { 
-
-      // prefetch a block of values to register
-      __complex__ double reg_x_0 = static_cast<__complex__ double>(in_buffer[0][i]);
-      __complex__ double reg_x_1 = static_cast<__complex__ double>(in_buffer[1][i]);
-      __complex__ double reg_x_2 = static_cast<__complex__ double>(in_buffer[2][i]);
-      __complex__ double reg_x_3 = static_cast<__complex__ double>(in_buffer[3][i]);
-
-      __complex__ double reg_y_0 = static_cast<__complex__ double>(in_buffer[y][i]);
-      __complex__ double reg_y_1 = static_cast<__complex__ double>(in_buffer[y+1][i]);
-      __complex__ double reg_y_2 = static_cast<__complex__ double>(in_buffer[y+2][i]);
-      __complex__ double reg_y_3 = static_cast<__complex__ double>(in_buffer[y+3][i]);
-
-      for (int x = 0; x <= y; x+=4) {
-
-	out_buffer[x+0][y+0] += reg_x_0 * ~reg_y_0;
-	out_buffer[x+0][y+1] += reg_x_0 * ~reg_y_1;
-	out_buffer[x+0][y+2] += reg_x_0 * ~reg_y_2;
-	out_buffer[x+0][y+3] += reg_x_0 * ~reg_y_3;
-
-	reg_x_0 = in_buffer[x+4][i];
-
-	out_buffer[x+1][y+0] += reg_x_1 * ~reg_y_0;
-	out_buffer[x+1][y+1] += reg_x_1 * ~reg_y_1;
-	out_buffer[x+1][y+2] += reg_x_1 * ~reg_y_2;
-	out_buffer[x+1][y+3] += reg_x_1 * ~reg_y_3;
-	
-	reg_x_1 = in_buffer[x+5][i];
-
-	out_buffer[x+2][y+0] += reg_x_2 * ~reg_y_0;
-	out_buffer[x+2][y+1] += reg_x_2 * ~reg_y_1;
-	out_buffer[x+2][y+2] += reg_x_2 * ~reg_y_2;
-	out_buffer[x+2][y+3] += reg_x_2 * ~reg_y_3;
-
-	reg_x_2 = in_buffer[x+6][i];
-
-	out_buffer[x+3][y+0] += reg_x_3 * ~reg_y_0;
-	out_buffer[x+3][y+1] += reg_x_3 * ~reg_y_1;
-	out_buffer[x+3][y+2] += reg_x_3 * ~reg_y_2;
-	out_buffer[x+3][y+3] += reg_x_3 * ~reg_y_3;
-
-	reg_x_3 = in_buffer[x+7][i];
+  __complex__ double reg_x_0, reg_x_1, reg_x_2, reg_x_3;
+  __complex__ double reg_y_0, reg_y_1, reg_y_2, reg_y_3;
+  
+  int A;
+  for (int time=0; time<1000; time++) {
+    //loop over the Y dimension
+    for (int B=0; B< NSTATIONS; B+=2) {
+      // addressing:   getBufferElement(    channel, station, time, pol)
+      reg_B0_X = inDH->getBufferElement(int channel, B,       time, 0);     
+      reg_B0_Y = inDH->getBufferElement(int channel, B,       time, 1);     
+      reg_B1_X = inDH->getBufferElement(int channel, B+1,     time, 0);     
+      reg_B1_Y = inDH->getBufferElement(int channel, B+1,     time, 1);     
+      // now loop over the A dimension
+      for (A=0; A<NSTATIONS-2-B; A+=2) {
+	// now correlate stations A,A+1,B,B+1 in both polarisations
+	//cout << A   << " - " << B   << endl;
+	//cout << A   << " - " << B+1 << endl;
+	//cout << A+1 << " - " << B   << endl;
+	//cout << A+1 << " - " << B+1 << endl;
+ 	// load inputs into registers
+	reg_A0_X = inDH->getBufferElement(int channel, A  , time, 0);     
+	reg_A0_Y = inDH->getBufferElement(int channel, A  , time, 1);     
+	reg_A1_X = inDH->getBufferElement(int channel, A+1, time, 0);     
+	reg_A1_Y = inDH->getBufferElement(int channel, A+1, time, 1);      
+	// calculate all correlations; 
+	// todo: prefetch new A dimesnsions on the way
+	// addressing:     (station, station, PolComb)
+	*(outDH->getElement(A      , B      , 0)) += reg_A0_X * ~reg_B0_X;
+	*(outDH->getElement(A      , B      , 1)) += reg_A0_X * ~reg_B0_Y;
+	*(outDH->getElement(A      , B+1    , 0)) += reg_A0_X * ~reg_B1_X;
+	*(outDH->getElement(A      , B+1    , 1)) += reg_A0_X * ~reg_B1_Y;
+	// done with A0_X
+	*(outDH->getElement(A      , B      , 2)) += reg_A0_Y * ~reg_B0_X;
+	*(outDH->getElement(A      , B      , 3)) += reg_A0_Y * ~reg_B0_Y;
+	*(outDH->getElement(A      , B+1    , 2)) += reg_A0_Y * ~reg_B1_X;
+	*(outDH->getElement(A      , B+1    , 3)) += reg_A0_Y * ~reg_B1_Y;
+	// done with A0_Y
+	*(outDH->getElement(A      , B      , 0)) += reg_A1_X * ~reg_B0_X;
+	*(outDH->getElement(A      , B      , 1)) += reg_A1_X * ~reg_B0_Y;
+	*(outDH->getElement(A      , B+1    , 0)) += reg_A1_X * ~reg_B1_X;
+	*(outDH->getElement(A      , B+1    , 1)) += reg_A1_X * ~reg_B1_Y;
+	// done with A1_X
+	*(outDH->getElement(A      , B      , 2)) += reg_A1_Y * ~reg_B0_X;
+	*(outDH->getElement(A      , B      , 3)) += reg_A1_Y * ~reg_B0_Y;
+	*(outDH->getElement(A      , B+1    , 2)) += reg_A1_Y * ~reg_B1_X;
+	*(outDH->getElement(A      , B+1    , 3)) += reg_A1_Y * ~reg_B1_Y;
+	// done with A1_Y
       }
     }
+    // done all sqaures
+    // now correlate the last triangle;
+    //cout << A   << " - " << B   << endl;
+    //cout << A   << " - " << B+1 << endl;
+    //cout << A+1 << " - " << B   << endl;
+    reg_A0_X = getBufferElement(int channel, A  , time, 0);     
+    reg_A0_Y = getBufferElement(int channel, A  , time, 1);     
+    reg_A1_X = getBufferElement(int channel, A+1, time, 0);     
+    reg_A1_Y = getBufferElement(int channel, A+1, time, 1);     
+    // calculate all correlations in the triangle; 
+    // todo: prefetch new A dimesnsions on the way
+    *(outDH->getElement(A      , B      , 0))  += reg_A0_X * ~reg_B0_X;
+    *(outDH->getElement(A      , B      , 1))  += reg_A0_X * ~reg_B0_Y;
+    *(outDH->getElement(A      , B+1    , 0))  += reg_A0_X * ~reg_B1_X;
+    *(outDH->getElement(A      , B+1    , 1))  += reg_A0_X * ~reg_B1_Y;
+    // done with A0_X
+    *(outDH->getElement(A      , B      , 2))  += reg_A0_Y * ~reg_B0_X;
+    *(outDH->getElement(A      , B      , 3))  += reg_A0_Y * ~reg_B0_Y;
+    *(outDH->getElement(A      , B+1    , 2))  += reg_A0_Y * ~reg_B1_X;
+    *(outDH->getElement(A      , B+1    , 3))  += reg_A0_Y * ~reg_B1_Y;
+    // done with A0_Y
+    *(outDH->getElement(A      , B      , 0))  += reg_A1_X * ~reg_B0_X;
+    *(outDH->getElement(A      , B      , 1))  += reg_A1_X * ~reg_B0_Y;
+    // done with A1_X
+    *(outDH->getElement(A      , B      , 2))  += reg_A1_Y * ~reg_B0_X;
+    *(outDH->getElement(A      , B      , 3))  += reg_A1_Y * ~reg_B0_Y;
+    // done with A1_Y 
   }
+
+ 
 
   // this is allowed since we're sure the outDH is equal in size to the 
   // out_buffer (the assert didn't fail in the preprocess method)
