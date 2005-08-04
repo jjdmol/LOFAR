5c76e5feba4f0328e8d44ade870426084529894a
Chris Broekema <broekema@users.noreply.github.com>
2005-08-04 10:29:23 +0000

    BugID: 199 Fix segfault by implementing the init() method instead of the prerun() method

--- Demo/CEP/TFlopCorrelator/TFC_BGLProc/src/AH_BGLProcessing.cc
+++ Demo/CEP/TFlopCorrelator/TFC_BGLProc/src/AH_BGLProcessing.cc
@@ -70,8 +70,6 @@ void AH_BGLProcessing::define(const LOFAR::KeyValueMap&) {
 
   ACC::APS::ParameterSet itsParamSet("TFlopCorrelator.cfg");
 
-//   int itsNFIRF  = itsParamSet.getInt32("NFIRF");  // number of FIR filters in the application
-  
   int lowestFreeNode = 0;
   
   LOG_TRACE_FLOW_STR("Create the top-level composite");
@@ -80,11 +78,11 @@ void AH_BGLProcessing::define(const LOFAR::KeyValueMap&) {
   // The processing section consists of the FIR filter
   // and correlators
 
-//   LOG_TRACE_FLOW_STR("Create input side interface stubs");
-//   itsInStub = new Stub_FIR(false, itsParamSet);
+  LOG_TRACE_FLOW_STR("Create input side interface stubs");
+  itsInStub = new Stub_FIR(false, itsParamSet);
 
-//   LOG_TRACE_FLOW_STR("Create output side interface stubs");
-//   itsOutStub = new Stub_Corr(false, itsParamSet);
+  LOG_TRACE_FLOW_STR("Create output side interface stubs");
+  itsOutStub = new Stub_Corr(false, itsParamSet);
 
 //   LOG_TRACE_FLOW_STR("Create the FIR filter  workholders");
   
@@ -98,54 +96,6 @@ void AH_BGLProcessing::define(const LOFAR::KeyValueMap&) {
   string itsInServer = itsParamSet.getString("InServer");
   string itsOutServer = itsParamSet.getString("OutServer");
 
-//   for (int pb = 0; pb < noProcBlock; pb++) {
-
-    // This is the basic define for the next version of the BGL processing pipe
-    // For now we only use a single block of correlators
-
-//     int corID = 0;
-//     vector<WH_Correlator*> Cors;
-//     for (int fil = 0; fil < noFiltsPerBlock; fil++) {
-//       // create WH_FIR and WH_FFT
-//       snprintf(WH_Name, 40, "FIRFilter_%d_of_%d_ofBlock_%d", fil, noFiltsPerBlock, pb);
-//       WH_FIR* FIRFNode = new WH_FIR(WH_Name, subband++);
-//       itsWHs.push_back(FIRFNode);
-//       itsWHs.back()->runOnNode(lowestFreeNode++);   
-
-//       snprintf(WH_Name, 40, "FFT_%d_of_%d_ofBlock_%d", fil, noFiltsPerBlock, pb);
-//       WH_FFT* FFTNode = new WH_FFT(WH_Name);
-//       itsWHs.push_back(FFTNode);
-//       itsWHs.back()->runOnNode(lowestFreeNode++);   
-
-//       // todo: connect to inputSection using Stub_FIR
-//       connectWHs(FIRFNode, 0, FFTNode, 0);
-      
-//       for (int cor = 0; cor < noCorsPerFilt; cor++, corID++) {
-// 	// create correlator nodes
-// 	snprintf(WH_Name, 40, "Correlator_%d_of_%d_ofBlock_%d", corID, noFiltsPerBlock*noCorsPerFilt, pb);
-// 	WH_Correlator* CorNode = new WH_Correlator(WH_Name);
-// 	itsWHs.push_back(CorNode);
-// 	itsWHs.back()->runOnNode(lowestFreeNode++);   
-// 	Cors.push_back(CorNode);
-// 	connectWHs(FFTNode, cor, CorNode, 0);
-//       }
-//     }
-
-//     // create collect node
-//     snprintf(WH_Name, 40, "Collect_ofBlock_%d", pb);
-// //     WH_Collect* ColNode = new WH_Collect(WH_Name);
-// //     itsWHs.push_back(ColNode);
-// //     itsWHs.back()->runOnNode(lowestFreeNode++);   
-// //     for (int cor = 0; cor <= corId; cor++) {
-// //       connectWHs(Cors[cor], 0, ColNode, cor);
-// //     }
-//     // todo: create a dummy (or more) so that the number of processes in this block
-//     // corresponds to the size of this block on BG/L (8 in CPM or 16 in VNM)
-    
-//     // todo: connect to storage section using the Stub_Corr
-//   }
-
-
   // define a block of correlators
 
   for (int cor = 0; cor < noCorsPerFilt; cor++) {
@@ -155,43 +105,42 @@ void AH_BGLProcessing::define(const LOFAR::KeyValueMap&) {
     itsWHs.push_back(CorNode);
     itsWHs.back()->runOnNode(lowestFreeNode++);
 
-    DataHolder* itsInDH = new DH_FIR("itsIn1", 0, itsParamSet);
-    DataHolder* itsOutDH = new DH_Vis("itsOut1", 0, itsParamSet);
+    itsInStub->connect(cor, itsWHs.back()->getDataManager(), 0);
+    itsOutStub->connect(cor, itsWHs.back()->getDataManager(), 0);
+
+//     DataHolder* itsInDH = new DH_FIR("itsIn1", 0, itsParamSet);
+//     DataHolder* itsOutDH = new DH_Vis("itsOut1", 0, itsParamSet);
         
-    string itsInService(formatString("%d", itsBasePort+2*cor));
-    string itsOutService(formatString("%d", itsBasePort+2*cor+1));
+//     string itsInService(formatString("%d", itsBasePort+2*cor));
+//     string itsOutService(formatString("%d", itsBasePort+2*cor+1));
 
 //     TransportHolder* itsInTH = new TH_Mem();
-    TransportHolder* itsInTH = new TH_Socket(itsInServer, itsInService);
-    itsTHs.push_back(itsInTH);
+//     TransportHolder* itsInTH = new TH_Socket(itsInServer, itsInService);
+//     itsTHs.push_back(itsInTH);
     
-    Connection* itsInConnection = new Connection("itsInCon",
-						 itsInDH,
-						 itsWHs.back()->getDataManager().getInHolder(0),
-						 itsTHs.back(),
-						 true); // connection is blocking
-    itsConnections.push_back(itsInConnection);
-    itsWHs.back()->getDataManager().setInConnection(0, itsConnections.back());
+//     Connection* itsInConnection = new Connection("itsInCon",
+// 						 0,
+// 						 itsWHs.back()->getDataManager().getInHolder(0),
+// 						 itsTHs.back(),
+// 						 true); // connection is blocking
+//     itsConnections.push_back(itsInConnection);
 
 //     TransportHolder* itsOutTH = new TH_Mem();
-    TransportHolder* itsOutTH = new TH_Socket(itsOutServer, itsOutService);
-    itsTHs.push_back(itsOutTH);
+//     TransportHolder* itsOutTH = new TH_Socket(itsOutServer, itsOutService);
+//     itsTHs.push_back(itsOutTH);
     
-    Connection* itsOutConnection = new Connection("itsOutCon",
-						  itsWHs.back()->getDataManager().getOutHolder(0),
-						  itsOutDH,
-						  itsTHs.back(), 
-						  true); // connection is blocking
-    itsConnections.push_back(itsOutConnection);
-    itsWHs.back()->getDataManager().setOutConnection(0, itsConnections.back());
+//     Connection* itsOutConnection = new Connection("itsOutCon",
+// 						  itsWHs.back()->getDataManager().getOutHolder(0),
+// 						  0,
+// 						  itsTHs.back(), 
+// 						  true); // connection is blocking
+//     itsConnections.push_back(itsOutConnection);
   }
   
   LOG_TRACE_FLOW_STR("Finished define()");
 }
 
-
-
-void AH_BGLProcessing::prerun() {
+void AH_BGLProcessing::init() {
   vector<WorkHolder*>::iterator it = itsWHs.begin();
   for (; it < itsWHs.end(); it++) {
     (*it)->basePreprocess();
@@ -210,12 +159,12 @@ void AH_BGLProcessing::run(int steps) {
   LOG_TRACE_FLOW_STR("Finished AH_BGLProcessing::run() "  );
 }
 
-void AH_BGLProcessing::postrun() {
-  vector<WorkHolder*>::iterator it = itsWHs.begin();
-  for (; it < itsWHs.end(); it++) {
-    (*it)->basePostprocess();
-  }
-}
+// void AH_BGLProcessing::postrun() {
+//   vector<WorkHolder*>::iterator it = itsWHs.begin();
+//   for (; it < itsWHs.end(); it++) {
+//     (*it)->basePostprocess();
+//   }
+// }
 
 
 void AH_BGLProcessing::dump() const {
