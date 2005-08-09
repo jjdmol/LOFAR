c3225be885d7975f8a0ca60a008a4e2c1570933e
lang <lang@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-08-09 11:43:42 +0000

    BugID: 217 Test modified

--- Demo/CEP/TFlopCorrelator/TFC_InputSection/test/CyclicBufferTest.cc
+++ Demo/CEP/TFlopCorrelator/TFC_InputSection/test/CyclicBufferTest.cc
@@ -34,7 +34,7 @@ using namespace LOFAR;
 
 typedef struct
 {
-  BufferController* bufcontrol;
+  BufferController* bc;
 } thread_args;
 
 
@@ -43,19 +43,13 @@ void* produce(void* argument)
   cout << "producer thread started" << endl;
 
   thread_args* arg = (thread_args*)argument;
-  
-  int blocksize = 1;
+  BufferController* bc = arg->bc;
 
-  timestamp_t ts[blocksize];
-  for (int b=0; b< blocksize;b++) {
-    ts[b].setStamp(0,b);
-  }
-  for (int i=0; i< 1000; i++) {
-    
-    arg->bufcontrol->writeElements(&ts,ts[0], blocksize, 1 );
-    for (int j=0; j< blocksize;j++) {
-      ts[j] += blocksize;
-    }
+  
+  timestamp_t ts(0,0);
+  for (int i=0; i< 120; i++) {
+    bc->writeElements(&ts,ts, 1, 0 );
+    ts++;
   }
 
 }
@@ -65,19 +59,19 @@ void* consume(void* argument)
   cout << "consumer thread started" << endl;
 
   thread_args* arg = (thread_args*)argument;
+  BufferController *bc = arg->bc;
 
+  int blocksize = 12;
   timestamp_t ts(0,0);
-  int invalidcount, seqid, blockid;
-  int blocksize = 5;
-  char data[8*blocksize];
-  for (int i=0; i< 200; i++) {
-    arg->bufcontrol->getElements(data,invalidcount, ts, blocksize);
+  timestamp_t data[blocksize];
+  int invalidcount;
+  
+  for (int i=0; i< 10; i++) {
+    bc->getElements(data,invalidcount, ts, blocksize);
     ts+=blocksize;
-
+    
     for (int t=0; t<blocksize; t++) { 
-      seqid = ((int*)&data[t*8])[0];
-      blockid = ((int*)&data[t*8+4])[0];
-      cout << seqid << "," << blockid << endl;  
+      cout << data[t].getSeqId() << "," << data[t].getBlockId() << endl;
     }
     cout << "invalid: " << invalidcount << endl;
     cout << endl;
@@ -86,32 +80,23 @@ void* consume(void* argument)
 }
 
 
+
 int main (int argc, const char** argv)
 { 
   try {
-
-    // if (argc < 4) {
-//       cout << "\nusage: CyclicBufferTest [buffersize] [readblocksize] [readblockoffset]\n" << endl;
-//       exit (1);
-//     }
-    
-//    // get buffersize
-//    int buffersize = atoi(argv[1]);
-//    int blocksize = atoi(argv[2]);
-//    int offset = atoi(argv[3]); 
-   
 
    // create Parameter Object
    ACC::APS::ParameterSet ps("TFlopCorrelator.cfg"); 
 
    // create BufferController Object
-   BufferController* BufControl = new BufferController(ps); 
+   //BufferController* BufControl = new BufferController(ps); 
+   BufferController BufControl(ps);
     
    // start producing data thread
    pthread_t  producer;
    thread_args producerdata;
     
-   producerdata.bufcontrol = BufControl;
+   producerdata.bc = &BufControl;
 
    if (pthread_create(&producer, NULL, produce, &producerdata) < 0)
    {
@@ -123,7 +108,7 @@ int main (int argc, const char** argv)
    pthread_t consumer;
    thread_args consumerdata;
 
-   consumerdata.bufcontrol = BufControl;
+   consumerdata.bc = &BufControl;
 
    if (pthread_create(&consumer, NULL, consume, &consumerdata) < 0)
    {
@@ -133,8 +118,6 @@ int main (int argc, const char** argv)
   
    pthread_join(producer, NULL);
    pthread_join(consumer, NULL);
-
-   delete BufControl;
    
      
   } catch (std::exception& x) {
