93d3a0e750bba349eb0b1b2df42aaef201dee37d
lang <lang@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-08-26 14:58:28 +0000

    BugID: 217 BufferController redesigned

--- Demo/CEP/TFlopCorrelator/TFC_InputSection/src/BufferController.cc
+++ Demo/CEP/TFlopCorrelator/TFC_InputSection/src/BufferController.cc
@@ -23,227 +23,441 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <CEPFrame/Lock.h>
 #include <TFC_InputSection/BufferController.h>
 #include <Common/hexdump.h>
 
 namespace LOFAR {
 
-BufferController::BufferController()
+
+BufferIndex::BufferIndex(int maxidx) :
+  itsIndex(0),
+  itsMaxIndex(maxidx)
 {
-  // get cyclicbuffersize
-  itsCyclicBufferSize = 1000;
-  
-  // create cyclic buffer
-  itsMetadataBuf = new CyclicBuffer<MetadataType>(itsCyclicBufferSize);  
- 
-  // create subband data structure
-  itsSubbandData = new SubbandType[itsCyclicBufferSize];
 }
 
-BufferController::BufferController(ParameterSet &ps)
-  : itsPS (ps)
+void BufferIndex::checkIndex()
 {
-  // get cyclicbuffersize
-  itsCyclicBufferSize = ps.getInt32("Input.CyclicBufferSize");
+  if (itsIndex >= itsMaxIndex) {
+    itsIndex -= itsMaxIndex;
+  }
+  else if (itsIndex < 0) {
+    itsIndex += itsMaxIndex;
+  }
+}
+
+int BufferIndex::getIndex()
+{
+  return itsIndex;
+}
+
+void BufferIndex::operator+= (int increment)
+{
+  itsIndex += increment;
+  checkIndex();
+}
+
+void BufferIndex::operator++ (int)
+{
+  itsIndex++;
+  checkIndex();
+}
+
+int BufferIndex::operator+ (int increment)
+{
+  return itsIndex + increment;
+}
+
+void BufferIndex::operator-= (int decrement)
+{
+  itsIndex -= decrement;
+  checkIndex();
+}
+
+void BufferIndex::operator-- (int)
+{
+  itsIndex--;
+  checkIndex();
+}
+
+int BufferIndex::operator- (BufferIndex& other)
+{
+  int increment = itsIndex-other.itsIndex;  
+  if (increment < 0) {
+    return increment + itsMaxIndex;
+  }
+  return increment; 
+}
+
+bool BufferIndex::operator== (BufferIndex& other)
+{
+  return (itsIndex == other.itsIndex);
+}
+
+
+BufferController::BufferController(int buffersize, int nsubbands)
+  : itsBufferSize(buffersize),
+    itsNSubbands(nsubbands),
+    itsHead(buffersize),
+    itsTail(buffersize),
+    itsOldHead(buffersize),
+    itsOldTail(buffersize),
+    itsOverwritingAllowed(true)
+{
+  // create metadata buffer
+  itsMetadataBuffer = new MetadataType[itsBufferSize];
   
-  // create cyclic buffer
-  itsMetadataBuf = new CyclicBuffer<MetadataType>(itsCyclicBufferSize);
+  // create subbands buffer
+  for (int s=0; s<itsNSubbands; s++) {
+    itsSubbandBuffer.push_back(new SubbandType[itsBufferSize]);
+  }
   
-  // create subband data structure
-  itsSubbandData = new SubbandType[itsCyclicBufferSize];
-  
+  // init buffer lock and -triggers
+  pthread_mutex_init(&buffer_mutex, NULL);
+  pthread_cond_init (&data_available,  NULL);
+  pthread_cond_init (&space_available, NULL);
 }
 
 BufferController::~BufferController()
 {
-  delete [] itsSubbandData;
-  delete itsMetadataBuf;
+  vector<SubbandType*>::iterator sit = itsSubbandBuffer.begin();
+  for (; sit!=itsSubbandBuffer.end(); sit++) {
+    delete *sit;
+  }
+  itsSubbandBuffer.clear();
+
+  delete itsMetadataBuffer;
 }
  
-timestamp_t BufferController::getFirstStamp()
+timestamp_t BufferController::getOldestStamp()
 {
-  MetadataType* mt;
-  timestamp_t stamp;
-  int mid;
-  
-  // get element
-  mt = itsMetadataBuf->getFirstReadPtr(mid);
-  stamp = mt->timestamp;
-  
-  // unlock element
-  itsMetadataBuf->ReadUnlockElements(mid, 1);
+  int bid;
 
-  return stamp;
+  pthread_mutex_lock(&buffer_mutex);
+
+  // wait until at least one element is available
+  while (getCount() <= 0) 
+  {
+    pthread_cond_wait(&data_available, &buffer_mutex);
+  }
+  // CONDITION: Count > 0
+  bid = itsTail.getIndex();
+
+  pthread_mutex_unlock(&buffer_mutex);
+
+  return itsMetadataBuffer[bid].timestamp;
 }
 
-bool BufferController::getElements(void* buf, int& invalidcount, timestamp_t startstamp, int nelements)
+timestamp_t BufferController::getNewestStamp()
 {
-  MetadataType* mt;
-  int mid, startid;
+  int bid;
 
-  // get timestamp of first element
-  mt = itsMetadataBuf->getFirstReadPtr(mid);
-  
-  // calculate offset
-  int offset = startstamp - mt->timestamp;
+  pthread_mutex_lock(&buffer_mutex);
 
-  // unlock element
-  itsMetadataBuf->ReadUnlockElement(mid); 
-  
-  DBGASSERTSTR(std::abs(offset) <= MAX_OFFSET , 
-	       "BufferController: timestamp offset invalid");
-
-  // determine start position in buffer
-  itsMetadataBuf->setOffset(offset, startid);
-  
-  // get metadata of requested block
-  invalidcount = 0;
-  for (int i=0; i<nelements; i++) {
-    mt = itsMetadataBuf->getAutoReadPtr(mid);
-    if (mt->invalid != 0) {
-      invalidcount++;
-    }
+  // wait until at least one element is available
+  while (getCount() <= 0) 
+  {
+    pthread_cond_wait(&data_available, &buffer_mutex);
   }
- 
-  // get subbanddata of requested block
-  if (startid + nelements  > itsCyclicBufferSize) {
-    // do copy in 2 blocks because end of subband data array will be crossed 
-    int n1 = itsCyclicBufferSize - startid;
-    memcpy(buf, &itsSubbandData[startid], n1*sizeof(SubbandType));
+  // CONDITION: Count > 0
 
-    int n2 = nelements - n1;
-    (char*)buf += n1*sizeof(SubbandType);
-    memcpy(buf, &itsSubbandData[0], n2*sizeof(SubbandType));
-  }
-  else {
-    // copy can be executed in one block
-    memcpy(buf, &itsSubbandData[startid], nelements*sizeof(SubbandType));
-  }
+  // this method is called before the first read,
+  // so it is allowed to move the tails;
+  bid = itsOldHead.getIndex() - 1;
 
-  //reading is done so unlock the elements in the cyclic buffer
-  itsMetadataBuf->ReadUnlockElements(startid, nelements);
-  
-  return true; 
+  pthread_mutex_unlock(&buffer_mutex);
+
+  return itsMetadataBuffer[bid].timestamp;
 }
 
-bool BufferController::writeElements(void* buf, timestamp_t rspstamp, int nelements, int invalid)
+int BufferController::setReadOffset(int offset)
 {
-  MetadataType* mt;
-  int mid, startid;
+  int bid;
 
-  // write the metadata for this block
-  for (int i=0; i<nelements; i++) {
-    mt = itsMetadataBuf->getAutoWritePtr(mid);
-    mt->invalid = invalid;
-    mt->timestamp = rspstamp;
-    // hold startid
-    if (i==0) {
-      startid = mid;
-    }
-    // increment rspstamp 
-    rspstamp++;
+  pthread_mutex_lock(&buffer_mutex);
+
+  // wait until enough data becomes available
+  while (getCount() - offset < MIN_COUNT)
+  {
+    pthread_cond_wait(&data_available, &buffer_mutex);
   }
   
-  // write the subbanddata
-  if (startid + nelements  > itsCyclicBufferSize) {
-    // do copy in 2 blocks because end of subband data array will be crossed 
-    int n1 = itsCyclicBufferSize - startid;
-    memcpy(&itsSubbandData[startid], buf, n1*sizeof(SubbandType));
+  // This method is called when there is no reader,
+  // so tail == oldTail
+  itsTail += offset;
+  itsOldTail = itsTail;
+  bid = itsTail.getIndex();
+   
+  pthread_mutex_unlock(&buffer_mutex);
 
-    int n2 = nelements - n1;
-    (char*)buf += n1*sizeof(SubbandType);
-    memcpy(&itsSubbandData[0], buf, n2*sizeof(SubbandType));
-  }
-  else {
-    // copy can be executed in one block
-    memcpy(&itsSubbandData[startid], buf, nelements*sizeof(SubbandType));
-  }
- 
-  // writing is done so unlock the elements in the cyclic buffer
-  itsMetadataBuf->WriteUnlockElements(startid, nelements);
-
-  return true;
+  return bid;
 }
 
-bool BufferController::rewriteElements(void* buf, timestamp_t startstamp, int nelements)
+int BufferController::setRewriteOffset(int offset)
 {
-  MetadataType* mt;
-  int mid, firstid, startid, offset;
 
-  // get timestamp of first element in cyclic buffer to find offset
-  mt = itsMetadataBuf->getFirstReadPtr(firstid);
-  offset = startstamp - mt->timestamp;
-  itsMetadataBuf->ReadUnlockElement(firstid);
+  pthread_mutex_lock(&buffer_mutex);
 
-  
-  // determine position in cyclic buffer where to start writing
-  if (offset < 0 || offset >= itsCyclicBufferSize) {
-    return false;
+  // check if there are enough elements in buffer
+  if (offset >= getCount()) {
+    return -1;
   } 
-  startid = firstid + offset;
-  if (startid >= itsCyclicBufferSize) {
-    startid -= itsCyclicBufferSize;
-  }
+  // CONDITION: offset + nelements > Count
+   
+  itsOldHead = itsTail + offset;
 
-  // rewrite the metadata for this block
-  mid = startid;
-  for (int i=0; i<nelements; i++) {
-    mt = itsMetadataBuf->getManualWritePtr(mid);
-    mt->invalid = 0;
-    mid++;
-    if (mid >= itsCyclicBufferSize) {
-      mid = 0;
-    }
-  }
-  
-  // rewrite the subbanddata for this block
-  if (startid + nelements  > itsCyclicBufferSize) {
-    // do copy in 2 blocks because end of subband data array will be crossed 
-    int n1 = itsCyclicBufferSize - startid;
-    memcpy(&itsSubbandData[startid], buf, n1*sizeof(SubbandType));
+  pthread_mutex_unlock(&buffer_mutex);
 
-    int n2 = nelements - n1;
-    (char*)buf += n1*sizeof(SubbandType);
-    memcpy(&itsSubbandData[0], buf, n2*sizeof(SubbandType));
-  }
-  else {
-    // copy can be executed in one block
-    memcpy(&itsSubbandData[startid], buf, nelements*sizeof(SubbandType));
-  }
-  
-  // writing is done so unlock the elements in the cyclic buffer
-  itsMetadataBuf->WriteUnlockElements(startid, nelements);
-
-
-  return true;
+  return itsOldHead.getIndex();
 }
 
-timestamp_t  BufferController::startBufferRead()
+int BufferController::getWritePtr()
 {
-  itsMetadataBuf->setOverwritingAllowed(false);
-  int mid;
+  int bid;
+
+  pthread_mutex_lock(&buffer_mutex);
+  
+  // wait until space becomes available
+  while ((itsHead - itsOldTail >= MAX_COUNT) && !itsOverwritingAllowed)
+  {
+    pthread_cond_wait(&space_available, &buffer_mutex);
+  }
+  
+  // CONDITION: Count < MAX_COUNT 
+  bid = itsHead.getIndex();
+  itsHead++;
+
+  // if allowed, overwrite previous written elements
+  if (itsHead == itsTail && itsOverwritingAllowed) {
+    // push tail forwards
+    itsTail++;
+    itsOldTail++;
+  }
+  
+  pthread_mutex_unlock(&buffer_mutex);
+
+  return bid;
+}
+
+int BufferController::getReadPtr()
+{
+  int bid;
+
+  pthread_mutex_lock(&buffer_mutex);
+
+  // overwriting not allowed when reading is started
+  itsOverwritingAllowed = false;
+  
+  // wait until enough elements are available
+  while (getCount() < MIN_COUNT) 
+  {
+    pthread_cond_wait(&data_available, &buffer_mutex);
+  }
+  
+  // CONDITION: Count >= MIN_COUNT 
+  bid = itsTail.getIndex();
+  itsTail++;
+  
+  pthread_mutex_unlock(&buffer_mutex);
+  
+  return bid;
+}
+
+void BufferController::releaseWriteBlock()
+{
+  pthread_mutex_lock(&buffer_mutex);
+  
+  // synchronize writepointers
+  itsOldHead = itsHead;
+
+  // signal that data has become available 
+  pthread_cond_broadcast(&data_available);
+
+  pthread_mutex_unlock(&buffer_mutex);
+ 
+}
+
+void BufferController::releaseReadBlock()
+{
+  pthread_mutex_lock(&buffer_mutex);
+  
+  // synchronize readpointers
+  itsOldTail = itsTail;
+  
+  // signal that space has become available
+  pthread_cond_broadcast(&space_available);
+
+  pthread_mutex_unlock(&buffer_mutex);
+}
+
+void BufferController::releaseRewriteBlock()
+{
+  pthread_mutex_lock(&buffer_mutex);
+  
+  // synchronize writepointers
+  itsOldHead = itsHead;
+
+  pthread_mutex_unlock(&buffer_mutex);
+ 
+}
+
+timestamp_t BufferController::startBufferRead()
+{
+  // start reading so overwriting not allowed anymore 
+  itsOverwritingAllowed = false;
+ 
+  // get oldest stamp
+  timestamp_t oldestStamp = getOldestStamp();
 
   // get newest stamp
-  timestamp_t newestStamp = itsMetadataBuf->getNewestReadPtr(mid)->timestamp;
-  itsMetadataBuf->ReadUnlockElements(mid, 1);
+  timestamp_t newestStamp = getNewestStamp();
 
-  // return newest stamp
-  cout<<" oldest stamp " << newestStamp << endl;
+  // set offset
+  setReadOffset(newestStamp - oldestStamp);
+
   return newestStamp;
 }
 
-void  BufferController::startBufferRead(timestamp_t stamp)
+void BufferController::startBufferRead(timestamp_t stamp)
 {
+  // start reading so overwriting not allowed anymore 
+  itsOverwritingAllowed = false;
   
-  itsMetadataBuf->setOverwritingAllowed(false);
-  int mid;
   // get oldest stamp
-  timestamp_t oldestStamp = itsMetadataBuf->getFirstReadPtr(mid)->timestamp;
-  cout<<" oldest stamp " << oldestStamp << " stamp " << stamp << endl;
-  itsMetadataBuf->ReadUnlockElements(mid, 1);
-  // set offset
-  itsMetadataBuf->setOffset(stamp - oldestStamp, mid);
+  timestamp_t oldestStamp = getOldestStamp();
 
+  // set offset
+  setReadOffset(stamp - oldestStamp);
+}
+
+void BufferController::getElements(vector<SubbandType*> buf, int& invalidcount, timestamp_t startstamp, int nelements)
+{
+  // get oldest timestamp
+  timestamp_t oldestStamp = getOldestStamp();
+  
+  // calculate offset
+  int offset = startstamp - oldestStamp;
+
+  // check offset
+  ASSERTSTR(std::abs(offset) <= MAX_OFFSET , 
+	       "BufferController: timestamp offset invalid");
+
+  // set offset, get startindex
+  int sid = setReadOffset(offset);
+
+  // get metadata for requested block
+  invalidcount = 0;
+  int bid;
+  for (int m=0; m<nelements; m++) {
+    bid = getReadPtr(); 
+    if (itsMetadataBuffer[bid].invalid != 0) {
+      invalidcount++;
+    }
+  }
+  
+  // get subbands for requested block
+  int n1, n2;
+  for (int s=0; s<itsNSubbands; s++) {
+    if (sid + nelements  > itsBufferSize) {
+      // do copy in 2 blocks because end of subband data array will be crossed 
+      n1 = itsBufferSize - sid;
+      memcpy(buf[s], &(itsSubbandBuffer[s][sid]), n1*sizeof(SubbandType));
+
+      n2 = nelements - n1;
+      memcpy(&(buf[s][n1]), &(itsSubbandBuffer[s][0]), n2*sizeof(SubbandType));
+    }
+    else {
+      // copy can be executed in one block
+      memcpy(buf[s], &(itsSubbandBuffer[s][sid]), nelements*sizeof(SubbandType));
+    }   
+  }
+ 
+  // reading is done, free block
+  releaseReadBlock();
+}
+
+void BufferController::writeElements(SubbandType* buf, timestamp_t rspstamp)
+{
+  // write the metadata
+  int bid;
+  bid = getWritePtr();
+  itsMetadataBuffer[bid].invalid = 0;
+  itsMetadataBuffer[bid].timestamp = rspstamp;
+  
+  
+  // write the subbanddata
+  for (int s=0; s<itsNSubbands; s++) {
+    memcpy(&(itsSubbandBuffer[s][bid]), &buf[s], sizeof(SubbandType));
+  }
+ 
+  // writing is done, free block
+  releaseWriteBlock();
+}
+
+void BufferController::writeDummy(SubbandType* dum, timestamp_t startstamp, int nelements)
+{
+  int bid, sid;
+
+  // write the metadata for this dummy block
+  for (int i=0; i<nelements; i++) {
+    bid = getWritePtr();
+    itsMetadataBuffer[bid].invalid = 0;
+    itsMetadataBuffer[bid].timestamp = startstamp;
+    if (i==0) {
+      sid = bid;
+    }
+    startstamp++;
+  }
+
+  // write the subbanddata
+  for (int s=0; s<itsNSubbands; s++) {
+    if (sid + nelements  > itsBufferSize) {
+      // do copy in 2 blocks because end of subband data array will be crossed 
+      int n1 = itsBufferSize - sid;
+      memcpy(&(itsSubbandBuffer[s][sid]), dum, n1*sizeof(SubbandType));
+
+      int n2 = nelements - n1;
+      memcpy(&(itsSubbandBuffer[s][0]), &dum[n1], n2*sizeof(SubbandType));
+    }
+    else {
+      // copy can be executed in one block
+      memcpy(&(itsSubbandBuffer[s][sid]), dum, nelements*sizeof(SubbandType));
+    }
+  }
+}
+
+bool BufferController::rewriteElements(SubbandType* buf, timestamp_t startstamp)
+{
+  // get oldest timestamp
+  timestamp_t oldestStamp = getOldestStamp();
+
+  // calculate offset
+  int offset = startstamp - oldestStamp;
+
+  // set offset, get startindexr
+  int bid = setRewriteOffset(offset);
+  if (bid == -1) {
+    return false;
+  }
+ 
+  // rewrite the metadata 
+  itsMetadataBuffer[bid].invalid = 0;
+  
+  // rewrite the subbanddata
+  for (int s=0; s<itsNSubbands; s++) {
+    memcpy(&(itsSubbandBuffer[s][bid]), &buf[s], sizeof(SubbandType));
+  }
+ 
+  // rewriting is done, free block
+  releaseRewriteBlock();
+
+  return true;
+}
+
+
+int BufferController::getCount()
+{
+  return itsOldHead - itsTail;
 }
 
 }
