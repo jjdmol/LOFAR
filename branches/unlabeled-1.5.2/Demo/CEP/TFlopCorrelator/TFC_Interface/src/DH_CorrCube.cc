42bc402623c5420f0862a41f1cceb78972e85308
broekema <broekema@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-09-12 07:06:36 +0000

    BugID: 315 Commit updated corrcube

--- branches/unlabeled-1.5.2/Demo/CEP/TFlopCorrelator/TFC_Interface/src/DH_CorrCube.cc
+++ branches/unlabeled-1.5.2/Demo/CEP/TFlopCorrelator/TFC_Interface/src/DH_CorrCube.cc
@@ -19,15 +19,17 @@ namespace LOFAR
   DH_CorrCube::DH_CorrCube (const string& name, short subband)
     : DataHolder     (name, "DH_CorrCube"),
       itsBuffer      (0),
-      itsSubBand     (subband),
-      itsNPol        (0)
+      itsSubBand     (subband)//,
+      //itsNPol        (0)
   {
+#if 0
     ACC::APS::ParameterSet  myPS("TFlopCorrelator.cfg");
     //ParameterCollection	myPC(myPS);
     itsNFChannels = myPS.getInt32("DH_CorrCube.freqs");
     itsNStations  = myPS.getInt32("DH_CorrCube.stations");
     itsNTimes     = myPS.getInt32("DH_CorrCube.times");
     itsNPol       = myPS.getInt32("Input.NPolarisations");
+#endif
   }
   
 DH_CorrCube::DH_CorrCube(const DH_CorrCube& that)
@@ -35,16 +37,16 @@ DH_CorrCube::DH_CorrCube(const DH_CorrCube& that)
     itsBuffer(0)
 {
     itsSubBand    = that.itsSubBand;
+#if 0
     itsNFChannels = that.itsNFChannels;
     itsNStations  = that.itsNStations; 
     itsNTimes     = that.itsNTimes;
     itsNPol       = that.itsNPol;
+#endif
 }
 
 DH_CorrCube::~DH_CorrCube()
 {
-  //  delete [] (char*)(itsDataPacket);
-  itsBuffer = 0;
 }
 
 DataHolder* DH_CorrCube::clone() const
@@ -55,14 +57,15 @@ DataHolder* DH_CorrCube::clone() const
 void DH_CorrCube::init()
 {
   // Determine the number of bytes needed for DataPacket and buffer.
-  itsBufSize = itsNStations * itsNFChannels * itsNTimes * itsNPol;
+  //itsBufSize = itsNStations * itsNFChannels * itsNTimes * itsNPol;
   
   addField ("Flag", BlobField<int>(1, 1));
-  addField ("Buffer", BlobField<BufferType>(1, itsBufSize));
+  addField ("Buffer", BlobField<fcomplex>(1, getBufSize()));
   
   createDataBlock();  // calls fillDataPointers
   // itsBuffer = getData<BufferType> ("Buffer");
-  memset(itsBuffer, 0, itsBufSize*sizeof(BufferType)); 
+  //memset(itsBuffer, 0, itsBufSize*sizeof(BufferType)); 
+  memset(itsBuffer, 0, sizeof(BufferType));
 }
 
 void DH_CorrCube::fillDataPointers() {
@@ -71,15 +74,22 @@ void DH_CorrCube::fillDataPointers() {
 
 void DH_CorrCube::print()
 {
+  unsigned long long chksum = 0;
+
   int lines = 0;
-  for (int channel = 0; channel < itsNFChannels; channel ++)
-    for (int station = 0; station < itsNStations; station ++)
-      for (int time = 0; time < itsNTimes; time ++)
-	for (int polarization = 0; polarization < itsNPol; polarization ++) {
-	  if (++ lines >= 100)
-	    std::cerr << channel << ' ' << station << ' ' << time << ' ' << polarization << ": " << *getBufferElement(channel, station, time, polarization) << '\n';
-	  if (lines == 110) return;
+  for (int station = 0; station < NR_STATIONS; station ++) {
+    for (int polarization = 0; polarization < NR_POLARIZATIONS; polarization ++)
+      for (int time = 0; time < NR_SAMPLES_PER_INTEGRATION; time ++) {
+	fcomplex chksum4 = makefcomplex(0,0);
+
+	for (int channel = 0; channel < NR_SUB_CHANNELS; channel ++) {
+	  chksum4 += (*itsBuffer)[station][polarization][time][channel];
 	}
+
+	chksum ^= * (unsigned long long *) &chksum4;
+      }
+  }
+  std::cerr << chksum << '\n';
 }
 
 }
