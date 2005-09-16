f1880584d36470ee8362b110862e2f280a26515b
blaakmeer <blaakmeer@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-09-16 12:46:31 +0000

    BugID: 169 Reverted change in Logical Device name. The idea was to let the nodeID be the name of an LD, but this does not work

--- MAC/APL/APLCommon/src/LogicalDevice.cc
+++ MAC/APL/APLCommon/src/LogicalDevice.cc
@@ -1184,7 +1184,8 @@ void LogicalDevice::_sendScheduleToClients()
         TLogicalDeviceTypes ldType = APLUtilities::convertLogicalDeviceType(ldTypeString);
         boost::shared_ptr<STARTDAEMONScheduleEvent> scheduleEvent(new STARTDAEMONScheduleEvent);
         scheduleEvent->logicalDeviceType = ldType;
-        scheduleEvent->taskName = getName() + string(".") + startDaemonKey;
+	//        scheduleEvent->taskName = getName() + string(".") + startDaemonKey;
+        scheduleEvent->taskName = startDaemonKey;
         scheduleEvent->fileName = parameterFileName;
 
         _sendEvent(scheduleEvent,*startDaemonPort);
