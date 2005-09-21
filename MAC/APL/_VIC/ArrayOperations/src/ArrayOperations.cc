e7ac7034346cced3d4d5bcf7c5d8897b57ee8d50
blaakmeer <blaakmeer@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-09-21 14:36:52 +0000

    BugID: 169 various bugs solved

--- MAC/APL/_VIC/ArrayOperations/src/ArrayOperations.cc
+++ MAC/APL/_VIC/ArrayOperations/src/ArrayOperations.cc
@@ -239,7 +239,7 @@ GCFEvent::TResult ArrayOperations::concrete_releasing_state(GCFEvent& event, GCF
 {
   LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW,formatString("%s - event=%s",getName().c_str(),evtstr(event)).c_str());
   GCFEvent::TResult status = GCFEvent::HANDLED;
-  newState=LOGICALDEVICE_STATE_IDLE;
+  newState=LOGICALDEVICE_STATE_GOINGDOWN;
   
   switch(event.signal)
   {
