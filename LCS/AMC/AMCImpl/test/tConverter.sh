9d101a6321dbe7165e539cd3f63422dec0fa4258
Marcel Loose <gmloose@users.noreply.github.com>
2005-10-11 08:09:47 +0000

    BugID: 196
    Use port 31339 for testing, instead of the default port 31337, which could
    already be in use.

--- LCS/AMC/AMCImpl/test/tConverter.sh
+++ LCS/AMC/AMCImpl/test/tConverter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "impl" > tConverter.in
 $lofar_sharedir/runtest.sh tConverter > tConverter_impl.log
-echo "client" > tConverter.in
+echo "client localhost 31339" > tConverter.in
 $lofar_sharedir/runtest.sh tConverter > tConverter_client.log
