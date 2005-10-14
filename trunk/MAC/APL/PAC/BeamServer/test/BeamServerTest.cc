114f1776d017193356b4a460bc92393c39d699ad
wierenga <wierenga@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-10-14 08:53:38 +0000

    bugid:450 Updated BeamServerTest to do detailed performance measurements.

--- trunk/MAC/APL/PAC/BeamServer/test/BeamServerTest.cc
+++ trunk/MAC/APL/PAC/BeamServer/test/BeamServerTest.cc
@@ -30,12 +30,27 @@
 #include "Timestamp.h"
 #include "SubArray.h"
 
+#include <GCF/TM/GCF_Control.h>
+#include <PSAccess.h>
+
+//#define CLIENT
+#ifdef CLIENT
+#include <AMCBase/AMCClient/ConverterClient.h>
+#else
+#include <AMCImpl/ConverterImpl.h>
+#endif
+
 #include <iostream>
 
 #include <blitz/array.h>
 
-#define N_BEAMS              (8)
-#define N_SUBBANDS_PER_BEAM  (MEPHeader::N_BEAMLETS/N_BEAMS)
+#define TEST_N_BEAMS              8
+#define TEST_N_BEAMLETS           256 // MEPHeader::N_BEAMLETS
+#define TEST_N_SUBBANDS_PER_BEAM  (TEST_N_BEAMLETS/TEST_N_BEAMS)
+#define TEST_N_LOOPS              4
+
+#define MAX_N_BEAMS              256
+#define MAX_COMPUTE_INTERVAL     16
 
 using namespace blitz;
 using namespace std;
@@ -46,10 +61,10 @@ using namespace BS_Protocol;
 using namespace RTC;
 using namespace CAL;
 
-#define UPDATE_INTERVAL  1
-#define COMPUTE_INTERVAL 10
-#define N_ELEMENTS       100
+#define COMPUTE_INTERVAL 5
+#define N_ELEMENTS       96
 #define N_POLARIZATIONS  2
+#define SIMULATE_SECONDS 2 * MAX_COMPUTE_INTERVAL
 
 namespace LOFAR {
 
@@ -57,13 +72,13 @@ namespace LOFAR {
 
   private:
     Beams m_beams;
-    Beam* m_beamptr[MEPHeader::N_BEAMLETS];
+    Beam* m_beamptr[TEST_N_BEAMLETS];
 
   public:
 
     BeamServerTest() :
       Test("BeamServerTest"),
-      m_beams(MEPHeader::N_BEAMLETS,
+      m_beams(TEST_N_BEAMLETS,
 	      MEPHeader::N_SUBBANDS,
 	      AMC::EarthCoord(1.0,1.0,0.0))
     {
@@ -74,12 +89,12 @@ namespace LOFAR {
 #if 0
       allocate();
       deallocate();
-#endif
       subbandSelection();
       oneTooManyBeam();
       oneTooManyBeamlet();
       emptyBeam();
       pointing();
+#endif
       convert_pointings();
     }
 
@@ -87,10 +102,10 @@ namespace LOFAR {
     {
       Beamlet2SubbandMap allocation;
 
-      for (int bi = 0; bi < N_BEAMS; bi++) {
+      for (int bi = 0; bi < TEST_N_BEAMS; bi++) {
 	allocation().clear();
-	for (int si = 0; si < N_SUBBANDS_PER_BEAM; si++) {
-	  allocation()[si + (N_SUBBANDS_PER_BEAM * bi)] = si;
+	for (int si = 0; si < TEST_N_SUBBANDS_PER_BEAM; si++) {
+	  allocation()[si + (TEST_N_SUBBANDS_PER_BEAM * bi)] = si;
 	}
 	
 	char nodeid[32];
@@ -102,7 +117,7 @@ namespace LOFAR {
 
     void deallocate()
     {
-      for (int i = 0; i < N_BEAMS; i++)
+      for (int i = 0; i < TEST_N_BEAMS; i++)
 	{
 	  TESTC(true == m_beams.destroy(m_beamptr[i]));
 	}
@@ -117,7 +132,7 @@ namespace LOFAR {
       Beamlet2SubbandMap selection;
       selection = m_beams.getSubbandSelection();
 
-      TESTC(selection().size() == N_BEAMS*N_SUBBANDS_PER_BEAM);
+      TESTC(selection().size() == TEST_N_BEAMS * TEST_N_SUBBANDS_PER_BEAM);
 
       deallocate();
 
@@ -135,7 +150,7 @@ namespace LOFAR {
 
       Beamlet2SubbandMap allocation;
 
-      for (int i = 0; i < N_SUBBANDS_PER_BEAM; i++) {
+      for (int i = 0; i < TEST_N_SUBBANDS_PER_BEAM; i++) {
 	allocation()[i] = i;
       }
 
@@ -152,7 +167,7 @@ namespace LOFAR {
  
       Beamlet2SubbandMap allocation;
 
-      for (int i = 0; i < MEPHeader::N_BEAMLETS + 1; i++) {
+      for (int i = 0; i < TEST_N_BEAMLETS + 1; i++) {
 	allocation()[i] = i;
       }
 
@@ -178,7 +193,7 @@ namespace LOFAR {
       START_TEST("pointing", "check addPointing on allocated and deallocated beam");
 
       Beamlet2SubbandMap allocation;
-      for (int i = 0; i < MEPHeader::N_BEAMLETS; i++) {
+      for (int i = 0; i < TEST_N_BEAMLETS; i++) {
 	allocation()[i] = i;
       }
 
@@ -200,51 +215,115 @@ namespace LOFAR {
       Range all = Range::all();
 
       Beamlet2SubbandMap allocation;
-      for (int i = 0; i < MEPHeader::N_BEAMLETS; i++) {
-	allocation()[i] = i;
-      }
-      TESTC(0 != (m_beamptr[0] = m_beams.get("beam.0", "unspecified", allocation)));
 
-      Timestamp now = Timestamp::now();
-
-      // add a few pointings
-      m_beamptr[0]->addPointing(Pointing(0.0, 1.0, now + (long)1, Pointing::LOFAR_LMN));
-      m_beamptr[0]->addPointing(Pointing(0.0, 0.2, now + (long)3, Pointing::LOFAR_LMN));
-      m_beamptr[0]->addPointing(Pointing(0.3, 0.0, now + (long)5, Pointing::LOFAR_LMN));
-      m_beamptr[0]->addPointing(Pointing(0.4, 0.0, now + (long)8, Pointing::LOFAR_LMN));
-      m_beamptr[0]->addPointing(Pointing(0.5, 0.0, now + (long)COMPUTE_INTERVAL, Pointing::LOFAR_LMN));
-
-      // start timer
-      struct timeval start, delay;
-      gettimeofday(&start, 0);
+      //       for (int i = 0; i < TEST_N_BEAMLETS; i++) {
+      // 	allocation()[i] = i;
+      //       }
 
       Array<double, 3>          pos(N_ELEMENTS, N_POLARIZATIONS, 3);
       Array<bool, 2>            select(N_ELEMENTS, N_POLARIZATIONS);
-      Array<complex<double>, 3> weights(COMPUTE_INTERVAL, N_ELEMENTS * N_POLARIZATIONS, MEPHeader::N_BEAMLETS);
       Array<double, 1> loc(3);
 
       pos = 1.0; // x,y coordiante = 1
       pos(all, all, 2) = 0.0; // z-coordinate = 0
-
+      
       loc = 0.0, 0.0, 0.0;
       select = true;
       SubArray subarray("subarray", loc, pos, select, 160000000.0, 1, MEPHeader::N_SUBBANDS, 0xB0 /* LBA */);
 
-      m_beamptr[0]->setSubarray(subarray);
-      m_beams.calculate_weights(now, COMPUTE_INTERVAL, weights, 0);
+#ifdef CLIENT
+      AMC::ConverterClient* converter = 0;
+      try { converter = new AMC::ConverterClient("localhost"); }
+      catch (Exception e) {
+	LOG_FATAL("Failed to connect to amcserver on localhost");
+	exit(EXIT_FAILURE);
+      }
+#else
+      AMC::ConverterImpl* converter = new AMC::ConverterImpl();
+#endif
 
-      // stop timer
-      gettimeofday(&delay, 0);
-      delay.tv_sec -= start.tv_sec;
-      delay.tv_usec -= start.tv_usec;
-      if (delay.tv_usec < 0)
-	{
-	  delay.tv_sec -= 1;
-	  delay.tv_usec = 1000000 + delay.tv_usec;
+      for (int nbeams = 1; nbeams <= MAX_N_BEAMS; nbeams *= 2) {
+	//if (nbeams > TEST_N_BEAMLETS) nbeams = TEST_N_BEAMLETS;
+	for (int compute_interval = 1; compute_interval <= MAX_COMPUTE_INTERVAL; compute_interval *= 2) {
+
+	  int nsubbands_per_beam = TEST_N_BEAMLETS / nbeams;
+
+	  Array<complex<double>, 3> weights(compute_interval, N_ELEMENTS * N_POLARIZATIONS, TEST_N_BEAMLETS);
+
+	  // mark the time
+	  Timestamp now = Timestamp::now();
+	  
+	  bool alloc_ok = true;
+	  for (int bi = 0; bi < nbeams; bi++) {
+	    allocation().clear();
+	    for (int si = 0; si < nsubbands_per_beam; si++) {
+	      allocation()[si + (nsubbands_per_beam * bi)] = si;
+	    }
+	
+	    char nodeid[32];
+	    snprintf(nodeid, 32, "beam.%d", bi);
+    
+	    if (0 == (m_beamptr[bi] = m_beams.get(nodeid, "unspecified", allocation))) alloc_ok = false;
+
+	    // add a few pointings
+	
+	    //3C461 Cassiopeia A
+	    m_beamptr[bi]->addPointing(Pointing(6.123662, 1.026719, RTC::Timestamp::now(0), Pointing::J2000));
+	    // 3C405 Cygnus A
+	    m_beamptr[bi]->addPointing(Pointing(5.233748, 0.711018, RTC::Timestamp::now(3), Pointing::J2000));
+	    // 3C144 Crab nebula (NGC 1952)
+	    m_beamptr[bi]->addPointing(Pointing(1.459568, 0.384089, RTC::Timestamp::now(5), Pointing::J2000));
+	    // 3C274 Virgo NGC4486(M87)
+	    m_beamptr[bi]->addPointing(Pointing(3.276114, 0.216275, RTC::Timestamp::now(8), Pointing::J2000));
+
+	    m_beamptr[bi]->setSubarray(subarray);
+	  }
+	  TESTC(alloc_ok);
+
+	  // start timer
+	  struct timeval start, delay;
+	  gettimeofday(&start, 0);
+
+	  printf("\n");
+	  int loop = 0;
+	  for (loop = 0; loop < TEST_N_LOOPS; loop ++) {
+	    // calculate_weights
+	    m_beams.calculate_weights(now + (long)loop, compute_interval, weights, converter);
+
+	    printf("\r%d/%d", loop, TEST_N_LOOPS); fflush(stdout);
+	  }
+	  printf("\r%d/%d done\n", loop, loop);
+
+	  // stop timer
+	  gettimeofday(&delay, 0);
+	  delay.tv_sec -= start.tv_sec;
+	  delay.tv_usec -= start.tv_usec;
+	  if (delay.tv_usec < 0) {
+	    delay.tv_sec -= 1;
+	    delay.tv_usec = 1000000 + delay.tv_usec;
+	  }
+	  
+	  // average over loop times
+	  delay.tv_usec += delay.tv_sec * 1000000;
+	  delay.tv_usec /= loop;
+
+	  // convert back to sec.usec
+	  delay.tv_sec = delay.tv_usec / 1000000;
+	  delay.tv_usec -= (delay.tv_sec * 1000000);
+	  
+	  printf("\nnbeams\tnsubbands_per_beam\tcompute_interval\tavg. time\n");
+	  printf("%d\t\t%d\t\t\t%d\t\t%ld.%03ld\n", nbeams, nsubbands_per_beam, compute_interval, delay.tv_sec, delay.tv_usec/1000);
+	  printf("\n");
+
+	  bool destroy_ok = true;
+	  for (int i = 0; i < nbeams; i++) {
+	    if (!m_beams.destroy(m_beamptr[i])) destroy_ok = false;
+	  }
+	  TESTC(destroy_ok);
 	}
-      LOG_INFO(formatString("calctime = %d sec %d msec", delay.tv_sec, delay.tv_usec/1000));
+      }
 
-      TESTC(true == m_beams.destroy(m_beamptr[0]));
+      if (converter) delete converter;
 
       STOP_TEST();
     }
@@ -252,18 +331,21 @@ namespace LOFAR {
   };
 };
 
-int main(int /*argc*/, char** /*argv*/)
+int main(int argc, char** argv)
 {
-  Pointing p;
-  p = p;
+  GCFTask::init(argc, argv);
 
-  char prop_path[PATH_MAX];
-  const char* mac_config = getenv("MAC_CONFIG");
+  LOG_INFO(formatString("Program %s has started", argv[0]));
 
-  snprintf(prop_path, PATH_MAX-1,
-	   "%s/%s", (mac_config?mac_config:"."),
-	   "log4cplus.properties");
-  INIT_LOGGER(prop_path);
+  try 
+  {
+    GCF::ParameterSet::instance()->adoptFile("BeamServer.conf");
+  }
+  catch (Exception e)
+  {
+    cerr << "Failed to load configuration files: " << e.text() << endl;
+    exit(EXIT_FAILURE);
+  }
 
   Suite s("Beam Server Test Suite", &cout);
 
@@ -271,5 +353,8 @@ int main(int /*argc*/, char** /*argv*/)
   s.run();
   long nFail = s.report();
   s.free();
+
+  LOG_INFO(formatString("Normal termination of program %s", argv[0]));
+
   return nFail;
 }
