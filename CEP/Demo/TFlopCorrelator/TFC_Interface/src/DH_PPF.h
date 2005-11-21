ad46910df59105e8d37f24b41447b17591f74ad1
romein <romein@astron.nl>
2005-11-21 16:09:07 +0000

    BugID: 225 Added optional different input test values

--- CEP/Demo/TFlopCorrelator/TFC_Interface/src/DH_PPF.h
+++ CEP/Demo/TFlopCorrelator/TFC_Interface/src/DH_PPF.h
@@ -67,9 +67,25 @@ public:
   void setTestPattern()
   {
     (std::cerr << "DH_PPF::setTestPattern() ... ").flush();
+
+#if 0
+    for (int stat = 0; stat < MAX_STATIONS_PER_PPF; stat ++) {
+      for (int sample = 0; sample < NR_STATION_SAMPLES; sample ++) {
+	for (int pol = 0; pol < NR_POLARIZATIONS; pol ++) {
+	  float phi = 3 * 2 * 3.14159265358979323846f * sample / NR_STATION_SAMPLES + stat + 1.5707963267948966192f * pol;
+	  float s, c;
+	  sincosf(phi, &s, &c);
+
+	  (*itsBuffer)[stat][sample][pol] = makei16complex((int) (32767*s), (int) (32767*c));
+	}
+      }
+    }
+#else
     for (size_t i = 0; i < getBufferSize(); i++) {
       ((BufferElementType *) itsBuffer)[i] = makei16complex(rand() << 20 >> 20, rand() << 20 >> 20);
     }
+#endif
+
     std::cerr << "done.\n";
   }
 
