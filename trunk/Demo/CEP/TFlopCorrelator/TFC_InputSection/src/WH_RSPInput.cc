75af123308da0cdf1545b75b1831ac257c79fe99
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-12-23 11:29:55 +0000

    BugID: 260
    Splitted the code for the thread into a different file

--- trunk/Demo/CEP/TFlopCorrelator/TFC_InputSection/src/WH_RSPInput.cc
+++ trunk/Demo/CEP/TFlopCorrelator/TFC_InputSection/src/WH_RSPInput.cc
@@ -26,8 +26,6 @@
 // General includes
 #include <Common/LofarLogger.h>
 #include <Transport/TransportHolder.h>
-#include <Transport/TH_File.h>
-#include <Transport/TH_Null.h>
 
 // Application specific includes
 #include <TFC_Interface/DH_RSP.h>
@@ -38,219 +36,6 @@
 #include <Common/hexdump.h>
 
 using namespace LOFAR;
-
-struct RewriteStruct {
-  timestamp_t oldStamp;
-  timestamp_t expectedStamp;
-  int succeeded;
-};
-
-void printTimers(vector<NSTimer*>& timers)
-{
-  vector<NSTimer*>::iterator it = timers.begin();
-  for (; it != timers.end(); it++) {
-    (*it)->print(cout);
-  }
-
-}
-void cleanupWriteBuffer(void* args)
-{
-  cout<<"\nThread Timers:"<<endl;
-  printTimers(*((vector<NSTimer*>*)args));
-}
-
-void* WriteToBufferThread(void* arguments)
-{
-  LOG_TRACE_FLOW_STR("WH_RSPInput WriterThread");   
-
-  thread_args* args = (thread_args*)arguments;
-  cout<<"Framesize: "<<args->FrameSize<<endl;
-  int seqid, blockid;
-  timestamp_t actualstamp, expectedstamp;
-  vector<timestamp_t> missedStamps;
-  vector<RewriteStruct> oldStamps;
-  missedStamps.reserve(500);
-  oldStamps.reserve(500);
-  bool readnew = true;
-  bool firstloop = true;
-
-  // buffer for incoming rsp data
-  char recvframe[args->FrameSize];
-
-  // define a block of dummy subband data
-  SubbandType dummyblock[args->nrPacketsInFrame];
-  memset(dummyblock, 0, args->nrPacketsInFrame*sizeof(SubbandType));
-
-  // used for debugging
-  int cnt_missed = 0;
-  int cnt_rewritten = 0;
-  int cnt_total = 0;
-
-  int speedCounter = 0;
-
-  vector<NSTimer*> itsTimers;
-  NSTimer threadTimer("threadTimer");
-  NSTimer receiveTimer("receiveTimer");
-  NSTimer writeTimer("writeTimer");
-  NSTimer rewriteTimer("rewriteTimer");
-  NSTimer writeDummyTimer("writeDummyTimer");
-  itsTimers.push_back(&threadTimer);
-  itsTimers.push_back(&receiveTimer);
-  itsTimers.push_back(&writeTimer);
-  itsTimers.push_back(&rewriteTimer);
-  itsTimers.push_back(&writeDummyTimer);
-
-  //  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-  //  pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
-  //pthread_cleanup_push(&cleanupWriteBuffer, &itsTimers);
-
-  // init Transportholder
-  ASSERTSTR(args->Connection->init(), "Could not init TransportHolder");
-
-  int strideSize = args->EPAPacketSize / sizeof(SubbandType);
-
-  while(!args->Stopthread) {
-    threadTimer.start();
-
-    // check stop condition
-    if (args->Stopthread == true) {
-      pthread_exit(NULL);
-    }
-    // catch a frame from input connection
-    if (readnew) {
-      try {
-	if (args->Connection != 0){
-	  receiveTimer.start();
-	  args->Connection->recvBlocking( (void*)recvframe, args->FrameSize, 0);
-	  receiveTimer.stop();
-	}
-      } catch (Exception& e) {
-	LOG_TRACE_FLOW_STR("WriteToBufferThread couldn't read from TransportHolder, stopping thread");
-	pthread_exit(NULL);
-      }	
-    }
-  
-
-    if (args->Connection->getType() != "TH_Null") {
-      // get the actual timestamp of first EPApacket in frame
-      seqid   = ((int*)&recvframe[8])[0];
-      blockid = ((int*)&recvframe[12])[0];
-      actualstamp.setStamp(seqid ,blockid);
-    } else {
-      actualstamp += args->nrPacketsInFrame; 
-    }      
-  
-    // firstloop
-    if (firstloop) {
-      expectedstamp.setStamp(seqid, blockid); // init expectedstamp
-      
-      //get stationid
-      //*args->StationIDptr =((int*)&recvframe[4])[0];
-      if (args->IsMaster) {  // temporary hardcoded statioID's master->0, slave->1 
-        *args->StationIDptr = 0;  
-      }
-      else {
-        *args->StationIDptr = 1;  
-      } //end (temporary hardcoded statioID's)
-      firstloop = false;
-    }
-    
-    // check and process the incoming data
-    if (actualstamp < expectedstamp) {
-      RewriteStruct rewritten;
-      rewritten.oldStamp = actualstamp;
-      rewritten.expectedStamp = expectedstamp;
-      rewriteTimer.start();
-      /* old packet received 
-	 Packet can be saved when its dummy is available in cyclic buffer. 
-         Otherwise this packet will be lost */
-
-      int idx;
-      int p=0;
-      for (; p<args->nrPacketsInFrame; p++) {
-        idx = (p*args->EPAPacketSize) + args->EPAHeaderSize;
-        cnt_rewritten++;
-        if (!args->BBuffer->writeElements((SubbandType*)&recvframe[idx], actualstamp, 1, 1, true)) {
-          cnt_rewritten--;
-          break;  
-	}
-        actualstamp++;   
-      }
-      rewritten.succeeded = p;
-      oldStamps.push_back(rewritten);
-      //cout << cnt_rewritten << " delayed packets recovered." << endl;  // debugging
-
-      // read new frame in next loop
-      readnew = true;
-      // do not increase the expectedstamp
-      rewriteTimer.stop();
-    } else if (actualstamp > expectedstamp) {
-      writeDummyTimer.start();
-      missedStamps.push_back(expectedstamp);
-      // missed a packet so create dummy for that missing packet
-      args->BBuffer->writeElements((SubbandType*)dummyblock, expectedstamp, args->nrPacketsInFrame, 0, false);
-      cnt_missed += args->nrPacketsInFrame;
-      //cout << "Dummy created for " << cnt_missed << " missed packets." << endl; // debugging
-      // read same frame again in next loop
-      readnew = false;
-      // increase the expectedstamp
-      expectedstamp += args->nrPacketsInFrame; 
-      writeDummyTimer.stop();
-    } else {
-      writeTimer.start();
-      // expected packet received so write data into corresponding buffer
-      int idx;
-#if 1
-      args->BBuffer->writeElements((SubbandType*)&recvframe[args->EPAHeaderSize], actualstamp, args->nrPacketsInFrame, strideSize, true);
-      actualstamp += args->nrPacketsInFrame;
-#else
-      for (int p=0; p<args->nrPacketsInFrame; p++) {
-	idx = (p*args->EPAPacketSize) + args->EPAHeaderSize;
-	args->BBuffer->writeElements((SubbandType*)&recvframe[idx], actualstamp);
-      	actualstamp++;
-      }
-#endif
-      // read new frame in next loop
-      readnew = true;
-      // increase the expectedstamp
-      expectedstamp += args->nrPacketsInFrame; 
-      writeTimer.stop();
-    }
-#if 0
-    cnt_total++;
-    if (speedCounter-- < 1) {
-      threadTimer.stop();
-      if (args->IsMaster) {
-	cout<<"master: ";
-      } else {
-	cout<<"slave : ";
-      }
-      cout << cnt_total*args->nrPacketsInFrame <<" EPApackets: "<< cnt_missed<<" missed and "<<cnt_rewritten<<" rewritten"<<endl;
-      cnt_total = cnt_missed = cnt_rewritten = 0;
-      speedCounter = 999;
-
-      printTimers (itsTimers);
-      threadTimer.start();
-    }
-#endif
-    threadTimer.stop();
-  }
-  //pthread_cleanup_pop(1);
-  printTimers(itsTimers);
-  cout<<"Timestamps of missed packets:"<<endl;
-  vector<timestamp_t>::iterator it = missedStamps.begin();
-  for (; it != missedStamps.end(); it++) {
-    cout<<"MIS " << (*it)<<endl;
-  }
-  cout<<"Rewritten packets:"<<endl;
-  vector<RewriteStruct>::iterator rit = oldStamps.begin();
-  for (; rit != oldStamps.end(); rit++) {
-    cout<<"REW " << rit->oldStamp<<" "<< rit->expectedStamp<<" "<<rit->succeeded<<endl;
-  }
-  
-  pthread_exit(NULL);
-}
-
 
 WH_RSPInput::WH_RSPInput(const string& name, 
                          ParameterSet& ps,
@@ -353,13 +138,13 @@ void WH_RSPInput::startThread()
   writerinfo.EPAPacketSize      = itsEPAPacketSize;
   writerinfo.IsMaster           = itsSyncMaster;
   
-  if ((itsTH.getType() == "TH_File") || ((dynamic_cast<TH_Null*>(&itsTH)) != 0)) {
+  if ((itsTH.getType() == "TH_File") || (itsTH.getType() == "TH_Null")) {
     // if we are reading from file, overwriting the buffer should not be allowed
     // this way we can work with smaller files
-    //itsBBuffer->setAllowOverwrite(false);
+    itsBBuffer->setAllowOverwrite(false);
   }
 
-  if (pthread_create(&writerthread, NULL, WriteToBufferThread, &writerinfo) < 0)
+  if (pthread_create(&writerthread, NULL, ReaderThread, &writerinfo) < 0)
   {
     perror("WH_RSPInput: thread creation failure");
     exit(1);
