ffaf23b759132f979c85132765212c64f9d6193a
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-12-23 10:53:14 +0000

    BugID: 260
    Implemented a new buffer (BeamletBuffer and LockedRange)
    The old buffer is still present

---
+++ Demo/CEP/TFlopCorrelator/TFC_InputSection/src/LockedRange.h
@@ -0,0 +1,103 @@
+//#  LockedRange.h: a lock that is done on a range of any type (int, float etc)
+//#
+//#  Copyright (C) 2005
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LOFAR_TFC_INPUTSECTION_LOCKEDRANGE_H
+#define LOFAR_TFC_INPUTSECTION_LOCKEDRANGE_H
+
+// \file LockedRange.h
+// a lock on a range of any type
+
+//# Never #include <config.h> or #include <lofar_config.h> in a header file!
+
+//# Includes
+#include <boost/thread.hpp>
+#include <Common/Timer.h>
+#include <ostream>
+
+namespace LOFAR 
+{
+    using namespace boost;
+
+    // \addtogroup TFC_InputSection
+    // @{
+
+    // Description of class.
+    // This class was written in such a way that T can be any type that supports assignment and comparison operators
+    // When using this class with integers, beware that the upper value of a range is not included in the range
+    // The class S is the result type of a subtraction of two variables of type T
+    template<class T, class S>
+    class LockedRange
+    {
+    public:
+      LockedRange(const S size, const S min, const S max, const T nullOfType);
+      ~LockedRange();
+
+      T writeLock(const T& desiredBegin, const T& desiredEnd);
+      void writeUnlock(const T& end);
+
+      T readLock(const T& desiredBegin, const T& desiredEnd);
+      void readUnlock(const T& end);
+
+      void clear();
+
+      void setOverwriting (bool o) {itsIsOverwriting = o; };
+
+      void printTimers(ostream& os);
+ private:
+      // Copying is not allowed
+      LockedRange (const LockedRange& that);
+      LockedRange& operator= (const LockedRange& that);
+
+      //# Datamembers
+      const S itsSize;
+      const S itsMin;
+      const S itsMax;
+      const T itsNullOfType;
+
+      T itsReadHead;
+      T itsReadTail;
+      T itsWriteHead;
+      T itsWriteTail;
+  
+      mutex itsMutex;
+      condition itsDataAvailCond;
+      condition itsSpaceAvailCond;
+
+      // If this buffer IsOverwriting the readtail and readhead are shifted when the buffer is full
+      // this means old data is lost, but all new data is stored in the buffer
+      bool itsIsOverwriting;
+
+      NSTimer itsWriteLockTimer;
+      NSTimer itsWriteUnlockTimer;
+      NSTimer itsReadLockTimer;
+      NSTimer itsReadUnlockTimer;
+      NSTimer itsWaitingForDataTimer;
+      NSTimer itsWaitingForSpaceTimer;
+    };
+
+    // @}
+
+} // namespace LOFAR
+
+#include <TFC_InputSection/LockedRange.tcc>
+
+#endif
