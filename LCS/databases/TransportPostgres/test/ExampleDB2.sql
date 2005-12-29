ab9f1ccf7d0fa3ecda1dabb5ed64cfeecc71e20a
meijeren <meijeren@b3281354-443f-0410-9bd6-bf5212d75c11>
2005-12-29 14:55:51 +0000

    BugID:554 Add package TransportPostgres, which implements a TransportHolder which uses Postgresql directly.

---
+++ LCS/databases/TransportPostgres/test/ExampleDB2.sql
@@ -0,0 +1,11 @@
+-- This file creates the databasetable for class ExampleDB
+--
+-- WARNING: THE TABLE IS DROPPED BEFORE CREATION!!!
+--
+drop table ExampleDB2;
+create table ExampleDB2 (
+	SEQNR   BIGINT,
+	TAG     INTEGER,
+	DATA	BYTEA
+);
+
