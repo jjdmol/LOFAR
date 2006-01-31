c23d127909df520ffa4bcf15f7263fd11a9505bb
blaakmeer <blaakmeer@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-01-31 13:07:11 +0000

    BugID: 586
    Added PIC Table Model

--- trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/TablePanel.java
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/TablePanel.java
@@ -5,6 +5,7 @@
  */
 
 package nl.astron.lofar.sas.otbcomponents;
+import javax.swing.table.AbstractTableModel;
 
 /**
  *
@@ -16,9 +17,10 @@ public class TablePanel extends javax.swing.JPanel {
     public TablePanel() {
         initComponents();
 
-        // Give our own model to the Table
-        TableModel tm = new TableModel();
-        jTable1.setModel(tm);       
+    }
+    
+    public void setTableModel(AbstractTableModel tableModel) {
+        jTable1.setModel(tableModel);
     }
     
     /** This method is called from within the constructor to
@@ -28,6 +30,7 @@ public class TablePanel extends javax.swing.JPanel {
      */
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
+        jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
         setLayout(new java.awt.BorderLayout());
@@ -43,14 +46,18 @@ public class TablePanel extends javax.swing.JPanel {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        add(jTable1, java.awt.BorderLayout.CENTER);
+        jScrollPane1.setViewportView(jTable1);
+
+        add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
     }
     // </editor-fold>//GEN-END:initComponents
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
+    
     
 }
