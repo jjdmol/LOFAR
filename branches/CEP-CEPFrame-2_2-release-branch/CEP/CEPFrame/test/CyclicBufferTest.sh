963049756e7078db06046649ddd5e145d8c3348a
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-02-06 11:27:50 +0000

    BugID: 403
    Prepare for release 2.2

--- branches/CEP-CEPFrame-2_2-release-branch/CEP/CEPFrame/test/CyclicBufferTest.sh
+++ branches/CEP-CEPFrame-2_2-release-branch/CEP/CEPFrame/test/CyclicBufferTest.sh
@@ -4,8 +4,8 @@
 # Limit the running time of this test to 90 seconds 
 # to get out of possible deadlock situation
 #
-   echo 'call "../../../test/CyclicBufferTest.gnuplot" "CB.writers" 0 20' >  gnuplot.cmd \
-&& echo 'call "../../../test/CyclicBufferTest.gnuplot" "CB.readers" 0 20' >> gnuplot.cmd \
+   echo "call '${top_srcdir}/test/CyclicBufferTest.gnuplot' 'CB.writers' 0 20" >  gnuplot.cmd \
+&& echo "call '${top_srcdir}/test/CyclicBufferTest.gnuplot' 'CB.readers' 0 20" >> gnuplot.cmd \
 && $lofar_sharedir/runtest.sh ./CyclicBufferTest 90 > CB.log 2> CB.err \
 && cat CB.*.write > CB.writers \
 && cat CB.*.read  > CB.readers \
