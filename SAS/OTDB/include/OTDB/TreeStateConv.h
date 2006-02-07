d9bd143b6950084eb890e98240d6dc4983f7fdbf
coolen <coolen@astron.nl>
2006-02-07 09:56:20 +0000

    BugID: 626 Added OTDBtypes.h to include

--- SAS/OTDB/include/OTDB/TreeStateConv.h
+++ SAS/OTDB/include/OTDB/TreeStateConv.h
@@ -29,6 +29,7 @@
 //# Never #include <config.h> or #include <lofar_config.h> in a header file!
 //# Includes
 #include <OTDB/Converter.h>
+#include <OTDB/OTDBtypes.h>
 
 namespace LOFAR {
   namespace OTDB {
