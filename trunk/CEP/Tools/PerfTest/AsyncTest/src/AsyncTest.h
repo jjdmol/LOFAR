8ebbaf02f9038914eef86449b0b204eaac06c618
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-02-15 13:48:37 +0000

    BugID: 394
    Minor textual changes to fix some documentation "bugs".

--- trunk/CEP/Tools/PerfTest/AsyncTest/src/AsyncTest.h
+++ trunk/CEP/Tools/PerfTest/AsyncTest/src/AsyncTest.h
@@ -1,32 +1,33 @@
-//  AsyncTest.h: Concrete ApplicationHolder class for performance measurements
-//               on a number of steps with buffered (asynchronous) transport
-//
-//  Copyright (C) 2000, 2002
-//  ASTRON (Netherlands Foundation for Research in Astronomy)
-//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//  $Id$
-//
-//
-//
-//////////////////////////////////////////////////////////////////////////
+//#  AsyncTest.h: Concrete ApplicationHolder class for performance measurements
+//#               on a number of steps with buffered (asynchronous) transport
+//#
+//#  Copyright (C) 2000, 2002
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
 
 #ifndef LOFAR_ASYNCTEST_ASYNCTEST_H
 #define LOFAR_ASYNCTEST_ASYNCTEST_H
+
+// \file
+// Concrete ApplicationHolder class for performance measurements on a number
+// of steps with buffered (asynchronous) transport
+
 
 #include <CEPFrame/ApplicationHolder.h>
 #include <Blob/KeyValueMap.h>
--- trunk/CEP/Tools/PerfTest/InOutTest/src/WH_Source.h
+++ trunk/CEP/Tools/PerfTest/InOutTest/src/WH_Source.h
@@ -1,32 +1,31 @@
-//  WH_Source.h: WorkHolder class using DH_Example() objects and 
-// 
-//
-//  Copyright (C) 2000, 2001
-//  ASTRON (Netherlands Foundation for Research in Astronomy)
-//  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//  $Id$
-//
-//
-//
-//////////////////////////////////////////////////////////////////////
+//#  WH_Source.h: WorkHolder class using DH_Example() objects and 
+//# 
+//#
+//#  Copyright (C) 2000, 2001
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
 
 #ifndef LOFAR_INOUTTEST_WH_SOURCE_H
 #define LOFAR_INOUTTEST_WH_SOURCE_H
+
+// \file
+// WorkHolder class using DH_Example() objects and 
 
 #include <tinyCEP/WorkHolder.h>
 #include <Common/lofar_string.h>
