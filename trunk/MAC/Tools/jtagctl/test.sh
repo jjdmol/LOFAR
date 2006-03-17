7ca8273b5de47b839b84daea86532989e846c41b
wierenga <wierenga@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-03-17 08:37:40 +0000

    BugID: 676
    Completed test suite. Added feature to generate the reference data files
    assuming the code is correct.

--- trunk/MAC/Tools/jtagctl/test.sh
+++ trunk/MAC/Tools/jtagctl/test.sh
@@ -1,19 +1,51 @@
 #!/bin/sh
 
+#
+# Check whether nc (netcat) is available,
+# it is required to run the tests
+#
 nc -h > /dev/null 2>&1
 if [ $? -ne 1 ]; then
     echo "This testsuite requires the 'nc' utility. Please install it."
     exit 1
 fi
 
+#
+# Parse arguments
+#
+if [ $# -eq 1 ]; then
+    case $1 in
+	-h) echo "usage: ./test.sh [-h | -genref]"
+	    echo " -h      this help"
+	    echo " -genref generate reference files"
+	    exit
+	    ;;
+	-genref)
+	    ref=".ref"
+	    ;;
+    esac
+fi
+
+#
+# Definitions
+#
 OD='od -An -tx1'
 NC='nc -l -p 3800'
 STUB='./jtagctl -T'
 JT='./jtagctl -h localhost'
 
+#
+# Global variables
+#
 declare -i passcount=0
 declare -i failcount=0
 
+#
+# checkresult
+#
+# Compare output with reference output
+# Keep pass/fail statistics
+#
 function checkresult
 {
     filename=$1
@@ -31,26 +63,74 @@ function checkresult
     rm -f $filename
 }
 
-testname=listfiles
-$STUB -l | $NC | $OD > $testname.stub 2>&1 &
-$JT   -l             > $testname.dat  2>&1
-wait
-checkresult $testname.stub
-checkresult $testname.dat
+#
+# finalize
+# check test results
+#
+function finalize
+{
+    if [ $# -ge 1 ]; then testname=$1; shift; fi
+    if [ $# -ge 1 ]; then ref=$1;      shift; fi
 
+    if [ "x$ref" = "x" ]; then checkresult $testname.stub; fi
+    if [ "x$ref" = "x" ]; then checkresult $testname.dat;  fi
+}
+
+#
+# ipupdate
+#
+testname=ipupdate
+$STUB -i 1.2.3.4 | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -i 1.2.3.4             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
+
+#
+# savefile
+#
 testname=savefile
-$STUB -s testdata.bin | $NC | $OD > $testname.stub 2>&1 &
-$JT   -s testdata.bin             > $testname.dat  2>&1
-wait
-checkresult $testname.stub
-checkresult $testname.dat
+$STUB -s testdata.bin | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -s testdata.bin             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
 
+#
+# testexec
+#
 testname=testexec
-$STUB -g genfile -a aplfile | $NC | $OD > $testname.stub 2>&1 &
-$JT   -g genfile -a aplfile             > $testname.dat  2>&1
-wait
-checkresult $testname.stub
-checkresult $testname.dat
+$STUB -g genfile -a aplfile | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -g genfile -a aplfile             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
+
+#
+# erase (-tg)
+#
+testname=erase_tg
+$STUB -e genfile -tg | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -e genfile -tg             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
+
+#
+# erase (-ta)
+#
+testname=erase_ta
+$STUB -e genfile -ta | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -e genfile -ta             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
+
+#
+# erase (-tc)
+#
+testname=erase_tc
+$STUB -e genfile -tc | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -e genfile -tc             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
+
+#
+# listfiles
+#
+testname=listfiles
+$STUB -l | $NC | $OD > $testname.stub$ref 2>&1 &
+$JT   -l             > $testname.dat$ref  2>&1
+wait; finalize $testname $ref
 
 # report statistics
 if [ $failcount -gt 0 ]; then
