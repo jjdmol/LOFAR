9217b97001f30a9fc7e279375764370520800acb
nobody <nobody@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-03-24 10:56:53 +0000

    This commit was manufactured by cvs2svn to create tag 'LCS-AMC-Bug697-task-root'.

---
+++ tags/LCS-AMC-Bug697-task-root/LCS/AMC/AMCBase/include/AMCBase/SkyCoord.h
@@ -0,0 +1,120 @@
+//# SkyCoord.h: Class to hold a sky coordinate as 2 angles
+//#
+//# Copyright (C) 2002
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef LOFAR_AMCBASE_SKYCOORD_H
+#define LOFAR_AMCBASE_SKYCOORD_H
+
+// \file
+// Class to hold a sky coordinate as 2 angles
+
+//# Forward Declarations.
+#include <Common/lofar_iosfwd.h>
+#include <Common/lofar_string.h>
+#include <Common/lofar_vector.h>
+
+namespace LOFAR
+{
+  namespace AMC
+  {
+
+    // \addtogroup AMCBase
+    // @{
+
+    // This class represents a position in the sky. The position is stored as
+    // a direction using a pair of angles. The context where the object is
+    // used defines the coordinate system and frame, so the class can be used
+    // for any pair of sky coordinates (like RA/DEC and AZ/ELEV). The correct
+    // interpretation of the coordinates should be done by the user of this
+    // class.
+    class SkyCoord
+    {
+    public:
+      // Types of sky coordinates. Currently, only three types are supported:
+      // J2000, AZEL and ITRF.
+      enum Types {
+        INVALID = -1,   ///< Used when specified value is out of range.
+        J2000,         
+        AZEL,
+        ITRF,
+        //# Insert new types HERE !!
+        N_Types         ///< Number of reference types.
+      };
+
+      // Default constructor uses 0 for the angles and J2000 as reference
+      // type.
+      SkyCoord()
+        : itsAngle0(0), itsAngle1(0), itsType(J2000) {}
+
+      // Create a sky coordinate by giving the longitude \a angle0 and
+      // latitude \a angle1 in radians and the reference type \a typ.
+      SkyCoord (double angle0, double angle1, Types typ = J2000);
+
+      // Return angle0 in radians. This could be, for example, right ascension
+      // (RA) or azimuth (AZ).
+      double angle0() const
+      { return itsAngle0; }
+
+      // Return angle1 in radians. This could be, for example, declination
+      // (DEC) or elevation (EL).
+      double angle1() const
+      { return itsAngle1; }
+
+      // Return the reference type.
+      Types type() const
+      { return itsType; }
+
+      // Return the reference type as a string.
+      const string& showType() const;
+
+      // Return whether sky coordinate type is valid.
+      bool isValid() const
+      { return itsType != INVALID; }
+
+      // Return the sky coordinate in cartesion coordinates.
+      vector<double> xyz() const;
+
+    private:
+      // Angle0 in radians.
+      double itsAngle0;
+
+      // Angle1 in radians.
+      double itsAngle1;
+
+      // Type of sky coordinate.
+      Types itsType;
+    };
+
+    // Output a SkyCoord in ASCII format.
+    ostream& operator<< (ostream&, const SkyCoord&);
+
+    // Compare two SkyCoord objects for equality. 
+    // \note Two invalid objects can \e never be equal.
+    bool operator==(const SkyCoord& lhs, const SkyCoord& rhs);
+
+    // @}
+
+  } // namespace AMC
+
+} // namespace LOFAR
+
+
+#endif
---
+++ tags/LCS-AMC-Bug697-task-root/LCS/AMC/AMCBase/test/tBlobIO.cc
@@ -0,0 +1,199 @@
+//#  tBlobIO.cc: test program for the BlobIO class.
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+//# Includes
+#include <AMCBase/BlobIO.h>
+#include <AMCBase/ConverterCommand.h>
+#include <AMCBase/ConverterStatus.h>
+#include <AMCBase/SkyCoord.h>
+#include <AMCBase/EarthCoord.h>
+#include <AMCBase/TimeCoord.h>
+#include <Blob/BlobIBufChar.h>
+#include <Blob/BlobIStream.h>
+#include <Blob/BlobOBufChar.h>
+#include <Blob/BlobOStream.h>
+#include <Common/LofarLogger.h>
+#include <Common/lofar_iomanip.h>
+
+using namespace LOFAR;
+using namespace LOFAR::AMC;
+
+
+void test(const ConverterCommand& cco)
+{
+  BlobOBufChar bob;
+  BlobOStream bos(bob);
+
+  bos.putStart(typeid(ConverterCommand).name(), 1);
+  bos << cco;
+  bos.putEnd();
+
+  BlobIBufChar bib(bob.getBuffer(), bob.size());
+  BlobIStream bis(bib);
+
+  ConverterCommand cci;
+  bis.getStart(typeid(ConverterCommand).name());
+  bis >> cci;
+  bis.getEnd();
+
+  ASSERTSTR(cci == cco, "cci = " << cci << "; cco = " << cco);
+}
+
+
+void test(const ConverterStatus& cso)
+{
+  BlobOBufChar bob;
+  BlobOStream bos(bob);
+
+  bos.putStart(typeid(ConverterStatus).name(), 1);
+  bos << cso;
+  bos.putEnd();
+
+  BlobIBufChar bib(bob.getBuffer(), bob.size());
+  BlobIStream bis(bib);
+
+  ConverterStatus csi;
+  bis.getStart(typeid(ConverterStatus).name());
+  bis >> csi;
+  bis.getEnd();
+
+  ASSERTSTR(csi.get() == cso.get() && 
+            csi.text() == cso.text(),
+            "csi = " << csi << "; cso = " << cso);
+}
+
+
+void test(const SkyCoord& sco)
+{
+  BlobOBufChar bob;
+  BlobOStream bos(bob);
+
+  bos.putStart(typeid(SkyCoord).name(), 1);
+  bos << sco;
+  bos.putEnd();
+
+  BlobIBufChar bib(bob.getBuffer(), bob.size());
+  BlobIStream bis(bib);
+
+  SkyCoord sci;
+  bis.getStart(typeid(SkyCoord).name());
+  bis >> sci;
+  bis.getEnd();
+
+  ASSERTSTR(sci == sco, "sci = " << sci << "; sco = " << sco);
+}
+
+
+void test(const EarthCoord& eco)
+{
+  BlobOBufChar bob;
+  BlobOStream bos(bob);
+
+  bos.putStart(typeid(EarthCoord).name(), 1);
+  bos << eco;
+  bos.putEnd();
+
+  BlobIBufChar bib(bob.getBuffer(), bob.size());
+  BlobIStream bis(bib);
+
+  EarthCoord eci;
+  bis.getStart(typeid(EarthCoord).name());
+  bis >> eci;
+  bis.getEnd();
+
+  ASSERTSTR(eci == eco, "eci = " << eci << "; eco = " << eco);
+}
+
+
+void test(const TimeCoord& tco)
+{
+  BlobOBufChar bob;
+  BlobOStream bos(bob);
+
+  bos.putStart(typeid(TimeCoord).name(), 1);
+  bos << tco;
+  bos.putEnd();
+
+  BlobIBufChar bib(bob.getBuffer(), bob.size());
+  BlobIStream bis(bib);
+
+  TimeCoord tci;
+  bis.getStart(typeid(TimeCoord).name());
+  bis >> tci;
+  bis.getEnd();
+
+  // We must make the detour through the doubles dci and dco, otherwise the
+  // comparison will be done directly inside the FPU (using 80 bits instead of
+  // 64), which will cause the assert to fail.
+  double dci(tci.mjd());
+  double dco(tco.mjd());
+  ASSERTSTR(dci == dco, "tci = " << tci << "; tco = " << tco);
+}
+
+
+int main(int /*argc*/, const char* const argv[])
+{
+  INIT_LOGGER(argv[0]);
+
+  try {
+
+    try { 
+      test(ConverterCommand(ConverterCommand::INVALID)); 
+    } catch (AssertError& e) {}
+    test(ConverterCommand(ConverterCommand::J2000toAZEL));
+    test(ConverterCommand(ConverterCommand::J2000toITRF));
+    test(ConverterCommand(ConverterCommand::AZELtoJ2000));
+    test(ConverterCommand(ConverterCommand::ITRFtoJ2000));
+    try {
+      // Force the use of an undefined enumerated value.
+      test(ConverterCommand(static_cast<ConverterCommand::Commands>(1000)));
+    } catch (AssertError& e) {}
+
+    test(ConverterStatus());
+    test(ConverterStatus(ConverterStatus::UNKNOWN, "Unknown error"));
+    test(ConverterStatus(ConverterStatus::OK, "Success"));
+    test(ConverterStatus(ConverterStatus::ERROR, "Converter error"));
+    // Use of an undefined enumerated value result in UNKNOWN.
+    test(ConverterStatus(static_cast<ConverterStatus::Status>(1000),
+                         "Undefined error"));
+
+    test(SkyCoord());
+    test(SkyCoord(0.4, -0.19, SkyCoord::ITRF));
+    test(SkyCoord(-1.2, 2.38, SkyCoord::AZEL));
+
+    test(EarthCoord());
+    test(EarthCoord(0.25*M_PI, -0.33*M_PI));
+    test(EarthCoord(-0.67*M_PI, 0.75*M_PI, 249.98, EarthCoord::WGS84));
+
+    test(TimeCoord());
+    test(TimeCoord(0));
+
+  } catch (Exception& e) {
+    LOG_ERROR_STR(e);
+    return 1;
+  }
+
+  return 0;
+}
