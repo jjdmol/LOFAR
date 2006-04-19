618aad7d6a3fb4ba14ea515684fca318a7216b72
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 14:16:17 +0000

    701 Additions: - New Interface to make access to data sources modular and expandable in the future

---
+++ trunk/JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/IPlotDataAccess.java
@@ -0,0 +1,29 @@
+/*
+ * IPlotDataAccess.java
+ *
+ * Copyright (C) 2006
+ * ASTRON (Netherlands Foundation for Research in Astronomy)
+ * P.O. Box 2, 7990AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ */
+
+package nl.astron.lofar.sas.plotter;
+
+import java.util.HashMap;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
+
+/**
+ * @created 19-04-2006, 11:00
+ * @author pompert
+ * @version $Id$
+ * @updated 19-apr-2006 11:00:00
+ */
+public interface IPlotDataAccess{
+    
+    /**
+     * @param constraint
+     */
+    public HashMap retrieveData(String constraint) throws PlotterException;
+    
+}
+
