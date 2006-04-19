1285f9659c426657f4070a3792458e769c062591
renting <renting@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 14:31:04 +0000

    BugID:700 First compilable version of the CS1 Imager

--- trunk/Appl/CEP/CS1/CS1_Imager/include/CS1_Imager/ImagerProcessControl.h
+++ trunk/Appl/CEP/CS1/CS1_Imager/include/CS1_Imager/ImagerProcessControl.h
@@ -1,24 +1,28 @@
-/***************************************************************************
- *   Copyright (C) 2006 by Adriaan Renting                                 *
- *   renting@astron.nl                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-#ifndef LOFARFLAGGERPROCESSCONTROL_H
-#define LOFARFLAGGERPROCESSCONTROL_H
+//#  ImagerProcessControl.h: one line description
+//#
+//#  Copyright (C) 2006
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#  @author Adriaan Renting, renting@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LOFARIMAGERPROCESSCONTROL_H
+#define LOFARIMAGERPROCESSCONTROL_H
 
 #include <PLC/ProcessControl.h>
 #include <APS/ParameterSet.h>
@@ -26,33 +30,44 @@
 /**
 @author Adriaan Renting
 */
-namespace WSRT
+namespace casa
 { 
-  class MS_File; //foreward declaration
-  class ComplexMedianFlagger;
-} //namespace WSRT
+  class MeasurementSet; //foreward declaration
+  class Imager;
+} //namespace casa
 
 namespace LOFAR 
 {
   namespace CS1
   {
-    class FlaggerProcessControl : public LOFAR::ACC::PLC::ProcessControl
+    class ImagerProcessControl : public LOFAR::ACC::PLC::ProcessControl
     {
     private:
       std::string  itsMS;
-      bool         itsFlagData; 
-      bool         itsFlagRMS;
-      bool         itsExisting;
-      int          itsWindow;
-      bool         itsCrosspol;
-      double       itsMin;
-      double       itsMax;
-      WSRT::MS_File*              myMS;
-      WSRT::ComplexMedianFlagger* itsFlagger;
+      bool         itsCompress; 
+      std::string  itsMode;
+      int          itsNChannel;
+      int          itsStart;
+      int          itsStep;
+      int          itsNX;
+      int          itsNY;
+      int          itsCellX;
+      int          itsCellY;
+      std::string  itsStokes;
+      std::string  itsWeightType;
+      int          itsWeightNPixels;
+      int          itsTile;
+      double       itsPadding;
+      std::string  itsGridFunction;
+      std::string  itsImageType;
+      std::string  itsImageName;
+      
+      casa::MeasurementSet* myMS;
+      casa::Imager*         myImager;
     public:
-      FlaggerProcessControl();
+      ImagerProcessControl();
 
-      ~FlaggerProcessControl();
+      ~ImagerProcessControl();
       // \name Command to control the processes.
       // There are a dozen commands that can be sent to a application process
       // to control its flow. The return values for these command are:<br>
@@ -77,15 +92,40 @@ namespace LOFAR
       // The run phase stays active until another command is send.
       tribool run      ();
 
+      // With the \c pause command the process stops its run phase and starts
+      // waiting for another command. The \c condition argument contains the
+      // contition the process should use for ending the run phase. This
+      // condition is a key-value pair that can eg. contain a timestamp or a
+      // number of a datasample.
       tribool pause(const std::string&);
+      
+      // \c Quit stops the process.
+      // The process \b must call \c unregisterAtAC at ProcControlServer during 
+      // the execution of this command to pass the final results to the 
+      // Application Controller.
       tribool quit();
+      
+      // \c Recover reconstructs the process as it was saved some time earlier.
+      // The \c source argument contains the database info the process must use
+      // to find the information it needs.
       tribool recover(const std::string&);
+      
+      // With \c reinit the process receives a new parameterset that it must use
+      // to reinitialize itself.
       tribool reinit(const  std::string&);
+      
+      // With the \c snapshot command the process is instructed to save itself
+      // in a database is such a way that on another moment in time it can
+      // be reconstructed and can continue it task.<br>
+      // The \c destination argument contains database info the process
+      // must use to save itself.
       tribool snapshot(const std::string&);
+      
+      // Define a generic way to exchange info between client and server.
       std::string askInfo(const std::string&);
+      // @}
+    };//class ImagerProcessControl
+  } //namespace CS1
+};//namespace LOFAR
 
-    }; //class FlaggerProcessControl
-  } //namespace CS1_Flagger
-}; //namespace LOFAR
-
-#endif
+#endif //LOFARIMAGERPROCESSCONTROL_H
--- trunk/Appl/CEP/CS1/CS1_Imager/src/Imager.cc
+++ trunk/Appl/CEP/CS1/CS1_Imager/src/Imager.cc
@@ -1,46 +1,43 @@
-/***************************************************************************
- *   Copyright (C) 2006 by ASTRON, Adriaan Renting                         *
- *   renting@astron.nl                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+//#  Imager.cc: one line description
+//#
+//#  Copyright (C) 2006
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#  @author Adriaan Renting, renting@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
 
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 #include <PLC/ACCmain.h>
 #include <casa/Exceptions.h>
-#include <CS1_Flagger/FlaggerProcessControl.h>
+#include <CS1_Imager/ImagerProcessControl.h>
 
 int main(int argc, char *argv[])
 {
   try
   {
-    LOFAR::CS1::FlaggerProcessControl myProcess;
+    LOFAR::CS1::ImagerProcessControl myProcess;
     return LOFAR::ACC::PLC::ACCmain(argc, argv, &myProcess);
   } //try
   catch(casa::AipsError& err)
   {
     std::cerr << "Aips++ error detected: " << err.getMesg() << std::endl;
     return -2;
-  }
-  catch(...)
-  {
-    std::cerr << "** PROBLEM **: Unhandled exception caught." << std::endl;
-    return -3;
   }
 }
