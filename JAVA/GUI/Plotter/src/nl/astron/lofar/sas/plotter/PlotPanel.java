e95be253763700c1a987461d4e071aa1ef96b0eb
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 07:57:24 +0000

    701 Additions: - Added Exception handling - Test method for tackling resize issues of a plotpanel (SGT doesnt resize properly) Mods: - None

--- JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotPanel.java
+++ JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotPanel.java
@@ -11,8 +11,12 @@ package nl.astron.lofar.sas.plotter;
 
 import java.awt.Image;
 import java.awt.PrintJob;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
 
 /**
  * @created 11-04-2006, 15:00
@@ -28,6 +32,7 @@ public class PlotPanel extends JPanel{
 
 	public PlotPanel(){
             m_PlotController = new PlotController();
+            //addComponentListener(resizeListener);
         }
 
 	public void finalize() throws Throwable {
@@ -36,9 +41,34 @@ public class PlotPanel extends JPanel{
 
 	public void createPlot(String constraint){
             currentDataConstraint = constraint;
-            plot = m_PlotController.createPlot(constraint);
-            this.add(plot);
+            try {
+                plot = m_PlotController.createPlot(constraint);
+                
+                add(plot);
+            } catch (PlotterException ex) {
+                add(new JLabel(ex.getMessage()));
+                //ex.printStackTrace();
+            }
         }
+        
+        private ComponentListener resizeListener = new ComponentListener(){
+            public void componentHidden(ComponentEvent e){}
+            public void componentMoved(ComponentEvent e){}
+            public void componentResized(ComponentEvent e){
+                int height = getHeight();
+                int width = getWidth();
+                int height2 = plot.getHeight();
+                int width2 = plot.getWidth();
+                System.out.println("Width/Height of PlotPanel: "+width+"/"+height);
+                plot.setSize(width,height);
+                //plot.validate();
+                System.out.println("Width/Height of PlotLayout: "+width2+"/"+height2);
+                //plot.firePropertyChange("Height",height,height2);
+                //plot.firePropertyChange("Width",width,width2);
+                
+            }
+            public void componentShown(ComponentEvent e){}
+        };
 
 	public Image exportImage(){
 		return null;
@@ -60,4 +90,5 @@ public class PlotPanel extends JPanel{
         public JComponent getPlot(){
             return plot;
         }
+        
  }
