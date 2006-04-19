b6743b67f91205acc9e3b69d403d7c564e53ea0a
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 14:19:00 +0000

    701 Additions: - Adding string locators for the standard data access class implementation and data export class implementation - Adding exception strings for data access exceptions Mods: - Improved naming of variables

--- trunk/JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotConstants.java
+++ trunk/JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotConstants.java
@@ -19,31 +19,45 @@ public abstract class PlotConstants{
     //Data Model constants
     
     //Data Model Metadata
-    public static final String DATASET_NAME    = "DataSetName";
-    public static final String DATASET_SUBNAME = "DataSetSubName";
+    public static final String DATASET_NAME       = "DataSetName";
+    public static final String DATASET_SUBNAME    = "DataSetSubName";
     public static final String DATASET_XAXISLABEL = "DataSetXAxisLabel";
     public static final String DATASET_XAXISUNIT  = "DataSetXAxisUnit";
     public static final String DATASET_YAXISLABEL = "DataSetYAxisLabel";
     public static final String DATASET_YAXISUNIT  = "DataSetYAxisUnit";
     public static final String DATASET_ZAXISLABEL = "DataSetZAxisLabel";
-    public static final String DATASET_ZAXISUNIT = "DataSetZAxisUnit";
-    public static final String DATASET_VALUES = "DataSetValues";
+    public static final String DATASET_ZAXISUNIT  = "DataSetZAxisUnit";
+    public static final String DATASET_VALUES     = "DataSetValues";
     
     //Data Model ValueSets
     
-    public static final String DATASET_XVALUES = "XValues";
-    public static final String DATASET_YVALUES = "YValues";
-    public static final String DATASET_ZVALUES = "ZValues";
+    public static final String DATASET_XVALUES    = "XValues";
+    public static final String DATASET_YVALUES    = "YValues";
+    public static final String DATASET_ZVALUES    = "ZValues";
     public static final String DATASET_VALUELABEL = "DataSetValueLabel";
     
     //Supported plotter framework
-    public static final String FRAMEWORK = "nl.astron.lofar.sas.plotter.SGTPlot";
+    public static final String FRAMEWORK         = "nl.astron.lofar.sas.plotter.PlotSGTImpl";
+    
+    //Supported plotter data access class
+    //public static final String DATA_ACCESS_CLASS = "nl.astron.lofar.sas.plotter.PlotDataAccessParmDBImpl";
+    public static final String DATA_ACCESS_CLASS = "nl.astron.lofar.sas.plotter.test.PlotDataAccessTestImpl";
+    
+    //Supported plotter data export class
+    //public static final String DATA_EXPORT_CLASS = "nl.astron.lofar.sas.plotter.PlotDataExportRootImpl";
 
     //Exception messages
-    public static final String EXCEPTION_GENERIC = "An internal error occurred with no leads to its cause. Please contact software support.";
-    public static final String EXCEPTION_EMPTYDATASET = "An internal error occurred : An empty data set was passed to the plotter framework.";
-    public static final String EXCEPTION_FRAMEWORKINIT = "An internal error occurred : The plotter framework class specified in the constants file could not be accessed and/or instantiated by the plotter.";
-    public static final String EXCEPTION_FRAMEWORKNOTFOUND = "An internal error occurred : The plotter framework class specified in the constants file could not be found by the plotter.";
-    public static final String EXCEPTION_FRAMEWORKNOTCOMPATIBLE = "An internal error occurred : The plotter framework class specified in the constants file does not implement the IPlot interface, and therefore is NOT compatible with the plotter.";
+    public static final String EXCEPTION_GENERIC                = "An internal error occurred with no leads to its cause. Please contact software support.";
+    public static final String EXCEPTION_NOT_IMPLEMENTED         = "An internal error occurred : The plotter has detected that the feature you requested is not (fully) implemented in this release. Details: ";
+    public static final String EXCEPTION_OPERATION_NOT_SUPPORTED  = "An internal error occurred : The plotter has detected that the operation you requested is not supported by this plotter. Details: ";
     
+    public static final String EXCEPTION_EMPTY_DATASET           = "An internal error occurred : An empty data set was passed to the plotter framework.";
+    
+    public static final String EXCEPTION_FRAMEWORK_INIT          = "An internal error occurred : The plotter framework class specified in the constants file could not be accessed and/or instantiated by the plotter.";
+    public static final String EXCEPTION_FRAMEWORK_NOT_FOUND      = "An internal error occurred : The plotter framework class specified in the constants file could not be found by the plotter.";
+    public static final String EXCEPTION_FRAMEWORK_NOT_COMPATIBLE = "An internal error occurred : The plotter framework class specified in the constants file does not implement the IPlot interface, and therefore is NOT compatible with the plotter.";
+    
+    public static final String EXCEPTION_DATA_ACCESS_INIT           = "An internal error occurred : The plotter data access class specified in the constants file could not be accessed and/or instantiated by the plotter.";
+    public static final String EXCEPTION_DATA_ACCESS_NOT_FOUND      = "An internal error occurred : The plotter data access class specified in the constants file could not be found by the plotter.";
+    public static final String EXCEPTION_DATA_ACCESS_NOT_COMPATIBLE = "An internal error occurred : The plotter data access class specified in the constants file does not implement the IPlotDataAccess interface, and therefore is NOT compatible with the plotter.";
 }
