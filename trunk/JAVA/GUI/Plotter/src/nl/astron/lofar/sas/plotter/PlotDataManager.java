885f695188e45fbf688f92c0106db296aa042d6c
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 14:21:36 +0000

    701 Additions: - None Mods: - Removed test data from retrieveData method, instead it loads the default IPlotDataAccess implementation class and calls its retrieveData method

--- trunk/JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotDataManager.java
+++ trunk/JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotDataManager.java
@@ -10,7 +10,11 @@
 package nl.astron.lofar.sas.plotter;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import nl.astron.lofar.sas.plotter.exceptions.NotImplementedException;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterDataAccessorInitializationException;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterDataAccessorNotCompatibleException;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterDataAccessorNotFoundException;
+import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
 
 /**
  * @created 11-04-2006, 15:00
@@ -19,110 +23,69 @@ import java.util.HashSet;
  * @updated 13-apr-2006 11:19:42
  */
 public class PlotDataManager{
-
-        private static PlotDataManager instance = null;
-	//private ParmDB m_ParmDB;
-
-	public static PlotDataManager getInstance(){
-           if(instance == null){
-              instance = new PlotDataManager();    
-           }
-           return instance;
+    
+    private static PlotDataManager instance = null;
+    //private ParmDB m_ParmDB;
+    
+    public static PlotDataManager getInstance(){
+        if(instance == null){
+            instance = new PlotDataManager();
         }
-      
-        private PlotDataManager(){
- 
-	}
+        return instance;
+    }
+    
+    private PlotDataManager(){
         
-
-	public void finalize() throws Throwable {
-            instance = null;
-	}
-
-	/**
-	 * @param constraint
-	 * 
-	 */
-	public HashMap retrieveData(String constraint){
-            //create the hashmap to be returned
-            HashMap<String,Object> data = new HashMap<String,Object>();
+    }
+    
+    
+    public void finalize() throws Throwable {
+        instance = null;
+    }
+    
+    /**
+     * @param constraint
+     *
+     */
+    public HashMap retrieveData(String constraint) throws PlotterException{
+        Object aClass = null;
+        IPlotDataAccess aDataAccessor = null;
+        try {
+            Class implementator = PlotDataManager.class.forName(PlotConstants.DATA_ACCESS_CLASS);
+            aClass = implementator.newInstance();
+            aDataAccessor = (IPlotDataAccess)aClass;
+        } catch (IllegalAccessException ex) {
+            //TODO Log!
+            throw new PlotterDataAccessorInitializationException();
+        } catch (ClassNotFoundException ex) {
+            //TODO Log!
+            throw new PlotterDataAccessorNotFoundException();
+        } catch (InstantiationException ex) {
+            //TODO Log!
+            throw new PlotterDataAccessorInitializationException();
+        } catch (ClassCastException ex) {
+            //TODO LOG!
+            throw new PlotterDataAccessorNotCompatibleException();
+        }
+        
+        if(aDataAccessor != null){
             
-            String plotTitle = "Testset LOFAR Plotter";
-            data.put(PlotConstants.DATASET_NAME,plotTitle);
-            String plotSubTitle = "Using dummy data";
-            data.put(PlotConstants.DATASET_SUBNAME,plotSubTitle);
-            String xAxisTitle = "t";
-            data.put(PlotConstants.DATASET_XAXISLABEL,xAxisTitle);
+            HashMap retrieveableData =
+                    aDataAccessor.retrieveData(constraint);
             
-            String xAxisUnits = "seconds";
-            data.put(PlotConstants.DATASET_XAXISUNIT,xAxisUnits);
+            return retrieveableData;
             
-            String yAxisTitle = "Frequency";
-            data.put(PlotConstants.DATASET_YAXISLABEL,yAxisTitle);
-            
-            String yAxisUnits = "MHz";
-            data.put(PlotConstants.DATASET_YAXISUNIT,yAxisUnits);
-                        
-            HashSet<HashMap> values = new HashSet<HashMap>();
-            
-            HashMap<String,Object> aLine = new HashMap<String,Object>();
-            
-            aLine.put(PlotConstants.DATASET_VALUELABEL,"Lijntje 1");
-             
-             double[] xArray = new double[10];
-             double[] yArray = new double[10];
-             
-             for(int i = 0;i<10;i++){
-                 xArray[i] = Math.random()*10;
-                 yArray[i] = Math.random()*100;
-             }
-             
-             aLine.put(PlotConstants.DATASET_XVALUES,xArray);
-             aLine.put(PlotConstants.DATASET_YVALUES,yArray);            
-             values.add(aLine);
-             
-             HashMap<String,Object> aLine2 = new HashMap<String,Object>();
-            
-             aLine2.put(PlotConstants.DATASET_VALUELABEL,"Lijntje 2");
-             double[] xArray2 = new double[10];
-             double[] yArray2 = new double[10];
-             
-             xArray2[0] = 1;
-             xArray2[1] = 2;
-             xArray2[2] = 3;
-             xArray2[3] = 4;
-             xArray2[4] = 5;
-             xArray2[5] = 6;
-             xArray2[6] = 7;
-             xArray2[7] = 8;
-             xArray2[8] = 9;
-             xArray2[9] = 10;
-             
-             yArray2[0] = xArray2[0]*xArray2[0];
-             yArray2[1] = xArray2[1]*xArray2[1];
-             yArray2[2] = xArray2[2]*xArray2[2];
-             yArray2[3] = xArray2[3]*xArray2[3];
-             yArray2[4] = xArray2[4]*xArray2[4];
-             yArray2[5] = xArray2[5]*xArray2[5];
-             yArray2[6] = xArray2[6]*xArray2[6];
-             yArray2[7] = xArray2[7]*xArray2[7];
-             yArray2[8] = xArray2[8]*xArray2[8];
-             yArray2[9] = xArray2[9]*xArray2[9];
-                                       
-             aLine2.put(PlotConstants.DATASET_XVALUES,xArray2);
-             aLine2.put(PlotConstants.DATASET_YVALUES,yArray2);            
-             values.add(aLine2);    
-             data.put(PlotConstants.DATASET_VALUES,values);
-            return data; 
-	}
-
-	/**
-	 * @param rootParams
-	 * @param data
-	 * 
-	 */
-	public void exportRoot(String[] rootParams, HashMap data){
-
-	}
-
+        }
+        return null;
+    }
+    
+    /**
+     * @param rootParams
+     * @param data
+     *
+     */
+    public void exportData(String[] rootParams, HashMap data) throws PlotterException{
+        throw new NotImplementedException("Export of data is not yet implemented in this release.");
+    }
+    
 }
