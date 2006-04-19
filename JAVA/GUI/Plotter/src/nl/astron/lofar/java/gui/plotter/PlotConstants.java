84547c18967767f80f507fd8d04404ebcf68aa67
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-19 07:46:23 +0000

    701 Additions: - Added exception package strings Mods: -Improved naming for dataset keys.

--- JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotConstants.java
+++ JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotConstants.java
@@ -19,24 +19,31 @@ public abstract class PlotConstants{
     //Data Model constants
     
     //Data Model Metadata
-    public static final String DATASETNAME    = "DataSetName";
-    public static final String DATASETSUBNAME = "DataSetSubName";
-    public static final String DATASETXAXISLABEL = "DataSetXAxisLabel";
-    public static final String DATASETXAXISUNIT  = "DataSetXAxisUnit";
-    public static final String DATASETYAXISLABEL = "DataSetYAxisLabel";
-    public static final String DATASETYAXISUNIT  = "DataSetYAxisUnit";
-    public static final String DATASETZAXISLABEL = "DataSetZAxisLabel";
-    public static final String DATASETZAXISUNIT = "DataSetZAxisUnit";
-    public static final String DATASETVALUES = "DataSetValues";
+    public static final String DATASET_NAME    = "DataSetName";
+    public static final String DATASET_SUBNAME = "DataSetSubName";
+    public static final String DATASET_XAXISLABEL = "DataSetXAxisLabel";
+    public static final String DATASET_XAXISUNIT  = "DataSetXAxisUnit";
+    public static final String DATASET_YAXISLABEL = "DataSetYAxisLabel";
+    public static final String DATASET_YAXISUNIT  = "DataSetYAxisUnit";
+    public static final String DATASET_ZAXISLABEL = "DataSetZAxisLabel";
+    public static final String DATASET_ZAXISUNIT = "DataSetZAxisUnit";
+    public static final String DATASET_VALUES = "DataSetValues";
     
     //Data Model ValueSets
     
-    public static final String XVALUES = "XValues";
-    public static final String YVALUES = "YValues";
-    public static final String ZVALUES = "ZValues";
-    public static final String VALUELABEL = "DataSetValueLabel";
+    public static final String DATASET_XVALUES = "XValues";
+    public static final String DATASET_YVALUES = "YValues";
+    public static final String DATASET_ZVALUES = "ZValues";
+    public static final String DATASET_VALUELABEL = "DataSetValueLabel";
     
     //Supported plotter framework
     public static final String FRAMEWORK = "nl.astron.lofar.sas.plotter.SGTPlot";
 
+    //Exception messages
+    public static final String EXCEPTION_GENERIC = "An internal error occurred with no leads to its cause. Please contact software support.";
+    public static final String EXCEPTION_EMPTYDATASET = "An internal error occurred : An empty data set was passed to the plotter framework.";
+    public static final String EXCEPTION_FRAMEWORKINIT = "An internal error occurred : The plotter framework class specified in the constants file could not be accessed and/or instantiated by the plotter.";
+    public static final String EXCEPTION_FRAMEWORKNOTFOUND = "An internal error occurred : The plotter framework class specified in the constants file could not be found by the plotter.";
+    public static final String EXCEPTION_FRAMEWORKNOTCOMPATIBLE = "An internal error occurred : The plotter framework class specified in the constants file does not implement the IPlot interface, and therefore is NOT compatible with the plotter.";
+    
 }
