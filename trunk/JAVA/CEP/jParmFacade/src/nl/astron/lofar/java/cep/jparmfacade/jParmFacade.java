9d913b7044cb4df7b447a7303c4a2873f4b3e4fd
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-21 12:01:31 +0000

    BugID: 703 JAVA Interface to ParmDB class

---
+++ trunk/JAVA/CEP/jParmFacade/src/nl/astron/lofar/java/cep/jparmfacade/jParmFacade.java
@@ -0,0 +1,45 @@
+/*
+ * jParmFacade.java  JNI Interface base class for the CEP/BB/ParmFacade class
+ *
+ * Created on April 18, 2006, 9:54 AM
+ *
+ */
+
+package nl.astron.lofar.java.cep.jparmfacade;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ *
+ * @author coolen
+ */
+public class jParmFacade {
+    
+    /** Creates a new instance of jParmFacade */
+    public jParmFacade(String tableName) {
+        initParmFacade(tableName);
+    }
+    
+    // Make a connection to the given ParmTable.
+    private native void initParmFacade (String tableName);
+    
+    // Get the domain range (as startx,endx,starty,endy) of the given
+    // parameters in the table.
+    // This is the minimum start value and maximum end value for all parameters.
+    // An empty name pattern is the same as * (all parm names).
+    public native Vector<Double> getRange(String parmNamePattern);
+    
+    // Get parameter names in the table matching the pattern.
+    // An empty name pattern is the same as * (all parm names).
+    public native Vector<String> getNames(String parmNamePattern);
+    
+
+    // Get the parameter values for the given parameters and domain.
+    // The domain is given by the start and end values, while the grid is
+    // given by nx and ny.
+    public native HashMap<String,Vector<Double>> getValues(String parmNamePattern,
+            double startx, double endx, int nx,
+            double starty, double endy, int ny);
+    
+}
