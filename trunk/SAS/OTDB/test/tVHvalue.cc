1ac0fa55d00dfddbe9dc1a28c87d0064443376e3
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-24 14:52:49 +0000

    BugID:  664 Correction in tVHvalue test.

--- trunk/SAS/OTDB/test/tVHvalue.cc
+++ trunk/SAS/OTDB/test/tVHvalue.cc
@@ -242,8 +242,8 @@ int main (int	argc, char*	argv[]) {
 		ASSERTSTR(tv.addKVTparamSet(aPS), "Could NOT add the OTDBvalue class");
 		LOG_INFO("ParameterSet added, going to query it");
 
-		LOG_INFO ("Searching Node: Observation.Virt Telescope%");
-		vector<OTDBnode>	nodeList=tm.getItemList(treeID, "Observation.Virt Telescope%");
+		LOG_INFO ("Searching Node: Observation.Virt Telescope[4]%");
+		vector<OTDBnode>	nodeList=tm.getItemList(treeID, "Observation.Virt Telescope[4]%");
 		showNodeList(nodeList);
 
 		nodeIDType		nodeID = nodeList[0].nodeID();
