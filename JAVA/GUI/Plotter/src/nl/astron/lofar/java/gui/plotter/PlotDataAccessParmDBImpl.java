eea38b7f487792d7d63ad5ded542602a363839d5
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-24 15:02:20 +0000

    701 Additions: - Initial version containing a sample ParmDB call (for testing purposes) Mods: - None

--- JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotDataAccessParmDBImpl.java
+++ JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotDataAccessParmDBImpl.java
@@ -10,6 +10,9 @@
 package nl.astron.lofar.sas.plotter;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
+import nl.astron.lofar.java.cep.jparmfacade.jParmFacade;
 import nl.astron.lofar.sas.plotter.exceptions.NotImplementedException;
 import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
 
@@ -21,14 +24,20 @@ import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
  */
 public class PlotDataAccessParmDBImpl implements IPlotDataAccess{
 
+        private jParmFacade parmDB = null;
        
+        static {
+           System.loadLibrary("jparmfacade");
+        }
+        
+        
         public PlotDataAccessParmDBImpl(){
- 
-	}
+            parmDB = new jParmFacade(PlotConstants.DATA_INMEP_FILE_PATH);
+        }
         
 
 	public void finalize() throws Throwable {
-          
+           parmDB = null;
 	}
 
 	/**
@@ -36,6 +45,44 @@ public class PlotDataAccessParmDBImpl implements IPlotDataAccess{
 	 * 
 	 */
 	public HashMap retrieveData(String constraint) throws PlotterException{
-           throw new NotImplementedException("ParmDB Implementation not fully implemented"); 
+           
+            HashMap<String,Object> returnMap = new HashMap<String, Object>();
+            
+            Vector names = parmDB.getNames(constraint);
+            System.out.println("Parameter Names found for constraint "+constraint+ ": " +names.size()+"x"); 
+            
+            if(names.size()==1){
+                 Vector paramValues = parmDB.getRange(names.get(0).toString());
+                 System.out.println("Parameter Range Values found: "+paramValues.size()+"x"); 
+                 double startx = 0.0;
+                 double endx = 0.0;
+                 double starty = 0.0;
+                 double endy = 0.0;
+                 
+                 if(paramValues.size()==4){
+                     startx = Double.parseDouble(paramValues.get(0).toString());
+                     endx =Double.parseDouble(paramValues.get(1).toString());
+                     starty = Double.parseDouble(paramValues.get(2).toString());
+                     endy = Double.parseDouble(paramValues.get(3).toString());
+                     System.out.println("Parameter Range : startx:"+startx+" endx:" +endx+ " starty: "+starty+" endy:"+endy ); 
+                 }
+                 HashMap<String,Vector<Double>> values = parmDB.getValues(constraint,startx,endx,5,starty,endy,10);
+                 
+                 Iterator anIterator = values.keySet().iterator();
+                 while(anIterator.hasNext()){
+                     String aValue = (String)anIterator.next();
+                     System.out.println("Parameter Value Found: "+aValue);
+                     Vector<Double> valueDoubles = (Vector<Double>)values.get(aValue);
+                     System.out.println("Parameter doubles inside " +aValue+": "+valueDoubles.size()+"x");
+                     
+                     for(int i = 0;(i<valueDoubles.size());i++){
+                        System.out.println("Parameter double inside " +aValue+": "+valueDoubles.get(i)+" ");
+                       
+                     }
+                 }
+                 
+            }
+            
+            throw new NotImplementedException("WARNING! ParmDB Implementation not fully completed"); 
 	}
 }
