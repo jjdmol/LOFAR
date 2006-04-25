73f7348d19e71edc6971b0adb3b8a1f06ace9ed7
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-25 14:29:15 +0000

    701 Additions: -Adding return value for retrieveData method Mods: -First implementation of the retrieveData method, using test logic for now. Not thoroughly tested and not final!

--- JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotDataAccessParmDBImpl.java
+++ JAVA/GUI/Plotter/src/nl/astron/lofar/sas/plotter/PlotDataAccessParmDBImpl.java
@@ -9,11 +9,14 @@
 
 package nl.astron.lofar.sas.plotter;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.TimeZone;
 import java.util.Vector;
 import nl.astron.lofar.java.cep.jparmfacade.jParmFacade;
-import nl.astron.lofar.sas.plotter.exceptions.NotImplementedException;
 import nl.astron.lofar.sas.plotter.exceptions.PlotterException;
 
 /**
@@ -47,42 +50,84 @@ public class PlotDataAccessParmDBImpl implements IPlotDataAccess{
 	public HashMap retrieveData(String constraint) throws PlotterException{
            
             HashMap<String,Object> returnMap = new HashMap<String, Object>();
+            HashSet<HashMap> returnValues = new HashSet<HashMap>();
             
             Vector names = parmDB.getNames(constraint);
-            System.out.println("Parameter Names found for constraint "+constraint+ ": " +names.size()+"x"); 
+            //System.out.println("Parameter Names found for constraint "+constraint+ ": " +names.size()+"x"); 
             
-            if(names.size()==1){
-                 Vector paramValues = parmDB.getRange(names.get(0).toString());
-                 System.out.println("Parameter Range Values found: "+paramValues.size()+"x"); 
-                 double startx = 0.0;
-                 double endx = 0.0;
-                 double starty = 0.0;
-                 double endy = 0.0;
-                 
-                 if(paramValues.size()==4){
-                     startx = Double.parseDouble(paramValues.get(0).toString());
-                     endx =Double.parseDouble(paramValues.get(1).toString());
-                     starty = Double.parseDouble(paramValues.get(2).toString());
-                     endy = Double.parseDouble(paramValues.get(3).toString());
-                     System.out.println("Parameter Range : startx:"+startx+" endx:" +endx+ " starty: "+starty+" endy:"+endy ); 
-                 }
-                 HashMap<String,Vector<Double>> values = parmDB.getValues(constraint,startx,endx,5,starty,endy,10);
-                 
-                 Iterator anIterator = values.keySet().iterator();
-                 while(anIterator.hasNext()){
-                     String aValue = (String)anIterator.next();
-                     System.out.println("Parameter Value Found: "+aValue);
-                     Vector<Double> valueDoubles = (Vector<Double>)values.get(aValue);
-                     System.out.println("Parameter doubles inside " +aValue+": "+valueDoubles.size()+"x");
+            
+            if(names.size()>=1){
+                returnMap.put(PlotConstants.DATASET_NAME,"Parameter Database Filter : "+constraint);
+                TimeZone utcZone = TimeZone.getTimeZone("UTC");
+                utcZone.setDefault(utcZone);
+                Calendar aCalendar = Calendar.getInstance(utcZone);
+                Date utcDate = aCalendar.getTime();
+                
+                returnMap.put(PlotConstants.DATASET_SUBNAME,"Generated at "+ utcDate.toString());
+
+                for(int n = 0; n < names.size();n++){
                      
-                     for(int i = 0;(i<valueDoubles.size());i++){
-                        System.out.println("Parameter double inside " +aValue+": "+valueDoubles.get(i)+" ");
-                       
+                     Vector paramValues = parmDB.getRange(names.get(n).toString());
+                     System.out.println("Parameter Range Values found: "+paramValues.size()+"x"); 
+                     double startx = 0;
+                     double endx = 5;
+                     double starty = 0;
+                     double endy = 5;
+                     int numx = 10;
+                     int numy = 10;
+                     /*
+                     if(paramValues.size()==4){
+                         startx = Double.parseDouble(paramValues.get(0).toString());
+                         endx =Double.parseDouble(paramValues.get(1).toString());
+                         starty = Double.parseDouble(paramValues.get(2).toString());
+                         endy = Double.parseDouble(paramValues.get(3).toString());
+                         System.out.println("Parameter Range : startx:"+startx+" endx:" +endx+ " starty: "+starty+" endy:"+endy ); 
+                     }*/
+                                  
+                     returnMap.put(PlotConstants.DATASET_XAXIS_RANGE_START,Double.toString(startx));
+                     returnMap.put(PlotConstants.DATASET_XAXIS_RANGE_END,Double.toString(endx));
+                     returnMap.put(PlotConstants.DATASET_YAXIS_RANGE_START,Double.toString(starty));
+                     returnMap.put(PlotConstants.DATASET_YAXIS_RANGE_END,Double.toString(endy));
+                     
+                     HashMap<String,Vector<Double>> values = parmDB.getValues((String)names.get(n),startx,endx,numx,starty,endy,numy);
+                     //HashMap<String,Vector<Double>> values = parmDB.getValues((String)names.get(n),1,5,numx,1,5,numy);
+
+                     Iterator anIterator = values.keySet().iterator();
+                     while(anIterator.hasNext()){
+                         HashMap<String,Object> aValueMap = new HashMap<String,Object>();
+
+                         String aValue = (String)anIterator.next();
+                         System.out.println("Parameter Value Found: "+aValue);
+
+                         aValueMap.put(PlotConstants.DATASET_VALUELABEL,aValue);
+
+                         Vector<Double> valueDoubles = (Vector<Double>)values.get(aValue);
+                         System.out.println("Parameter doubles inside " +aValue+": "+valueDoubles.size()+"x");
+                         double[] xArray = new double[valueDoubles.size()];
+                         double[] yArray = new double[valueDoubles.size()];
+
+                         for(int i = 0;(i<valueDoubles.size());i++){
+                               if(numx > 1){
+                                   xArray[i] = startx + ( (endx-startx) / (numx*i+1) );
+                                   yArray[i] = valueDoubles.get(i);
+                                   System.out.println("xArray["+xArray[i]+"] yArray["+yArray[i]+"]");  
+                               }
+                               else if(numy > 1){
+                                   yArray[i] = starty + ( (endy-starty) / (numy*i+1) );
+                                   xArray[i] = valueDoubles.get(i);
+                                   System.out.println("xArray["+xArray[i]+"] yArray["+yArray[i]+"]");
+                               }
+                               
+                             
+                         }
+                         aValueMap.put(PlotConstants.DATASET_XVALUES,xArray);
+                         aValueMap.put(PlotConstants.DATASET_YVALUES,yArray);
+                         returnValues.add(aValueMap);
                      }
-                 }
-                 
+                     
+                }
+                returnMap.put(PlotConstants.DATASET_VALUES,returnValues);
             }
-            
-            throw new NotImplementedException("WARNING! ParmDB Implementation not fully completed"); 
+            return returnMap; 
 	}
 }
