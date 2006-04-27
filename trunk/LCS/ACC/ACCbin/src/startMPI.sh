e312c9bd250d61a1a96d1964aa0536f51f8e60e4
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-04-27 12:51:02 +0000

    BugID: 707
    Restructured creation of parameterSet for child processes. This was needed for
    BG/L jobs

--- trunk/LCS/ACC/ACCbin/src/startMPI.sh
+++ trunk/LCS/ACC/ACCbin/src/startMPI.sh
@@ -1,11 +1,10 @@
-# startMPI.sh mpi_variant number_of_processes machinefile procID executable paramfile
+# startMPI.sh jobName machinefile executable paramfile noNodes
 #
-# $1 mpi_variant         mpich or scampi
-# $2 number_of_processes number
-# $3 machinefile         procID.machinefile
-# $4 procID              ???
-# $5 executable          processname
-# $6 parameterfile       procID.ps
+# $1 jobName             identifier for this job
+# $2 machinefile         procID.machinefile
+# $3 executable          processname
+# $4 parameterfile       procID.ps
+# $5 numberOfNodes
 #
 # calls mpirun and remembers the pid
 #
@@ -13,14 +12,10 @@
 # now all ACC processes expect to be started with ACC as first parameter
 
 # start process
-# TODO: in future something like: rsh $1 start_script $2 $3 $4
-if [ "$1" = "scampi" ]; then
-    echo "mpirun -np $2 -machinefile $3 $5 ACC $6 " > startMPI.output
-    ( ( mpirun -np $2 -machinefile $3 $5 ACC $6 ) &> $4.output ) &
-fi
-
-if [ "$1" = "mpich" ]; then
-  ( mpirun_mpich -np $2 -mf $3 $5 ACC $6 ) &
+# TODO: on some hosts, mpirun has a different name (or a specific path)
+#       on some hosts, we should use -hostfile instead of -machinefile
+echo "mpirun -np $5 -machinefile $2 $3 ACC $4 " > startMPI.output
+( ( mpirun -np $5 -machinefile $2 $3 ACC $4 ) &> $1.output ) &
 fi
 
 # get its pid
--- trunk/LCS/ACC/ACCbin/src/stopBGL.sh
+++ trunk/LCS/ACC/ACCbin/src/stopBGL.sh
@@ -1,9 +1,9 @@
-# stopAP.sh partition procID 
+# stopAP.sh partition jobName
 #
 # partition BG/L partition the job is running on
-# procID    The name of the job
+# jobName   The name of the job
 #
 
-echo -n "Killing job " procID
+echo -n "Killing job " $2
 killjob $1 `cat $2.jobID`
 rm -f $2.pid $2.ps
