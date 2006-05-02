a21b5288a7d1c900d7573bbaa4d77a0bedba9dc8
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-05-02 14:12:57 +0000

    BugID: 703
    Exception handling added.

--- trunk/JAVA/CEP/jParmFacade/src/nl_astron_lofar_java_cep_jparmfacade_jParmFacade.cc
+++ trunk/JAVA/CEP/jParmFacade/src/nl_astron_lofar_java_cep_jparmfacade_jParmFacade.cc
@@ -27,6 +27,7 @@
 #include <jParmFacade/nl_astron_lofar_java_cep_jparmfacade_jParmFacade.h>
 #include <ParmFacade/ParmFacade.h>
 #include <iostream>
+#include <string>
 
 using namespace LOFAR::ParmDB;
 using namespace std;
@@ -46,8 +47,8 @@ JNIEXPORT void JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_ini
     theirPF = new ParmFacade(t);
   } catch (exception &ex) {
     cout << "Exception during new ParmFacade(" << t << ") : "<< ex.what() << endl;
+    
     env->ThrowNew(env->FindClass("java/lang/Exception"),ex.what());
-    //    env->ExceptionClear();
   }
   env->ReleaseStringUTFChars(tableName,tablename);
 }
@@ -60,38 +61,40 @@ JNIEXPORT void JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_ini
 JNIEXPORT jobject JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_getRange  (JNIEnv *env, jobject, jstring parmNamePattern) {
 
   jboolean isCopy;
+  jobject rangeVector;
+
   const char* pattern = env->GetStringUTFChars (parmNamePattern, &isCopy);
   vector<double> rangeList;
   try {
     rangeList = theirPF->getRange(pattern);
 
+    env->ReleaseStringUTFChars (parmNamePattern, pattern);
+
+    vector<double>::iterator rangeIterator;
+    
+    // Construct java Vector
+    jclass class_Vector = env->FindClass("java/util/Vector");
+    jmethodID mid_Vector_cons = env->GetMethodID(class_Vector, "<init>", "()V");
+    rangeVector = env->NewObject(class_Vector, mid_Vector_cons);
+    jmethodID mid_Vector_add = env->GetMethodID(class_Vector, "add", "(Ljava/lang/Object;)Z");
+    
+    // Double
+    jobject jDouble;
+    jclass class_Double = env->FindClass ("java/lang/Double");
+    jmethodID mid_Double_cons = env->GetMethodID (class_Double, "<init>", "(D)V");
+    
+    for (rangeIterator = rangeList.begin(); rangeIterator != rangeList.end(); rangeIterator++) {
+      jDouble = env->NewObject (class_Double, mid_Double_cons, *rangeIterator);
+      
+      env->CallObjectMethod(rangeVector, mid_Vector_add, jDouble);
+    }
   } catch (exception &ex) {
+    string aStr= (string)ex.what();
     cout << "Exception during getRange("<< pattern << "): "<< ex.what() << endl;
-    env->ThrowNew(env->FindClass("java/lang/Exception"),ex.what());
-    //    env->ExceptionClear();
+    env->ThrowNew(env->FindClass("java/lang/Exception"),aStr.c_str());
   }
   
-  env->ReleaseStringUTFChars (parmNamePattern, pattern);
 
-  vector<double>::iterator rangeIterator;
-
-  // Construct java Vector
-  jobject rangeVector;
-  jclass class_Vector = env->FindClass("java/util/Vector");
-  jmethodID mid_Vector_cons = env->GetMethodID(class_Vector, "<init>", "()V");
-  rangeVector = env->NewObject(class_Vector, mid_Vector_cons);
-  jmethodID mid_Vector_add = env->GetMethodID(class_Vector, "add", "(Ljava/lang/Object;)Z");
-
-  // Double
-  jobject jDouble;
-  jclass class_Double = env->FindClass ("java/lang/Double");
-  jmethodID mid_Double_cons = env->GetMethodID (class_Double, "<init>", "(D)V");
-
-  for (rangeIterator = rangeList.begin(); rangeIterator != rangeList.end(); rangeIterator++) {
-    jDouble = env->NewObject (class_Double, mid_Double_cons, *rangeIterator);
-
-    env->CallObjectMethod(rangeVector, mid_Vector_add, jDouble);
-  }
   return rangeVector;
 }
 
@@ -104,32 +107,34 @@ JNIEXPORT jobject JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_
 JNIEXPORT jobject JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_getNames  (JNIEnv *env, jobject, jstring parmNamePattern) {
 
   jboolean isCopy;
+  jobject nameVector;
+
   const char* pattern = env->GetStringUTFChars (parmNamePattern, &isCopy);
   vector<string> nameList;
   try {
     nameList = theirPF->getNames(pattern);
+
+    env->ReleaseStringUTFChars (parmNamePattern, pattern);
+
+    vector<string>::iterator nameIterator;
+
+    // Construct java Vector
+    jclass class_Vector = env->FindClass("java/util/Vector");
+    jmethodID mid_Vector_cons = env->GetMethodID(class_Vector, "<init>", "()V");
+    nameVector = env->NewObject(class_Vector, mid_Vector_cons);
+    jmethodID mid_Vector_add = env->GetMethodID(class_Vector, "add", "(Ljava/lang/Object;)Z");
+
+    jstring jstr;
+    for (nameIterator = nameList.begin(); nameIterator != nameList.end(); nameIterator++) {
+      jstr = env->NewStringUTF (((string)*nameIterator).c_str());
+      env->CallObjectMethod(nameVector, mid_Vector_add, jstr);
+    }
   } catch (exception &ex) {
+    string aStr= (string)ex.what();
     cout << "Exception during getNames("<< pattern << "): "<< ex.what() << endl;
-    env->ThrowNew(env->FindClass("java/lang/Exception"),ex.what());
-    //    env->ExceptionClear();
+    env->ThrowNew(env->FindClass("java/lang/Exception"),aStr.c_str());
   }
 
-  env->ReleaseStringUTFChars (parmNamePattern, pattern);
-
-  vector<string>::iterator nameIterator;
-
-  // Construct java Vector
-  jobject nameVector;
-  jclass class_Vector = env->FindClass("java/util/Vector");
-  jmethodID mid_Vector_cons = env->GetMethodID(class_Vector, "<init>", "()V");
-  nameVector = env->NewObject(class_Vector, mid_Vector_cons);
-  jmethodID mid_Vector_add = env->GetMethodID(class_Vector, "add", "(Ljava/lang/Object;)Z");
-
-  jstring jstr;
-  for (nameIterator = nameList.begin(); nameIterator != nameList.end(); nameIterator++) {
-    jstr = env->NewStringUTF (((string)*nameIterator).c_str());
-    env->CallObjectMethod(nameVector, mid_Vector_add, jstr);
-  }
   return nameVector;
 }
 
@@ -143,66 +148,70 @@ JNIEXPORT jobject JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_
 JNIEXPORT jobject JNICALL Java_nl_astron_lofar_java_cep_jparmfacade_jParmFacade_getValues (JNIEnv *env, jobject, jstring parmNamePattern, jdouble startx, jdouble endx, jint nx, jdouble starty, jdouble endy, jint ny) {
 
   jboolean isCopy;
+  jobject result;
+
   const char* pattern = env->GetStringUTFChars (parmNamePattern, &isCopy);
   map<string,vector<double> > valMap;
   try {
     valMap = theirPF->getValues(pattern,startx,endx,nx,starty,endy,ny);
-  } catch (exception ex) {
+    env->ReleaseStringUTFChars (parmNamePattern, pattern);
+
+    // Construct java Map
+    jclass mapClass, doubleClass, vectorClass;
+    jmethodID mapInit, mapPut, vectorAdd, doubleInit, vectorInit;
+    
+    mapClass = env->FindClass("java/util/HashMap");
+    mapInit = env->GetMethodID(mapClass, "<init>", "()V");
+    result = env->NewObject(mapClass, mapInit);
+    mapPut= env->GetMethodID(mapClass, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+    
+    // Construct java Double
+    jobject jDouble;
+    doubleClass = env->FindClass ("java/lang/Double");
+    doubleInit = env->GetMethodID (doubleClass, "<init>", "(D)V");
+    
+    
+    // Loop through map and convert to HashMap
+    
+    for (map<string,vector<double> >::const_iterator valIter=valMap.begin();
+         valIter != valMap.end();
+         valIter++) {
+      
+      // Construct java Vector
+      jobject valVector;
+      vectorClass = env->FindClass("java/util/Vector");
+      vectorInit = env->GetMethodID(vectorClass, "<init>", "()V");
+      valVector = env->NewObject(vectorClass, vectorInit);
+      vectorAdd = env->GetMethodID(vectorClass, "add", "(Ljava/lang/Object;)Z");
+      
+      
+      for (vector<double>::const_iterator iter=valIter->second.begin();
+	   iter != valIter->second.end();
+	   iter++) {
+	
+        jDouble = env->NewObject (doubleClass, doubleInit, *iter);
+	
+	env->CallObjectMethod(valVector, vectorAdd, jDouble);
+      }
+      
+      
+      env->CallObjectMethod(result, mapPut, env->NewStringUTF(valIter->first.c_str()),valVector);
+      
+    }
+  } catch (exception &ex) {
     cout << "Exception during getValues("<< pattern << "," << startx << ","
 	 << endx << "," << nx << "," << starty << "," << endy << "," << ny 
 	 << ") : "<< ex.what() << endl;
-    env->ThrowNew(env->FindClass("java/lang/Exception"),ex.what());
-    //    env->ExceptionClear();
-  }
-
-  env->ReleaseStringUTFChars (parmNamePattern, pattern);
-
-
-
-  // Construct java Map
-  jobject result;
-  jclass mapClass, doubleClass, vectorClass;
-  jmethodID mapInit, mapPut, vectorAdd, doubleInit, vectorInit;
-    
-  mapClass = env->FindClass("java/util/HashMap");
-  mapInit = env->GetMethodID(mapClass, "<init>", "()V");
-  result = env->NewObject(mapClass, mapInit);
-  mapPut= env->GetMethodID(mapClass, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
-  
-  // Construct java Double
-  jobject jDouble;
-  doubleClass = env->FindClass ("java/lang/Double");
-  doubleInit = env->GetMethodID (doubleClass, "<init>", "(D)V");
-
-
-  // Loop through map and convert to HashMap
-
-  for (map<string,vector<double> >::const_iterator valIter=valMap.begin();
-         valIter != valMap.end();
-         valIter++) {
-
-    // Construct java Vector
-    jobject valVector;
-    vectorClass = env->FindClass("java/util/Vector");
-    vectorInit = env->GetMethodID(vectorClass, "<init>", "()V");
-    valVector = env->NewObject(vectorClass, vectorInit);
-    vectorAdd = env->GetMethodID(vectorClass, "add", "(Ljava/lang/Object;)Z");
-
-
-      for (vector<double>::const_iterator iter=valIter->second.begin();
-         iter != valIter->second.end();
-         iter++) {
-	
-        jDouble = env->NewObject (doubleClass, doubleInit, *iter);
-
-	env->CallObjectMethod(valVector, vectorAdd, jDouble);
-      }
-
-
-      env->CallObjectMethod(result, mapPut, env->NewStringUTF(valIter->first.c_str()),valVector);
-	
+    string aStr= (string)ex.what();
+    jclass newExcCls = env->FindClass("java/lang/Exception");
+    if (newExcCls == 0) { 
+      cout << "Unable to find the new exception class, give up." << endl;
+      //      env->ReleaseStringUTFChars (parmNamePattern, pattern);
+      return result;
     }
- 
+    env->ThrowNew(newExcCls,aStr.c_str());
+  }
+  
   return result;
 }
 
