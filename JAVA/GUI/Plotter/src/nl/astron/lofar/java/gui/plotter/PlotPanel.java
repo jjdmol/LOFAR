5535b931d28dc222668103776cc4194a42ca3f33
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-05-04 14:35:34 +0000

    701 Additions: - separateLegend boolean now available in the interface for getPlot() Mods: - Javadoc improvements

--- JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotPanel.java
+++ JAVA/GUI/Plotter/src/nl/astron/lofar/java/gui/plotter/PlotPanel.java
@@ -1,4 +1,4 @@
-/*
+/**
  * PlotPanel.java
  *
  *  Copyright (C) 2002-2007
@@ -44,50 +44,96 @@ public class PlotPanel extends JPanel{
         private JComponent plot;
         private JComponent legend;
         private String[] currentDataConstraint;
-
+        
+        /**
+         * - Constructs a new PlotPanel
+         * - Gets a new PlotController to work with
+         */
 	public PlotPanel(){
             m_PlotController = new PlotController();
             this.setBackground(Color.WHITE);
             this.setLayout(new BorderLayout());
         }
-
+        /**
+         * Finalize method to cleanup instance variables.
+         */
 	public void finalize() throws Throwable {
             plot = null;
             legend = null;
             m_PlotController = null;
             currentDataConstraint = null;
 	}
-
-	public void createPlot(int type, String[] constraints) throws PlotterException{
+        /**
+         * This method will attempt to generate a plot using several key arguments:
+         * -int type (this is defined by PlotConstants.PLOT_*)
+         * -boolean separateLegend (this will tell the plotter that you would like a separate JComponent legend,
+         *                          otherwise you will see a legend where the plotter puts it by default.
+         * -String[] constraints (These are the arguments you need for the data access layer to get the data
+         *                        you want, which can be anything, as long as your configured data access layer
+         *                        supports it!)
+         * The plot will be added to this class' ContentPane so you can view it directly. It can also be retrieved separately
+         * by calling the getPlot() method. 
+         *                         
+         * @param type The type of plot to be generated (these types are defined in PlotConstants.PLOT_* )
+         * @param separateLegend Tells the plotter if you like a separate legend JComponent, or leave it embedded in the plot itself. Dont forget to call getLegendForPlot() to get the separate legend!
+         * @param constraints The arguments to be passed to the configured data access layer
+         */
+	public void createPlot(int type, boolean separateLegend, String[] constraints) throws PlotterException{
             this.removeAll();
             plot = null;
             legend = null;
             currentDataConstraint = constraints;
-            
-            plot = m_PlotController.createPlot(type,constraints);
+            plot = m_PlotController.createPlot(type, separateLegend, constraints);
             this.add(plot,BorderLayout.CENTER);
         }
-        
-	public Image exportImage(){
-		return null;
+        /**
+         * This method will attempt to generate a plot image using several key arguments:
+         * -int type (this is defined by PlotConstants.PLOT_*)
+         * -String[] constraints (These are the arguments you need for the data access layer to get the data
+         *                        you want, which can be anything, as long as your configured data access layer
+         *                        supports it!)
+         *                         
+         * @param type The type of plot to be generated (these types are defined in PlotConstants.PLOT_* )
+         * @param constraints The arguments to be passed to the configured data access layer
+         * @return The image file which can be saved to a file or used in different software.
+         */
+	public Image exportImage(int type, String[] constraints) throws PlotterException{
+            return m_PlotController.createPlotImage(constraints);
 	}
-
-	public void exportData(){
+        /**
+         * This method will attempt to export the data currently in the plot to your configured data export layer using a single argument:
+         * -String[] arguments (These are the arguments you need for the data export layer to export the data
+         *                        you currently have in the plot, and these strings can mean anything, as long as your configured data export layer
+         *                        supports them!)
+         */
+	public void exportData(String[] arguments) throws PlotterException{
+            m_PlotController.exportData(arguments);
+        }
+         /**
+          * TODO: JavaDoc!
+          */
+	public void modifyDataSelection() throws PlotterException{
 
 	}
-
-	public void modifyDataSelection(){
-
-	}
-
-	public PrintJob printPlot(){
+        /**
+         * This method will attempt to print the current plot
+         */
+	public PrintJob printPlot() throws PlotterException{
             
             return null;
 	}
-        
+        /**
+         * This method will return the plot currently in memory
+         * @return The plot currently in memory
+         */
         public JComponent getPlot(){
             return plot;
         }
+        /**
+         * This method will return the legend for the plot currently in memory.
+         * It will throw a NotSupportedException should the plot not have a separate legend available.
+         * @return The plot legend currently in memory
+         */
         public JComponent getLegendForPlot() throws PlotterException{
             if(legend == null && plot != null){
                legend = m_PlotController.getLegendForPlot(plot);
