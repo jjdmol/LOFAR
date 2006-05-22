7bc1b90467b9e13c4b4354ad94d58d5866acefab
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-05-22 09:44:28 +0000

    BugID: 604 Keeps track of pannels that should be dynamicly placed in a tab window in the browser, based upon the name (This should later ofcourse be obtained from the database...)

---
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/ResultPanelHelper.java
@@ -0,0 +1,79 @@
+/*
+ * ResultPanelHelper.java
+ *
+ * Created on 16 mei 2006, 11:09
+ *
+ * This class maintains a list of available panels that can be shown on a given (VIC)name in the ResultBrowser.
+ * 
+ * 
+ */
+
+package nl.astron.lofar.sas.otb.util;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ *
+ * @author coolen
+ */
+public class ResultPanelHelper {
+   
+    private HashMap<String,Vector<String> > itsPanelMap;
+    private Vector<String> itsVector;
+    
+    /** Creates a new instance of ResultPanelHelper */
+    public ResultPanelHelper() {
+        initMap();
+    }
+    
+    private void initMap() {
+        itsPanelMap = new HashMap<String, Vector<String> >();
+        
+        //generic panels
+        itsVector = new Vector<String>();
+        addBasePanels();
+        itsPanelMap.put("*",itsVector);
+
+        
+        //BBS
+        itsVector = new Vector<String>();
+        addBasePanels();
+        itsVector.add("nl.astron.lofar.sas.otbcomponents.parmDBPlotPanel");
+        itsPanelMap.put("BBS",itsVector);
+        
+        
+    }
+    
+    
+    private void addBasePanels() {
+        //generic logging panel
+        itsVector.add("nl.astron.lofar.sas.otbcomponents.LogParamPanel");
+        //generic node panel
+        itsVector.add("nl.astron.lofar.sas.otbcomponents.NodeViewPanel");
+        //generic parameter panel
+        itsVector.add("nl.astron.lofar.sas.otbcomponents.ParameterViewPanel");
+    }
+    
+    /**
+     * Returns the possible panels for this Key
+     *
+     * @param aKey  the key that you want the panellist for
+     * @return the Vector that contains all panels for this key
+     */
+    public Vector getPanels(String aKey) {
+        return itsPanelMap.get(aKey);
+    }
+    
+    /**
+     * Look if a key available at all.
+     *
+     *@param aKey The key that you want to test
+     *@return true if key was available, false if not
+     */
+    public boolean isKey(String aKey) {
+        return itsPanelMap.containsKey(aKey);
+    }
+    
+    
+}
