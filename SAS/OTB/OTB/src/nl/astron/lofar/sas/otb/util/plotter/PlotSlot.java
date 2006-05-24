6381ac6b5915a4bbcd844de38a49829825cd33f0
pompert <pompert@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-05-24 14:43:50 +0000

    BugID: 701 Additions: -Initial versions of support classes to help implement the SAS/OTB ParmDB plotter

---
+++ SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/plotter/PlotSlot.java
@@ -0,0 +1,130 @@
+/*
+ * PlotSlot.java
+ *
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package nl.astron.lofar.sas.otb.util.plotter;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import nl.astron.lofar.java.gui.plotter.PlotConstants;
+import nl.astron.lofar.java.gui.plotter.PlotPanel;
+import nl.astron.lofar.java.gui.plotter.exceptions.PlotterException;
+
+/**
+ * @version $Id$
+ * @created May 24, 2006, 11:12 AM
+ * @author pompert
+ */
+public class PlotSlot extends JPanel{
+    
+    private PlotGroup itsPlotGroup;
+    private PlotPanel itsPlot;
+    private String slotLabel;
+    
+    /** Creates a new instance of PlotSlot */
+    public PlotSlot() {
+        itsPlotGroup = null;
+        itsPlot = new PlotPanel();
+        slotLabel = "Default Slot";
+        setLayout(new BorderLayout());
+    }
+    /** 
+     * Creates a new instance of PlotSlot using a supplied label
+     * @param name The name to be given to the slot.
+     * 
+     */
+    public PlotSlot(String label) {
+        this();
+        slotLabel = label;
+    }
+    
+    public void setLabel(String label){
+        slotLabel = label;
+    }
+    
+    public String getLabel(){
+        return slotLabel;
+    }
+    
+    public PlotPanel getPlot(){
+        return itsPlot;
+    }
+    
+    public void addPlot(Object constraints){
+        removeAll();
+        try {            
+            itsPlot.createPlot(PlotConstants.PLOT_XYLINE,true,constraints);
+            add(itsPlot,BorderLayout.CENTER);
+        } catch (PlotterException ex) {
+            JTextArea error = new JTextArea(ex.getMessage());
+            error.setColumns(50);
+            add(new JTextArea(ex.getMessage()),BorderLayout.CENTER);
+            ex.printStackTrace();
+        }
+    }
+    
+    public void modifyPlot(Object constraints){
+        if(containsPlot()){
+         
+            try {
+                itsPlot.modifyPlot(constraints);
+                add(itsPlot,BorderLayout.CENTER);
+            } catch (PlotterException ex) {
+                JTextArea error = new JTextArea(ex.getMessage());
+                error.setColumns(50);
+                add(new JTextArea(ex.getMessage()),BorderLayout.CENTER);
+                ex.printStackTrace();
+            }
+
+        }
+    }
+    
+    public void removePlot(){
+        if(containsPlot()){
+            this.remove(itsPlot);
+        }
+        itsPlot = null;
+    }
+    
+    public boolean containsPlot(){
+        return itsPlot != null;
+    }
+    
+    public PlotGroup getPlotGroup(){
+       return itsPlotGroup; 
+    }
+    
+    public void setPlotGroup(PlotGroup aPlotGroup){
+        itsPlotGroup = aPlotGroup;
+    }
+    
+    public void removePlotGroup(){
+        itsPlotGroup = null;
+    }
+    
+    public boolean hasPlotGroup(){
+        return itsPlotGroup != null;
+    }
+    
+    
+}
