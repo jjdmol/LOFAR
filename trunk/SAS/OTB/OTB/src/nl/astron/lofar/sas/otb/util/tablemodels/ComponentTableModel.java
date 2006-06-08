0e8248ecf6a04d55a1008a1b870502bc62b313ad
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-06-08 09:30:24 +0000

    BugId: 742 Gnu license included

--- trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/tablemodels/ComponentTableModel.java
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/tablemodels/ComponentTableModel.java
@@ -1,12 +1,24 @@
 /*
  * ComponentTableModel.java
  *
- * Created on April 4th, 2006, 15:28
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
- */
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 package nl.astron.lofar.sas.otb.util.tablemodels;
 
@@ -18,7 +30,10 @@ import org.apache.log4j.Logger;
 /**
  * Implements the data model behind the component table 
  *
+ * @created 04-04-2006, 15:28
  * @author coolen
+ * @version $Id$
+ * @updated
  */
 public class ComponentTableModel extends javax.swing.table.AbstractTableModel {
     
