68bac2653facae0cafdb02eb76a081fd69cc8bbd
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-06-08 09:17:49 +0000

    BugId: 742 Gnu license imported

--- trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/OtdbRmi.java
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/OtdbRmi.java
@@ -1,11 +1,23 @@
 /*
  * OtdbRmi.java
  *
- * Created on January 16, 2006, 4:33 PM
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package nl.astron.lofar.sas.otb.util;
@@ -21,8 +33,17 @@ import nl.astron.lofar.sas.otbcomponents.SetServerDialog;
 import org.apache.log4j.Logger;
 
 /**
+ * This class provides the interface with the OTDB server.
+ *
+ * @see nl.astron.lofar.sas.otb.jotdb2
+ *
+ * @created 16-01-2006, 16:33
  *
  * @author blaakmeer
+ *
+ * @version $Id$
+ *
+ * @updated
  */
 public class OtdbRmi {
     
