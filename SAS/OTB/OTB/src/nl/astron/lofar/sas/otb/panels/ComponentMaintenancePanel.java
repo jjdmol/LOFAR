d550ffd521348908f14d461feb88636a0c86e7ca
coolen <coolen@astron.nl>
2006-06-08 09:02:35 +0000

    BugId: 742 Gnu license imported

--- SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/panels/ComponentMaintenancePanel.java
+++ SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/panels/ComponentMaintenancePanel.java
@@ -1,7 +1,24 @@
 /*
  * ComponentMaintenancePanel.java
  *
- * Created on 24 januari 2006, 12:47
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 package nl.astron.lofar.sas.otb.panels;
@@ -17,8 +34,13 @@ import nl.astron.lofar.sas.otbcomponents.VICnodeDefViewPanel;
 import org.apache.log4j.Logger;
 
 /**
+ * Class that will show the components to the user, all component actions can be
+ * handled from within this Gui panel.
  *
+ * @created 24-01-2006
  * @author  Coolen
+ * @version $Id$
+ * @updated
  */
 public class ComponentMaintenancePanel extends javax.swing.JPanel
         implements IPluginPanel {
--- SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/panels/TemplateMaintenancePanel.java
+++ SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/panels/TemplateMaintenancePanel.java
@@ -1,7 +1,23 @@
 /*
  * TemplateMaintenancePanel.java
  *
- * Created on March 17, 2006, 1:53 PM
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package nl.astron.lofar.sas.otb.panels;
@@ -30,7 +46,10 @@ import org.apache.log4j.Logger;
  * (status usable, being specified and example).
  * or specifying an instrument configuration for an observation request.
  *
+ * @created 17-03-2006, 13:53
  * @author  Coolen
+ * @version $Id$
+ * @updated
  */
 public class TemplateMaintenancePanel extends javax.swing.JPanel
         implements IPluginPanel {
