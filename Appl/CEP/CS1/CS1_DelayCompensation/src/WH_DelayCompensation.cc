51e5483f971670ab54047a67e84e050aa892608d
Marcel Loose <gmloose@users.noreply.github.com>
2006-06-08 10:43:30 +0000

    BugID: 687
    Putting in the last pieces. Now only need to fill the DataHolders and I guess
    everything will be fine.

--- Appl/CEP/CS1/CS1_DelayCompensation/src/WH_DelayCompensation.cc
+++ Appl/CEP/CS1/CS1_DelayCompensation/src/WH_DelayCompensation.cc
@@ -47,16 +47,19 @@ namespace LOFAR
 
     WH_DelayCompensation::WH_DelayCompensation(const string& name,
                                                const ParameterSet& ps) :
-      WorkHolder(0,                                      // inputs
-                 ps.getUint32("Input.NRSPBoards"),       // outputs
-                 name,                                   // name
-                 "WH_DelayCompensation"),                // type
-      itsParamSet(ps),
+      WorkHolder   (0,                                   // inputs
+                    ps.getUint32("Input.NRSPBoards"),    // outputs
+                    name,                                // name
+                    "WH_DelayCompensation"),             // type
+      itsParamSet  (ps),
+      itsNrBeams   (ps.getUint32("Observation.NBeams")),
+      itsNrStations(ps.getUint32("Observation.NStations")),
+      itsNrDelaysPerEpoch(itsNrBeams*itsNrStations),
       itsSampleRate(ps.getDouble("Observation.SampleRate")),
-      itsConverter(0),
-      itsLoopCount(0)
+      itsLoopCount (0),
+      itsConverter (0)
     {
-      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, name.c_str());
+      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, "");
 
       // Initialize our private data structures.
       getConverterConfig(itsParamSet);
@@ -65,8 +68,8 @@ namespace LOFAR
       getObservationEpoch(itsParamSet);
       setPositionDiffs();
 
-      // Create outgoing dataholders for the delay information; one for each
-      // stations (should be one for each RSP board).
+      // Create outgoing dataholders for the delay information; 
+      // one for each RSP board.
       for (int i = 0; i < itsNoutputs; ++i) {
         string str = "DH_Delay_out_" + formatString("%02d", i);
         LOG_TRACE_LOOP_STR("Creating " << str);
@@ -84,7 +87,7 @@ namespace LOFAR
 
     void WH_DelayCompensation::preprocess()
     {
-      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, getName().c_str());
+      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, "");
 
       itsLoopCount = 0;
 
@@ -92,70 +95,73 @@ namespace LOFAR
       ASSERT(!itsConverter);
       itsConverter = createConverter();
       ASSERT(itsConverter);
+
+      // Pre-allocate and initialize storage for the delay vectors.
+      itsDelaysAtBegin.resize(itsNrDelaysPerEpoch);
+      itsDelaysAfterEnd.resize(itsNrDelaysPerEpoch);
+      
+      // Initialize \c itsDelaysAfterEnd with the conversion results for the
+      // epoch after the end of the first time interval.
+      calculateDelays();
     }
 
 
     void WH_DelayCompensation::process()
     {
-      LOG_TRACE_LIFETIME_STR(TRACE_LEVEL_FLOW, getName() << 
-                             ", loop count: " << itsLoopCount);
-
-      // Each integration period, we must send the new CoarseDelay,
-      // FineDelayAtBegin, and FineDelayAfterEnd data for all stations.
-
-      // For the time being we will call itsConverter->j2000ToItrf() every
-      // cycle. This can be made more efficient by precalculating, say, for 30
-      // integration periods, and only call itsConverter->j2000ToItrf() once
-      // every 30 cycles.
-      RequestData request (itsBeamDirections, itsStationPositions, 
-                           itsObservationEpoch[itsLoopCount]);
-      ResultData result;
-      itsConverter->j2000ToItrf(result, request);
-
-      LOG_TRACE_VAR("Beam directions:");
-      for (uint i = 0; i < itsBeamDirections.size(); ++i) {
-        LOG_TRACE_VAR_STR(" [" << i << "]: " << itsBeamDirections[i]);
-      }
-      LOG_TRACE_VAR("Station positions:");
-      for (uint i = 0; i < itsStationPositions.size(); ++i) {
-        LOG_TRACE_VAR_STR(" [" << i << "]: " << itsStationPositions[i]);
-      }
-      LOG_TRACE_VAR("Observation epochs:");
-      LOG_TRACE_VAR_STR(" [" << itsLoopCount << "]: " << 
-                        itsObservationEpoch[itsLoopCount]);
-      
-      LOG_TRACE_CALC("Conversion result:");
-      for (uint i = 0; i < result.direction.size(); ++i) {
-        LOG_TRACE_CALC_STR(" [" << i << "]: " << result.direction[i]);
-      }
-
-      // First calculate the geometrical delays. Please remember that the
-      // result.direction vector stores the directions per epoch, per
-      // position, per direction. I.e. the first itsBeamDirections.size()
-      // elements contain the converted directions for itsStationPositions[0],
-      // the second for itsStationPositions[1], etc.
-      vector<double> delays(result.direction.size());
-
-      LOG_TRACE_CALC_STR("Geometrical delays:");
-      for (uint i = 0; i < delays.size(); ++i) {
-        uint j = i / itsBeamDirections.size();
-        cout << "*** i = " << i << "; j = " << j << " ***" << endl;
-        delays[i] = (result.direction[i] * itsPositionDiffs[j]) / speedOfLight;
-        LOG_TRACE_CALC_STR(" [" << i << "]: " << delays[i]);
-      }
-
-      // The time differences need to be split into a coarse (sample) delay
-      // and a fine (subsample) delay. The sample rate should be read from the
-      // configuration file.
-
-      // Finally, we must update the process loop counter. 
+      // Start by incrementing the loop count
       itsLoopCount++;
+      
+      LOG_TRACE_LIFETIME_STR(TRACE_LEVEL_FLOW, "count = " << itsLoopCount);
+
+      // Calculate the delays for the epoch after the end of the current time
+      // interval.
+      calculateDelays();
+      
+      // The time differences need to be split into a coarse (sample) delay
+      // and a fine (subsample) delay. 
+      vector<int> coarseDelay(itsNrDelaysPerEpoch);
+      vector<float> fineDelayAtBegin(itsNrDelaysPerEpoch);
+      vector<float> fineDelayAfterEnd(itsNrDelaysPerEpoch);
+
+      for (uint i = 0; i < itsNrDelaysPerEpoch; ++i) {
+	// Get delays "at begin" and "after end".
+	double db = itsDelaysAtBegin[i];
+	double de = itsDelaysAfterEnd[i];
+
+	LOG_TRACE_VAR_STR("Beamlet #" << i << ":");
+	
+// 	LOG_TRACE_VAR_STR("itsDelaysAtBegin[" << i << "]  = " <<
+// 			  itsDelaysAtBegin[i]);
+// 	LOG_TRACE_VAR_STR("itsDelaysAfterEnd[" << i << "] = " <<
+// 			  itsDelaysAfterEnd[i]);
+	
+	// The coarse delay will be determined for the center of the current
+	// time interval and will be expressed in \e samples.
+	coarseDelay[i] = (int)floor(0.5 * (db + de) * itsSampleRate + 0.5);
+	double d = coarseDelay[i] / itsSampleRate;
+	
+	// The fine delay will be determined for the boundaries of the current
+	// time interval and will be expressed in seconds.
+	fineDelayAtBegin[i] = (float)(db - d);
+	fineDelayAfterEnd[i] = (float)(de - d);
+
+	LOG_TRACE_CALC_STR(" coarseDelay       = " << coarseDelay[i]);
+	LOG_TRACE_CALC_STR(" fineDelayAtBegin  = " << fineDelayAtBegin[i]);
+	LOG_TRACE_CALC_STR(" fineDelayAfterEnd = " << fineDelayAfterEnd[i]);
+      }
+
+      // We need to send the coarse and fine delay info to all RSP boards.
+      for (int rsp = 0; rsp < itsNoutputs; ++rsp) {
+	DH_Delay* dh = (DH_Delay*)getDataManager().getOutHolder(rsp);
+	dh;
+      }
+
     }
 
 
     void WH_DelayCompensation::postprocess()
     {
-      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, getName().c_str());
+      LOG_TRACE_LIFETIME(TRACE_LEVEL_FLOW, "");
 
       // Delete the AMC converter
       ASSERT(itsConverter);
@@ -189,8 +195,7 @@ namespace LOFAR
       LOG_TRACE_FLOW(AUTO_FUNCTION_NAME);
 
       // First let's find out how many sources we have.
-      uint32 nrBeams = ps.getUint32("Observation.NBeams");
-      LOG_TRACE_VAR_STR(nrBeams << " beam direction(s)");
+      LOG_TRACE_VAR_STR(itsNrBeams << " beam direction(s):");
 
       // What coordinate system is used for these source directions?
       // Currently, we support J2000, ITRF, and AZEL.
@@ -207,15 +212,16 @@ namespace LOFAR
       // consists of two doubles.
       vector<double> dir;
       dir = ps.getDoubleVector("Observation.BeamDirections");
-      ASSERTSTR(dir.size() == 2 * nrBeams, 
-                dir.size() << " == " << 2 * nrBeams);
+      ASSERTSTR(dir.size() == 2 * itsNrBeams, 
+                dir.size() << " == " << 2 * itsNrBeams);
       
       // Reserve space in \a itsBeamDirections to avoid reallocations.
-      itsBeamDirections.reserve(nrBeams);
+      itsBeamDirections.reserve(itsNrBeams);
       
       // Split the \a dir vector into separate Direction objects.
-      for (uint i = 0; i < nrBeams; ++i) {
+      for (uint i = 0; i < itsNrBeams; ++i) {
         itsBeamDirections.push_back(Direction(dir[2*i], dir[2*i+1], dirType));
+	LOG_TRACE_VAR_STR(" [" << i << "] = " << itsBeamDirections[i]);
       }
     }
 
@@ -225,8 +231,7 @@ namespace LOFAR
       LOG_TRACE_FLOW(AUTO_FUNCTION_NAME);
 
       // First, let's find out how many stations we have.
-      uint32 nrStations = ps.getUint32("Observation.NStations");
-      LOG_TRACE_VAR_STR(nrStations << " station position(s)");
+      LOG_TRACE_VAR_STR(itsNrStations << " station position(s):");
 
       // Station positions must be given in ITRF; there is currently no
       // support in the AMC package to convert between WGS84 and ITRF.
@@ -239,16 +244,17 @@ namespace LOFAR
       // positions are stored as one large vector of doubles.
       vector<double> pos;
       pos = ps.getDoubleVector("Observation.StationPositions");
-      ASSERTSTR(pos.size() == 3 * nrStations,
-                pos.size() << " == " << 3 * nrStations);
+      ASSERTSTR(pos.size() == 3 * itsNrStations,
+                pos.size() << " == " << 3 * itsNrStations);
 
       // Reserve space in \a itsStationPositions to avoid reallocations.
-      itsStationPositions.reserve(nrStations);
+      itsStationPositions.reserve(itsNrStations);
 
       // Split the \a pos vector into separate Position objects.
-      for (uint i = 0; i < nrStations; ++i) {
+      for (uint i = 0; i < itsNrStations; ++i) {
         itsStationPositions.
           push_back(Position(pos[3*i], pos[3*i+1], pos[3*i+2], posType));
+	LOG_TRACE_VAR_STR(" [" << i << "] = " << itsStationPositions[i]);
       }
     }
 
@@ -259,29 +265,33 @@ namespace LOFAR
 
       // The observation epoch should be given by a start time and a stop
       // time. Both times must be specified in Modified Julian Date (MJD).
-      // The integration period must be specified in seconds.
       Epoch startTime = ps.getDouble("Observation.StartTime");
-      LOG_TRACE_VAR_STR("startTime = " << startTime);
       Epoch stopTime  = ps.getDouble("Observation.StopTime");
-      LOG_TRACE_VAR_STR("stopTime = " << stopTime);
+
+      // The time step is equal to one integration period, which, in turn, is
+      // defined as the number of samples used per integration period divided
+      // by the sample rate in seconds. Divide by 86400 to get it in days.
       Epoch stepTime  = ps.getDouble("Observation.NSubbandSamples") / 
         ps.getDouble("Observation.SampleRate") / 86400;
-      LOG_TRACE_VAR_STR("stepTime = " << stepTime);
 
+      LOG_TRACE_VAR("Observation:");
+      LOG_TRACE_VAR_STR(" startTime = " << startTime);
+      LOG_TRACE_VAR_STR(" stopTime  = " << stopTime);
+      LOG_TRACE_VAR_STR(" stepTime  = " << stepTime);
+      
       ASSERTSTR(startTime <= stopTime, startTime << " <= " << stopTime);
 
       // Reserve space in \a itsObservationEpoch to avoid reallocations
-      uint sz = uint(ceil((stopTime - startTime).mjd() / stepTime.mjd()));
-      LOG_TRACE_VAR_STR(sz << " integration period(s)");
+      uint sz = uint(1 + ceil((stopTime - startTime).mjd() / stepTime.mjd()));
+      LOG_TRACE_VAR_STR(sz << " epoch(s)");
       itsObservationEpoch.reserve(sz);
       
       // Fill a vector of Epoch, with a time interval \a stepTime, starting
       // at \a startTime and ending at \a stopTime.
-      for (Epoch time = startTime; time <= stopTime; time += stepTime) {
+      Epoch time = startTime;
+      for (uint i = 0; i < sz; i++, time += stepTime) {
         itsObservationEpoch.push_back(Epoch(time));
-//         LOG_TRACE_LOOP_STR("itsObservationEpoch[" << 
-//                            itsObservationEpoch.size()-1 << "] = " <<
-//                            itsObservationEpoch[itsObservationEpoch.size()-1]);
+	LOG_TRACE_LOOP_STR(" [" << i << "] = " << itsObservationEpoch[i]);
       }
     }
 
@@ -295,11 +305,11 @@ namespace LOFAR
 
       // Calculate the station to reference station position difference vector
       // all stations.
+      LOG_TRACE_VAR("Position difference vectors:");
       const Position& p0 = itsStationPositions[0];
       for (uint i = 0; i < itsStationPositions.size(); ++i) {
         itsPositionDiffs.push_back(itsStationPositions[i] - p0);
-        LOG_TRACE_LOOP_STR("itsPositionDiffs[" << i << "] = " << 
-                           itsPositionDiffs[i]);
+        LOG_TRACE_VAR_STR(" [" << i << "] = " << itsPositionDiffs[i]);
       }
     }
 
@@ -315,6 +325,53 @@ namespace LOFAR
       return 0;
     }
 
+
+    void WH_DelayCompensation::calculateDelays()
+    {
+      // Avoid invalid memory reads.
+      ASSERTSTR(itsLoopCount < itsObservationEpoch.size(),
+		itsLoopCount << " < " << itsObservationEpoch.size());
+      
+      LOG_TRACE_LIFETIME_STR(TRACE_LEVEL_FLOW,
+			     itsObservationEpoch[itsLoopCount]);
+
+      // Convert the source coordinates to ITRF, for all beams and all
+      // stations for the epoch "after-end" of the current time interval.
+      RequestData request (itsBeamDirections, itsStationPositions, 
+                           itsObservationEpoch[itsLoopCount]);
+      ResultData result;
+      itsConverter->j2000ToItrf(result, request);
+
+      // Since we've calculated the coordinates for only one epoch, the number
+      // of directions in the result vector must be equal to the number of
+      // delays per epoch.
+      ASSERTSTR(result.direction.size() == itsNrDelaysPerEpoch,
+		result.direction.size() << " == " << itsNrDelaysPerEpoch);
+      
+      LOG_TRACE_CALC("Beamlet directions:");
+      for (uint i = 0; i < result.direction.size(); ++i) {
+        LOG_TRACE_CALC_STR(" [" << i << "] = " << result.direction[i]);
+      }
+
+      // Copy the current contents of itsDelaysAfterEnd to itsDelaysAtBegin.
+      itsDelaysAtBegin = itsDelaysAfterEnd;
+      
+      // From the source coordinates in ITRF, calculate the geometrical
+      // delays. Please remember that the vector result.direction stores the
+      // directions per epoch, per position, per direction. I.e. the first
+      // itsBeamDirections.size() elements contain the converted directions
+      // for itsStationPositions[0], the second for itsStationPositions[1],
+      // etc.
+      LOG_TRACE_CALC_STR("Beamlet geometrical delays:");
+      for (uint i = 0; i < itsNrDelaysPerEpoch; ++i) {
+        uint j = i / itsBeamDirections.size();
+        itsDelaysAfterEnd[i] = 
+	  (result.direction[i] * itsPositionDiffs[j]) / speedOfLight;
+        LOG_TRACE_CALC_STR(" [" << i << "]: " << itsDelaysAfterEnd[i]);
+      }
+
+    }
+    
 
   } // namespace CS1
 
