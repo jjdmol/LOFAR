9f92f8583906e2d5234a00966b83cb5625201084
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-06-08 18:31:54 +0000

    BugId: 742 Gnu license included

--- trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/ComponentPanel.java
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/ComponentPanel.java
@@ -1,7 +1,23 @@
 /*
  * ComponentPanel
  *
- * Created on 06 April 2006
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package nl.astron.lofar.sas.otbcomponents;
@@ -23,8 +39,13 @@ import nl.astron.lofar.sas.otb.util.UserAccount;
 import org.apache.log4j.Logger;
 
 /**
+ * @created 06-04-2006, 16:00
  *
  * @author  coolen
+ *
+ * @version $Id$
+ *
+ * @updated
  */
 public class ComponentPanel extends javax.swing.JPanel implements IViewPanel{
     
