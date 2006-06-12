6db646ff115f316e0c15758073c6ee65ba4eb4f8
Joris van Zwieten <0xA000@users.noreply.github.com>
2006-06-12 14:25:30 +0000

    BugID:745 - Updated tMeqTabular.

--- CEP/BB/BBS/test/tMeqTabular.cc
+++ CEP/BB/BBS/test/tMeqTabular.cc
@@ -31,142 +31,19 @@
 using namespace LOFAR;
 using namespace casa;
 
-// bool compare (const MeqMatrix& m1, const MeqMatrix& m2)
-// {
-//   if (m1.nx() != m2.nx()  ||  m1.ny() != m2.ny()) {
-//     return false;
-//   }
-//   MeqMatrix res = sum(sqr(m1-m2));
-//   if (!res.isComplex()) {
-//     return (res.getDouble() < 1.e-7);
-//   }
-//   dcomplex resc = res.getDComplex();
-//   return (LOFAR::real(resc) < 1.e-7  &&  LOFAR::imag(resc) < 1.e-7);
-// }
+#define EPSILON 1.0e-12
 
-// void eval3 (int ncy, int ndx, int ndy, double stepx, double stepy,
-// 	    const double* coeffData, double pert, MeqResult& res,
-// 	    bool makediff=true)
-// {
-//   res = MeqResult(3*ncy);
-//   double* value = res.setDoubleFormat (ndx, ndy);
-//   if (makediff) {
-//     for (int i=0; i<3*ncy; ++i) {
-//       ///      res.setPerturbedDouble (i, ndx, ndy);
-//     }
-//   }
-//   NSTimer timer1("new");
-//   timer1.start();
-//       double valy = 0;
-//       for (int j=0; j<ndy; j++) {
-// 	const double* mvalue = value;
-// 	// Calculate the y-factors.
-// 	double lastval = coeffData[0];
-// 	double fact1   = coeffData[1];
-// 	double fact2   = coeffData[2];
-// 	double y = valy;
-// 	for (int iy=1; iy<ncy; ++iy) {
-// 	  lastval += y*coeffData[3*iy];
-// 	  fact1   += y*coeffData[3*iy+1];
-// 	  fact2   += y*coeffData[3*iy+2];
-// 	  y *= valy;
-// 	}
-// 	*value++ = lastval;
-// 	fact1 *= stepx;
-// 	fact2 *= stepx*stepx;
-// 	fact1 += fact2;
-// 	fact2 *= 2;
-// 	for (int ix=1; ix<ndx; ++ix) {
-// 	  lastval += fact1;
-// 	  fact1 += fact2;
-// 	  *value++ = lastval;
-// 	}
-// 	if (makediff) {
-// 	  // Calculate the perturbed value for the coefficients without
-// 	  // a factor of x (thus c00, c10*y, c20*y^2, etc.).
-// 	  // Do the same for c01*x, c11*x*y, etc. using step.
-// 	  // Do the same for c02*x^2, c12*x^2*y, etc. using step1,step2.
-// 	  double perty=pert;
-// 	  for (int iy=0; iy<ncy; ++iy) {
-// 	    double* valuep0 = res.getPerturbedValueRW(iy*3).doubleStorage()
-// 	                      + j*ndx;
-// 	    for (int i=0; i<ndx; ++i) {
-// 	      valuep0[i] = mvalue[i] + perty;
-// 	    }
-// 	    perty *= valy;
-// 	  }
-// 	  perty=pert;
-// 	  for (int iy=0; iy<ncy; ++iy) {
-// 	    double* valuep1 = res.getPerturbedValueRW(iy*3+1).doubleStorage()
-// 	                      + j*ndx;
-// 	    double step1 = perty * stepx;
-// 	    double val1 = 0;
-// 	    for (int i=0; i<ndx; ++i) {
-// 	      valuep1[i] = mvalue[i] + val1;
-// 	      val1 += step1;
-// 	    }
-// 	    perty *= valy;
-// 	  }
-// 	  perty=pert;
-// 	  for (int iy=0; iy<ncy; ++iy) {
-// 	    double* valuep2 = res.getPerturbedValueRW(iy*3+2).doubleStorage()
-// 	                      + j*ndx;
-// 	    double step2a = perty * stepx * stepx;
-// 	    double step2b = 2*step2a;
-// 	    double val2 = 0;
-// 	    for (int i=0; i<ndx; ++i) {
-// 	      valuep2[i] = mvalue[i] + val2;
-// 	      val2 += step2a;
-// 	      step2a += step2b;
-// 	    }
-// 	    perty *= valy;
-// 	  }
-// // 	  double perty=pert;
-// // 	  for (int iy=0; iy<ncy; ++iy) {
-// // 	    double* valuep0 = res.getPerturbedValueRW(iy*3).doubleStorage();
-// // 	    double* valuep1 = res.getPerturbedValueRW(iy*3+1).doubleStorage();
-// // 	    double step1 = perty * stepx;
-// // 	    double val1 = 0;
-// // 	    double* valuep2 = res.getPerturbedValueRW(iy*3+2).doubleStorage();
-// // 	    double step2a = step1 * stepx;
-// // 	    double step2b = 2*step2a;
-// // 	    double val2 = 0;
-// // 	    for (int i=0; i<ndx; ++i) {
-// // 	      valuep0[i+j*ndx] = mvalue[i] + perty;
-// // 	      valuep1[i+j*ndx] = mvalue[i] + val1;
-// // 	      val1 += step1;
-// // 	      valuep2[i+j*ndx] = mvalue[i] + val2;
-// // 	      val2 += step2a;
-// // 	      step2a += step2b;
-// // 	    }
-// // 	    perty *= valy;
-// // 	  }
-// 	}
-// 	valy += stepy;
-//       }
-//   timer1.stop();
-//   cout << ">>>" << endl;
-//   timer1.print(cout);
-//   cout << "<<<" << endl;
-// 	
-// }
-
-bool evaluate_and_check(MeqTabular &tabular, const MeqDomain &domain, int nx, int ny, const double* const answer)
+// evaluate_and_check(): evaluates the request and compares the result with the given answer. Two numbers a
+// and b are compared as follows: a == b <=> (abs(a - b) <= EPSILON) to allow for round-off error.
+bool evaluate_and_check(MeqTabular &tabular, const MeqRequest &request, const double* const answer)
 {
   // Do not compute perturbed values.
   tabular.clearSolvable();
   
-  MeqRequest request(domain, nx, ny);
   MeqResult result = tabular.getResult(request, 0, 0);
   
-  cout << "domain: " << domain << ' ' << nx << ' ' << ny;
+  cout << "domain: " << request.domain() << ' ' << request.nx() << ' ' << request.ny();
   cout << "... ";
-  
-  /*
-  cout << "domain : " << domain << ' ' << nx << ' ' << ny << endl;
-  cout << "tabular: " << tabular.getCoeff().nx() << ' ' << tabular.getCoeff().ny() << endl;
-  cout << "result : " << result.getValue() << endl;
-  */
   
   if(answer == NULL)
   {
@@ -181,7 +58,7 @@ bool evaluate_and_check(MeqTabular &tabular, const MeqDomain &domain, int nx, in
     bool ok = true;
     for(i = 0; i < request.nx() * request.ny(); i++)
     {
-      if(abs(resultValues[i] - answer[i]) > 1.0e-7)
+      if(abs(resultValues[i] - answer[i]) > EPSILON)
       {
         ok = false;
         break;
@@ -191,7 +68,13 @@ bool evaluate_and_check(MeqTabular &tabular, const MeqDomain &domain, int nx, in
     if(!ok)
     {
       cout << "FAIL" << endl;
-      cout << "!!!!" << endl << result.getValue() << endl << resultValues[i] << " <> " << answer[i] << endl << "!!!!" << endl;
+      cout << "!!!!" << endl;
+      cout << "result:" << endl;
+      cout << setprecision(15);
+      cout << result.getValue() << endl << endl;
+      cout << "mismatch at index " << i << ": " << resultValues[i] << " != " << answer[i] << endl;
+      cout << setprecision(5);
+      cout << "!!!!" << endl;
     }
     else
     {
@@ -206,28 +89,23 @@ bool evaluate_and_check(MeqTabular &tabular, const MeqDomain &domain, int nx, in
 bool test_constant()
 {
   cout << "test_constant(): testing with a constant input pattern - ";
-  
+
+  // Constuct constant (1x1) tabular  
   ParmDB::ParmValue parm;
   parm.rep().setType("tabular");
   parm.rep().setDomain(ParmDB::ParmDomain(0.0, 1.0, 0.0, 1.0));
   MeqTabular tabular(parm);
+  tabular.setCoeff(MeqMatrix(0.23, 1, 1), false);
 
-  bool mask[25];
-  
-  for(int i=0; i < 25; i++)
-  {
-    mask[i] = false;
-  }
-
+  // Construct expected answer
+  MeqRequest request(MeqDomain(0.0, 1.0, 0.0, 1.0), 5, 5);
   double answer[25];
-  for(int i=0; i<25; i++)
+  for(int i=0; i < 25; i++)
   {
     answer[i] = 0.23;
   }
   
-  tabular.setCoeff(MeqMatrix(0.23, 1, 1), mask);
-  
-  return evaluate_and_check(tabular, MeqDomain(0.0, 1.0, 0.0, 1.0), 5, 5, answer);
+  return evaluate_and_check(tabular, request, answer);
 }
 
 
@@ -235,106 +113,117 @@ bool test_line()
 {
   cout << "test_line(): testing with a line input pattern" << endl;
   
+  // Construct tabular representing a line from (0, 0, -1) to (1, 1, 1).
   ParmDB::ParmValue parm;
   parm.rep().setType("tabular");
   parm.rep().setDomain(ParmDB::ParmDomain(0.0, 1.0, 0.0, 1.0));
   MeqTabular tabular(parm);
-
   bool mask[2] = {false, false};
   double values[2] = {-1.0, 1.0};
-    
-  double inScale = ((1.0 - 0.0) / 2.0);
-  double outScale = ((0.75 - 0.25) / 50.0);
-  double inCenter = 0.5 * inScale;
-  double outCenter = 0.5 * outScale;
+  tabular.setCoeff(MeqMatrix(values, 2, 1), mask);
+
   double answer[50];
-  for(int i=0; i<50; i++)
+  int outCount = 50;
+  double inScale = ((1.0 - 0.0) / 2.0);
+  double inCenter = 0.5 * inScale;
+  double outCenter;
+  double x;
+    
+  cout << "  - without boundary cells - ";
+  // Construct expected answer
+  MeqDomain outDomain(0.25, 0.75, 0.0, 1.0); 
+  double outScale = ((outDomain.endX() - outDomain.startX()) / outCount);
+  for(int i = 0; i < outCount; i++)
   {
-    outCenter = 0.25 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - inCenter) / inScale;
     answer[i] = values[0] * x + values[1] * (1.0 - x);
   }
-    
-  tabular.setCoeff(MeqMatrix(values, 2, 1), mask);
-  cout << "  - without boundary cells - ";
-  if(!evaluate_and_check(tabular, MeqDomain(0.25, 0.75, 0.0, 1.0), 50, 1, answer))
+  // Test  
+  MeqRequest request(outDomain, outCount, 1);
+  if(!evaluate_and_check(tabular, request, answer))
   {
     return false;
   }
-    
-  cout << "  - with boundary cells (left) - ";
   
-  outScale = ((0.75 - 0.0) / 50.0);
-  inCenter = 0.5 * inScale;
+  cout << "  - with boundary cells (left) - ";
+  // Construct expected answer
+  outDomain = MeqDomain(0.0, 0.75, 0.0, 1.0); 
+  outScale = ((outDomain.endX() - outDomain.startX()) / outCount);
   outCenter = 0.5 * outScale;
   int leftBoundaryCells = (int) ceil((inCenter - outCenter) / outScale);
-  for(int i=0; i<leftBoundaryCells; i++)
+  for(int i = 0; i < leftBoundaryCells; i++)
   {
-    outCenter = 0.0 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - (inCenter - inScale)) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - (inCenter - inScale)) / inScale;
     answer[i] = 0.0 * x + values[0] * (1.0 - x);
   }
-  
-  for(int i=leftBoundaryCells; i<50; i++)
+  for(int i = leftBoundaryCells; i < 50; i++)
   {
-    outCenter = 0.0 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - inCenter) / inScale;
     answer[i] = values[0] * x + values[1] * (1.0 - x);
   }
-  if(!evaluate_and_check(tabular, MeqDomain(0.0, 0.75, 0.0, 1.0), 50, 1, answer))
+  // Test  
+  request = MeqRequest(outDomain, outCount, 1);
+  if(!evaluate_and_check(tabular, request, answer))
   {
     return false;
   }
-    
-  outScale = ((1.0 - 0.25) / 50.0);
-  inCenter = 0.5 * inScale;
+
+  cout << "  - with boundary cells (right) - ";
+  // Construct expected answer
+  outDomain = MeqDomain(0.25, 1.0, 0.0, 1.0); 
+  outScale = ((outDomain.endX() - outDomain.startX()) / outCount);
   outCenter = 0.5 * outScale;
   int rightBoundaryCells = (int) ceil((inCenter - outCenter) / outScale);
-  for(int i=0; i<50-rightBoundaryCells; i++)
+  for(int i = 0; i < 50 - rightBoundaryCells; i++)
   {
-    outCenter = 0.25 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - inCenter) / inScale;
     answer[i] = values[0] * x + values[1] * (1.0 - x);
   }
-  inCenter += inScale;
   for(int i=50-rightBoundaryCells; i<50; i++)
   {
-    outCenter = 0.25 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - (inCenter + inScale)) / inScale;
     answer[i] = values[1] * x + 0.0 * (1.0 - x);
   }
-  cout << "  - with boundary cells (right) - ";
-  if(!evaluate_and_check(tabular, MeqDomain(0.25, 1.0, 0.0, 1.0), 50, 1, answer))
+  // Test  
+  request = MeqRequest(outDomain, outCount, 1);
+  if(!evaluate_and_check(tabular, request, answer))
   {
     return false;
   }
     
-  outScale = ((1.0 - 0.0) / 50.0);
-  inCenter = 0.5 * inScale;
+  cout << "  - with boundary cells (left and right) - ";
+  // Construct expected answer
+  outDomain = MeqDomain(0.0, 1.0, 0.0, 1.0); 
+  outScale = ((outDomain.endX() - outDomain.startX()) / outCount);
   outCenter = 0.5 * outScale;
   leftBoundaryCells = (int) ceil((inCenter - outCenter) / outScale);
   rightBoundaryCells = (int) ceil((inCenter - outCenter) / outScale);
-  for(int i=0; i<leftBoundaryCells; i++)
+  for(int i = 0; i < leftBoundaryCells; i++)
   {
-    outCenter = 0.0 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - (inCenter - inScale)) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - (inCenter - inScale)) / inScale;
     answer[i] = 0.0 * x + values[0] * (1.0 - x);
   }
-  for(int i=leftBoundaryCells; i<50-rightBoundaryCells; i++)
+  for(int i = leftBoundaryCells; i < 50 - rightBoundaryCells; i++)
   {
-    outCenter = 0.0 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - inCenter) / inScale;
     answer[i] = values[0] * x + values[1] * (1.0 - x);
   }
-  inCenter += inScale;
-  for(int i=50-rightBoundaryCells; i<50; i++)
+  for(int i = 50 - rightBoundaryCells; i < 50; i++)
   {
-    outCenter = 0.0 + i * outScale + 0.5 * outScale;
-    double x = 1.0 - (outCenter - inCenter) / inScale;
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    x = 1.0 - (outCenter - (inCenter + inScale)) / inScale;
     answer[i] = values[1] * x + 0.0 * (1.0 - x);
   }
-  cout << "  - with boundary cells (left and right) - ";
-  if(!evaluate_and_check(tabular, MeqDomain(0.0, 1.0, 0.0, 1.0), 50, 1, answer))
+  // Test  
+  request = MeqRequest(outDomain, outCount, 1);
+  if(!evaluate_and_check(tabular, request, answer))
   {
     return false;
   }
@@ -342,46 +231,97 @@ bool test_line()
   return true;
 }
     
+
+bool test_sin()
+{
+  cout << "test_sin(): testing with a sine input pattern (2 cycles, 512 samples)" << endl;
+  
+  // Construct tabular representing two cycles of a sine wave.
+  ParmDB::ParmValue parm;
+  parm.rep().setType("tabular");
+  parm.rep().setDomain(ParmDB::ParmDomain(0.0, 1.0, 0.0, 1.0));
+  MeqTabular tabular(parm);
+  bool mask[512];
+  double values[512];
+  for(int i = 0; i < 512; i++)
+  {
+    mask[i] = false;
+    values[i] = 10.0 * sin((i * 2.0 * 3.1415927) / 256.0);
+  }
+  tabular.setCoeff(MeqMatrix(values, 512, 1), mask);
+    
+  double inScale = ((1.0 - 0.0) / 512.0);
+ 
+  // To test copy, use values as answers (as no interpolation
+  // should be done in this case).
+  cout << "  - complete copy - ";
+  MeqRequest request(MeqDomain(0.0, 1.0, 0.0, 1.0), 512, 1);
+  if(!evaluate_and_check(tabular, request, values))
+  {
+    return false;
+  }
+  
+  cout << "  - partial copy (left 3/4) - ";
+  request = MeqRequest(MeqDomain(0.0, 1.0 - 128.0 * inScale, 0.0, 1.0), 512 - 128, 1);
+  if(!evaluate_and_check(tabular, request, values))
+  {
+    return false;
+  }
+  
+  cout << "  - partial copy (right 3/4) - ";
+  request = MeqRequest(MeqDomain(128.0 * inScale, 1.0, 0.0, 1.0), 512 - 128, 1);
+  if(!evaluate_and_check(tabular, request, &(values[128])))
+  {
+    return false;
+  }
+  
+  cout << "  - partial copy (middle 1/2) - ";
+  request = MeqRequest(MeqDomain(128.0 * inScale, 1.0 - 128.0 * inScale, 0.0, 1.0), 512 - 256, 1);
+  if(!evaluate_and_check(tabular, request, &(values[128])))
+  {
+    return false;
+  }
+  
+  cout << "  - downsampling (512 -> 50 samples) - ";
+  // Construct expected answer, using linear interpolation between
+  // sample points.
+  double answer[50];
+  int outCount = 50;
+  MeqDomain outDomain(0.0, 1.0, 0.0, 1.0);
+  double outScale = ((outDomain.endX() - outDomain.startX()) / outCount);
+  double inCenter, outCenter;
+  double x;
+  int index;
+  for(int i = 0; i < 50; i++)
+  {
+    outCenter = outDomain.startX() + i * outScale + 0.5 * outScale;
+    index = (int) ((outCenter - 0.5 * inScale) / inScale);
+    inCenter = index * inScale + 0.5 * inScale;
+    x = 1.0 - (outCenter - inCenter) / inScale;
+    answer[i] = x * values[index] + (1.0 - x) * values[index + 1];
+  }
+  // Test
+  request = MeqRequest(outDomain, 50, 1);
+  if(!evaluate_and_check(tabular, request, answer))
+  {
+    return false;
+  }
+  
+  return true;
+}
+
+
 int main()
 {
   bool ok = true;
   
   try
   {
-    ok = test_constant() && test_line();
-        
-/*    
-    double values[9] = {1.0, 2.0, 3.0, 4.0, 5.0, 4.0, 3.0, 2.0, 1.0};
-//    bool mask[9] = {false, false, false, false, false, false, false, false, false};
-    tabular.setCoeff(MeqMatrix(values, 9, 1), mask);
-    
-    evaluate(tabular, MeqDomain(0.10, 0.90, 0.0, 1.0), 50, 1, NULL);
-    evaluate(tabular, MeqDomain(0.0, 0.9, 0.0, 1.0), 50, 1, NULL);
-    evaluate(tabular, MeqDomain(0.1, 1.0, 0.0, 1.0), 50, 1, NULL);
-    evaluate(tabular, MeqDomain(0.0, 1.0, 0.0, 1.0), 50, 1, NULL);
-    
-    double values2[512];
-    for(int i=0; i < 512; i++)
-    {
-      values2[i] = 10.0*sin((i*2.0*3.1415927)/50.0);
-    }
-    tabular.setCoeff(MeqMatrix(values2, 512, 1), mask);
-    
-    evaluate(tabular, MeqDomain(0.0, 1.0, 0.0, 1.0), 50, 1, NULL);
-    evaluate(tabular, MeqDomain(0.25, 0.75, 0.0, 1.0), 256, 1, NULL);
-    evaluate(tabular, MeqDomain(0.0, 1.0, 0.0, 1.0), 512, 1, NULL);
-    */
+    ok = test_constant() && test_line() && test_sin();
   } catch (std::exception& ex) {
     cout << "Caught exception: " << ex.what() << endl;
     return 1;
   }
   
-  if(!ok)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
+  return (ok ? 0 : 1);
 }
