7d082f04f828c65fc5972994022c83d90ac7de10
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-06-14 09:43:12 +0000

    BugID: 703 Added GNU msg

--- trunk/JAVA/CEP/jParmFacade/src/nl/astron/lofar/java/cep/jparmfacade/jParmFacade.java
+++ trunk/JAVA/CEP/jParmFacade/src/nl/astron/lofar/java/cep/jparmfacade/jParmFacade.java
@@ -1,9 +1,25 @@
-/*
- * jParmFacade.java  JNI Interface base class for the CEP/BB/ParmFacade class
+/* jParmFacade.java  JNI Interface base class for the CEP/BB/ParmFacade class
  *
- * Created on April 18, 2006, 9:54 AM
+ *  Copyright (C) 2002-2007
+ *  ASTRON (Netherlands Foundation for Research in Astronomy)
+ *  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+
 
 package nl.astron.lofar.java.cep.jparmfacade;
 
@@ -11,8 +27,9 @@ import java.util.HashMap;
 import java.util.Vector;
 
 /**
- *
+ * @created 18-04-2006, 9:54
  * @author coolen
+ * @version $Id$
  */
 public class jParmFacade {
     private String itsParmFacadeDB="";
