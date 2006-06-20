2f056910a3a3fd8885e4fc1dd7824a93bfacfd79
overeem <overeem@astron.nl>
2006-06-20 09:32:07 +0000

    BugID:  679 Reformatted ResourceAllocator sources.

--- MAC/APL/APLCommon/include/APL/APLCommon/ResourceAllocator.h
+++ MAC/APL/APLCommon/include/APL/APLCommon/ResourceAllocator.h
@@ -28,112 +28,101 @@
 #include <set>
 #include <bitset>
 
-namespace LOFAR 
-{
-  namespace APLCommon 
-  {
+namespace LOFAR {
+  namespace APLCommon {
 
-/**
-*/
 class LogicalDevice;
 
 class ResourceAllocator
 {
-  public:   
-    typedef boost::shared_ptr<ResourceAllocator> ResourceAllocatorPtr;
-    typedef boost::shared_ptr<LogicalDevice> LogicalDevicePtr;
-    typedef std::bitset<256> TRcuSubset;
+public:   
+	typedef boost::shared_ptr<ResourceAllocator> ResourceAllocatorPtr;
+	typedef boost::shared_ptr<LogicalDevice> 	 LogicalDevicePtr;
+	typedef std::bitset<256> 					 TRcuSubset;
 
-    ~ResourceAllocator();
-    static ResourceAllocatorPtr instance();
+	~ResourceAllocator();
+	static ResourceAllocatorPtr instance();
 
-  public: // member functions
-    bool claimSO(ResourceAllocator::LogicalDevicePtr ld, uint16 priority, double samplingFrequency);
-    void releaseSO(ResourceAllocator::LogicalDevicePtr ld);
-    void logSOallocation() const;
-  
-    bool claimSRG(ResourceAllocator::LogicalDevicePtr ld, uint16 priority, 
-        TRcuSubset rcuSubset, int16 nyquistZone, uint8 rcuControl);
-    void releaseSRG(ResourceAllocator::LogicalDevicePtr ld);
-    void logSRGallocation() const;
-  
-  private:
-    ResourceAllocator();
+	// member functions
+	bool claimSO	(ResourceAllocator::LogicalDevicePtr ld, 
+					 uint16 							 priority, 
+					 double 							 samplingFrequency);
+	void releaseSO	(ResourceAllocator::LogicalDevicePtr ld);
+	void logSOallocation() const;
 
-  private: // helper methods
+	bool claimSRG	(ResourceAllocator::LogicalDevicePtr ld, 
+					 uint16 							 priority, 
+					 TRcuSubset 						 rcuSubset, 
+					 int16 								 nyquistZone, 
+					 uint8 								 rcuControl);
+	void releaseSRG	(ResourceAllocator::LogicalDevicePtr ld);
+	void logSRGallocation() const;
 
-  private: // data members
+private:
+	ResourceAllocator();
 
     // generic types
-    struct TAllocation
-    {
-      LogicalDevicePtr ld;
-      uint16           priority;
+    struct TAllocation {
+		LogicalDevicePtr ld;
+		uint16           priority;
 
-      TAllocation( LogicalDevicePtr _ld, uint16 _priority) :
-        ld(_ld),
-        priority(_priority)
-      {
-      };
-      
-      //
-      // The comparison operator used to order the priority queue.
-      //
-      struct higherPriority
-      {
-        bool operator()(const TAllocation &a, const TAllocation &b) const
-        {
-          return a.priority < b.priority;
-        }
-      };
-      
-    };
+		TAllocation (LogicalDevicePtr _ld, uint16 _priority) :
+			ld(_ld),
+			priority(_priority)
+		{ };
 
-    // SO types
-    struct TSOAllocation : public TAllocation
-    {
-      double samplingFrequency;
+		//
+		// The comparison operator used to order the priority queue.
+		//
+		struct higherPriority {
+			bool operator()(const TAllocation &a, const TAllocation &b) const {
+				return (a.priority < b.priority);
+			}
+		};
       
-      TSOAllocation( LogicalDevicePtr _ld, uint16 _priority, 
-          double _sf) :
-        TAllocation(_ld, _priority),
-        samplingFrequency(_sf)
-      {
-      };
-      
-    };
+	};
 
-    // SRG types
-    struct TSRGAllocation : public TAllocation
-    {
-      TRcuSubset  rcuSubset;
-      int16       nyquistZone;
-      uint8       rcuControl;
-      
-      TSRGAllocation( LogicalDevicePtr _ld, uint16 _priority, 
-          TRcuSubset _rcuSubset, int16 _nyquistZone, uint8 _rcuControl) :
-        TAllocation(_ld, _priority),
-        rcuSubset(_rcuSubset),
-        nyquistZone(_nyquistZone),
-        rcuControl(_rcuControl)
-      {
-      };
-      
-    };
+	// SO types
+	struct TSOAllocation : public TAllocation {
+		double samplingFrequency;
 
-    struct TRcuSettings
-    {
-      int16 nyquistZone;
-      uint8 rcuControl;
-      
-      TRcuSettings() : nyquistZone(0), rcuControl(0) {};
-      TRcuSettings(int16 n, uint8 r) : nyquistZone(n), rcuControl(r) {};
-    };
+		TSOAllocation( LogicalDevicePtr _ld, uint16 _priority, double _sf) :
+			TAllocation(_ld, _priority),
+			samplingFrequency(_sf)
+		{ };
+
+	};
+
+	// SRG types
+	struct TSRGAllocation : public TAllocation {
+		TRcuSubset  rcuSubset;
+		int16       nyquistZone;
+		uint8       rcuControl;
+
+		TSRGAllocation (LogicalDevicePtr _ld, uint16 _priority, 
+						TRcuSubset _rcuSubset, int16 _nyquistZone, uint8 _rcuControl) :
+			TAllocation(_ld, _priority),
+			rcuSubset(_rcuSubset),
+			nyquistZone(_nyquistZone),
+			rcuControl(_rcuControl)
+		{ };
+
+	};
+
+	struct TRcuSettings {
+		int16 nyquistZone;
+		uint8 rcuControl;
+
+		TRcuSettings() : 
+			nyquistZone(0), rcuControl(0) {};
+		TRcuSettings(int16 n, uint8 r) : 
+			nyquistZone(n), rcuControl(r) {};
+	};
 
     // typedefs
-    typedef std::map< uint16, TRcuSettings > TRcuSettingsMap;
-    typedef std::multiset< TSOAllocation, TAllocation::higherPriority >  TSOAllocationSet;
-    typedef std::multiset< TSRGAllocation, TAllocation::higherPriority > TSRGAllocationSet;
+    typedef std::map     <uint16, 		  TRcuSettings> 			   TRcuSettingsMap;
+    typedef std::multiset<TSOAllocation,  TAllocation::higherPriority> TSOAllocationSet;
+    typedef std::multiset<TSRGAllocation, TAllocation::higherPriority> TSRGAllocationSet;
     
     // private functions
     bool _matchSOparameters(const TSOAllocation& current, const TSOAllocation& requested);
@@ -141,15 +130,14 @@ class ResourceAllocator
     void _addSRGallocation(TRcuSubset rcuSubset, int16 nyquistZone, uint8 rcuControl);
     
     // private data
-    TSOAllocationSet  m_allocatedSOSet;
-    TSRGAllocationSet m_allocatedSRGSet;
-    TRcuSubset        m_allocatedSRGRcuSubset;
-    TRcuSettingsMap   m_allocatedSRGRcuSettingsMap;
+    TSOAllocationSet  			m_allocatedSOSet;
+    TSRGAllocationSet 			m_allocatedSRGSet;
+    TRcuSubset        			m_allocatedSRGRcuSubset;
+    TRcuSettingsMap   			m_allocatedSRGRcuSettingsMap;
 
-  private: // admin members  
+	// admin members  
     static ResourceAllocatorPtr m_theInstance;
      
-    ALLOC_TRACER_CONTEXT  
 };
 
  } // namespace APLCommon
