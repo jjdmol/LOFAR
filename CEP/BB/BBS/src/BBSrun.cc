262b4383d775f8f5387f7c246cebe387fdf5a651
Ger van Diepen <gervandiepen@users.noreply.github.com>
2006-06-30 08:15:08 +0000

    BugID: 711 Added program BBSrun

---
+++ CEP/BB/BBS/src/BBSrun.cc
@@ -0,0 +1,252 @@
+//# BBSrun.cc: Main program to do a BBS run
+//#
+//# Copyright (C) 2006
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <lofar_config.h>
+
+#include <BBS/Prediffer.h>
+#include <BBS/Solver.h>
+#include <ParmDB/ParmDB.h>
+#include <MS/MSDesc.h>
+#include <APS/ParameterSet.h>
+#include <Blob/BlobIStream.h>
+#include <Blob/BlobIBufStream.h>
+#include <Common/LofarLogger.h>
+#include <Common/VectorUtil.h>
+
+#include <casa/Exceptions/Error.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+using namespace LOFAR;
+using namespace LOFAR::ParmDB;
+using namespace LOFAR::ACC::APS;
+
+
+void predict (Prediffer& prediffer, const MSDesc& msd,
+	      const string& columnName,
+	      double timeStep, int startChan, int endChan)
+{
+  double time = msd.startTime;
+  double endTime = msd.endTime;
+  while (time < endTime) {
+    prediffer.setWorkDomain (startChan, endChan, time, time+timeStep);
+    prediffer.writePredictedData (columnName);
+    time += timeStep;
+  }
+}           
+
+void subtract (Prediffer& prediffer, const MSDesc& msd,
+	       const string& columnNameIn, const string& columnNameOut,
+	       double timeStep, int startChan, int endChan)
+{
+  double time = msd.startTime;
+  double endTime = msd.endTime;
+  while (time < endTime) {
+    prediffer.setWorkDomain (startChan, endChan, time, time+timeStep);
+    prediffer.subtractData (columnNameIn, columnNameOut, false);
+    time += timeStep;
+  }
+}           
+
+void solve (Prediffer& prediffer, const MSDesc& msd,
+	    const string& columnName,
+	    double timeStep, int startChan, int endChan,
+	    const vector<string>& solvParms, const vector<string>& exclParms,
+	    const vector<int32>& nrinterval,
+	    int maxIterations,
+	    bool saveSolution)
+{
+  prediffer.clearSolvableParms();
+  prediffer.setSolvableParms (solvParms, exclParms);
+  double time = msd.startTime;
+  double endTime = msd.endTime;
+  while (time < endTime) {
+    // Use given channels and time steps.
+    prediffer.setWorkDomain (startChan, endChan, time, time+timeStep);
+    // Form the solve domains.
+    const MeqDomain& workDomain = prediffer.getWorkDomain();
+    vector<MeqDomain> solveDomains;
+    double freq = workDomain.startX();
+    double stepf = (workDomain.endX() - freq) / nrinterval[0];
+    double stept = (workDomain.endX() - time) / nrinterval[1];
+    double sdTime = workDomain.startX();
+    for (int i=0; i<nrinterval[1]; ++i) {
+      double sdFreq = workDomain.startX();
+      for (int j=0; j<nrinterval[0]; ++j) {
+	solveDomains.push_back (MeqDomain(sdFreq, sdFreq+stepf,
+					  sdTime, sdTime+stept));
+	sdFreq += stepf;
+      }
+      sdTime += stept;
+    }
+    prediffer.initSolvableParms (solveDomains);
+
+    Solver solver;
+    solver.initSolvableParmData (1, solveDomains, prediffer.getWorkDomain());
+    solver.setSolvableParmData (prediffer.getSolvableParmData(), 0);
+    prediffer.showSettings();
+    cout << "Before: " << setprecision(10)
+	 << solver.getSolvableValues(0) << endl;
+    
+    for(int i=0; i<maxIterations; ++i) {
+      // Get the fitter data from the prediffer and give it to the solver.
+      vector<casa::LSQFit> fitters;
+      prediffer.fillFitters (fitters, columnName);
+      solver.mergeFitters (fitters, 0);
+
+      // Do the solve.
+      solver.solve(false);
+      cout << "iteration " << i << ":  " << setprecision(10)
+	   << solver.getSolvableValues(0) << endl;
+      cout << solver.getQuality(0) << endl;
+
+      prediffer.updateSolvableParms (solver.getSolvableParmData());
+    }
+    if (saveSolution) {
+      cout << "Writing solutions into ParmDB ..." << endl;
+      prediffer.writeParms();
+    }
+    time += timeStep;
+  }
+}
+
+
+bool doIt (const string& parsetName)
+{
+  string user, measurementSet, skyPDB, instrumentPDB, instrumentModelType;
+  string operation, columnNameIn, columnNameOut;
+  double timeDomainSize;
+  vector<string> solvParms, exclParms;
+  vector<int32> nrSolveInterval;
+  int startChan, endChan, nriter;
+  bool calcUVW, saveSolution;
+  
+  // Read & parse parameters
+  try
+  {
+    ParameterSet parameters(parsetName);
+    
+    user = parameters.getString ("user");
+    instrumentPDB = parameters.getString ("instrument_parmdb");
+    skyPDB = parameters.getString ("sky_parmdb");
+    measurementSet = parameters.getString ("measurement_set");
+    instrumentModelType = parameters.getString ("instrument_model");
+    calcUVW = parameters.getBool ("calculate_UVW");
+    operation = parameters.getString ("operation");
+    columnNameIn = parameters.getString ("data_column_in");
+    columnNameOut = parameters.getString ("data_column_out");
+    timeDomainSize = parameters.getDouble ("time_domain_size");
+    startChan = parameters.getInt32 ("start_channel");
+    endChan = parameters.getInt32 ("end_channel");
+    solvParms = parameters.getStringVector ("solvable_parms");
+    exclParms = parameters.getStringVector ("solvable_parms_excluded");
+    nrSolveInterval = parameters.getInt32Vector ("nr_solve_interval");
+    nriter = parameters.getInt32 ("nriter");
+    saveSolution = parameters.getBool ("save_solution");
+  }
+  catch (exception& _ex)
+  {
+    cout << "Parameter read or parse error: " << _ex.what() << endl;
+    return false;
+  }
+  
+  cout << "user                   : " << user << endl;
+  cout << "instrument ParmDB      : " << instrumentPDB << endl;
+  cout << "sky ParmDB             : " << skyPDB << endl;
+  cout << "measurement set        : " << measurementSet << endl;
+  cout << "instrument model       : " << instrumentModelType << endl;
+  cout << "calculate UVW          : " << calcUVW << endl;
+  cout << "start channel          : " << startChan << endl;
+  cout << "end channel            : " << endChan << endl;
+  cout << "operation              : " << operation << endl;
+  cout << "time domain size       : " << timeDomainSize << endl;
+  
+  // Get meta data from description file.
+  string name(measurementSet+"/vis.des");
+  std::ifstream istr(name.c_str());
+  ASSERTSTR (istr, "File " << measurementSet
+	     << "/vis.des could not be opened");
+  BlobIBufStream bbs(istr);
+  BlobIStream bis(bbs);
+  MSDesc msd;
+  bis >> msd;
+
+  vector<int> antennaSelector(msd.antNames.size());
+  for (uint i=0; i<antennaSelector.size(); ++i) {
+    antennaSelector[i] = i;
+  }
+  vector<vector<int> > sourceGroups;
+  // Construct prediffer.
+  Prediffer prediffer(measurementSet, 
+		      ParmDBMeta("aips", instrumentPDB),
+		      ParmDBMeta("aips", skyPDB),
+		      antennaSelector, instrumentModelType, sourceGroups,
+		      calcUVW);
+  try {
+    if (operation == "solve") {
+      ASSERT (nrSolveInterval.size()==2);
+      ASSERT (nrSolveInterval[0] > 0  &&  nrSolveInterval[1] > 0);
+      cout << "input column name      : " << columnNameIn << endl;
+      cout << "solvable parms         : " << solvParms << endl;
+      cout << "solvable parms excluded: " << exclParms << endl;
+      cout << "solve nrintervals      : " << nrSolveInterval << endl;
+      cout << "solve nriter           : " << nriter << endl;
+      solve (prediffer, msd, columnNameIn,
+	     timeDomainSize, startChan, endChan,
+	     solvParms, exclParms,
+	     nrSolveInterval, nriter, saveSolution);
+    } else if (operation == "predict") {
+      cout << "output column name     : " << columnNameOut << endl;
+      predict (prediffer, msd, columnNameOut,
+	       timeDomainSize, startChan, endChan);
+    } else if (operation == "subtract") {
+      cout << "input column name      : " << columnNameIn << endl;
+      cout << "output column name     : " << columnNameOut << endl;
+      subtract (prediffer, msd, columnNameIn, columnNameOut,
+		timeDomainSize, startChan, endChan);
+    } else {
+      cout << "Only operations solve, predict, and subtract are valid" << endl;
+      return 1;
+    }
+  }
+  catch (exception& _ex)
+  {
+    cout << "error: " << _ex.what() << endl;
+    return false;
+  }
+  return true;
+}
+
+int main (int argc, const char* argv[])
+{
+  if (argc < 2) {
+    cout << "Run as: tBandpass parset-name" << endl;
+    return 1;
+  }
+  if (doIt (argv[1])) {
+    cout << "OK" << endl;
+    return 0; 
+  }
+  return 1;
+}
