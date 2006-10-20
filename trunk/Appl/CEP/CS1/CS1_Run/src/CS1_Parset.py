b7a04664c9fd7ba96b005f8600e9eb17d2018eff
zwart <zwart@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-10-20 12:37:00 +0000

    BugID: 951
    Checked in first versions of CS1 startup scripts

---
+++ trunk/Appl/CEP/CS1/CS1_Run/src/CS1_Parset.py
@@ -0,0 +1,73 @@
+import time
+import Parameters
+
+class CS1_Parset(Parameters.Parset):
+
+    def __init__(self):
+        Parameters.Parset.__init__(self)
+        #self.clock = '160MHz'
+        #self.firstSB = 60e6
+        #self.nSubbands = 8
+        #self.setClock('160MHz')
+        #self.setSubbands('60MHz', 8)
+        #self.setInterval(time.time() + 600, 600)
+        #self.setIntegrationTime(1)
+
+    def setClock(self, clock):
+        self.clock = clock
+        if self.clock == '160MHz':
+            self['Observation.NSubbandSamples'] = 155648
+            self['Observation.SampleRate'] = 156250
+        elif self.clock == '200MHz':
+            self['Observation.NSubbandSamples'] = 196608
+            self['Observation.SampleRate'] = 195312.5
+        self.updateSBValues()
+        
+    def getClockString(self):
+        return self.clock
+
+    def setSubbands(self, first, number):        
+        self.firstSB = int(1e6 * float((first.split('MHz')[0])))
+        self.nSubbands = number
+        self.updateSBValues()
+
+    def setNStations(self, nstations):
+        self['Observation.NStations'] = nstations
+        self['Input.NRSPBoards'] = nstations
+        
+    def getFirstSubband(self):
+        return self.firstSB
+
+    def setInterval(self, start, duration):
+        self['Observation.StartTime'] = start
+        self['Observation.StopTime'] = start + duration
+
+    def setIntegrationTime(self, integrationTime):
+        self['Storage.IntegrationTime'] = integrationTime
+
+    def setMSName(self, msName):
+        self['Storage.MSName'] = msName
+
+    def getMSName(self):
+        return self['Storage.MSName']
+
+    def updateSBValues(self):
+        if self.clock == '160MHz':
+            subbandwidth = 156250
+        elif self.clock == '200MHz':
+            subbandwidth = 195312.5
+        
+        if not self.__dict__.has_key('nSubbands'):
+            return
+        sbs = list()
+        for sb in range(0, self.nSubbands):
+            sbs.append(self.firstSB + sb * subbandwidth)
+
+        # create the frequencies for all subbands
+        self['Observation.RefFreqs'] = '[' + ','.join(str(sb) for sb in sbs) + ']'
+        self['Observation.NSubbands'] = self.nSubbands
+        
+        #the number of subbands should be dividable by the number of subbands per pset
+        if not self.nSubbands % self.getInt32('General.SubbandsPerPset') == 0:
+            raise Exception('Number of subbands(%d) in not dividable by the number of subbands per pset (%d).' % self.nSubbands, self['General.SubbandsPerPset'])
+            
