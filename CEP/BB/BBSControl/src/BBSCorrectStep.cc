a4954d580cf1466e2c9b7b32b6dafab9f2177234
Marcel Loose <gmloose@users.noreply.github.com>
2006-10-26 10:03:58 +0000

    BugId: 753
    Created separate header and implementation file for each BBSStep class.

---
+++ CEP/BB/BBSControl/src/BBSCorrectStep.cc
@@ -0,0 +1,50 @@
+//#  BBSCorrectStep.cc: 
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <lofar_config.h>
+#include <BBSControl/BBSCorrectStep.h>
+
+namespace LOFAR
+{
+  namespace BBS
+  {
+    // Register BBSCorrectStep with the BBSStreamableFactory. Use an anonymous
+    // namespace. This ensures that the variable `dummy' gets its own private
+    // storage area and is only visible in this compilation unit.
+    namespace
+    {
+      bool dummy = BlobStreamableFactory::instance().
+	registerClass<BBSCorrectStep>("BBSCorrectStep");
+    }
+
+
+    const string& BBSCorrectStep::classType() const 
+    {
+      static string theType("BBSCorrectStep");
+      return theType;
+    }
+  
+  } // namespace BBS
+
+} // namespace LOFAR
+    
+    
---
+++ CEP/BB/BBSControl/src/BBSRefitStep.cc
@@ -0,0 +1,50 @@
+//#  BBSRefitStep.cc: 
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <lofar_config.h>
+#include <BBSControl/BBSRefitStep.h>
+
+namespace LOFAR
+{
+  namespace BBS
+  {
+    // Register BBSRefitStep with the BBSStreamableFactory. Use an anonymous
+    // namespace. This ensures that the variable `dummy' gets its own private
+    // storage area and is only visible in this compilation unit.
+    namespace
+    {
+      bool dummy = BlobStreamableFactory::instance().
+	registerClass<BBSRefitStep>("BBSRefitStep");
+    }
+
+
+    const string& BBSRefitStep::classType() const 
+    {
+      static string theType("BBSRefitStep");
+      return theType;
+    }
+  
+  } // namespace BBS
+
+} // namespace LOFAR
+    
+    
