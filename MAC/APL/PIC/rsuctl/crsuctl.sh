293cd5e4e409ff448c0eb2b7215121f5fc507fd3
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2006-11-08 08:42:27 +0000

    bugid:691 Cleanup. Use new -q options to rsuctl to prevent delay checking for running RSPDriver.

--- MAC/APL/PIC/rsuctl/crsuctl.sh
+++ MAC/APL/PIC/rsuctl/crsuctl.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-#
-# Script to run rsuctl on a number of MAC addresses using a MAC address template
-# and filling in the missing index (MM).
-#
-
 MM="00 01 02 03 04 05 06 07 08 09 0a 0b"
-#MM="08 09 0a 0b"
 
 if [ $# -eq 0 ]; then
 	echo "usage: `basename $0` arg*"
@@ -16,10 +10,10 @@ if [ $# -eq 0 ]; then
 fi
 
 #echo args=$*
+here=`dirname $0`
 
 for mac in $MM; do
-	realargs=`echo $* | sed -e "s/MM/$mac/"`
+	realargs=`echo -m 10:fa:00:00:MM:00 $* | sed -e "s/MM/$mac/"`
 	echo Running rsuctl $realargs
-	sudo rsuctl $realargs
-#	sudo rsuctl -m $mac_address -i eth1 -w -p $page -f $image $extra_options
+	sudo $here/rsuctl -q $realargs
 done
