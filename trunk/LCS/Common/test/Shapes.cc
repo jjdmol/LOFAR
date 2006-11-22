51fcd77f55d42dc7e130732c714c91791671ffbd
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2006-11-22 21:18:08 +0000

    BugID: 947
    Added checks for HAVE_BOOST in several files, so that the Boost package can be
    optinal again.

--- trunk/LCS/Common/test/Shapes.cc
+++ trunk/LCS/Common/test/Shapes.cc
@@ -23,6 +23,9 @@
 //# Always #include <lofar_config.h> first!
 #include <lofar_config.h>
 
+//# This code can only be compiled if Boost is available.
+#ifdef HAVE_BOOST
+
 //# Includes
 #include "Shapes.h"
 #include <Common/lofar_iostream.h>
@@ -71,3 +74,4 @@ namespace LOFAR
 
 }
 
+#endif
