3de1251825ee3eb86125bbea6676a3a761737e94
Marcel Loose <gmloose@users.noreply.github.com>
2006-12-15 13:09:17 +0000

    BugID: 61
    Added #include of StringUtil.h whenever required (e.g., when tracing macros
    are enabled).

--- LCS/Common/include/Common/LofarLogCout.h
+++ LCS/Common/include/Common/LofarLogCout.h
@@ -32,8 +32,10 @@
 #include <Common/lofar_string.h>
 #include <Common/lofar_map.h>
 #include <Common/Exception.h>
+
 #ifdef ENABLE_LATENCY_STATS
-#include <sys/time.h>
+# include <Common/StringUtil.h>
+# include <sys/time.h>
 #endif
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
