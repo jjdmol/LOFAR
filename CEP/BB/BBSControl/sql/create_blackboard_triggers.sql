063ba36b500df11d0af571a141c692201f1a965e
Joris van Zwieten <0xA000@users.noreply.github.com>
2007-03-13 09:32:38 +0000

    BugID: 1037 - Removed strategy trigger in favour of a boolean return value from set_strategy, because   on the c++ side user exceptions cannot be descriminated from sql (syntax) errors.

--- CEP/BB/BBSControl/sql/create_blackboard_triggers.sql
+++ CEP/BB/BBSControl/sql/create_blackboard_triggers.sql
@@ -1,16 +1,3 @@
-CREATE FUNCTION blackboard.check_strategy_constraint() RETURNS TRIGGER AS
-$$
-    BEGIN
-        IF (SELECT COUNT(*) FROM blackboard.strategy) <> 0 THEN
-            RAISE EXCEPTION 'Only one strategy can exist at any time.';
-        END IF;
-        
-        RETURN NEW;
-    END;
-$$
-LANGUAGE plpgsql;
-
-
 CREATE OR REPLACE FUNCTION blackboard.notify_client() RETURNS TRIGGER AS
 $$
     BEGIN
@@ -19,9 +6,6 @@ $$
     END;
 $$
 LANGUAGE plpgsql;
-
-
-CREATE TRIGGER insert_strategy BEFORE INSERT ON blackboard.strategy FOR EACH ROW EXECUTE PROCEDURE blackboard.check_strategy_constraint();
 
 CREATE TRIGGER insert_command AFTER INSERT ON blackboard.command FOR EACH STATEMENT EXECUTE PROCEDURE blackboard.notify_client();
 CREATE TRIGGER insert_result AFTER INSERT ON blackboard.result FOR EACH STATEMENT EXECUTE PROCEDURE blackboard.notify_client();
