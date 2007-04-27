c1bc96a6f128d0c295b0ca0b1e050ed0ae7466eb
Marcel Loose <gmloose@users.noreply.github.com>
2007-04-27 12:52:57 +0000

    BugID: 991
    * Re-added "Operation" field.
    * Added functions to add Initialize, Finalize and NextChunk commands.

--- CEP/BB/BBSControl/sql/create_blackboard_types.sql
+++ CEP/BB/BBSControl/sql/create_blackboard_types.sql
@@ -1,6 +1,7 @@
 CREATE TYPE blackboard.iface_single_step_args AS
 (
     "Name"                  TEXT,
+    "Operation"             TEXT,
     "Baselines.Station1"    TEXT,
     "Baselines.Station2"    TEXT,
     "Correlation.Selection" TEXT,
@@ -13,6 +14,7 @@ CREATE TYPE blackboard.iface_single_step_args AS
 CREATE TYPE blackboard.iface_predict_args AS
 (
     "Name"                  TEXT,
+    "Operation"             TEXT,
     "Baselines.Station1"    TEXT,
     "Baselines.Station2"    TEXT,
     "Correlation.Selection" TEXT,
@@ -25,6 +27,7 @@ CREATE TYPE blackboard.iface_predict_args AS
 CREATE TYPE blackboard.iface_subtract_args AS
 (
     "Name"                  TEXT,
+    "Operation"             TEXT,
     "Baselines.Station1"    TEXT,
     "Baselines.Station2"    TEXT,
     "Correlation.Selection" TEXT,
@@ -37,6 +40,7 @@ CREATE TYPE blackboard.iface_subtract_args AS
 CREATE TYPE blackboard.iface_correct_args AS
 (
     "Name"                  TEXT,
+    "Operation"             TEXT,
     "Baselines.Station1"    TEXT,
     "Baselines.Station2"    TEXT,
     "Correlation.Selection" TEXT,
@@ -49,6 +53,7 @@ CREATE TYPE blackboard.iface_correct_args AS
 CREATE TYPE blackboard.iface_solve_args AS
 (
     "Name"                  TEXT,
+    "Operation"             TEXT,
     "Baselines.Station1"    TEXT,
     "Baselines.Station2"    TEXT,
     "Correlation.Selection" TEXT,
