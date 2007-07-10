61e79f6f0f19576fe0c73a6e183e000d588cbcdf
warmels <warmels@b3281354-443f-0410-9bd6-bf5212d75c11>
2007-07-10 14:29:34 +0000

    BugID: 1079
    toevoegen van meldingen (bijna klaar, alleen het daadwerkelijke toevoegen nog)

---
+++ trunk/support/tool/CRAFT/admin_meldingen/historie.php
@@ -0,0 +1,48 @@
+<?php
+
+	session_start();
+
+	include_once("../includes/vars.php");
+
+	echo("<HTML>");
+	echo("<HEAD></HEAD>");
+	echo("<BODY bgcolor=\"#B3CCE6\">");
+	
+	if(isset($_GET['c']))  {
+		
+		$query = "SELECT Laatste_Melding FROM comp_lijst WHERE Comp_Lijst_ID = '". $_GET['c'] ."'";
+		$resultaat = mysql_query($query);
+		$data = mysql_fetch_array($resultaat);
+
+		$query = "SELECT * FROM melding_lijst WHERE Meld_Lijst_ID ='". $data[0] ."'";
+		$resultaat = mysql_query($query);
+		$data = mysql_fetch_array($resultaat);
+
+		
+		//splitten op de spatie (formaat is als volgt: 2007-08-26 12:01:56)
+		$gedeeldveld=split(" ",$data['Meld_Datum']);
+		//datum veld opdelen zodat de jaar, maand en dagvelden makkelijk te benaderen zijn
+		$datum = split("-",$gedeeldveld[0]);
+
+
+		echo("<table border =\"1\">");
+		echo("<tr><td>". $datum[2] ."-". $datum[1] ."-". $datum[0]. "</td><td>" . substr($data[6], 0, 40) . "...</td><td><a href=\"../admin_meldingen/melding_info.php?c=".$data['Meld_Lijst_ID']."\" target=\"_blank\">Meer</a></td></tr>");
+
+			
+		while ($data['Voorgaande_Melding'] != 1) { 
+			$query = "SELECT * FROM melding_lijst WHERE Meld_Lijst_ID ='". $data['Voorgaande_Melding'] ."'";
+			$resultaat = mysql_query($query);
+			$data = mysql_fetch_array($resultaat);
+
+			//splitten op de spatie (formaat is als volgt: 2007-08-26 12:01:56)
+			$gedeeldveld=split(" ",$data['Meld_Datum']);
+			//datum veld opdelen zodat de jaar, maand en dagvelden makkelijk te benaderen zijn
+			$datum = split("-",$gedeeldveld[0]);
+		
+			echo("<tr><td>". $datum[2] ."-". $datum[1] ."-". $datum[0]. "</td><td>" .substr($data[6], 0, 40) . "...</td><td><a href=\"../admin_meldingen/melding_info.php?c=".$data['Meld_Lijst_ID']."\" target=\"_blank\">Meer</a></td></tr>");
+		}
+		echo("</table>");
+	}
+	echo("</BODY>");
+	echo("</HTML>");
+?>
