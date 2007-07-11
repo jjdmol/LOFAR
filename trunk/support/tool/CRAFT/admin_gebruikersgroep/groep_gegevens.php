388b6ba3e9b2f79d058da6e23c4fc8dbde20ac42
warmels <warmels@b3281354-443f-0410-9bd6-bf5212d75c11>
2007-07-11 08:56:30 +0000

    BugID: 1079
    meldingen kunnen toegevoegd / bewerkt / verwijderd worden.
    klein bugje bij het toevoegen van een gebruikersgroep gefixt

--- trunk/support/tool/CRAFT/admin_gebruikersgroep/groep_gegevens.php
+++ trunk/support/tool/CRAFT/admin_gebruikersgroep/groep_gegevens.php
@@ -19,35 +19,35 @@
 				<table>
 					<tr>
 						<td>Intro scherm zichtbaar:</td>
-						<td><input name="Intro_Zichtbaar" id="Intro_Zichtbaar" type="checkbox" value="<?php echo($data['Intro_Zichtbaar']); ?>"></td>
+						<td><input name="Intro_Zichtbaar" id="Intro_Zichtbaar" type="checkbox" value="<?php echo($data['Intro_Zichtbaar']); ?>" <?php if($data['Intro_Zichtbaar'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Componentscherm zichtbaar:</td>
-						<td><input name="Comp_Zichtbaar" id="Comp_Zichtbaar" type="checkbox" value="<?php echo($data['Comp_Zichtbaar']); ?>"></td>
+						<td><input name="Comp_Zichtbaar" id="Comp_Zichtbaar" type="checkbox" value="<?php echo($data['Comp_Zichtbaar']); ?>" <?php if($data['Comp_Zichtbaar'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Meldingscherm zichtbaar:</td>
-						<td><input name="Melding_Zichtbaar" id="Melding_Zichtbaar" type="checkbox" value="<?php echo($data['Melding_Zichtbaar']); ?>"></td>
+						<td><input name="Melding_Zichtbaar" id="Melding_Zichtbaar" type="checkbox" value="<?php echo($data['Melding_Zichtbaar']); ?>" <?php if($data['Melding_Zichtbaar'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Statistiekenscherm zichtbaar:</td>
-						<td><input name="Stats_Zichtbaar" id="Stats_Zichtbaar" type="checkbox" value="<?php echo($data['Stats_Zichtbaar']); ?>"></td>
+						<td><input name="Stats_Zichtbaar" id="Stats_Zichtbaar" type="checkbox" value="<?php echo($data['Stats_Zichtbaar']); ?>" <?php if($data['Stats_Zichtbaar'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Instellingenscherm zichtbaar:</td>
-						<td><input name="Inst_Zichtbaar" id="Inst_Zichtbaar" type="checkbox" value="<?php echo($data['Instel_Zichtbaar']); ?>"></td>
+						<td><input name="Inst_Zichtbaar" id="Inst_Zichtbaar" type="checkbox" value="<?php echo($data['Instel_Zichtbaar']); ?>" <?php if($data['Instel_Zichtbaar'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Toevoegrechten:</td>
-						<td><input name="Toevoeg_Rechten" id="Toevoeg_Rechten" type="checkbox" value="<?php echo($data['Toevoegen']); ?>"></td>
+						<td><input name="Toevoeg_Rechten" id="Toevoeg_Rechten" type="checkbox" value="<?php echo($data['Toevoegen']); ?>" <?php if($data['Toevoegen'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Bewerkenrechten:</td>
-						<td><input name="Bewerk_Rechten" id="Bewerk_Rechten" type="checkbox" value="<?php echo($data['Bewerken']); ?>"></td>
+						<td><input name="Bewerk_Rechten" id="Bewerk_Rechten" type="checkbox" value="<?php echo($data['Bewerken']); ?>" <?php if($data['Bewerken'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 					<tr>
 						<td>Verwijderrechten:</td>
-						<td><input name="Verwijder_Rechten" id="Verwijder_Rechten" type="checkbox" value="<?php echo($data['Verwijderen']); ?>"></td>
+						<td><input name="Verwijder_Rechten" id="Verwijder_Rechten" type="checkbox" value="<?php echo($data['Verwijderen']); ?>" <?php if($data['Verwijderen'] == 1) echo("CHECKED"); ?>></td>
 					</tr>
 				</table>
 				</form>
