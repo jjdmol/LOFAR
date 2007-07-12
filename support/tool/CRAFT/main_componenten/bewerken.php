09ce26fd08564bedd6cf6e1bb8f4fa57aaa9588f
warmels <warmels@b3281354-443f-0410-9bd6-bf5212d75c11>
2007-07-12 10:14:04 +0000

    BugID: 1079
    bewerken van componenten in het hoofdprogramma ingebouwd

--- support/tool/CRAFT/main_componenten/bewerken.php
+++ support/tool/CRAFT/main_componenten/bewerken.php
@@ -4,6 +4,7 @@
 	  $_SESSION['huidige_pagina'] = $_SESSION['pagina'] . 'main.php?p='.$_SESSION['main_deel'].'&s=3';
 	  
 	  require_once($_SESSION['pagina'] . 'includes/login_funcs.php');
+		include_once($_SESSION['pagina'] . 'includes/datum_tijd_functies.php');
 		
 	  //controleren of er iemand ingelogd is...
 	  if ($LOGGED_IN = user_isloggedin()) {
@@ -11,6 +12,7 @@
 	  	?>
 	  	<div id="linkerdeel">
 	  		<?php 
+	  			echo("<script language=\"JavaScript\" src=\"". $_SESSION['pagina'] ."algemene_functionaliteit/comp_toevoegen_functies.php\"></script>");
 	  			echo("<script language=\"JavaScript\" src=\"". $_SESSION['pagina'] ."includes/tree.js\"></script>");
 					echo("<script language=\"JavaScript\" src=\"". $_SESSION['pagina'] ."includes/tree_items.php\"></script>");
 					echo("<script language=\"JavaScript\" src=\"". $_SESSION['pagina'] ."includes/tree_tpl.js\"></script>");
@@ -25,7 +27,11 @@
 	    <div id="rechterdeel">
 	    	
 	    	<h2>Componenten bewerken</h2>
-	    
+	    	
+	    	<?php
+					include_once($_SESSION['pagina'] . 'algemene_functionaliteit/comp_bewerken.php');	
+	    	?>
+	    	
 	    </div>
 	
 	<?php  
