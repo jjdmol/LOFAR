f1069c38671b260ee1d97192fa99bee536324403
warmels <warmels@b3281354-443f-0410-9bd6-bf5212d75c11>
2007-08-31 14:56:51 +0000

    BugID: 1079
    database wachtwoord aangepast

--- support/tool/CRAFT/includes/vars.php
+++ support/tool/CRAFT/includes/vars.php
@@ -1,7 +1,7 @@
 <?php
 	$db_host = 'localhost';
 	$db_user = 'root';
-	$db_password = 'root';
+	$db_password = 'zw0lle';
 	$database = 'lofar-craft';
 	mysql_connect($db_host, $db_user, $db_password);
 	mysql_select_db($database);
