f2be64552b344dcbcf11cebf6b1d75f465eaa984
romein <romein@b3281354-443f-0410-9bd6-bf5212d75c11>
2007-09-17 15:16:53 +0000

    BugID: 225 IP addresses renumbered.

--- trunk/Appl/CEP/CS1/CS1_Run/src/CS1_Hosts.py
+++ trunk/Appl/CEP/CS1/CS1_Run/src/CS1_Hosts.py
@@ -1,20 +1,25 @@
 from Hosts import *
+import sys
 
 
 liifen    = ClusterFEN(name = 'liifen',
                        address = '129.125.99.51')
-liifen.setSlavesByPattern('lii%03d', '10.20.150.%d', [1,2,3,4,5,6,7,8,9,10,11,12,13])
+liifen.setSlavesByPattern('lii%03d', '10.162.0.%d', [1,2,3,4,5,6,7,8,9,10,11,12,13])
 
 listfen   = ClusterFEN(name = 'listfen'    ,
                        address = '129.125.99.50')
-#listfen.setSlavesByPattern('list%03d', '10.20.170.%d', [1,2,3,4,5,6,7,8,9,10,11,12])
-listfen.setSlavesByPattern('list%03d', '10.20.170.%d', [1,3,5,7,2,4,10,11,12])
+listfen.setSlavesByPattern('list%03d', '10.181.0.%d', [1,3,5,7,2,4,6])
+listfen.slaves.append(ClusterSlave('lifs001', '10.182.0.1'))
+listfen.slaves.append(ClusterSlave('lifs002', '10.182.0.2'))
+listfen.slaves.append(ClusterSlave('lifs003', '10.182.0.3'))
+listfen.slaves.append(ClusterSlave('lifs004', '10.182.0.4'))
+listfen.slaves.append(ClusterSlave('lifs005', '10.182.0.5'))
 
 
 list001   = Host(name = 'list001'  , \
-                 address = '10.20.170.1')
+                 address = '10.181.0.1')
 list002   = Host(name = 'list002'  , \
-                 address = '10.20.170.2')
+                 address = '10.181.0.2')
 hpclf     = Host(name = 'hpclf'  , \
                  address = 'hpclf1.service.rug.nl')
 bglfen1   = Host(name = 'bglfen1', \
