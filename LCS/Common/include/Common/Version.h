be340a5e97201235564bb7fcecb7853f08fd6f92
Ger van Diepen <gervandiepen@users.noreply.github.com>
2008-01-22 13:20:17 +0000

    bug 468: Handle version, revision, and build time and machine

--- LCS/Common/include/Common/Version.h
+++ LCS/Common/include/Common/Version.h
@@ -25,54 +25,119 @@
 
 #include <string>
 #include <vector>
-#include <Common/StreamUtil.h>
+#include <iosfwd>
 
-// Class to get the version (revision) of the package and the packages it uses.
+// Class to get info like version and revision of the package and the
+// packages it uses.
 
 namespace LOFAR {
 
   class Version
   {
   public:
-    // Add package/version to vector if not existing yet.
-    // It returns false if the version differs from the main version.
-    static bool fillVersion (const std::string& package,
-			     const std::string& version,
-			     const std::string& versionSuffix,
-			     const std::string& mainVersion,
-			     std::vector<std::string>& v);
+    // Define the function to get the version info of a package.
+    typedef Version GetInfoFunc();
+    // Define the struct to hold the level and getInfo function of a package.
+    typedef std::pair<int, GetInfoFunc*> PkgInfo;
+
+    // Construct a Version object from the relevant package info.
+    Version (const std::string& packageName,
+	     const std::string& version,
+	     const std::string& confVersion,
+	     const std::string& revision,
+	     const std::string& packageRevision,
+	     const std::string& nrChangedFiles,
+	     const std::string& buildTime,
+	     const std::string& buildMachine);
+
+    // Get the package name.
+    const std::string& packageName() const
+      { return itsPackageName; }
+    // Get the package version (as in repository)
+    const std::string& version() const
+      { return itsVersion; }
+    // Get the package version as given in configure.in.
+    const std::string& confVersion() const
+      { return itsConfVersion; }
+    // Get the global revision number in repository.
+    const std::string& revision() const
+      { return itsRevision; }
+    // Ge the revision number of the package, thus the last revision something
+    // was checked in for this package version.
+    const std::string& packageRevision() const
+      { return itsPackageRevision; }
+    // Get the number of files in the ackage whch wre different from the
+    // repository when building the package.
+    const std::string& nrChangedFiles() const
+      { return itsNrChangedFiles; }
+    // Get the date/time the package was built (i.e. configured).
+    const std::string& buildTime() const
+      { return itsBuildTime; }
+    // Get the name of the machine on which the package was built.
+    const std::string& buildMachine() const
+      { return itsBuildMachine; }
+
+    // The package name can be changed.
+    void setPackageName (const std::string& name)
+      { itsPackageName = name; }
+
+    // Get the package T and all packages it uses.
+    template<typename T>
+    static std::vector<PkgInfo> getPackages()
+    {
+      std::vector<PkgInfo> vec;
+      T::fillInfo (0, vec);
+      return vec;
+    }
 
     // Show the package or application name and the versions of the packages
     // being used.
+    // Possible types are:
+    // - tree:  show used packages as a tree (giving dependencies).
+    // - full:  show each paclage fully (but only once)
+    // - other: show each package briefly (name, version, revision, nrchanged)
     // Use like:
     // @code
     //  Version::show<BlobVersion> ("applname", std::cout);
     // @endcode
-    template<typename T> static void show (const std::string& name,
+    template<typename T> static void show (const std::string& type,
+					   const std::string& applName,
 					   std::ostream& os)
     {
-      std::vector<std::string> v;
-      bool sameVers = T::fillVersion (T::getVersion(), v);
-      os << name << ": main version = " << T::getVersion();
-      if (!sameVers) os << '*';
-      os << endl;
-      os << "  packages: " << v << endl;
+      doShow (type, applName, os, getPackages<T>());
     }
 
-    // Get the main version and the individual package versions being used.
-    // It returns false if a package version differs from the main version.
-    // Use like:
-    // @code
-    //  std::string mainVersion;
-    //  std::vector<std::string> packageVersions;
-    //  Version::get<BlobVersion> (mainVersion, packageVersions);
-    // @endcode
-    template<typename T> static bool get (std::string& mainVersion,
-					  std::vector<std::string>& pkgVers)
-    {
-      mainVersion = T::getVersion();
-      return T::fillVersion (mainVersion, pkgVers);
-    }
+    // Show all info. It is indented as given.
+    // A warning is printed if sameRev is false, which indicates that some
+    // package used have a different revision number.
+    void showAll (const std::string& indent,
+		  bool sameRev, std::ostream& os) const;
+
+    // Show brief info. It is indented as given.
+    void showBrief (const std::string& indent,
+		    std::ostream& os) const;
+
+  private:
+    // Show the info as needed.
+    static void doShow (const std::string& type,
+			const std::string& applName,
+			std::ostream& os,
+			const std::vector<PkgInfo>&);
+
+    // Show the info as a tree of all packages used.
+    static void doShowTree (const Version& versRoot,
+			    std::ostream& os,
+			    const std::vector<PkgInfo>& vec);
+
+    //# Data members.
+    std::string itsPackageName;
+    std::string itsVersion;
+    std::string itsConfVersion;
+    std::string itsRevision;
+    std::string itsPackageRevision;
+    std::string itsNrChangedFiles;
+    std::string itsBuildTime;
+    std::string itsBuildMachine;
   };
 
 }
--- LCS/Common/src/Version.cc
+++ LCS/Common/src/Version.cc
@@ -21,29 +21,151 @@
 //#  $Id$
 
 #include <Common/Version.h>
+#include <ostream>
+#include <map>
 
 namespace LOFAR {
 
-  bool Version::fillVersion (const std::string& package,
-			     const std::string& version,
-			     const std::string& versionSuffix,
-			     const std::string& mainVersion,
-			     std::vector<std::string>& v)
+  Version::Version (const std::string& packageName,
+		    const std::string& version,
+		    const std::string& confVersion,
+		    const std::string& revision,
+		    const std::string& packageRevision,
+		    const std::string& nrChangedFiles,
+		    const std::string& buildTime,
+		    const std::string& buildMachine)
+    : itsPackageName     (packageName),
+      itsVersion         (version),
+      itsConfVersion     (confVersion),
+      itsRevision        (revision),
+      itsPackageRevision (packageRevision),
+      itsNrChangedFiles  (nrChangedFiles),
+      itsBuildTime       (buildTime),
+      itsBuildMachine    (buildMachine)
+  {}
+
+  void Version::doShow (const std::string& type,
+			const std::string& applName,
+			std::ostream& os,
+			const std::vector<PkgInfo>& vec)
   {
-    bool sameVers = (version == mainVersion);
-    std::string pv = package + '/' + version + versionSuffix;
-    // Only insert if not existing yet.
-    // Later you might remove the existing one, so the dependencies are
-    // shown in a somewhat better way.
-    for (std::vector<std::string>::const_iterator iter=v.begin();
-	 iter!=v.end();
+    if (vec.empty()) {
+      return;
+    } 
+    // Get main info.
+    // Set name to given name.
+    Version versRoot = vec[0].second();
+    versRoot.setPackageName (applName);
+    std::string mainRevision = versRoot.revision();
+    // Print tree if required.
+    if (type == "tree") {
+      doShowTree (versRoot, os, vec);
+      return;
+    }
+    bool full = (type == "full");
+    // Only keep a single package in the vector at the highest level.
+    // Map package to vector index.
+    bool sameRev = true;
+    int maxLevel = 0;
+    std::vector<PkgInfo> vflat;
+    std::map<GetInfoFunc*, int> m;
+    for (std::vector<PkgInfo>::const_iterator iter = vec.begin();
+	 iter != vec.end();
 	 ++iter) {
-      if (*iter == pv) {
-	return sameVers;
+      int level = iter->first;
+      GetInfoFunc* func = iter->second;
+      std::map<GetInfoFunc*,int>::iterator mfnd = m.find(func);
+      if (mfnd == m.end()) {
+	m[func] = vflat.size();
+	vflat.push_back (*iter);
+	sameRev = sameRev && (func().revision() == mainRevision);
+      } else {
+	int index = mfnd->second;
+	if (level > vflat[index].first) {
+	  vflat[index].first= level;
+	}
+      }
+      if (level > maxLevel) {
+	maxLevel = level;
       }
     }
-    v.push_back (pv);
-    return sameVers;
+    versRoot.showAll (std::string(), sameRev, os);
+    os << " packages: " << std::endl;
+    for (int level=1; level<=maxLevel; ++level) {
+      for (std::vector<PkgInfo>::const_iterator viter = vflat.begin();
+	   viter != vflat.end();
+	   ++viter) {
+	if (viter->first == level) {
+	  Version vers = viter->second();
+	  if (full) {
+	    vers.showAll ("  ", true, os);
+	  } else {
+	    vers.showBrief ("  ", os);
+	  }
+	}
+      }
+    }
+  }
+
+  void Version::showAll (const std::string& indent,
+			 bool sameRev, std::ostream& os) const
+  { 
+    os << indent << packageName() << ": version = " << version();
+    if (version() != confVersion()) {
+      os << " (in configure.in: " << confVersion() << ')';
+    }
+    os << std::endl;
+    os << indent << " overall revision  = " << revision();
+    if (!sameRev) os << " (note: used packages have different revision)";
+    os << std::endl;
+    os << indent << " package revision  = " << packageRevision()
+       << " (last change in package)" << std::endl;
+    os << indent << " built on " << buildMachine() << " at "
+       << buildTime() << std::endl;
+    if (nrChangedFiles() != "0") {
+      os << indent << "  " << nrChangedFiles()
+	 << " files were different from the repository" << std::endl;
+    }
+    os << std::endl;
+  }
+
+  void Version::showBrief (const std::string& indent,
+			   std::ostream& os) const
+  {
+    os << indent << packageName();
+    if (version() != "trunk") {
+      os << '-' << version();
+    }
+    os << " (rev." << revision() << ')';
+    if (nrChangedFiles() != "0") {
+      os << ' ' << nrChangedFiles()
+	 << " changed files" << std::endl;
+    }
+    os << std::endl;
+  }
+
+  void Version::doShowTree (const Version& versRoot,
+			    std::ostream& os,
+			    const std::vector<PkgInfo>& vec)
+  {
+    versRoot.showAll ("", true, os);
+    std::vector<PkgInfo>::const_iterator viter = vec.begin();
+    // Skip first one if name is the same as applName in versRoot.
+    if (viter->second().packageName() == versRoot.packageName()) {
+      ++viter;
+    }
+    for (; viter != vec.end(); ++viter) {
+      // Blanks according to level.
+      for (int i=0; i<viter->first; ++i) {
+	os << ' ';
+      }
+      Version vers = viter->second();
+      os << "  " << vers.packageName();
+      if (vers.version() != "trunk") {
+	os << '-' << vers.version();
+      }
+      os << std::endl;
+    }
   }
 
 }
