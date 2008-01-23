476eae20cdb04de05f1563dc55b7706b5a5a949a
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-01-23 14:50:54 +0000

    Bug 1000: Corrected sources for the split of RTCCommon/Marshalling into Marshalling and MarshallBlitz.

--- trunk/MAC/APL/PIC/RSP_Protocol/src/WGSettings.cc
+++ trunk/MAC/APL/PIC/RSP_Protocol/src/WGSettings.cc
@@ -24,7 +24,7 @@
 #include <Common/LofarLogger.h>
 
 #include <APL/RSP_Protocol/WGSettings.h>
-#include <APL/RTCCommon/Marshalling.h>
+#include <APL/RTCCommon/MarshallBlitz.h>
 #include "EPA_Protocol.ph"
 
 #include <math.h>
