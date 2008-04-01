fc68954ccdf2a6d38d81d608609f217ebe682e9d
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-04-01 12:47:33 +0000

    Bug 1000: CDO lenght increased from 22 to 30;

--- trunk/MAC/APL/PIC/RSP_Protocol/include/APL/RSP_Protocol/MEPHeader.h
+++ trunk/MAC/APL/PIC/RSP_Protocol/include/APL/RSP_Protocol/MEPHeader.h
@@ -304,7 +304,7 @@ namespace LOFAR {
        * allow setting the UDP/IP header and some
        * settings to set interleaving of beamlets.
        */
-      static const uint16 CDO_SETTINGS_SIZE     = 22;
+      static const uint16 CDO_SETTINGS_SIZE     = 30;
       static const uint16 CDO_HEADER_SIZE       = 32;
 
       static const uint16 BS_NOF_SAMPLES_PER_SYNC_SIZE = 4;
