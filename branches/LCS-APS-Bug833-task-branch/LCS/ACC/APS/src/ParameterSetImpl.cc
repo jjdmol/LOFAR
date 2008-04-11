bda921ebc0459578673a99060fecf0acf70f226a
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-04-11 08:01:16 +0000

    Bug 833: Oops! Something went terribly wrong, so I reverted this merge. Things got a little complicated because this experimental work has been lying around for too long, and the move from CVS to Subversion did not make things easier.
    Now in order to get this experimental work merged with the trunk, I should
    draw a picture first.
    
                        10668
        +--------^--------+----  Bug833
       /        /      \
      +--------+--------v------  trunk
    10089    10163    10176
    
    10089 is the task-root (the beginning of the Bug833 task branch)
    10163 is a merge from the trunk with the Bug833 task branch
    10176 is a merge from the Bug833 task branch with the trunk
    10668 is the commit of the lexical_cast<> stuff.
    
    I should only merge the changes that were made after the merge with the
    trunk. I.e., merge 10176:HEAD on the branch with HEAD on the trunk.

--- branches/LCS-APS-Bug833-task-branch/LCS/ACC/APS/src/ParameterSetImpl.cc
+++ branches/LCS-APS-Bug833-task-branch/LCS/ACC/APS/src/ParameterSetImpl.cc
@@ -69,7 +69,13 @@ ParameterSetImpl::~ParameterSetImpl()
 //
 std::ostream&	operator<< (std::ostream& os, const ParameterSetImpl &thePS)
 {
-	thePS.writeStream(os);
+	KeyValueMap::const_iterator		iter = thePS.begin();
+
+	while (iter != thePS.end()) {
+		os << "[" << iter->first << "],[" << iter->second << "]" << endl;
+		iter++;
+	}
+
 	return os;
 }
 
@@ -201,7 +207,7 @@ void ParameterSetImpl::readFile(const	string&	theFilename,
 		       formatString("Unable to open file %s", theFilename.c_str()));
 	}
 
-	readStream(paramFile, prefix, merge);
+	addStream(paramFile, prefix, merge);
 
 	paramFile.close();
 }
@@ -218,20 +224,20 @@ void ParameterSetImpl::readBuffer(const	string&	theBuffer,
 {
 	istringstream		iss(theBuffer, istringstream::in);
 
-	readStream(iss, prefix, merge);
+	addStream(iss, prefix, merge);
 
 }
 
 //
-// readStream
+// addStream
 // (private)
 //
 // Disentangles the stream and adds the Key-Values pair to the current 
 // ParameterSetImpl.
 //
-void ParameterSetImpl::readStream(istream&	inputStream, 
-				  const string&	prefix,
-				  bool		merge)
+void ParameterSetImpl::addStream(istream&	inputStream, 
+				 const string&	prefix,
+				 bool		merge)
 {
 	char	paramLine[1024];
 	char*	keyStr;
@@ -1026,9 +1032,22 @@ void ParameterSetImpl::writeFile(const string&	theFilename,
 	}
 
 	// Write all the pairs to the file
-	writeStream(paramFile);
+	const_iterator		curPair = begin();
+	while (curPair != end()) {
+		// Key can always be written.
+		paramFile << curPair->first << "=";
 
-	// Close the file
+		//* value may begin or end in a space: use quotes
+		if (*(curPair->second.begin()) == ' ' || *(curPair->second.rbegin()) == ' ') {
+			paramFile << "\"" << curPair->second << "\"" << endl;
+		}
+		else {
+ 			paramFile << curPair->second << endl;
+		}
+
+		curPair++;
+	}
+
 	paramFile.close();
 }
 
@@ -1040,37 +1059,23 @@ void ParameterSetImpl::writeFile(const string&	theFilename,
 //
 void ParameterSetImpl::writeBuffer(string&	aBuffer) const
 {
-	ostringstream oss;
-	writeStream(oss);
-	aBuffer = oss.str();
-}
-
-//
-// writeStream
-//
-// Writes the Key-Value pairs from the current ParameterSetImpl to the given
-// output stream.
-//
-void ParameterSetImpl::writeStream(ostream&	os) const
-{
 	// Write all the pairs to the file
 	const_iterator		curPair = begin();
 	while (curPair != end()) {
 		// Key can always be written.
-		os << curPair->first << "=";
+		aBuffer += (curPair->first + "=");
 
 		//* value may begin or end in a space: use quotes
 		if (*(curPair->second.begin()) == ' ' || *(curPair->second.rbegin()) == ' ') {
-			os << "\"" << curPair->second << "\"" << endl;
+			aBuffer += ("\"" + curPair->second + "\"\n");
 		}
 		else {
- 			os << curPair->second << endl;
+ 			aBuffer += (curPair->second + "\n");
 		}
 
 		curPair++;
 	}
 }
-
 //-------------------------- global functions -----------------------------
 //
 // StringToTime_t (aString) 
