df2dc37ddf81c3ede4e509d2d320ee217371e358
gels <gels@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-04-14 09:01:36 +0000

    bug 1005: Added new stations.

--- Appl/CEP/CS1/CS1_Run/src/CS1_Stations.py
+++ Appl/CEP/CS1/CS1_Run/src/CS1_Stations.py
@@ -25,7 +25,7 @@ CS010_HBA0 = [Station('CS010_HBA0')]
 CS010_HBA1 = [Station('CS010_HBA1')]
 CS010_HBA2 = [Station('CS010_HBA2')]
 CS010_HBA3 = [Station('CS010_HBA3')]
-CS010_HBAs = CS010_HBA0 + CS010_HBA1 + CS010_HBA2 + CS010_HBA3
+CS010_4HBAs = CS010_HBA0 + CS010_HBA1 + CS010_HBA2 + CS010_HBA3
 
 CS010_us0  = [Station('CS010_us0')]
 CS010_us1  = [Station('CS010_us1')]
@@ -57,7 +57,7 @@ CS008_HBA0 = [Station('CS008_HBA0')]
 CS008_HBA1 = [Station('CS008_HBA1')]
 CS008_HBA2 = [Station('CS008_HBA2')]
 CS008_HBA3 = [Station('CS008_HBA3')]
-CS008_HBAs = CS008_HBA0 + CS008_HBA1 + CS008_HBA2 + CS008_HBA3
+CS008_4HBAs = CS008_HBA0 + CS008_HBA1 + CS008_HBA2 + CS008_HBA3
 
 CS008_us0  = [Station('CS008_us0')]
 CS008_us1  = [Station('CS008_us1')]
@@ -81,7 +81,7 @@ CS001_HBA0 = [Station('CS001_HBA0')]
 CS001_HBA1 = [Station('CS001_HBA1')]
 CS001_HBA2 = [Station('CS001_HBA2')]
 CS001_HBA3 = [Station('CS001_HBA3')]
-CS001_HBAs = CS001_HBA0 + CS001_HBA1 + CS001_HBA2 + CS001_HBA3
+CS001_4HBAs = CS001_HBA0 + CS001_HBA1 + CS001_HBA2 + CS001_HBA3
 
 CS001_us0  = [Station('CS001_us0')]
 CS001_us1  = [Station('CS001_us1')]
@@ -105,7 +105,7 @@ CS016_HBA0 = [Station('CS016_HBA0')]
 CS016_HBA1 = [Station('CS016_HBA1')]
 CS016_HBA2 = [Station('CS016_HBA2')]
 CS016_HBA3 = [Station('CS016_HBA3')]
-CS016_HBAs = CS016_HBA0 + CS016_HBA1 + CS016_HBA2 + CS016_HBA3
+CS016_4HBAs = CS016_HBA0 + CS016_HBA1 + CS016_HBA2 + CS016_HBA3
 
 CS016_us0  = [Station('CS016_us0')]
 CS016_us1  = [Station('CS016_us1')]
@@ -119,17 +119,35 @@ CS030_dipole10 = [Station('CS030_dipole10')]
 CS030_dipole14 = [Station('CS030_dipole14')]
 CS030_4dipoles = CS030_dipole0 + CS030_dipole7 + CS030_dipole10 + CS030_dipole14
 
+CS030_HBA_S02  = [Station('CS030_HBA_S02')]
+CS030_HBA_S04  = [Station('CS030_HBA_S04')]
+CS030_HBA_S10  = [Station('CS030_HBA_S10')]
+CS030_HBA_S15  = [Station('CS030_HBA_S15')]
+CS030_4HBAs     = CS030_HBA_S02 + CS030_HBA_S04 + CS030_HBA_S10 + CS030_HBA_S15
+
 CS031_dipole16 = [Station('CS031_dipole16')]
 CS031_dipole22 = [Station('CS031_dipole22')]
 CS031_dipole24 = [Station('CS031_dipole24')]
 CS031_dipole31 = [Station('CS031_dipole31')]
 CS031_4dipoles = CS031_dipole16 + CS031_dipole22 + CS031_dipole24 + CS031_dipole31
 
+CS031_HBA_S18 = [Station('CS031_HBA_S18')]
+CS031_HBA_S24 = [Station('CS031_HBA_S24')]
+CS031_HBA_S25 = [Station('CS031_HBA_S25')]
+CS031_HBA_S27 = [Station('CS031_HBA_S27')]
+CS031_4HBAs    = CS031_HBA_S18 + CS031_HBA_S24 + CS031_HBA_S25 + CS031_HBA_S27
+
 CS032_dipole34 = [Station('CS032_dipole34')]
 CS032_dipole37 = [Station('CS032_dipole37')]
 CS032_dipole42 = [Station('CS032_dipole42')]
 CS032_dipole46 = [Station('CS032_dipole46')]
 CS032_4dipoles = CS032_dipole34 + CS032_dipole37 + CS032_dipole42 + CS032_dipole46
+
+CS032_TILE1 = [Station('CS032_TILE1')]
+CS032_TILE2 = [Station('CS032_TILE2')]
+CS032_TILE3 = [Station('CS032_TILE3')]
+CS032_TILE4 = [Station('CS032_TILE4')]
+CS032_4Tiles = CS032_TILE1 + CS032_TILE2 + CS032_TILE3 + CS032_TILE4
 
 CS001T_1_dipole0  = [Station('CS001T_1.dipole0')]
 CS001T_1_dipole4  = [Station('CS001T_1.dipole4')]
@@ -184,7 +202,8 @@ I_91 = [Station('I_91')]
 AllMicroStations = CS010_4us + CS001_4us + CS008_4us + CS016_4us
 AllDipoles       = CS010_4dipoles_1 + CS001_4dipoles_1 + CS008_4dipoles_1 + CS016_4dipoles_1
 AllDipolesMixed  = CS010_4dipoles_1 + CS001_4dipoles_2 + CS008_4dipoles_2 + CS016_4dipoles_2
-AllHBAs          = CS010_HBAs + CS001_HBAs + CS008_HBAs + CS016_HBAs
+AllHBAs          = CS010_4HBAs + CS001_4HBAs + CS008_4HBAs + CS016_4HBAs
+All24HBAs        = CS030_4HBAs + CS031_4HBAs + CS032_4Tiles + CS001_4HBAs + CS008_4HBAs + CS016_4HBAs
 Mixed            = CS010_us0 + CS010_dipole4 + CS010_dipole8 + CS010_dipole12 + CS001_4dipoles_1 + CS008_4dipoles_1 + CS016_4dipoles_1
 All24Dipoles     = CS001_4dipoles_1 + CS008_4dipoles_1 + CS016_4dipoles_1 + CS030_4dipoles + CS031_4dipoles + CS032_4dipoles
 All23Dipoles     = CS001_4dipoles_1 + CS008_4dipoles_1 + CS016_us0 + CS016_us1 + CS016_us2 + CS030_4dipoles + CS031_4dipoles + CS032_4dipoles
