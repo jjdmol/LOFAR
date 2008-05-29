4b2f6b183c187ed4282e5177af43aca1b01ffbe9
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-05-29 12:36:16 +0000

    bug 1193: First version

---
+++ trunk/CEP/BB/MWControl/include/MWControl/PredifferProxy.h
@@ -0,0 +1,45 @@
+/// @file
+/// @brief Base class for BBSKernel prediffer behaviour.
+///
+/// @copyright (c) 2007 ASKAP, All Rights Reserved.
+/// @author Ger van Diepen (diepen AT astron nl)
+///
+//# $Id$
+
+#ifndef LOFAR_MWCONTROL_PREDIFFERPROXY_H
+#define LOFAR_MWCONTROL_PREDIFFERPROXY_H
+
+#include <MWCommon/WorkerProxy.h>
+
+
+namespace LOFAR { namespace CEP {
+
+  /// @ingroup mwcontrol
+  /// @brief Base class for BBSKernel prediffer behaviour.
+
+  /// This class is the base class for BBSKernel prediffer behaviour.
+  /// A derived class needs to do the concrete implementation of the
+  /// virtual BBSProxy functions.
+  /// By registering the desired concrete class with type name "Prediffer"
+  /// in the WorkerFactory, the MW framework will use that class as the
+  /// prediffer proxy.
+  ///
+  /// In this way it is possible to use a simple test class (which can
+  /// merely output the command on cout) to check the control flow.
+  /// This is used by the test program \a tMWControl.
+
+  class PredifferProxy: public WorkerProxy
+  {
+  public:
+    PredifferProxy()
+    {}
+
+    ~PredifferProxy();
+
+    /// Get the work types supported by the proxy (which is solve).
+    virtual std::vector<int> getWorkTypes() const;
+  };
+
+}} /// end namespaces
+
+#endif
