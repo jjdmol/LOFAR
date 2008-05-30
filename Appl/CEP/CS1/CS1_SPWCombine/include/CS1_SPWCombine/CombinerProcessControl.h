c042e1a043b6346b53ce7a578a9c9ca6a4195d74
renting <renting@astron.nl>
2008-05-30 13:28:12 +0000

    Bug #1064: added missing function

--- Appl/CEP/CS1/CS1_SPWCombine/include/CS1_SPWCombine/CombinerProcessControl.h
+++ Appl/CEP/CS1/CS1_SPWCombine/include/CS1_SPWCombine/CombinerProcessControl.h
@@ -74,6 +74,7 @@ namespace LOFAR
       tribool run      (void);
 
       tribool pause(const std::string&);
+      tribool release(void);
       tribool quit(void);
       tribool recover(const std::string&);
       tribool reinit(const  std::string&);
