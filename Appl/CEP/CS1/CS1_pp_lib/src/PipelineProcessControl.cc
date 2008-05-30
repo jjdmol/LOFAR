871976e7fb1d2f04a9f6f71a280a33659ec7a0f8
renting <renting@astron.nl>
2008-05-30 15:48:39 +0000

    Bug #1113: added missing file

---
+++ Appl/CEP/CS1/CS1_pp_lib/src/PipelineProcessControl.cc
@@ -0,0 +1,161 @@
+/***************************************************************************
+ *   Copyright (C) 2006-8 by ASTRON/LOFAR, Adriaan Renting                 *
+ *   renting@astron.nl                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <iostream>
+#include <casa/Inputs/Input.h>
+#include <ms/MeasurementSets.h>
+
+#include <CS1_IDPPP/PipelineProcessControl.h>
+//#include "MS_File.h"
+//#include "ComplexMedianPipeline.h"
+
+#define PIPELINE_VERSION "0.10"
+
+namespace LOFAR
+{
+  namespace CS1
+  {
+    //===============>>> PipelineProcessControl::PipelineProcessControl  <<<===============
+    PipelineProcessControl::PipelineProcessControl()
+    : ProcessControl()
+    {
+      myMS       = NULL;
+      itsPipeline = NULL;
+    }
+
+    //===============>>> PipelineProcessControl::~PipelineProcessControl  <<<==============
+    PipelineProcessControl::~PipelineProcessControl()
+    {
+      if (myMS)
+      { delete myMS;
+      }
+      if (itsPipeline)
+      { delete itsPipeline;
+      }
+    }
+
+    //===============>>> PipelineProcessControl::define  <<<==============================
+    tribool PipelineProcessControl::define()
+    {
+      LOFAR::ACC::APS::ParameterSet* ParamSet = LOFAR::ACC::APS::globalParameterSet();
+      itsMS       = ParamSet->getString("ms");
+//      itsFlagData = ParamSet->getBool("flagdata");
+//      itsFlagRMS  = ParamSet->getBool("flagrms");
+//      itsExisting = ParamSet->getBool("existing");
+//      itsWindow   = ParamSet->getInt32("window");
+//      itsCrosspol = ParamSet->getBool("crosspol");
+//      itsMin      = ParamSet->getDouble("min");
+//      itsMax      = ParamSet->getDouble("max");
+      return true;
+    }
+
+    //===============>>> PipelineProcessControl::run  <<<=================================
+    tribool PipelineProcessControl::run()
+    {
+      try{
+      std::cout << "Runnning pipeline please wait..." << std::endl;
+//      itsPipeline->FlagDataOrBaselines(itsFlagData,
+//                                      itsFlagRMS,
+//                                      itsExisting);
+      }
+      catch(casa::AipsError& err)
+      {
+        std::cerr << "Aips++ error detected: " << err.getMesg() << std::endl;
+        return false;
+      }
+      return true;
+    }
+
+    //===============>>> PipelineProcessControl::init  <<<================================
+    tribool PipelineProcessControl::init()
+    {
+      try {
+      using std::cout;
+      using std::cerr;
+      using std::endl;
+
+      cout  << string(PIPELINE_VERSION) + string(" Integrated Data Post Processing Pipeline by Adriaan Renting and others for LOFAR CS1 data\n") +
+              string("This is experimental software, please report errors or requests to renting@astron.nl\n") +
+              string("Documentation can be found at: www.lofar.org/operations\n");
+      cout << itsMS << endl;
+//      myMS       = new WSRT::MS_File(itsMS);
+//      itsPipeline = new WSRT::ComplexMedianPipeline (myMS,
+//                                                  itsWindow,
+//                                                  itsCrosspol,
+//                                                  itsMin,
+//                                                  itsMax);
+      }
+      catch(casa::AipsError& err)
+      {
+        std::cerr << "Aips++ error detected: " << err.getMesg() << std::endl;
+        return false;
+      }
+      return true;
+    }
+
+    //===============>>> PipelineProcessControl::pause  <<<===============================
+    tribool PipelineProcessControl::pause(const std::string&)
+    { return false;
+    }
+
+    //===============>>> PipelineProcessControl::quit  <<<================================
+    tribool PipelineProcessControl::quit()
+    {
+      if (myMS)
+      {
+        delete myMS;
+        myMS = NULL;
+      }
+      if (itsPipeline)
+      {
+        delete itsPipeline;
+        itsPipeline = NULL;
+      }
+      return true;
+    }
+
+    //===============>>> PipelineProcessControl::release  <<<=============================
+    tribool PipelineProcessControl::release()
+    { return false;
+    }
+
+    //===============>>> PipelineProcessControl::recover  <<<=============================
+    tribool PipelineProcessControl::recover(const std::string&)
+    { return false;
+    }
+
+    //===============>>> PipelineProcessControl::reinit  <<<==============================
+    tribool PipelineProcessControl::reinit(const  std::string&)
+    { return false;
+    }
+
+    //===============>>> PipelineProcessControl::askInfo  <<<=============================
+    std::string PipelineProcessControl::askInfo(const std::string&)
+    { return std::string("");
+    }
+
+    //===============>>> PipelineProcessControl::snapshot  <<<============================
+    tribool PipelineProcessControl::snapshot(const std::string&)
+    { return false;
+    }
+  } //namespace CS1
+}; //namespace LOFAR
