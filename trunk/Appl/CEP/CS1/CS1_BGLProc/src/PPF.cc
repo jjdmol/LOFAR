c839e62fc9dac476de5a6b37685605d9235ef0e7
romein <romein@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-07-15 12:16:29 +0000

    bug 225: i4complex now represents -7.5, -6.5, -5.5 ... -0.5, +0.5 ... +7.5 instead of -8, -7, ... +7.

--- trunk/Appl/CEP/CS1/CS1_BGLProc/src/PPF.cc
+++ trunk/Appl/CEP/CS1/CS1_BGLProc/src/PPF.cc
@@ -62,7 +62,8 @@ PPF::PPF(unsigned nrStations, unsigned nrSamplesPerIntegration, double channelBa
 #if !defined PPF_C_IMPLEMENTATION
 #if NR_BITS_PER_SAMPLE == 4
   static const float map[] = {
-    0, 1, 2, 3, 4, 5, 6, 7, -8, -7, -6, -5, -4, -3, -2, -1,
+     0.5,  1.5,  2.5,  3.5,  4.5,  5.5,  6.5,  7.5, 
+    -7.5, -6.5, -5.5, -4.5, -3.5, -2.5, -1.5, -0.5,
   };
 
   for (unsigned i = 0; i < 16; i ++)
