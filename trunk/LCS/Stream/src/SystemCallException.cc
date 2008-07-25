0a6cf2937dcfabf3d1886ac554b87e4d1607baae
romein <romein@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-07-25 20:41:53 +0000

    bug 225: check for two incompatible versions of strerror_r()

--- trunk/LCS/Stream/src/SystemCallException.cc
+++ trunk/LCS/Stream/src/SystemCallException.cc
@@ -24,10 +24,16 @@ std::string SystemCallException::errorMessage(int error)
 {
   char buffer[128];
 
+  // there are two incompatible versions of versions of strerror_r()
+
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
   if (strerror_r(error, buffer, sizeof buffer) == 0)
     return std::string(buffer);
   else
     return "could not convert error to string";
+#else
+  return strerror_r(error, buffer, sizeof buffer);
+#endif
 }
 
 
