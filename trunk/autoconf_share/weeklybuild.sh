524b0f66a54c52632bdb2f92885588bde9086f02
lofar <lofar@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-09-22 07:59:53 +0000

    Bug 523: Replaced "lofar17" with `uname -n`. Now make use of lofarconf.in for most variants.

--- trunk/autoconf_share/weeklybuild.sh
+++ trunk/autoconf_share/weeklybuild.sh
@@ -26,60 +26,60 @@ cd LOFAR
 #
 #source $INSTALLDIR/gnu_debug/lofarinit.sh
 rm -rf $INSTALLDIR/gnu_debug/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" Appl LCS CEP MAC Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnu_debug.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" $(cat lofarconf.in) > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_debug.log 2>&1
 
 
 # GNU_DEBUG_VALGRIND Build
 #
 rm -rf $INSTALLDIR/gnu_valgrind/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" -checktool "/usr/local/bin/valgrind --tool=memcheck --num-callers=50 --leak-check=yes --track-fds=yes --log-file=$INSTALLDIR/gnu_valgrind/[PROGNAME].valgrind" Appl LCS CEP MAC Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnu_valgrind.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" -checktool "/usr/local/bin/valgrind --tool=memcheck --num-callers=50 --leak-check=yes --track-fds=yes --log-file=$INSTALLDIR/gnu_valgrind/[PROGNAME].valgrind" $(cat lofarconf.in) > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_valgrind.log 2>&1
 
 # GNU_DEBUG_DISTCHECK
 #
-#autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" -distcheck Appl LCS CEP MAC/{Test/Suite,GCF/GCFCommon,GCF/TM,APL/RTCCommon,APL/PAC/BeamServer,APL/PAC/BS_Protocol,APL/PAC/CAL_Protocol,APL/PAC/CalServer,APL/PIC/RSPDriver,APL/PIC/RSP_Protocol} Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnu_distcheck.log 2>&1
+#autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_debug" -build "gnu_debug" -distcheck Appl LCS CEP MAC/{Test/Suite,GCF/GCFCommon,GCF/TM,APL/RTCCommon,APL/PAC/BeamServer,APL/PAC/BS_Protocol,APL/PAC/CAL_Protocol,APL/PAC/CalServer,APL/PIC/RSPDriver,APL/PIC/RSP_Protocol} Demo > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_distcheck.log 2>&1
 
 # GNU_OPT Build
 #
 #source $INSTALLDIR/gnu_opt/lofarinit.sh
 rm -rf $INSTALLDIR/gnu_opt/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_opt" -build "gnu_opt" Appl LCS CEP MAC Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnu_opt.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_opt" -build "gnu_opt" $(cat lofarconf.in) > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_opt.log 2>&1
 
 # GNU_NOLOG4CPLUS
 #
 #source $INSTALLDIR/gnunew_nolog4cplus/lofarinit.sh
 rm -rf $INSTALLDIR/gnunew_nolog4cplus/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnunew_nolog4cplus" -build "gnunew_nolog4cplus" Appl LCS CEP Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnunew_nolog4cplus.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnunew_nolog4cplus" -build "gnunew_nolog4cplus" $(cat lofarconf.in) > $BUILDROOT/`uname -n`-week`date +%g%W`-gnunew_nolog4cplus.log 2>&1
 
 # GNUNEW_DEBUG
 #
 #source $INSTALLDIR/gnunew_debug/lofarinit.sh
 rm -rf $INSTALLDIR/gnunew_debug/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnunew_debug" -build "gnunew_debug" Appl LCS CEP MAC Demo > $BUILDROOT/lofar17-week`date +%g%W`-gnunew.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnunew_debug" -build "gnunew_debug" $(cat lofarconf.in) > $BUILDROOT/`uname -n`-week`date +%g%W`-gnunew.log 2>&1
 
 # GNU_MPICH
 #
 #source $INSTALLDIR/gnu_mpich/lofarinit.sh
 rm -rf $INSTALLDIR/gnu_mpich/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_mpich" -build "gnu_mpich" LCS/Common LCS/databases/PL LCS/Transport LCS/databases/TransportPL LCS/ACC CEP/tinyCEP CEP/CEPFrame > $BUILDROOT/lofar17-week`date +%g%W`-gnu_mpich.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_mpich" -build "gnu_mpich" LCS/Common LCS/databases/PL LCS/Transport LCS/databases/TransportPL LCS/ACC CEP/tinyCEP CEP/CEPFrame > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_mpich.log 2>&1
 
 # GNU_FAKEBGL
 #
 #source $INSTALLDIR/gnu_fakebgl/lofarinit.sh
 rm -rf $INSTALLDIR/gnu_fakebgl/*
-autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_fakebgl" -build "gnu_fakebgl" LCS/Common LCS/Transport CEP/tinyCEP > $BUILDROOT/lofar17-week`date +%g%W`-gnu_fakebgl.log 2>&1
+autoconf_share/rub -k -noupdate -release=main -confopt "--prefix=$INSTALLDIR/gnu_fakebgl" -build "gnu_fakebgl" LCS/Common LCS/Transport CEP/tinyCEP > $BUILDROOT/`uname -n`-week`date +%g%W`-gnu_fakebgl.log 2>&1
 
 # ICC_DEBUG
 #
 #source $INSTALLDIR/icc_debug/lofarinit.sh
 #rm -rf $INSTALLDIR/icc_debug/*
-#autoconf_share/rub -system -noupdate -release=main -confopt "--prefix=$INSTALLDIR/icc_debug" -build "icc_debug" Appl LCS CEP Demo > $BUILDROOT/lofar17-week`date +%g%W`-iccdebug.log 2>&1
+#autoconf_share/rub -system -noupdate -release=main -confopt "--prefix=$INSTALLDIR/icc_debug" -build "icc_debug" Appl LCS CEP Demo > $BUILDROOT/`uname -n`-week`date +%g%W`-iccdebug.log 2>&1
 
 # DOCUMENTATION
 #
 #source $INSTALLDIR/gnu_opt/lofarinit.sh
 rm -rf $INSTALLDIR/docxxhtml
 mkdir -p $INSTALLDIR/docxxhtml
-export DOXYGEN="/usr/local/doxygen/bin/doxygen";make docxx  PACKAGES="LCS CEP MAC SAS Demo" > $BUILDROOT/lofar17-week`date +%g%W`-docxx.log 2>&1
+export DOXYGEN="/usr/local/doxygen/bin/doxygen";make docxx  PACKAGES="LCS CEP MAC SAS Demo" > $BUILDROOT/`uname -n`-week`date +%g%W`-docxx.log 2>&1
 
 
 cd $BUILDROOT
