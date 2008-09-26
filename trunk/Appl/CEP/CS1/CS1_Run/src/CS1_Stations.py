3545f2296aa7432f579493c69a1e8729b58e2afd
gels <gels@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-09-26 13:02:22 +0000

    Bug 1005: For all stations added dipoles 0,4,8,12.

--- trunk/Appl/CEP/CS1/CS1_Run/src/CS1_Stations.py
+++ trunk/Appl/CEP/CS1/CS1_Run/src/CS1_Stations.py
@@ -23,6 +23,12 @@ CS001_us2 = [Station('CS001_us2', '10.170.0.37', ['10.170.0.37:4348'])]
 CS001_us3 = [Station('CS001_us3', '10.170.0.38', ['10.170.0.38:4349'])]
 CS001_4us = CS001_us0 + CS001_us1 + CS001_us2 + CS001_us3
 
+CS001_dipole0 = [Station('CS001_dipole0', '10.170.0.33', ['10.170.0.33:4346'])]
+CS001_dipole4 = [Station('CS001_dipole4', '10.170.0.34', ['10.170.0.34:4347'])]
+CS001_dipole8 = [Station('CS001_dipole8', '10.170.0.37', ['10.170.0.37:4348'])]
+CS001_dipole12 = [Station('CS001_dipole12', '10.170.0.38', ['10.170.0.38:4349'])]
+CS001_4dipoles = CS001_dipole0 + CS001_dipole4 + CS001_dipole8 + CS001_dipole12
+
 CS001_HBA0 = [Station('CS001_HBA0', '10.170.0.33', ['10.170.0.33:4346'])]
 CS001_HBA1 = [Station('CS001_HBA1', '10.170.0.34', ['10.170.0.34:4347'])]
 CS001_HBA2 = [Station('CS001_HBA2', '10.170.0.37', ['10.170.0.37:4348'])]
@@ -37,6 +43,12 @@ CS010_us2 = [Station('CS010_us2', '10.170.0.45', ['10.170.0.45:4352'])]
 CS010_us3 = [Station('CS010_us3', '10.170.0.46', ['10.170.0.46:4353'])]
 CS010_4us = CS010_us0 + CS010_us1 + CS010_us2 + CS010_us3
 
+CS010_dipole0 = [Station('CS010_dipole0', '10.170.0.41', ['10.170.0.41:4346'])]
+CS010_dipole4 = [Station('CS010_dipole4', '10.170.0.42', ['10.170.0.42:4351'])]
+CS010_dipole8 = [Station('CS010_dipole8', '10.170.0.45', ['10.170.0.45:4352'])]
+CS010_dipole12 = [Station('CS010_dipole12', '10.170.0.46', ['10.170.0.46:4353'])]
+CS010_4dipoles = CS010_dipole0 + CS010_dipole4 + CS010_dipole8 + CS010_dipole12
+
 CS010_HBA0 = [Station('CS010_HBA0', '10.170.0.41', ['10.170.0.41:4346'])]
 CS010_HBA1 = [Station('CS010_HBA1', '10.170.0.42', ['10.170.0.42:4351'])]
 CS010_HBA2 = [Station('CS010_HBA2', '10.170.0.45', ['10.170.0.45:4352'])]
@@ -50,6 +62,12 @@ CS016_us1 = [Station('CS016_us1', '10.170.0.50', ['10.170.0.50:4347'])]
 CS016_us2 = [Station('CS016_us2', '10.170.0.53', ['10.170.0.53:4348'])]
 CS016_us3 = [Station('CS016_us3', '10.170.0.54', ['10.170.0.54:4349'])]
 CS016_4us = CS016_us0 + CS016_us1 + CS016_us2 + CS016_us3
+
+CS016_dipole0 = [Station('CS016_dipole0', '10.170.0.49', ['10.170.0.49:4346'])]
+CS016_dipole4 = [Station('CS016_dipole4', '10.170.0.50', ['10.170.0.50:4347'])]
+CS016_dipole8 = [Station('CS016_dipole8', '10.170.0.53', ['10.170.0.53:4348'])]
+CS016_dipole12 = [Station('CS016_dipole12', '10.170.0.54', ['10.170.0.54:4349'])]
+CS016_4dipoles = CS016_dipole0 + CS016_dipole4 + CS016_dipole8 + CS016_dipole12
 
 CS016_HBA0 = [Station('CS016_HBA0', '10.170.0.49', ['10.170.0.49:4346'])]
 CS016_HBA1 = [Station('CS016_HBA1', '10.170.0.50', ['10.170.0.50:4347'])]
@@ -78,5 +96,6 @@ B03_1 = [Station('B03_1', '10.170.0.14', ['10.170.0.14:4346', '10.170.0.14:4347'
 B03 = B03_0 + B03_1
 
 AllStations      = CS001 + CS010 + CS016
-AllMicroStations = CS001_4us + CS001_4us + CS016_4us
+AllMicroStations = CS001_4us + CS010_4us + CS016_4us
+AllDipoles       = CS001_4dipoles + CS010_4dipoles + CS016_4dipoles
 AllHBAs          = CS010_4HBAs + CS001_4HBAs + CS016_4HBAs
