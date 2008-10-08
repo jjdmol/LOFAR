24c49761233c87a64478bfe72741b7cf61843e52
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-10-08 07:32:45 +0000

    Bug 61: Extra blank line

--- trunk/Appl/CEP/CS1/CS1_pp_lib/include/CS1_pp_lib/Pipeline.h
+++ trunk/Appl/CEP/CS1/CS1_pp_lib/include/CS1_pp_lib/Pipeline.h
@@ -17,6 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
 #ifndef __CS1_PP_PIPELINE_H__
 #define __CS1_PP_PIPELINE_H__
 
