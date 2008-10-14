c10a8445c3f8de629cac96eb7473370c0d46df81
gels <gels@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-10-14 13:43:51 +0000

    Bug 1005: Moved OLAP apps to LOFAR/RTCP.

---
+++ trunk/RTCP/IONProc/src/RSP.h
@@ -0,0 +1,52 @@
+//#  RSP: RSP data format
+//#
+//#  Copyright (C) 2008
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef LOFAR_IONPROC_RSP_H
+#define LOFAR_IONPROC_RSP_H
+
+
+namespace LOFAR {
+namespace RTCP {
+
+#include <cstddef>
+
+
+// All data is in Little Endian format!
+
+struct RSP {
+  struct header {
+    uint16_t version;
+    uint16_t configuration;
+    uint16_t station;
+    uint8_t  nrBeamlets;
+    uint8_t  nrBlocks;
+    uint32_t timestamp;
+    uint32_t blockSequenceNumber;
+  } header;
+
+  char       data[8130];
+};
+
+} // namespace RTCP
+} // namespace LOFAR
+
+#endif
---
+++ trunk/RTCP/Run/src/Parset.py
@@ -0,0 +1,263 @@
+import time
+import datetime
+import LOFAR_Parset
+import math
+import sys
+import copy
+import string
+import struct
+from Numeric import zeros
+from Host_Names import *
+from sets import Set
+
+class StationRSPpair(object):
+    def __init__(self, station, rsp):
+        self.station	= station
+        self.rsp	= rsp
+
+    def getStation(self):
+        return self.station
+
+    def getRsp(self):
+	return self.rsp
+	
+    def setStation(self, station):
+        self.station = station
+
+    def setRsp(self, rsp):
+	self.rsp = rsp
+	
+class Parset(LOFAR_Parset.Parset):
+
+    def __init__(self):
+        LOFAR_Parset.Parset.__init__(self)
+        self.inputFromMemory = False
+        self.inputFromRaw = False
+        self.stationList = list()
+
+    def setClock(self, clock):
+        self.clock = clock
+        
+        if self.clock == '160MHz':
+	    self['Observation.sampleClock'] = 160
+	    self['OLAP.BGLProc.integrationSteps'] = 608
+        elif self.clock == '200MHz':
+	    self['Observation.sampleClock'] = 200
+	    self['OLAP.BGLProc.integrationSteps'] = 768
+	    #self['OLAP.BGLProc.integrationSteps'] = 384
+	    #self['OLAP.BGLProc.integrationSteps'] = 192
+       
+    def getClockString(self):
+        return self.clock
+
+    def subbandsPerPset(self):
+	if not self.isDefined('OLAP.subbandsPerPset'):
+	    nrSubbands = self.getNrSubbands()
+	    if nrSubbands == 1 : self['OLAP.subbandsPerPset'] = nrSubbands
+	    elif nrSubbands % 2 != 0:
+	        print 'Number of subbands(%d) is not even.' %(nrSubbands)
+		sys.exit(0)
+	    else:
+		maxPSets = len(IONodes.get(self.getPartition()))
+		nrStorageNodes = len(listfen.getSlaves())
+		for pSets in range(maxPSets, 0 , -1):
+		    if nrSubbands % pSets == 0 and pSets % nrStorageNodes == 0:
+			subbandPersSet =nrSubbands / pSets
+			self['OLAP.subbandsPerPset'] = subbandPersSet
+			break
+		    
+    def psetsPerStorage(self):
+        if not self.isDefined('OLAP.psetsPerStorage'):
+            nrSubbands = self.getNrSubbands()
+	    if nrSubbands == 1 : self['OLAP.psetsPerStorage'] = nrSubbands
+	    else:
+	        if not self.isDefined('OLAP.subbandsPerPset'):
+	            self.subbandsPerPset()
+	        nrStorageNodes = len(listfen.getSlaves())
+	    
+	        self['OLAP.psetsPerStorage'] = nrSubbands / self.getInt32('OLAP.subbandsPerPset') / nrStorageNodes
+	    
+    def setStations(self, stationList):
+        self.stationList = stationList
+        self['OLAP.nrRSPboards'] = len(stationList)
+        self['OLAP.storageStationNames'] = [s.getName() for s in stationList]
+	
+	for station in stationList:
+	  self['PIC.Core.Station.' + station.name + '.RSP.ports'] = station.inputs
+	for pset in range(len(IONodes.get(self.partition))):
+	  self['PIC.Core.IONProc.' + self.partition + '[' + str(pset) + '].inputs'] = [station.name + '/RSP' + str(rsp) for station in stationList if station.getPset(self.partition) == pset for rsp in range(len(station.inputs))]
+
+    def setTiedArrayStations(self):
+        tiedArrayStationList = list()
+ 	stationList = copy.deepcopy(self.stationList)
+	beamform = False
+	index=0
+	
+	while self.isDefined('Observation.Beamformer[' + str(index) + '].stationList'):
+	    beamform = True
+	    tiedArrayStationList.append(string.replace(self.getString('Observation.Beamformer[' + str(index) + '].stationList').strip('"').rstrip('"'),',','+'))
+	    superStations = self.getString('Observation.Beamformer[' + str(index) + '].stationList').strip('"').rstrip('"').split(',')
+	    for sp in superStations:
+		for s in stationList:
+		    if s.getName() == sp:
+		       stationList.remove(s)
+		       break
+	    index +=1
+	
+	if (beamform):
+	    for p in stationList:
+	        tiedArrayStationList.append(p.getName())
+	
+	self['OLAP.tiedArrayStationNames'] = [sp for sp in tiedArrayStationList]
+
+    def tabMapping(self):
+	tabList = list()
+	
+	if len(self.getStringVector('OLAP.tiedArrayStationNames')) > 0:
+	    for s in self.stationList:
+	        tabStations = self.getStringVector('OLAP.tiedArrayStationNames')
+	        for tab in tabStations:
+		    if tab.find('+') != -1 :
+		       stations = tab.split('+')
+		       for st in stations:
+		           if s.getName() == st:
+		               tabList.append(tabStations.index(tab))
+		    else:
+		       if s.getName() == tab:
+		         tabList.append(tabStations.index(tab))  
+	    
+	    if len(tabList) != len(self.stationList):
+	        print 'tabList contains wrong number (' + str(len(tabList)) + ') of elements (expected '+ str(len(self.stationList)) + ')'
+		sys.exit(0)
+		
+	self['OLAP.BGLProc.tabList'] = [tab for tab in tabList]
+	
+    def getStations(self):
+	return self.stationList
+    
+    def setPartition(self, partition):
+        self.partition = partition
+	self['OLAP.BGLProc.partition'] = partition	
+	
+    def getPartition(self):
+        return self.partition
+	
+    def setInputToMem(self):
+        self.inputFromMemory = True
+	self['OLAP.OLAP_Conn.station_Input_Transport'] = 'NULL'
+
+    def getNStations(self):
+        return int(len(self.stationList))
+        
+    def setInterval(self, start, duration):
+        self['Observation.startTime'] = datetime.datetime.fromtimestamp(start)
+        self['Observation.stopTime'] = datetime.datetime.fromtimestamp(start + duration)
+
+    def setIntegrationTime(self, integrationTime):
+	self['OLAP.IONProc.integrationSteps']     = integrationTime
+	self['OLAP.StorageProc.integrationSteps'] = 1
+
+    def setMSName(self, msName):
+        self['Observation.MSNameMask'] = msName
+
+    def getMSName(self):
+        return self['Observation.MSNameMask']
+
+    def getNPsets(self):
+        subbands = self.getNrSubbands()
+        subbandsperpset = self.getInt32('OLAP.subbandsPerPset')
+        return subbands / subbandsperpset
+    
+    def nrSlotsInFrame(self):
+        return self.getInt32('OLAP.nrSlotsInFrame')
+    
+    def subbandToRSPboardMapping(self):
+        return self.getExpandedInt32Vector('Observation.rspBoardList')
+    
+    def subbandToRSPslotMapping(self):
+        return self.getExpandedInt32Vector('Observation.rspSlotList')
+    
+    def getNrSubbands(self):
+        if self.isDefined('Observation.subbandList'):
+	    return len(self.getExpandedInt32Vector('Observation.subbandList'))
+	else:
+	    index=0
+	    subbands = Set() 
+	    while self.isDefined('Observation.Beam[' + str(index) + '].subbandList'):
+	        subbandList = self.getExpandedInt32Vector('Observation.Beam[' + str(index) + '].subbandList')
+		for s in subbandList:
+		    subbands.add(s)
+		index +=1
+	    return len(subbands)
+
+    def getStationNamesAndRSPboardNumbers(self, psetNumber):
+	inputs = self.getStringVector('PIC.Core.IONProc.' + self.getPartition() + '[' + str(psetNumber) + '].inputs')
+	stationsAndRSPs = list()   
+	for i in range(0, len(inputs)):
+            split = inputs[i].split('/')
+	    if len(split) != 2 or split[1][0:3] != 'RSP':
+	        print 'expected stationname/RSPn pair in "%s"' % inputs[i]
+	        sys.exit(0)
+            
+	    stationsAndRSPs.append(StationRSPpair(split[0],split[1][3]))
+	
+	return stationsAndRSPs
+    
+    def checkBeamformList(self):
+	index = 0
+
+	while self.isDefined('Observation.Beamformer[' + str(index) + '].stationList'):
+	    superStations = self.getString('Observation.Beamformer[' + str(index) + '].stationList').strip('"').rstrip('"').split(',')    	
+            for sp in superStations:
+	        found = False
+		for s in self.stationList:
+		    if sp == s.getName():
+		        found = True
+		        break
+		if not (found):
+		    print 'invalid value: Observation.Beamformer[%d].stationList = ' % index + sp + ', or the station isn\'t found in the stationList.'
+		    sys.exit(0)
+	    index +=1
+	    
+    def checkSubbandCount(self, key):
+        if len(self.getExpandedInt32Vector(key)) != self.getNrSubbands():
+	    print key + ' contains wrong number (' + str(len(self.getExpandedInt32Vector(key))) + ') of subbands (expected ' + str(self.getNrSubbands()) + ')'
+	    sys.exit(0)
+	    
+    def check(self):
+        if self.isDefined('Observation.subbandList'):
+            self.checkSubbandCount('Observation.beamList');
+            self.checkSubbandCount('Observation.rspBoardList');
+            self.checkSubbandCount('Observation.rspSlotList');
+	
+	    slotsPerFrame        = self.nrSlotsInFrame()
+	    boards		 = self.subbandToRSPboardMapping()
+	    slots		 = self.subbandToRSPslotMapping()
+	
+	    for subband in range(0, len(slots)):
+	        if slots[subband] >= slotsPerFrame:
+	            print 'Observation.rspSlotList contains slot numbers >= OLAP.nrSlotsInFrame'
+		    sys.exit(0)
+
+    def inputPsets(self):
+        return self.getInt32Vector('OLAP.BGLProc.inputPsets')
+	    
+    def checkRspBoardList(self):
+        if self.isDefined('Observation.subbandList'):
+	    inputs		 = self.inputPsets()
+	    boards		 = self.subbandToRSPboardMapping()
+	
+	    for pset in inputs:
+	        nrRSPboards = len(self.getStationNamesAndRSPboardNumbers(pset))
+	        for subband in range(0, len(boards)):
+		    if boards[subband] >= nrRSPboards:
+		        print 'Observation.rspBoardList contains rsp board numbers that do not exist'
+		        sys.exit(0)
+		
+	
+	
+	
+	
+	
+    
+    
