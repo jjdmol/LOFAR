db97c6df6a81e23e43a55b1cc61de7dfcb04d40d
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2008-10-17 14:04:23 +0000

    bug 1246: Adapted to new ParmDB

--- trunk/CEP/BB/ParmFacade/include/ParmFacade/ParmFacade.h
+++ trunk/CEP/BB/ParmFacade/include/ParmFacade/ParmFacade.h
@@ -29,124 +29,81 @@
 //# Includes
 #include <ParmDB/ParmDB.h>
 #include <casa/Containers/Record.h>
-#include <vector>
-#include <map>
-#include <set>
+#include <Common/lofar_vector.h>
+#include <Common/lofar_map.h>
+#include <Common/lofar_set.h>
+#include <Common/lofar_string.h>
 
 
-namespace LOFAR {
-namespace ParmDB {
+namespace LOFAR { namespace BBS {
 
 
-// \ingroup ParmFacade
-// @{
-
-// ParmFacade is the high level interface to the Parameter Data Base.
-// The current version assumes it is an AIPS++ table; with a few extra
-// constructor arguments it can easily be changed to other types of
-// databases.
-//
-// The class provides a few functions:
-// <ul>
-// <li> getNames returns a vector of the parameter names in the table
-// <li> getRange returns a vector of 4 elements giving the boundary box
-//      of the domains of the parameters.
-// <li> getValues returns the values of the parameters a calculated on
-//      a grid given by the caller.
-// <li> getHistory returns the coefficients of parameters in a given
-//      time frame. It shows how a solution converges (or diverges).
-// </ul>
-//
-// The parameter names can be given as a pattern. This is the same as a
-// file name pattern that can be given in the UNIX shells (e.g. RA:*).
-// Thus it is not a full regular expression.
-
-class ParmFacade
-{
-public:
-  // Make a connection to the given ParmTable.
-  ParmFacade (const std::string& tableName);
-
-  // The destructor disconnects.
-  ~ParmFacade();
-
-  // Get the domain range (as startx,endx,starty,endy) of the given
-  // parameters in the table.
-  // This is the minimum start value and maximum end value for all parameters.
-  // An empty name pattern is the same as * (all parm names).
-  std::vector<double> getRange (const std::string& parmNamePattern = "") const;
-
-  // Get parameter names in the table matching the pattern.
-  // An empty name pattern is the same as * (all parm names).
-  std::vector<std::string> getNames
-                               (const std::string& parmNamePattern = "") const;
-
-  // Get the parameter values for the given parameters and domain.
-  // The domain is given by the start and end values, while the grid is
-  // given by nx and ny.
-  // The vector values in the map are in fact 2-dim arrays with axes nx and ny.
+  // \ingroup ParmFacade
   // @{
-  std::map<std::string, std::vector<double> >
-    getValues (const std::string& parmNamePattern,
-	       double startx, double endx, int nx,
-	       double starty, double endy, int ny);
 
-  // The Record contains a map of parameter name to Array<double>.
-  casa::Record
-    getValuesRec (const std::string& parmNamePattern,
-		  double startx, double endx, int nx,
-		  double starty, double endy, int ny);
+  // ParmFacade is the high level interface to the Parameter Data Base.
+  // The current version assumes it is an AIPS++ table; with a few extra
+  // constructor arguments it can easily be changed to other types of
+  // databases.
+  //
+  // The class provides a few functions:
+  // <ul>
+  // <li> getNames returns a vector of the parameter names in the table
+  // <li> getRange returns a vector of 4 elements giving the boundary box
+  //    of the domains of the parameters.
+  // <li> getValues returns the values of the parameters a calculated on
+  //      a grid given by the caller.
+  // </ul>
+  //
+  // The parameter names can be given as a pattern. This is the same as a
+  // file name pattern that can be given in the UNIX shells (e.g. RA:*).
+  // Thus it is not a full regular expression.
+
+  class ParmFacade
+  {
+  public:
+    // Make a connection to the given ParmTable.
+    ParmFacade (const string& tableName);
+
+    // The destructor disconnects.
+    ~ParmFacade();
+
+    // Get the domain range (as startx,endx,starty,endy) of the given
+    // parameters in the table.
+    // This is the minimum start value and maximum end value for all parameters.
+    // An empty name pattern is the same as * (all parm names).
+    vector<double> getRange (const string& parmNamePattern = "") const;
+
+    // Get parameter names in the table matching the pattern.
+    // An empty name pattern is the same as * (all parm names).
+    vector<string> getNames (const string& parmNamePattern = "") const;
+
+    // Get the parameter values for the given parameters and domain.
+    // The domain is given by the start and end values, while the grid is
+    // given by nx and ny.
+    // The vector values in the map are in fact 2-dim arrays with axes nx and ny.
+    // @{
+    map<string, vector<double> > getValues (const string& parmNamePattern,
+                                            double startx, double endx, int nx,
+                                            double starty, double endy, int ny);
+
+    // The Record contains a map of parameter name to Array<double>.
+    casa::Record getValuesRec (const string& parmNamePattern,
+                               double startx, double endx, int nx,
+                               double starty, double endy, int ny);
+    // @}
+
+  private:
+    // Copy the values from a record to a map.
+    map<string, vector<double> > record2Map (const casa::Record& rec) const;
+
+
+    //# Data members
+    ParmDB itsPDB;
+  };
+
   // @}
 
-  // Get the parameter coefficients for the given given parameters and domain
-  // which have been solved in the given time frame. Default is the
-  // entire time frame.
-  // The vector values in the map are in fact 2-dim arrays with shape
-  // [ncoeff, nsolutions]. Because ncoeff and nsolutions are unknown,
-  // for the caller, ncoeff is put in the vector as the first value.
-  // The solutions are in order of timestamp.
-  // A parameter can have multiple solve domains, so a domain name
-  // (:domain<seqnr>) is added to the name to make it unique.
-  // @{
-  std::map<std::string, std::vector<double> >
-    getHistory (const std::string& parmNamePattern,
-		double startx, double endx,
-		double starty, double endy,
-		double startSolveTime=0, double endSolveTime=1e25);
-
-  // The Record contains a map of parameter name to Array<double>.
-  casa::Record
-    getHistoryRec (const std::string& parmNamePattern,
-		   double startx, double endx,
-		   double starty, double endy,
-		   double startSolveTime=0, double endSolveTime=1e25);
-  // @}
-
-  // Get the expressions and the names of their children.
-  const std::map<std::string,std::set<std::string> >& getExprs() const
-    { return itsExprs; }
-
-private:
-  // Process a parmexpr and add the children names to the vector.
-  // It is done recursively, so a child can be an expression.
-  void processExpr (const std::map<std::string,ParmValueSet>& defValues, 
-		    const std::string& expr, std::set<std::string>& ch);
-
-  // Copy the values from a record to a map.
-  std::map<std::string, std::vector<double> >
-    record2Map (const casa::Record& rec) const;
-
-  // Compare function for sorting ParmValues in time order.
-  static bool pvLess (const ParmValue& left, const ParmValue& right);
-
-
-  ParmDB itsPDB;
-  std::map<std::string,std::set<std::string> > itsExprs;
-};
-
-// @}
-
-} // namespace ParmDB
-} // namespace LOFAR
+}} // namespaces
 
 #endif
--- trunk/CEP/BB/ParmFacade/src/ParmFacade.cc
+++ trunk/CEP/BB/ParmFacade/src/ParmFacade.cc
@@ -20,18 +20,18 @@
 //#
 //# $Id$
 
+#include <lofar_config.h>
 #include <ParmFacade/ParmFacade.h>
-#include <BBSKernel/MNS/MeqRequest.h>
-#include <BBSKernel/MNS/MeqParmFunklet.h>
-#include <BBSKernel/MNS/MeqResult.h>
-#include <BBSKernel/MNS/MeqMatrix.h>
+#include <ParmDB/ParmDB.h>
+#include <ParmDB/ParmSet.h>
+#include <ParmDB/ParmCache.h>
+#include <ParmDB/Parm.h>
 #include <Common/LofarLogger.h>
 #include <Common/StringUtil.h>
 #include <casa/Utilities/Regex.h>
 
 using namespace std;
 using namespace casa;
-using namespace LOFAR::BBS;
 
 // Create tParmFacade.in_mep with parmdb using:
 //   create tablename='tParmFacade.in_mep'
@@ -40,63 +40,14 @@ using namespace LOFAR::BBS;
 //   add parm3 type='expression',expression='parm1*parm2'
 
 namespace LOFAR {
-  namespace ParmDB {
+  namespace BBS {
 
     ParmFacade::ParmFacade (const string& tableName)
-      : itsPDB(ParmDBMeta("aips", tableName))
-    {
-      map<string,ParmValueSet> defValues;
-      itsPDB.getDefValues (defValues, "*");
-      for (map<string,ParmValueSet>::const_iterator iter = defValues.begin();
-	   iter != defValues.end();
-	   ++iter) {
-	const ParmValueRep& pval = iter->second.getValues()[0].rep();
-	if (pval.itsType == "parmexpr") {
-	  itsExprs[iter->first] = set<string>();
-	  // Add the expression to the map.
-	  map<string,set<string> >::iterator ch = itsExprs.find(iter->first);
-	  // Add all children to the set.
-	  processExpr (defValues, pval.itsExpr, ch->second);
-	}
-      }
-    }
+      : itsPDB(ParmDBMeta("casa", tableName))
+    {}
 
     ParmFacade::~ParmFacade()
     {}
-
-    void ParmFacade::processExpr (const map<string,ParmValueSet>& defValues, 
-				  const string& expr, set<string>& ch)
-    {
-      String str(expr);
-      // Replace function names by a blank..
-      str.gsub (Regex("[a-zA-Z_][a-zA-Z0-9_]* *\\("), " ");
-      // Replace parentheses, commas, and operators by a blank.
-      str.gsub (Regex("[(),*/+^-]") , " ");
-      // Replace multiple blanks by a single one.
-      str.gsub (Regex("  *") , " ");
-      // Now split the string and append to the children set.
-      vector<string> nms = StringUtil::split(str, ' ');
-      for (vector<string>::const_iterator iter=nms.begin();
-	   iter != nms.end();
-	   ++iter) {
-	if (! iter->empty()) {
-	  // If the name is an expression, process it recursively.
-	  bool isExpr = false;
-	  map<string,ParmValueSet>::const_iterator def = defValues.find (*iter);
-	  if (def != defValues.end()) {
-	    const ParmValueRep& pval = def->second.getValues()[0].rep();
-	    if (pval.itsType == "parmexpr") {
-	      isExpr = true;
-	      processExpr (defValues, pval.itsExpr, ch);
-	    }
-	  }
-	  if (!isExpr) {
-	    // No expression, so add parm name.
-	    ch.insert (*iter);
-	  }
-	}
-      }
-    }
 
     vector<double> ParmFacade::getRange (const string& parmNamePattern) const
     {
@@ -104,34 +55,12 @@ namespace LOFAR {
       if (pp.empty()) {
 	pp = "*";
       }
-      ParmDomain dom = itsPDB.getRange (pp);
-      // If no domain found, see if the name matches an expression.
-      if (dom.getStart()[0] == 0  &&  dom.getEnd()[0] == 1  &&
-	  dom.getStart()[1] == 0  &&  dom.getEnd()[1] == 1) {
-	map<string,set<string> >::const_iterator iter =
-	  itsExprs.find (parmNamePattern);
-	if (iter == itsExprs.end()) {
-	  // Not found; see if the name is part of an expression name.
-	  String pname(parmNamePattern);
-	  for (iter=itsExprs.begin(); iter!=itsExprs.end(); ++iter) {
-	    if (pname.matches (Regex(iter->first + ".*"))) {
-	      break;
-	    }
-	  }
-	}
-	// A match has been found,
-	// so get the range of the expression's children.
-	if (iter != itsExprs.end()) {
-	  // Copy the set to a vector.
-	  vector<string> nams(iter->second.begin(), iter->second.end());
-	  dom = itsPDB.getRange (nams);
-	}
-      }
+      Box dom = itsPDB.getRange (pp);
       vector<double> res(4);
-      res[0] = dom.getStart()[0];
-      res[1] = dom.getEnd()[0];
-      res[2] = dom.getStart()[1];
-      res[3] = dom.getEnd()[1];
+      res[0] = dom.lowerX();
+      res[1] = dom.upperX();
+      res[2] = dom.lowerY();
+      res[3] = dom.upperY();
       return res;
     }
 
@@ -142,7 +71,7 @@ namespace LOFAR {
       if (pp.empty()) {
 	pp = "*";
       }
-      return itsPDB.getAllNames(pp);
+      return itsPDB.getNames(pp);
     }
 
     // Get the parameter values for the given parameters and domain.
@@ -164,124 +93,26 @@ namespace LOFAR {
       vector<string> names = getNames (parmNamePattern);
       // The output is returned in a record.
       Record out;
-      MeqParmGroup parmGroup;
-      map<string,MeqExpr> exprs;
-      // Loop through all parameters.
-      for (vector<string>::const_iterator iter=names.begin();
-	   iter != names.end();
-	   ++iter) {
-	// Create an expression object.
-	exprs.insert (std::make_pair (*iter,
-				      MeqParmFunklet::create (*iter,
-							      parmGroup,
-							      &itsPDB)));
+      // Form the names to get.
+      // The returned parmId should be the index.
+      ParmSet parmSet;
+      for (uint i=0; i<names.size(); ++i) {
+        ASSERT (parmSet.addParm (itsPDB, names[i]) == i);
       }
-      // Get the parm values of parameters.
-      // All parameters are needed to deal with possible parmexpressions.
-      map<string,ParmValueSet> parmValues;
-      itsPDB.getValues (parmValues, vector<string>(),
-			ParmDomain(startx, endx, starty, endy));
-      // Make a request object for the given grid.
-      MeqDomain domain(startx, endx, starty, endy);
-      MeqRequest req(domain, nx, ny);
-      // Initialize all parameters.
-      //const vector<MeqParm*>& parmList = parmGroup.getParms();
-      //for (vector<MeqParm*>::const_iterator iter = parmList.begin();
-      // iter != parmList.end();
-      // ++iter) {
-      //  (*iter)->fillFunklets (parmValues, domain);
-      //}
-      for (MeqParmGroup::iterator iter = parmGroup.begin();
-	   iter != parmGroup.end(); ++iter) {
-	(*iter).second.fillFunklets (parmValues, domain);
+      // Create and fill the cache for the given domain.
+      ParmCache parmCache(parmSet, Box(Point(startx, starty),
+                                       Point(endx,   endy)));
+      // Create the predict grid.
+      Axis::ShPtr axisx (new RegularAxis(startx, endx, nx, true));
+      Axis::ShPtr axisy (new RegularAxis(starty, endy, ny, true));
+      Grid predictGrid (axisx, axisy);
+      // Now create the Parm object for each parm and get the values.
+      Array<double> result;
+      for (uint i=0; i<names.size(); ++i) {
+        Parm parm(parmCache, i);
+        parm.getResult (result, predictGrid);
+        out.define (names[i], result);
       }
-      // Define shape of result.
-      IPosition shape(2,nx,ny);
-      // Evaluate all expressions and store the result in the output map.
-      for (map<string,MeqExpr>::iterator iter = exprs.begin();
-	   iter != exprs.end();
-	   ++iter) {
-	// Get result and put in record.
-	// Note that putting an array in Record makes a deep copy, so we
-	// can take over the MeqMatrix storage.
-	MeqResult resVal = iter->second.getResult (req);
-	const MeqMatrix& mat = resVal.getValue();
-	const double* ptr = mat.doubleStorage();
-	Array<double> arr;
-	// Store the result in the array.
-	// If the result is a scalar, expand it.
-	if (mat.nelements() == 1) {
-	  arr = *ptr;
-	  out.define (iter->first, arr);
-	} else {
-	  ASSERT (nx*ny == mat.nelements());
-	  out.define (iter->first, Array<double>(shape,
-						 const_cast<double*>(ptr),
-						 SHARE));
-	}
-      }
-      return out;
-    }
-
-    // Get the coefficients of parameters of type 'history' matching the
-    // parmNamePattern and the specified domain.
-    // This is a temporary hack to facillitate viewing the solve
-    // history in the SAS GUI.
-    map<string, vector<double> >
-    ParmFacade::getHistory (const string& parmNamePattern,
-			    double startx, double endx,
-			    double starty, double endy,
-			    double startSolveTime, double endSolveTime)
-    {
-      return record2Map (getHistoryRec (parmNamePattern,
-					startx, endx,
-					starty, endy,
-					startSolveTime, endSolveTime));
-    }
-
-    Record ParmFacade::getHistoryRec (const string& parmNamePattern,
-				      double startx, double endx,
-				      double starty, double endy,
-				      double startSolveTime,
-				      double endSolveTime)
-    {
-      // Get ParmValues of all matching parameters (all timestamps).
-      map<string, ParmValueSet> parmValues;
-      itsPDB.getValues (parmValues, parmNamePattern,
-			ParmDomain(startx, endx, starty, endy), -1);
-      // The output is returned in a record.
-      Record out;
-      // Loop over all parameters.
-      // A parameter can have multiple ParmValues, one for each timestamp.
-      // Parameters of type 'history' have exactly one coefficient.
-      // For each parameter, create an array of coefficients
-      // (one coefficient per timestamp), sorted on timestamp.
-      vector<double> coeff;
-      for (map<string,ParmValueSet>::iterator it = parmValues.begin();
-	   it != parmValues.end();
-	   ++it) {
-	coeff.resize (0);
-	vector<ParmValue>& values = it->second.getValues();
-	// Sort ParmValues on timestamp.
-	sort(values.begin(), values.end(), &pvLess);
-        
-	for (vector<ParmValue>::const_iterator value_it = values.begin();
-	     value_it != values.end();
-	     ++value_it) {
-	  // Only take ParmValues of type 'history' with a timestamp
-	  // within the specified time range.
-	  if (value_it->rep().itsType == "history" && 
-	      value_it->rep().itsTimeStamp >= startSolveTime &&
-	      value_it->rep().itsTimeStamp <= endSolveTime) {
-	    ASSERTSTR(value_it->rep().itsCoeff.size() == 1,
-		      "parms of type 'history' should have exactly one coefficient.");
-	    coeff.push_back(value_it->rep().itsCoeff[0]);
-	  }
-	}
-	out.define (it->first,
-		    Array<double> (IPosition(1,coeff.size()),
-				   &(coeff[0]), SHARE));
-      }        
       return out;
     }
 
@@ -291,7 +122,7 @@ namespace LOFAR {
       map<string, vector<double> > out;
       // Copy all values from the record to the map.
       for (uint i=0; i<rec.nfields(); ++i) {
-	// First make empty vecor; thereafter copy values to it.
+	// First make empty vector; thereafter copy values to it.
 	vector<double>& vec = out[rec.name(i)];
 	ASSERT (vec.size() == 0);
 	// Get result and put in map.
@@ -302,11 +133,6 @@ namespace LOFAR {
 	vec.assign (ptr, ptr+arr.nelements());
       }
       return out;
-    }
-
-    bool ParmFacade::pvLess (const ParmValue& left, const ParmValue& right)
-    {
-      return (left.rep().itsTimeStamp < right.rep().itsTimeStamp);
     }
 
   } // namespace ParmDB
