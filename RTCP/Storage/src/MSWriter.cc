285b65ecbb270dfd3f3a75b8013246212892f6fd
Chris Broekema <broekema@users.noreply.github.com>
2008-11-11 10:59:01 +0000

    Bug #1011: Make MSWriter a virtual base class and add a CasaCore and Null implementation

--- RTCP/Storage/src/MSWriter.cc
+++ RTCP/Storage/src/MSWriter.cc
@@ -1,6 +1,6 @@
-// MSWriter.cc: interface class for filling a MeasurementSet
+//  MSMriter.cc: Base classs for MS writer
 //
-//  Copyright (C) 2001
+//  Copyright (C) 2008
 //  ASTRON (Netherlands Foundation for Research in Astronomy)
 //  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
 //
@@ -24,127 +24,33 @@
 
 #include <lofar_config.h>
 
-#if defined HAVE_AIPSPP
-#include <Storage/MSWriter.h>
-#include <Storage/MSWriterImpl.h>
-#include <casa/Exceptions/Error.h>
-#include <Common/lofar_iostream.h>
 #include <Common/LofarLogger.h>
+#include <Common/lofar_iostream.h>
+#include <Storage/MSWriter.h>
 
 namespace LOFAR
 {
   namespace RTCP
   {
-    using namespace casa;
 
-    MSWriter::MSWriter (const char* msName, double startTime, double timeStep,
-                        int nChan, int nPol, uint nAntennas, const vector<double>& antPos,
-			const vector<string>& storageStationNames,
-			uint timesToIntegrate)
-      : itsWriter (0)
+    MSWriter::MSWriter() {}
+    MSWriter::~MSWriter() {}
+
+    int MSWriter::addBand(int, int, double, double)
     {
-      ASSERTSTR(antPos.size() == 3*nAntennas, antPos.size() << " == " << 3*nAntennas <<
-                "Antenna position vector does not have the right size!");
-      try {
-        itsWriter = new MSWriterImpl (msName, startTime, timeStep, nChan, nPol, 
-                                      nAntennas, antPos, storageStationNames, timesToIntegrate);
-      } catch (AipsError x) {
-        cerr << "MSWriter exception: " << x.getMesg() << endl;
-        exit(0);
-      } catch (...) {
-        cerr << "Unexpected MSWriter exception while creating MSWriterImpl" << endl;
-        exit(0);
-      }
+      return 0;
     }
 
-    MSWriter::~MSWriter()
+    int MSWriter::addBand(int, int, double, const double*, const double*) 
     {
-      delete itsWriter;
+      return 0;
     }
+    
+    void MSWriter::addField(double, double, unsigned)
+    {}
+    
+    void MSWriter::write(int, int, int, int, int, const fcomplex*, const bool*, const float*)
+    {}
 
-    int MSWriter::addBand (int npolarizations, int nchannels,
-                           double refFreq, double chanWidth)
-    {
-      int res;
-      try {
-        res = itsWriter->addBand (npolarizations, nchannels, refFreq, chanWidth);
-      } catch (AipsError x) {
-        cerr << "AIPS exception in MSWriterImpl: " << x.getMesg() << endl;
-        exit(0);
-      } catch (...) {
-        cerr << "Unexpected MSWriter exception during addBand" << endl;
-        exit(0);
-      }
-      return res;
-    }
-
-    int MSWriter::addBand (int npolarizations, int nchannels,
-                           double refFreq, const double* chanFreqs,
-                           const double* chanWidths)
-    {
-      int res;
-      try {
-        res = itsWriter->addBand (npolarizations, nchannels, refFreq,
-                                  chanFreqs, chanWidths);
-      } catch (AipsError x) {
-        cerr << "AIPS exception in MSWriterImpl: " << x.getMesg() << endl;
-        exit(0);
-      } catch (...) {
-        cerr << "Unexpected MSWriter exception during addBand" << endl;
-        exit(0);
-      }
-      return res;
-    }
-
-    void MSWriter::addField (double RA, double DEC, unsigned beamIndex)
-    {
-      try {
-        itsWriter->addField (RA, DEC, beamIndex);
-      } catch (AipsError x) {
-        cerr << "AIPS exception in MSWriterImpl: " << x.getMesg() << endl;
-        exit(0);
-      } catch (...) {
-        cerr << "Unexpected MSWriter exception during addField" << endl;
-        exit(0);
-      }
-    }
-
-    void MSWriter::write (int bandId, int channelId, 
-                          int nrChannels, int timeCounter, int nrdata, 
-                          const fcomplex* data, const bool* flags,
-                          const float* weights)
-    {
-      try {
-        itsWriter->write (bandId, channelId, nrChannels, timeCounter,
-                          nrdata, data, flags, weights);
-      } catch (AipsError x) {
-        cerr << "AIPS exception in MSWriterImpl: " << x.getMesg() << endl;
-        exit(0);
-      } catch (std::exception &ex)  {
-        cerr << "Unexpected MSWriter exception during write:" << ex.what() << endl;
-	perror("perror returns: ");
-        exit(0);
-      }
-    }
-
-    int MSWriter::nrAntennas() const
-    { return itsWriter->nrAntennas(); }
-
-    int MSWriter::nrBands() const
-    { return itsWriter->nrBands(); }
-
-    int MSWriter::nrFields() const
-    { return itsWriter->nrFields(); }
-
-    int MSWriter::nrPolarizations() const
-    { return itsWriter->nrPolarizations(); }
-
-    int MSWriter::nrTimes() const
-    { return itsWriter->nrTimes(); }
-
-
-  } // namespace RTCP
-
-} // namespace LOFAR
-
-#endif // defined HAVE_AIPSPP
+  }
+}
