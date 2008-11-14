3912b83e45469ea89164472cc814a59f4141e837
Ger van Diepen <gervandiepen@users.noreply.github.com>
2008-11-14 15:35:50 +0000

    bug 1172: Made comments meet LOFAR standards

--- CEP/BB/MWControl/include/MWControl/MWMultiSpec.h
+++ CEP/BB/MWControl/include/MWControl/MWMultiSpec.h
@@ -1,13 +1,31 @@
-/// @file
-/// @brief Class representing a multi step in the MWSpec composite pattern
-///
-/// @copyright (c) 2007 ASKAP, All Rights Reserved.
-/// @author Ger van Diepen (diepen AT astron nl)
-///
+//# MWMultiSpec.h: Class representing a multi step in the MWSpec composite pattern
+//#
+//# Copyright (C) 2005
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
 //# $Id$
 
 #ifndef LOFAR_MWCONTROL_MWMULTISPEC_H
 #define LOFAR_MWCONTROL_MWMULTISPEC_H
+
+// @file
+// @brief Class representing a multi step in the MWSpec composite pattern
+// @author Ger van Diepen (diepen AT astron nl)
 
 //# Includes
 #include <MWControl/MWSpec.h>
@@ -15,21 +33,21 @@
 
 namespace LOFAR { namespace CEP {
 
-  /// @ingroup mwcontrol
-  /// @brief Class representing a multi step in the MWSpec composite pattern.
+  // @ingroup MWControl
+  // @brief Class representing a multi step in the MWSpec composite pattern.
 
-  /// This is the class representing a multi step in the MWSpec composite
-  /// pattern (see Design Patterns, Gamma et al, 1995).
-  /// It holds the information about the step in a ParameterSet object.
+  // This is the class representing a multi step in the MWSpec composite
+  // pattern (see Design Patterns, Gamma et al, 1995).
+  // It holds the information about the step in a ParameterSet object.
 
   class MWMultiSpec : public MWMultiStep
   {
   public:
-    /// Default constructor.
+    // Default constructor.
     MWMultiSpec();
 
-    /// Construct from the given .parset file.
-    /// Unspecified items are taken from the parent specification.
+    // Construct from the given .parset file.
+    // Unspecified items are taken from the parent specification.
     MWMultiSpec(const std::string& name,
 		const ParameterSet& parset,
 		const MWSpec* parent);
@@ -39,16 +57,16 @@ namespace LOFAR { namespace CEP {
     // Create as a new MWStep object.
     static MWStep::ShPtr create();
 
-    /// Clone the step object.
+    // Clone the step object.
     virtual MWMultiSpec* clone() const;
 
-    /// Print the contents and type. Indent as needed.
+    // Print the contents and type. Indent as needed.
     virtual void print (std::ostream& os, const std::string& indent) const;
 
   private:
     MWSpec itsSpec;
   };
 
-}} /// end namespaces
+}} // end namespaces
 
 #endif
--- CEP/BB/MWControl/include/MWControl/MWStrategySpec.h
+++ CEP/BB/MWControl/include/MWControl/MWStrategySpec.h
@@ -1,13 +1,31 @@
-/// @file
-/// @brief Specification of a BBS strategy
-///
-/// @copyright (c) 2007 ASKAP, All Rights Reserved.
-/// @author Ger van Diepen (diepen AT astron nl)
-///
+//# MWStrategySpec.h: Specification of a BBS strateg
+//#
+//# Copyright (C) 2005
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
 //# $Id$
 
 #ifndef LOFAR_MWCONTROL_MWSTRATEGYSPEC_H
 #define LOFAR_MWCONTROL_MWSTRATEGYSPEC_H
+
+// @file
+// @brief Specification of a BBS strategy
+// @author Ger van Diepen (diepen AT astron nl)
 
 //# Includes
 #include <MWControl/MWMultiSpec.h>
@@ -18,43 +36,43 @@
 
 namespace LOFAR { namespace CEP {
 
-  /// @ingroup mwcontrol
-  /// @brief Specification of a BBS strategy
+  // @ingroup MWControl
+  // @brief Specification of a BBS strategy
 
-  /// This class contains the specification of a BBS strategy.
-  /// It consists of two parts:
-  /// <ul>
-  ///  <li> The work domain specification which defines the work domain size and
-  ///       optionally the basic data selection and integration.
-  ///  <li> The name of the MWMultiSpec object containing the steps to be performed
-  ///       when processing the data for this strategy.
-  /// </ul>
-  /// The strategy specification is read from a LOFAR .parset file.
+  // This class contains the specification of a BBS strategy.
+  // It consists of two parts:
+  // <ul>
+  //  <li> The work domain specification which defines the work domain size and
+  //       optionally the basic data selection and integration.
+  //  <li> The name of the MWMultiSpec object containing the steps to be performed
+  //       when processing the data for this strategy.
+  // </ul>
+  // The strategy specification is read from a LOFAR .parset file.
 
   class MWStrategySpec
   {
   public:
-    /// Default constructor.
+    // Default constructor.
     MWStrategySpec()
       {}
 
-    /// Construct a MWStrategySpec having the name \a name. Configuration
-    /// information for this step can be retrieved from the parameter set \a
-    /// parset, by searching for keys <tt>Strategy.\a name</tt>.
+    // Construct a MWStrategySpec having the name \a name. Configuration
+    // information for this step can be retrieved from the parameter set \a
+    // parset, by searching for keys <tt>Strategy.\a name</tt>.
     MWStrategySpec (const std::string& name, const ParameterSet& parset);
 
     // Get the work domain specification.
     WorkDomainSpec getWorkDomainSpec() const;
 
-    /// Print the contents in human readable form into the output stream.
+    // Print the contents in human readable form into the output stream.
     void print (std::ostream& os) const;
 
-    /// Return the step (possibly multi step) specification in this strategy.
+    // Return the step (possibly multi step) specification in this strategy.
     const MWMultiSpec& getSteps() const
       { return itsSteps; }
 
-    /// @name Accessor methods
-    /// @{
+    // @name Accessor methods
+    // @{
     const std::string&       getName() const
       { return itsName; }
     std::vector<std::string> getStations() const;
@@ -63,21 +81,21 @@ namespace LOFAR { namespace CEP {
     std::string              getCorrSelection() const;
     DomainShape              getWorkDomainSize() const;
     DomainShape              getIntegration() const;
-    /// @}
+    // @}
 
   private:
-    /// The name of the strategy.
+    // The name of the strategy.
     std::string  itsName;
-    /// The strategy parset.
+    // The strategy parset.
     ParameterSet itsParSet;
-    /// The step(s) in this strategy.
+    // The step(s) in this strategy.
     MWMultiSpec  itsSteps;
   };
 
 
-  /// Write the contents of a BBSStrategy to an output stream.
+  // Write the contents of a BBSStrategy to an output stream.
   std::ostream& operator<< (std::ostream&, const MWStrategySpec&);
 
-}} /// end namespaces
+}} // end namespaces
 
 #endif
