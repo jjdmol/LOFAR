aafc2a7e490d637e5acf7e1e3daf44488ce5d407
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-01-23 14:32:08 +0000

    bug 1314: First (incomplete)version of a distributed ParmFacade

--- trunk/CEP/ParmDB/src/ParmFacade.cc
+++ trunk/CEP/ParmDB/src/ParmFacade.cc
@@ -22,13 +22,7 @@
 
 #include <lofar_config.h>
 #include <ParmDB/ParmFacade.h>
-#include <ParmDB/ParmDB.h>
-#include <ParmDB/ParmSet.h>
-#include <ParmDB/ParmCache.h>
-#include <ParmDB/Parm.h>
-#include <Common/LofarLogger.h>
-#include <Common/StringUtil.h>
-#include <casa/Utilities/Regex.h>
+#include <ParmDB/ParmFacadeLocal.h>
 
 using namespace std;
 using namespace casa;
@@ -43,36 +37,11 @@ namespace LOFAR {
   namespace BBS {
 
     ParmFacade::ParmFacade (const string& tableName)
-      : itsPDB(ParmDBMeta("casa", tableName))
+      : itsRep(new ParmFacadeLocal(tableName))
     {}
 
     ParmFacade::~ParmFacade()
     {}
-
-    vector<double> ParmFacade::getRange (const string& parmNamePattern) const
-    {
-      string pp = parmNamePattern;
-      if (pp.empty()) {
-	pp = "*";
-      }
-      Box dom = itsPDB.getRange (pp);
-      vector<double> res(4);
-      res[0] = dom.lowerX();
-      res[1] = dom.lowerY();
-      res[2] = dom.upperX();
-      res[3] = dom.upperY();
-      return res;
-    }
-
-    // Get all parameter names in the table.
-    vector<string> ParmFacade::getNames (const string& parmNamePattern) const
-    {
-      string pp = parmNamePattern;
-      if (pp.empty()) {
-	pp = "*";
-      }
-      return itsPDB.getNames(pp);
-    }
 
     // Get the parameter values for the given parameters and domain.
     map<string, vector<double> >
@@ -85,121 +54,6 @@ namespace LOFAR {
                                     freqv1, freqv2, nfreq,
                                     timev1, timev2, ntime,
                                     asStartEnd));
-    }
-
-    Record ParmFacade::getValues (const string& parmNamePattern,
-                                  double freqv1, double freqv2, int nfreq,
-                                  double timev1, double timev2, int ntime,
-                                  bool asStartEnd)
-    {
-      // Create the predict grid.
-      Axis::ShPtr axisx (new RegularAxis(freqv1, freqv2, nfreq, asStartEnd));
-      Axis::ShPtr axisy (new RegularAxis(timev1, timev2, ntime, asStartEnd));
-      return doGetValues (parmNamePattern, Grid(axisx, axisy));
-    }
-
-    Record ParmFacade::getValues (const string& parmNamePattern,
-                                  const vector<double>& freqv1,
-                                  const vector<double>& freqv2,
-                                  const vector<double>& timev1,
-                                  const vector<double>& timev2,
-                                  bool asStartEnd)
-    {
-      // Create the predict grid.
-      Axis::ShPtr axisx (new OrderedAxis(freqv1, freqv2, asStartEnd));
-      Axis::ShPtr axisy (new OrderedAxis(timev1, timev2, asStartEnd));
-      return doGetValues (parmNamePattern, Grid(axisx, axisy));
-    }
-
-    Record ParmFacade::doGetValues (const string& parmNamePattern,
-                                    const Grid& predictGrid)
-    {
-      // Get all matching parm names.
-      vector<string> names = getNames (parmNamePattern);
-      // The output is returned in a record.
-      Record out;
-      // Form the names to get.
-      // The returned parmId should be the index.
-      ParmSet parmSet;
-      for (uint i=0; i<names.size(); ++i) {
-        ASSERT (parmSet.addParm (itsPDB, names[i]) == i);
-      }
-      const Axis& axisx = *predictGrid[0];
-      const Axis& axisy = *predictGrid[1];
-      uint nfreq = axisx.size();
-      uint ntime = axisy.size();
-      // Create and fill the cache for the given domain.
-      Box domain (Point(axisx.lower(0), axisy.lower(0)),
-                  Point(axisx.upper(nfreq-1), axisy.upper(ntime-1)));
-      ParmCache parmCache(parmSet, domain);
-      // Now create the Parm object for each parm and get the values.
-      Array<double> result;
-      for (uint i=0; i<names.size(); ++i) {
-        Parm parm(parmCache, i);
-        parm.getResult (result, predictGrid);
-        // If the value is constant, the array has only one element.
-        // In that case resize it to the full grid.
-        if (result.nelements() == 1) {
-          double dval = *result.data();
-          result.resize (IPosition(2, axisx.size(), axisy.size()));
-          result = dval;
-        }
-        out.define (names[i], result);
-      }
-      return out;
-    }
-
-    Record ParmFacade::getValuesGrid (const string& parmNamePattern,
-                                      double sfreq, double efreq,
-                                      double stime, double etime)
-    {
-      // Get all matching parm names.
-      vector<string> names = getNames (parmNamePattern);
-      // The output is returned in a record.
-      Record out;
-      Record ginfo;
-      // Form the names to get.
-      // The returned parmId should be the index.
-      ParmSet parmSet;
-      for (uint i=0; i<names.size(); ++i) {
-        ASSERT (parmSet.addParm (itsPDB, names[i]) == i);
-      }
-      // Create and fill the cache for the given domain.
-      Box domain (Point(sfreq, stime), Point(efreq, etime));
-      ParmCache parmCache(parmSet, domain);
-      // Now create the Parm object for each parm and get the values.
-      Array<double> result;
-      for (uint i=0; i<names.size(); ++i) {
-        Grid grid (getGrid (parmCache.getValueSet(i), domain));
-        Parm parm(parmCache, i);
-        parm.getResult (result, grid);
-        out.define (names[i], result);
-        ginfo.define (names[i] + ";freqs",
-                      Vector<double>(grid[0]->centers()));
-        ginfo.define (names[i] + ";times",
-                      Vector<double>(grid[1]->centers()));
-        ginfo.define (names[i] + ";freqwidths",
-                      Vector<double>(grid[0]->widths()));
-        ginfo.define (names[i] + ";timewidths",
-                      Vector<double>(grid[1]->widths()));
-      }
-      out.defineRecord ("_grid", ginfo);
-      return out;
-    }
-
-    Grid ParmFacade::getGrid (const ParmValueSet& valueSet, const Box& domain)
-    {
-      Grid grid(valueSet.getGrid());
-      if (valueSet.getType() == ParmValue::Scalar) {
-        // For scalars the detailed grids have to be combined.
-        vector<Grid> grids;
-        grids.reserve (valueSet.size());
-        for (uint i=0; i<valueSet.size(); ++i) {
-          grids.push_back (valueSet.getParmValue(i).getGrid());
-        }
-        grid = Grid(grids, true);
-      }
-      return grid.subset (domain);
     }
 
     map<string,vector<double> >
