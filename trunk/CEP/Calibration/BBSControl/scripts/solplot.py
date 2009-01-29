5117df6d03fa1d5c571f8907b174405857df3bf5
zwieten <zwieten@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-01-29 13:30:01 +0000

    Bug 1283: update of solplot.py

--- trunk/CEP/Calibration/BBSControl/scripts/solplot.py
+++ trunk/CEP/Calibration/BBSControl/scripts/solplot.py
@@ -2,18 +2,26 @@ import numpy
 import pylab
 import lofar.parmdb
 
-def fetch(db,stations, phasors=False, parmname="Gain:11"):
+def fetch(db, stations, phasors=False, parm="Gain:11", direction=""):
     print stations
+
+    suffix = ""
+    if direction:
+        suffix = ":%s" % direction
 
     ampl = []
     phase = []
     for station in stations:
         if phasors:
-            ampl.append(numpy.array(db.getValuesGrid("%s:Ampl:%s" % (parmname,station))["%s:Ampl:%s" % (parmname,station)]))
-            phase.append(numpy.array(db.getValuesGrid("%s:Phase:%s" % (parmname,station))["%s:Phase:%s" % (parmname,station)]))
+            fqname = "%s:Ampl:%s%s" % (parm, station, suffix)
+            ampl.append(numpy.array(db.getValuesGrid(fqname)[fqname]))
+            fqname = "%s:Phase:%s%s" % (parm, station, suffix)
+            phase.append(numpy.array(db.getValuesGrid(fqname)[fqname]))
         else:        
-            re = numpy.array(db.getValuesGrid("%s:Real:%s" % (parmname,station))["%s:Real:%s" % (parmname,station)])
-            im = numpy.array(db.getValuesGrid("%s:Imag:%s" % (parmname,station))["%s:Imag:%s" % (parmname,station)])
+            fqname = "%s:Real:%s%s" % (parm, station, suffix)
+            re = numpy.array(db.getValuesGrid(fqname)[fqname])
+            fqname = "%s:Imag:%s%s" % (parm, station, suffix)
+            im = numpy.array(db.getValuesGrid(fqname)[fqname])
             
             tmp = numpy.sqrt(re * re + im * im)
             tmp.transpose()
@@ -25,13 +33,14 @@ def fetch(db,stations, phasors=False, parmname="Gain:11"):
 
     return (ampl, phase)
     
-def plot(sol):
+def plot(sol, stack=True, sep=5.0, fig=None):
+    pylab.figure(fig)
     pylab.clf()
     
     offset = 0.0
     for i in range(0,len(sol)):
         pylab.plot(sol[i] + offset)
-        offset += sol[i].mean() + 5.0 * sol[i].std()
+        if stack:
+            offset += sol[i].mean() + sep * sol[i].std()
         
     pylab.show()
-    
