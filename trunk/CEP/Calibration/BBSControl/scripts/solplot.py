d20467f352f5900a3c1a0527cb2077c848aedd93
mevius <mevius@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-01-29 11:12:53 +0000

    Bug 1322: adjusted solplot, added possibility to select on name

--- trunk/CEP/Calibration/BBSControl/scripts/solplot.py
+++ trunk/CEP/Calibration/BBSControl/scripts/solplot.py
@@ -2,18 +2,18 @@ import numpy
 import pylab
 import lofar.parmdb
 
-def fetch(db, stations, phasors=False):
+def fetch(db,stations, phasors=False, parmname="Gain:11"):
     print stations
 
     ampl = []
     phase = []
     for station in stations:
         if phasors:
-            ampl.append(numpy.array(db.getValuesGrid("Gain:11:Ampl:%s" % station)["Gain:11:Ampl:%s" % station]))
-            phase.append(numpy.array(db.getValuesGrid("Gain:11:Phase:%s" % station)["Gain:11:Phase:%s" % station]))
+            ampl.append(numpy.array(db.getValuesGrid("%s:Ampl:%s" % (parmname,station))["%s:Ampl:%s" % (parmname,station)]))
+            phase.append(numpy.array(db.getValuesGrid("%s:Phase:%s" % (parmname,station))["%s:Phase:%s" % (parmname,station)]))
         else:        
-            re = numpy.array(db.getValuesGrid("Gain:11:Real:%s" % station)["Gain:11:Real:%s" % station])
-            im = numpy.array(db.getValuesGrid("Gain:11:Imag:%s" % station)["Gain:11:Imag:%s" % station])
+            re = numpy.array(db.getValuesGrid("%s:Real:%s" % (parmname,station))["%s:Real:%s" % (parmname,station)])
+            im = numpy.array(db.getValuesGrid("%s:Imag:%s" % (parmname,station))["%s:Imag:%s" % (parmname,station)])
             
             tmp = numpy.sqrt(re * re + im * im)
             tmp.transpose()
