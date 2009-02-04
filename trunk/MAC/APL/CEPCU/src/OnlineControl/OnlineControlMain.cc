97a0e679ee62c09b3c2b9ad5b902176f356b4004
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-02-04 14:08:36 +0000

    Bug 1000: OnlineControlled still used the old way of initializing the logsystem.

--- trunk/MAC/APL/CEPCU/src/OnlineControl/OnlineControlMain.cc
+++ trunk/MAC/APL/CEPCU/src/OnlineControl/OnlineControlMain.cc
@@ -37,7 +37,7 @@ int main(int argc, char* argv[])
 		return(-1);
 	}
 
-	GCFTask::init(argc, argv);
+	GCFTask::init(argc, argv, "OnlineControl");
 
 	ParentControl*	pc = ParentControl::instance();
 	pc->start();	// make initial transition
