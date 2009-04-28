47aa847be67533204c8c93a00cc1cbc0818ed4eb
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-04-28 11:21:25 +0000

    Bug #1315: extra TBB size and Memory test

--- trunk/StationTest/station_production.py
+++ trunk/StationTest/station_production.py
@@ -33,8 +33,20 @@ if opts.rsp_nr==None:
 if opts.tbb_nr==None:
   op.error('Option -t must specify the number of tbb boards')
 
+if opts.rsp_nr == 4:
+        RspBrd = 'rsp0,rsp1,rsp2,rsp3'
+        SubBrd = 'rsp0'
+        SubRck = 'sub0'      
+if opts.rsp_nr == 12:
+        RspBrd = 'rsp0,rsp1,rsp2,rsp3,rsp4,rsp5,rsp6,rsp7,rsp8,rsp9,rsp10,rsp11'
+        SubBrd = 'rsp0,rsp4,rsp8'
+        SubRck = 'sub0,sub1,sub2'  
+if opts.rsp_nr == 24:
+        RspBrd = 'rsp0,rsp1,rsp2,rsp3,rsp4,rsp5,rsp6,rsp7,rsp8,rsp9,rsp10,rsp11,rsp12,rsp13,rsp14,rsp15,rsp16,rsp17,rsp18,rsp19,rsp20,rsp21,rsp22,rsp23'
+        SubBrd = 'rsp0,rsp4,rsp8,rsp12,rsp16,rsp20'
+        SubRck = 'sub0,sub1,sub2,sub3,sub4,sub5'
 
-################################################################################
+###############################################################################
 # Define subrack testlog class for pass/fail and logging
 vlev = opts.verbosity
 testId = ''
@@ -87,17 +99,51 @@ else:
   sr.appendLog(11,'Expected:')
   sr.appendFile(11,'gold/tbb_version.gold')
   sr.setResult('FAILED')
-  
 
+################################################################################
+sr.setId('TBB size check - ')
+sr.appendLog(21,'')
+sr.appendLog(21,'### Verify the size of the TBB memory modules')
+sr.appendLog(21,'')
+res = cli.command('./tbb_size.sh')
+if res.find('wrong')==-1:
+  sr.appendLog(11,'>>> TBB size test went OK')
+else:
+  sr.appendLog(11,'>>> TBB size test went wrong')
+  sr.appendLog(11,'CLI:')
+  sr.appendLog(11,res,1,1,1)
+  sr.appendLog(11,'Result:')
+  sr.appendFile(11,'tbb_size.log')
+  sr.appendLog(11,'Expected:')
+  sr.appendFile(11,'gold/tbb_size.gold')
+  sr.setResult('FAILED')
+ 
+ 
+################################################################################
+sr.setId('TBB memory check - ')
+sr.appendLog(21,'')
+sr.appendLog(21,'### Verify TBB memory modules on the TBB')
+sr.appendLog(21,'')
+res = cli.command('./tbb_memory.sh')
+if res.find('wrong')==-1:
+  sr.appendLog(11,'>>> TBB memory test went OK')
+else:
+  sr.appendLog(11,'>>> TBB memory test went wrong')
+  sr.appendLog(11,'CLI:')
+  sr.appendLog(11,res,1,1,1)
+  sr.appendLog(11,'Result:')
+  sr.appendFile(11,'tbb_memory.log')
+  sr.appendLog(11,'Expected:')
+  sr.appendFile(11,'gold/tbb_memory.gold')
+  sr.setResult('FAILED')
+ 
 ################################################################################
 sr.setId('SPU status - ')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Verify the RSP - SPU I2C interface by reading the SPU sensor data')
 sr.appendLog(21,'')
-
-res = cli.command('python i2c_spu.py')
-res = cli.command('python i2c_spu.py')
-if res.find('wrong')==-1:
+res = cli.command('python i2c_spu.py --sub %s --rep 1 -v 11' %(SubRck,))
+if res.find('FAILED')==-1:
   sr.appendLog(11,'>>> RSP - SPU I2c interface test went OK')
 else:
   sr.appendLog(11,'>>> RSP - SPU I2c interface test went wrong')
@@ -113,9 +159,8 @@ sr.setId('TD status - ')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Verify the RSP - TD I2C interface by reading the TD sensor data')
 sr.appendLog(21,'')
-
-res = cli.command('python i2c_td.py')
-if res.find('wrong')==-1:
+res = cli.command('python i2c_td.py --sub %s --rep 1 -v 11' %(SubBrd,))
+if res.find('FAILED')==-1:
   sr.appendLog(11,'>>> RSP - TD I2c interface test went OK')
 else:
   sr.appendLog(11,'>>> RSP - TD I2c interface test went wrong')
@@ -131,11 +176,8 @@ sr.setId('RCU-RSP - ')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Verify the RCU -> RSP LVDS interfaces by capturing pseudo random data on RSP')
 sr.appendLog(21,'')
-
-
-res = cli.command('python verify.py --brd rsp0,rsp1 --fpga blp0,blp1,blp2,blp3 --pol x,y --rep 1 -v 11 --te tc/prsg.py')
-
-if res.find('FAILED')==-1:
+res = cli.command('python verify.py --brd %s --fpga blp0,blp1,blp2,blp3 --pol x,y --rep 1 -v 11 --te tc/prsg.py' %(RspBrd,)) 
+if res.find('wrong')==-1:
   sr.appendLog(11,'>>> RCU-RSP interface test went OK')
   sr.appendFile(21,'tc/prsg.log')
 else:
@@ -150,10 +192,8 @@ sr.setId('Serdes ring -')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Verify the Serdes ring connection between the RSP boards')
 sr.appendLog(21,'')
-
-res = cli.command('python verify.py --brd rsp0,rsp1 --rep 1 -v 21 --te tc/serdes.py')
-
-if res.find('FAILED')==-1:
+res = cli.command('python verify.py --brd %s --rep 1 -v 21 --te tc/serdes.py --diag_sync 0' %(RspBrd,))
+if res.find('wrong')==-1:
   sr.appendLog(11,'>>> Serdes ring test went OK')
   sr.appendLog(21,'tc/serdes.log')
 else:
@@ -168,10 +208,8 @@ sr.setId('Build In Self Test -')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Build In Self Test (BIST)')
 sr.appendLog(21,'')
-
-res = cli.command('python verify.py --brd rsp0,rsp1 tc/bist.py')
-
-if res.find('FAILED')==-1:
+res = cli.command('python verify.py --brd %s --rep 1 -v 21 --te tc/bist.py' %(RspBrd,))
+if res.find('wrong')==-1:
   sr.appendLog(11,'>>> BIST went OK')
   sr.appendLog(21,'tc/bist.log')
 else:
@@ -186,7 +224,6 @@ sr.setId('RCU-RSP-TBB - ')
 sr.appendLog(21,'')
 sr.appendLog(21,'### Verify the RCU - RSP - TBB LVDS interfaces by capturing pseudo random data on TBB')
 sr.appendLog(21,'')
-
 res = cli.command('./tbb_prbs_tester.sh')
 if res.find('wrong')==-1:
   sr.appendLog(11,'>>> RCU - RSP - TBB LVDS interfaces test went OK')
