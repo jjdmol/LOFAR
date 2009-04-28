6b07beb78f325a04cd528e0f303fb06f8a9f0f01
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-04-28 11:17:36 +0000

    Bug #1315: bugs fixed in prbs test by Gijs

--- StationTest/prbs_dir_test.py
+++ StationTest/prbs_dir_test.py
@@ -1,6 +1,12 @@
 
 """ script for testing PRBS data in dir.
-Gijs, 16 dec 07
+This script can be used for testing data from the TBB, it will be used by TBB test scripts.
+
+Started by Gijs, 16 dec 07
+Modified by Gijs on March 17 2009: 
+-PRBS test bug fixed, when data is all 0 error did't count.
+-CRC test on files with RRBS errors. When a PRBS error and no CRC, error in RCU-to-RSP communications, when both has errors, error between RSP-to-TBB communication
+
 """
 # INIT
 
@@ -10,13 +16,14 @@ import os
 import time
 import commands
 
-# Open file for processing	
+# Look for files to test	
 def open_dir() :
 	files = os.listdir('./prbs/.')
 	files.sort()
-#	print files
+	print files
         return files
 
+# Open de file for testing
 def open_file(files, file_nr) :
         file_name = './prbs/' + files[file_nr][:]
  	if files[file_nr][-3:] == 'dat':
@@ -41,9 +48,11 @@ def read_frame(f, info_plot, frame_nr,f_log):
 		time_string = time.ctime(time_info[1])
 #		string_info =  'Frame nr %(FR)d Station %(ST)d  RSP %(RSP)d  RCU %(RCU)d  Sample rate %(S)d MHz  time of data  %(ti_D)s and %(SN)00.6f seconds'%\
 #				{"FR": frame_nr, "ST": station_info[0] ,"RSP": station_info[1], "RCU": station_info[2], "S": station_info[3], "ti_D": time_string,"SN": float(time_info[2])/float(200000000)}
-		string_info =  'Frame nr %(FR)d Station %(ST)d  RSP %(RSP)d  RCU %(RCU)d  Sample rate %(S)d MHz'%\
-				{"FR": frame_nr, "ST": station_info[0] ,"RSP": station_info[1], "RCU": station_info[2], "S": station_info[3]}
-#		print string_info
+		string_info =  'Frame nr %(FR)d RSP %(RSP)d  RCU %(RCU)d  Sample rate %(S)d MHz'%\
+				{"FR": frame_nr,"RSP": station_info[1], "RCU": station_info[2], "S": station_info[3]}
+
+
+#	print string_info
 		f_log.write(string_info + '\n')
 	div_info = array.array('H')
 	div_info.fromfile(f,36)          # Bytes 16..87
@@ -68,6 +77,11 @@ def PRBS_CHECK(data_list, prev):
 			prev = data_list[i] & 0x07FF
 		elif data_list[i] == 0xFFFF :
 			prbs_err = prbs_err + 1
+                elif data_list[i]  == data_list[i-1]: 
+                        cur = data_list[i]
+                        samples_chk = samples_chk + 1
+                        prbs_err = prbs_err + 1
+                        prev = data_list[i] & 0x07FF
 		else :
 			cur = data_list[i] & 0x0FFE
 			samples_chk = samples_chk + 1
@@ -77,6 +91,98 @@ def PRBS_CHECK(data_list, prev):
 			prev = data_list[i] & 0x07FF
 	return samples_chk, prbs_err, prev
 
+# Function for testing CRC of header
+def CRC16_check(buf) :
+        CRC=0
+        CRC_poly=0x18005
+        bits=16
+        data=0
+        CRCDIV = (CRC_poly & 0x7fffffff) * 32768 # << 15
+        data = (buf[0] & 0x7fffffff) << 16
+        len_buf = len(buf)
+        for cnt in range(1,len_buf) :
+                data = data + buf[cnt]
+                for  cnt in range(bits) :
+                        if data & 0x80000000 :
+                                data = data ^ CRCDIV
+                        data = data & 0x7fffffff
+                        data = data * 2 # << 1
+        CRC = data >> 16
+        return CRC
+
+# Function for testing CRC of data
+def CRC32_check(buf) :
+        CRC=0
+        CRC_poly=0x104C11DB7    # 1 0000 0100 1100 0001 0001 1101 1011 0111 
+        bits=16
+        data=0
+        CRCDIV = (CRC_poly & 0x7fffffffffff) * 32768 #<< 15
+        data = buf[0]
+        data = data & 0x7fffffffffff
+        data = data << 16
+        data = data + buf[1]
+        data = data & 0x7fffffffffff
+        data = data << 16
+        len_buf = len(buf)
+        for cnt in range(2,len_buf) :
+                data = data + buf[cnt]
+                for  cnt in range(bits) :
+                        if data & 0x800000000000 :
+                                data = data ^ CRCDIV
+                        data = data & 0x7fffffffffff
+                        data = data * 2 # << 1
+        CRC = int(data >> 16)
+        return CRC
+
+#Function for testing CRC of complete frame (header and data)
+
+def crc_frame(f, info_plot, frame_nr,f_log):
+        CRC_ERROR=0
+        header = array.array('H')
+        data_in = array.array('H')
+        data_crc = array.array('H')
+
+        # READING HEADER INFORMATION
+        header.fromfile(f,44)       # Bytes 0..88
+        # remove SEQNR from header, this data is added after CRC calculations   
+        header[2]=0
+        header[3]=0
+        if CRC16_check(header) :
+                str_info = 'CRC ERROR IN HEADER '
+#                f_log.write(str_info )
+                CRC_ERROR=1
+
+        Station_id = header[0] & 0xFF
+        RSP_id = header[0] >> 8
+        RCU_id = header[1] &0xFF
+        Sample_rate = header[1] >> 8
+        Time = float((header[5] * 65536) + header[4])
+        Sample_nr = (header[7] * 65536) + header[6]
+        Samples = header[8]
+        if (info_plot) :
+                time_string = time.ctime(Time)
+
+#                str_info =  'Frame nr %(FR)d Station %(ST)d  RSP %(RSP)d  RCU %(RCU)d  Sample rate %(S)d MHz  time of data  %(ti_D)s and %(SN)00.6f seconds'%\
+#                                {"FR": frame_nr, "ST": Station_id ,"RSP": RSP_id, "RCU": RCU_id, "S": Sample_rate, "ti_D": time_string,"SN": float(Sample_nr)/float(200000000)}
+
+
+#               print string_info
+#                f_log.write(str_info + '\n')
+
+        del(header)
+        # READ DATA SAMPLES
+        data_in.fromfile(f,1024)
+        data_crc.fromfile(f,2)
+        data_list = data_in.tolist()
+        for cnt in range(len(data_in)):
+                data_in[cnt] = (data_in[cnt] & 0x0FFF)
+        data_in.append(data_crc[1])
+        data_in.append(data_crc[0])
+        if CRC32_check(data_in):
+                str_info = 'CRC ERROR IN DATA, '
+#                f_log.write(str_info )
+                CRC_ERROR=1
+        return CRC_ERROR
 
 # Main loop
 def main() :
@@ -106,7 +212,16 @@ def main() :
 			# plot results		
 #			print 'Samples checked : ' + str(samples_chk) + ' PRBS errors: ' + str(prbs_err)
 			f_log.write('Samples checked : ' + str(samples_chk) + ' PRBS errors: ' + str(prbs_err) + '\n')
-		
+		f.close
+		if prbs_err > 0:
+			(f, frames_to_proces) = open_file(files, file_cnt)
+	                if frames_to_proces >0 :
+				crc_err=0
+        	                for frame_cnt in range(frames_to_proces):
+                	                crc_err = crc_err + crc_frame(f, (frame_cnt==0), frame_cnt, f_log)
+#                       print 'Samples checked : ' + str(samples_chk) + ' PRBS errors: ' + str(prbs_err)
+                        f_log.write('Number of frames with CRC  errors: ' + str(crc_err) + '\n')
+
 		f.close
 	f_log.close
 
