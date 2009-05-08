f6cc0f9ce8ac1616d3af2323f7102c96ae9f9edc
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-05-08 13:16:47 +0000

    Bug 1276: Adapted flash_images.sh and added flash_tbb.sh

--- trunk/Firmware/tools/src/flash_images.sh
+++ trunk/Firmware/tools/src/flash_images.sh
@@ -1,31 +1,79 @@
-# !/bin/bash
+#!/bin/bash
 # version 2.2, date 05-11-2008,  M.J.Norden
-eval "swlevel 1"
-page=15
+
+SyntaxError()
+{
+	Msg=$1
+
+	[ -z "${Msg}" ] || echo "ERROR: ${Msg}"
+	echo ""
+	echo "Syntax: $(basename $0) -p [pagenr] -a [aphexfile] -b [bphexfile]"
+	exit 1
+}
+
+page=-1
+bphexfile=""
+aphexfile=""
+eval set argv=`getopt "p:a:b:h" $*`
+shift
+
+if [ "$1" == "--" ]; then SyntaxError; fi
+
+while [ "$1" != "--" ]
+do
+	case "$1" in
+		-p)	page=$2
+			shift 2
+			;;
+		-a)	aphexfile=$2
+			shift 2
+			;;
+		-b)	bphexfile=$2
+			shift 2
+			;;
+
+		-h) SyntaxError
+			;;
+
+	esac
+done
+shift
+
+
 station=`hostname -s`
-let rspboards=`sed -n  's/^\s*RS\.N_RSPBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
-bphexfile=bp3b_v5_3.hex
-aphexfile=ap3b_v5_3.hex
+
+if [ -e /opt/lofar/etc/RemoteStation.conf ]; then
+  let rspboards=`sed -n  's/^\s*RS\.N_RSPBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
+else
+  echo "Could not find /opt/lofar/etc/RemoteStation.conf"
+  let rspboards=12
+fi
 
 echo "This station is "$station
 echo "The number of rspboards is "$rspboards
 echo "The selected image page is "$page
 echo "The bp hex file is "$bphexfile
 echo "The ap hex file is "$aphexfile
+read -p "Is this ok [y/n]" answer
 
-for ((ind=0; ind < $rspboards; ind++)) do
-  MACadr=$(printf "10:FA:00:00:%02x:00" $ind)
-  sudo rsuctl3 -w -q -p $page -b $bphexfile -a $aphexfile -m $MACadr -F
-done
-
-
-if [ $page != 0 ] ; then
+if [[ "$answer" =~ "([yY])" ]]; then   
+   eval "swlevel 1"
+   if [ -e $aphexfile -a -e $bphexfile ]; then
+      for ((ind=0; ind < $rspboards; ind++)) do
+	MACadr=$(printf "10:FA:00:00:%02x:00" $ind)
+	sudo rsuctl3 -w -q -p $page -b $bphexfile -a $aphexfile -m $MACadr -F
+      done
+      if [ $page != 0 ] ; then
 	for ((ind=0; ind < $rspboards; ind++)) do
   		MACadr=$(printf "10:FA:00:00:%02x:00" $ind)
   		sudo rsuctl3_reset -m $MACadr -p $page -x -q;
         done
-    else 
-       echo "When the RSP flash action was sucessful please"
-       echo "set page to page=0 and reset the RSP boards"
-       echo "with ./restart_images.sh"
-fi       
+      else 
+        echo "When the RSP flash action was sucessful please"
+        echo "set page to page=0 and reset the RSP boards"
+        echo "with restart_images.sh"
+      fi       
+   else 
+     echo "Could not find one of the ap/bp hex files"
+   fi
+fi
