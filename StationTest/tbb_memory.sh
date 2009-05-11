14dde104cedf3cd36f304e5db3c30b9a7101d5cb
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-05-11 10:40:04 +0000

    bug 1315#: more general structure

--- StationTest/tbb_memory.sh
+++ StationTest/tbb_memory.sh
@@ -6,115 +6,26 @@
 
 rm -f tbb_memory*.log
 rm -f tbb_memory*.diff
-tbbctl --testddr=0 > tbb_memory0.log
-tbbctl --testddr=1 > tbb_memory1.log
-tbbctl --testddr=2 > tbb_memory2.log
-tbbctl --testddr=3 > tbb_memory3.log
-tbbctl --testddr=4 > tbb_memory4.log
-tbbctl --testddr=5 > tbb_memory5.log
-tbbctl --testddr=6 > tbb_memory6.log
-tbbctl --testddr=7 > tbb_memory7.log
-tbbctl --testddr=8 > tbb_memory8.log
-tbbctl --testddr=9 > tbb_memory9.log
-tbbctl --testddr=10 > tbb_memory10.log
-tbbctl --testddr=11 > tbb_memory11.log
 
-
-
-
-diff tbb_memory0.log gold/tbb_memory0.gold > tbb_memory0.diff
-if [ -e tbb_memory0.log ] && [ -e gold/tbb_memory0.gold ] && [ -e tbb_memory0.diff ] && ! [ -s tbb_memory0.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (0) test went OK"
+if [ -e /opt/lofar/etc/RemoteStation.conf ]; then
+  let tbboards=`sed -n  's/^\s*RS\.N_TBBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
 else
-  echo "TBB memory (0) test went wrong"
+  echo "Could not find /opt/lofar/etc/RemoteStation.conf"
+  exit 1
 fi
 
 
-diff tbb_memory1.log gold/tbb_memory1.gold > tbb_memory1.diff
-if [ -e tbb_memory1.log ] && [ -e gold/tbb_memory1.gold ] && [ -e tbb_memory1.diff ] && ! [ -s tbb_memory1.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (1) test went OK"
-else
-  echo "TBB memory (1) test went wrong"
-fi
+for ((ind=0; ind < $tbboards; ind++)) do
+        tbbctl --testddr=$ind > tbb_memory$ind.log
+      done
 
-diff tbb_memory2.log gold/tbb_memory2.gold > tbb_memory2.diff
-if [ -e tbb_memory2.log ] && [ -e gold/tbb_memory2.gold ] && [ -e tbb_memory2.diff ] && ! [ -s tbb_memory2.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (2) test went OK"
-else
-  echo "TBB memory (2) test went wrong"
-fi
+for ((ind=0; ind < $tbboards; ind++)) do
+            diff tbb_memory$ind.log gold/tbb_memory$ind.gold > tbb_memory$ind.diff
+            if [ -e tbb_memory$ind.log ] && [ -e gold/tbb_memory$ind.gold ] && [ -e tbb_memory$ind.diff ] && ! [ -s tbb_memory$ind.diff ]; then
+              # The files exists AND has the diff size 0
+              echo "TBB memory ($ind) test went OK"
+            else
+              echo "TBB memory ($ind) test went wrong"
+            fi
+      done
 
-diff tbb_memory3.log gold/tbb_memory3.gold > tbb_memory3.diff
-if [ -e tbb_memory3.log ] && [ -e gold/tbb_memory3.gold ] && [ -e tbb_memory3.diff ] && ! [ -s tbb_memory3.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (3) test went OK"
-else
-  echo "TBB memory (3) test went wrong"
-fi
-
-diff tbb_memory4.log gold/tbb_memory4.gold > tbb_memory4.diff
-if [ -e tbb_memory4.log ] && [ -e gold/tbb_memory4.gold ] && [ -e tbb_memory4.diff ] && ! [ -s tbb_memory4.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (4) test went OK"
-else
-  echo "TBB memory (4) test went wrong"
-fi
-
-diff tbb_memory5.log gold/tbb_memory5.gold > tbb_memory5.diff
-if [ -e tbb_memory5.log ] && [ -e gold/tbb_memory5.gold ] && [ -e tbb_memory5.diff ] && ! [ -s tbb_memory5.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (5) test went OK"
-else
-  echo "TBB memory (5) test went wrong"
-fi
-
-diff tbb_memory6.log gold/tbb_memory6.gold > tbb_memory6.diff
-if [ -e tbb_memory6.log ] && [ -e gold/tbb_memory6.gold ] && [ -e tbb_memory6.diff ] && ! [ -s tbb_memory6.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (6) test went OK"
-else
-  echo "TBB memory (6) test went wrong"
-fi
-
-diff tbb_memory7.log gold/tbb_memory7.gold > tbb_memory7.diff
-if [ -e tbb_memory7.log ] && [ -e gold/tbb_memory7.gold ] && [ -e tbb_memory7.diff ] && ! [ -s tbb_memory7.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (7) test went OK"
-else
-  echo "TBB memory (7) test went wrong"
-fi
-
-diff tbb_memory8.log gold/tbb_memory8.gold > tbb_memory8.diff
-if [ -e tbb_memory8.log ] && [ -e gold/tbb_memory8.gold ] && [ -e tbb_memory8.diff ] && ! [ -s tbb_memory8.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (8) test went OK"
-else
-  echo "TBB memory (8) test went wrong"
-fi
-
-diff tbb_memory9.log gold/tbb_memory9.gold > tbb_memory9.diff
-if [ -e tbb_memory9.log ] && [ -e gold/tbb_memory9.gold ] && [ -e tbb_memory9.diff ] && ! [ -s tbb_memory9.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (9) test went OK"
-else
-  echo "TBB memory (9) test went wrong"
-fi
-
-diff tbb_memory10.log gold/tbb_memory10.gold > tbb_memory10.diff
-if [ -e tbb_memory10.log ] && [ -e gold/tbb_memory10.gold ] && [ -e tbb_memory10.diff ] && ! [ -s tbb_memory10.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (10) test went OK"
-else
-  echo "TBB memory (10) test went wrong"
-fi
-
-diff tbb_memory11.log gold/tbb_memory11.gold > tbb_memory11.diff
-if [ -e tbb_memory11.log ] && [ -e gold/tbb_memory11.gold ] && [ -e tbb_memory11.diff ] && ! [ -s tbb_memory11.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "TBB memory (11) test went OK"
-else
-  echo "TBB memory (11) test went wrong"
-fi
