c3bf603aac2ffbabe7a3e64271fdbd9c1f45a3f1
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-05-15 13:22:59 +0000

    bug 1315#: modified code

--- StationTest/test/xcstatistics/xcstatistics_lbl.sh
+++ StationTest/test/xcstatistics/xcstatistics_lbl.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-# 1.2 xcstatistics test to check SerDes Ring
-# 7-05-09, M.J Norden
-# LBL input with Antenna
+# 1.3 xcstatistics test to check SerDes Ring with LBL antennas
+# 15-05-09, M.J Norden
+# LBL input with antenna
+
 
 rspctl --rcuprsg=0
 rspctl --wg=0
 rspctl --rcumode=1
+sleep 2
+
+rspctl --splitter=1
 sleep 2
 
 echo "check xcstat and xcangle"
