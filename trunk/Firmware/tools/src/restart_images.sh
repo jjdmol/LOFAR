864bc65c61f804fb6429e3a36761cac5a550538e
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-06-03 07:25:57 +0000

    bug 1276: Added commandline option to choose pagenumber

--- trunk/Firmware/tools/src/restart_images.sh
+++ trunk/Firmware/tools/src/restart_images.sh
@@ -1,15 +1,69 @@
 # !/bin/bash
 # version 2.2, date 05-11-2008,  M.J.Norden
-eval "swlevel 1"
-page=1
-station=`hostname -s`
-let rspboards=`sed -n  's/^\s*RS\.N_RSPBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
+SyntaxError()
+{
+        Msg=$1
 
-echo "This station is "$station
+        [ -z "${Msg}" ] || echo "ERROR: ${Msg}"
+        echo ""
+        echo "Syntax: $(basename $0) -p [pagenr]"
+        echo "        Default pagenr is 1"
+
+        exit 1
+}
+
+page=-1
+if [ "$1" == "" ]; then 
+   page=1
+else
+  eval set argv=`getopt "p:h" $*`
+  shift
+  if [ "$1" == "--" ]; then SyntaxError; fi
+
+  while [ "$1" != "--" ]
+  do
+        case "$1" in
+                -p)     page=$2
+                        shift 2
+                        ;;
+
+                -h) SyntaxError
+                        ;;
+
+                *)  SyntaxError
+                        ;;
+        esac
+  done
+  shift
+fi
+
+if [ -e /opt/lofar/etc/RemoteStation.conf ]; then
+  let rspboards=`sed -n  's/^\s*RS\.N_RSPBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
+else
+  echo "Could not find /opt/lofar/etc/RemoteStation.conf"
+  let rspboards=12
+fi
+
+echo "You have selected page "$page
+echo "Nr. of RSPBoards is: "$rspboards
+read -p "Is this OK (y/n) [n]:" ok
+[ -z "$ok" ] && ok="n"
+case "$ok" in
+   [yY] )
+      ;;
+   [nN] ) exit 0
+      ;;
+   *    ) echo "Assuming this means no..."
+	  exit 0
+      ;;
+esac
+
+eval "swlevel 1"
 
 for ((ind=0; ind < $rspboards; ind++)) do
   MACadr=$(printf "10:FA:00:00:%02x:00" $ind)
   sudo rsuctl3_reset -q -x -p $page -m $MACadr;
 done
+
 eval "swlevel 2"
 
