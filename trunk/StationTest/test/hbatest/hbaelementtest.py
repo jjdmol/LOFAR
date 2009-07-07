25e1a70c0869525a9a0c07faf758e32a94640d13
gunst <gunst@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-07-07 19:52:26 +0000

    bug 1350: Two measurements are taken place now (one with control word 128 and one with control word 253)

--- trunk/StationTest/test/hbatest/hbaelementtest.py
+++ trunk/StationTest/test/hbatest/hbaelementtest.py
@@ -59,6 +59,34 @@ def read_frame(f):
 	sst_data = sst_data.tolist()
 	return sst_data
 
+def capture_data(dir_name,num_rcu,hba_elements,ctrl_word,sleeptime,subband_nr,element):
+	meet_data=range(0, num_rcu)
+        rm_files(dir_name,'*')
+        ctrl_string='='
+        for ind in range(hba_elements) :
+               if ind == element:
+			ctrl_string=ctrl_string + '128,'
+	       else:	
+			ctrl_string=ctrl_string + '2,'
+	strlength=len(ctrl_string)
+        ctrl_string=ctrl_string[0:strlength-1]
+	cmd_str='rspctl --hbadelay' + ctrl_string + ' 2>/dev/null'
+        os.popen(cmd_str)
+	time.sleep(sleeptime)
+        print 'Capture HBA element ' + str(element+1) + ' data'
+        rec_stat(dir_name,num_rcu)
+        # get list of all files in dir_name
+ 	files = open_dir(dir_name)
+        
+        # start processing the element measurements
+	for file_cnt in range(len(files)) :
+		f, frames_to_process, rcu_nr  = open_file(files, file_cnt)
+                if frames_to_process > 0 : 
+		   	sst_data = read_frame(f)
+                   	sst_subband = sst_data[subband_nr]
+		   	meet_data[rcu_nr] = sst_subband
+		f.close
+	return meet_data
 
 # Main loop
 def main() :
@@ -70,6 +98,7 @@ def main() :
   	rmfile = '*.log'
 	hba_elements=2
         factor=1000
+        sleeptime=1
  	ctrl_string='='
 	# read in arguments
         if len(sys.argv) < 2 :
@@ -83,12 +112,14 @@ def main() :
 		num_rcu = int(sys.argv[2])
         print ' Number of RCUs is ' + str(num_rcu)
         # init log file
-        f_log = file('HBA_elements.log', 'w')
-        f_log.write(' ************ \n \n LOG File for HBA element test \n \n *************** \n \n')
-        f_logfac = file('HBA_factors.log', 'w')
+        f_log1 = file('HBA_elements1.log', 'w')
+        f_log1.write(' ************ \n \n LOG File for HBA element test (ctrl word 128) \n \n *************** \n \n')
+        f_log1fac = file('HBA_factors1.log', 'w')
+        f_log2 = file('HBA_elements2.log', 'w')
+        f_log2.write(' ************ \n \n LOG File for HBA element test (ctrl word 253) \n \n *************** \n \n')
+        f_log2fac = file('HBA_factors2.log', 'w')
 	# initialize data arrays
 	ref_data=range(0, num_rcu)
-	meet_data=range(0, num_rcu)
 	os.chdir(dir_name)
 	#os.popen("rspctl --clock=200")
 	#print 'Clock is set to 200 MHz'
@@ -104,7 +135,7 @@ def main() :
         ctrl_string=ctrl_string[0:strlength-1]
 	cmd_str='rspctl --hbadelay' + ctrl_string + ' 2>/dev/null'
         os.popen(cmd_str)
-        time.sleep(10)
+        time.sleep(sleeptime)
         print 'Capture reference data'
         rec_stat(dir_name,num_rcu)
 	#rm_files(dir_name,rmfile)
@@ -124,45 +155,34 @@ def main() :
         # capture hba element data for all elements
 
         for element in range(hba_elements) :
-        	rm_files(dir_name,'*')
-        	ctrl_string='='
-        	for ind in range(hba_elements) :
-                        if ind == element:
-				ctrl_string=ctrl_string + '128,'
-			else:	
-				ctrl_string=ctrl_string + '2,'
-		strlength=len(ctrl_string)
-        	ctrl_string=ctrl_string[0:strlength-1]
-		cmd_str='rspctl --hbadelay' + ctrl_string + ' 2>/dev/null'
-        	os.popen(cmd_str)
-		time.sleep(10)
-        	print 'Capture HBA element ' + str(element+1) + ' data'
-        	rec_stat(dir_name,num_rcu)
-		#rm_files(dir_name,rmfile)
-        	# get list of all files in dir_name
- 		files = open_dir(dir_name)
-        
-        	# start processing the element measurements
-		for file_cnt in range(len(files)) :
-			f, frames_to_process, rcu_nr  = open_file(files, file_cnt)
-                	if frames_to_process > 0 : 
-		   		sst_data = read_frame(f)
-                   		sst_subband = sst_data[subband_nr]
-		   		meet_data[rcu_nr] = sst_subband
-		   		#if rcu_nr==0:
-		   		#	print ' waarde is ' + str(sst_subband)
-			f.close
+                #First capture when the control word is 128 of one of the elements
+                ctrl_word=128
+                meet_data1=capture_data(dir_name,num_rcu,hba_elements,ctrl_word,sleeptime,subband_nr,element)
+                ctrl_word=253
+                meet_data2=capture_data(dir_name,num_rcu,hba_elements,ctrl_word,sleeptime,subband_nr,element)
+
+                #Second capture when the control word is 253 of one of the elements
+
+
 		for rcuind in range(num_rcu) :
-			#print 'factor: ' + str(meet_data[rcuind]/ref_data[rcuind]) + ' RCU: ' + str(rcuind)
-                        f_logfac.write(str(element+1) + ' ' + str(rcuind) + ' ' + str(round(meet_data[rcuind]/ref_data[rcuind])) + '\n')  
-			if meet_data[rcuind] < factor*ref_data[rcuind] :        
+                        f_log1fac.write(str(element+1) + ' ' + str(rcuind) + ' ' + str(round(meet_data1[rcuind]/ref_data[rcuind])) + '\n')  
+			if meet_data1[rcuind] < factor*ref_data[rcuind] :        
 				if rcuind == 0 :
 					tilenumb=0
 				else:
 					tilenumb=int(rcuind/2)
-                                f_log.write('Element ' + str(element+1) + ', Tile ' + str(tilenumb) + ' in RCU: ' + str(rcuind)+ ' factor: ' + str(round(meet_data[rcuind]/ref_data[rcuind])) + '\n')  
+                                f_log1.write('Element ' + str(element+1) + ', Tile ' + str(tilenumb) + ' in RCU: ' + str(rcuind)+ ' factor: ' + str(round(meet_data1[rcuind]/ref_data[rcuind])) + '\n')  
+                        f_log2fac.write(str(element+1) + ' ' + str(rcuind) + ' ' + str(round(meet_data2[rcuind]/ref_data[rcuind])) + '\n')  
+			if meet_data2[rcuind] < factor*ref_data[rcuind] :        
+				if rcuind == 0 :
+					tilenumb=0
+				else:
+					tilenumb=int(rcuind/2)
+                                f_log2.write('Element ' + str(element+1) + ', Tile ' + str(tilenumb) + ' in RCU: ' + str(rcuind)+ ' factor: ' + str(round(meet_data2[rcuind]/ref_data[rcuind])) + '\n')  
 
-        f_log.close
-	f_logfac.close
+        f_log1.close
+	f_log1fac.close
+        f_log2.close
+	f_log2fac.close
 
 main()
