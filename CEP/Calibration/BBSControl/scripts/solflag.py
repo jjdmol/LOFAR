ed65ed09a7dc2ae176a51e661c9463b44f0e68e9
Joris van Zwieten <0xA000@users.noreply.github.com>
2009-08-06 09:43:54 +0000

    Bug 1283: update to support new pyparmdb interface.

--- CEP/Calibration/BBSControl/scripts/solflag.py
+++ CEP/Calibration/BBSControl/scripts/solflag.py
@@ -1,57 +1,44 @@
 import sys
 import numpy
 import pylab
-import lofar.parmdb
 import pyrap.tables
+import solfetch
 
-def fetch(db, elements, sources, stations):
-    parmRe = {}
-    parmIm = {}
-    
-    # Seem to be a limit to the number of elements in {} in pyparmdb??
-    for station in stations:
-        parmRegexRe = "Gain:{%s}:Real:%s:{%s}" % (",".join(elements), station, ",".join(sources))
-        parmRegexIm = "Gain:{%s}:Imag:%s:{%s}" % (",".join(elements), station, ",".join(sources))
+def fetch(db, elements, stations, directions=[None]):
+    result = None
 
-        tmp = db.getValuesGrid(parmRegexRe)
-        del tmp["_grid"]
-        parmRe.update(tmp)
-
-        tmp = db.getValuesGrid(parmRegexIm)
-        del tmp["_grid"]
-        parmIm.update(tmp)
-    
-    ampl = None
     for i in range(0, len(elements)):
-        for j in range(0, len(sources)):
-            for k in range(0, len(stations)):
-                name = "Gain:%s:Real:%s:%s" % (elements[i], stations[k], sources[j])
-                re = numpy.array(parmRe[name])
-                name = "Gain:%s:Imag:%s:%s" % (elements[i], stations[k], sources[j])
-                im = numpy.array(parmIm[name])
-       
-                tmp = numpy.sqrt(re * re + im * im)
-                tmp = tmp.transpose()
-               
-                if ampl is None:
-                    ampl = numpy.zeros((len(elements), len(sources), len(stations), len(tmp[0])))
+        for j in range(0, len(directions)):
+            # Fetch solutions.
+            (ampl, phase) = solfetch.fetch(db, stations, parm="Gain:%s" %
+                elements[i], direction=directions[j])
 
-                ampl[i,j,k] = tmp[0]
+            # Allocate result array if necessary.
+            if result is None:
+                result = numpy.zeros((len(elements), len(directions),
+                    len(stations), ampl.shape[-1]))
 
-    return ampl
-    
-def flag(msName, dbName, half_window, threshold, storeFlags=True, updateMain=False, cutoffLow=None, cutoffHigh=None, debug=False):
+            # Copy solutions into result array.
+            assert(result[i][j].shape == ampl.shape)
+            result[i][j] = ampl
+
+    return result
+
+def flag(msName, dbName, half_window, threshold, sources=None, storeFlags=True,
+    updateMain=True, cutoffLow=None, cutoffHigh=None, debug=False):
     """
     Solution based flagging.
-    
+
     msName:         name of the measurement to flag
     dbName:         name of solution parameter database
     half_window:    half the size of the window used in the flagging algorithm
     threshold:      threshold for the flagging algorithm (median of the absolute
                     distance to the median); typical values 2, 3, 4
+    sources:        (default None) for directional gains solutions, specify the
+                    source directions that should be considered
     storeFlags:     (default True) if set to False, the flags will not be
                     written to the measurement
-    updateMain:     (default False) if set to True, both the FLAG and the
+    updateMain:     (default True) if set to True, both the FLAG and the
                     FLAG_ROW column will be updated if set to False, only the
                     FLAG_ROW column will be updated
     cutoffLow:      (default None) if set, all values less than or equal to
@@ -59,7 +46,7 @@ def flag(msName, dbName, half_window, threshold, storeFlags=True, updateMain=Fal
     cutoffHigh:     (default None) if set, all values greater than or equal to
                     cutoffHigh will be flagged
     debug:          (default False) if set to True, a plot is generated for each
-                    station that show what has been flagged
+                    source - station combination that shows what has been flagged.
     """
 
     # Read station names from MS.
@@ -67,78 +54,115 @@ def flag(msName, dbName, half_window, threshold, storeFlags=True, updateMain=Fal
     stations = antennaTable.getcol("NAME")
     antennaTable.done()
     del antennaTable
-    
+
     # Open main MS table.
     ms = None
     if storeFlags:
         ms = pyrap.tables.table(msName, readonly=False)
-    
+
     # Open solution database.
     db = lofar.parmdb.parmdb(dbName)
-    
+
     # Get solutions from solution database.
     elements = ["11","22"]
-    sources = ["CasA","CygA"]
-    ampl = fetch(db, elements, sources, stations)
-    
+    if sources is None:
+        sources = [None]
+
+    print "fetching solutions from %s..." % dbName,
+    sys.stdout.flush()
+    ampl = fetch(db, elements, stations, sources)
+    print "done."
+    sys.stdout.flush()
+
     # Get the number of time samples.
-    n_samples = ampl.shape[3]
-    
+    n_samples = ampl.shape[-1]
+
     # Flag based on solutions.
+    print "flagging..."
+    sys.stdout.flush()
+
     for stat in range(0, len(stations)):
-        # Allocate flag array.
+        # Allocate flag array for this station.
         flags = numpy.zeros(n_samples, bool)
-    
+
         for src in range(0, len(sources)):
             for el in range(0, len(elements)):
-                data = ampl[el, src, stat, :]
-                assert(len(data) == n_samples)
-                
-                data_padded = numpy.zeros(n_samples + 2 * half_window)
-                data_padded[half_window:half_window + n_samples] = data
-                data_padded[0:half_window] = data[half_window:0:-1]
-                data_padded[len(data_padded) - half_window + 1:len(data_padded)] = data[n_samples:n_samples-half_window:-1]
+                # Create padded data array.
+                sol = numpy.zeros(n_samples + 2 * half_window)
+                sol[half_window:half_window + n_samples] = ampl[el][src][stat]
 
-                for i in range(0, n_samples):
+                for i in range(0, half_window):
+                    # Mirror at left edge.
+                    idx = min(n_samples - 1, half_window - i)
+                    sol[i] = ampl[el][src][stat][idx]
+
+                    # Mirror at right edge
+                    idx = max(0, n_samples - 2 - i)
+                    sol[n_samples + half_window + i] = ampl[el][src][stat][idx]
+
+                sol_flag = numpy.zeros(n_samples + 2 * half_window, dtype=bool)
+
+                # Thresholding.
+                if not cutoffLow is None:
+                    sol_flag[sol <= cutoffLow] = True
+
+                if not cutoffHigh is None:
+                    sol_flag[sol >= cutoffHigh] = True
+
+                for i in range(half_window, half_window + n_samples):
                     # Compute median of the absolute distance to the median.
-                    window = data_padded[i:i+2*half_window]
-                    median = numpy.median(window)
-                    q = 1.4826 * numpy.median(numpy.abs(window - median))
-            
+                    window = sol[i - half_window:i + half_window + 1]
+                    window_flag = sol_flag[i - half_window:i + half_window + 1]
+                    window_masked = window[~window_flag]
+
+                    if len(window_masked) < math.sqrt(len(window)):
+                        # Not enough data to get accurate statistics.
+                        continue
+
+                    median = numpy.median(window_masked)
+                    q = 1.4826 * numpy.median(numpy.abs(window_masked - median))
+
                     # Flag sample if it is more than 1.4826 * threshold * the
                     # median distance away from the median.
-                    if abs(data[i] - median) > (threshold * q):
-                        flags[i] = True
-                        data[i] = median
-
-                if not cutoffLow is None:
-                    flags[data <= cutoffLow] = True
-                    
-                if not cutoffHigh is None:
-                    flags[data >= cutoffHigh] = True
+                    if abs(sol[i] - median) > (threshold * q):
+                        sol_flag[i] = True
 
                 if debug:
-                    pylab.clf()
-                    pylab.plot(data_padded[half_window:len(data)+half_window])
-                    pylab.plot(data)
-                    pylab.show()
+                    # Get masked x-axis and solutions.
+                    mask = ~sol_flag[half_window:half_window + n_samples]
+                    x_axis = numpy.array(range(0, n_samples))
+                    x_axis = x_axis[mask]
+                    
+                    sol_masked = sol[half_window:half_window + n_samples]
+                    sol_masked = sol_masked[mask]
+                    
+                    fig_index = stat * len(sources) + src + 1
+                    pylab.figure(fig_index)
+                    if el == 0:
+                        pylab.clf()
 
-#                raw_input("Please press enter to continue...")
-        
+                    pylab.subplot("21%d" % (el + 1))
+                    pylab.plot(ampl[el][src][stat], 'k-')
+                    pylab.plot(x_axis, sol_masked, 'go', markersize=4)
+
+                # Merge flags based on the solutions for the current direction
+                # into the station flags.
+                flags = flags | sol_flag[half_window:half_window + n_samples]
+
         print "(%.2f%%) %s" % (100.0 * numpy.sum(flags) / n_samples, stations[stat])
         sys.stdout.flush()
 
-        if storeFlags:                
+        if storeFlags:
             stationTable = ms.query("ANTENNA1 == %d || ANTENNA2 == %d" % (stat, stat), sortlist="TIME,ANTENNA1,ANTENNA2")
             baselineIter = pyrap.tables.tableiter(stationTable, ["ANTENNA1", "ANTENNA2"])
             for baseline in baselineIter:
                 assert(baseline.nrows() == len(flags))
-                
+
                 # Update row flags
                 msRowFlags = baseline.getcol("FLAG_ROW")
                 msRowFlags |= flags
                 baseline.putcol("FLAG_ROW", msRowFlags)
-                
+
                 # Update main flags
                 if updateMain:
                     msFlags = baseline.getcol("FLAG")
@@ -146,3 +170,4 @@ def flag(msName, dbName, half_window, threshold, storeFlags=True, updateMain=Fal
                         msFlags[i, :, :] |= flags[i]
                     baseline.putcol("FLAG", msFlags)
 
+    print "done."
--- CEP/Calibration/BBSControl/scripts/solplot.py
+++ CEP/Calibration/BBSControl/scripts/solplot.py
@@ -1,57 +1,10 @@
 import math
 import numpy
 import pylab
-import lofar.parmdb
+from solfetch import fetch
 
 styles = ["%s%s" % (x, y) for y in ["-", ":"] for x in ["b", "g", "r", "c", "m",
     "y", "k"]]
-
-def fetch(db, stations, phasors=False, parm="Gain:11", direction="",
-    asPolar=True):
-
-    suffix = ""
-    if direction:
-        suffix = ":%s" % direction
-
-    el0_infix = "Real"
-    el1_infix = "Imag"
-
-    if phasors:
-        el0_infix = "Ampl"
-        el1_infix = "Phase"
-
-    el0 = []
-    el1 = []
-    for station in stations:
-        fqname = "%s:%s:%s%s" % (parm, el0_infix, station, suffix)
-        el0.append(numpy.squeeze(db.getValuesGrid(fqname)[fqname]))
-        fqname = "%s:%s:%s%s" % (parm, el1_infix, station, suffix)
-        el1.append(numpy.squeeze(db.getValuesGrid(fqname)[fqname]))
-
-    el0 = numpy.array(el0)
-    el1 = numpy.array(el1)
-
-    if phasors and not asPolar:
-        re = numpy.zeros(el0.shape)
-        im = numpy.zeros(el1.shape)
-
-        for i in range(0, len(stations)):
-            re[i] = el0[i] * numpy.cos(el1[i])
-            im[i] = el0[i] * numpy.sin(el1[i])
-
-        return (re, im)
-
-    if not phasors and asPolar:
-        ampl = numpy.zeros(el0.shape)
-        phase = numpy.zeros(el1.shape)
-
-        for i in range(0, len(stations)):
-            ampl[i] = numpy.sqrt(numpy.power(el0[i], 2) + numpy.power(el1[i], 2))
-            phase[i] = numpy.arctan2(el1[i], el0[i])
-
-        return (ampl, phase)
-
-    return (el0, el1)
 
 def unwrap(phase, tol=0.25, delta_tol=0.25):
     """
