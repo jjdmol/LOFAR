cb01ed35a7812aca4e666538d1373ff21d0d9b09
Marcel Loose <gmloose@users.noreply.github.com>
2009-08-13 13:48:33 +0000

    Bug 1310: Marked FFTW_* variables as advanced

--- CMake/FindFFTW3.cmake
+++ CMake/FindFFTW3.cmake
@@ -127,12 +127,14 @@ set(_check_list)
 foreach(_lib ${_libraries})
   string(TOUPPER ${_lib} _LIB)
   find_library(${_LIB}_LIBRARY ${_lib})
+  mark_as_advanced(${_LIB}_LIBRARY)
   list(APPEND FFTW3_LIBRARIES ${${_LIB}_LIBRARY})
   list(APPEND _check_list ${_LIB}_LIBRARY)
 endforeach(_lib ${_libraries})
 
 # Search for the header file.
 find_path(FFTW3_INCLUDE_DIR fftw3.h)
+mark_as_advanced(FFTW3_INCLUDE_DIR)
 list(APPEND _check_list FFTW3_INCLUDE_DIR)
 
 # Handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
