a0c9d553ec4b82df3bc311185b55479377ac853d
Jan David Mol <mol@astron.nl>
2009-09-02 14:46:54 +0000

    bug 1362: made station check resilient to no data

--- RTCP/Run/src/util/Commands.py
+++ RTCP/Run/src/util/Commands.py
@@ -4,6 +4,7 @@ import socket
 from subprocess import Popen,STDOUT,call,PIPE
 from Hosts import ropen
 from tee import Tee
+from Aborter import runFunc
 
 
 DRYRUN = False
@@ -11,11 +12,24 @@ DRYRUN = False
 # define our own PIPE as an alias to subprocess.PIPE
 PIPE=PIPE
 
-def backquote( cmdline ):
+def backquote( cmdline, timeout = 0 ):
   """ Run a command line and return the output. """
 
   debug("RUN backquote: %s" % (cmdline,) )
-  return Popen( cmdline.split(), stdout=PIPE, stderr=STDOUT ).communicate()[0]
+  class process:
+    def __init__(self):
+      self.output = ""
+
+    def run(self):
+      self.output = Popen( cmdline.split(), stdout=PIPE, stderr=STDOUT ).communicate()[0]
+
+  p = process()
+  if timeout:
+    runFunc( p.run, timeout )
+  else:
+    p.run()
+
+  return p.output
 
 def debug( str ):
   """ Override with custom logging function. """
