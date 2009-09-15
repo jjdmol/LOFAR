425381eb70747df908dc61b9f3a5846bdf8b34d6
schoenmakers <schoenmakers@astron.nl>
2009-09-15 15:28:54 +0000

    Bug 1407: Change database server

--- SHM/Components/LofarPython/lofar/shm/config.py
+++ SHM/Components/LofarPython/lofar/shm/config.py
@@ -6,8 +6,7 @@ def initialize():
     __config.clear()
     
     # set defaults
-    __config["database.host"] = "shm001.lofar.nl"
-    #__config["database.host"] = "10.87.2.54"
+    __config["database.host"] = "sas001.lofar"
     __config["database.port"] = 5432
     __config["job_control.uid"] = os.getuid()
     __config["job_control.gid"] = os.getgid()
@@ -17,11 +16,11 @@ def initialize():
     # parse config file
     parser = ConfigParser.ConfigParser()
     try:
-        # parser.read(glob.glob(os.path.join(os.sep, "/home/lofartest/SHM/Components/LofarPython", "etc", "lofar-shm", "*.conf")))
-        # parser.read(glob.glob(os.path.join(os.sep, os.getenv('HOME'),"SHM/Components/LofarPython", "etc", "lofar-shm", "*.conf")))
-        # parser.read(glob.glob(os.path.join(os.sep, "/home/avruch","SHM/Components/LofarPython", "etc", "lofar-shm", "*.conf")))
-        parser.read(glob.glob(os.path.join(os.sep, "/home/lofartest","SHM/Components/LofarPython", "etc", "lofar-shm", "*.conf")))
+#        parser.read(glob.glob(os.path.join(os.sep, "etc", "lofar-shm", "*.conf")))
+#        parser.read(glob.glob(os.path.join(os.sep, "/dop54_0/guest1", "etc", "lofar-shm", "*.conf")))
+        parser.read(glob.glob(os.path.join(os.sep, "/home/avruch/work/lofar/shm/versions/Components/LofarPython", "etc", "lofar-shm", "*.conf")))
     except (IOError, ConfigParser.ParsingError):
+        print 'MAX was here\n'
         return
     
     # insert all option, value pairs into the config dictionary
