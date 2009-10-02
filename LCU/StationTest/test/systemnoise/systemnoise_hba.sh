33bb066ac6d53dd5df83adab41f6584e10d2a8a7
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-10-02 11:51:52 +0000

    bug 1315: rcu variable

--- LCU/StationTest/test/systemnoise/systemnoise_hba.sh
+++ LCU/StationTest/test/systemnoise/systemnoise_hba.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
-# 1.1 system noise HBA test 
-# 29-06-09, M.J Norden
+# 1.2 system noise HBA test 
+# 02-10-09, M.J Norden
 # HBA input with antenna (one antenna biased and one not)
+
+let bias=26
+let nobias=28
 
 rspctl --rcuprsg=0
 rspctl --wg=0
-rspctl --rcumode=5 --sel=80
-rspctl --rcu=0x1007a080 --sel=82
-rspctl --specinv=1 --sel=80,82
+rspctl --rcumode=5 --sel=$bias
+rspctl --rcu=0x1007a080 --sel=$nobias
+rspctl --specinv=1 --sel=$bias,$nobias
 rspctl --rcuenable=1
 sleep 2
 
 echo ==========================
 echo "System noise HBA" `hostname -s`
 echo ==========================
-rspctl --stati --sel=80,82& 
+rspctl --stati --sel=$bias,$nobias& 
 sleep 20 && kill $!
 
 rspctl --specinv=0
