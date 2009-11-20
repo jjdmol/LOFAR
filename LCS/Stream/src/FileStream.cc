ca572f0bf66ffa57c0007440f926465f75904153
Jan David Mol <mol@astron.nl>
2009-11-20 08:04:28 +0000

    bug 1362: FileStream now reports which file could not be opened

--- LCS/Stream/src/FileStream.cc
+++ LCS/Stream/src/FileStream.cc
@@ -36,21 +36,21 @@ namespace LOFAR {
 FileStream::FileStream(const char *name)
 {
   if ((fd = open(name, O_RDONLY)) < 0)
-    throw SystemCallException("open", errno, THROW_ARGS);
+    throw SystemCallException(std::string("open ") + name, errno, THROW_ARGS);
 }
 
 
 FileStream::FileStream(const char *name, int mode)
 {
   if ((fd = open(name, O_RDWR | O_CREAT | O_TRUNC, mode)) < 0)
-    throw SystemCallException("open", errno, THROW_ARGS);
+    throw SystemCallException(std::string("open ") + name, errno, THROW_ARGS);
 }
 
 
 FileStream::FileStream(const char *name, int flags, int mode)
 {
   if ((fd = open(name, flags, mode)) < 0) 
-    throw SystemCallException("open", errno, THROW_ARGS);
+    throw SystemCallException(std::string("open ") + name, errno, THROW_ARGS);
 }
 
 FileStream::~FileStream()
