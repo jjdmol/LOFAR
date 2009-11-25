1169ba0827ba58fdc55bcef7b5eb3b2e44bd132d
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-11-25 17:47:26 +0000

    bug 1315: modified test scripts

--- LCU/StationTest/test/beamformertest/beamformertest_HBA_RCU192.sh
+++ LCU/StationTest/test/beamformertest/beamformertest_HBA_RCU192.sh
@@ -1,18 +1,25 @@
-# beamformer test for 192 rcu's in LBA mode (LBH input)
-# twentyfour antenna's per beam, four beams
+# beamformer test for 192 rcu's in HBA mode (with splitter OFF)
 # object zenith
-# version 1.3  23 maa 2009 M.J.Norden
+# version 1.0,  26 nov 2009, M.J.Norden
+
+
+rspctl --wg=0
+sleep 1
+rspctl --rcuprsg=0
+sleep 1
 
 killall beamctl
+sleep 3
+eval swlevel 3
+sleep 2
+rspctl --splitter=0
+sleep 3
 
-beamctl --array=HBA --rcumode=5 --rcus=0:47 --subbands=100:139 --beamlets=0:39 --direction=0,0,LOFAR_LMN&
-beamctl --array=HBA --rcumode=5 --rcus=48:95 --subbands=150:189 --beamlets=50:89 --direction=0,0,LOFAR_LMN&
-beamctl --array=HBA --rcumode=5 --rcus=96:143 --subbands=200:239 --beamlets=100:139 --direction=0,0,LOFAR_LMN&
-beamctl --array=HBA --rcumode=5 --rcus=144:191 --subbands=250:289 --beamlets=150:189 --direction=0,0,LOFAR_LMN&
-
-sleep 5
-rspctl --wg=0
-rspctl --rcuprsg=0
-rspctl --rcuenable=1
-rspctl --rcumode=5
+beamctl --array=HBA --rcumode=5 --rcus=0:1 --subbands=320:381 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=HBA --rcumode=5 --rcus=0:95 --subbands=320:381 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=HBA --rcumode=5 --rcus=96:97 --subbands=320:381 --beamlets=124:185 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=HBA --rcumode=5 --rcus=0:191 --subbands=320:381 --beamlets=186:247 --direction=0,0,LOFAR_LMN&
 
--- LCU/StationTest/test/beamformertest/beamformertest_LBA_RCU96.sh
+++ LCU/StationTest/test/beamformertest/beamformertest_LBA_RCU96.sh
@@ -1,19 +1,25 @@
-# beamformermformer test for 96 rcu's in LBA mode (LBL & LBH input)
-# two antennas per beam, four beams
+# beamformer test for 96 rcu's in LBA mode (with splitter OFF)
 # object zenith
-# version 1.3  23 maa 2009 M.J.Norden
+# version 1.0,  26 nov 2009, M.J.Norden
+
+
+rspctl --wg=0
+sleep 1
+rspctl --rcuprsg=0
+sleep 1
 
 killall beamctl
+sleep 3
+swlevel 3
+sleep 2
+rspctl --splitter=0
+sleep 3
 
-beamctl --array=DUMMY48 --rcumode=3 --rcus=0 --subbands=100:139 --beamlets=0:39 --direction=0,0,LOFAR_LMN&
-beamctl --array=DUMMY48 --rcumode=3 --rcus=0:47 --subbands=150:189 --beamlets=50:89 --direction=0,0,LOFAR_LMN&
-beamctl --array=DUMMY48 --rcumode=1 --rcus=48 --subbands=200:239 --beamlets=100:139 --direction=0,0,LOFAR_LMN&
-beamctl --array=DUMMY48 --rcumode=1 --rcus=48:95 --subbands=250:289 --beamlets=150:189 --direction=0,0,LOFAR_LMN&
-
-sleep 5
-rspctl --wg=0
-rspctl --rcuprsg=0
-rspctl --rcuenable=1
-rspctl --rcumode=3 --sel=0:95
-rspctl --rcumode=1 --sel=48:95
+beamctl --array=LBA --rcumode=3 --rcus=0:1 --subbands=320:381 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=LBA --rcumode=3 --rcus=0:47 --subbands=320:381 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=LBA --rcumode=3 --rcus=48:49 --subbands=320:381 --beamlets=124:185 --direction=0,0,LOFAR_LMN&
+sleep 3
+beamctl --array=LBA --rcumode=3 --rcus=0:95 --subbands=320:381 --beamlets=186:247 --direction=0,0,LOFAR_LMN&
 
