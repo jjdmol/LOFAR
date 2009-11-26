0d4e986c176e3b35e06df4cd0bb6cb2bc16146a6
romein <romein@b3281354-443f-0410-9bd6-bf5212d75c11>
2009-11-26 19:51:46 +0000

    bug 225: Fixed includes.

--- trunk/RTCP/Interface/src/Parset.cc
+++ trunk/RTCP/Interface/src/Parset.cc
@@ -42,7 +42,10 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/format.hpp>
 #include <boost/lexical_cast.hpp>
+
 #include <algorithm>
+#include <cstdio>
+
 
 namespace LOFAR {
 namespace RTCP {
