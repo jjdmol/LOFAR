7ea9dcae7b17c9d26f9bbbc3bb717240529cb923
romein <romein@astron.nl>
2009-12-07 10:52:20 +0000

    bug 225: Some bug fixes; (at least) one remains.

--- RTCP/Storage/test/AH_TestStorage.cc
+++ RTCP/Storage/test/AH_TestStorage.cc
@@ -28,119 +28,124 @@
 #include <Storage/SubbandWriter.h>
 #include <Interface/CorrelatedData.h>
 
-namespace LOFAR
+namespace LOFAR {
+namespace RTCP {
+
+AH_TestStorage::AH_TestStorage()
+:
+  itsPS("RTCP.parset")
 {
-  namespace RTCP
-  {
+}
 
-    AH_TestStorage::AH_TestStorage()
-    {
-    }
+AH_TestStorage::~AH_TestStorage()
+{
+  undefine();
+}
 
-    AH_TestStorage::~AH_TestStorage() {
-      undefine();
-    }
+void AH_TestStorage::define(const KeyValueMap&)
+{
 
-    void AH_TestStorage::define(const KeyValueMap&) {
-
-      LOG_TRACE_FLOW_STR("Start of tAH_Storage::define()");
-      
-      ParameterSet parameterSet("RTCP.parset");
-      Parset itsPS(&parameterSet);
-       
-      
-      int nrSubbands = itsPS.nrSubbands();
-      
-      
-      uint nrSubbandsPerPset = itsPS.nrSubbandsPerPset();
-      uint nrPsetsPerStorage = itsPS.getUint32("OLAP.psetsPerStorage");
-      uint nrWriters = nrSubbands / nrSubbandsPerPset / nrPsetsPerStorage;
-      ASSERT(nrSubbands % nrSubbandsPerPset == 0);
-      ASSERT(nrSubbands / nrSubbandsPerPset % nrPsetsPerStorage == 0);
-      LOG_TRACE_VAR_STR("Creating " << nrWriters << " subband writers ...");
-      
-      for (unsigned nw = 0; nw < nrWriters; ++nw)
-      {
-        vector<uint> sbIDs(nrSubbandsPerPset * nrPsetsPerStorage);
-	for (uint i = 0; i < nrSubbandsPerPset * nrPsetsPerStorage; ++i) {
-          sbIDs[i] = nrSubbandsPerPset * nrPsetsPerStorage * nw + i;     
-	  LOG_TRACE_LOOP_STR("Writer " << nw << ": sbIDs[" << i << "] = " 
-                             << sbIDs[i]);
-        }
-
-        SubbandWriter wh(&itsPS, 0);
-      }
-    
-      LOG_TRACE_FLOW_STR("Finished define()");
-
-    }
+  LOG_TRACE_FLOW_STR("Start of tAH_Storage::define()");
   
-    void AH_TestStorage::setTestPattern(DH_Visibilities &dh, int factor) {
-      unsigned nrPolarizations = itsPS.getUint32("Observation.nrPolarisations");
-      unsigned nrChannels      = itsPS.nrChannelsPerSubband();
-      unsigned nrStations      = itsPS.nrStations();
-      unsigned nrBaselines     = (nrStations + 1) * nrStations / 2;
+   
+  
+  int nrSubbands = itsPS.nrSubbands();
+  
+  
+  uint nrSubbandsPerPset = itsPS.nrSubbandsPerPset();
+  uint nrPsetsPerStorage = itsPS.getUint32("OLAP.psetsPerStorage");
+  uint nrWriters = nrSubbands / nrSubbandsPerPset / nrPsetsPerStorage;
+  ASSERT(nrSubbands % nrSubbandsPerPset == 0);
+  ASSERT(nrSubbands / nrSubbandsPerPset % nrPsetsPerStorage == 0);
+  LOG_TRACE_VAR_STR("Creating " << nrWriters << " subband writers ...");
+  
+  for (unsigned nw = 0; nw < nrWriters; ++nw)
+  {
+    vector<uint> sbIDs(nrSubbandsPerPset * nrPsetsPerStorage);
+    for (uint i = 0; i < nrSubbandsPerPset * nrPsetsPerStorage; ++i) {
+      sbIDs[i] = nrSubbandsPerPset * nrPsetsPerStorage * nw + i;     
+      LOG_TRACE_LOOP_STR("Writer " << nw << ": sbIDs[" << i << "] = " 
+			 << sbIDs[i]);
+    }
 
-      for (unsigned bl = 0; bl < nrBaselines; bl++) {
-        for (unsigned ch = 0; ch < nrChannels; ch++) {
-          // Set number of valid samples
-          //dh.getNrValidSamples(bl, ch) = bl * ch;
+    // SubbandWriter wh(&itsPS, 0); FIXME: needs extra parameter
+  }
 
-          // Set visibilities
-          for (unsigned pol1 = 0; pol1 < nrPolarizations; pol1 ++) {
-            for (unsigned pol2 = 0; pol2 < nrPolarizations; pol2 ++) {
-              //dh.getVisibility(bl, ch, pol1, pol2) = makefcomplex(bl + ch, factor * (pol1 + pol2));
-            }
-          }
-        }
+  LOG_TRACE_FLOW_STR("Finished define()");
+
+}
+
+void AH_TestStorage::setTestPattern(DH_Visibilities &dh, int factor)
+{
+  unsigned nrPolarizations = itsPS.getUint32("Observation.nrPolarisations");
+  unsigned nrChannels      = itsPS.nrChannelsPerSubband();
+  unsigned nrStations      = itsPS.nrStations();
+  unsigned nrBaselines     = (nrStations + 1) * nrStations / 2;
+
+  for (unsigned bl = 0; bl < nrBaselines; bl++) {
+    for (unsigned ch = 0; ch < nrChannels; ch++) {
+      // Set number of valid samples
+      //dh.getNrValidSamples(bl, ch) = bl * ch;
+
+      // Set visibilities
+      for (unsigned pol1 = 0; pol1 < nrPolarizations; pol1 ++) {
+	for (unsigned pol2 = 0; pol2 < nrPolarizations; pol2 ++) {
+	  //dh.getVisibility(bl, ch, pol1, pol2) = makefcomplex(bl + ch, factor * (pol1 + pol2));
+	}
       }
     }
+  }
+}
 
-    void AH_TestStorage::prerun() {
-      // Fill inDHs here
-      for (uint i = 0; i < itsInDHs.size(); i++)
-      {
-        //itsInDHs[i]->init();
-        setTestPattern(*itsInDHs[i], i);
-      }
+void AH_TestStorage::prerun()
+{
+  // Fill inDHs here
+  for (uint i = 0; i < itsInDHs.size(); i++)
+  {
+    //itsInDHs[i]->init();
+    setTestPattern(*itsInDHs[i], i);
+  }
+}
+
+void AH_TestStorage::run(int nsteps)
+{
+
+  for (int i = 0; i < nsteps; i++) {
+    for (uint nrInp = 0; nrInp < itsInConns.size(); nrInp++)
+    {
+      //itsInConns[nrInp]->write();
     }
 
-    void AH_TestStorage::run(int nsteps) {
+    LOG_TRACE_LOOP_STR("processing run " << i );
+    cout<<"run "<<i+1<<" of "<<nsteps<<endl;
+    //getComposite().process();
+  
+  }    
+}
 
-      for (int i = 0; i < nsteps; i++) {
-        for (uint nrInp = 0; nrInp < itsInConns.size(); nrInp++)
-        {
-          //itsInConns[nrInp]->write();
-        }
+void AH_TestStorage::postrun()
+{
+  // check outresult here
+  // do an assert or exit(1) if results are not correct
 
-        LOG_TRACE_LOOP_STR("processing run " << i );
-        cout<<"run "<<i+1<<" of "<<nsteps<<endl;
-        //getComposite().process();
-      
-      }    
-    }
+}
 
-    void AH_TestStorage::postrun() {
-      // check outresult here
-      // do an assert or exit(1) if results are not correct
+void AH_TestStorage::undefine()
+{
+  for (uint i = 0; i < itsInDHs.size(); i++)
+  {
+    //delete itsInDHs[i];
+    //delete itsInConns[i];
+  }
+  itsInDHs.clear();
+  itsInConns.clear();
+}  
 
-    }
+void AH_TestStorage::quit()
+{
+}
 
-    void AH_TestStorage::undefine() {
-      for (uint i = 0; i < itsInDHs.size(); i++)
-      {
-        //delete itsInDHs[i];
-        //delete itsInConns[i];
-      }
-      itsInDHs.clear();
-      itsInConns.clear();
-    }  
-
-    void AH_TestStorage::quit() {
-    }
-
-  } // namespace RTCP
-
+} // namespace RTCP
 } // namespace LOFAR
 
 
