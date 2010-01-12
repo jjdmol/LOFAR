105b52c3f5a7732dafbd18041d9c3d0ad577c2cb
Marcel Loose <gmloose@users.noreply.github.com>
2010-01-12 10:31:24 +0000

    Bug 1310: Moved the lofarinit script templates from LCS/Tools to the top-level LOFAR directory. They are now processed by LofarConfig.cmake.

--- CMake/LofarConfig.cmake
+++ CMake/LofarConfig.cmake
@@ -18,12 +18,26 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+# Only process this file when we're ate the top-level source directory.
+if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
 
-# Configure header containing global configurations for LOFAR builds. 
-# We should only do this when we're at the top-level source directory.
-if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
+  # Configure header containing global configurations for LOFAR builds. 
   message(STATUS "Generating lofar_config.h ...")
   configure_file(
-    ${LOFAR_ROOT}/lofar_config.h.cmake
+    ${CMAKE_SOURCE_DIR}/lofar_config.h.cmake
     ${CMAKE_BINARY_DIR}/include/lofar_config.h)
-endif(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
+
+  # Configure the lofarinit script files and mark them for install
+  message(STATUS "Generating lofarinit scripts ...")
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/lofarinit.sh.in
+    ${CMAKE_BINARY_DIR}/lofarinit.sh @ONLY)
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/lofarinit.csh.in
+    ${CMAKE_BINARY_DIR}/lofarinit.csh @ONLY)
+  install(FILES
+    ${CMAKE_BINARY_DIR}/lofarinit.sh
+    ${CMAKE_BINARY_DIR}/lofarinit.csh
+    DESTINATION .)
+
+endif("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
