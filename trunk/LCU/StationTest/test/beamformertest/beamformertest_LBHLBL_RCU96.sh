f994c23d708269e27870a77575d789961691e289
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-01-13 12:15:58 +0000

    bug 1315: modified beamformertest

--- trunk/LCU/StationTest/test/beamformertest/beamformertest_LBHLBL_RCU96.sh
+++ trunk/LCU/StationTest/test/beamformertest/beamformertest_LBHLBL_RCU96.sh
@@ -1,21 +1,21 @@
 # beamformermformer test for 96 rcu's in LBA mode (LBL & LBH input)
 # two antennas per beam, four beams
 # object zenith
-# version 1.6  1 jul 2009 M.J.Norden
+# version 1.7  13 jan 2010 M.J.Norden
 
 
 rspctl --wg=0
 rspctl --rcuprsg=0
 
 killall beamctl
-eval swlevel 3
+swlevel 3
 sleep 2
 
-beamctl --array=LBAOUTER --rcumode=1 --rcus=48:49 --subbands=270:331 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
+beamctl --array=LBA_OUTER --rcumode=1 --rcus=48:49 --subbands=270:331 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
 sleep 3
-beamctl --array=LBAINNER --rcumode=3 --rcus=0:1 --subbands=270:331 --beamlets=124:185 --direction=0,0,LOFAR_LMN&
+beamctl --array=LBA_INNER --rcumode=3 --rcus=0:1 --subbands=270:331 --beamlets=124:185 --direction=0,0,LOFAR_LMN&
 sleep 3
-beamctl --array=LBAOUTER --rcumode=1 --rcus=48:95 --subbands=270:331 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
+beamctl --array=LBA_OUTER --rcumode=1 --rcus=48:95 --subbands=270:331 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
 sleep 3
-beamctl --array=LBAINNER --rcumode=3 --rcus=0:47 --subbands=270:331 --beamlets=186:247 --direction=0,0,LOFAR_LMN&
+beamctl --array=LBA_INNER --rcumode=3 --rcus=0:47 --subbands=270:331 --beamlets=186:247 --direction=0,0,LOFAR_LMN&
 
