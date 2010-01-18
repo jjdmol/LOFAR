ba4e7dec9a718093304b9f2421ef36e0884c12b6
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-01-18 16:02:32 +0000

    Bug 1448: Second version, slightly better, but still needs a lot of polishing

--- branches/LOFAR-CMake-Bug1448-task-branch/CMake/LofarPackageVersion.cmake
+++ branches/LOFAR-CMake-Bug1448-task-branch/CMake/LofarPackageVersion.cmake
@@ -1,1 +1,25 @@
-include(MakePackageVersion)
+configure_file(
+  ${LOFAR_ROOT}/CMake/MakePackageVersion.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/MakePackageVersion.cmake @ONLY)
+
+#add_custom_target(${PACKAGE_NAME}_Version ALL
+#  COMMAND ${CMAKE_COMMAND} 
+#  ARGS -P ${CMAKE_CURRENT_BINARY_DIR}/MakePackageVersion.cmake)
+
+include(${CMAKE_CURRENT_BINARY_DIR}/MakePackageVersion.cmake)
+
+if(0)
+# Mark files Package__Version.cc and version<pkg>.cc as generated;
+# otherwise CMake will complain it cannot find the source files.
+string(TOLOWER ${PACKAGE_NAME} _lpkg)
+message(STATUS "_lpkg=${_lpkg}")
+set(_generated 
+  ${CMAKE_CURRENT_BINARY_DIR}/Package__Version.cc 
+  ${CMAKE_CURRENT_BINARY_DIR}/version${_lpkg}.cc)
+message(STATUS "set_source_files_properties(${_generated} PROPERTIES GENERATED ON)")
+#set_source_files_properties(${_generated} PROPERTIES GENERATED ON)
+set_source_files_properties(Package__Version.cc version${_lpkg}.cc
+  PROPERTIES GENERATED ON)
+endif(0)
+
+include_directories(${CMAKE_BINARY_DIR}/include/Package__Version)
