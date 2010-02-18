457ffd9f6509dc69425b514be171e76d872c6d5a
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-02-18 14:43:02 +0000

    bug 1315: update versions

--- trunk/LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-6.sh
+++ trunk/LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-6.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# 1.2 subband statistics HBA test 
-# 18-01-10, M.J Norden
+# 1.3 subband statistics HBA test 
+# 18-02-10, M.J Norden
 # HBA input with antennas
 
 
@@ -10,8 +10,10 @@ rspctl --clock=160 && sleep 60
 rspctl --rcuprsg=0
 rspctl --wg=0
 rspctl --splitter=0
+killall beamctl
 
 swlevel 3
+sleep 5
 beamctl --array=HBA --rcus=0:95 --rcumode=6 --subbands=100:110 --beamlets=0:10 --direction=0,0,LOFAR_LMN&
 sleep 2
 
@@ -20,6 +22,7 @@ echo "Subband Statistics HBA rcumode=6" `hostname -s`
 echo ==========================
 rspctl --stati& 
 sleep 20 && kill $!
+killall beamctl
 echo "Do not forget to switch the clock back to 200MHz"
 
 
