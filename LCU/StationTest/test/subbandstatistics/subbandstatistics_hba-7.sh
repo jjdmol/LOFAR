c647eacebf3b1a3904ef112f0e568c3e4828e200
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-02-18 14:43:02 +0000

    bug 1315: update versions

--- LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-7.sh
+++ LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-7.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-# 1.2 subband statistics HBA test 
-# 18-01-10, M.J Norden
+# 1.3 subband statistics HBA test 
+# 18-02-10, M.J Norden
 # HBA input with antennas
 
 rspctl --rcuprsg=0
 rspctl --wg=0
 rspctl --splitter=0
+killall beamctl
 
 swlevel 3
+sleep 5
 beamctl --array=HBA --rcus=0:95 --rcumode=7 --subbands=100:110 --beamlets=0:10 --direction=0,0,LOFAR_LMN&
 sleep 2
 
@@ -16,4 +18,4 @@ echo "Subband Statistics HBA rcumode=7" `hostname -s`
 echo ==========================
 rspctl --stati& 
 sleep 20 && kill $!
-
+killall beamctl
