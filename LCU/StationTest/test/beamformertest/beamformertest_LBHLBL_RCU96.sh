185234e559852ba4c69ff227783ff4cfaf334aa1
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-03-05 12:25:13 +0000

    bug 1315: sleep 5  (old sleep 2)

--- LCU/StationTest/test/beamformertest/beamformertest_LBHLBL_RCU96.sh
+++ LCU/StationTest/test/beamformertest/beamformertest_LBHLBL_RCU96.sh
@@ -1,7 +1,7 @@
 # beamformermformer test for 96 rcu's in LBA mode (LBL & LBH input)
 # two antennas per beam, four beams
 # object zenith
-# version 1.7  13 jan 2010 M.J.Norden
+# version 1.8  5 maa 2010 M.J.Norden
 
 
 rspctl --wg=0
@@ -9,7 +9,7 @@ rspctl --rcuprsg=0
 
 killall beamctl
 swlevel 3
-sleep 2
+sleep 5
 
 beamctl --array=LBA_OUTER --rcumode=1 --rcus=48:49 --subbands=270:331 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
 sleep 3
