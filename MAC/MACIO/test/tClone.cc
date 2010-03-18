2294ed4cc9531f979929773ee3a9b07e38488412
overeem <overeem@astron.nl>
2010-03-18 08:52:04 +0000

    Bug 1284: Fixed tests that were broken.

--- MAC/MACIO/test/tClone.cc
+++ MAC/MACIO/test/tClone.cc
@@ -26,12 +26,13 @@
 //# Includes
 #include <Common/LofarLogger.h>
 #include <Common/StringUtil.h>
+#include <Common/hexdump.h>
 #include "Clone_Protocol.ph"
 
 using namespace LOFAR;
 using namespace LOFAR::MACIO;
 
-int main (int	argc, char* argv[]) 
+int main (int	/*argc*/, char* argv[]) 
 {
 	INIT_VAR_LOGGER(argv[0], argv[0]);
 
@@ -40,7 +41,11 @@ int main (int	argc, char* argv[])
 	LOG_INFO_STR(theOrgGCF);
 	GCFEvent*	theClonedGCF = theOrgGCF.clone();
 	LOG_INFO_STR(*theClonedGCF);
-	ASSERTSTR(memcmp(&theOrgGCF, theClonedGCF, sizeof(GCFEvent)) == 0, "Clone of GCFEvent failed");
+	LOG_INFO_STR("sizeof(GCFEvent)=" << sizeof(GCFEvent));
+	ASSERTSTR(theOrgGCF.signal == theClonedGCF->signal, "signal differs: " << theOrgGCF.signal << " <> " << theClonedGCF->signal);
+	ASSERTSTR(theOrgGCF.length == theClonedGCF->length, "length differs: " << theOrgGCF.length << " <> " << theClonedGCF->length);
+	ASSERTSTR(theOrgGCF._buffer == theClonedGCF->_buffer, "_buffer differs: " << theOrgGCF._buffer << " <> " << theClonedGCF->_buffer);
+//	ASSERTSTR(memcmp(&theOrgGCF, theClonedGCF, sizeof(GCFEvent)) == 0, "Clone of GCFEvent failed");
 
 	LOG_INFO("Cloning a simple Test event");
 	CloneSimpleEvent		theOrgSimple;
@@ -48,7 +53,7 @@ int main (int	argc, char* argv[])
 	theOrgSimple.Int	= -45896;
 	theOrgSimple.Double	= 5447.67483;
 	theOrgSimple.Float	= 67483.5447;
-	theOrgSimple.Secret	= "Ssst, don't tell anyone!";
+	theOrgSimple.Secret	= "Ssst, don't tell anyone!";		// this is a 'non-printable' field in the definition
 	theOrgSimple.String	= "At least also some letters. All those digits!";
 	LOG_INFO_STR(theOrgSimple);
 
