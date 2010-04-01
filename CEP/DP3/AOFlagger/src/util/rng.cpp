8969abb15823ff9f94482579078f902bdb792cca
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-04-01 21:41:30 +0000

    Bug 1491: Fixing complex Gaussian noise.

--- CEP/DP3/AOFlagger/src/util/rng.cpp
+++ CEP/DP3/AOFlagger/src/util/rng.cpp
@@ -28,41 +28,41 @@ RNG::RNG()
 {
 }
 
-double RNG::Uniform() throw()
+double RNG::Uniform()
 {
 	return (long double) rand() / (long double) RAND_MAX;
 }
 
-double RNG::Guassian() throw()
+double RNG::Guassian()
 {
 	long double a, b;
 	DoubleGaussian(a, b);
 	return a;
 }
 
-double RNG::GuassianProduct() throw()
+double RNG::GuassianProduct()
 {
 	long double a, b;
 	DoubleGaussian(a, b);
 	return a * b;
 }
 
-double RNG::GuassianPartialProduct() throw()
+double RNG::GuassianPartialProduct()
 {
 	long double a, b;
 	DoubleGaussian(a, b);
 	if(a >= 0.0)
-		a = pow(a, sqrt(2.0)/2.0);
+		a = powl(a, sqrtl(2.0)/2.0);
 	else
-		a = -pow(-a, sqrt(2.0)/2.0);
+		a = -powl(-a, sqrtl(2.0)/2.0);
 	if(b >= 0.0)
-		b = pow(b, sqrt(2.0)/2.0);
+		b = powl(b, sqrtl(2.0)/2.0);
 	else
-		b = -pow(-b, sqrt(2.0)/2.0);
+		b = -powl(-b, sqrtl(2.0)/2.0);
 	return a*b;
 }
 
-void RNG::DoubleGaussian(long double &a, long double &b) throw()
+void RNG::DoubleGaussian(long double &a, long double &b)
 {
 	long double x1, x2, w;
 	
@@ -74,23 +74,23 @@ void RNG::DoubleGaussian(long double &a, long double &b) throw()
 		w = x1 * x1 + x2 * x2;
 	} while ( w >= 1.0 );
 
-	w = sqrt( (-2.0 * logl( w ) ) / w );
+	w = sqrtl( (-2.0 * logl( w ) ) / w );
 	a = x1 * w;
 	b = x2 * w;
 }
 
-double RNG::Rayleigh() throw()
+double RNG::Rayleigh()
 {
 	double x = Guassian(), y = Guassian();
-	return sqrt(x*x + y*y);
+	return sqrtl(x*x + y*y);
 }
 
-double RNG::EvaluateRayleigh(double x, double sigma) throw()
+double RNG::EvaluateRayleigh(double x, double sigma)
 {
-	return x * exp(-x*x/(2.0*sigma*sigma)) / (sigma * sigma);
+	return x * expl(-x*x/(2.0l*sigma*sigma)) / (sigma * sigma);
 }
 
-double RNG::IntegrateGaussian(long double upperLimit) throw()
+double RNG::IntegrateGaussian(long double upperLimit)
 {
 	long double integral = 0.0L, term = 0.0L, stepSize = 1e-4L;
 	upperLimit -= stepSize/2.0L;
@@ -99,15 +99,31 @@ double RNG::IntegrateGaussian(long double upperLimit) throw()
 		upperLimit -= stepSize;
 		integral += term * stepSize;
 	} while(term >= 1e-6 || upperLimit >= 0);
-	return integral / sqrt(2.0L * M_PI);
+	return integral / sqrtl(2.0L * M_PI);
 }
 
-long double RNG::EvaluateGaussian(long double x, long double sigma) throw()
+long double RNG::EvaluateGaussian(long double x, long double sigma)
 {
-	return 1.0L / (sigma * sqrt(2.0L*M_PI)) * exp(-0.5L*x*x/sigma);
+	return 1.0L / (sigma * sqrtl(2.0L*M_PI)) * expl(-0.5L*x*x/sigma);
 }
 
-double RNG::EvaluateGaussian2D(long double x1, long double x2, long double sigmaX1, long double sigmaX2) throw()
+double RNG::EvaluateGaussian2D(long double x1, long double x2, long double sigmaX1, long double sigmaX2)
 {
-	return 1.0L / (2.0L*M_PI*sigmaX1*sigmaX2) * exp(-0.5L*(x1*(1.0L/sigmaX1)*x1 + x2*(1.0L/sigmaX2)*x2));
+	return 1.0L / (2.0L*M_PI*sigmaX1*sigmaX2) * expl(-0.5L*(x1*(1.0L/sigmaX1)*x1 + x2*(1.0L/sigmaX2)*x2));
+}
+
+void RNG::ComplexGaussianAmplitude(long double &r, long double &i)
+{
+	long double amplitude = Guassian();
+	long double phase = Uniform() * 2.0l * M_PIl;
+	r = amplitude * cosl(phase);
+	i = amplitude * sinl(phase);
+}
+
+void RNG::ComplexGaussianAmplitude(num_t &r, num_t &i)
+{
+	num_t amplitude = Guassian();
+	num_t phase = Uniform() * 2.0 * M_PI;
+	r = amplitude * cos(phase);
+	i = amplitude * sin(phase);
 }
