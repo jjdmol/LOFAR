492313915360a7bf3993059108c6ddd5de2b3955
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-04-16 15:19:35 +0000

    Bug 1310: Improved support for building shared libraries from Matlab files, using the Matlab to C/C++ Compiler. Created separate FindMatlab.cmake that searches for MCC; it overrides the outdated FindMatlab.cmake that comes with CMake.

--- trunk/CMake/LofarMatlab.cmake
+++ trunk/CMake/LofarMatlab.cmake
@@ -3,6 +3,8 @@
 #  lofar_add_matlab_library(<name> <m-file> [<m-file> ...])
 # which compiles one or more M-files into a shared library lib<name>.so,
 # and a header file lib<name>.h that defines the interface to lib<name>.so.
+# Furthermore, the generated library is added to the list of libraries for 
+# the current package (global property ${PACKAGE_NAME}_LIBRARIES).
 
 # $Id$
 #
@@ -25,38 +27,28 @@
 # with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
 
 # Check that the Matlab to C/C++ Compiler is available.
-find_program(MATLAB_COMPILER mcc
-  HINTS ${MATLAB_ROOT_DIR}
-  DOC "Matlab to C/C++ Compiler")
-mark_as_advanced(MATLAB_COMPILER)
-
+include(LofarFindPackage)
+lofar_find_package(Matlab)
 
 macro(lofar_add_matlab_library _name)
 
-  # MCC must be present in order to proceed.
+  # MCC, the Matlab to C/C++ Compiler, must be present.
   if(NOT MATLAB_COMPILER)
     message(FATAL_ERROR "Could not find MCC - the Matlab to C/C++ Compiler")
   endif(NOT MATLAB_COMPILER)
 
   # The names of the generated files must all be prefixed with "lib"
   set(_libname "lib${_name}")
-  
-  # Add a custom target <name> to trigger the generation of the C++ files
-  add_custom_target(${_name} ALL
-    DEPENDS ${_libname}.h ${_libname}.cpp ${_libname}.so)
-
-  # Add a dependency of the current package on the target <name>
-  add_dependencies(${PACKAGE_NAME} ${_name})
 
   # Determine the destination directory for the generated C++ header file.
   # This directory is chosen such that other MAC packages only have to
   # add <binary-root>/include/MAC to their -I path.
   if(DEFINED ${PACKAGE_NAME}_INCLUDE_PATH_SUFFIX)
-    set(_dest_dir
+    set(_incdir
       "${CMAKE_BINARY_DIR}/include/MAC/${${PACKAGE_NAME}_INCLUDE_PATH_SUFFIX}")
   else()
     string(REGEX REPLACE "${LOFAR_ROOT}" "${CMAKE_BINARY_DIR}/include"
-      _dest_dir "${${PACKAGE_NAME}_SOURCE_DIR}")
+      _incdir "${${PACKAGE_NAME}_SOURCE_DIR}")
   endif(DEFINED ${PACKAGE_NAME}_INCLUDE_PATH_SUFFIX)
 
   # If we're doing a debug build, then also pass debug flags to MCC
@@ -64,15 +56,29 @@ macro(lofar_add_matlab_library _name)
     set(MATLAB_COMPILER_OPTIONS "-g")
   endif(LOFAR_BUILD_VARIANT MATCHES "^DEBUG$")
 
+  # List of generated files
+  set(_generated_files 
+    ${_libname}.h ${_libname}.cpp ${_libname}.so "${_incdir}/${_name}.h")
+
   # Define the command to generate a C++ header and source file, and a shared
   # library, from one or more Matlab M-files. The C++ header file will be
   # copied to the correct destination directory.
-  add_custom_command(OUTPUT ${_libname}.h ${_libname}.cpp ${_libname}.so
+  add_custom_command(OUTPUT ${_generated_files}
     COMMAND "${MATLAB_COMPILER}" ${MATLAB_COMPILER_OPTIONS}
       -I "${CMAKE_CURRENT_SOURCE_DIR}" -B cpplib:${_libname} ${ARGN}
     COMMAND "${CMAKE_COMMAND}"
-      -E copy "${_libname}.h" "${_dest_dir}/${_libname}.h"
+      -E copy "${_libname}.h" "${_incdir}/${_name}.h"
     DEPENDS ${ARGN}
     COMMENT "[MCC][${_name}] Building CXX shared library from ${ARGN}")
+
+  # Add a custom target <name> to trigger the generation of the C++ files
+  add_custom_target(${_name} ALL DEPENDS ${_generated_files})
+
+  # Add a dependency of the current package on the target <name>
+  add_dependencies(${PACKAGE_NAME} ${_name})
+
+  # Add the generated library to the list of libraries for the current package.
+  set_property(GLOBAL APPEND PROPERTY 
+    ${PACKAGE_NAME}_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/${_libname}.so)
 
 endmacro(lofar_add_matlab_library)
