87f673cfe503974991a319e1f93d5488611f6402
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-05-11 12:57:47 +0000

    bug 1362: repaired stopBGL.sh by providing the obsid to start/stopBGL.sh

--- trunk/MAC/APL/Appl_Controller/stopBGL.sh
+++ trunk/MAC/APL/Appl_Controller/stopBGL.sh
@@ -1,14 +1,21 @@
 # stopAP.sh partition jobName
 #
-# partition BG/L partition the job is running on
-# jobName   The name of the job
+# jobName       The name of the job
+# partition     BG/L partition the job is running on
+# observationID Observation number
 #
 
 #echo -n "Killing job " $2
 #killjob $1 `cat $2.jobID`
 #rm -f $2.pid $2.ps
 
-JOB=$2
-PARTITION=$1
+JOB=$1
+PARTITION=$2
+OBSID=$3
 
-/opt/lofar/bin/commandOLAP.py -P $PARTITION cancel $JOB
+# stopBGL.sh is used both to abort an observation and at the end of
+# an observation. Sleep a bit so that OLAP can temrinate on its own
+# in the latter case.
+sleep 10
+
+/opt/lofar/bin/commandOLAP.py -P $PARTITION cancel $OBSID
