3eb8caacd31d5a842dcc34bd07d51c9e04e711b4
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-05-11 12:34:29 +0000

    bug 1362: start/stopBGL now work for any specified partition

--- trunk/MAC/APL/Appl_Controller/stopBGL.sh
+++ trunk/MAC/APL/Appl_Controller/stopBGL.sh
@@ -7,4 +7,8 @@
 #echo -n "Killing job " $2
 #killjob $1 `cat $2.jobID`
 #rm -f $2.pid $2.ps
-/opt/lofar/bin/commandOLAP.py -P R00 cancel $2
+
+JOB=$2
+PARTITION=$1
+
+/opt/lofar/bin/commandOLAP.py -P $PARTITION cancel $JOB
