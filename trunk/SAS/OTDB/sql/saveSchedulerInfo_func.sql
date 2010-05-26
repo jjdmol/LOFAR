c7b09a1c3048ec1d435e5c96bb0242b049e1871e
jong <jong@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-05-26 14:45:11 +0000

    Bug1459: removed status in the scheduler branch because the SAS status now contains all statusses of the scheduler (i.e. ON_HOLD, PRESCHEDULED and ERROR status were added)

--- trunk/SAS/OTDB/sql/saveSchedulerInfo_func.sql
+++ trunk/SAS/OTDB/sql/saveSchedulerInfo_func.sql
@@ -37,7 +37,7 @@
 CREATE OR REPLACE FUNCTION saveSchedulerInfo(INT4, INT4, 
 					TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT,
 		TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, 
-		TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT)
+		TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT)
   RETURNS BOOLEAN AS $$ 
 	DECLARE
 		TThierarchy CONSTANT	INT2 := 30;
@@ -128,8 +128,6 @@ CREATE OR REPLACE FUNCTION saveSchedulerInfo(INT4, INT4,
 		UPDATE VIChierarchy SET value=$26 WHERE treeID=$2 AND nodeID=vRecord.nodeID;
 		FETCH fieldList INTO vRecord;
 		UPDATE VIChierarchy SET value=$27 WHERE treeID=$2 AND nodeID=vRecord.nodeID;
-		FETCH fieldList INTO vRecord;
-		UPDATE VIChierarchy SET value=$28 WHERE treeID=$2 AND nodeID=vRecord.nodeID;
 	    RETURN TRUE;
 	END;
 $$ LANGUAGE plpgsql;
