966354e556b20683e5caa3a4fcc3f5567232e41e
Marcel Loose <gmloose@users.noreply.github.com>
2010-05-26 14:28:05 +0000

    Bug 1310: Added support for OpenMP. By default, USE_OPENMP is set to OFF

--- CMake/LofarOptions.cmake
+++ CMake/LofarOptions.cmake
@@ -80,6 +80,12 @@ if(NOT DEFINED LOFAR_OPTIONS_INCLUDED)
     lofar_find_package(MPI REQUIRED)
   endif(USE_MPI)
   
+  if(USE_OPENMP)
+    lofar_find_package(OpenMP REQUIRED)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+  endif(USE_OPENMP)
+
   if(USE_SHMEM)
     set(HAVE_SHMEM 1)
   endif(USE_SHMEM)
