1689d10ee894c2259fec836e4e5d2971ab9f3aeb
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-06-15 21:05:08 +0000

    Bug 1491: retry getting it compiled without lofarstman

--- CEP/DP3/AOFlagger/scripts/package-without-lofarstman.sh
+++ CEP/DP3/AOFlagger/scripts/package-without-lofarstman.sh
@@ -5,8 +5,10 @@ else
     cd /tmp
     rm -rf aoexport
     mkdir -p aoexport/LOFAR/CEP/DP3
+    mkdir -p aoexport/LOFAR/LCS/Common
     cd aoexport
     svn export https://svn.astron.nl/LOFAR/trunk/CEP/DP3/AOFlagger/ LOFAR/CEP/DP3/AOFlagger
+    svn export https://svn.astron.nl/LOFAR/trunk/LCS/Common/include LOFAR/LCS/Common/include
     svn export https://svn.astron.nl/LOFAR/trunk/CMake LOFAR/CMake
     svn export https://svn.astron.nl/LOFAR/trunk/lofar_config.h.cmake LOFAR/lofar_config.h.cmake
     svn export https://svn.astron.nl/LOFAR/trunk/lofarinit.sh.in LOFAR/lofarinit.sh.in
@@ -15,6 +17,8 @@ else
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-CEP.txt LOFAR/CEP/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-DP3.txt LOFAR/CEP/DP3/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-AOFlagger-without-lofarstman.txt LOFAR/CEP/DP3/AOFlagger/CMakeLists.txt
+    cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-LCS.txt LOFAR/LCS/CMakeLists.txt
+    cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-Common-without-lofarstman.txt LOFAR/LCS/Common/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/README LOFAR/README
     CONF_FILE="LOFAR/CEP/DP3/AOFlagger/include/AOFlagger/configuration.h"
     echo \#ifndef CONFIGURATION_H > ${CONF_FILE}
