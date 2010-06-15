edc0d227f3f059fa4580f4930ff5d6b402fa304a
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-06-15 21:45:16 +0000

    Bug 1491: tweaking configuration

--- CEP/DP3/AOFlagger/scripts/package-without-lofarstman.sh
+++ CEP/DP3/AOFlagger/scripts/package-without-lofarstman.sh
@@ -9,6 +9,7 @@ else
     cd aoexport
     svn export https://svn.astron.nl/LOFAR/trunk/CEP/DP3/AOFlagger/ LOFAR/CEP/DP3/AOFlagger
     svn export https://svn.astron.nl/LOFAR/trunk/LCS/Common/include LOFAR/LCS/Common/include
+    svn export https://svn.astron.nl/LOFAR/trunk/LCS/Common/src LOFAR/LCS/Common/src
     svn export https://svn.astron.nl/LOFAR/trunk/CMake LOFAR/CMake
     svn export https://svn.astron.nl/LOFAR/trunk/lofar_config.h.cmake LOFAR/lofar_config.h.cmake
     svn export https://svn.astron.nl/LOFAR/trunk/lofarinit.sh.in LOFAR/lofarinit.sh.in
@@ -19,6 +20,7 @@ else
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-AOFlagger-without-lofarstman.txt LOFAR/CEP/DP3/AOFlagger/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-LCS.txt LOFAR/LCS/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-Common-without-lofarstman.txt LOFAR/LCS/Common/CMakeLists.txt
+    cp LOFAR/CEP/DP3/AOFlagger/scripts/CMakeLists-Common-src-without-lofarstman.txt LOFAR/LCS/Common/src/CMakeLists.txt
     cp LOFAR/CEP/DP3/AOFlagger/README LOFAR/README
     CONF_FILE="LOFAR/CEP/DP3/AOFlagger/include/AOFlagger/configuration.h"
     echo \#ifndef CONFIGURATION_H > ${CONF_FILE}
