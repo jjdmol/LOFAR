3e46219b6e86fd2d387f876aa99defdf1bdc176f
Jan David Mol <mol@astron.nl>
2010-08-20 11:07:45 +0000

    bug 1362: changed logging to /opt/lofar/log

--- MAC/APL/Appl_Controller/startBGL.sh
+++ MAC/APL/Appl_Controller/startBGL.sh
@@ -20,7 +20,7 @@ PARTITION=$2
 sed -i 's/.*OLAP.CNProc.integrationSteps.*//' $PARSET
 sed -i 's/.*OLAP.IONProc.integrationSteps.*//' $PARSET
 
-/opt/lofar/bin/runParset.py -P $PARTITION parset=$PARSET >>/globalhome/lofarsystem/log/run.runParset.py.log 2>&1 &
+/opt/lofar/bin/runParset.py -P $PARTITION parset=$PARSET >>/opt/lofar/log/run.runParset.py.log 2>&1 &
 
 #echo "executing /usr/local/bin/submitjob $2 $3 $4 virtual_node_mode BGLMPI_SIZE=$6 ACC $5" > startBGL.output
 #/usr/local/bin/submitjob $2 $3 $4 virtual_node_mode BGLMPI_SIZE=$6 ACC $5 2>&1 | awk '{split($1, a, "="); print a[2];}' > $1.jobID
