4cee498095ed7c8d362254e20c73b1811920cd50
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-08-20 14:22:04 +0000

    Bug 1569: Added getBasename() and getDirname(). Rewrote test program.

--- trunk/LCS/Common/test/tSystemUtil.cc
+++ trunk/LCS/Common/test/tSystemUtil.cc
@@ -23,22 +23,68 @@
 //# Always #include <lofar_config.h> first!
 #include <lofar_config.h>
 
+#include <Common/LofarLogger.h>
 #include <Common/StringUtil.h>
 #include <Common/SystemUtil.h>
 #include <Common/hexdump.h>
+#include <Common/lofar_iostream.h>
+#include <Common/lofar_iomanip.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 using namespace LOFAR;
-using namespace std;
+
+void testHostname()
+{
+  string hostname;
+  hostname = myHostname(false);
+  ASSERT(!hostname.empty());
+  cout << "My short hostname is: " << hostname << endl;
+  hostname = myHostname(true);
+  ASSERT(!hostname.empty());
+  cout << "My long hostname is : " << hostname << endl;
+}
+
+void testIP()
+{
+  uint32 address = myIPV4Address();
+  ASSERT(address != 0);
+  cout << formatString("My IPV4 address is  : %08lX\n", ntohl(address));
+  hexdump ((char*) &address, sizeof(uint32));
+}
+
+void testGetExePath()
+{
+  string exePath = getExecutablePath();
+  ASSERT(!exePath.empty());
+  cout << "My executable path  : " << getExecutablePath() << endl;
+}
+
+void testDirnameBasename()
+{
+  const char* paths[] = { 
+    "/usr/lib", "/usr/", "usr", "/", "///", 
+    "//usr//lib", ".", "..", "../a", "../a/.b"
+  };
+  cout << "Test of getDirname() and getBasename() :" << endl;
+  for(int i = 0; i < 10; i++) {
+    cout << setw(11) << left << paths[i] << "--> ('" 
+         << getDirname(paths[i]) << "', '" 
+         << getBasename(paths[i]) << "')" << endl;
+  }
+}
 
 int main()
 {
-	cout << "My short hostname is: " << myHostname(false) << endl;
-	cout << "My long hostname is : " << myHostname(true) << endl;
-	uint32	address = myIPV4Address();
-	cout << formatString("My IPV4 address is  : %08lX\n", ntohl(address));
-	hexdump ((char*) &address, sizeof(uint32));
-	cout << "My executable path  : " << getExecutablePath() << endl;
-	return (0);
+  INIT_LOGGER("tSystemUtil");
+  try {
+    testHostname();
+    testIP();
+    testGetExePath();
+    testDirnameBasename();
+  } catch(Exception& e) {
+    cerr << e << endl;
+    return 1;
+  }
+  return (0);
 }
