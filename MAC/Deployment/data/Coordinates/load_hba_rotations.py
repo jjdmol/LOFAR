a830e925b4addfb334788ac6e5233e70b6871ccc
schoenmakers <schoenmakers@astron.nl>
2010-09-01 14:58:43 +0000

    Bug 1319: Added data directory name

--- MAC/Deployment/data/Coordinates/load_hba_rotations.py
+++ MAC/Deployment/data/Coordinates/load_hba_rotations.py
@@ -21,7 +21,7 @@ def getRotationLines(filename):
     """
     Returns a list containing all lines with rotations
     """
-    f = open(filename,'r')
+    f = open('data/'+filename,'r')
     lines = f.readlines()
     f.close()
     return [ line.strip().split(',') for line in lines[1:]]
