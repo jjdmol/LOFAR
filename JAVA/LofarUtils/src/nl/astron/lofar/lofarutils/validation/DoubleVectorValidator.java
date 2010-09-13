b5195785d181bace57e63c0af6eaf2adfaff1ced
coolen <coolen@astron.nl>
2010-09-13 12:00:04 +0000

    Bug756:regex validators + validation subclasses for input validation of SAS params

---
+++ JAVA/LofarUtils/src/nl/astron/lofar/lofarutils/validation/DoubleVectorValidator.java
@@ -0,0 +1,34 @@
+package nl.astron.lofar.lofarutils.validation;
+
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JTextField;
+
+/**
+ * A class for performing pre-validation on Integers
+ *
+ * @author Arthur Coolen
+ */
+
+public class DoubleVectorValidator extends AbstractValidator {
+
+       public DoubleVectorValidator(JDialog parent, JTextField c) {
+        super(parent, c, "");
+    }
+
+    protected boolean validationCriteria(JComponent c) {
+        String input = ((JTextField)c).getText();
+
+
+        String msg = Validators.validateDoubleVector(input);
+        if (msg.isEmpty() ) {
+            return true;
+        } else {
+            setMessage(msg);
+            return false;
+        }
+
+    }
+
+}
+
