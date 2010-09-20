46d787a553a1738fcbf049b14f2513cb0430e5f9
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-09-20 09:41:15 +0000

    bug 1362: rotate logs after 1GB as well as on midnight

--- trunk/RTCP/Run/src/LOFAR/Logger.py
+++ trunk/RTCP/Run/src/LOFAR/Logger.py
@@ -1,9 +1,11 @@
 import sys
+import os
 from math import modf
-from time import time,strftime
+from time import time,strftime,localtime
 import logging
-import logging.handlers
+from logging.handlers import TimedRotatingFileHandler
 from traceback import format_exception
+from itertools import count
 
 DEBUG=False
 
@@ -42,10 +44,64 @@ def initLogger():
 
   sys.excepthook = my_excepthook  
 
+class TimedSizeRotatingFileHandler(TimedRotatingFileHandler):
+  """
+    Rolls over both at midnight and after the log has reached a certain size.
+  """
+  def __init__(self, filename):
+    TimedRotatingFileHandler.__init__( self, filename, when = "midnight", interval = 1, backupCount = 0 )
+
+    self.maxBytes = 1024*1024*1024
+    self.fileCount = 1
+
+  def shouldRollover(self, record):
+    if TimedRotatingFileHandler.shouldRollover(self, record):
+      return 1
+
+    msg = "%s\n" % self.format(record)
+
+    if self.stream.tell() + len(msg) >= self.maxBytes:
+      return 1
+
+    return 0  
+
+  def doRollover(self):
+    def rename(f,t):
+      if not os.path.exists(f):
+        return
+
+      if os.path.exists(t):
+        os.remove(t)
+      os.rename(f,t)  
+
+    t = self.rolloverAt - self.interval
+    timeTuple = localtime(t)
+
+    base = self.baseFilename + "." + strftime(self.suffix, timeTuple)
+
+    if os.path.exists( base ):
+      # increment the suffix number of older logfiles, since TimedRotatingFileHandler
+      # will overwrite "base"
+      for b in xrange( self.fileCount, -1, -1 ):
+        if b == 0:
+          sfn = base
+        else:  
+          sfn = base + "." + str(b)
+
+        dfn = base + "." + str(b+1)
+
+        rename( sfn, dfn )
+
+      self.fileCount += 1  
+    else:
+      self.fileCount = 1
+
+    TimedRotatingFileHandler.doRollover(self)
+
 def rotatingLogger( appname, filename ):
   logger = logging.getLogger( appname )
 
-  handler = logging.handlers.TimedRotatingFileHandler( filename, when = "midnight", interval = 1, backupCount = 0 )
+  handler = TimedSizeRotatingFileHandler( filename )
 
   logger.propagate = False
   logger.addHandler( handler )
