d0abe2c5775e3fdbe31f4966f2b796b9d5a6fa75
diepen <diepen@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-09-29 13:55:25 +0000

    bug 1436: Created a W-projection gridder that applies the corrections

--- trunk/CEP/Imager/LofarGridder/include/LofarGridder/LofarGridder.h
+++ trunk/CEP/Imager/LofarGridder/include/LofarGridder/LofarGridder.h
@@ -1,4 +1,4 @@
-//# LofarGridder.h: Test visibility gridder.
+//# LofarGridder.h: Gridder for LOFAR data correcting for DD effects
 //#
 //# Copyright (C) 2009
 //# ASTRON (Netherlands Institute for Radio Astronomy)
@@ -22,23 +22,26 @@
 //#
 //# @author Ger van Diepen <diepen at astron dot nl>
 
-#ifndef LOFAR_LOFARGRIDDER_TESTGRIDDER_H
-#define LOFAR_LOFARGRIDDER_TESTGRIDDER_H
+#ifndef LOFAR_LOFARGRIDDER_LOFARGRIDDER_H
+#define LOFAR_LOFARGRIDDER_LOFARGRIDDER_H
 
 #include <gridding/TableVisGridder.h>
 #include <Common/ParameterSet.h>
+#include <Common/lofar_vector.h>
+#include <Common/lofar_string.h>
 
 namespace LOFAR
 {
-  // @brief Gridder to test dynamic loading of gridders
+  // @brief Gridder for LOFAR data correcting for DD effects
   //
   // @ingroup testgridder
-  class LofarGridder : public askap::synthesis::TableVisGridder
+
+  class LofarGridder : public askap::synthesis::IVisGridder
   {
   public:
 
-    // Standard two dimensional box gridding
-    LofarGridder();
+    // Construct from the given parset.
+    explicit LofarGridder (const ParameterSet&);
 
     // Clone this Gridder
     virtual askap::synthesis::IVisGridder::ShPtr clone();
@@ -55,19 +58,89 @@ namespace LOFAR
     // @brief Register the gridder create function with its name.
     static void registerGridder();
 
+    // @brief Initialise the gridding
+    // @param axes axes specifications
+    // @param shape Shape of output image: u,v,pol,chan
+    // @param dopsf Make the psf?
+    virtual void initialiseGrid (const askap::scimath::Axes& axes,
+                                 const casa::IPosition& shape,
+                                 const bool dopsf=true);
+
+    // @brief Grid the visibility data.
+    // @param acc const data accessor to work with
+    // @note a non-const adapter is created behind the scene. If no on-the-fly
+    // visibility correction is performed, this adapter is equivalent to the
+    // original const data accessor.
+    virtual void grid (askap::synthesis::IConstDataAccessor& acc);
+      
+    // Form the final output image
+    // @param out Output double precision image or PSF
+    virtual void finaliseGrid (casa::Array<double>& out);
+
+    // @brief Calculate weights image
+    // @details Form the sum of the convolution function squared, 
+    // multiplied by the weights for each different convolution 
+    // function. This is used in the evaluation of the position
+    // dependent sensitivity
+    // @param out Output double precision sum of weights images
+    virtual void finaliseWeights (casa::Array<double>& out);
+
+    // @brief Initialise the degridding
+    // @param axes axes specifications
+    // @param image Input image: cube: u,v,pol,chan
+    virtual void initialiseDegrid (const askap::scimath::Axes& axes,
+                                   const casa::Array<double>& image);
+
+    // @brief Make context-dependant changes to the gridder behaviour
+    // @param context context
+    virtual void customiseForContext (casa::String context);
+      
+    // @brief assign weights
+    // @param viswt shared pointer to visibility weights
+    virtual void initVisWeights (askap::synthesis::IVisWeights::ShPtr viswt);
+      
+    // @brief Degrid the visibility data.
+    // @param[in] acc non-const data accessor to work with  
+    virtual void degrid (askap::synthesis::IDataAccessor& acc);
+
+    // @brief Finalise degridding.
+    virtual void finaliseDegrid();
+
     // @brief Initialise the indices
     // @param[in] acc const data accessor to work with
-    virtual void initIndices(const askap::synthesis::IConstDataAccessor& acc);
+    ///    virtual void initIndices (const askap::synthesis::IConstDataAccessor& acc);
 
     // @brief Correct for gridding convolution function
     // @param image image to be corrected
-    virtual void correctConvolution(casa::Array<double>& image);
+    ///    virtual void correctConvolution(casa::Array<double>& image);
 				
   protected:
     // Initialize convolution function
     // @param[in] acc const data accessor to work with
-    virtual void initConvolutionFunction
-    (const askap::synthesis::IConstDataAccessor& acc);
+    ///    virtual void initConvolutionFunction
+    ///    (const askap::synthesis::IConstDataAccessor& acc);
+
+  private:
+    // Initialize the corrections.
+    void initCorrections (const askap::synthesis::IConstDataAccessor& acc);
+
+
+    //# Data members.
+    // Is the gridder correction initialized?
+    bool itsInitialized;
+    // Nr of frequency channels to average.
+    uint itsFreqAvg;
+    // Nr of time slots to average.
+    uint itsTimeAvg;
+    // The effects to correct for.
+    vector<string> itsCorrect;
+    // The ParmDB to use.
+    string itsParmDBName;
+    // The gridder to use after correcting and averaging.
+    //# Unfortunatelty we need to use TableVisGridder because the
+    //# function getImageCntre is available only there.
+    askap::synthesis::IVisGridder::ShPtr itsIGridder;
+    askap::synthesis::TableVisGridder* itsGridder;
   };
 
 } //# end namespace
