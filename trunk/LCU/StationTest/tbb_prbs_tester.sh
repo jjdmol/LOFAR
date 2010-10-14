4f6b0203e45c8152a5f203425c7f6de4df079102
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-10-14 10:26:41 +0000

    bug 1315: new  versions for NL/INT stations

--- trunk/LCU/StationTest/tbb_prbs_tester.sh
+++ trunk/LCU/StationTest/tbb_prbs_tester.sh
@@ -3,12 +3,15 @@
 #
 # Test the LVDS interfaces between RCU -> RSP -> TBB using the Pseudo Random generator in the RCUs.
 #
+# Modified for INT station 14-10-2010 M.J.Norden
+
 
 let rspboards=`sed -n  's/^\s*RS\.N_RSPBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
+let tbboards=`sed -n  's/^\s*RS\.N_TBBOARDS\s*=\s*\([0-9][0-9]*\).*$/\1/p' /opt/lofar/etc/RemoteStation.conf`
 let nof_rcu=8*$rspboards
 
-rm -f *.log
-rm -f *.diff
+rm -f prbs_dir*.log
+rm -f prbs_dir*.diff
 rm -f ./prbs/*.*
 rm -r ./prbs/.svn
 
@@ -41,11 +44,24 @@ cd ..
 python prbs_dir_test.py
 rspctl --rcuprsg=0
 
-echo ""
-diff prbs_dir_test.log ./gold/prbs_dir_test.gold > prbs_dir_test.diff
-if [ -e prbs_dir_test.log ] && [ -e ./gold/prbs_dir_test.gold ] && [ -e prbs_dir_test.diff ] && ! [ -s prbs_dir_test.diff ]; then
-  # The files exists AND has the diff size 0
-  echo "RCU -> RSP -> TBB interfaces test went OK"
+if [ $tbboards == 6 ]; then
+     # This is a NL station
+     diff prbs_dir_test.log ./gold/prbs_dir_test.gold > prbs_dir_test.diff
+     if [ -e prbs_dir_test.log ] && [ -e ./gold/prbs_dir_test.gold ] && [ -e prbs_dir_test.diff ] && ! [ -s prbs_dir_test.diff ]; then
+     # The files exists AND has the diff size 0
+         echo "RCU -> RSP -> TBB interfaces test went OK"
+     else
+         echo "RCU -> RSP -> TBB interfaces test went wrong"
+     fi
 else
-  echo "RCU -> RSP -> TBB interfaces test went wrong"
+     # This is a INT station
+     diff prbs_dir_test.log ./gold/prbs_dir_test_int.gold > prbs_dir_test_int.diff
+     if [ -e prbs_dir_test.log ] && [ -e ./gold/prbs_dir_test_int.gold ] && [ -e prbs_dir_test_int.diff ] && ! [ -s prbs_dir_test_int.diff ]; then
+     # The files exists AND has the diff size 0
+         echo "RCU -> RSP -> TBB interfaces test went OK"
+     else
+         echo "RCU -> RSP -> TBB interfaces test went wrong"
+     fi
 fi
+
+
