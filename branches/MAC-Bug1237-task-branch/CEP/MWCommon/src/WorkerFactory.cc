4629033d4c6f897560fd116b216a951c3148f710
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-10-18 15:27:40 +0000

    Bug 1237: creating task branch for station calibration functionality in Beamserver

---
+++ branches/MAC-Bug1237-task-branch/CEP/MWCommon/src/WorkerFactory.cc
@@ -0,0 +1,44 @@
+//# WorkerFactory.cc: Factory pattern to generate a workerproxy object
+//#
+//# Copyright (c) 2007
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#include <lofar_config.h>
+
+#include <MWCommon/WorkerFactory.h>
+#include <MWCommon/MWError.h>
+#include <Common/LofarLogger.h>
+
+namespace LOFAR { namespace CEP {
+
+  void WorkerFactory::push_back (const std::string& name, Creator* creator)
+  {
+    itsMap[name] = creator;
+  }
+    
+  WorkerProxy::ShPtr WorkerFactory::create (const std::string& name) const
+  {
+    std::map<std::string,Creator*>::const_iterator iter = itsMap.find(name);
+    ASSERTSTR (iter != itsMap.end(),
+	       "WorkerProxy " << name << " is unknown");
+    return (*iter->second)();
+  }
+
+}} // end namespaces
---
+++ branches/MAC-Bug1237-task-branch/MAC/APL/PIC/TBB_Driver/src/ResetCmd.cc
@@ -0,0 +1,149 @@
+//#  ResetCmd.cc: implementation of the ResetCmd class
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <lofar_config.h>
+#include <Common/LofarLogger.h>
+#include <Common/StringUtil.h>
+#include <unistd.h>
+
+#include "ResetCmd.h"
+
+using namespace LOFAR;
+using namespace GCF::TM;
+using namespace TBB_Protocol;
+using namespace TP_Protocol;
+using	namespace TBB;
+
+//--Constructors for a ResetCmd object.----------------------------------------
+ResetCmd::ResetCmd():
+	itsBoardMask(0), itsBoardNr(0)
+{
+	TS = TbbSettings::instance();
+	
+	for(int boardnr = 0; boardnr < MAX_N_TBBOARDS; boardnr++) { 
+		itsStatus[boardnr] = TBB_NO_BOARD;
+	}
+	setWaitAck(true);	
+	setRetry(false);	
+}
+
+//--Destructor for ResetCmd.---------------------------------------------------
+ResetCmd::~ResetCmd() { }
+
+// ----------------------------------------------------------------------------
+bool ResetCmd::isValid(GCFEvent& event)
+{
+	if ((event.signal == TBB_RESET)||(event.signal == TP_RESET_ACK)) {
+		return(true);
+	}
+	return(false);
+}
+
+// ----------------------------------------------------------------------------
+void ResetCmd::saveTbbEvent(GCFEvent& event)
+{
+	TBBResetEvent tbb_event(event);
+	
+	itsBoardMask = tbb_event.boardmask;
+	
+	LOG_DEBUG_STR("boardMask= " << formatString("%08x",itsBoardMask));
+	
+	for (int boardnr = 0; boardnr < TS->maxBoards(); boardnr++) {
+		itsStatus[boardnr] = TBB_NO_BOARD;
+	}
+	
+	// look for first board in mask
+	while ((itsBoardMask & (1 << itsBoardNr)) == 0) {
+		itsBoardNr++;
+		if (itsBoardNr >= TS->maxBoards()) { 
+			break;	
+		}
+	}
+	
+	if (itsBoardNr < TS->maxBoards()) {
+		setBoardNr(itsBoardNr);
+	} else {
+		setDone(true);
+	}
+}
+
+// ----------------------------------------------------------------------------
+void ResetCmd::sendTpEvent()
+{
+	
+	TPResetEvent tp_event;
+	tp_event.opcode = oc_RESET;
+	tp_event.status = 0;
+
+	if (TS->boardPort(getBoardNr()).isConnected()) {
+		TS->boardPort(getBoardNr()).send(tp_event);
+		TS->boardPort(getBoardNr()).setTimer(5.0);
+	}
+	LOG_DEBUG_STR("Reset is send to boardnr " << getBoardNr());
+}
+
+// ----------------------------------------------------------------------------
+void ResetCmd::saveTpAckEvent(GCFEvent& event)
+{
+	// in case of a time-out, set error mask
+	if (event.signal == F_TIMER) {
+		TS->setBoardState(getBoardNr(),noBoard);
+	}	else {
+		TPResetAckEvent tp_ack(event);
+		TS->setImageNr(getBoardNr(), 0);
+		itsStatus[getBoardNr()] = tp_ack.status;
+		if (tp_ack.status != 0) {
+			TS->setBoardState(getBoardNr(),boardError);
+		}
+	}
+	
+	itsBoardNr++;
+	// look for next board in mask
+	while ((itsBoardMask & (1 << itsBoardNr)) == 0) {
+		itsBoardNr++;
+		if (itsBoardNr >= TS->maxBoards()) { 
+			break;
+		}	
+	}
+	if (itsBoardNr < TS->maxBoards()) {
+		setBoardNr(itsBoardNr);
+	} else {
+		setSleepTime(10.0);
+		setDone(true);
+	}
+}
+
+// ----------------------------------------------------------------------------
+void ResetCmd::sendTbbAckEvent(GCFPortInterface* clientport)
+{
+	TBBResetAckEvent tbb_ack;
+	
+	for (int32 boardnr = 0; boardnr < MAX_N_TBBOARDS; boardnr++) { 
+		if (itsStatus[boardnr] == 0) {
+			tbb_ack.status_mask[boardnr] = TBB_SUCCESS;
+		} else {
+			tbb_ack.status_mask[boardnr] = itsStatus[boardnr];
+		}
+	}
+	
+	if (clientport->isConnected()) { clientport->send(tbb_ack); }
+}
