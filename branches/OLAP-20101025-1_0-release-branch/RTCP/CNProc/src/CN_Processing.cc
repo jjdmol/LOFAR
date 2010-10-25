3f86c79cdb0fa4e84a7ae1e05afe606bbdfe2ce1
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-10-25 08:43:27 +0000

    bug 1362: disable excessive CNProc logging

--- branches/OLAP-20101025-1_0-release-branch/RTCP/CNProc/src/CN_Processing.cc
+++ branches/OLAP-20101025-1_0-release-branch/RTCP/CNProc/src/CN_Processing.cc
@@ -45,8 +45,8 @@ using boost::format;
 
 #if defined HAVE_BGP
 //#define LOG_CONDITION	(itsLocationInfo.rankInPset() == 0)
-//#define LOG_CONDITION	(itsLocationInfo.rank() == 0)
-#define LOG_CONDITION	1
+#define LOG_CONDITION	(itsLocationInfo.rank() == 0)
+//#define LOG_CONDITION	1
 #else
 #define LOG_CONDITION	1
 #endif
