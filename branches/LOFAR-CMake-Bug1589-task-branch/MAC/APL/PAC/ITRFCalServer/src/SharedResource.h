5b1247f220a65388d91d9ffa3545bcb248145a43
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-10-27 15:50:29 +0000

    Bug 1589: Replaced SharedResource.h with the newer version from the trunk.

--- branches/LOFAR-CMake-Bug1589-task-branch/MAC/APL/PAC/ITRFCalServer/src/SharedResource.h
+++ branches/LOFAR-CMake-Bug1589-task-branch/MAC/APL/PAC/ITRFCalServer/src/SharedResource.h
@@ -38,111 +38,106 @@
 namespace LOFAR {
   namespace ICAL {
 
-    class SharedResource
-    {
-    public:
-      explicit SharedResource(int maxreaders = 1, int maxwriters = 1) :
-	m_semaphore(0),
-	m_maxreaders(maxreaders),
-	m_maxwriters(maxwriters)
-      {
-	(void)pthread_mutex_init(&m_mutex, 0);
-      }
-      virtual ~SharedResource() {}
-
-      inline int mutex_lock()   { return pthread_mutex_lock(&m_mutex);   }
-      inline int mutex_unlock() { return pthread_mutex_unlock(&m_mutex); }
-    
-      /*@{*/
-      /**
-       * Lock the resource for reading or writing.
-       * @return true if the locking succeeded, false otherwise.
-       */
-      inline bool writeLock() {
-	mutex_lock();
-	int success = m_semaphore;
-	if (m_semaphore <= 0 && m_semaphore > -m_maxwriters) m_semaphore--;
-	success -= m_semaphore;
-	mutex_unlock();
-	return success;
-      }
-
-      inline bool readLock() {
-	mutex_lock();
-	int success = m_semaphore;
-	if (m_semaphore >= 0 && m_semaphore <  m_maxreaders) m_semaphore++;
-	success -= m_semaphore;
-	mutex_unlock();
-	return success;
-      }
-
-      /*@}*/
-
-      /*@{*/
-      /**
-       * Unlock the resource.
-       */
-      inline void writeUnlock() {
-	mutex_lock();
-	m_semaphore++;
-	if (m_semaphore > 0) {
-#ifdef CHECK_LOCKS
-	  LOG_WARN("no matching writeLock for writeUnlock");
-#endif
-	  m_semaphore = 0;
+class SharedResource
+{
+public:
+	explicit SharedResource(int maxreaders = 1, int maxwriters = 1) :
+		itsSemaphore(0),
+		itsMaxreaders(maxreaders),
+		itsMaxwriters(maxwriters)
+	{
+		(void)pthread_mutex_init(&itsMutex, 0);
 	}
-	mutex_unlock();
-      }
+	virtual ~SharedResource() {}
 
-      inline void readUnlock() {
-	mutex_lock();
-	m_semaphore--;
-	if (m_semaphore < 0) {
-#ifdef CHECK_LOCKS
-	  LOG_WARN("no matching readLock for readUnlock");
-#endif
-	  m_semaphore = 0;
+	inline int mutex_lock()   { return pthread_mutex_lock(&itsMutex);   }
+	inline int mutex_unlock() { return pthread_mutex_unlock(&itsMutex); }
+
+	/*@{*/
+	// Lock the resource for reading or writing.
+	// @return true if the locking succeeded, false otherwise.
+	inline bool writeLock() {
+		mutex_lock();
+		int success = itsSemaphore;
+		if (itsSemaphore <= 0 && itsSemaphore > -itsMaxwriters) 
+			itsSemaphore--;
+		success -= itsSemaphore;
+		mutex_unlock();
+		return success;
 	}
-	mutex_unlock();
-      }
-      /*@}*/
 
-      /*@{*/
-      /**
-       * Check whether the resource is locked for
-       * reading or writing, or for reading
-       * @return true if the resource is locked, false otherwise.
-       */
-      bool isLocked() {
-	bool success;
-	mutex_lock();
-	success = (0 != m_semaphore);
-	mutex_unlock();
-	return success;
-      }
+	inline bool readLock() {
+		mutex_lock();
+		int success = itsSemaphore;
+		if (itsSemaphore >= 0 && itsSemaphore <  itsMaxreaders) 
+			itsSemaphore++;
+		success -= itsSemaphore;
+		mutex_unlock();
+		return success;
+	}
+	/*@}*/
 
-      bool isWriteLocked() {
-	mutex_lock();
-	bool success;
-	success = (m_semaphore < 0);
-	mutex_unlock();
-	return success; 
-      }
+	/*@{*/
+	// Unlock the resource.
+	inline void writeUnlock() {
+		mutex_lock();
+		itsSemaphore++;
+		if (itsSemaphore > 0) {
+#ifdef CHECK_LOCKS
+			LOG_WARN("no matching writeLock for writeUnlock");
+#endif
+			itsSemaphore = 0;
+		}
+		mutex_unlock();
+	}
 
-      bool isReadLocked() {
-	mutex_lock();
-	bool success;
-	success = (m_semaphore > 0);
-	mutex_unlock();
-	return success;
-      }
+	inline void readUnlock() {
+		mutex_lock();
+		itsSemaphore--;
+		if (itsSemaphore < 0) {
+#ifdef CHECK_LOCKS
+			LOG_WARN("no matching readLock for readUnlock");
+#endif
+			itsSemaphore = 0;
+		}
+		mutex_unlock();
+	}
+	/*@}*/
 
-    private:
-      int m_semaphore;
-      int m_maxreaders;
-      int m_maxwriters;
-      pthread_mutex_t m_mutex;
-    };
+	/*@{*/
+	// Check whether the resource is locked for
+	// reading or writing, or for reading
+	// @return true if the resource is locked, false otherwise.
+	bool isLocked() {
+		bool success;
+		mutex_lock();
+		success = (0 != itsSemaphore);
+		mutex_unlock();
+		return success;
+	}
+
+	bool isWriteLocked() {
+		mutex_lock();
+		bool success;
+		success = (itsSemaphore < 0);
+		mutex_unlock();
+		return success; 
+	}
+
+	bool isReadLocked() {
+		mutex_lock();
+		bool success;
+		success = (itsSemaphore > 0);
+		mutex_unlock();
+		return success;
+	}
+
+private:
+	int 			itsSemaphore;
+	int 			itsMaxreaders;
+	int 			itsMaxwriters;
+	pthread_mutex_t itsMutex;
+};
 
   }; // namespace ICAL
 }; // namespace LOFAR
