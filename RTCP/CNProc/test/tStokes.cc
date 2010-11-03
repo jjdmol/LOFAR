f3c6fa30e59d75cd2a6d4a23d83079ccc9ea556e
Jan David Mol <mol@astron.nl>
2010-11-03 10:11:36 +0000

    bug 1362: tStokes matched with updated Stokes API

--- RTCP/CNProc/test/tStokes.cc
+++ RTCP/CNProc/test/tStokes.cc
@@ -70,7 +70,7 @@ void test_incoherent_stokes( unsigned NRSTOKES, unsigned INTEGRATION ) {
   }
 
   // calculate
-  Stokes     s = Stokes( NRSTOKES, NRCHANNELS, NRSAMPLES, INTEGRATION );
+  Stokes     s = Stokes( NRSTOKES, NRCHANNELS, NRSAMPLES, INTEGRATION, 1 );
   if (NRSTOKES == 4) {
     s.calculateIncoherent<true>( &in, &out, stationMapping );
   } else {
@@ -158,7 +158,7 @@ void test_coherent_stokes( unsigned NRSTOKES, unsigned INTEGRATION ) {
   }
 
   // calculate
-  Stokes     s = Stokes( NRSTOKES, NRCHANNELS, NRSAMPLES, INTEGRATION );
+  Stokes     s = Stokes( NRSTOKES, NRCHANNELS, NRSAMPLES, INTEGRATION, 1 );
   for( unsigned b = 0; b < NRPENCILBEAMS; b++ ) {
     if (NRSTOKES == 4) {
       s.calculateCoherent<true>( &in, &out, b );
