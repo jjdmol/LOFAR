ed6647d0d124d007284149c38a04772642802239
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-12-03 12:54:18 +0000

    bug 1315: new beamformer test with ITRF beamformer

--- LCU/StationTest/test/beamformertest/beamformertest_splitter.sh
+++ LCU/StationTest/test/beamformertest/beamformertest_splitter.sh
@@ -1,6 +1,6 @@
 # beamformer test for 96 rcu's in HBA mode (with splitter ON)
 # object zenith
-# version 1.0,  1 oct 2009, M.J.Norden
+# version 1.1,  3 dec 2010, M.J.Norden
 
 
 rspctl --wg=0
@@ -15,11 +15,11 @@ sleep 2
 rspctl --splitter=1
 sleep 3
 
-beamctl --array=HBA --rcumode=5 --rcus=0:1 --subbands=320:381 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=HBA_ZERO --rcumode=5 --rcus=0:1 --subbands=320:381 --beamlets=0:61 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=HBA --rcumode=5 --rcus=0:47 --subbands=320:381 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=HBA_ZERO --rcumode=5 --rcus=0:47 --subbands=320:381 --beamlets=62:123 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=HBA --rcumode=5 --rcus=48:49 --subbands=320:381 --beamlets=1124:1185 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=HBA_ONE --rcumode=5 --rcus=48:49 --subbands=320:381 --beamlets=1124:1185 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=HBA --rcumode=5 --rcus=48:95 --subbands=320:381 --beamlets=1186:1247 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=HBA_ONE --rcumode=5 --rcus=48:95 --subbands=320:381 --beamlets=1186:1247 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 
