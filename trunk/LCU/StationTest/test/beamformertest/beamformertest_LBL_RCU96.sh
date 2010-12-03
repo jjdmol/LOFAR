81f69d5ea3001919f0a4902a1a8e670e64e797fe
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-12-03 12:54:18 +0000

    bug 1315: new beamformer test with ITRF beamformer

--- trunk/LCU/StationTest/test/beamformertest/beamformertest_LBL_RCU96.sh
+++ trunk/LCU/StationTest/test/beamformertest/beamformertest_LBL_RCU96.sh
@@ -1,6 +1,6 @@
 # beamformer test for 96 rcu's in LBL mode (with splitter OFF)
 # object zenith
-# version 1.1,  13 jan 2010, M.J.Norden
+# version 1.2,  3 dec 2010, M.J.Norden
 
 
 rspctl --wg=0
@@ -15,11 +15,11 @@ sleep 2
 rspctl --splitter=0
 sleep 3
 
-beamctl --array=LBA_OUTER --rcumode=1 --rcus=0:1 --subbands=270:331 --beamlets=0:61 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=LBA_OUTER --rcumode=1 --rcus=0:1 --subbands=270:331 --beamlets=0:61 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=LBA_OUTER --rcumode=1 --rcus=0:47 --subbands=270:331 --beamlets=62:123 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=LBA_OUTER --rcumode=1 --rcus=0:47 --subbands=270:331 --beamlets=62:123 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=LBA_OUTER --rcumode=1 --rcus=48:49 --subbands=270:331 --beamlets=124:185 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=LBA_OUTER --rcumode=1 --rcus=48:49 --subbands=270:331 --beamlets=124:185 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 sleep 3
-beamctl --array=LBA_OUTER --rcumode=1 --rcus=0:95 --subbands=270:331 --beamlets=186:247 --direction=0,0,LOFAR_LMN&
+beamctl --antennaset=LBA_OUTER --rcumode=1 --rcus=0:95 --subbands=270:331 --beamlets=186:247 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
 
