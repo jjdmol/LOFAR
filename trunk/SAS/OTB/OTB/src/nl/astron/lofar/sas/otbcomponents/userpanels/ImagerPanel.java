eca47a7301859afb5430a694dbbce1a9575b42f7
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-12-09 19:38:08 +0000

    Bug 756: nullpointer check added and new range  for TBB component

--- trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/userpanels/ImagerPanel.java
+++ trunk/SAS/OTB/OTB/src/nl/astron/lofar/sas/otbcomponents/userpanels/ImagerPanel.java
@@ -70,6 +70,7 @@ public class ImagerPanel extends javax.swing.JPanel implements IViewPanel{
         itsNode=aNode;
         initialize();
         initPanel();
+        
     }
     
     /** Creates new form ImagerPanel */
