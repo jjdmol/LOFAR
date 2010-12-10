8f93d158af9388971ee361134d77b07c291ac3bc
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-12-10 12:31:41 +0000

    bug 1315: modemtest updated for 192 rcu's

--- LCU/StationTest/test/hbatest/modemtest.sh
+++ LCU/StationTest/test/hbatest/modemtest.sh
@@ -2,8 +2,8 @@
 #
 # Send delays from RCU to HBA and compare results with the expected golden result.
 # To verify modem communication between RCU and HBA
-#
-# Version 1.4  09-07-10   M.J.Norden
+# Modified for INT stations
+# Version 1.5  10-12-10   M.J.Norden
 
 rm -f hba_modem*.log
 rm -f hba_modem*.diff
@@ -44,49 +44,97 @@ fi
 echo "The rcumode is "$hbamode 
 sleep 2
 
-echo "rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
-rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
-sleep $ontime
+if [ $rspboards == 12 ]; then
+     # This is a NL station
+     echo "rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
+     rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+     sleep $ontime
+     rspctl --realdelays > hba_modem1.log
 
-rspctl --realdelays > hba_modem1.log
-diff hba_modem1.log gold/hba_modem1.gold > hba_modem1.diff
+     diff hba_modem1.log gold/hba_modem1.gold > hba_modem1.diff
 
-if [ -e hba_modem1.log ] && [ -e gold/hba_modem1.gold ] && [ -e hba_modem1.diff ] && ! [ -s hba_modem1.diff ]; then
-    # The files exists AND the diff has size 0
-    echo "HBA modem test went OK"
+     if [ -e hba_modem1.log ] && [ -e gold/hba_modem1.gold ] && [ -e hba_modem1.diff ] && ! [ -s hba_modem1.diff ]; then
+       # The files exists AND the diff has size 0
+       echo "HBA modem test went OK"
+     else
+       rspctl --realdelays
+       echo "HBA modem test went wrong"
+     fi
+     echo "rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+     rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+     sleep $ontime
+
+     rspctl --realdelays > hba_modem2.log
+     diff hba_modem2.log gold/hba_modem2.gold > hba_modem2.diff
+
+     if [ -e hba_modem2.log ] && [ -e gold/hba_modem2.gold ] && [ -e hba_modem2.diff ] && ! [ -s hba_modem2.diff ]; then
+        # The files exists AND the diff has size 0
+        echo "HBA modem test went OK"
+     else
+        rspctl --realdelays
+        echo "HBA modem test went wrong"
+     fi
+
+     echo "rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253"
+     rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253
+     sleep $ontime
+
+     rspctl --realdelays > hba_modem3.log
+     diff hba_modem3.log gold/hba_modem3.gold > hba_modem3.diff
+
+     if [ -e hba_modem3.log ] && [ -e gold/hba_modem3.gold ] && [ -e hba_modem3.diff ] && ! [ -s hba_modem3.diff ]; then
+        # The files exists AND the diff has size 0
+        echo "HBA modem test went OK"
+     else
+        rspctl --realdelays
+        echo "HBA modem test went wrong"
+     fi
 else
-    rspctl --realdelays
-    echo "HBA modem test went wrong"
+     # This is a INT station
+     echo "rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
+     rspctl --hbadelays=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+     sleep $ontime
+     rspctl --realdelays > hba_modem1.log
+
+     diff hba_modem1.log gold/hba_modem1_int.gold > hba_modem1.diff
+
+     if [ -e hba_modem1.log ] && [ -e gold/hba_modem1_int.gold ] && [ -e hba_modem1.diff ] && ! [ -s hba_modem1.diff ]; then
+       # The files exists AND the diff has size 0
+       echo "HBA modem test went OK"
+     else
+       rspctl --realdelays
+       echo "HBA modem test went wrong"
+     fi
+     echo "rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+     rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+     sleep $ontime
+
+     rspctl --realdelays > hba_modem2.log
+     diff hba_modem2.log gold/hba_modem2_int.gold > hba_modem2.diff
+
+     if [ -e hba_modem2.log ] && [ -e gold/hba_modem2_int.gold ] && [ -e hba_modem2.diff ] && ! [ -s hba_modem2.diff ]; then
+        # The files exists AND the diff has size 0
+        echo "HBA modem test went OK"
+     else
+        rspctl --realdelays
+        echo "HBA modem test went wrong"
+     fi
+
+     echo "rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253"
+     rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253
+     sleep $ontime
+
+     rspctl --realdelays > hba_modem3.log
+     diff hba_modem3.log gold/hba_modem3_int.gold > hba_modem3.diff
+
+     if [ -e hba_modem3.log ] && [ -e gold/hba_modem3_int.gold ] && [ -e hba_modem3.diff ] && ! [ -s hba_modem3.diff ]; then
+        # The files exists AND the diff has size 0
+        echo "HBA modem test went OK"
+     else
+        rspctl --realdelays
+        echo "HBA modem test went wrong"
+     fi
 fi
 
-echo "rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
-rspctl --hbadelays=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-sleep $ontime
-
-rspctl --realdelays > hba_modem2.log
-diff hba_modem2.log gold/hba_modem2.gold > hba_modem2.diff
-
-if [ -e hba_modem2.log ] && [ -e gold/hba_modem2.gold ] && [ -e hba_modem2.diff ] && ! [ -s hba_modem2.diff ]; then
-    # The files exists AND the diff has size 0
-    echo "HBA modem test went OK"
-else
-    rspctl --realdelays
-    echo "HBA modem test went wrong"
-fi
-
-echo "rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253"
-rspctl --hbadelays=253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253
-sleep $ontime
-
-rspctl --realdelays > hba_modem3.log
-diff hba_modem3.log gold/hba_modem3.gold > hba_modem3.diff
-
-if [ -e hba_modem3.log ] && [ -e gold/hba_modem3.gold ] && [ -e hba_modem3.diff ] && ! [ -s hba_modem3.diff ]; then
-    # The files exists AND the diff has size 0
-    echo "HBA modem test went OK"
-else
-    rspctl --realdelays
-    echo "HBA modem test went wrong"
-fi
 
 
