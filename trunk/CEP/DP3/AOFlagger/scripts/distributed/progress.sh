084c6ef92595bfdd76a11128b15cd06b20a900f6
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2010-12-18 12:14:24 +0000

    Bug 1491: adding eor scripts

--- trunk/CEP/DP3/AOFlagger/scripts/distributed/progress.sh
+++ trunk/CEP/DP3/AOFlagger/scripts/distributed/progress.sh
@@ -6,18 +6,19 @@ fi
 while true ; do
     clear
     iteration=${iteration}+1
-    for l in `ls -1 out|grep lce` ; do
+    for l in `ls -1 out|grep node` ; do
 	if [[ -f out/$l/progress.txt ]] ; then
 	    cd out/$l
 	    last=`ls -1|sort -g|tail -n 1`
 	    cd ../..
 	    if [[ -f out/$l/${last}/stdout.txt ]] ; then
-		echo $l: `cat out/$l/progress.txt` `cat out/$l/${last}/stdout.txt|grep \%|tail -n 1`
+		echo $l: `cat out/$l/progress.txt` `cat out/$l/${last}/stderr.txt|grep \%|tail -n 1`
 	    else
 		echo $l: `cat out/$l/progress.txt` -
 	    fi
 	fi
     done
+    echo End of iteration.
     sleep 60
     collect.sh
     sleep 1
