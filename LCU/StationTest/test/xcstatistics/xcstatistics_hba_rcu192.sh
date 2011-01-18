765f1800d0ac3fc9dab0efc97bc49f71ef2e3f1e
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-01-18 12:26:14 +0000

    bug 1315: AZELGEO i.s.o AZEL and 61 beamlets

--- LCU/StationTest/test/xcstatistics/xcstatistics_hba_rcu192.sh
+++ LCU/StationTest/test/xcstatistics/xcstatistics_hba_rcu192.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# 1.0 xcstatistics test to check SerDes Ring with HBA antennas
-# 06-12-10, M.J Norden
+# 1.1 xcstatistics test to check SerDes Ring with HBA antennas
+# 18-01-2011, M.J Norden
 # HBA input with antenna
 
 killall beamctl
@@ -10,7 +10,7 @@ rspctl --splitter=0
 
 swlevel 3
 sleep 5
-beamctl --antennaset=HBA_JOINED --rcus=0:191 --rcumode=5 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
+beamctl --antennaset=HBA_JOINED --rcus=0:191 --rcumode=5 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZELGEO --digdir=0,1.5708,AZELGEO&
 sleep 5
 echo "check xcstat and xcangle"
 
