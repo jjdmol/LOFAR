857148314906700a7f0f2321cbeca62ed190f6d2
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-01-18 12:25:59 +0000

    bug 1315: AZELGEO i.s.o AZEL and 61 beamlets

--- trunk/LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-6.sh
+++ trunk/LCU/StationTest/test/subbandstatistics/subbandstatistics_hba-6.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# 1.4 subband statistics HBA test 
-# 03-12-10, M.J Norden
+# 1.5 subband statistics HBA test 
+# 18-01-2011, M.J Norden
 # HBA input with antennas
 
 
@@ -14,7 +14,7 @@ killall beamctl
 
 swlevel 3
 sleep 5
-beamctl --antennaset=HBA_JOINED --rcus=0:95 --rcumode=6 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
+beamctl --antennaset=HBA_JOINED --rcus=0:95 --rcumode=6 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZELGEO --digdir=0,1.5708,AZELGEO&
 sleep 15
 
 echo ==========================
