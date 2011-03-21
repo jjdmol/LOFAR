5b17b12039bc08d7fc85cf9bd3aabcf72ee01100
Marcel Loose <gmloose@users.noreply.github.com>
2011-03-21 13:41:14 +0000

    Bug 1664: Moved implementation of ep_interface from .h to .cc file

--- CEP/Pipeline/mac/src/ep_interface.h
+++ CEP/Pipeline/mac/src/ep_interface.h
@@ -6,9 +6,6 @@
   \ingroup pipeline
 */
 
-#include <boost/python.hpp>
-#include <lofar_config.h>
-#include <Common/LofarLogger.h>
 #include <MACIO/EventPort.h>
 #include <string>
 
@@ -19,24 +16,17 @@
   \ingroup pipeline
   \brief Event Port Interface
 */
-class EP_Interface {
-private:
-    LOFAR::MACIO::EventPort* my_EventPort;
+class EP_Interface
+{
 public:
-    EP_Interface(std::string servicename, short protocol_id, std::string host = "") {
-        this->my_EventPort = new LOFAR::MACIO::EventPort(servicename, false, protocol_id, host, true);
-    }
-    GenericEventWrapper* receive_event() {
-        LOFAR::MACIO::GCFEvent* ackPtr;
-        Py_BEGIN_ALLOW_THREADS
-        ackPtr = my_EventPort->receive();
-        Py_END_ALLOW_THREADS
-        return new GenericEventWrapper(ackPtr);
-    }
-    void send_event(GenericEventWrapper* wrapped_event) {
-        this->my_EventPort->send(wrapped_event->get_event_ptr());
-        }
-    ~EP_Interface() { delete this->my_EventPort; }
+  EP_Interface(std::string servicename, 
+               short protocol_id, 
+               std::string host = "");
+  GenericEventWrapper* receive_event();
+  void send_event(GenericEventWrapper* wrapped_event);
+  ~EP_Interface();
+private:
+  LOFAR::MACIO::EventPort* my_EventPort;
 };
 
 #endif
