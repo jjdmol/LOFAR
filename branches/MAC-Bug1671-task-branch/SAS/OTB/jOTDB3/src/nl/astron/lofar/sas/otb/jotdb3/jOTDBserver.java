886c68b309972124c637e0c7f4b4c24abb81eedb
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-04-29 12:34:13 +0000

    Bug756:Refresh table entries in Main after each tab switch.  jOTDB3 server will use argumenst and both jOTDB3 serverand OTB client will have defaults where possible.  OTB will accept new parameters for username and database name

--- branches/MAC-Bug1671-task-branch/SAS/OTB/jOTDB3/src/nl/astron/lofar/sas/otb/jotdb3/jOTDBserver.java
+++ branches/MAC-Bug1671-task-branch/SAS/OTB/jOTDB3/src/nl/astron/lofar/sas/otb/jotdb3/jOTDBserver.java
@@ -21,10 +21,14 @@
 
 package nl.astron.lofar.sas.otb.jotdb3;
 
+import com.darwinsys.lang.GetOpt;
+import com.darwinsys.lang.GetOptDesc;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Iterator;
+import java.util.Map;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -41,7 +45,7 @@ public class jOTDBserver {
     // Keep a static reference to the access object to prevent the server from exitting.
     static jOTDBaccessInterface access = null;
 
-    public static void main(String[] args)  {
+    public static void main(String[] argv)  {
         try {
             String logConfig = "jOTDB3.log_prop";
             
@@ -66,12 +70,10 @@ public class jOTDBserver {
             // needed args for an rmi connection:
 
             // given to this server:
-            // 1) database username
-            // 2) database username pwd
-            // 4) host where database runs
-            // 5) hostname where this server runs
-            // 6) rmi port used by the server for client contact(OPTIONAL)
-            // 7) rmi port used for firewall/tunneling purposes (OPTIONAL)
+            // 1) host where database runs
+            // 2) hostname where this server runs
+            // 3) rmi port used by the server for client contact(OPTIONAL)
+            // 4) rmi port used for firewall/tunneling purposes (OPTIONAL)
 
 
             // given by client that wants to connect :
@@ -80,44 +82,98 @@ public class jOTDBserver {
 
             //
 
-            if (args.length < 2) {
-                System.out.println("Usage: java -jar jOTDBnewServer.jar <db-hostname> <hostname> <rmiportnumber-OPTIONAL> <rmi objects portnumber for firewall/tunneling purposes-OPTIONAL>");
+            
+            String dbHostName     = "";
+            String serverHostName = "";
+            int rmiPort           = 0;
+            int rmiObjectPort     = 0;
+            boolean errs          = false;
+
+
+            // try to resolve the arguments
+            GetOptDesc options[] = {
+                new GetOptDesc('s', "serverHostName", true),
+                new GetOptDesc('d', "dbHostName", true),
+                new GetOptDesc('p', "rmiPort", true),
+                new GetOptDesc('o', "rmiObjectPort", true),
+                new GetOptDesc('h', "help", false),
+                new GetOptDesc('?', "help", false)
+            };
+
+            GetOpt parser = new GetOpt(options);
+            Map optionsFound = parser.parseArguments(argv);
+            Iterator it = optionsFound.keySet().iterator();
+            while (it.hasNext()) {
+                String key = (String)it.next();
+                char c = key.charAt(0);
+                switch (c) {
+                    case 's':
+                        serverHostName = (String)optionsFound.get(key);
+                        break;
+                    case 'd':
+                        dbHostName = (String)optionsFound.get(key);
+                        break;
+                    case 'p':
+                        rmiPort = ((Integer)optionsFound.get(key)).intValue();
+                        break;
+                    case 'o':
+                        rmiObjectPort = ((Integer)optionsFound.get(key)).intValue();
+                        break;
+                    case 'h':
+                        errs = true;
+                        break;
+                    case '?':
+                        errs = true;
+                        break;
+                    default:
+                        throw new IllegalStateException(
+                                "Unexpected option character: "+ c);
+                }
+            }
+            if (errs) {
+                System.err.println("Usage: java -jar jOTDBnewServer.jar  -p rmiPort -o rmiObjectPort [-s server] [-d database server]  [-h] [-?]");
+                System.err.println("");
+                System.err.println("       The rmi port is needed for rmi traffic and must be unique fior each server");
+                System.err.println("       The rmiObjectPort is needed for firewall/tunneling java objects and must be unique for each server");
+                System.err.println("       When the servername is not provided the server will try to resolve one itself for the local machine");
+                System.err.println("       When the database server name is not provided it is assumed to run on the localmachine");
+            }
+
+            if (rmiPort <= 0 || rmiObjectPort <= 0 || rmiPort==rmiObjectPort) {
+                logger.fatal("Invalid rmiPort or rmiObjectPort provided");
                 System.exit(0);
             }
-            
-            
-            String aHostName = new String(args[1]);
-            
-            logger.info("Running on: "+aHostName);
-            System.setProperty("java.rmi.server.hostname", aHostName);
 
+            if (serverHostName.equals("")) {
+                try {
+                    java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+                    logger.info("Hostname of local machine: " + localMachine.getHostName());
+                    serverHostName=localMachine.getHostName();
+                }
+                catch (java.net.UnknownHostException ex) {
+                    logger.error("Couldn't resolve hostname" + ex);
+                    logger.fatal("restart using -h hostname switch");
+                    System.exit(0);
+                }
+            }
 
+            System.setProperty("java.rmi.server.hostname", serverHostName);
+
+            // if dbHostname not provided, the server runs on same machin as the database
+            if (dbHostName.equals("")) dbHostName=serverHostName;
 
             // Create the access object that will take care of registering to rmi
-            int rmiPort = 0;
-            int rmiObjectPort=0;
-            Registry registry = null;
+            logger.info("jOTDBserver creating a local RMI registry on port "+rmiPort+" ...");
+            Registry registry=null;
             try {
-                rmiPort = Integer.parseInt(args[2]);
-            } catch (NumberFormatException ex) {
-                logger.error("jOTDBserver rmiPort error: "+args[2]+ "  " + ex);
-            }
-            try {
-                rmiObjectPort = Integer.parseInt(args[3]);
-            } catch (NumberFormatException ex) {
-                logger.error("jOTDBserver rmiObjectPort error: "+args[3]+ "  " + ex);
-            }
-
-            if (rmiPort <= 0) {
-                logger.info("jOTDBaccess creating a local RMI registry on port " + Registry.REGISTRY_PORT + " ...");
-                registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-            } else {
-                logger.info("jOTDBserver creating a local RMI registry on port "+rmiPort+" ...");
                 registry = LocateRegistry.createRegistry(rmiPort);
-                logger.info("jOTDBserver setting up RMI server objects on port "+rmiObjectPort+" ...");
+            } catch (RemoteException ex) {
+                logger.fatal("Error creating registry port:" + ex);
+                System.exit(0);
             }
+            logger.info("jOTDBserver setting up RMI server objects on port "+rmiObjectPort+" ...");
 
-            access = new jOTDBaccess(args[0], args[1],rmiPort,rmiObjectPort,registry);
+            access = new jOTDBaccess(dbHostName, serverHostName, rmiPort, rmiObjectPort, registry);
             jOTDBaccessInterface stub =
                 (jOTDBaccessInterface) UnicastRemoteObject.exportObject(access, rmiObjectPort);
 
