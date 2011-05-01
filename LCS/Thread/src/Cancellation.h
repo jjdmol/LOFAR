7e14b85ac8138d1bac7f0aae50ba78621df0eca3
Jan David Mol <mol@astron.nl>
2011-05-01 09:00:45 +0000

    bug 1362: Thread cancellation uses a reference count per thread

--- LCS/Thread/src/Cancellation.h
+++ LCS/Thread/src/Cancellation.h
@@ -25,7 +25,10 @@
 
 //# Never #include <config.h> or #include <lofar_config.h> in a header file!
 
+#ifdef USE_THREADS
 #include <pthread.h>
+#include <map>
+#endif
 
 namespace LOFAR {
 
@@ -33,37 +36,104 @@ namespace LOFAR {
 class Cancellation {
 public:
   // allows cancellation here explicitly
-  static void point() {
-    pthread_testcancel();
-  }
+  static void point();
 
   // set (enable or disable) the cancellability of this thread.
   // returns the previous value.
-  static bool set( bool enable ) {
-    int oldState;
+  static bool set( bool enable );
 
-    pthread_setcanceltype( enable ? PTHREAD_CANCEL_ENABLE : PTHREAD_CANCEL_DISABLE, &oldState );
-
-    return oldState == PTHREAD_CANCEL_ENABLE;
-  }
+  // return the current state of cancellability of this thread.
+  static bool get();
 
   static bool disable() { return set( false ); }
   static bool enable()  { return set( true ); }
+
+
+  // push_disable() and pop_disable() maintain a reference count on how many objects
+  // requested disabling cancellation. After the first push_disable(), cancellation
+  // is disabled until the last pop_disable(), after which the original cancellation
+  // state is restored.
+  static void push_disable();
+  static void pop_disable();
+
+private:
+#ifdef USE_THREADS
+  static std::map<pthread_t, unsigned> refcounts; 
+  static std::map<pthread_t, int> oldstates; 
+#endif  
 };
 
 
 class ScopedDelayCancellation {
 public:
   ScopedDelayCancellation() {
-   itsOldState = Cancellation::disable();
+    Cancellation::push_disable();
   };
 
   ~ScopedDelayCancellation() {
-    Cancellation::set( itsOldState );
+    Cancellation::pop_disable();
   }
-private:
-  bool itsOldState;
 };
+
+
+inline void Cancellation::point() {
+#ifdef USE_THREADS
+  pthread_testcancel();
+#endif  
+}
+
+
+inline bool Cancellation::set( bool enable ) {
+#ifdef USE_THREADS
+  int oldState;
+
+  pthread_setcanceltype( enable ? PTHREAD_CANCEL_ENABLE : PTHREAD_CANCEL_DISABLE, &oldState );
+
+  return oldState == PTHREAD_CANCEL_ENABLE;
+#else
+  // return whatever value we consider default if no threads are used
+  return get();
+#endif  
+}
+
+
+inline bool Cancellation::get() {
+#ifdef USE_THREADS
+  // pthread_getcanceltype doesn't exist unfortunately, so emulate it.
+  bool state = disable(); // never accidently toggle to enabled!
+
+  set( state );
+  return state;
+#else
+  return false;
+#endif  
+}
+
+
+inline void Cancellation::push_disable() {
+#ifdef USE_THREADS
+  pthread_t myid = pthread_self();
+
+  // can't use refcounts[myid] directly since the default constructor of unsigned
+  // won't set it to 0 if myid is not in refcounts
+  unsigned oldcount = refcounts.find(myid) == refcounts.end() ? 0 : refcounts[myid];
+
+  refcounts[myid] = oldcount + 1;
+
+  if (oldcount == 0)
+    oldstates[myid] = disable();
+#endif  
+}
+
+
+inline void Cancellation::pop_disable() {
+#ifdef USE_THREADS
+  pthread_t myid = pthread_self();
+
+  if (--refcounts[myid] == 0)
+    set( oldstates[myid] );
+#endif    
+}
 
 
 } // namespace LOFAR
