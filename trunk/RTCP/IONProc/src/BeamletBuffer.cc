6cc01c778941772f891ee747993ee8aec1489857
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-05-01 11:42:21 +0000

    bug 1362: more scoped locks in case of exceptions or cancellation

--- trunk/RTCP/IONProc/src/BeamletBuffer.cc
+++ trunk/RTCP/IONProc/src/BeamletBuffer.cc
@@ -125,7 +125,8 @@ template<typename SAMPLE_TYPE> inline void BeamletBuffer<SAMPLE_TYPE>::writePack
 
 template<typename SAMPLE_TYPE> inline void BeamletBuffer<SAMPLE_TYPE>::updateValidData(const TimeStamp &begin, const TimeStamp &end)
 {
-  itsValidDataMutex.lock();
+  ScopedLock sl(itsValidDataMutex);
+
   itsValidData.exclude(0, end - itsSize);  // forget old ValidData
 
   // add new ValidData (except if range list will grow too long, to avoid long
@@ -135,8 +136,6 @@ template<typename SAMPLE_TYPE> inline void BeamletBuffer<SAMPLE_TYPE>::updateVal
 
   if (ranges.size() < 64 || ranges.back().end == begin) 
     itsValidData.include(begin, end);
-
-  itsValidDataMutex.unlock();
 }
 
 
@@ -190,18 +189,17 @@ template<typename SAMPLE_TYPE> void BeamletBuffer<SAMPLE_TYPE>::resetCurrentTime
   if (!aligned(itsCurrentI, itsNrTimesPerPacket)) {
     // RSP board reset?  Recompute itsOffset and clear the entire buffer.
 
-    itsReadMutex.lock(); // avoid reset while other thread reads
+    ScopedLock sl(itsReadMutex); // avoid reset while other thread reads
 
     int oldOffset = itsOffset;
     itsOffset   = - (newTimeStamp % itsNrTimesPerPacket);
     itsCurrentI = mapTime2Index(newTimeStamp);
     assert(aligned(itsCurrentI, itsNrTimesPerPacket));
 
-    itsValidDataMutex.lock();
-    itsValidData.reset();
-    itsValidDataMutex.unlock();
-
-    itsReadMutex.unlock();
+    {
+      ScopedLock sl(itsValidDataMutex);
+      itsValidData.reset();
+    }
 
     if (!firstPacket) {
       LOG_WARN_STR(itsLogPrefix << "Reset BeamletBuffer at " << newTimeStamp << "; itsOffset was " << oldOffset << " and becomes " << itsOffset);
@@ -250,9 +248,10 @@ template<typename SAMPLE_TYPE> void BeamletBuffer<SAMPLE_TYPE>::writePacketData(
       itsOffset = - (startI % itsNrTimesPerPacket);
       startI    = mapTime2Index(begin);
 
-      itsValidDataMutex.lock();
-      itsValidData.reset();
-      itsValidDataMutex.unlock();
+      {
+        ScopedLock sl(itsValidDataMutex);
+        itsValidData.reset();
+      }
     }
 
     //LOG_DEBUG_STR(""timestamp = " << (uint64_t) begin << ", itsOffset = " << itsOffset");
@@ -276,17 +275,17 @@ template<typename SAMPLE_TYPE> void BeamletBuffer<SAMPLE_TYPE>::writePacketData(
   writePacket(itsSBBuffers[0][startI].origin(), data);
   
   // forget old ValidData
-  itsValidDataMutex.lock();
-  itsValidData.exclude(0, end - itsSize);
+  {
+    ScopedLock sl(itsValidDataMutex);
+    itsValidData.exclude(0, end - itsSize);
 
-  unsigned rangesSize = itsValidData.getRanges().size();
+    unsigned rangesSize = itsValidData.getRanges().size();
 
-  // add new ValidData (except if range list will grow too long, to avoid long
-  // computations)
-  if (rangesSize < 64 || itsValidData.getRanges()[rangesSize - 1].end == begin) 
-    itsValidData.include(begin, end);
-
-  itsValidDataMutex.unlock();
+    // add new ValidData (except if range list will grow too long, to avoid long
+    // computations)
+    if (rangesSize < 64 || itsValidData.getRanges()[rangesSize - 1].end == begin) 
+      itsValidData.include(begin, end);
+  }  
 
   itsLockedRanges.unlock(startI, endI);
 
