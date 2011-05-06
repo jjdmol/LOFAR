a812e5bb002ca094fa22529a2d3236f4ad56b96a
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-05-06 11:00:05 +0000

    Bug 1491: Disabling results test in normal mode

--- CEP/DP3/AOFlagger/test/aotest.cpp
+++ CEP/DP3/AOFlagger/test/aotest.cpp
@@ -3,11 +3,14 @@
 #include <AOFlagger/test/strategy/algorithms/algorithmstestgroup.h>
 #include <AOFlagger/test/results/resultstestgroup.h>
 
-int main(int, char *[])
+int main(int argc, char *argv[])
 {
 	AlgorithmsTestGroup mainGroup;
 	mainGroup.Run();
 	
-	ResultsTestGroup resultsGroup;
-	resultsGroup.Run();
+	if(argc > 1 && std::string(argv[1])=="all")
+	{
+		ResultsTestGroup resultsGroup;
+		resultsGroup.Run();
+	}
 }
