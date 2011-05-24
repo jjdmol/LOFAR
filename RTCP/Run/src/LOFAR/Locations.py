e6a6c30286c17d09c693319656bc5c6cb6a06c66
Jan David Mol <mol@astron.nl>
2011-05-24 14:53:18 +0000

    bug 1362: (hopefully) the final location for Storage_main

--- RTCP/Run/src/LOFAR/Locations.py
+++ RTCP/Run/src/LOFAR/Locations.py
@@ -103,7 +103,7 @@ class Locations:
         # the locations of the main executables
 	"cnproc":  "${BASEDIR}/bgp_cn/bin/%s" % (self.executables["CNProc"],),
 	"ionproc": "${BASEDIR}/bgp_ion/bin/%s" % (self.executables["IONProc"],),
-	"storage": "/opt/lofar/bin/%s" % (self.executables["Storage"],),
+	"storage": "/opt/storage/current/bin/%s" % (self.executables["Storage"],),
 
         # where to start the executables. rundir needs to be reachable
         # for all sections.
