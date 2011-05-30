3eb390141ace53395daa8a37b353925a87e6f80f
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-05-30 06:48:47 +0000

    Bug 1698: Creating root tag for MAC release branch MAC-20110530-1_0-release-branch

---
+++ tags/MAC-20110530-1_0-release-root/CEP/Calibration/BBSKernel/src/Solver.cc
@@ -0,0 +1,115 @@
+//# Solver.cc:
+//#
+//# Copyright (C) 2007
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#include <lofar_config.h>
+#include <BBSKernel/Solver.h>
+
+#include <Common/LofarTypes.h>
+#include <Common/LofarLogger.h>
+#include <Common/StreamUtil.h>
+#include <Common/Timer.h>
+#include <Common/lofar_iostream.h>
+
+namespace LOFAR
+{
+namespace BBS
+{
+using LOFAR::operator<<;
+
+SolverOptions::SolverOptions()
+    :   maxIter(0),
+        epsValue(1e-8),
+        epsDerivative(1e-8),
+        colFactor(1e-6),
+        lmFactor(1e-3),
+        balancedEq(false),
+        useSVD(false)
+{
+}
+
+ostream& operator<<(ostream& os, const SolverOptions& obj)
+{
+    os << "Solver options:";
+    Indent id;
+    os << endl << indent << "Max nr. of iterations: " << obj.maxIter
+        << endl << indent << "Epsilon value: " << obj.epsValue
+        << endl << indent << "Epsilon derivative: " << obj.epsDerivative
+        << endl << indent << "Colinearity factor: " << obj.colFactor
+        << endl << indent << "LM factor: " << obj.lmFactor
+        << boolalpha
+        << endl << indent << "Balanced equations: " << obj.balancedEq
+        << endl << indent << "Use SVD: " << obj.useSVD
+        << noboolalpha;
+
+    return os;
+}
+
+Solver::Solver()
+{
+    SolverOptions options;
+    reset(options);
+}
+
+Solver::Solver(const SolverOptions &options)
+{
+    reset(options);
+}
+
+void Solver::reset(const SolverOptions &options)
+{
+    itsMaxIter = options.maxIter;
+    itsEpsValue = options.epsValue;
+    itsEpsDerivative = options.epsDerivative;
+    itsColFactor = options.colFactor;
+    itsLMFactor = options.lmFactor;
+    itsBalancedEq = options.balancedEq;
+    itsUseSVD = options.useSVD;
+
+    itsCells.clear();
+    itsCoeffIndex.clear();
+    itsCoeffMapping.clear();
+}
+
+void Solver::setCoeffIndex(size_t kernelId, const CoeffIndex &local)
+{
+    vector<casa::uInt> &mapping = itsCoeffMapping[kernelId];
+    mapping.resize(local.getCoeffCount());
+    for(CoeffIndex::const_iterator it = local.begin(), end = local.end();
+        it != end; ++it)
+    {
+        const CoeffInterval &interval =
+            itsCoeffIndex.insert(it->first, it->second.length);
+
+        for(size_t i = 0; i < interval.length; ++i)
+        {
+            mapping[it->second.start + i] = interval.start + i;
+        }
+    }
+}
+
+CoeffIndex Solver::getCoeffIndex() const
+{
+    return itsCoeffIndex;
+}
+
+} // namespace BBS
+} // namespace LOFAR
---
+++ tags/MAC-20110530-1_0-release-root/LCS/MSLofar/src/makebeamtables.cc
@@ -0,0 +1,72 @@
+//# makebeamtables.cc: Program 
+//# Copyright (C) 2011
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+//#
+//# @author Ger van Diepen
+
+#include <lofar_config.h>
+
+#include <MSLofar/BeamTables.h>
+#include <casa/Inputs.h>
+#include <ms/MeasurementSets/MeasurementSet.h>
+
+using namespace LOFAR;
+using namespace casa;
+
+int main (int argc, char* argv[])
+{
+  try {
+    Input inputs(1);
+    // define the input structure
+    inputs.version("2011Mar31-GvD");
+    inputs.create ("ms", "",
+		   "Name of MeasurementSet",
+		   "string");
+    inputs.create ("antennaset", "",
+		   "Antenna set used (e.g. LBA_INNER)",
+		   "string");
+    inputs.create ("antennasetfile", "/opt/cep/lofar/share/AntennaSets.conf",
+		   "Name of the AntennaSet file",
+		   "string");
+    inputs.create ("antennafielddir", "/opt/cep/lofar/share/AntennaFields",
+		   "Directory where the AntennaField.conf files reside",
+		   "string");
+    inputs.create ("ihbadeltadir", "/opt/cep/lofar/share/iHBADeltas",
+		   "Directory where the iHBADelta.conf files reside",
+		   "string");
+    inputs.create ("overwrite", "false",
+                   "Overwriting existing beam subtables?"
+                   "bool");
+    inputs.readArguments (argc, argv);
+    String msName      = inputs.getString("ms");
+    String antSet      = inputs.getString("antennaset");
+    String antSetFile  = inputs.getString("antennasetfile");
+    String antFieldDir = inputs.getString("antennafielddir");
+    String hbaDeltaDir = inputs.getString("ihbadeltadir");
+    Bool   overwrite   = inputs.getBool  ("overwrite");
+    MeasurementSet ms(msName, Table::Update);
+    BeamTables::create (ms, overwrite);
+    BeamTables::fill   (ms, antSet, antSetFile, antFieldDir, hbaDeltaDir, true);
+  } catch (std::exception& x) {
+    cout << "Unexpected exception: " << x.what() << endl;
+    return 1;
+  }
+  return 0;
+}
