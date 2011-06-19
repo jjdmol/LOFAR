660a52064282e757cf1f470eb597c1f286adace5
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-06-19 09:19:39 +0000

    bug 1315: test beamcript for aartfaac project with 240 beams

--- trunk/LCU/StationTest/test/beamformertest/beamaartfaac.sh
+++ trunk/LCU/StationTest/test/beamformertest/beamaartfaac.sh
@@ -1,7 +1,7 @@
 # beamaartfaac.sh
 # beamformer script for 96 rcu's in LBA_OUTER mode (with splitter OFF)
-# object zenith, 48 beams of 5 subbands around 60MHz
-# version 1.0,  16 jun 2011, M.J.Norden
+# object zenith, 240 beams, 48 antennas with 5 subbands around 60MHz
+# version 1.1,  17 jun 2011, M.J.Norden
 
 
 rspctl --wg=0
@@ -15,12 +15,25 @@ swlevel 3
 sleep 2
 rspctl --splitter=0
 sleep 3
-
+let r=0
 for (( i=0; i<=239; i=i+5 ))
   do 
-     let b=i+4
-     echo "Define beam and wait 1 seconds"
-     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=0:95 --subbands=306:310 --beamlets=$i:$b --digdir=0,1.5708,AZELGEO&
-     sleep 1 
+     let a=(2*$i)/5
+     let b=$a+1
+     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=$a:$b --subbands=306 --beamlets=$r --digdir=0,1.5708,AZELGEO&
+     sleep 2
+     let r=$r+1
+     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=$a:$b --subbands=307 --beamlets=$r --digdir=0,1.5708,AZELGEO&
+     sleep 2
+     let r=$r+1
+     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=$a:$b --subbands=308 --beamlets=$r --digdir=0,1.5708,AZELGEO&
+     sleep 2
+     let r=$r+1
+     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=$a:$b --subbands=309 --beamlets=$r --digdir=0,1.5708,AZELGEO&
+     sleep 2
+     let r=$r+1
+     beamctl --antennaset=LBA_OUTER --rcumode=2 --rcus=$a:$b --subbands=310 --beamlets=$r --digdir=0,1.5708,AZELGEO&
+     sleep 2
+     let r=$r+1
   done
 
