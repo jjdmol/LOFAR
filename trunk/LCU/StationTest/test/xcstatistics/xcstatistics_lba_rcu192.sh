d9096b93c5724b8f2496ea3f78fc641c742e1213
norden <norden@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-06-24 15:01:11 +0000

    bug 1315: Azelgeo

--- trunk/LCU/StationTest/test/xcstatistics/xcstatistics_lba_rcu192.sh
+++ trunk/LCU/StationTest/test/xcstatistics/xcstatistics_lba_rcu192.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# 1.0 xcstatistics test to check SerDes Ring with LBA antennas
-# 06-12-10, M.J Norden
-# HBA input with antenna
+# 1.1 xcstatistics test to check SerDes Ring with LBA antennas
+# 24-06-11, M.J Norden
+# LBA input with antenna
 
 killall beamctl
 rspctl --rcuprsg=0
@@ -10,7 +10,7 @@ rspctl --splitter=0
 
 swlevel 3
 sleep 5
-beamctl --antennaset=LBA_INNER --rcus=0:191 --rcumode=3 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZEL --digdir=0,1.5708,AZEL&
+beamctl --antennaset=LBA_INNER --rcus=0:191 --rcumode=3 --subbands=100:110 --beamlets=0:10 --anadir=0,1.5708,AZELGEO --digdir=0,1.5708,AZELGEO&
 sleep 5
 echo "check xcstat and xcangle"
 
