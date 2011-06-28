76773417345530c7dd3bde76d864e29dc94f0527
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-06-28 14:14:50 +0000

    Bug 1491: improving units of plots

--- CEP/DP3/AOFlagger/include/AOFlagger/strategy/plots/rfiplots.h
+++ CEP/DP3/AOFlagger/include/AOFlagger/strategy/plots/rfiplots.h
@@ -36,11 +36,11 @@ class RFIPlots{
 		static void MakePowerSpectrumPlot(class Plot &plot, Image2DCPtr image, Mask2DCPtr mask, TimeFrequencyMetaDataCPtr metaData);
 		static void MakeRMSSpectrumPlot(class Plot &plot, Image2DCPtr image, Mask2DCPtr mask);
 		static void MakeSNRSpectrumPlot(class Plot &plot, Image2DCPtr image, Image2DCPtr model, Mask2DCPtr mask);
-		static void MakePowerTimePlot(class Plot &plot, Image2DCPtr image, Mask2DCPtr mask);
+		static void MakePowerTimePlot(class Plot &plot, Image2DCPtr image, Mask2DCPtr mask, TimeFrequencyMetaDataCPtr metaData);
 		static void MakeComplexPlanePlot(class Plot &plot, const class TimeFrequencyData &data, size_t xStart, size_t length, size_t y, size_t yAvgSize, Mask2DCPtr mask, bool realVersusImaginary, bool imaginary);
 		static void MakeFittedComplexPlot(class Plot &plot, const TimeFrequencyData &data, size_t xStart, size_t length, size_t y, size_t yAvgSize, Mask2DCPtr mask, num_t sampleFringeFrequency, bool realVersusImaginary, bool imaginary);
-		static void MakeScatterPlot(class MultiPlot &plot, size_t plotIndex, Image2DCPtr image, Mask2DCPtr mask);
-		static void MakeScatterPlot(class MultiPlot &plot, const TimeFrequencyData &data);
+		static void MakeScatterPlot(class MultiPlot &plot, size_t plotIndex, Image2DCPtr image, Mask2DCPtr mask, TimeFrequencyMetaDataCPtr metaData);
+		static void MakeScatterPlot(class MultiPlot &plot, const TimeFrequencyData &data, TimeFrequencyMetaDataCPtr metaData);
 		static void MakeScatterPlot(class MultiPlot &plot, size_t plotIndex, SampleRowCPtr row);
 		static void MakeQualityPlot(class Plot &plot, const TimeFrequencyData &original, const TimeFrequencyData &model, size_t partCount);
 	private:
