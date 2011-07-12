d8d0c6b22ca82323f27ce754a97970cf0d5defd9
duscha <duscha@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-07-12 09:02:06 +0000

    Bug1561: GvD fixes for VisSet imaging columns addition error

--- branches/BBSKernel-Bug1561-task-branch/CEP/Calibration/BBSTools/src/addUV2MS.cc
+++ branches/BBSKernel-Bug1561-task-branch/CEP/Calibration/BBSTools/src/addUV2MS.cc
@@ -29,7 +29,7 @@
 // created.
 //
 // Any existing MODEL_DATA column is copied over to a temporary column,
-// MODEL_DATA_temp, and will be restored back to MODEL_DATA after the predictions.
+// MODEL_DATA_bak, and will be restored back to MODEL_DATA after the predictions.
 //
 // If MODEL_DATA_<patchname> columns are already present, overwrite or abort is
 // offered.
@@ -88,106 +88,106 @@ void usage(const char *);
 //
 int main(int argc, char *argv[])
 {
-    casa::String MSfilename;        // Filename of LafarMS
-    Vector<String> patchNames;      // vector with filenames of patches used as models
+  casa::String MSfilename;        // Filename of LafarMS
+  Vector<String> patchNames;      // vector with filenames of patches used as models
     
-    if(argc < 3)        // if not enough parameters given, display usage information
+  if(argc < 3)        // if not enough parameters given, display usage information
     {
-       usage(argv[0]);
-       exit(0);
+      usage(argv[0]);
+      exit(0);
     }
-    else
+  else
     {
-        MSfilename=argv[1];
-        for(int i=2; i < argc; i++)
+      MSfilename=argv[1];
+      for(int i=2; i < argc; i++)
         {
-            Int length;                          // Shape, i.e. length of Vector
-            patchNames.shape(length);            // determine length of Vector    
-            patchNames.resize(length+1, True);   // resize and copy values
-            patchNames[length]=argv[i];
+	  Int length;                          // Shape, i.e. length of Vector
+	  patchNames.shape(length);            // determine length of Vector    
+	  patchNames.resize(length+1, True);   // resize and copy values
+	  patchNames[length]=argv[i];
         }    
     }
-    if(MSfilename=="")
+  if(MSfilename=="")
     {
-        casa::AbortError("No MS filename given");         // raise exception
-        exit(0);
+      casa::AbortError("No MS filename given");         // raise exception
+      exit(0);
     }
-    if(patchNames.size()==0)
+  if(patchNames.size()==0)
     {
-        casa::AbortError("No patch image filename given");
-        exit(0);
+      casa::AbortError("No patch image filename given");
+      exit(0);
     }
     
-    //-------------------------------------------------------------------------------
-    // Do the work    
+  //-------------------------------------------------------------------------------
+  // Do the work    
     
-    // remove existing MODEL_DATA_temp or MODEL_DATA_<patch> columns (only for patches supplied as 
-    // parameter,other columns will be left untouched)
-    removeExistingColumns(MSfilename, patchNames);    
-    MeasurementSet LofarMS(MSfilename, Table::Update);          // Open LOFAR MS read/write
-    // Make sure the imager columns exist for the CASA ft function.
-    addImagerColumns (LofarMS);
-    LofarMS.flush();
+  // remove existing MODEL_DATA_<patch> columns (only for patches supplied as 
+  // parameter, other columns will be left untouched)
+  removeExistingColumns(MSfilename, patchNames);    
+  MeasurementSet LofarMS(MSfilename, Table::Update);          // Open LOFAR MS read/write
 
-    
-    // Remvoe any existing MODEL_DATA_temp (this should only happen because of an aborted/crashed previous run)
-    if(LofarMS.tableDesc().isColumn("MODEL_DATA_temp"))
-    {
-        LofarMS.removeColumn("MODEL_DATA_temp");
-    }
-    // Rename any existing MODEL_DATA.
-    if(LofarMS.tableDesc().isColumn("MODEL_DATA"))
-    {
-        LofarMS.renameColumn ("MODEL_DATA_temp", "MODEL_DATA");
-    }
-    
+  // In case a previous run failed, MODEL_DATA-bak may still exist
+  // and should be renamed to MODEL_DATA. An existing MODEL_DATA column
+  // is for a patch and should be removed.
+  if(LofarMS.tableDesc().isColumn("MODEL_DATA_bak"))
+  {
+      if(LofarMS.tableDesc().isColumn("MODEL_DATA"))
+      {
+	    LofarMS.removeColumn("MODEL_DATA");
+      }
+      LofarMS.renameColumn("MODEL_DATA", "MODEL_DATA_bak");
+  }
 
-    // Casarest Imager object which has ft method
-    // (last parameter casa::True "use MODEL_DATA column")
-    Imager imager(LofarMS, casa::True, casa::True);        // create an Imager object needed for predict with ft
-    Bool incremental=False;                                // create incremental UV data from models NO!    
+  // Make sure the imager columns exist for the CASA ft function.
+  addImagerColumns (LofarMS);
+  LofarMS.flush();
+
+  // Rename the existing MODEL_DATA.
+  if(LofarMS.tableDesc().isColumn("MODEL_DATA"))
+  {
+      LofarMS.renameColumn ("MODEL_DATA_bak", "MODEL_DATA");
+  }
  
-    // Loop over patchNames
-    for(unsigned int i=0; i < patchNames.size(); i++)
+  // Loop over patchNames
+  for(unsigned int i=0; i < patchNames.size(); i++)
     {
-        string columnName;              // columnName for uv data of this patch in table
-        Vector<String> model(1);        // we need a ft per model to write to each column
+      string columnName;              // columnName for uv data of this patch in table
+      Vector<String> model(1);        // we need a ft per model to write to each column
 
-        columnName=createColumnName(patchNames[i]);       
-        model[0]=patchNames[i];
+      columnName=createColumnName(patchNames[i]);       
+      model[0]=patchNames[i];
 
-        // Add the MODEL_DATA column for this patch.
-        // If already existing, rename to MODEL_DATA.
-        if (LofarMS.tableDesc().isColumn (columnName)) {
-          LofarMS.renameColumn ("MODEL_DATA", columnName);
-        } else {
-          addModelColumn(LofarMS, columnName);
-        }
-        LofarMS.flush();
+      // Add the MODEL_DATA column for this patch.
+      addModelColumn(LofarMS, columnName);
+      LofarMS.flush();
 
-        // Do a predict with the casarest ft() function, complist="", because we only use the model images
-        imager.ft(model, "", incremental);
+      //showColumnNames(LofarMS);
+      // Do a predict with the casarest ft() function, complist="", because we only use the model images
+      // Casarest Imager object which has ft method
+      // (last parameter casa::True "use MODEL_DATA column")
+      Imager imager(LofarMS, True, True);
+      imager.ft(model, "", False);
        
-        // rename MODEL_DATA column to MODEL_DATA_patchname column
-        LofarMS.renameColumn (columnName, "MODEL_DATA");
+      // rename MODEL_DATA column to MODEL_DATA_patchname column
+      LofarMS.renameColumn (columnName, "MODEL_DATA");
         
-        addDirectionKeyword(LofarMS, patchNames[i]);           
+      addDirectionKeyword(LofarMS, patchNames[i]);           
        
-        LofarMS.flush();
+      LofarMS.flush();
     }
     
     
-    // Rename temporary MODEL_DATA_temp column back to MODEL_DATA, if necessary
-    if(LofarMS.canRenameColumn("MODEL_DATA_temp"))
-    {
-        LofarMS.renameColumn ("MODEL_DATA", "MODEL_DATA_temp"); 
-    }
+  // Rename MODEL_DATA_bak column back to MODEL_DATA.
+  if(LofarMS.canRenameColumn("MODEL_DATA_bak"))
+  {
+    LofarMS.renameColumn ("MODEL_DATA", "MODEL_DATA_bak"); 
+  }
     
-    //-------------------------------------------------------------------------------
-    // Cleanup
-    //
-    LofarMS.flush();
-    LofarMS.closeSubTables();                            // close Lofar MS
+  //-------------------------------------------------------------------------------
+  // Cleanup
+  //
+  LofarMS.flush();
+  LofarMS.closeSubTables();                            // close Lofar MS
 }
 
 
@@ -197,20 +197,20 @@ int main(int argc, char *argv[])
 //casa::DirectionCoordinate::DirectionCoordinate getPatchDirection(const string &patchName)
 casa::Vector<casa::Double> getPatchDirection(const string &patchName)
 {
-    casa::IPosition imageShape;                             // shape of image
-    casa::Vector<casa::Double> Pixel(2);                    // pixel coords vector of image centre
-    casa::Vector<casa::Double> World(2);                    // world coords vector of image centre
-    casa::PagedImage<casa::Float> image(patchName);         // open image
+  casa::IPosition imageShape;                             // shape of image
+  casa::Vector<casa::Double> Pixel(2);                    // pixel coords vector of image centre
+  casa::Vector<casa::Double> World(2);                    // world coords vector of image centre
+  casa::PagedImage<casa::Float> image(patchName);         // open image
     
-    imageShape=image.shape();                               // get centre pixel
-    Pixel[0]=floor(imageShape[0]/2);
-    Pixel[1]=floor(imageShape[1]/2);
+  imageShape=image.shape();                               // get centre pixel
+  Pixel[0]=floor(imageShape[0]/2);
+  Pixel[1]=floor(imageShape[1]/2);
 
-    // Determine DirectionCoordinate
-    casa::DirectionCoordinate dir(image.coordinates().directionCoordinate (image.coordinates().findCoordinate(casa::Coordinate::DIRECTION)));
-    dir.toWorld(World, Pixel);
+  // Determine DirectionCoordinate
+  casa::DirectionCoordinate dir(image.coordinates().directionCoordinate (image.coordinates().findCoordinate(casa::Coordinate::DIRECTION)));
+  dir.toWorld(World, Pixel);
 
-    return World;
+  return World;
 }
 
 
@@ -220,16 +220,16 @@ casa::Vector<casa::Double> getPatchDirection(const string &patchName)
 //
 void addDirectionKeyword(casa::Table LofarTable, const string &patchName)
 {  
-    casa::Vector<casa::Double> direction;
-    string columnName=createColumnName(patchName);
+  casa::Vector<casa::Double> direction;
+  string columnName=createColumnName(patchName);
   
-    // write it to the columnDesc
-    casa::TableColumn LofarModelColumn(LofarTable, columnName);
-    casa::TableRecord &Model_keywords = LofarModelColumn.rwKeywordSet();
+  // write it to the columnDesc
+  casa::TableColumn LofarModelColumn(LofarTable, columnName);
+  casa::TableRecord &Model_keywords = LofarModelColumn.rwKeywordSet();
 
-    direction=getPatchDirection(patchName);
-    Model_keywords.define("Ra", direction[0]);
-    Model_keywords.define("Dec", direction[1]);
+  direction=getPatchDirection(patchName);
+  Model_keywords.define("Ra", direction[0]);
+  Model_keywords.define("Dec", direction[1]);
 }
 
 
@@ -238,17 +238,17 @@ void addDirectionKeyword(casa::Table LofarTable, const string &patchName)
 //
 string createColumnName(const string &ModelFilename)
 {
-    string columnName="MODEL_DATA_";
-    string patchName;
-    unsigned long pos=ModelFilename.find(".");       // remove .image or .img extension from Patchname
+  string columnName="MODEL_DATA_";
+  string patchName;
+  unsigned long pos=ModelFilename.find(".");       // remove .image or .img extension from Patchname
 
-    if(pos!=string::npos)                       // if we have a file suffix
-    {
-        patchName=ModelFilename.substr(0, pos); // remove it
-    }
-    columnName+=patchName.substr(0,pos);        // create complete column name according to scheme
+  if(pos!=string::npos)                       // if we have a file suffix
+  {
+      patchName=ModelFilename.substr(0, pos); // remove it
+  }
+  columnName+=patchName.substr(0,pos);        // create complete column name according to scheme
 
-    return columnName;
+  return columnName;
 }
 
 
@@ -257,39 +257,32 @@ string createColumnName(const string &ModelFilename)
 //
 void removeExistingColumns(const string &MSfilename, const Vector<String> &patchNames)
 {
-    string columnName;
-    casa::Table LofarTable(MSfilename, casa::Table::Update);     
+  string columnName;
+  casa::Table LofarTable(MSfilename, casa::Table::Update);     
 
-    // First rename MODEL_DATA column to MODEL_DATA_temp in case it already contains data   
-    //
-    if(LofarTable.tableDesc().isColumn("MODEL_DATA_temp"))
-    {
-        LofarTable.removeColumn("MODEL_DATA_temp");
-    }
+  // Remove existing Patchnames
+  //
+  for(unsigned int i=0; i < patchNames.size(); i++)      
+  {
+      columnName=createColumnName(patchNames[i]);        
+      if(LofarTable.tableDesc().isColumn(columnName))
+      {
+	    LofarTable.removeColumn(columnName);
+      }
+  }
 
-    // Remove existing Patchnames
-    //
-    for(unsigned int i=0; i < patchNames.size(); i++)      
-    {
-        columnName=createColumnName(patchNames[i]);        
-        if(LofarTable.tableDesc().isColumn(columnName))
-        {
-            LofarTable.removeColumn(columnName);
-        }
-    }
-
-    LofarTable.flush();
-    LofarTable.closeSubTables();
+  LofarTable.flush();
+  LofarTable.closeSubTables();
 }
 
 // Display usage info
 //
 void usage(const char *programname)
 {
-    cout << "Usage: " << programname << ": LofarMS <patchname[s]>" << endl;
-    cout << "LofarMS        - MS to add model data to" << endl;
-    cout << "<patchname[s]> - list of patchname[s] of image[s], these filenames are used to name the column and should be" << endl;
-    cout << "                 referred to in the skymodel file with .MS extension removed" << endl;
+  cout << "Usage: " << programname << ": LofarMS <patchname[s]>" << endl;
+  cout << "LofarMS        - MS to add model data to" << endl;
+  cout << "<patchname[s]> - list of patchname[s] of image[s], these filenames are used to name the column and should be" << endl;
+  cout << "                 referred to in the skymodel file with .MS extension removed" << endl;
 }
 
 
@@ -319,7 +312,7 @@ void addImagerColumns (MeasurementSet& ms)
     TiledColumnStMan stMan("TiledCorrectedData", dataTileShape);
     ms.addColumn (td, stMan);
   }
-  addModelColumn (ms, "TiledModelData");
+  ////  addModelColumn (ms, "TiledModelData");
   colName = MS::columnName(MS::IMAGING_WEIGHT);
   if (! ms.tableDesc().isColumn(colName)) {
     TableDesc td;
@@ -380,21 +373,21 @@ void addModelColumn (MeasurementSet& ms, const String& dataManName)
 //
 Vector<String> getColumnNames(const Table &table)
 {
-    TableDesc td=table.tableDesc();          // get TableDesc
-    return td.columnNames();
+  TableDesc td=table.tableDesc();          // get TableDesc
+  return td.columnNames();
 }
 
 // Display the column Names of the table
 //
 void showColumnNames(Table &table)
 {
-    Vector<String> columnNames;
+  Vector<String> columnNames;
     
-    columnNames=getColumnNames(table);
+  columnNames=getColumnNames(table);
     
-    for(Vector<String>::iterator it=columnNames.begin(); it!=columnNames.end(); ++it)
-    {
-        cout << *it << "\t";
-    }
-    cout << endl;
+  for(Vector<String>::iterator it=columnNames.begin(); it!=columnNames.end(); ++it)
+  {
+      cout << *it << "\t";
+  }
+  cout << endl;
 }
