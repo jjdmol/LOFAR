c577fc1a7c88d11a4296b7e383dd04e11489d24f
duscha <duscha@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-07-22 09:51:25 +0000

    Bug1561: fixed a few bugs in bbstests.sh

--- trunk/CEP/Calibration/BBSTools/scripts/bbstests.sh
+++ trunk/CEP/Calibration/BBSTools/scripts/bbstests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # BBS test script that runs individual testbbs python scripts
 #
@@ -10,77 +10,88 @@
 
 
 bbstestdir='/globaldata/bbs/tests'
-wd = '/data/scratch/bbstests'
-verbose = 0
+wd='/data/scratch/bbstests'
+verbosity=0
 
 
-function usage()
+usage()
 {
   echo "usage: ${0} <options> <tests>"
   echo "<options> are "
   echo "--wd          set working directory to execute tests in (default=/data/scratch/bbstests"
-  echo "--verbose     display verbose information on test progress"
+  echo "--verbosity   display verbose information on test progress"
   echo "--help        show this help information"
   echo "<tests> to perform"
   echo "calibration   perform a gain calibration on 3C196"
-  echo ""
+  echo "simulation    simulate a 3C196 two-source-model"
+  echo "directional   solve and substract for CasA, Cyga, substract these and correct for 3C196"
 }
 
 
 # Parse command line arguments
+args=$@     # keep the arguments
 while test $# -ne 0
 do
-  if [ test "${$1}" = "--wd" ]; then
-    if [ test $# -le 1 ]; then
+  if test ${1} = "--wd"; then
+    if test $# -le 1; then
         error "${1} needs an additional argument"
     fi
     shift
-    wd=${1}
+    wd=$1
     shift
-  elif [ test "${1}" = "--verbose" ]; then
-    verbose=1
+  elif test ${1} = "-v" -o "${1}" = "--verbose"; then
+    verbosity=1
     shift
-  elif [ test "${1}" = "--help" ]; then
+  elif test ${1} = "-h" -o "${1}" = "--help"; then
     usage
-    exit(0)
+    exit 0
+  else
+    shift
+  fi
 done
-
 
 # Check if tests working directory does exist
 if [ ! -d ${wd}  ]; then
-  if [ verbose = 1 ]; then
+  if [ ${verbosity} = 1 ]; then
     echo "mkdir ${wd}"
   fi
   mkdir ${wd}
 fi
 # MS directory containing reference Measurementsets used for tests
 if [ ! -d ${wd}/MS ]; then
-  if [ verbose = 1 ]; then
+  if [ ${verbosity} = 1 ]; then
     echo "mkdir ${wd}/MS"
   fi
   mkdir ${wd}/MS
 fi
 
+# Copy over MS files (if we have to many different MS files
+# we might prceed to copy only those needed for individual tests;
+# for now we copy the whole MS directory
+if [ {$verbosity} = 1 ]; then
+  echo "rsync -avz ${bbstestdir}/MS ${wd}/"
+  #rsync -avz ${bbstestdir}/MS ${wd}/
+else
+  echo ""
+  #rsync -az ${bbstestdir}/MS ${wd}/
+fi
+
+# DEBUG
+#for a in $args
+#do
+#  echo $a
+#done
+#exit 0
 
 # Loop over remaining command line arguments
 # to copy test files to working directory
-for arg in $@
+for arg in $args
 do
-  # Copy over MS files
-  if [ $verbose -eq 1 ]; then
-  
-    #rsync -avz ${bbstestdir}/MS ${wd}/
-  else
-    #rsync -az ${bbstestdir}/MS ${wd}/
-  fi
-
-
   if [ $arg = "all" ]; then
-    if [ ${verbose} -eq 1 ]; then 
+    if [ ${verbosity} -eq 1 ]; then 
       echo "rsync -avz ${bbstestdir}/simulation {$wd}/"
       echo "rsync -avz ${bbstestdir}/calibration {$wd}/"
       echo "rsync -avz ${bbstestdir}/directional {$wd}/"
-      echo "rsync -avz ${bbstestdir}/MS ${wd}/"
       #rsync -avz ${bbstestdir}/simulation {$wd}/
       #rsync -avz ${bbstestdir}/calibration {$wd}/
       #rsync -avz ${bbstestdir}/directional {$wd}/
@@ -89,21 +100,20 @@ do
     #rsync -az ${bbstestdir}/calibration {$wd}/
     #rsync -az ${bbstestdir}/directional {$wd}/
     break
-  if [ $arg = "calibration" ]; then
-    if [ ${verbose} -eq 1 ]; then 
+  elif [ $arg = "calibration" ]; then
+    if [ ${verbosity} -eq 1 ]; then 
       echo "rsync -avz ${bbstestdir}/calibration {$wd}/"
       #rsync -avz ${bbstestdir}/calibration {$wd}/
     fi
     #rsync -az ${bbstestdir}/calibration {$wd}/
   elif [ $arg = "simulation" ]; then
-    if [ ${verbose} -eq 1 ]; then 
+    if [ ${verbosity} -eq 1 ]; then 
       echo "rsync -avz ${bbstestdir}/simulation {$wd}/"    
       #rsync -avz ${bbstestdir}/simulation {$wd}/
     fi
     #rsync -az ${bbstestdir}/simulation {$wd}/
-  fi
   elif [ $arg = "directional" ]; then
-    if [ ${verbose} -eq 1 ]; then 
+    if [ ${verbosity} -eq 1 ]; then 
       echo "rsync -avz ${bbstestdir}/directional {$wd}/"    
       #rsync -avz ${bbstestdir}/directional {$wd}/
     fi
@@ -116,7 +126,7 @@ done
 for arg in $@
 do
   if [ $arg = "all" ]; then
-    if [ ${verbose} -eq 1 ]; then 
+    if [ ${verbosity} -eq 1 ]; then 
       echo "$wd}/calibration/testBBS_3C196_calibration.py"
       echo "$wd}/simulation/testBBS_3C196_simulation.py"
       echo "{$wd}/directional/testBBS_3C196_direction.py"
