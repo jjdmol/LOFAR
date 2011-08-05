93b7a9bcedccec4a30a506f6ee54b2264553ec30
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-08-05 15:49:00 +0000

    Bug 1713: Latest update of calibrator_pipeline. Stil not fully functional, yet.

--- branches/CEP-Pipeline-Bug1713-task-branch/CEP/Pipeline/recipes/sip/master/calibrator_pipeline.py
+++ branches/CEP-Pipeline-Bug1713-task-branch/CEP/Pipeline/recipes/sip/master/calibrator_pipeline.py
@@ -10,6 +10,7 @@ import sys
 
 from lofarpipe.support.control import control
 from lofar.parameterset import parameterset
+from lofarpipe.support.parset import Parset
 
 class calibrator_pipeline(control):
     """
@@ -31,66 +32,11 @@ class calibrator_pipeline(control):
         control.__init__(self)
         self.parset = parameterset()
 
+
     def usage(self):
         print >> sys.stderr, "Usage: %s [options] <parset-file>" % sys.argv[0]
         return 1
 
-    def pipeline_logic(self):
-        """
-        Define the individual tasks that comprise the current pipeline.
-        This method will be invoked by the base-class's `go()` method.
-        """
-
-        # Create a parameter-subset containing only python-control stuff.
-        py_parset = self.parset.makeSubset(
-            'ObsSW.Observation.ObservationControl.PythonControl.')
-
-        # Generate a datamap-file, which is a parset-file containing
-        # key/value pairs of hostname and list of MS-files.
-        mapfile = self.run_task(
-            "cep2_datamapper",
-            observation_dir=py_parset.getString('observationDirectory')
-        )['mapfile']
-
-        # Create an empty parmdb for DPPP
-        self.run_task("parmdb", mapfile)
-
-        # Create an empty sourcedb for DPPP
-        sky_mapfile = self.run_task("sourcedb", mapfile)['sky_mapfile']
-
-        # Produce a GVDS file describing the data on the compute nodes.
-        self.run_task("vdsmaker", mapfile)
-
-        # Read metadata (start, end times, pointing direction) from GVDS.
-        vdsinfo = self.run_task("vdsreader")
-
-        # Create a parameter-subset for DPPP and write it to file.
-        ndppp_parset = os.path.join(
-            self.config.get("layout", "job_directory"),
-            "parsets", "NDPPP.parset")
-        py_parset.makeSubset('DPPP.').writeFile(ndppp_parset)
-
-        # Run the Default Pre-Processing Pipeline (DPPP);
-        mapfile = self.run_task(
-            "ndppp", mapfile,
-            data_start_time=vdsinfo['start_time'],
-            data_end_time=vdsinfo['end_time'],
-            parset=ndppp_parset
-        )['mapfile']
-
-        # Demix the relevant A-team sources
-        self.run_task("demixing", mapfile)
-
-        return 0
-
-        # Create a parameter-subset for BBS and write it to file.
-        bbs_parset = os.path.join(
-            self.config.get("layout", "job_directory"),
-            "parsets", "BBS.parset")
-        py_parset.makeSubset('BBS.').writeFile(bbs_parset)
-
-        # Run BBS to calibrate the calibrator source(s).
-        self.run_task("bbs")
 
     def go(self):
         """
@@ -109,6 +55,81 @@ class calibrator_pipeline(control):
                 os.path.splitext(os.path.basename(parset_file))[0]
             )
         super(calibrator_pipeline, self).go()
+
+
+    def pipeline_logic(self):
+        """
+        Define the individual tasks that comprise the current pipeline.
+        This method will be invoked by the base-class's `go()` method.
+        """
+
+        # Create a parameter-subset containing only python-control stuff.
+        py_parset = self.parset.makeSubset(
+            'ObsSW.Observation.ObservationControl.PythonControl.')
+
+        # Generate a datamap-file, which is a parset-file containing
+        # key/value pairs of hostname and list of MS-files.
+        data_mapfile = self.run_task(
+            "cep2_datamapper",
+            observation_dir=py_parset.getString('observationDirectory')
+        )['mapfile']
+
+        # Create an empty parmdb for DPPP
+        parmdb_mapfile = self.run_task("parmdb", data_mapfile)['mapfile']
+
+        # Create an empty sourcedb for DPPP
+        sourcedb_mapfile = self.run_task("sourcedb", data_mapfile)['mapfile']
+
+        # Produce a GVDS file describing the data on the compute nodes.
+        gvds_file = self.run_task("vdsmaker", data_mapfile)['gvds']
+
+        # Read metadata (start, end times, pointing direction) from GVDS.
+        vdsinfo = self.run_task("vdsreader", gvds=gvds_file)
+
+        # Create a parameter-subset for DPPP and write it to file.
+        ndppp_parset = os.path.join(
+            self.config.get("layout", "job_directory"),
+            "parsets", "NDPPP.parset")
+        py_parset.makeSubset('DPPP.').writeFile(ndppp_parset)
+
+        # Run the Default Pre-Processing Pipeline (DPPP);
+        data_mapfile = self.run_task(
+            "ndppp", data_mapfile,
+            data_start_time=vdsinfo['start_time'],
+            data_end_time=vdsinfo['end_time'],
+            parset=ndppp_parset
+        )['mapfile']
+
+        #data_map = parameterset(data_mapfile)
+        #parmdb_map = parameterset(parmdb_mapfile)
+        #sourcedb_map = parameterset(sourcedb_mapfile)
+
+        #bbs_map = {}
+        #for key in data_map.keys():
+            #bbs_map[key] = zip(
+                #data_map.getStringVector(key),
+                #parmdb_map.getStringVector(key),
+                #sourcedb_map.getStringVector(key)
+            #)
+
+        ## Demix the relevant A-team sources
+        #self.run_task("demixing", mapfile)
+
+        #return 0
+
+        # Create a parameter-subset for BBS and write it to file.
+        bbs_parset = os.path.join(
+            self.config.get("layout", "job_directory"),
+            "parsets", "BBS.parset")
+        py_parset.makeSubset('BBS.').writeFile(bbs_parset)
+
+        # Run BBS to calibrate the calibrator source(s).
+        self.run_task(
+            "new_bbs", data_mapfile,
+            parset=bbs_parset,
+            gvds_file=gvds_file,
+            instrument_mapfile=parmdb_mapfile,
+            sky_mapfile=sourcedb_mapfile)
 
 if __name__ == '__main__':
     sys.exit(calibrator_pipeline().main())
