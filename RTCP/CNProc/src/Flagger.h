837a333390e57e538c76e1d12b99c6cf469674c6
Rob V. van Nieuwpoort <R.vanNieuwpoort@esciencecenter.nl>
2011-08-15 13:30:04 +0000

    Bug 1198: online flagging refactor, sumthreshold fix

--- RTCP/CNProc/src/Flagger.h
+++ RTCP/CNProc/src/Flagger.h
@@ -1,7 +1,12 @@
 #ifndef LOFAR_CNPROC_FLAGGER_H
 #define LOFAR_CNPROC_FLAGGER_H
 
+#include <Interface/MultiDimArray.h>
+
+#include <FlaggerHistory.h>
+
 #include <string>
+#include <vector>
 
 namespace LOFAR {
 namespace RTCP {
@@ -29,8 +34,23 @@ private:
   void oneDimensionalConvolution(const float* data, const unsigned dataSize, float*dest, const float* kernel, const unsigned kernelSize);
 
 protected:
+
+  // Does simple thresholding.
+  void thresholdingFlagger1D(std::vector<float>& powers, std::vector<bool>& flags);
+  void thresholdingFlagger2D(const MultiDimArray<float,2> &powers, MultiDimArray<bool,2> &flags);
+
+  // Does sum thresholding.
+  void sumThresholdFlagger1D(std::vector<float>& powers, std::vector<bool>& flags, const float sensitivity);
+
+  // Does sum thresholding on samples, a gaussion smooth, calculates difference, and flags again.
+  void sumThresholdFlaggerSmoothed1D(std::vector<float>& powers, std::vector<float>& smoothedPowers, std::vector<float>& powerDiffs, std::vector<bool>& flags);
+
+  void sumThresholdFlaggerSmoothedWithHistory1D(std::vector<float>& powers, std::vector<float>& smoothedPowers, std::vector<float>& powerDiffs, 
+						std::vector<bool>& flags, HistoryList& history);
+
   void calculateStatistics(const float* data, const unsigned size, float& mean, float& median, float& stdDev);
   float calcThresholdI(float threshold1, unsigned window, float p);
+  void sumThreshold(std::vector<float>& powers, std::vector<bool>& flags, const unsigned window, const float threshold);
   void oneDimensionalGausConvolution(const float* data, const unsigned dataSize, float*dest, const float sigma);
   void calculateStdDevAndSum(const float* data, const unsigned size, const float mean, float& stdDev, float& sum);
   float calculateMedian(const float* data, const unsigned size);
