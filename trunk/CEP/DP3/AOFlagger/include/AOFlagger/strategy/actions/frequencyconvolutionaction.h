bad76bfc17c324b022cf22ba73a121322c61478d
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-09-20 17:21:47 +0000

    Bug 1491: testing some other convolution techniques

--- trunk/CEP/DP3/AOFlagger/include/AOFlagger/strategy/actions/frequencyconvolutionaction.h
+++ trunk/CEP/DP3/AOFlagger/include/AOFlagger/strategy/actions/frequencyconvolutionaction.h
@@ -27,6 +27,7 @@
 #include <AOFlagger/imaging/uvimager.h>
 
 #include <AOFlagger/util/aologger.h>
+#include <AOFlagger/util/progresslistener.h>
 
 #include <AOFlagger/strategy/actions/action.h>
 
@@ -40,9 +41,9 @@ namespace rfiStrategy {
 	class FrequencyConvolutionAction : public Action
 	{
 		public:
-			enum KernelKind { RectangleKernel, SincKernel };
+			enum KernelKind { RectangleKernel, SincKernel, TotalKernel };
 			
-			FrequencyConvolutionAction() : Action(), _kernelKind(RectangleKernel), _convolutionSize(2.0), _inSamples(false)
+			FrequencyConvolutionAction() : Action(), _kernelKind(TotalKernel), _convolutionSize(4.0), _inSamples(false)
 			{
 			}
 			virtual std::string Description()
@@ -50,25 +51,38 @@ namespace rfiStrategy {
 				return "Frequency convolution";
 			}
 			virtual ActionType Type() const { return FrequencyConvolutionActionType; }
-			virtual void Perform(ArtifactSet &artifacts, class ProgressListener &)
+			virtual void Perform(ArtifactSet &artifacts, class ProgressListener &listener)
 			{
 				TimeFrequencyData &data = artifacts.ContaminatedData();
-				if(data.ImageCount() != 1)
-					throw std::runtime_error("A frequency convolution can only be applied on single component data");
-
-				Image2DPtr newImage;
-				switch(_kernelKind)
+				if(_kernelKind == TotalKernel)
 				{
-					default:
-					case RectangleKernel:
-					newImage = ThresholdTools::FrequencyRectangularConvolution(data.GetImage(0), (unsigned) roundn(_convolutionSize));
-					break;
-					case SincKernel:
-					newImage = sincConvolution(artifacts.MetaData(), data.GetImage(0));
-					break;
+					Image2DPtr
+						rImage = Image2D::CreateCopy(data.GetImage(0)),
+						iImage = Image2D::CreateCopy(data.GetImage(1));
+					Convolve(rImage, iImage, artifacts.MetaData(), listener);
+					data.SetImage(0, rImage);
+					data.SetImage(1, iImage);
+					artifacts.SetRevisedData(data);
 				}
-				
-				data.SetImage(0, newImage);
+				else
+				{
+					if(data.ImageCount() != 1)
+						throw std::runtime_error("A frequency convolution can only be applied on single component data");
+
+					Image2DPtr newImage;
+					switch(_kernelKind)
+					{
+						default:
+						case RectangleKernel:
+						newImage = ThresholdTools::FrequencyRectangularConvolution(data.GetImage(0), (unsigned) roundn(_convolutionSize));
+						break;
+						case SincKernel:
+						newImage = sincConvolution(artifacts.MetaData(), data.GetImage(0));
+						break;
+					}
+					
+					data.SetImage(0, newImage);
+				}
 			}
 			
 			numl_t ConvolutionSize() const { return _convolutionSize; }
@@ -123,6 +137,64 @@ namespace rfiStrategy {
 					return _convolutionSize;
 				else
 					return _convolutionSize * height / uvDist;
+			}
+			
+			void Convolve(Image2DPtr rImage, Image2DPtr iImage, TimeFrequencyMetaDataCPtr metaData, ProgressListener &listener)
+			{
+				Image2DPtr copyReal = Image2D::CreateCopy(rImage);
+				Image2DPtr copyImag = Image2D::CreateCopy(iImage);
+				const size_t
+					width = rImage->Width(),
+					height = rImage->Height();
+				const std::vector<UVW> &uvws = metaData->UVW();
+				
+				const num_t factor = M_PIn / _convolutionSize;
+				
+				for(size_t y=0;y<height;++y)
+				{
+					for(size_t x=0;x<width;++x)
+					{
+						const UVW uvw = uvws[x];
+						const num_t sol = UVImager::SpeedOfLight();
+						const double freq1 = metaData->Band().channels[y].frequencyHz / sol;
+						const num_t
+							u1 = uvw.u * freq1,
+							v1 = uvw.v * freq1;
+						//	w1 = uvw.w * freq1;
+						num_t real = 0.0, imaginary = 0.0;
+						
+						for(size_t yi=y;yi<y+1;++yi) //0;yi<height;++yi)
+						{
+							const double freq2 = metaData->Band().channels[yi].frequencyHz / sol;
+							for(size_t xi=0;xi<width;++xi)
+							{
+								if(xi == x && yi == y)
+								{
+									real += copyReal->Value(xi, yi);
+									imaginary += copyImag->Value(xi, yi);
+								} else {
+									const UVW uvwi = uvws[xi];
+									const num_t
+										du = uvwi.u * freq2 - u1,
+										dv = uvwi.v * freq2 - v1;
+										//dw = uvwi.w * freq2 - w1;
+									const num_t dist = sqrtn(du*du + dv*dv); // + dw*dw
+									const num_t sincVal = sinn(dist * factor) / (dist * factor);
+									/*if(xi == 0)
+									{
+										AOLogger::Debug << dist << '*' << factor << " -> " << sincVal << '\n';
+									}*/
+									
+									real += sincVal * copyReal->Value(xi, yi);
+									imaginary += sincVal * copyImag->Value(xi, yi);
+								}
+							}
+						}
+						rImage->SetValue(x, y, real / sqrtn(width));
+						iImage->SetValue(x, y, imaginary / sqrtn(width));
+					}
+					listener.OnProgress(*this, y+1, height);
+				}
 			}
 			
 			enum KernelKind _kernelKind;
