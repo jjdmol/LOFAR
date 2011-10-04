5720eefbdfe510a6f656b9391f52570935c4b967
donker <donker@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-10-04 09:19:52 +0000

    Bug 335: sending of ARP messages disabled)

--- branches/MAC-20110530-1_0-release-branch/MAC/APL/PIC/TBB_Driver/src/DriverSettings.h
+++ branches/MAC-20110530-1_0-release-branch/MAC/APL/PIC/TBB_Driver/src/DriverSettings.h
@@ -35,7 +35,7 @@ namespace LOFAR {
 	using GCF::TM::GCFPortInterface;
 	namespace TBB {
 
-static const int DRIVER_VERSION = 238;
+static const int DRIVER_VERSION = 239;
 
 enum BoardStateT {noBoard,
 				  setImage1, image1Set,
