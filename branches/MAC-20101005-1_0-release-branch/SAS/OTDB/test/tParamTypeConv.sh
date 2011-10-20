b939a78eed1b9dda351c174789f207563e598989
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-10-20 11:20:24 +0000

    Task #2649: Clean up of old branches and tags

--- branches/MAC-20101005-1_0-release-branch/SAS/OTDB/test/tParamTypeConv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# do a hard copy until a variable is available
-cp $srcdir/DATABASENAME .
-./runctest.sh tParamTypeConv 2>&1 > tParamTypeConv.log
-rm -f DATABASENAME
--- branches/MAC-20101005-1_0-release-branch/SAS/OTDB/test/tVHvalue.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# do a hard copy until a variable is available
-cp ../../../test/DATABASENAME .
-./runctest.sh tVHvalue 2>&1 > tVHvalue_test.log
-rm -f DATABASENAME
