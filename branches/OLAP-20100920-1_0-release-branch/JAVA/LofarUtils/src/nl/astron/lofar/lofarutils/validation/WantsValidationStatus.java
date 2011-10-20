7d2dde46e81942c7e8f511e7e81dd041f6157870
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-10-20 15:50:10 +0000

    Task #2649: Clean up of old branches and tags

--- branches/OLAP-20100920-1_0-release-branch/JAVA/LofarUtils/src/nl/astron/lofar/lofarutils/validation/WantsValidationStatus.java
+++
@@ -1,7 +0,0 @@
-package nl.astron.lofar.lofarutils.validation;
-
-public interface WantsValidationStatus {
-    void validateFailed();  // Called when a component has failed validation.
-    void validatePassed();  // Called when a component has passed validation.
-}
-
