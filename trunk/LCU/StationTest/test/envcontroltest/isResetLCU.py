586066bc213a4c9cf833510452e4e99c72a7b9a8
donker <donker@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-11-07 13:00:12 +0000

    Task #1723 add header info

--- trunk/LCU/StationTest/test/envcontroltest/isResetLCU.py
+++ trunk/LCU/StationTest/test/envcontroltest/isResetLCU.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
+## "isResetLCU.py"
+## RESET LCU power on IS (international station)
+## can only be used on IS LCU
+##
+## USE ONLY IF NORMAL SHUTDOWN IS NOT POSSIBLE
+## usage: ./isResetLCU.py
+##
+## Author: Pieter Donker (ASTRON)
+## Last change: november 2011 
+
 from isEcLib import *
 import sys
 import time
 
-VERSION = '1.0.0' # version of this script    
+VERSION = '1.1.0' # version of this script    
 
 # used variables
 version = 0   # EC version
@@ -25,6 +35,8 @@ if __name__ == '__main__':
         time.sleep(1.0)
         ec.printInfo(True)
         ec.getPowerStatus()
+        print "Turn off the mains voltage for 10 seconds"
+        print "Use only if normal shutdown in not possible"
         if 'yes' == raw_input("Do you realy want to cycle LCU power [yes/no] : "):
             print
             print "================================"
