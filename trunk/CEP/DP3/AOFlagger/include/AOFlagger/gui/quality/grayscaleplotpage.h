9b668d5a8be855a38decf0944c00443bdb7ee28c
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-11-30 09:29:14 +0000

    Task #1892: adding plot properties window to grayscale plots

--- trunk/CEP/DP3/AOFlagger/include/AOFlagger/gui/quality/grayscaleplotpage.h
+++ trunk/CEP/DP3/AOFlagger/include/AOFlagger/gui/quality/grayscaleplotpage.h
@@ -55,7 +55,7 @@ class GrayScalePlotPage : public Gtk::HBox {
 		void initStatisticKinds();
 		void initPolarizations();
 		void initPhaseButtons();
-		void initRanges();
+		void initPlotOptions();
 		
 		void onSelectCount() { _selectStatisticKind = QualityTablesFormatter::CountStatistic; UpdateImage(); }
 		void onSelectMean() { _selectStatisticKind = QualityTablesFormatter::MeanStatistic; UpdateImage(); }
@@ -65,6 +65,7 @@ class GrayScalePlotPage : public Gtk::HBox {
 		void onSelectDVariance() { _selectStatisticKind = QualityTablesFormatter::DVarianceStatistic; UpdateImage(); }
 		void onSelectRFIRatio() { _selectStatisticKind = QualityTablesFormatter::RFIRatioStatistic; UpdateImage(); }
 		void onSelectSNR() { _selectStatisticKind = QualityTablesFormatter::SignalToNoiseStatistic; UpdateImage(); }
+		void onPropertiesClicked();
 		
 		void onSelectMinMaxRange() { _imageWidget.SetRange(ImageWidget::MinMax); _imageWidget.Update(); }
 		void onSelectWinsorizedRange() { _imageWidget.SetRange(ImageWidget::Winsorized); _imageWidget.Update(); }
@@ -98,16 +99,19 @@ class GrayScalePlotPage : public Gtk::HBox {
 		
 		Gtk::RadioButton _amplitudePhaseButton, _phasePhaseButton, _realPhaseButton, _imaginaryPhaseButton;
 		
-		Gtk::Frame _rangeFrame;
-		Gtk::VBox _rangeBox;
+		Gtk::Frame _plotFrame;
+		Gtk::VBox _plotBox;
 		
 		Gtk::RadioButton _rangeMinMaxButton, _rangeWinsorizedButton, _rangeSpecified;
 		Gtk::CheckButton _logarithmicScaleButton;
+		Gtk::Button _plotPropertiesButton;
 		
 		QualityTablesFormatter::StatisticKind _selectStatisticKind;
 		ImageWidget _imageWidget;
 		
 		bool _ready;
+		
+		class ImagePropertiesWindow *_imagePropertiesWindow;
 };
 
 #endif
