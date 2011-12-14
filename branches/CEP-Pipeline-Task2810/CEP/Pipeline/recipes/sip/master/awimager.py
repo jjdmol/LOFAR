61713584b12e71b7134fe20388b49c8833c7696f
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-12-14 13:38:29 +0000

    Task #2810: Add awimager master and node scripts

---
+++ branches/CEP-Pipeline-Task2810/CEP/Pipeline/recipes/sip/master/awimager.py
@@ -0,0 +1,98 @@
+#                                                         LOFAR IMAGING PIPELINE
+#
+#                                    Example recipe with simple job distribution
+#                                                            John Swinbank, 2010
+#                                                      swinbank@transientskp.org
+# ------------------------------------------------------------------------------
+
+import os
+import sys
+import collections
+import lofarpipe.support.lofaringredient as ingredient
+from lofarpipe.support.baserecipe import BaseRecipe
+from lofarpipe.support.remotecommand import RemoteCommandRecipeMixIn
+from lofarpipe.support.remotecommand import ComputeJob
+from lofarpipe.support.group_data import load_data_map
+
+class AWImager(BaseRecipe, RemoteCommandRecipeMixIn):
+    """
+    Run the AWImager on the nodes and the data files suplied in the mapfile
+    **Arguments**
+    A mapfile containing node->datafile pairs
+
+    """
+    inputs = {
+        'executable': ingredient.ExecField(
+            '--executable',
+            help="The full path to the relevant AWImager executable"
+        ),
+        'initscript': ingredient.FileField(
+            '--initscript',
+            help='''The full path to an (Bourne) shell script which will\
+             intialise the environment (ie, ``lofarinit.sh``)'''
+        ),
+        'parset': ingredient.FileField(
+            '-p', '--parset',
+            help="The full path to a AWImager configuration parset."
+        ),
+        'working_directory': ingredient.StringField(
+            '-w', '--working-directory',
+            help="Working directory used on outpuconfigt nodes. Results location"
+        ),
+        'suffix': ingredient.StringField(
+            '--suffix',
+            default=".awimager",
+            help="Added to the input filename to generate the output filename"
+        )  
+    }
+                  
+    def go(self): 
+        self.logger.info("Starting AWImager run")     
+        super(AWImager, self).go()
+              
+        # Load datafile containing node --> dataset pairs     
+        self.logger.debug("Loading map from %s" % self.inputs['args'])
+        data = load_data_map(self.inputs['args'][0])  
+        
+        # Output
+        outnames = collections.defaultdict(list)
+        
+        # Compile the command to be executed on the remote machine
+        nodeCommand = "python %s" % (self.__file__.replace("master", "nodes"))
+        
+        # Environment type variables
+        executable = self.inputs['executable']  #absolute path, daily build
+        initScript = self.inputs['initscript']
+        parset = self.inputs['parset']                  #Using parameter set             
+        workingDir = self.inputs['working_directory']     
+             
+
+        # Create the jobs
+        jobs = []
+        for host, measurementSet in data:
+            #construct and save the output name
+            outnames[host].append(os.path.join(
+                            self.inputs['working_directory'],
+                            self.inputs['job_name'],
+                            os.path.basename(measurementSet.rstrip('/')) + 
+                            self.inputs['suffix']))
+            
+            arguments=[executable, initScript, parset, workingDir, \
+                       measurementSet]
+            
+            jobs.append(ComputeJob(host, nodeCommand, arguments))
+        
+        # Hand over the job(s) to the pipeline scheduler
+        self._schedule_jobs(jobs)
+        
+        #todo: What to do with the output??
+        
+        # Test for errors
+        if self.error.isSet():
+            self.logger.warn("Failed AWImager run detected")
+            return 1
+        else:
+            return 0
+
+if __name__ == "__main__":
+    sys.exit(AWImager().main())
