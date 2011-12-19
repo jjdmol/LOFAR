22759eeb6d09a707cdbf2bde1d07f060bfcdcba2
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-12-19 07:45:19 +0000

    Task #2810: Fix example scripts and add addition print to clarify functioning

--- branches/CEP-Pipeline-Task2810/CEP/Pipeline/recipes/examples/master/example_parallel.py
+++ branches/CEP-Pipeline-Task2810/CEP/Pipeline/recipes/examples/master/example_parallel.py
@@ -3,6 +3,10 @@
 #                                    Example recipe with simple job distribution
 #                                                            John Swinbank, 2010
 #                                                      swinbank@transientskp.org
+#
+#                                                Fixed example script
+#                                                             Wouter Klijn, 2012
+#                                                                klijn@astron.nl
 # ------------------------------------------------------------------------------
 
 import sys
@@ -12,9 +16,15 @@ from lofarpipe.support.remotecommand import ComputeJob
 
 class example_parallel(BaseRecipe, RemoteCommandRecipeMixIn):
     def go(self):
+        self.inputs["job_name"] = "example_parallel"              
         super(example_parallel, self).go()
+        node_name = "locus040"
+        print("\n\n\n\n***********************************************************")
+        print("Run script with option -v (verbose) or -d (debug) to validate functioning of the node script")
+        print("***********************************************************\n\n\n\n")
         node_command = "python %s" % (self.__file__.replace("master", "nodes"))
-        job = ComputeJob("localhost", node_command, arguments=["example_argument"])
+
+        job = ComputeJob(node_name, node_command, arguments=["CommandLineArgument"])
         self._schedule_jobs([job])
         if self.error.isSet():
             return 1
--- branches/CEP-Pipeline-Task2810/CEP/Pipeline/recipes/examples/nodes/example_parallel.py
+++ branches/CEP-Pipeline-Task2810/CEP/Pipeline/recipes/examples/nodes/example_parallel.py
@@ -3,18 +3,25 @@
 #                                                Example of a simple node script
 #                                                            John Swinbank, 2010
 #                                                      swinbank@transientskp.org
+#
+#                                                Fixed example script
+#                                                             Wouter Klijn, 2012
+#                                                                klijn@astron.nl
 # ------------------------------------------------------------------------------
 
 import sys
-from lofarpipe.support.lofarnode import LOFARnode
+from lofarpipe.support.lofarnode import LOFARnodeTCP
 
 class example_parallel(LOFARnodeTCP):
-    def run(self, *args):
-        for arg in args:
-            self.logger.info("Received %s as argument" % str(arg))
+    def run(self, *args):        
+        log4CPlusName = "example_parallel" 
+        command_line_argument = args[0]  
+            
+        self.logger.info(log4CPlusName)
+        self.logger.info(command_line_argument)
+
         return 0
 
 if __name__ == "__main__":
     jobid, jobhost, jobport = sys.argv[1:4]
     sys.exit(example_parallel(jobid, jobhost, jobport).run_with_stored_arguments())
-
