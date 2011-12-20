53f9a0e6c6f5b0a400acfa111f95d7dbbfacef72
coolen <coolen@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-12-20 14:22:28 +0000

    Task #2843: added ibeam objects

---
+++ branches/LOFAR-TiedArray-Task2843/SAS/OTB/OTB/src/nl/astron/lofar/sas/otb/util/IBeam.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.astron.lofar.sas.otb.util;
+
+/**
+ *
+ * @author coolen
+ */
+
+public interface IBeam {
+    // give type of beam
+    public String getType();   
+    
+}
