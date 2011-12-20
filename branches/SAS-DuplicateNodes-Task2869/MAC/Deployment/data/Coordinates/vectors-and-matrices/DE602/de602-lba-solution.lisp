76053bcb86c0685ff520773a37862d10faec4226
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-12-20 09:55:30 +0000

    Task #2869: Copy of trunk.

---
+++ branches/SAS-DuplicateNodes-Task2869/MAC/Deployment/data/Coordinates/vectors-and-matrices/DE602/de602-lba-solution.lisp
@@ -0,0 +1,12 @@
+(discarded-points
+    NIL)
+(reduced-chi-squared 7.079028358164548)
+(normal-vector               (0.6628865 0.1096938 0.7406408))
+(normal-vector-uncertainties (0.0000201 0.0000265 0.0000177))
+(normal-vector-direction-uncertainties-arcsec :max  5.47 :rms  4.49 :avg  4.42)
+(reference-point :etrs (4152561.241 828868.343  4754356.780)
+                 :station-pqr (0.0 0.0 0.0))
+(station-pqr-to-etrs-matrix
+    #2A((-0.13931024326571673 -0.735645434113131 0.6628864543719006)
+        (0.9900056752226666 -0.08863428465533096 0.1096937856514667)
+        (-0.021941265874123858 0.6715428198202561 0.7406408184806588)))
---
+++ branches/SAS-DuplicateNodes-Task2869/support/tools/BaseSimGUI/src/org/astron/lofarsim/DHBeamBand.java
@@ -0,0 +1,22 @@
+package org.astron.lofarsim;
+
+import org.astron.basesim.GraphDataHolder;
+import java.awt.*;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class DHBeamBand extends GraphDataHolder {
+
+  public DHBeamBand() {
+    super();
+    setBackground(Color.magenta);
+  }
+  public String getClassName() { return "DH_BeamBand"; }
+}
