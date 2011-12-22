f3452ab0a72dd364bc71d0b6e5a0a4e0c365fa41
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2011-12-22 16:13:40 +0000

    Task #2807: prepare_imager first version. Not working but, save point before leaving on vacation. The input mapfile for the node was send to the nodes as files. The current plan is to put it pm a mounted drive (home\task\bla ) and read it on the node themselve based on indexes in the output set. I was in the middle of performing this operation: combining the two processes in a single loop. Current state: error when runnin:
    Traceback (most recent call last):
      File "/home/klijn/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/cuisine/WSRTrecipe.py", line 132, in run
        status = self.go()
      File "prepare_imager.py", line 88, in go
        self.inputs['output_map']))
      File "/home/klijn/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/support/lofaringredient.py", line 241, in __getitem__
        raise KeyError(key)
    KeyError: 'output_map'
    No results

--- branches/CEP-Pipeline-Task2807/CEP/Pipeline/recipes/sip/master/prepare_imager.py
+++ branches/CEP-Pipeline-Task2807/CEP/Pipeline/recipes/sip/master/prepare_imager.py
@@ -1,8 +1,12 @@
-#                                                         LOFAR IMAGING PIPELINE
+# LOFAR IMAGING PIPELINE
 #
-#                                    Example recipe with simple job distribution
-#                                                            John Swinbank, 2010
-#                                                      swinbank@transientskp.org
+# Prepare phase of the imager pipeline
+#     collect data from nodes and collect it on the compute node
+#     start bbs, concats the files and 
+#     
+# Wouter Klijn 
+# 2011
+# klijn@astron.nl
 # ------------------------------------------------------------------------------
 
 import os
@@ -12,7 +16,7 @@ import lofarpipe.support.lofaringredient as ingredient
 from lofarpipe.support.baserecipe import BaseRecipe
 from lofarpipe.support.remotecommand import RemoteCommandRecipeMixIn
 from lofarpipe.support.remotecommand import ComputeJob
-from lofarpipe.support.group_data import load_data_map
+from lofarpipe.support.group_data import load_data_map, store_data_map
 
 class prepare_imager(BaseRecipe, RemoteCommandRecipeMixIn):
     """
@@ -22,11 +26,10 @@ class prepare_imager(BaseRecipe, RemoteCommandRecipeMixIn):
 
     """
     inputs = {
-# hier zullen wel executables voor de dppp voor in de plaats moeten komen
-#        'executable': ingredient.ExecField(
-#            '--executable',
-#            help="The full path to the relevant AWImager executable"
-#        ),
+        'ndppp': ingredient.ExecField(
+            '--ndppp',
+            help="The full path to the ndppp executable"
+        ),
         'initscript': ingredient.FileField(
             '--initscript',
             help='''The full path to an (Bourne) shell script which will\
@@ -38,7 +41,7 @@ class prepare_imager(BaseRecipe, RemoteCommandRecipeMixIn):
         ),
         'working_directory': ingredient.StringField(
             '-w', '--working-directory',
-            help="Working directory used on outpuconfigt nodes. Results location"
+            help="Working directory used on output nodes. Results location"
         ),
         'suffix': ingredient.StringField(
             '--suffix',
@@ -48,66 +51,145 @@ class prepare_imager(BaseRecipe, RemoteCommandRecipeMixIn):
         'output_mapfile': ingredient.FileField(
             '--output-mapfile',
             help="Added to the input filename to generate the output filename"
-        )    
+        ),
+        'slices_per_image': ingredient.IntField(
+            '--slices-per-image',
+            help="The number of (time) slices for each output image"
+        ),
+        'subbands_per_image': ingredient.IntField(
+            '--subbands-per-image',
+            help="The number of subbands to be collected in each output image"
+        )                
     } 
                   
     def go(self): 
         self.logger.info("Starting prepare_imager run")     
         super(prepare_imager, self).go()
-#command line arguments
-#python prepare_imager.py ~/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/recipes/input.map --job-name "prepare_mager" --config ~/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/recipes/pipeline.cfg --initscript /opt/cep/LofIm/daily/lofar/lofarinit.sh -p ~/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/recipes/master/parset.test -w "/data/scratch/klijn" --output-mapfile ~/build/gnu_debug/installed/lib/python2.6/dist-packages/lofarpipe/recipes/output.map
-         
-   
-#        # Load datafiles containing node --> dataset pairs     
-        self.logger.debug("Loading input map from {0}".format(self.inputs['args']))
+#command line arguments:
+#python python prepare_imager.py ~/build/preparation/input.map --job prepare_imager --config ~/build/preparation/pipeline.cfg --initscript /opt/cep/LofIm/daily/lofar/lofarinit.sh --output-mapfile ~/build/preparation/output.map --parset ~/build/preparation/parset.par --working-directory "/data/scratch/klijn" --subbands-per-image 10 --slices-per-image 9 --ndppp /opt/cep/LofIm/daily/lofar/bin/NDPPP -v
+
+        # *********************************************************************
+        # Load datafiles containing node --> dataset pairs
+        # The input mapfile is structured each line contains:
+        # node name , measurement set
+        #    the measurement sets are sorted first timeslice and the sub sorted
+        #    on subbands eg:   slice1_SB001
+        #                      slice1_SB002
+        #                      slice2_SB001
+        #                      etc
+        # *********************************************************************  
+        self.logger.debug("Loading input map from {0}".format(
+                                                    self.inputs['args']))
         input_mapfile = eval(open(self.inputs['args'][0]).read())
 
-        #output mapping locus -> file
-        self.logger.debug("Loading output map from {0}".format(self.inputs['output_mapfile']))
-        output_mapfile = eval(open(self.inputs['output_mapfile']).read())
+        # output mapping locus -> file
+        # The output locus also determines the computation node
+        self.logger.debug("Loading output map from {0}".format(
+                                                self.inputs['output_map']))
+        output_map = eval(open(self.inputs['output_map']).read())
  
-        
-
-        
         # Compile the command to be executed on the remote machine
         nodeCommand = "python %s" % (self.__file__.replace("master", "nodes"))
         
-        # Environment type variables
-        slices_per_image = 9 # inputs['slices_per_image']
-        subbands_per_image = 10 # inputs['subbands_per_image']          
-        initScript = self.inputs['initscript']
+        # Environment variables
+        slices_per_image = self.inputs['slices_per_image']
+        subbands_per_image = self.inputs['subbands_per_image']          
+        init_script = self.inputs['initscript']
         parset = self.inputs['parset']                  #Using parameter set             
-        workingDir = self.inputs['working_directory']     
+        working_dir = self.inputs['working_directory']     
 
-        #todo:
-        # 1. copy the files to the correct location
-        #   a. Create a submapset and copy to the node runtime location
-        # 
-        
-        
+        # Validate inputs:
+        if len(input_mapfile) != len(output_map) * \
+                                   (slices_per_image * subbands_per_image):
+            self.logger.warn("ERROR: incorrect number of input ms for "+
+                              "supplied parameters")
+            return 1
+                       
+        # ********************************************************************    
+        # For each output image (is a subband group) create a input mapfile and
+        # send it to the compute node
+        # ********************************************************************
+#        n_subband_groups = len(output_map)
+#        for  idx_sb_group, (node, path) in enumerate(output_map):
+#
+#            
+#            inputs_for_image = []
+#            # collect the inputs: first step over the time slices
+#            for idx_slice in range(slices_per_image):
+#                # calculate the first line for current time slice and subband group
+#                line_idx_start = idx_slice * \
+#                    (n_subband_groups * subbands_per_image) + \
+#                    (idx_sb_group * subbands_per_image)
+#                line_idx_end = line_idx_start + subbands_per_image
+#                
+#                #extend inputs with the files for the current time slice
+#                inputs_for_image.extend(input_mapfile[line_idx_start: line_idx_end])  
+#            
+#            # Create a file, fill with the file names to send to the remote node
+#            temp_input_map = os.path.join(self.config.get("layout", "job_directory"),
+#                                          "node_input_{0}.map".format(idx_sb_group) )
+#            
+#            store_data_map(temp_input_map, inputs_for_image)
+            
+#            fp = open(temp_input_map, "w")  #TODO : working 
+#            fp.write(repr(inputs_for_image))    
+#            fp.close()
+            #input_mapfile = eval(open(temp_input_map).read())
+            # Send the file 
+            
+            
+#            print "{0}:{1}".format(node, temp_input_map)          
+#            system_command_string =  \
+#                            "scp {0} {1}:{0}".format(temp_input_map ,node )
+#
+#            sys.stdout.flush()
 
-        
+            os.system(system_command_string)  #TODO test corrert write? or local node problem?         
+               
         # Output
-        #outnames = collections.defaultdict(list)             
+        #start the nodes 
+        outnames = collections.defaultdict(list)             
 
-        print "we have a winner!!!!"
-#        # Create the jobs
-#        jobs = []
-#        for host, measurementSet in input_mapfile:
-#            #construct and save the output name
-#            outnames[host].append(os.path.join(
-#                            self.inputs['working_directory'],
-#                            self.inputs['job_name'],
-#                            os.path.basename(measurementSet.rstrip('/')) + 
-#                            self.inputs['suffix']))
-#            
-#            arguments=[executable, initScript, parset, workingDir, \
-#                       measurementSet]
-#            
-#            jobs.append(ComputeJob(host, nodeCommand, arguments))
-#        
-#        # Hand over the job(s) to the pipeline scheduler
-#        self._schedule_jobs(jobs)
+        # Create the jobs
+        jobs = []
+        n_subband_groups = len(output_map)
+        #for  idx_sb_group, (node, path) in enumerate(output_map):
+        for idx_sb_group, (host, measurement_set) in enumerate(output_map):
+            #construct and save the output name
+            inputs_for_image = []
+            # collect the inputs: first step over the time slices
+            for idx_slice in range(slices_per_image):
+                # calculate the first line for current time slice and subband group
+                line_idx_start = idx_slice * \
+                    (n_subband_groups * subbands_per_image) + \
+                    (idx_sb_group * subbands_per_image)
+                line_idx_end = line_idx_start + subbands_per_image
+                
+                #extend inputs with the files for the current time slice
+                inputs_for_image.extend(input_mapfile[line_idx_start: line_idx_end])  
+            
+            # Create a file, fill with the file names to send to the remote node
+            temp_input_map = os.path.join(self.config.get("layout", "job_directory"),
+                                          "node_input_{0}.map".format(idx_sb_group) )
+            
+            store_data_map(temp_input_map, inputs_for_image)
+            outnames[host].append(os.path.join(
+                            self.inputs['working_directory'],
+                            self.inputs['job_name'],
+                            os.path.basename(measurement_set.rstrip('/')) + 
+                            self.inputs['suffix']))
+            
+            arguments=[init_script, parset, os.path.join(
+                            self.inputs['working_directory'],
+                            self.inputs['job_name']),
+                        self.inputs['ndppp'], 
+                        measurement_set, slices_per_image, 
+                        subbands_per_image, temp_input_map]
+            
+            jobs.append(ComputeJob(host, nodeCommand, arguments))
+        
+        # Hand over the job(s) to the pipeline scheduler
+        self._schedule_jobs(jobs)
         
         #todo: What to do with the output??
         
--- branches/CEP-Pipeline-Task2807/CEP/Pipeline/recipes/sip/nodes/prepare_imager.py
+++ branches/CEP-Pipeline-Task2807/CEP/Pipeline/recipes/sip/nodes/prepare_imager.py
@@ -1,145 +1,162 @@
 # Prepare fase of the LOFAR AWIMAGER RECIPE
-# collect datasets and start prepropressing fases
+# collect datasets and start prepropressing fases:
+# dppp and concat of timeslices
 # Wouter Klijn 2012
 # klijn@astron.nl
 # ------------------------------------------------------------------------------
 
 from __future__ import with_statement
-#import os
 import sys
-#import tempfile  
-#import shutil  
+import errno
 import os.path
-#import math
 
 from lofarpipe.support.pipelinelogging import CatchLog4CPlus
 from lofarpipe.support.pipelinelogging import log_time
 from lofarpipe.support.utilities import patch_parset
-from lofarpipe.support.utilities import get_parset
 from lofarpipe.support.utilities import read_initscript
 from lofarpipe.support.utilities import catch_segfaults
 from lofarpipe.support.lofarnode import  LOFARnodeTCP
-import pyrap.tables as pt                   #@UnresolvedImport
 from subprocess import CalledProcessError
-
+import pyrap.tables as pt #@UnresolvedImport
  
 class prepare_imager(LOFARnodeTCP):
-    def run(self, executable, initScript, parset, workingDir):        
-        log4CPlusName = "prepare_imagerRecipe" 
+    def run(self, init_script, parset, working_dir, ndppp, measurement_set,
+            slices_per_image, subbands_per_image, input_map_path):
+        #working_dir = tempfile.mkdtemp()
+        log4CPlusName = "prepare_imager_node_recipe" 
+        group_dir = "group_sets"
+        sets_dir = "working_sets"
         
         # Time execution of this job 
-        # todo do we need executable?
-        #maybee multiple
-        with log_time(self.logger):
-            if not os.access(executable, os.X_OK):
-                self.logger.error("Could not find executable: {0}".format(
-                                                                executable))         
-                return 1
+        # TODO: The datacolumn is depending on output/functioning of previous
+        # pipelines. When update ms then use corrected data
+        
+        with log_time(self.logger):          
+            # Load the input file names
+            self.logger.info("Loading input map from {0}".format(
+                os.path.join(working_dir, "node_input.map")))
+            input_map = eval(open(input_map_path).read())
+            
+            # *********************************************************
+            # Collect all the measurement sets in a single directory
+            # ******************************************************
+            working_set_path = os.path.join(working_dir, sets_dir) 
+            self._create_dir(working_set_path)
+            missing_files = []
+            
+            #loop all measurement sets
+            for idx, (node, path) in enumerate(input_map):
+                self.logger.info("copy file: {0}".format(path))
 
-#            # Calculate AWImager parameters that depend on measurement set      
-#            maxbaseline = get_parset(parset).getInt('maxbaseline')
-#            cellSize, npix, wMax, wProjPlanes = \
-#                self._calcParamFromMeasurement(measurementSet, maxbaseline)
-#            cellSizeFormatted = str(int(round(cellSize))) + 'arcsec'
-            
-            # Update the parset (supplied to awimager) with calculated parameters
-#            patchDictionary = {'uselogger': 'True',  # enables log4cpluscd log
-#                               'ms': measurementSet,  
-#                               'cellsize': cellSizeFormatted,
-#                               'npix': str(npix),
-#                               'wmax': str(wMax),
-#                               'wprojplanes':str(wProjPlanes)
-#                                }      
-            patchDictionary = {}
-            try:
-                tempParsetFilename = patch_parset(parset, patchDictionary)
-            except Exception, e:
-                self.logger.error("failed loading and updating the parset: {0}",
-                                  format(parset))
-                self.logger.error(e)
-                return 1
-            
-            # The command and parameters to be run
-            cmd = [executable, tempParsetFilename]
-                              
-            try:
-                environment = read_initscript(self.logger, initScript)               
-                with CatchLog4CPlus(workingDir,
-                    self.logger.name + "." + os.path.basename(log4CPlusName),
-                    os.path.basename(executable)) as logger:                      
-                        catch_segfaults(cmd, workingDir, environment, logger,
-                                    cleanup=None)
-            
-            # Thrown by catch_segfault
-            except CalledProcessError, e:                
-                self.logger.error(str(e))
-                return 1
-            
-            except Exception, e:
-                self.logger.error(str(e))
-                return 1
-            
-            finally:
-                # Cleanup of temporary parameterset
-                os.unlink(tempParsetFilename)
+                #construct copy command
+                copy_command = "rsync -r {0}:{1} {2} ".format(node, path, 
+                                                            working_set_path)
+                exit_status = os.system(copy_command)
 
+                #if copy failed log the missing file
+                if  exit_status != 0: 
+                    missing_files.append(path.split('/')[-1])
+                    self.logger.info("Failed loading file: {0}".format(path))
+                    self.logger.info("continuing")
+            
+            # ***************************************************
+            # start NDPPP:  
+            # for each timeslice 
+            # 1. Remove international stations 
+            # 2. Combine subbands into groups
+            # ******************************************************           
+            #create the directory to save subband group data sets
+            group_measurement_directory = os.path.join(working_dir, group_dir)
+            group_measurements_collected = []
+            self._create_dir(group_measurement_directory)
+                
+            for idx_time_slice in range(slices_per_image):
+                #collect the subband for this timeslice in a single list
+                input_map_subgroup = \
+                   input_map[(idx_time_slice * subbands_per_image): \
+                             ((idx_time_slice + 1) * subbands_per_image)]
+                   
+                #get the path on the remote machine. get the directory names
+                input_subgroups = map(lambda x: x.split("/")[-1] ,
+                                          list(zip(*input_map_subgroup)[1]))
+
+                #Remove the missing files
+                input_subgroups = filter(lambda x: not(x in missing_files),
+                                      input_subgroups)
+               
+                #join with the group_measurement_directory
+                # TODO: Fixme the write of the ms is a layer to deep!!
+                # Remove addition x: due to incorrect path structure of source files!!!
+                input_path = map(lambda x: os.path.join(working_dir,
+                             sets_dir, x, x), input_subgroups)
+                
+                group_ms_name = "time_slice_{0}.dppp.ms".format(idx_time_slice)
+                group_measurements_collected.append(
+                        os.path.join(working_dir, group_dir, group_ms_name))
+                
+                # create the directory to save group measurement set for this 
+                # group
+                group_ms_path = os.path.join(group_measurement_directory,
+                                             group_ms_name)
+                
+                # Update the parset with calculated parameters
+                patchDictionary = {'uselogger': 'True', # enables log4cplus
+                                   'msin': repr(input_path),  
+                                   'msout':group_ms_path
+                                   }               
+                temp_parset_filename = None                     
+                try:
+                    temp_parset_filename = patch_parset(parset, patchDictionary)
+                except Exception, e:
+                    self.logger.error("failed loading and updating the " +
+                                          "parset: {0}".format(parset))
+
+                #run ndppp
+                cmd = [ndppp, temp_parset_filename]              
+                try:
+                    environment = read_initscript(self.logger, init_script)               
+                    with CatchLog4CPlus(working_dir, self.logger.name + 
+                                        "." + os.path.basename(log4CPlusName),
+                                        os.path.basename(ndppp)) as logger:                      
+                            catch_segfaults(cmd, working_dir, environment,
+                                            logger, cleanup=None)
+                except CalledProcessError, e:
+                    self.logger.error(str(e))
+                    return 1
+                except Exception, e:
+                    self.logger.error(str(e))
+                    return 1
+                finally:
+                    os.unlink(temp_parset_filename)
+                     
+
+            # *************************************************************
+            # run pyrap to combine the time slices in a single ms
+            # *************************************************************           
+            tn = pt.table(group_measurements_collected)            
+            tn.rename(os.path.join(working_dir, "output_concat.ms")) 
+            tn.close()
+ 
+# ***************************************************
+# TODO: Currently the concat does not work wait for fix ger
+# ***************************************************
+#            pt.msconcat(group_measurements_collected, 
+#                        os.path.join(working_dir, "output"), concatTime=True)
+#            self.logger.info("pyrap test !!1")
+
+                
+        # TODO: clean up of temporary files??
+        # TODO: FAILURE TO END?? WHAT TO DO: timeout 
         return 0
     
-#    def _nearestCeiledPower2(self, value):
-#        '''
-#        Return int value of  the nearest Ceiled power of 2 for the 
-#        suplied argument
-#        
-#        '''
-#        return int(pow(2, math.ceil(math.log(value, 2)))) 
-#    
-#    def _calcParamFromMeasurement(self, measurementSet, 
-#                                            baselineLimit):                   
-#        ardSecInDegree = 3600
-#        arcSecInRad = (180.0 / math.pi) * ardSecInDegree
-#        
-#        # Calculate the cellSize         
-#        maxBaseline = pt.taql('CALC sqrt(max([select sumsqr(UVW[:2]) from ' + \
-#            '{0} where sumsqr(UVW[:2]) <{1} giving as memory]))'.format( \
-#            measurementSet, baselineLimit *
-#            baselineLimit))[0]
-#            
-#        waveLength = pt.taql('CALC C()/REF_FREQUENCY ' + \
-#            'from {0}/SPECTRAL_WINDOW'.format(measurementSet))[0]
-#
-#        cellSize = (1.0/3) * (waveLength/float(maxBaseline)) * arcSecInRad            
-#                         
-#        # Calculate the number of pixels in x and y dim
-#        #    fov and diameter depending on the antenna name
-#        fov = None
-#        stationDiameter = None
-#        antennaName = pt.taql('select NAME from {0}/ANTENNA'.format( \
-#                          measurementSet)).getcell('NAME',0)
-#        if antennaName.count('HBA'):
-#            fov = 6  #(degrees)
-#            stationDiameter = 35 #(meters)
-#        elif antennaName.count('LBA'):
-#            fov = 3
-#            stationDiameter = 30
-#        else:
-#            self.logger.error('unknow antenna type for antenna: {0}').format( \
-#                                                                antennaName)
-#            return 1
-#           
-#        npix = (ardSecInDegree * fov) / cellSize
-#        npix = self._nearestCeiledPower2(npix)
-#            
-#        # Get the max w with baseline < 10000
-#        wMax = pt.taql('CALC max([select UVW[2] from ' + \
-#            '{0} where sumsqr(UVW[:2]) <{1} giving as memory])'.format(  
-#            measurementSet, baselineLimit*baselineLimit))[0]
-#                
-#        # Calculate number of projection planes
-#        wProjPlanes = (maxBaseline * waveLength) / (stationDiameter ** 2)
-#        wProjPlanes = int(round(wProjPlanes))
-#        
-#        return cellSize, npix, wMax, wProjPlanes
-    
+    def _create_dir(self,path):
+        try:
+            os.mkdir(path)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST: #if dir exists continue  
+                pass
+            else:
+                raise
 
 if __name__ == "__main__":
     jobid, jobhost, jobport = sys.argv[1:4]
