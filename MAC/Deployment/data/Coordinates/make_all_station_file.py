Task #2896: Had to change query to database to prevent wrong order of antennacoords in AntennaField file

MAC/Deployment/data/Coordinates/make_all_station_file.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
         frame = ''
     
         # from database select all antennas for given station and target-date
-        cursor.execute("select * from get_gen_coord(%s, %f) order by objtype desc", (station, float(sys.argv[1])))
+        cursor.execute("select * from get_gen_coord(%s, %f) order by objtype, number", (station, float(sys.argv[1])))
     
         # start with empty arrays
         aPosL = np.zeros((0,2,3))

MAC/Deployment/data/Coordinates/make_conf_files.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     # from database select all antennas for given station and target-date
     # The ''order by'' statement is needed to prevent mixup of even/odd pairs
     # as was seen on sas001 (Arno)
-    cursor.execute("select * from get_gen_coord(%s, %f) order by objtype desc", (station, float(sys.argv[2])))
+    cursor.execute("select * from get_gen_coord(%s, %f) order by objtype, number", (station, float(sys.argv[2])))
     
     # start with empty arrays
     aPosL = np.zeros((0,2,3))