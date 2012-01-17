68ad953c0f943d7926e72a341f73f880c7ae6383
schoenmakers <schoenmakers@astron.nl>
2012-01-17 08:55:26 +0000

    Task #3008: Merged bugfix from Dec 22 2011 in LOFAR-Release-0.7 into trunk

--- MAC/APL/APLCommon/src/ChildControl.cc
+++ MAC/APL/APLCommon/src/ChildControl.cc
@@ -1027,7 +1027,7 @@ void ChildControl::_doGarbageCollection()
 		if (!iter->port) {
 			restartTimer = true;
 			LOG_DEBUG_STR(time(0)<<"-"<<iter->requestTime<<">="<<itsStartupRetryInterval<<"*"<<itsMaxStartupRetries<<"?");
-			if ((uint32(time(0)-iter->requestTime)) >= MAC_SCP_TIMEOUT + itsStartupRetryInterval*itsMaxStartupRetries) {
+			if ((time(0)-iter->requestTime) >= int32(MAC_SCP_TIMEOUT+(itsStartupRetryInterval*itsMaxStartupRetries))) {
 				LOG_DEBUG_STR ("Controller " << iter->cntlrName << " is still unreachable, informing main task");
 				_setEstablishedState(iter->cntlrName, CTState::QUITED, time(0), CT_RESULT_LOST_CONNECTION);
 				iter->port = (GCFPortInterface*) -1;
