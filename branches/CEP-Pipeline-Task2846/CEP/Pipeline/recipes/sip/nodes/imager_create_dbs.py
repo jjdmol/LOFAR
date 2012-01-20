ec80c46b78f677beb6ce51f470043d252026fcbd
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-01-20 11:07:34 +0000

    Task #2846: Non camelcap class name

--- branches/CEP-Pipeline-Task2846/CEP/Pipeline/recipes/sip/nodes/imager_create_dbs.py
+++ branches/CEP-Pipeline-Task2846/CEP/Pipeline/recipes/sip/nodes/imager_create_dbs.py
@@ -32,7 +32,7 @@ quit
 """
 
 
-class ImagerCreateDBs(LOFARnodeTCP):
+class imager_create_dbs(LOFARnodeTCP):
     def run(self, concatenated_measurement_set, sourcedb_target_path,
             monet_db_hostname, monet_db_port, monet_db_name, monet_db_user,
             monet_db_password, assoc_theta, parmdb_executable, slice_paths,
@@ -43,7 +43,7 @@ class ImagerCreateDBs(LOFARnodeTCP):
         All inputs are path wise, and no information regarding the number of 
         slices is needed 
         """
-        self.logger.info("Starting ImagerCreateDBs Node")
+        self.logger.info("Starting imager_create_dbs Node")
 
         # create the source db
         # returns zero on succes
@@ -254,5 +254,5 @@ class ImagerCreateDBs(LOFARnodeTCP):
 if __name__ == "__main__":
     # args contain information regarding to the logging server
     jobid, jobhost, jobport = sys.argv[1:4]
-    sys.exit(ImagerCreateDBs(jobid, jobhost, jobport).run_with_stored_arguments())
+    sys.exit(imager_create_dbs(jobid, jobhost, jobport).run_with_stored_arguments())
 
