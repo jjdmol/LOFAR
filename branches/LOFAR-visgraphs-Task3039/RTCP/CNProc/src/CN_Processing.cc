fa915024067c3be2bfc3142b0224e2f1232fae10
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-02-17 16:02:25 +0000

    Task #3039: Upgraded log message from WARN to ERROR

--- branches/LOFAR-visgraphs-Task3039/RTCP/CNProc/src/CN_Processing.cc
+++ branches/LOFAR-visgraphs-Task3039/RTCP/CNProc/src/CN_Processing.cc
@@ -734,7 +734,7 @@ template <typename SAMPLE_TYPE> void CN_Processing<SAMPLE_TYPE>::checkInputForZe
     // Emit (at most) one message per 10 seconds, and only one per RSP board (TODO: this doesn't work as expected with DataSlots)
     unsigned logInterval = static_cast<unsigned>(ceil(10.0 / itsCNintegrationTime));
     if (itsBlock % logInterval == 0 && *itsCurrentSubband % itsNrSlotsInFrame == 0)
-      LOG_WARN_STR(itsLogPrefix << "Station " << itsStationNames[station] << " subband " << *itsCurrentSubband << " consists of only zeros.");
+      LOG_ERROR_STR(itsLogPrefix << "Station " << itsStationNames[station] << " subband " << *itsCurrentSubband << " consists of only zeros.");
   }
 
   timer.stop();
