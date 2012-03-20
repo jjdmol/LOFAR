fc0e3a8e621b4ed9513e467961f0cf3f354e40d4
duscha <duscha@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-03-20 16:56:45 +0000

    task #3075 initial skeleton implementation of ModelFftImage class; ExprVisDataImage shall provide uvw-data interface to BBS expression tree

---
+++ branches/CEP-BBS-Task2783/CEP/Calibration/BBSKernel/include/BBSKernel/ModelImageFFT.h
@@ -0,0 +1,170 @@
+//# ModelImageFFT.h: 
+//#
+//#
+//# Copyright (C) 2012
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id: ModelImageFFT.h 20029 2012-02-20 15:50:23Z duscha $
+
+
+#include <coordinates/Coordinates/DirectionCoordinate.h>    // DirectionCoordinate needed for patch direction
+#include <casa/OS/PrecTimer.h>
+#include <scimath/Mathematics/ConvolveGridder.h>
+#include <LofarFT/LofarCFStore.h>
+#include <LofarFT/LofarVisResampler.h>
+#include <LofarFT/LofarConvolutionFunction.h>
+// etc. need more
+
+// Options for the ModelImageFFT
+typedef struct ModelImageOptions
+{
+    casa::String ConvType;                  // convolution type
+    bool Wprojection;                       // should W-projection be used?
+    bool Aprojection;                       // should A-projection be used?
+    casa::Int Nwplanes;                     // number of w-projection planes (default: 0)
+    double wmax;                            // maximum w value to use
+    casa::MPosition Mlocation;              // location of array on earth
+    casa::Float padding;                    // padding used in gridding
+    casa::Int verbose;                      // verbosity level
+    casa::Int maxSupport;                   // maximum support
+    casa::uInt oversample;                  // FFT oversampling
+    casa::String imageName;                 // name of ModelImage
+    casa::Matrix<Bool> gridMuellerMask;     // grid Mueller mask
+    casa::Matrix<Bool> degridMuellerMask;   // degridding Mueller mask, LofarFTMachine needs this
+    unsigned int NThread;                   // number of threads used
+};
+
+class ModelImageFft
+{
+  public:
+    ModelImageFft();
+    ModelImageFft(const casa::String &name,
+                  const casa::MDirection phasedir=0,
+                  double wmax=0, 
+                  unsigned int nwplanes=1, 
+                  bool aprojection=false);
+//    void ModelImageFft(const casa::String &name, double nwmax=0);
+    ~ModelImageFft();
+
+    void setNWplanes(uInt n);               // manually set No. of wplanes
+    void computeConvolutionFunctions();     // compute w-plane convolution functions
+
+    // Access to options
+    void setoptions(const ModelImageOptions &options); // set all options for LofarFTMachine parameters
+    ModelImageOptions getOptions();         // get all options for LofarFTMachine parameters
+    void setDefaults();                     // set all options to default values
+    
+    // Setter functions for individual options
+    void setConvType(const casa::String type="SF");
+    void setWprojection(bool mode=true);
+    void setWmax(double wmax);
+    void setAprojection(bool mode=true);
+    void setNwplanes(casa::uInt nwplanes);
+    void setMlocation(casa::MPosition &location);
+    void setPadding(casa::Float padding);
+    void setVerbose(casa::uInt verbose);
+    void setMaxSupport(casa::Int maxsupport);
+    void setOversample(casa::uInt oversample);
+    void setImageName(const casa::String &imagename);
+    void setGridMuellerMask(casa::Matrix<Bool> muellerMask);
+    void setDegridMuellerMask(casa::Matrix<Bool> muellerMask);
+    void setNThread(unsigned int nthread);
+
+    // Getter functions for individual options
+    inline casa::String    getConvType() const { return itsOptions.ConvType; }
+    bool                   getWprojection() const { return itsOptions.Wprojection; }
+    bool                   getAprojection() const { return itsOptions.Aprojection; }
+    inline casa::Int       getNwplanes() const { return itsOptions.Nwplanes; }
+    inline double          getWmax() const { return itsOptions.wmax; }
+    inline casa::MPosition getMlocation() const { return itsOptions.Mlocation; }
+    inline casa::Float     getPadding() const { return itsOptions.padding; }
+    inline casa::uInt      getVerbose() const { return itsOptions.verbose; }
+    inline casa::uInt      getMaxSupport() const { return itsOptions.maxSupport; }
+    inline casa::uInt      getOversample() const { return itsOptions.oversample; }
+    inline casa::String    getImageName() const { return itsOptions.imageName; }
+    inline unsigned int    getNThread() const { return itsOptions.NThread; }
+
+    // Show the relative timings of the various steps.
+    void showTimings (std::ostream&, double duration) const;
+    static casa::Int determineNWplanes();      // determine No. of wplanes from baselines   
+    
+    bool wprojection() const { return itsOptions.Wprojection; }     // return if it uses w-projection
+    bool aprojection() const { return itsOptions.Aprojection; }     // return if it uses A-projection
+    
+  private:
+    casa::ImageInterface<Complex> *image;
+    casa::LatticeCache<Complex> *imageCache;        // Image / FFT of image (ffted in place)
+
+    void init();                  // initialize LofarFTMachine (might change to all-in-one init)
+    void initMaps(void);          // init polarization and channel maps
+
+    // Pre-computed convolution functions for w-projection (indexed by w-plane number)
+    LOFAR::LofarConvolutionFunction * itsConvFunc;
+    Vector<LOFAR::LofarConvolutionFunction *> itsConvolutionFunctions;
+//    Vector<casa::PagedImage *> itsConvolutionFunctionsImages;
+
+    // Constructor: cachesize is the size of the cache in words
+    // (e.g. a few million is a good number), tilesize is the
+    // size of the tile used in gridding (cannot be less than
+    // 12, 16 works in most cases), and convType is the type of
+    // gridding used (SF is prolate spheriodal wavefunction,
+    // and BOX is plain box-car summation). mLocation is
+    // the position to be used in some phase rotations. If
+    // mTangent is specified then the uvw rotation is done for
+    // that location iso the image center.
+    // <group>
+    ModelImageOptions itsOptions;                   // struct containing all options
+    double itsGriddingTime;                         // variable to keep track of timing of steps
+
+
+    //-----------------------------------------------------------------  
+    // FTmachine functions
+    void fftImage();                               // perform FFT on image
+
+    void initializeToVis( ImageInterface<Complex>& iimage,
+                          const VisBuffer& vb);
+
+    // Gridder functions
+//    LofarVisResampler visResamplers_p;
+//    CountedPtr<LOFAR::VisibilityResamplerBase> visResampler_p;         // visibility resampler
+    LOFAR::LofarVisResampler visResamplers_p;
+    LOFAR::ConvolveGridder<casa::Double, casa::Complex>* gridder;      // Gridder
+    CountedPtr<casa::Lattice<casa::Complex> > arrayLattice;            // Array lattice
+    // Lattice. For non-tiled gridding, this will point to arrayLattice,
+    //  whereas for tiled gridding, this points to the image (TODO: tidy up, we don't tile at all)
+    CountedPtr<Lattice<Complex> > lattice;
+    IPosition centerLoc, offsetLoc;         // Useful IPositions
+    Vector<Double> uvScale, uvOffset;       // Image Scaling and offset
+
+    // Arrays for non-tiled gridding (one per thread).
+    Array<Complex>  itsGriddedData;
+    Array<DComplex> itsGriddedData2;
+    IPosition padded_shape;                // Shape of the padded image
+
+    Bool useDoubleGrid_p;                  // use double precision grid
+    Bool canComputeResiduals_p;
+    Bool noPadding_p;                      //force no padding
+    Float padding_p;                       // Padding in FFT
+
+    Vector<Int> chanMap_p, polMap_p;       // channel map, polarization map
+    Vector<Int> ConjCFMap_p, CFMap_p;
+    Int nx,ny;                             // where else does this come from?
+    Int nchan, npol;                       // where else does this come from?
+    
+    CFStore cfs_p, cfwts_p;
+};
---
+++ branches/CEP-BBS-Task2783/CEP/Calibration/BBSKernel/src/ModelImageFFT.cc
@@ -0,0 +1,433 @@
+//# ModelImageFFT.cc: 
+//#
+//#
+//# Copyright (C) 2012
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id: ModelImageFFT.cc 20029 2012-02-20 15:50:23Z duscha $
+
+#include <lofar_config.h>
+
+#include <casa/Arrays/Matrix.h>
+
+#include <synthesis/MeasurementComponents/FTMachine.h>
+#include <LofarFT/LofarFTMachine.h>
+#include <LofarFT/LofarCFStore.h>
+//#include <LofarFT/LofarVisibilityResamplerBase.h>
+
+#include <Common/Exception.h>
+#include <Common/LofarLogger.h>   // for ASSERT and ASSERTSTR?
+#include <BBSKernel/Exceptions.h>
+#include <BBSKernel/ModelImageFFT.h>
+
+#define DORES True                // why on earth is this done in LofarFTMachine?
+
+using namespace std;
+using namespace casa;
+
+using namespace LOFAR;
+using namespace BBS;
+
+//*********************************************
+//
+// Constructors and destructor
+//
+//*********************************************
+
+ModelImageFft::ModelImageFft( const casa::String &name,
+                              const casa::MDirection phasedir,
+                              double wmax, 
+                              unsigned int nwplanes, 
+                              bool aprojection)
+{
+//  ImageInterface<Complex>& iimage;
+  
+  setDefaults();
+  setWmax(wmax);
+  if(nwplanes==1)
+  {
+    setWprojection(false);
+    setNwplanes(1);
+  }
+  else
+  {
+    setWprojection(true);
+    setWmax(wmax);
+    setNwplanes(nwplanes);
+  }
+
+  setAprojection(aprojection);
+}
+
+ModelImageFft::~ModelImageFft(void)
+{
+  // currently do nothing
+  // TODO: Release memory of LatticeCache?
+}
+
+//*********************************************
+//
+// Setter functions for options
+//
+//*********************************************
+
+void ModelImageFft::setDefaults()
+{
+    // http://www.lofar.org/operations/doku.php?id=engineering:software:tools:cimagerdoc:gridder&s[]=maxsupport
+    itsOptions.ConvType="SF";                   // convolution type?
+    itsOptions.Wprojection=False;               // don't use w-projection
+    itsOptions.Aprojection=False;               // don't use w-projection
+    itsOptions.Nwplanes=1;                      // number of w-projection planes (default: 1)
+//    itsOptions.Mlocation;                     // location of observation, how to get it?
+    itsOptions.padding=2;                       // padding used in gridding?
+    itsOptions.wmax=75;                         // maximum w value to use?
+//    itsBeamPath=getenv("LOFARROOT").append("/share");     // default path to beam model
+    itsOptions.verbose=0;                       // verbosity level
+    itsOptions.maxSupport=256;                  // maximum support
+    itsOptions.oversample=8;                    // FFT oversampling
+    itsOptions.imageName="";                    // name of ModelImage
+
+    Matrix<Bool> muellerMask(4,4);
+
+    itsOptions.gridMuellerMask=muellerMask;
+    itsOptions.degridMuellerMask=muellerMask;
+    itsOptions.NThread=1;                       // number of threads used
+}
+
+void ModelImageFft::setConvType(const casa::String type)
+{
+  if(type=="SF" || type=="BOX")
+  {
+    itsOptions.ConvType=type;
+  }
+  else
+  {
+    THROW(BBSKernelException, "Convolution type " << type << " is unknown.");
+  }
+}
+
+void ModelImageFft::setWprojection(bool mode)
+{
+  itsOptions.Wprojection=mode;
+}
+
+void ModelImageFft::setAprojection(bool mode)
+{
+  itsOptions.Aprojection=mode;
+}
+
+void ModelImageFft::setNwplanes(casa::uInt nwplanes)
+{
+  itsOptions.Nwplanes=nwplanes;
+}
+
+void ModelImageFft::setMlocation(casa::MPosition &location)
+{
+  itsOptions.Mlocation=location;
+}
+
+void ModelImageFft::setPadding(casa::Float padding)
+{
+  if(padding < 0)
+  {
+    itsOptions.padding=padding;
+  }
+  else
+  {
+    THROW(BBSKernelException, "Padding < 0.");
+  }
+}
+
+void ModelImageFft::setWmax(double wmax)
+{
+  itsOptions.wmax=wmax;
+}
+
+void ModelImageFft::setVerbose(casa::uInt verbose)
+{
+  itsOptions.verbose=verbose;
+}
+
+void ModelImageFft::setMaxSupport(casa::Int maxsupport)
+{
+  itsOptions.maxSupport=maxsupport;
+}
+
+void ModelImageFft::setOversample(casa::uInt oversample)
+{
+  itsOptions.oversample=oversample;
+}
+
+void ModelImageFft::setImageName(const casa::String &imagename)
+{
+  itsOptions.imageName=imagename;
+}
+
+void setGridMuellerMask(casa::Matrix<Bool> muellerMask)
+{
+  if(muellerMask.nrow() != 4 && muellerMask.ncolumn() != 4)
+  {
+    setGridMuellerMask(muellerMask);
+  }
+  else
+  {
+    THROW(BBSKernelException, "gridMuellerMask has wrong dimensions " << muellerMask.nrow() << "," 
+          << muellerMask.ncolumn());
+  }
+}
+
+void setDegridMuellerMask(casa::Matrix<Bool> muellerMask)
+{
+  if(muellerMask.nrow() != 4 && muellerMask.ncolumn() != 4)
+  {
+    setDegridMuellerMask(muellerMask);
+  }
+  else
+  {
+    setDegridMuellerMask(muellerMask);
+  }
+}
+
+void ModelImageFft::setNThread(unsigned int nthread)
+{
+  itsOptions.NThread=nthread;
+}
+
+
+//*********************************************
+//
+// LofarFtMachine functions
+//
+//*********************************************
+
+void ModelImageFft::computeConvolutionFunctions()
+{
+  itsConvolutionFunctions.resize(itsOptions.Nwplanes);  // do we really have a conv function per w-plane?
+  for(unsigned int i=0; i<itsOptions.Nwplanes; i++)
+  {
+    itsConvolutionFunctions[i]; // call LofarConvolutionFunction constructor here
+  }
+  /*
+  // Constructor (needs MS, so not much use)
+  LofarConvolutionFunction(const IPosition& shape,
+                           const DirectionCoordinate& coordinates,
+                           const MeasurementSet& ms,
+                           uInt nW, double Wmax,
+                           uInt oversample,
+                           Int verbose,
+                           Int maxsupport,
+                           const String& imgName,
+                           Bool Use_EJones,
+                           Bool Apply_Element,
+                           const casa::Record& parameters
+                          );
+
+    LofarCFStore makeConvolutionFunction(uInt stationA, uInt stationB,
+                                         Double time, Double w,
+                                         const Matrix<bool>& Mask_Mueller,
+                                         bool degridding_step,
+                                         double Append_average_PB_CF,
+                                         Matrix<Complex>& Stack_PB_CF,
+                                         double& sum_weight_square,
+					                               uInt spw, Int TaylorTerm, double RefFreq);
+
+    // Compute and store W-terms and A-terms in the fourier domain
+    void store_all_W_images();
+    */
+}
+
+// Initialize for a transform from the Sky domain. This means that
+// we grid-correct, and FFT the image
+void ModelImageFft::initializeToVis(ImageInterface<Complex>& iimage,
+                                    const VisBuffer& vb)
+{
+  if (itsOptions.verbose > 0) {
+    cout<<"---------------------------> initializeToVis"<<endl;
+  }
+  image=&iimage;
+
+//  ok();   // don't do this
+  init();
+
+  // Initialize the maps for polarization and channel. These maps
+  // translate visibility indices into image indices
+//  initMaps(vb);
+  initMaps();
+
+  visResamplers_p.init(useDoubleGrid_p);
+  visResamplers_p.setMaps(chanMap_p, polMap_p);
+  visResamplers_p.setCFMaps(CFMap_p, ConjCFMap_p);
+
+  // Need to reset nx, ny for padding
+  // Padding is possible only for non-tiled processing
+
+  // If we are memory-based then read the image in and create an
+  // ArrayLattice otherwise just use the PagedImage
+//  AlwaysAssert (!isTiled, AipsError);
+
+  //  cout<<"LofarFTMachine::initializeToVis === is_NOT_Tiled!"<<endl;
+  //cout << "npol="<<npol<<endl;
+  IPosition gridShape(4, nx, ny, npol, nchan);
+  // Size and initialize the grid buffer per thread.
+  // Note the other itsGriddedData buffers are assigned later.
+  itsGriddedData[0].resize (gridShape);
+  itsGriddedData[0] = Complex();
+  /* // Don't need beam stuff
+  for (int i=0; i<itsOptions.NThread; ++i) {
+    itsSumPB[i].resize (padded_shape[0], padded_shape[1]);
+    itsSumPB[i] = Complex();
+    itsSumCFWeight[i] = 0.;
+    itsSumWeight[i].resize(npol, nchan);
+    itsSumWeight[i] = 0.;
+  }
+  */
+
+  //griddedData can be a reference of image data...if not using model col
+  //hence using an undocumented feature of resize that if
+  //the size is the same as old data it is not changed.
+  //if(!usePut2_p) griddedData.set(0);
+
+  IPosition stride(4, 1);
+  IPosition blc(4, (nx-image->shape()(0)+(nx%2==0))/2, (ny-image->shape()(1)+(ny%2==0))/2, 0, 0);
+  IPosition trc(blc+image->shape()-stride);
+  if (getVerbose() > 0) {
+    cout<<"LofarFTMachine::initializeToVis === blc,trc,nx,ny,image->shape()"
+        <<blc<<" "<<trc<<" "<<nx<<" "<<ny<<" "<<image->shape()<<endl;
+  }
+  IPosition start(4, 0);
+  itsGriddedData[0](blc, trc) = image->getSlice(start, image->shape());
+  //if(arrayLattice) delete arrayLattice; arrayLattice=0;
+  //======================CHANGED
+  arrayLattice = new ArrayLattice<Complex>(itsGriddedData[0]);
+  // Array<Complex> result(IPosition(4, nx, ny, npol, nchan),0.);
+  // griddedData=result;
+  // arrayLattice = new ArrayLattice<Complex>(griddedData);
+  //======================END CHANGED
+  lattice=arrayLattice;
+
+  //AlwaysAssert(lattice, AipsError);
+//  logIO() << LogIO::DEBUGGING
+//	  << "Starting grid correction and FFT of image" << LogIO::POST;
+
+  LOG_INFO_STR("Starting grid correction and FFT of image");
+
+  //==========================
+  // Cyr: I have commeneted that part which does the spheroidal correction of the clean components in the image plane.
+  // We do this based on our estimate of the spheroidal function, stored in an image
+  // Do the Grid-correction.
+    // {
+    //   Vector<Complex> correction(nx);
+    //   correction=Complex(1.0, 0.0);
+    //   // Do the Grid-correction
+    //   IPosition cursorShape(4, nx, 1, 1, 1);
+    //   IPosition axisPath(4, 0, 1, 2, 3);
+    //   LatticeStepper lsx(lattice->shape(), cursorShape, axisPath);
+    //   LatticeIterator<Complex> lix(*lattice, lsx);
+    //   for(lix.reset();!lix.atEnd();lix++) {
+    //     gridder->correctX1D(correction, lix.position()(1));
+    // 	lix.rwVectorCursor()/=correction;
+    //   }
+    // }
+
+}
+
+void ModelImageFft::init() 
+{
+
+//  logIO() << LogOrigin("LofarFTMachine", "init")  << LogIO::NORMAL;
+  LOG_INFO_STR("LofarFTMachine init()");
+  canComputeResiduals_p = DORES;
+//  ok();   // we don't do this
+
+  // We are padding.
+  if(!noPadding_p){
+    CompositeNumber cn(uInt(image->shape()(0)*2));
+    nx    = cn.nextLargerEven(Int(padding_p*Float(image->shape()(0))-0.5));
+    ny    = cn.nextLargerEven(Int(padding_p*Float(image->shape()(1))-0.5));
+  }
+  else{
+    nx    = image->shape()(0);
+    ny    = image->shape()(1);
+  }
+  npol  = image->shape()(2);
+  nchan = image->shape()(3);
+    // }
+
+  uvScale.resize(3);
+  uvScale=0.0;
+  uvScale(0)=Float(nx)*image->coordinates().increment()(0);
+  uvScale(1)=Float(ny)*image->coordinates().increment()(1);
+  uvScale(2)=Float(1)*abs(image->coordinates().increment()(0));
+
+  uvOffset.resize(3);
+  uvOffset(0)=nx/2;
+  uvOffset(1)=ny/2;
+  uvOffset(2)=0;
+
+  // Now set up the gridder. The possibilities are BOX and SF
+  if(gridder) delete gridder; gridder=0;
+  gridder = new ConvolveGridder<Double, Complex>(IPosition(2, nx, ny),
+						 uvScale, uvOffset,
+						 itsOptions.ConvType);
+
+  // Setup the CFStore object to carry relavent info. of the Conv. Func.
+  cfs_p.xSupport = gridder->cSupport();
+  cfs_p.ySupport = gridder->cSupport();
+  cfs_p.sampling.resize(2);
+  cfs_p.sampling = gridder->cSampling();
+  if (cfs_p.rdata.null())
+    cfs_p.rdata = new Array<Double>(gridder->cFunction());
+  // else
+  //   (*cfs_p.rdata) = gridder->cFunction();
+
+  padded_shape = image->shape();
+  padded_shape(0) = nx;
+  padded_shape(1) = ny;
+  if (itsOptions.verbose > 0) {
+    cout << "Original shape " << image->shape()(0) << ","
+         << image->shape()(1) << endl;
+    cout << "Padded shape " << padded_shape(0) << ","
+         << padded_shape(1) << endl;
+  }
+  assert(padded_shape(0)!=image->shape()(0));
+  
+  // How to do this without the MS?
+  /*
+  itsConvFunc = new LofarConvolutionFunction( padded_shape,
+                                              image->coordinates().directionCoordinate (image->coordinates().findCoordinate(Coordinate::DIRECTION)),
+                                              itsMS, itsNWPlanes, itsWMax,
+                                              itsOversample, itsBeamPath,
+					                                    itsVerbose, itsMaxSupport,
+                                              itsImgName);
+  */
+}
+
+
+void ModelImageFft::fftImage()
+{
+    LatticeFFT::cfft2d(*lattice);       // Now do the FFT2D in place
+}
+
+// LOFAR specific rewritten function initialize polarization (polmap)
+// and channel maps (chanmap)
+//
+//void ModelImageFft::initMaps(const casa::VisBuffer &)
+void ModelImageFft::initMaps(void)
+{
+
+}
