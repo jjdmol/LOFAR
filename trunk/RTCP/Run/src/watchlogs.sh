70a9c8c4cb7737c269498959df32185ab350f594
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-03-22 13:54:14 +0000

    Task #2672: Updated paths to put all logfiles and parsets in the same directory, and cleaned up variables and settings referring to unused paths.

--- trunk/RTCP/Run/src/watchlogs.sh
+++ trunk/RTCP/Run/src/watchlogs.sh
@@ -1,49 +1,20 @@
 #!/bin/bash
 
+source locations.sh
+
 FLAGS="-n 10000"
 
-# ----- find multitail
-PATH=$PATH:/globalhome/broekema/bin
-
-# ----- find the logging directory
-#   .                        = current directory, to check first
-#   $HOME/log/latest         = default logging directory for production
-#   $HOME/projects/LOFAR/log = default logging directory for development
-
-function set_logdir {
-  LOGDIR=$1
-
-  CNPROC=$LOGDIR/CNProc.log
-  IONPROC=$LOGDIR/IONProc.log
-}
-
-for d in . "$HOME/production/lofar/bgfen/log/latest" "$HOME/log/latest" "$HOME/projects/LOFAR/log"
-do
-  set_logdir $d
-  if [ -e $IONPROC ]
-  then
-    break
-  fi
-done
-
-# ----- find the configuration for multitail
-for c in `dirname $0` /opt/lofar/etc $HOME/projects/LOFAR/RTCP/Run/src
-do
-  CONFDIR=$c
-  if [ -e $CONFDIR/multitail-olap.conf ]
-  then
-    break
-  fi
-done
+CNPROC_LOG=$LOGDIR/CNProc.log
+IONPROC_LOG=$LOGDIR/IONProc.log
 
 echo Reading logs from $LOGDIR
-echo Reading multitail configuration from $CONFDIR
+echo Reading multitail configuration from $ETCDIR
 
 ERRORLOGS=
 
-if [ "$USER" == "lofarsys" ]
+if [ $ISPRODUCTION -eq 1 ]
 then
-  for l in /opt/lofar/log/BlueGeneControl.log $HOME/log/run.runParset.py.log
+  for l in $LOGDIR/BlueGeneControl.log $LOGDIR/run.runParset.py.log
   do
     echo Reading additional error log $l
     ERRORLOGS="$ERRORLOGS $FLAGS -cS olap -fr errors -I $l"
@@ -51,11 +22,11 @@ then
 fi
 
 
-multitail --no-mark-change --follow-all --retry-all -m 10240 --basename -F $CONFDIR/multitail-olap.conf \
-  $FLAGS -t "-- FLAGS --"  -fr flags -ks flags -i $IONPROC \
-  $FLAGS -t "-- ERRORS --" -fr errors          -i $IONPROC \
-  $FLAGS                   -fr errors          -I $CNPROC \
+multitail --no-mark-change --follow-all --retry-all -m 10240 --basename -F $ETCDIR/multitail-olap.conf \
+  $FLAGS -t "-- FLAGS --"  -fr flags -ks flags -i $IONPROC_LOG \
+  $FLAGS -t "-- ERRORS --" -fr errors          -i $IONPROC_LOG \
+  $FLAGS                   -fr errors          -I $CNPROC_LOG \
   $ERRORLOGS \
-  $FLAGS -t "IONProc/Storage"                  -i $IONPROC \
-  $FLAGS -t "CNProc"       -wh 5               -i $CNPROC
+  $FLAGS -t "IONProc/Storage"                  -i $IONPROC_LOG \
+  $FLAGS -t "CNProc"       -wh 5               -i $CNPROC_LOG
 
