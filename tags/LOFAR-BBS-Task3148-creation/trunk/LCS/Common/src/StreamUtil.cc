f05d8db6ed372efd5f47be54ca3aa665ee35d42d
zwieten <zwieten@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-04-17 13:58:04 +0000

    Task #3148: Preparing to branch.

---
+++ tags/LOFAR-BBS-Task3148-creation/trunk/LCS/Common/src/StreamUtil.cc
@@ -0,0 +1,34 @@
+//# StreamUtil.cc: implementation of the stream utilities.
+//#
+//# Copyright (C) 2002-2005
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+#include <Common/StreamUtil.h>
+
+namespace LOFAR
+{
+  // Initialize static variables.
+  uint Indent::lvl = 0;
+  const string Indent::tok = ". ";
+
+} // namespace LOFAR
