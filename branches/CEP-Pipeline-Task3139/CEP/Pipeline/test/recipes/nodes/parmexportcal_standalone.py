4ac62a9af4fbd8b75977b5bbe640d2b8224b93e8
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-05-29 11:56:53 +0000

    task #3139: Complete the standalone test program: better usage description, validate correct function on the test script

--- branches/CEP-Pipeline-Task3139/CEP/Pipeline/test/recipes/nodes/parmexportcal_standalone.py
+++ branches/CEP-Pipeline-Task3139/CEP/Pipeline/test/recipes/nodes/parmexportcal_standalone.py
@@ -41,29 +41,51 @@ class ParmExportCalWrapper(ParmExportCal):
 
 if __name__ == "__main__":
 
+
     usage = """
     (shalow) wrapper for the parmexportcal node script allowing
     functional testing of the edit_parmdb functionality.
     Needed is a parmdb  created as folows:
-    
-    $ parmdbm 
+
+    $ parmdbm
 
     # Copy pasta:
-    create table='test_blank.parmdb' 
-    add Gain:0:0:Real:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], freqwidths=[2], times=[2], freqs=[1], timestep=[1]
-    add Gain:0:0:Imag:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], freqwidths=[2], times=[2], freqs=[1], timestep=[1]
-    add Gain:1:1:Real:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], freqwidths=[2], times=[2], freqs=[1], timestep=[1]
-    add Gain:1:1:Imag:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], freqwidths=[2], times=[2], freqs=[1], timestep=[1]
-    
+    create table='test_blank.parmdb'
+    add Gain:0:0:Real:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], f\
+reqwidths=[2], times=[2], freqs=[1], timestep=[1]
+    add Gain:0:0:Imag:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], f\
+reqwidths=[2], times=[2], freqs=[1], timestep=[1]
+    add Gain:1:1:Real:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], f\
+reqwidths=[2], times=[2], freqs=[1], timestep=[1]
+    add Gain:1:1:Imag:test ny=8, nx=2, values=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,10,10,100,100], f\
+reqwidths=[2], times=[2], freqs=[1], timestep=[1]
+
     After running the corrected values are displayed
     The last minus 1 should be 0.1 and not 10
+
+    usage: parmexportcal_standalone.py infile[creation detailed above] outfile sigma -t(est)
+
+    The test option print corrected data to the screen
+    The sigma controls when a value is an outlier.
+    For the above mentioned testset sigma=2 will result in the value 10 in the testset to be corrected to 0.1
+    A sigma of 3 will leave it intact/unchanged
+    
     """
-    print sys.argv
+
+    if len(sys.argv) < 4:
+        print usage
+        sys.exit()
+
+
+
     infile, outfile, sigma = sys.argv[1:4]
 
     parmdb = ParmExportCalWrapper()
     parmdb, corrected_data = parmdb._filter_stations_parmdb(infile, outfile, sigma)
-    for pol, datapoint in corrected_data.iteritems():
-        print datapoint.real
+
+    if len(sys.argv) == 5 and sys.argv[4][:2] == "-t":
+        print "***********converted values:*******************"
+        for pol, datapoint in corrected_data.iteritems():
+            print datapoint.real
 
     sys.exit()
