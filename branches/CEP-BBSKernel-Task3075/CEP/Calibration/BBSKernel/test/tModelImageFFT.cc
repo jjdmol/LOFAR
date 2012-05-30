135e43a8ac069c31b81b569853b2a69a2d494abb
duscha <duscha@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-05-30 16:58:12 +0000

    task #3075: implemented baseline retrieval and debug print ftns for tModelImageFft test program

--- branches/CEP-BBSKernel-Task3075/CEP/Calibration/BBSKernel/test/tModelImageFFT.cc
+++ branches/CEP-BBSKernel-Task3075/CEP/Calibration/BBSKernel/test/tModelImageFFT.cc
@@ -21,9 +21,16 @@
 //#
 //# $Id: tModelImageFFT.cc 20029 2012-05-14 20:50:23Z duscha $
 
-
+#include <lofar_config.h>
 #include <iostream>
+#include <Common/Exception.h>     // THROW macro for exceptions
 #include <BBSKernel/ModelImageFFT.h>
+
+#include <ms/MeasurementSets/MeasurementSet.h>
+#include <tables/Tables/Table.h>
+#include <tables/Tables/ArrayColumn.h>
+#include <casa/Arrays/IPosition.h>
+#include <casa/Arrays/Slicer.h>
 
 using namespace std;
 using namespace casa;
@@ -33,28 +40,38 @@ using namespace casa;
 //using std::complex;
 //using std::abs;
 
-void getBaselines(const Table &table, double *baselines[3], 
+unsigned int getBaselines(const MeasurementSet &ms, double *baselines[3], 
                   int rowStart=-1, int rowEnd=-1);
-vector<double> getMSFrequencies(const Table &table);
-void writeUVW(const Table &table, const string &name);
+Vector<Double> getMSFrequencies(const MeasurementSet &ms);
+void writeData(const MeasurementSet &ms, const string &name);
+void printBaselines(double **baselines, size_t nrows);
 
 int main(int argc, char **argv)
 {
+  string msName;
   string imageFilename=argv[1];
   if(argc==3)
-    string tableName=argv[2];
+    msName=argv[2];
+
 
   LOFAR::BBS::ModelImageFft modelImage(imageFilename, 20);  // nwplanes=20
 
   // TODO: Adjust all this to use ModelImageFFT class implementation
   // Change these if necessary to adjust run time
   //
+  MeasurementSet ms(msName, Table::Update);    // open MS
+  // allocate memory for array to hold baselines
+  double **baselines=static_cast<double**>(malloc(ms.nrow()*sizeof(double)));
+  for(unsigned int i=0; i<3; i++)
+  {
+    baselines[i]=static_cast<double*>(malloc(3*sizeof(double)));
+  }
 
-  MeasurementSet ms(tableName, Table::Update);
-  double *baselines[3];
+  Vector<Double> MSfreqs=getMSFrequencies(ms); // get MS frequencies
+  cout << "MSfreqs: " << MSfreqs << endl;       // DEBUG
 
-  // Get baselines from MS
-  getBaselines(ms, baselines);
+  unsigned int nbaselines=getBaselines(ms, baselines);  // get baselines from MS
+  printBaselines(baselines, nbaselines);      // DEBUG
 
   exit(0);    // DEBUG
   
@@ -148,24 +165,54 @@ int main(int argc, char **argv)
 
 // Get baselines from MS from rowStart till rowEnd
 //
-void getBaselines(const Table &table, double *baselines[3], unsigned int rowStart, unsigned int rowEnd)
+unsigned int getBaselines(const MeasurementSet &ms, double *baselines[3], 
+                          int rowStart, int rowEnd)   // range not supported yet
 {
+  // Example: 711.309, -357.693, 432.535
+ 
   // Check that rowStart and rowEnd are within the limits
-
-// Example: 711.309, -357.693, 432.535
+  uInt nrows=ms.nrow();
+  IPosition shape = ROTableColumn(ms, MS::columnName(MS::UVW)).shapeColumn();
+ 
+  cout << "getBaselines(): nrows: " << nrows << endl;
+  cout << "getBaselines(): shape: " << shape << endl;
 
   // ROTableColumn UVW: Double Array of Size [ 1 3 ]
-  // ROArrayColumn?
+  ROArrayColumn<Double> uvwColumn(ms, "UVW");
+  
+  Array<Double> uvw=uvwColumn.getColumn();
+  for(unsigned int i=0; i<nrows; i++)
+  {
+    baselines[i]=uvw[i].data();   // assign column to array pointer
+  }
+  return nrows;                   // return number of baselines
 }
 
-vector<double> getMSFrequencies(const Table &table)
+Vector<Double> getMSFrequencies(const MeasurementSet &ms)
 {
   // Table: SPECTRAL_WINDOW, Column: CHAN_FREQ
+  // Read MSfrequency from MS/SPECTRAL_WINDOW table
+  Table SpectralWindowTable(ms.keywordSet().asTable("SPECTRAL_WINDOW"));
+  ROArrayColumn<Double> ChanFreqColumn(SpectralWindowTable, "CHAN_FREQ");
 
+  Vector<Double> MSfrequencies=ChanFreqColumn(0);
+  return MSfrequencies;
 }
 
 
-void writeUVW(const Table &table, const string &name)
+void writeData(const Table &table, const string &name, double *data[4])
 {
+  // Add column of name
+
   // Complex Array of size [ 4 nchannels ]
+}
+
+// Helper function to print baselines[3]
+void printBaselines(double **baselines, size_t nrows)
+{
+  cout << "Baselines: u, v, w" << endl;
+  for(unsigned int i=0; i<nrows; i++)
+  {
+    cout << baselines[i][0] << ", " << baselines[i][1] << ", " << baselines[i][2] << endl;
+  }
 }
