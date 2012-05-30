02321afc93e20cd1ac3f8d2eb6dc96e73b51ecbe
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-05-30 13:04:56 +0000

    Task #2115: removed task branch

--- branches/PVSS310-Bug1719-task-branch/CEP/DP3/DPPP/test/tAverager.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./runctest.sh tAverager > tAverager.log 2>&1
--- branches/PVSS310-Bug1719-task-branch/RTCP/Run/src/gracefullyStopBGProcessing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source locations.sh
-
-echo "cancel all" > /dev/tcp/$FIRSTPSET/4000 2>/dev/null &&
-echo "quit"       > /dev/tcp/$FIRSTPSET/4000 2>/dev/null &&
-sleep 5 # allow processes to quit
