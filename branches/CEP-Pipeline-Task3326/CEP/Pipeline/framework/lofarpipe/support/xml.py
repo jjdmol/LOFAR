059b8941a40f3f99e33fd1ef21fe9f560e03f43c
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-06-08 11:08:26 +0000

    Task #3326: Add basic files and CMakeLists.txt

---
+++ branches/CEP-Pipeline-Task3326/CEP/Pipeline/framework/lofarpipe/support/xml.py
@@ -0,0 +1,15 @@
+import xml.com
+
+def read_parset_as_xml(parset_path):
+    """
+    This function reads a parset file using ans
+    returns it as a xml.dom object
+    """
+    pass
+
+def write_xml_to_parset(xml, path):
+    """
+    This function gets an xml object and writes it to path as in the parset 
+    format
+    """
+    pass
