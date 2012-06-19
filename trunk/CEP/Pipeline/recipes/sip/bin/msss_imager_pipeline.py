171fe2bb0ecec244e0d8136d30d72a03a8cf2155
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-06-19 09:54:56 +0000

    Task #3133: Write empty list when touch datamap to ensure valid datamap

--- trunk/CEP/Pipeline/recipes/sip/bin/msss_imager_pipeline.py
+++ trunk/CEP/Pipeline/recipes/sip/bin/msss_imager_pipeline.py
@@ -545,11 +545,11 @@ class msss_imager_pipeline(control):
         mapfile_path = os.path.join(mapfile_dir,
                          "{0}.map".format(mapfile_name))
 
-        if datamap != None:
-            store_data_map(mapfile_path, datamap)
-        else:
-            if not os.path.exists(mapfile_path):
-                open(mapfile_path, 'w').close()
+        # if none then write empty (and thus valid) array
+        if datamap == None:
+            datamap = []
+
+        store_data_map(mapfile_path, datamap)
 
         return mapfile_path
 
