34719bdb147cb8765ca2b16a00eb898cd3740196
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-06-27 08:12:16 +0000

    Task #514: Fixed memory leak when using Log4Cplus with multiple threads.

--- trunk/LCS/Common/include/Common/LofarLogCout.h
+++ trunk/LCS/Common/include/Common/LofarLogCout.h
@@ -75,8 +75,9 @@
 #define LOGCOUT_SETLEVEL(level) \
 	::LOFAR::LFDebug::setLevel("Global",level);
 
-// Each new thread might need a partial reinitialisation in the logger
-#define LOGGER_NEWTHREAD()
+// Each new thread might need a partial reinitialisation and destruction in the logger
+#define LOGGER_ENTER_THREAD()
+#define LOGGER_EXIT_THREAD()
 
 //# -------------------- Log Levels for the Operator messages -----------------
 //#
