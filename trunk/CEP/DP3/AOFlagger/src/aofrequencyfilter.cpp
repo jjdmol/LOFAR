c049d96246260742ded22e14d5f19d0d6b33e8f8
offringa <offringa@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-07-12 10:58:33 +0000

    Task #1892: fixing problem with iterating rows in aofrequencyfilter

--- trunk/CEP/DP3/AOFlagger/src/aofrequencyfilter.cpp
+++ trunk/CEP/DP3/AOFlagger/src/aofrequencyfilter.cpp
@@ -27,18 +27,33 @@ int main(int argc, char *argv[])
 			timerange.SetBandInfo(i, bands[i]);
 		
 		unsigned polarizationCount = 4;
-		size_t timestepCount = 128;
+		size_t rowCountPerRequest = 128;
 		
-		timerange.Initialize(polarizationCount, timestepCount);
+		timerange.Initialize(polarizationCount, rowCountPerRequest);
 		MSRowDataExt *rowBuffer[obs->Size()];
 		for(size_t i=0;i<obs->Size();++i)
-			rowBuffer[i] = new MSRowDataExt[timestepCount];
+			rowBuffer[i] = new MSRowDataExt[rowCountPerRequest];
+		
 		// We ask for "0" rows, which means we will ask for the total number of rows
 		commander.PushReadDataRowsTask(timerange, 0, 0, rowBuffer);
 		commander.Run(false);
 		commander.CheckErrors();
 		const size_t totalRows = commander.RowsTotal();
 		cout << "Total rows to filter: " << totalRows << '\n';
+		
+		size_t currentRow = 0;
+		while(currentRow < totalRows)
+		{
+			size_t currentRowCount = rowCountPerRequest;
+			if(currentRow + currentRowCount > totalRows)
+				currentRowCount = totalRows - currentRow;
+			commander.PushReadDataRowsTask(timerange, 0, currentRowCount, rowBuffer);
+			commander.Run(false);
+			commander.CheckErrors();
+			
+			currentRow += currentRowCount;
+			cout << "Read " << currentRow << '/' << totalRows << '\n';
+		}
 		
 		for(size_t i=0;i<obs->Size();++i)
 			delete[] rowBuffer[i];
