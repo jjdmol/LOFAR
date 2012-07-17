9c043a0f648ec15ecbb521ae247e368a80399b3d
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-07-17 12:57:33 +0000

    Task #3484: Creation of Release branch LOFAR-Release-1_3

---
+++ branches/LOFAR-Release-1_3/MAC/APL/CR_Protocol/src/CRstopRequest.cc
@@ -0,0 +1,67 @@
+//#  CRstopRequest.cc: Serialisable structure holding stop requests for TBBs
+//#
+//#  Copyright (C) 2011
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id: $
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+//# Includes
+#include <Common/LofarLogger.h>
+#include <APL/CR_Protocol/CRstopRequest.h>
+#include <MACIO/Marshalling.h>
+
+namespace LOFAR {
+  namespace CR_Protocol {
+
+// --- Output function --- 
+ostream& CRstopRequest::print (ostream& os) const
+{
+	os << "StopRequest(" << stationList << "," << rcuList << "," << stopTime << ")";
+	return (os);
+}
+
+
+// --- marshalling methods --- 
+unsigned int CRstopRequest::getSize()
+{
+	return(MSH_STRING_SIZE(stationList) + MSH_STRING_SIZE(rcuList) + stopTime.getSize());
+}
+
+unsigned int CRstopRequest::pack  (void* buffer)
+{
+	unsigned int	offset(0);
+	MSH_PACK_STRING(buffer, offset, stationList);	
+	MSH_PACK_STRING(buffer, offset, rcuList);	
+	offset += stopTime.pack((char*)(buffer)+offset);
+	return (offset);
+}
+
+unsigned int CRstopRequest::unpack(void *buffer)
+{
+	unsigned int	offset(0);
+	MSH_UNPACK_STRING(buffer, offset, stationList);	
+	MSH_UNPACK_STRING(buffer, offset, rcuList);	
+	offset += stopTime.unpack((char*)(buffer)+offset);
+	return (offset);
+}
+
+  } // namespace CR_Protocol
+} // namespace LOFAR
