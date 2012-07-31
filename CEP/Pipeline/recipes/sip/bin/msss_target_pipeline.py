9bae4383eb156aefa6aa07f0e4e0457580315e6f
Marcel Loose <gmloose@users.noreply.github.com>
2012-07-31 08:58:14 +0000

    Task #2942: Reintegrated task branch with the trunk

--- CEP/Pipeline/recipes/sip/bin/msss_target_pipeline.py
+++ CEP/Pipeline/recipes/sip/bin/msss_target_pipeline.py
@@ -170,7 +170,7 @@ class msss_target_pipeline(control):
                       mapfile_target=target_path,
                       mapfiles_dir=copier_map_path,
                       mapfile=copied_files_path,
-                      target_dir="instument_models")['mapfile']
+                      target_dir="instrument_models")['mapfile']
 
         return new_instrument_map
 
@@ -256,7 +256,11 @@ class msss_target_pipeline(control):
         vdsinfo = self.run_task("vdsreader", gvds=gvds_file)
 
         # Create an empty parmdb for DPPP
-        parmdb_mapfile = self.run_task("setupparmdb", data_mapfile)['mapfile']
+        parmdb_mapfile = self.run_task(
+            "setupparmdb", data_mapfile,
+            mapfile=os.path.join(mapfile_dir, 'dppp.parmdb.mapfile'),
+            suffix='.dppp.parmdb'
+        )['mapfile']
 
         # Create a sourcedb to be used by the demixing phase of DPPP
         # The path to the A-team sky model is currently hard-coded.
@@ -265,7 +269,9 @@ class msss_target_pipeline(control):
             skymodel=os.path.join(
                 self.config.get('DEFAULT', 'lofarroot'),
                 'share', 'pipeline', 'skymodels', 'Ateam_LBA_CC.skymodel'
-            )
+            ),
+            mapfile=os.path.join(mapfile_dir, 'dppp.sourcedb.mapfile'),
+            suffix='.dppp.sourcedb'
         )['mapfile']
 
         # Create a parameter-subset for DPPP and write it to file.
@@ -290,7 +296,9 @@ class msss_target_pipeline(control):
 
         # Create an empty sourcedb for BBS
         sourcedb_mapfile = self.run_task(
-            "setupsourcedb", data_mapfile
+            "setupsourcedb", data_mapfile,
+            mapfile=os.path.join(mapfile_dir, 'bbs.sourcedb.mapfile'),
+            suffix='.bbs.sourcedb'
         )['mapfile']
 
         # Create a parameter-subset for BBS and write it to file.
