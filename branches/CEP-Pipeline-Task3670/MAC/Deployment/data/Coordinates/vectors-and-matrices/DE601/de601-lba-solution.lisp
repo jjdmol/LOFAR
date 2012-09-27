64ba9c4575239dc4ca48ffe108fc489b0402012e
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-09-27 08:09:59 +0000

    Task #3670: create branche for gsm input validation and fix

---
+++ branches/CEP-Pipeline-Task3670/MAC/Deployment/data/Coordinates/vectors-and-matrices/DE601/de601-lba-solution.lisp
@@ -0,0 +1,12 @@
+(discarded-points
+    NIL)
+(reduced-chi-squared 0.000001820887470935997)
+(normal-vector               (0.6311391 0.0757765 0.7719595))
+(normal-vector-uncertainties (0.0000120 0.0001073 0.0000108))
+(normal-vector-direction-uncertainties-arcsec :max 22.14 :rms 12.93 :avg  8.95)
+(reference-point :etrs (4034031.501 487014.140 4900284.310)
+                 :station-pqr (0.0 0.0 0.0))
+(station-pqr-to-etrs-matrix
+    #2A((-0.1194530812011858 -0.7664166332431587 0.6311390541550764)
+        (0.9928398258194238 -0.09234175444499233 0.07577651781968499)
+        (2.0410389317959687e-4 0.63567172713142 0.7719594637462245)))
