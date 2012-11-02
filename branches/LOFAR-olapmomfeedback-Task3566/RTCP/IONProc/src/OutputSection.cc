8209e5cfc617f957c7e24b09718b07dbe4c68a24
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-11-02 14:14:52 +0000

    Task #3566: Merged trunk into branch

--- branches/LOFAR-olapmomfeedback-Task3566/RTCP/IONProc/src/OutputSection.cc
+++ branches/LOFAR-olapmomfeedback-Task3566/RTCP/IONProc/src/OutputSection.cc
@@ -233,7 +233,8 @@ void OutputSection::notDroppingData(unsigned stream)
 void OutputSection::mainLoop()
 {
 #if defined HAVE_BGP_ION
-  doNotRunOnCore0();
+  //doNotRunOnCore0();
+  runOnCore0();
   //runOnCore0();
   setPriority(2);
 #endif
