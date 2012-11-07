25af8387ac538fd8cb53dc23083edc967aefe690
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-11-07 10:56:47 +0000

    Task #3767: First version of a helper method that implements the quick fix (issue #3633) for the pipeline status feedback to MAC.

---
+++ branches/LOFAR-PipelineFeedback-Task3633/CEP/Pipeline/recipes/sip/helpers/mac_feedback_quickfix.py
@@ -0,0 +1,60 @@
+#                                                         LOFAR IMAGING PIPELINE
+#
+#                                                   MAC Status Feedback QuickFix
+#                                                             Marcel Loose, 2012
+#                                                                loose@astron.nl
+# ------------------------------------------------------------------------------
+
+"""
+This module implements the quick fix (Redmine issue #3633) for the pipeline
+status feedback to MAC. 
+"""
+
+import random
+import socket
+import time
+
+def __try_connect(host, port, tries=5, min_timeout=1.0, max_timeout=5.0):
+    """
+    Try to connect to `host`:`port` up time `tries` times, using a random
+    timeout interval ([`min_timeout` .. `max_timeout`) seconds ) between
+    retries.
+    Return a socket object.
+    Raises `socket.error` if all connect tries fail.
+    """
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        tries -= 1
+        try:
+            sock.connect((host, port))
+        except socket.error, err:
+            print("Could not connect to %s:%s (got %s)" %
+                  (host, str(port), str(err)))
+            if tries > 0:
+                timeout = random.uniform(min_timeout, max_timeout)
+                print("Retrying in %f seconds (%d more %s)." %
+                      (timeout, tries, "try" if tries==1 else "tries"))
+                time.sleep(timeout)
+            else:
+                raise
+        else:
+            return sock
+
+
+def send_status(host, port, status):
+    """
+    Send the pipeline status to a TCP listener at `host`:`port`. If `status` is
+    non-zero, send the string 'ABORT'; otherwise send the string 'FINISHED'.
+    """
+    message = "FINISHED" if status == 0 else "ABORT"
+    sock = __try_connect(host, port)
+    sock.sendall(message)
+    sock.close()
+
+
+if __name__ == "__main__":
+    send_status('localhost', 9999, 0)
+    send_status('localhost', 9999, 1)
+
+
