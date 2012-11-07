e5011e962bee081989bef32e0551f52ca25c22f5
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-11-07 11:07:30 +0000

    Task #3552: Add bugfix for incorrect calculated size and cellsize of images. It now ignores the parset values completely.

--- branches/LOFAR-Release-1_7/CEP/Pipeline/recipes/sip/nodes/imager_awimager.py
+++ branches/LOFAR-Release-1_7/CEP/Pipeline/recipes/sip/nodes/imager_awimager.py
@@ -132,20 +132,19 @@ class imager_awimager(LOFARnodeTCP):
 
     def _calc_par_from_measurement(self, measurement_set, parset):
         """
-        (1) calculate and format some parameters that are determined runtime. 
+        (1) calculate and format some parameters that are determined runtime.
         Based  on values in the measurementset and input parameter (set):
         
-        a. <string> The cellsize 
+        a. <string> The cellsize
         b. <int> The npixels in a each of the two dimension of the image
         c. <string> The largest baseline in the ms smaller then the maxbaseline
         d. <string> The number of projection planes
-                
+        
         The calculation of these parameters is done in three steps:
         
         1. Calculate intermediate results based on the ms. 
-        2. The calculation of the actual target values using intermediate result
-        3. Scaling of cellsize and npix to allow for user input of the npix
-        
+        2. The calculation of the actual target values using intermediate
+           result       
         """
         # *********************************************************************
         # 1. Get partial solutions from the parameter set
@@ -153,26 +152,27 @@ class imager_awimager(LOFARnodeTCP):
         parset_object = get_parset(parset)
         baseline_limit = parset_object.getInt('maxbaseline')
         # npix round up to nearest pow 2
-        parset_npix = self._nearest_ceiled_power2(parset_object.getInt('npix'))
 
-        # Get the longest baseline      
-        sqrt_max_baseline = pt.taql(
+        # Get the longest baseline
+        max_baseline = pt.taql(
                         'CALC sqrt(max([select sumsqr(UVW[:2]) from ' + \
             '{0} where sumsqr(UVW[:2]) <{1} giving as memory]))'.format(\
             measurement_set, baseline_limit *
-            baseline_limit))[0]  #ask ger van diepen for details if ness.
-
+            baseline_limit))[0]  # ask ger van diepen for details if ness.
         #Calculate the wave_length
         table_ms = pt.table(measurement_set)
-        table_spectral_window = pt.table(table_ms.getkeyword("SPECTRAL_WINDOW"))
+        table_spectral_window = pt.table(
+                                        table_ms.getkeyword("SPECTRAL_WINDOW"))
         freq = table_spectral_window.getcell("REF_FREQUENCY", 0)
+
         table_spectral_window.close()
         wave_length = pt.taql('CALC C()') / freq
+        wave_length = wave_length[0]
 
         #Calculate the cell_size from the ms
         arc_sec_in_degree = 3600
         arc_sec_in_rad = (180.0 / math.pi) * arc_sec_in_degree
-        cell_size = (1.0 / 3) * (wave_length / float(sqrt_max_baseline))\
+        cell_size = (1.0 / 3) * (wave_length / float(max_baseline))\
              * arc_sec_in_rad
 
         # Calculate the number of pixels in x and y dim
@@ -192,39 +192,32 @@ class imager_awimager(LOFARnodeTCP):
             measurement_set, baseline_limit * baseline_limit))[0]
 
         # Calculate number of projection planes
-        w_proj_planes = min(257, math.floor((sqrt_max_baseline * wave_length) /
+        w_proj_planes = min(257, math.floor((max_baseline * wave_length) /
                                              (station_diameter ** 2)))
-
         w_proj_planes = int(round(w_proj_planes))
-        self.logger.debug(
-                    "Calculated w_max and the number pf projection plances:"
-                    " {0} , {1}".format(w_max, w_proj_planes))
 
-        # MAximum number of proj planes set to 1024: George Heald, Ger van 
+        # MAximum number of proj planes set to 1024: George Heald, Ger van
         # Diepen if this exception occurs
         maxsupport = max(1024, npix)
         if w_proj_planes > maxsupport:
-            raise Exception("The number of projections planes for the current" +
-                            "measurement set is to large.")
+            raise Exception("The number of projections planes for the current"
+                            + "measurement set is to large.")
 
         # *********************************************************************
         # 3. if the npix from the parset is different to the ms calculations,
         # calculate a sizeconverter value  (to be applied to the cellsize)
-        size_converter = 1
-        if npix != parset_npix:
-            size_converter = npix / parset_npix
-            npix = parset_npix
-
         if npix < 256:
             self.logger.warn("Using a image size smaller then 256x256:"
                 " This leads to problematic imaging in some instances!!")
 
         cell_size_formatted = str(
-                        int(round(cell_size * size_converter))) + 'arcsec'
-        npix = int(npix)
-        self.logger.info("Using the folowing image"
-            " properties: npix: {0}, cell_size: {1}".format(
-              npix, cell_size_formatted))
+                        int(round(cell_size))) + 'arcsec'
+
+        print ("Using the following dynamic generated awimager parameters:"
+            " cell_size: {0}, npix: {1},".format(
+                        cell_size_formatted, npix) +
+             " w_max: {0}, w_proj_planes: {1}".format(w_max, w_proj_planes))
+
         return cell_size_formatted, npix, str(w_max), str(w_proj_planes)
 
     def _get_fov_and_station_diameter(self, measurement_set):
