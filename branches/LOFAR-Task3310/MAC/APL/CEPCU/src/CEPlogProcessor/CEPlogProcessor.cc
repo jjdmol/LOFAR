e465f2c4295422be9e8c6f01d19ef9486338d47a
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-11-22 15:44:39 +0000

    Task #3310: Fixed compilation errors in APL/CEPCU

--- branches/LOFAR-Task3310/MAC/APL/CEPCU/src/CEPlogProcessor/CEPlogProcessor.cc
+++ branches/LOFAR-Task3310/MAC/APL/CEPCU/src/CEPlogProcessor/CEPlogProcessor.cc
@@ -37,6 +37,7 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h> // usleep
+#include <cstdio>
 
 #include "CEPlogProcessor.h"
 #include "PVSSDatapointDefs.h"
@@ -843,7 +844,7 @@ void CEPlogProcessor::_processIONProcLine(const struct logline &logline)
         inputBuffer->setValue("process.logMsg", GCFPVString(logline.fullmsg), logline.timestamp, true);
     }
 
-    char*   result;
+    const char*   result;
 
     // IONProc@00 31-03-11 00:17:22.438 INFO  [obs 24811] ----- Creating new job
     // IONProc@00 31-03-11 00:17:22.550 INFO  [obs 24811] Waiting for job to start: sleeping until Thu Mar 31 00:18:50 2011
@@ -1023,7 +1024,7 @@ void CEPlogProcessor::_processIONProcLine(const struct logline &logline)
 
 void CEPlogProcessor::_processCNProcLine(const struct logline &logline)
 { 
-  char *result;
+  const char *result;
 
   // CNProc@0000 13-02-12 12:13:44.823 WARN  [obs 1003431 phases 111] Station S17 subband 0 consists of only zeros.
   if ((result = strstr(logline.msg, "consists of only zeros"))) {
@@ -1056,7 +1057,7 @@ void CEPlogProcessor::_processStorageLine(const struct logline &logline)
 
     hostNr--; // use 0-based indexing in our arrays
 
-    char*   result;
+    const char*   result;
 
     int writerNr = _getParam(logline.target, "writer ");
 
