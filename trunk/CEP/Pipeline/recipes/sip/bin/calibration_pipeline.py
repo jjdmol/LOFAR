fa1266cd540e61cb91d6b1e2c6bb37c0e60a82d3
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-12-04 12:50:43 +0000

    Task #3860: merge of release 1.9 bug fixes to the trunk: - Add duration loggin to calibrator and target using a context manager - add parmexportcal as option to the pipelines - additional logging on common errors - fix for possible race conditions in master node communication. - mail on errors for target calibrator and non msss pipelines

--- trunk/CEP/Pipeline/recipes/sip/bin/calibration_pipeline.py
+++ trunk/CEP/Pipeline/recipes/sip/bin/calibration_pipeline.py
@@ -14,6 +14,7 @@ from lofarpipe.support.group_data import validate_data_maps, tally_data_map
 from lofarpipe.support.lofarexceptions import PipelineException
 from lofarpipe.support.utilities import create_directory
 from lofar.parameterset import parameterset
+from lofarpipe.support.loggingdecorators import mail_log_on_exception, duration
 
 class calibration_pipeline(control):
     """
@@ -29,7 +30,7 @@ class calibration_pipeline(control):
     4. Create a sourcedb from the user-supplied sky model, and an empty parmdb.
     5. Run BBS to calibrate the data.
     """
-    
+
     def __init__(self):
         super(calibration_pipeline, self).__init__()
         self.parset = parameterset()
@@ -136,7 +137,7 @@ class calibration_pipeline(control):
         # Call the base-class's `go()` method.
         return super(calibration_pipeline, self).go()
 
-
+    @mail_log_on_exception
     def pipeline_logic(self):
         """
         Define the individual tasks that comprise the current pipeline.
@@ -173,10 +174,12 @@ class calibration_pipeline(control):
 
         # *********************************************************************
         # 2. Create VDS-file; it will contain important input-data for NDPPP
-        gvds_file = self.run_task("vdsmaker", input_data_mapfile)['gvds']
+        with duration(self, "vdsmaker"):
+            gvds_file = self.run_task("vdsmaker", input_data_mapfile)['gvds']
 
         # Read metadata (start, end times, pointing direction) from GVDS.
-        vdsinfo = self.run_task("vdsreader", gvds=gvds_file)
+        with duration(self, "vdsreader"):
+            vdsinfo = self.run_task("vdsreader", gvds=gvds_file)
 
         # *********************************************************************
         # 3. Average and flag data, using NDPPP.
@@ -184,11 +187,12 @@ class calibration_pipeline(control):
         py_parset.makeSubset('DPPP.').writeFile(ndppp_parset)
 
         # Run the Default Pre-Processing Pipeline (DPPP);
-        dppp_mapfile = self.run_task("ndppp",
-            input_data_mapfile,
-            data_start_time=vdsinfo['start_time'],
-            data_end_time=vdsinfo['end_time'],
-            parset=ndppp_parset)['mapfile']
+        with duration(self, "ndppp"):
+            dppp_mapfile = self.run_task("ndppp",
+                input_data_mapfile,
+                data_start_time=vdsinfo['start_time'],
+                data_end_time=vdsinfo['end_time'],
+                parset=ndppp_parset)['mapfile']
 
         # *********************************************************************
         # 4. Create a sourcedb from the user-supplied sky model, 
@@ -207,15 +211,16 @@ class calibration_pipeline(control):
             )
         if not os.path.isfile(skymodel):
             raise PipelineException("Skymodel %s does not exist" % skymodel)
+        with duration(self, "setupsourcedb"):
+            sourcedb_mapfile = self.run_task(
+                "setupsourcedb", dppp_mapfile,
+                skymodel=skymodel
+            )['mapfile']
 
-        sourcedb_mapfile = self.run_task(
-            "setupsourcedb", dppp_mapfile, 
-            skymodel=skymodel
-        )['mapfile']
-
-        parmdb_mapfile = self.run_task(
-            "setupparmdb", dppp_mapfile
-        )['mapfile']
+        with duration(self, "setupparmdb"):
+            parmdb_mapfile = self.run_task(
+                "setupparmdb", dppp_mapfile
+            )['mapfile']
 
         # *********************************************************************
         # 5. Run BBS to calibrate the data.
@@ -223,13 +228,15 @@ class calibration_pipeline(control):
         # Create a parameter subset for BBS
         bbs_parset = os.path.join(parset_dir, "BBS.parset")
         py_parset.makeSubset('BBS.').writeFile(bbs_parset)
+        with duration(self, "bbs_reducer"):
+            self.run_task("bbs_reducer",
+                (dppp_mapfile, output_data_mapfile),
+                parset=bbs_parset,
+                instrument_mapfile=parmdb_mapfile,
+                sky_mapfile=sourcedb_mapfile
+            )
 
-        self.run_task("bbs_reducer",
-            (dppp_mapfile, output_data_mapfile),
-            parset=bbs_parset,
-            instrument_mapfile=parmdb_mapfile,
-            sky_mapfile=sourcedb_mapfile
-        )
+        return 0
 
 
 if __name__ == '__main__':
