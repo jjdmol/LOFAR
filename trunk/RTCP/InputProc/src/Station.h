4b32311b736efadc1262c3184c0ed07146fd1ea0
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-12-04 15:24:43 +0000

    Task #3696: Put StationData classes in their own header files

---
+++ trunk/RTCP/InputProc/src/Station.h
@@ -0,0 +1,69 @@
+#ifndef __STATION__
+#define __STATION__
+
+#include <Common/LofarLogger.h>
+#include <Stream/Stream.h>
+#include <Interface/SmartPtr.h>
+#include "SampleBuffer.h"
+#include "StationSettings.h"
+#include "StationData.h"
+#include <string>
+#include <vector>
+
+namespace LOFAR {
+namespace RTCP {
+
+template<typename T> class Station: public StationStreams {
+public:
+  Station( const StationSettings &settings, const std::vector<std::string> &streamDescriptors );
+
+protected:
+  SampleBuffer<T> buffer;
+
+  virtual void processBoard( size_t nr );
+};
+
+template<typename T> Station<T>::Station( const StationSettings &settings, const std::vector<std::string> &streamDescriptors )
+:
+  StationStreams(str(boost::format("[station %s %s] [Station] ") % settings.station.stationName % settings.station.antennaSet), settings, streamDescriptors),
+
+  buffer(settings, true)
+{
+  LOG_INFO_STR( logPrefix << "Initialised" );
+}
+
+template<typename T> void Station<T>::processBoard( size_t nr )
+{
+  const std::string logPrefix(str(boost::format("[station %s %s board %u] [Station] ") % settings.station.stationName % settings.station.antennaSet % nr));
+
+  try {
+    LOG_INFO_STR( logPrefix << "Connecting to " << streamDescriptors[nr] );
+    SmartPtr<Stream> s = createStream(streamDescriptors[nr], true);
+
+    LOG_INFO_STR( logPrefix << "Connecting to shared memory buffer 0x" << std::hex << settings.dataKey );
+    RSPBoard<T> board(*s, buffer, nr, settings);
+
+    LOG_INFO_STR( logPrefix << "Start" );
+
+    for(;;)
+      if (board.readPacket())
+        board.writePacket();
+
+  } catch (Stream::EndOfStreamException &ex) {
+    LOG_INFO_STR( logPrefix << "End of stream");
+  } catch (SystemCallException &ex) {
+    if (ex.error == EINTR)
+      LOG_INFO_STR( logPrefix << "Aborted: " << ex.what());
+    else
+      LOG_ERROR_STR( logPrefix << "Caught Exception: " << ex);
+  } catch (Exception &ex) {
+    LOG_ERROR_STR( logPrefix << "Caught Exception: " << ex);
+  }
+
+  LOG_INFO_STR( logPrefix << "End");
+}
+
+}
+}
+
+#endif
