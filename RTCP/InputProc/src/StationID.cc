14c742169ac79732e3c6124a0f1b807daf99df49
Jan David Mol <mol@astron.nl>
2012-12-05 20:04:04 +0000

    Task #3696: antennaSet -> antennaField, and use strnlen to prevent illegal memory access

--- RTCP/InputProc/src/StationID.cc
+++ RTCP/InputProc/src/StationID.cc
@@ -2,27 +2,40 @@
 #include "StationID.h"
 #include <Common/LofarLogger.h>
 #include <cstdio>
+
 #include <cstring>
+
+#ifndef HAVE_STRNLEN
+static size_t strnlen( const char *s, size_t maxlen )
+{
+  size_t len = 0;
+
+  for(; *s && len < maxlen; ++s)
+    ++len;
+
+  return len;
+}
+#endif
 
 namespace LOFAR {
 namespace RTCP {
 
 
-StationID::StationID( const std::string &stationName, const std::string &antennaSet, unsigned clock, unsigned bitmode)
+StationID::StationID( const std::string &stationName, const std::string &antennaField, unsigned clock, unsigned bitmode)
 :
   clock(clock),
   bitmode(bitmode)
 {
   ASSERTSTR( stationName.size() < sizeof this->stationName, "Station name longer than " << (sizeof this->stationName - 1) << " characters.");
-  ASSERTSTR( antennaSet.size() < sizeof this->antennaSet, "Antenna-set name longer than " << (sizeof this->antennaSet - 1) << " characters.");
+  ASSERTSTR( antennaField.size() < sizeof this->antennaField, "Antenna-set name longer than " << (sizeof this->antennaField - 1) << " characters.");
 
   snprintf(this->stationName, sizeof this->stationName, "%s", stationName.c_str());
-  snprintf(this->antennaSet, sizeof this->antennaSet, "%s", antennaSet.c_str());
+  snprintf(this->antennaField, sizeof this->antennaField, "%s", antennaField.c_str());
 }
 
 bool StationID::operator==(const struct StationID &other) const {
   return !strncmp(stationName, other.stationName, sizeof stationName)
-      && !strncmp(antennaSet, other.antennaSet, sizeof antennaSet)
+      && !strncmp(antennaField, other.antennaField, sizeof antennaField)
       && clock == other.clock
       && bitmode == other.bitmode;
 }
@@ -32,37 +45,48 @@ bool StationID::operator!=(const struct StationID &other) const {
 }
 
 uint32 StationID::hash() const {
-  // convert to 32 bit value (human-readable in hexadecimal)
+  // convert to 32 bit value (human-readable in hexadecimal):
+  //
+  //
+  // 0x0106020F
+  //   \__||\||
+  //      || |\_ bitmode:       F  = 16-bit, 8 = 8-bit, 4 = 4-bit
+  //      || \__ clock:         20 = 200 MHz, 16 = 160 MHz
+  //      |\____ antenna field: 0 = HBA/HBA0/LBA, 1 = HBA1
+  //      \_____ station ID:    0x0106 = RS106
+  
   uint32 stationNr = 0;
 
-  const std::string stationNameStr(stationName);
-  const std::string antennaSetStr(antennaSet);
+  const std::string stationNameStr(stationName, strnlen(stationName, sizeof stationName));
+  const std::string antennaFieldStr(antennaField, strnlen(antennaField, sizeof antennaField));
 
   for(std::string::const_iterator c = stationNameStr.begin(); c != stationNameStr.end(); ++c)
     if(*c >= '0' && *c <= '9')
       stationNr = stationNr * 16 + (*c - '0');
 
-  uint32 antennaSetNr = 0;
+  uint32 antennaFieldNr = 0;
 
-  if (antennaSetStr == "HBA_ONE" || antennaSetStr == "HBA1" )
-    antennaSetNr = 1;
+  if (antennaFieldStr == "HBA1")
+    antennaFieldNr = 1;
   else
-    antennaSetNr = 0;
+    antennaFieldNr = 0;
 
+  // make sure everything fits
   ASSERT( stationNr    < (1L << 16) );
-  ASSERT( antennaSetNr < (1L << 4)  );
+  ASSERT( antennaFieldNr < (1L << 4)  );
 
   ASSERT( clock/1000000 == 200 || clock/1000000 == 160 );
   ASSERT( bitmode == 4 || bitmode == 8 || bitmode == 16 );
 
+  // derive the hash
   unsigned clockNr = clock/1000000 == 200 ? 0x20 : 0x16;
   unsigned bitmodeNr = bitmode == 16 ? 0xF : bitmode;
 
-  return (stationNr << 16) + (antennaSetNr << 12) + (clockNr << 4) + bitmodeNr;
+  return (stationNr << 16) + (antennaFieldNr << 12) + (clockNr << 4) + bitmodeNr;
 }
 
 std::ostream& operator<<( std::ostream &str, const struct StationID &s ) {
-  str << "station " << s.stationName << " antennaset " << s.antennaSet << " clock " << s.clock/1000000 << " bitmode " << s.bitmode;
+  str << "station " << s.stationName << " antenna field " << s.antennaField << " clock " << s.clock/1000000 << " bitmode " << s.bitmode;
 
   return str;
 }
