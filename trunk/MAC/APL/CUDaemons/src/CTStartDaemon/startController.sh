5063ebb4adc97db92a20a4cff2477d10860b812f
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-12-09 08:12:26 +0000

    Task #2669: Add date+time to startController output log lines

--- trunk/MAC/APL/CUDaemons/src/CTStartDaemon/startController.sh
+++ trunk/MAC/APL/CUDaemons/src/CTStartDaemon/startController.sh
@@ -4,7 +4,7 @@
 #
 
 # start process
-($1 $2 $3 $4 1>>"/opt/lofar/var/log/$2.stdout" 2>&1 ) &
+($1 $2 $3 $4 1 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S"),$0; }' >>"/opt/lofar/var/log/$2.stdout") &
 
 # get its pid
 # echo $!
