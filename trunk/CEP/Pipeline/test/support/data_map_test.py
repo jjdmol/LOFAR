31b694c3251035c7123bdc7ac2c566280a2bd184
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2012-12-14 12:17:04 +0000

    Task #3938: Bug Fix: On missing all ms of an image the mapfiles got out of sync. To solve this the mapfile skip had to be implemented in a part of the pipeline. Refactored some repetitive mapfile functionality into a helper function, including unittesting.

--- trunk/CEP/Pipeline/test/support/data_map_test.py
+++ trunk/CEP/Pipeline/test/support/data_map_test.py
@@ -5,8 +5,10 @@ import unittest
 
 from lofarpipe.support.data_map import (
     DataMap, MultiDataMap, DataProduct, MultiDataProduct,
-    DataMapError, load_data_map, store_data_map
+    DataMapError, load_data_map, store_data_map, align_data_maps
 )
+
+from lofarpipe.support.lofarexceptions import DataMapError
 
 class DataMapTest(unittest.TestCase):
     """
@@ -252,6 +254,161 @@ class MultiDataMapTest(unittest.TestCase):
         multi_data_map = MultiDataMap([])
         # Empty maps should be unequal also
         self.assertNotEqual(data_map, multi_data_map)
+
+
+class HelperFunctionDataMapTest(unittest.TestCase):
+    """
+    Test class for the helper functions in lofarpipe.support.data_map
+    Currently the align_data_maps is tested
+    """
+    def __init__(self, arg):
+        super(HelperFunctionDataMapTest, self).__init__(arg)
+        self.old_style_multi_map = [
+            ('locus001', ['L12345_SB101.MS']),
+            ('locus002', ['L12345_SB102.MS']),
+            ('locus003', ['L12345_SB103.MS']),
+            ('locus004', ['L12345_SB104.MS'])
+        ]
+        self.new_style_multi_map = [
+            {'host': 'locus001', 'file': ['L12345_SB101.MS'],
+             'file_skip':[True], 'skip': True},
+            {'host': 'locus002', 'file': ['L12345_SB102.MS'],
+             'file_skip':[False], 'skip': False},
+            {'host': 'locus003', 'file': ['L12345_SB103.MS'],
+             'file_skip':[True], 'skip': True},
+            {'host': 'locus004', 'file': ['L12345_SB104.MS'],
+             'file_skip':[False], 'skip': False}
+        ]
+        self.new_style_map = [
+            {'host': 'locus001', 'file': 'L12345_SB101.MS', 'skip': True},
+            {'host': 'locus002', 'file': 'L12345_SB102.MS', 'skip': False},
+            {'host': 'locus003', 'file': 'L12345_SB103.MS', 'skip': True},
+            {'host': 'locus004', 'file': 'L12345_SB104.MS', 'skip': False}
+        ]
+
+    def setUp(self):
+        """
+        Create scratch directory and create required input files in there.
+        """
+        self.tmpdir = tempfile.mkdtemp()
+        self.old_style_map_file = self._create_old_style_map_file()
+        self.new_style_map_file = self._create_new_style_map_file()
+        self.syntax_error_map_file = self._create_syntax_error_map_file()
+
+    def tearDown(self):
+        """
+        Cleanup all the files that were produced by this test
+        """
+        shutil.rmtree(self.tmpdir, ignore_errors=True)
+
+    def _create_old_style_map_file(self):
+        f = open(os.path.join(self.tmpdir, 'old_style.map'), 'w')
+        f.write(repr(self.old_style_multi_map))
+        f.close()
+        return f.name
+
+    def _create_new_style_map_file(self):
+        f = open(os.path.join(self.tmpdir, 'new_style.map'), 'w')
+        f.write(repr(self.new_style_multi_map))
+        f.close()
+        return f.name
+
+    def _create_syntax_error_map_file(self):
+        f = open(os.path.join(self.tmpdir, 'syntax_error.map'), 'w')
+        f.write("[{'host': 'locus001']")
+        f.close()
+        return f.name
+
+    def test_align_data_maps_not_enough_arguments(self):
+        # If called with zere datamaps expect DataMapError
+        self.assertRaises(DataMapError, align_data_maps)
+
+        # if called with single datamap expect DataMapError
+        data_map = MultiDataMap(self.new_style_multi_map)
+        self.assertRaises(DataMapError, align_data_maps, [data_map])
+
+    def test_align_data_maps_different_length_maps(self):
+        data_map = MultiDataMap(self.new_style_multi_map)
+        data_map_other_length = MultiDataMap(
+            [{'host': 'locus001', 'file': ['L12345_SB101.MS'],
+             'file_skip':[True], 'skip': True}])
+
+        self.assertRaises(DataMapError, align_data_maps, [
+                                data_map, data_map_other_length])
+
+
+    def test_align_data_maps_equal_maps_no_change(self):
+        data_map = MultiDataMap(self.new_style_multi_map)
+        data_map2 = MultiDataMap(self.new_style_multi_map)
+        data_map3 = MultiDataMap(self.new_style_multi_map)
+
+        # Perform an align
+        align_data_maps(data_map, data_map2)
+
+        # There should be no changes and the three maps should be the same
+        for entrie1, entrie2, entrie3 in zip(data_map, data_map2, data_map3):
+            self.assertEqual(entrie1, entrie3)
+            self.assertEqual(entrie2, entrie3)
+
+    def test_align_data_maps_equal_maps_skip_set(self):
+        data_map = MultiDataMap(self.new_style_multi_map)
+        data_map2 = MultiDataMap(self.new_style_multi_map)
+
+        # Change a single entrie in the datamap to True
+        data_map.data[1].skip = True
+
+        # Perform an align
+        align_data_maps(data_map, data_map2)
+
+        # The second entrie.skip should be set to True 
+        self.assertTrue(data_map2.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+
+    def test_align_data_maps_equal_maps_skip_set_5_maps(self):
+        # test for DataMap align 
+        data_map = DataMap(self.new_style_map)
+        data_map2 = DataMap(self.new_style_map)
+        data_map3 = DataMap(self.new_style_map)
+        data_map4 = DataMap(self.new_style_map)
+        data_map5 = DataMap(self.new_style_map)
+
+        # change single skip value 
+        data_map.data[1].skip = True
+
+        # Perform an align
+        align_data_maps(data_map, data_map2, data_map3, data_map4, data_map5)
+
+        self.assertTrue(data_map2.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map3.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map4.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map5.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+
+    def test_align_data_maps_equal_maps_skip_set_5_multi_maps(self):
+        # test for DataMap align 
+        data_map = MultiDataMap(self.new_style_multi_map)
+        data_map2 = MultiDataMap(self.new_style_multi_map)
+        data_map3 = MultiDataMap(self.new_style_multi_map)
+        data_map4 = MultiDataMap(self.new_style_multi_map)
+        data_map5 = MultiDataMap(self.new_style_multi_map)
+
+        # change single skip value 
+        data_map.data[1].skip = True
+
+        # Perform an align
+        align_data_maps(data_map, data_map2, data_map3, data_map4, data_map5)
+
+        self.assertTrue(data_map2.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map3.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map4.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
+        self.assertTrue(data_map5.data[2].skip, "The skip field was not"
+                         " alligned correctly in the second entrie")
 
 if __name__ == '__main__':
     import xmlrunner
