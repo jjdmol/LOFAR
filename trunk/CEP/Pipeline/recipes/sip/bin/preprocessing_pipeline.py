99976d827abb14de48d6d64e7017218749aade74
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-01-07 14:34:20 +0000

    Task #3995: Merged task branch into the trunk.

--- trunk/CEP/Pipeline/recipes/sip/bin/preprocessing_pipeline.py
+++ trunk/CEP/Pipeline/recipes/sip/bin/preprocessing_pipeline.py
@@ -192,6 +192,18 @@ class preprocessing_pipeline(control):
                 data_end_time=vdsinfo['end_time'],
                 parset=ndppp_parset)
 
+        # *********************************************************************
+        # 6. Create feedback file for further processing by the LOFAR framework
+        # (MAC)
+        # Create a parset-file containing the metadata for MAC/SAS
+        with duration(self, "get_metadata"):
+            self.run_task("get_metadata", output_data_mapfile,
+                parset_file=self.parset_feedback_file,
+                parset_prefix=(
+                    self.parset.getString('prefix') +
+                    self.parset.fullModuleName('DataProducts')),
+                product_type="Correlated")
+
         return 0
 
 
