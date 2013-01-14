29bdcf6d511f0b02d78d1c5ff653c074d8e5f7b1
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-01-14 10:52:06 +0000

    Task #3944: Created task branch

---
+++ branches/LCS-Common-Task3944/RTCP/GPUProc/src/OpenMP_Support.h
@@ -0,0 +1,54 @@
+#if !defined OPEN_MP_SUPPORT_H
+#define OPEN_MP_SUPPORT_H
+
+#include <omp.h>
+
+
+class OMP_Lock
+{
+  public:
+    OMP_Lock()
+    {
+      omp_init_lock(&omp_lock);
+    }
+
+    ~OMP_Lock()
+    {
+      omp_destroy_lock(&omp_lock);
+    }
+
+    void lock()
+    {
+      omp_set_lock(&omp_lock);
+    }
+
+    void unlock()
+    {
+      omp_unset_lock(&omp_lock);
+    }
+
+  private:
+    omp_lock_t omp_lock;
+};
+
+
+class OMP_ScopedLock
+{
+  public:
+    OMP_ScopedLock(OMP_Lock &omp_lock)
+    :
+      omp_lock(omp_lock)
+    {
+      omp_lock.lock();
+    }
+
+    ~OMP_ScopedLock()
+    {
+      omp_lock.unlock();
+    }
+
+  private:
+    OMP_Lock &omp_lock;
+};
+
+#endif
