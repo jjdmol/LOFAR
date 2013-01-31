3d8796b5837d3106af96277c7016863edbbc0e9c
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-01-31 13:00:49 +0000

    Task #4043: Removing task

--- branches/PVSS-DbUpdate-Task4043/CEP/GSM/bremen/run_parset.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import sys
-from src.gsmparset import GSMParset
-from src.pipeline import GSMPipeline
-parset = GSMParset(sys.argv[1])
-pip = GSMPipeline(use_monet=False, database='test')
-pip.run_parset(parset)
