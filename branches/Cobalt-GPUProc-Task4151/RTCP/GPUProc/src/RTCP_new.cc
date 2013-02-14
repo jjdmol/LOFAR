0f500b8a32dabac92d92f94895072efb15261bc3
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-14 12:39:30 +0000

    Task #4151: add IncoherentStokesKernel

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -49,6 +49,7 @@
 #include "Kernels/DelayAndBandPassKernel.h"
 #include "Kernels/CorrelatorKernel.h"
 #include "Kernels/IntToFloatKernel.h"
+#include "Kernels/IncoherentStokesKernel.h"
 
 #if defined __linux__
 #include <sched.h>
@@ -214,31 +215,6 @@ namespace LOFAR {
                 :
             FFT_Kernel(context, ps.nrChannelsPerSubband(), ps.nrStations() * NR_POLARIZATIONS * ps.nrSamplesPerChannel(), true, devFilteredData)
             {
-            }
-        };
-
-                
-        class IncoherentStokesKernel : public Kernel
-        {
-        public:
-            IncoherentStokesKernel(const Parset &ps, cl::CommandQueue &queue, cl::Program &program, cl::Buffer &devIncoherentStokes, cl::Buffer &devInputSamples)
-                :
-            Kernel(ps, program, "incoherentStokes")
-            {
-                setArg(0, devIncoherentStokes);
-                setArg(1, devInputSamples);
-
-                unsigned nrTimes = ps.nrSamplesPerChannel() / ps.incoherentStokesTimeIntegrationFactor();
-                size_t maxNrThreads;
-                getWorkGroupInfo(queue.getInfo<CL_QUEUE_DEVICE>(), CL_KERNEL_WORK_GROUP_SIZE, &maxNrThreads);
-                unsigned nrPasses = (nrTimes + maxNrThreads - 1) / maxNrThreads;
-                unsigned nrTimesPerPass = (nrTimes + nrPasses - 1) / nrPasses;
-                globalWorkSize = cl::NDRange(nrTimesPerPass * nrPasses, ps.nrChannelsPerSubband());
-                localWorkSize  = cl::NDRange(nrTimesPerPass, 1);
-
-                nrOperations   = ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * ps.nrStations() * (ps.nrIncoherentStokes() == 1 ? 8 : 20 + 2.0 / ps.incoherentStokesTimeIntegrationFactor());
-                nrBytesRead    = (size_t) ps.nrStations() * ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * NR_POLARIZATIONS * sizeof(std::complex<float>);
-                nrBytesWritten = (size_t) ps.nrIncoherentStokes() * nrTimes * ps.nrChannelsPerSubband() * sizeof(float);
             }
         };
 
