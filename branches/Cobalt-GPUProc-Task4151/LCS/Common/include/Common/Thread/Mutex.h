9736d466948d8494408c461ef6ad4b43fe47108b
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-14 10:08:38 +0000

    Task #4151: Modified Mutex class such that you can now create recursive mutexes and error-checked mutexes as well. Updated the test program as well.

--- branches/Cobalt-GPUProc-Task4151/LCS/Common/include/Common/Thread/Mutex.h
+++ branches/Cobalt-GPUProc-Task4151/LCS/Common/include/Common/Thread/Mutex.h
@@ -33,16 +33,36 @@ namespace LOFAR {
 class Mutex
 {
   public:
-    Mutex(), ~Mutex();
+    enum Type {
+#ifdef USE_THREADS
+      NORMAL = PTHREAD_MUTEX_NORMAL,
+      RECURSIVE = PTHREAD_MUTEX_RECURSIVE,
+      ERRORCHECK = PTHREAD_MUTEX_ERRORCHECK,
+      DEFAULT = PTHREAD_MUTEX_DEFAULT
+#else
+      NORMAL,
+      RECURSIVE,
+      ERRORCHECK,
+      DEFAULT
+#endif
+    };
+    
+    Mutex(Type type=DEFAULT);
+    ~Mutex();
 
-    void lock(), unlock();
+    void lock();
+    void unlock();
     bool trylock();
 
   private:
     friend class Condition;
 
+    Mutex(const Mutex&);
+    Mutex& operator=(const Mutex&);
+    
 #ifdef USE_THREADS    
     pthread_mutex_t mutex;
+    pthread_mutexattr_t mutexattr;
 #endif    
 };
 
@@ -61,13 +81,24 @@ class ScopedLock
 };
 
 
-inline Mutex::Mutex()
+inline Mutex::Mutex(Mutex::Type type)
 {
 #ifdef USE_THREADS
-  int error = pthread_mutex_init(&mutex, 0);
+  int error;
 
+  error = pthread_mutexattr_init(&mutexattr);
+  if (error != 0)
+    throw SystemCallException("pthread_mutexattr_init", error, THROW_ARGS);
+    
+  error = pthread_mutexattr_settype(&mutexattr, type);
+  if (error != 0)
+    throw SystemCallException("pthread_mutexattr_settype", error, THROW_ARGS);
+
+  error = pthread_mutex_init(&mutex, &mutexattr);
   if (error != 0)
     throw SystemCallException("pthread_mutex_init", error, THROW_ARGS);
+#else
+  (void)type;
 #endif    
 }
 
@@ -75,9 +106,10 @@ inline Mutex::Mutex()
 inline Mutex::~Mutex()
 {
 #ifdef USE_THREADS
-  // We can't log any errors because the logger will also use this mutex class.
-  // So it's no use recording the return value.
+  // We can't log any errors because the logger will also use this mutex
+  // class. So it's no use recording the return value.
   (void)pthread_mutex_destroy(&mutex);
+  (void)pthread_mutexattr_destroy(&mutexattr);
 #endif    
 }
 
