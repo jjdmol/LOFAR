26929cdcc05e4b32fe4a64f7947f7b496de4dec5
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-14 12:48:34 +0000

    Task #4151: add BeamFormerKernel

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -50,6 +50,7 @@
 #include "Kernels/CorrelatorKernel.h"
 #include "Kernels/IntToFloatKernel.h"
 #include "Kernels/IncoherentStokesKernel.h"
+#include "Kernels/BeamFormerKernel.h"
 
 #if defined __linux__
 #include <sched.h>
@@ -215,35 +216,6 @@ namespace LOFAR {
                 :
             FFT_Kernel(context, ps.nrChannelsPerSubband(), ps.nrStations() * NR_POLARIZATIONS * ps.nrSamplesPerChannel(), true, devFilteredData)
             {
-            }
-        };
-
-
-        class BeamFormerKernel : public Kernel
-        {
-        public:
-            BeamFormerKernel(const Parset &ps, cl::Program &program, cl::Buffer &devComplexVoltages, cl::Buffer &devCorrectedData, cl::Buffer &devBeamFormerWeights)
-                :
-            Kernel(ps, program, "complexVoltages")
-            {
-                setArg(0, devComplexVoltages);
-                setArg(1, devCorrectedData);
-                setArg(2, devBeamFormerWeights);
-
-                globalWorkSize = cl::NDRange(NR_POLARIZATIONS, ps.nrTABs(0), ps.nrChannelsPerSubband());
-                localWorkSize  = cl::NDRange(NR_POLARIZATIONS, ps.nrTABs(0), 1);
-
-                // FIXME: nrTABs
-                //queue.enqueueNDRangeKernel(*this, cl::NullRange, cl::NDRange(16, ps.nrTABs(0), ps.nrChannelsPerSubband()), cl::NDRange(16, ps.nrTABs(0), 1), 0, &event);
-
-                size_t count = ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * NR_POLARIZATIONS;
-                size_t nrWeightsBytes = ps.nrStations() * ps.nrTABs(0) * ps.nrChannelsPerSubband() * NR_POLARIZATIONS * sizeof(std::complex<float>);
-                size_t nrSampleBytesPerPass = count * ps.nrStations() * sizeof(std::complex<float>);
-                size_t nrComplexVoltagesBytesPerPass = count * ps.nrTABs(0) * sizeof(std::complex<float>);
-                unsigned nrPasses = std::max((ps.nrStations() + 6) / 16, 1U);
-                nrOperations   = count * ps.nrStations() * ps.nrTABs(0) * 8;
-                nrBytesRead    = nrWeightsBytes + nrSampleBytesPerPass + (nrPasses - 1) * nrComplexVoltagesBytesPerPass;
-                nrBytesWritten = nrPasses * nrComplexVoltagesBytesPerPass;
             }
         };
 
