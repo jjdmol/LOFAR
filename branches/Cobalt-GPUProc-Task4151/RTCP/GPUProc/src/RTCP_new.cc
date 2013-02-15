d8747122891b3f136156f78f57fabdab1d9577cb
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-15 11:01:31 +0000

    Task #4151: Add the first unittest in seperate directory

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -64,6 +64,9 @@
 #include "Kernels/Filter_FFT_Kernel.h"
 #include "Kernels/DedispersionForwardFFTkernel.h"
 #include "Kernels/DedispersionBackwardFFTkernel.h"
+
+#include "UnitTests/IncoherentStokesTest.h"
+
 #if defined __linux__
 #include <sched.h>
 #include <sys/time.h>
@@ -1115,31 +1118,31 @@ namespace LOFAR {
 #endif
 
 
-        struct IncoherentStokesTest : public UnitTest
-        {
-            IncoherentStokesTest(const Parset &ps)
-                :
-            UnitTest(ps, "BeamFormer/IncoherentStokes.cl")
-            {
-                if (ps.nrStations() >= 5 && ps.nrChannelsPerSubband() >= 14 && ps.nrSamplesPerChannel() >= 108) {
-                    MultiArraySharedBuffer<std::complex<float>, 4> inputData(boost::extents[ps.nrStations()][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY);
-                    MultiArraySharedBuffer<float, 3> stokesData(boost::extents[ps.nrIncoherentStokes()][ps.nrSamplesPerChannel() / ps.incoherentStokesTimeIntegrationFactor()][ps.nrChannelsPerSubband()], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
-                    IncoherentStokesKernel kernel(ps, queue, program, stokesData, inputData);
+        //struct IncoherentStokesTest : public UnitTest
+        //{
+        //    IncoherentStokesTest(const Parset &ps)
+        //        :
+        //    UnitTest(ps, "BeamFormer/IncoherentStokes.cl")
+        //    {
+        //        if (ps.nrStations() >= 5 && ps.nrChannelsPerSubband() >= 14 && ps.nrSamplesPerChannel() >= 108) {
+        //            MultiArraySharedBuffer<std::complex<float>, 4> inputData(boost::extents[ps.nrStations()][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY);
+        //            MultiArraySharedBuffer<float, 3> stokesData(boost::extents[ps.nrIncoherentStokes()][ps.nrSamplesPerChannel() / ps.incoherentStokesTimeIntegrationFactor()][ps.nrChannelsPerSubband()], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
+        //            IncoherentStokesKernel kernel(ps, queue, program, stokesData, inputData);
 
-                    inputData[4][13][107][0] = std::complex<float>(2, 3);
-                    inputData[4][13][107][1] = std::complex<float>(4, 5);
+        //            inputData[4][13][107][0] = std::complex<float>(2, 3);
+        //            inputData[4][13][107][1] = std::complex<float>(4, 5);
 
-                    inputData.hostToDevice(CL_FALSE);
-                    kernel.enqueue(queue, counter);
-                    stokesData.deviceToHost(CL_TRUE);
+        //            inputData.hostToDevice(CL_FALSE);
+        //            kernel.enqueue(queue, counter);
+        //            stokesData.deviceToHost(CL_TRUE);
 
-                    const static float expected[] = { 54, -28, 46, 4 };
+        //            const static float expected[] = { 54, -28, 46, 4 };
 
-                    for (unsigned stokes = 0; stokes < ps.nrIncoherentStokes(); stokes ++)
-                        check(stokesData[stokes][107 / ps.incoherentStokesTimeIntegrationFactor()][13], expected[stokes]);
-                }
-            }
-        };
+        //            for (unsigned stokes = 0; stokes < ps.nrIncoherentStokes(); stokes ++)
+        //                check(stokesData[stokes][107 / ps.incoherentStokesTimeIntegrationFactor()][13], expected[stokes]);
+        //        }
+        //    }
+        //};
 
 
         struct IntToFloatTest : public UnitTest
