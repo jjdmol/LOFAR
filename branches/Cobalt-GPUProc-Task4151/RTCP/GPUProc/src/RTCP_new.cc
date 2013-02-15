44e48856a561364bc4145b2df3b9c6f50bed209c
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-15 12:43:58 +0000

    Task #4151: add DedispersionChirpTest

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -68,6 +68,7 @@
 #include "UnitTests/IncoherentStokesTest.h"
 #include "UnitTests/IntToFloatTest.h"
 #include "UnitTests/BeamFormerTransposeTest.h"
+#include "UnitTests/DedispersionChirpTest.h"
 
 #if defined __linux__
 #include <sched.h>
@@ -1153,29 +1154,29 @@ namespace LOFAR {
             }
         };
 
-        struct DedispersionChirpTest : public UnitTest
-        {
-            DedispersionChirpTest(const Parset &ps)
-                :
-            UnitTest(ps, "BeamFormer/Dedispersion.cl")
-            {
-                if (ps.nrTABs(0) > 3 && ps.nrChannelsPerSubband() > 13 && ps.nrSamplesPerChannel() / ps.dedispersionFFTsize() > 1 && ps.dedispersionFFTsize() > 77) {
-                    MultiArraySharedBuffer<std::complex<float>, 5> data(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel() / ps.dedispersionFFTsize()][ps.dedispersionFFTsize()], queue, CL_MEM_READ_WRITE, CL_MEM_READ_WRITE);
-                    MultiArraySharedBuffer<float, 1> DMs(boost::extents[ps.nrTABs(0)], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
-                    DedispersionChirpKernel dedispersionChirpKernel(ps, program, queue, data, DMs);
+        //struct DedispersionChirpTest : public UnitTest
+        //{
+        //    DedispersionChirpTest(const Parset &ps)
+        //        :
+        //    UnitTest(ps, "BeamFormer/Dedispersion.cl")
+        //    {
+        //        if (ps.nrTABs(0) > 3 && ps.nrChannelsPerSubband() > 13 && ps.nrSamplesPerChannel() / ps.dedispersionFFTsize() > 1 && ps.dedispersionFFTsize() > 77) {
+        //            MultiArraySharedBuffer<std::complex<float>, 5> data(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel() / ps.dedispersionFFTsize()][ps.dedispersionFFTsize()], queue, CL_MEM_READ_WRITE, CL_MEM_READ_WRITE);
+        //            MultiArraySharedBuffer<float, 1> DMs(boost::extents[ps.nrTABs(0)], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
+        //            DedispersionChirpKernel dedispersionChirpKernel(ps, program, queue, data, DMs);
 
-                    data[3][1][13][1][77] = std::complex<float>(2, 3);
-                    DMs[3] = 2;
+        //            data[3][1][13][1][77] = std::complex<float>(2, 3);
+        //            DMs[3] = 2;
 
-                    DMs.hostToDevice(CL_FALSE);
-                    data.hostToDevice(CL_FALSE);
-                    dedispersionChirpKernel.enqueue(queue, counter, 60e6);
-                    data.deviceToHost(CL_TRUE);
+        //            DMs.hostToDevice(CL_FALSE);
+        //            data.hostToDevice(CL_FALSE);
+        //            dedispersionChirpKernel.enqueue(queue, counter, 60e6);
+        //            data.deviceToHost(CL_TRUE);
 
-                    std::cout << data[3][1][13][1][77] << std::endl;
-                }
-            }
-        };
+        //            std::cout << data[3][1][13][1][77] << std::endl;
+        //        }
+        //    }
+        //};
 
 
         struct CoherentStokesTest : public UnitTest
