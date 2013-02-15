b2f30e544ab80c8077f6af17dee6fcc9b7136926
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-15 13:33:04 +0000

    Task #4151: CorrelatorTest and FFT_Test

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -76,6 +76,8 @@
 #include "UnitTests/CorrelateTriangleTest.h"
 #include "UnitTests/UHEP_TriggerTest.h"
 #include "UnitTests/CorrelateRectangleTest.h"
+#include "UnitTests/CorrelatorTest.h"
+#include "UnitTests/FFT_Test.h"
 
 #if defined __linux__
 #include <sched.h>
@@ -1029,90 +1031,9 @@ namespace LOFAR {
             }
         }
 
-        struct CorrelatorTest : public UnitTest
-        {
-            CorrelatorTest(const Parset &ps)
-                :
-#if defined USE_NEW_CORRELATOR
-            UnitTest(ps, "NewCorrelator.cl")
-#else
-            UnitTest(ps, "Correlator.cl")
-#endif
-            {
-                if (ps.nrStations() >= 5 && ps.nrChannelsPerSubband() >= 6 && ps.nrSamplesPerChannel() >= 100) {
-                    MultiArraySharedBuffer<std::complex<float>, 4> visibilities(boost::extents[ps.nrBaselines()][ps.nrChannelsPerSubband()][NR_POLARIZATIONS][NR_POLARIZATIONS], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
-                    MultiArraySharedBuffer<std::complex<float>, 4> inputData(boost::extents[ps.nrStations()][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY);
-                    CorrelatorKernel correlator(ps, queue, program, visibilities, inputData);
-
-                    //inputData[3][5][99][1] = std::complex<float>(3, 4);
-                    //inputData[4][5][99][1] = std::complex<float>(5, 6);
-                    inputData[0][5][99][1] = std::complex<float>(3, 4);
-                    inputData[2][5][99][1] = std::complex<float>(5, 6);
-
-                    visibilities.hostToDevice(CL_FALSE);
-                    inputData.hostToDevice(CL_FALSE);
-                    correlator.enqueue(queue, counter);
-                    visibilities.deviceToHost(CL_TRUE);
-
-                    //check(visibilities[13][5][1][1], std::complex<float>(39, 2));
-                    //check(visibilities[5463][5][1][1], std::complex<float>(39, 2));
-                    for (unsigned bl = 0; bl < ps.nrBaselines(); bl ++)
-                        if (visibilities[bl][5][1][1] != std::complex<float>(0, 0))
-                            std::cout << "bl = " << bl << ", visibility = " << visibilities[bl][5][1][1] << std::endl;
-                }
-            }
-        };
+        
 
 
-    
-
-
-#if 0
-        struct FFT_Test : public UnitTest
-        {
-            FFT_Test(const Parset &ps)
-                : UnitTest(ps, "fft.cl")
-            {
-                MultiArraySharedBuffer<std::complex<float>, 1> in(boost::extents[8], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY);
-                MultiArraySharedBuffer<std::complex<float>, 1> out(boost::extents[8], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY);
-
-                for (unsigned i = 0; i < 8; i ++)
-                    in[i] = std::complex<float>(2 * i + 1, 2 * i + 2);
-
-                clAmdFftSetupData setupData;
-                cl::detail::errHandler(clAmdFftInitSetupData(&setupData), "clAmdFftInitSetupData");
-                setupData.debugFlags = CLFFT_DUMP_PROGRAMS;
-                cl::detail::errHandler(clAmdFftSetup(&setupData), "clAmdFftSetup");
-
-                clAmdFftPlanHandle plan;
-                size_t dim[1] = { 8 };
-
-                cl::detail::errHandler(clAmdFftCreateDefaultPlan(&plan, context(), CLFFT_1D, dim), "clAmdFftCreateDefaultPlan");
-                cl::detail::errHandler(clAmdFftSetResultLocation(plan, CLFFT_OUTOFPLACE), "clAmdFftSetResultLocation");
-                cl::detail::errHandler(clAmdFftSetPlanBatchSize(plan, 1), "clAmdFftSetPlanBatchSize");
-                cl::detail::errHandler(clAmdFftBakePlan(plan, 1, &queue(), 0, 0), "clAmdFftBakePlan");
-
-                in.hostToDevice(CL_FALSE);
-                cl_mem ins[1] = { ((cl::Buffer) in)() };
-                cl_mem outs[1] = { ((cl::Buffer) out)() };
-#if 1
-                cl::detail::errHandler(clAmdFftEnqueueTransform(plan, CLFFT_FORWARD, 1, &queue(), 0, 0, 0, ins, outs, 0), "clAmdFftEnqueueTransform");
-#else
-                cl::Kernel kernel(program, "fft_fwd");
-                kernel.setArg(0, (cl::Buffer) in);
-                kernel.setArg(1, (cl::Buffer) out);
-                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(64, 1, 1), cl::NDRange(64, 1, 1));
-#endif
-                out.deviceToHost(CL_TRUE);
-
-                for (unsigned i = 0; i < 8; i ++)
-                    std::cout << out[i] << std::endl;
-
-                cl::detail::errHandler(clAmdFftDestroyPlan(&plan), "clAmdFftDestroyPlan");
-                cl::detail::errHandler(clAmdFftTeardown(), "clAmdFftTeardown");
-            }
-        };
-#endif
 
 
     } // namespace RTCP
