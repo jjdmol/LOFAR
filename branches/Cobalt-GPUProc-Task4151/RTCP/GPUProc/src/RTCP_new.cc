f79f9e253859d9b9adeec2abbfa5de238e9c0916
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-18 14:06:57 +0000

    Task #4151: BeamFormerWorkQueue

--- branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
+++ branches/Cobalt-GPUProc-Task4151/RTCP/GPUProc/src/RTCP_new.cc
@@ -72,6 +72,7 @@
 
 #include "WorkQueues/WorkQueue.h"
 #include "WorkQueues/CorrelatorWorkQueue.h"
+#include "WorkQueues/BeamFormerWorkQueue.h"
 
 #if defined __linux__
 #include <sched.h>
@@ -123,26 +124,26 @@ namespace LOFAR {
 
 
 
-        class BeamFormerWorkQueue : public WorkQueue
-        {
-        public:
-            BeamFormerWorkQueue(BeamFormerPipeline &, unsigned queueNumber);
+        //class BeamFormerWorkQueue : public WorkQueue
+        //{
+        //public:
+        //    BeamFormerWorkQueue(BeamFormerPipeline &, unsigned queueNumber);
 
-            void doWork();
+        //    void doWork();
 
-            BeamFormerPipeline	&pipeline;
+        //    BeamFormerPipeline	&pipeline;
 
-            MultiArraySharedBuffer<char, 4>		   inputSamples;
-            cl::Buffer					   devFilteredData;
-            MultiArraySharedBuffer<float, 1>		   bandPassCorrectionWeights;
-            MultiArraySharedBuffer<float, 3>		   delaysAtBegin, delaysAfterEnd;
-            MultiArraySharedBuffer<float, 2>		   phaseOffsets;
-            cl::Buffer					   devCorrectedData;
-            MultiArraySharedBuffer<std::complex<float>, 3> beamFormerWeights;
-            cl::Buffer					   devComplexVoltages;
-            MultiArraySharedBuffer<std::complex<float>, 4> transposedComplexVoltages;
-            MultiArraySharedBuffer<float, 1>		   DMs;
-        };
+        //    MultiArraySharedBuffer<char, 4>		   inputSamples;
+        //    cl::Buffer					   devFilteredData;
+        //    MultiArraySharedBuffer<float, 1>		   bandPassCorrectionWeights;
+        //    MultiArraySharedBuffer<float, 3>		   delaysAtBegin, delaysAfterEnd;
+        //    MultiArraySharedBuffer<float, 2>		   phaseOffsets;
+        //    cl::Buffer					   devCorrectedData;
+        //    MultiArraySharedBuffer<std::complex<float>, 3> beamFormerWeights;
+        //    cl::Buffer					   devComplexVoltages;
+        //    MultiArraySharedBuffer<std::complex<float>, 4> transposedComplexVoltages;
+        //    MultiArraySharedBuffer<float, 1>		   DMs;
+        //};
 
 
         class UHEP_WorkQueue : public WorkQueue
@@ -173,118 +174,118 @@ namespace LOFAR {
         };
 
 
-
-        BeamFormerWorkQueue::BeamFormerWorkQueue(BeamFormerPipeline &pipeline, unsigned queueNumber)
-            :
-        WorkQueue(pipeline, queueNumber),
-            pipeline(pipeline),
-            inputSamples(boost::extents[ps.nrStations()][ps.nrSamplesPerChannel() * ps.nrChannelsPerSubband()][NR_POLARIZATIONS][ps.nrBytesPerComplexSample()], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            devFilteredData(pipeline.context, CL_MEM_READ_WRITE, ps.nrStations() * NR_POLARIZATIONS * ps.nrSamplesPerChannel() * ps.nrChannelsPerSubband() * sizeof(std::complex<float>)),
-            bandPassCorrectionWeights(boost::extents[ps.nrChannelsPerSubband()], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            delaysAtBegin(boost::extents[ps.nrBeams()][ps.nrStations()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            delaysAfterEnd(boost::extents[ps.nrBeams()][ps.nrStations()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            phaseOffsets(boost::extents[ps.nrBeams()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            devCorrectedData(cl::Buffer(pipeline.context, CL_MEM_READ_WRITE, ps.nrStations() * ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * NR_POLARIZATIONS * sizeof(std::complex<float>))),
-            beamFormerWeights(boost::extents[ps.nrStations()][ps.nrChannelsPerSubband()][ps.nrTABs(0)], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
-            devComplexVoltages(cl::Buffer(pipeline.context, CL_MEM_READ_WRITE, ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * ps.nrTABs(0) * NR_POLARIZATIONS * sizeof(std::complex<float>))),
-            //transposedComplexVoltages(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrSamplesPerChannel()][ps.nrChannelsPerSubband()], queue, CL_MEM_READ_ONLY, CL_MEM_READ_WRITE)
-            transposedComplexVoltages(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel()], queue, CL_MEM_READ_ONLY, CL_MEM_READ_WRITE),
-            DMs(boost::extents[ps.nrTABs(0)], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY)
-        {
-            if (ps.correctBandPass()) {
-                BandPass::computeCorrectionFactors(bandPassCorrectionWeights.origin(), ps.nrChannelsPerSubband());
-                bandPassCorrectionWeights.hostToDevice(CL_TRUE);
-            }
-        }
-
-
-        void BeamFormerWorkQueue::doWork()
-        {
-            //queue.enqueueWriteBuffer(devFIRweights, CL_TRUE, 0, firWeightsSize, firFilterWeights);
-            bandPassCorrectionWeights.hostToDevice(CL_TRUE);
-            DMs.hostToDevice(CL_TRUE);
-
-            IntToFloatKernel intToFloatKernel(ps, queue, pipeline.intToFloatProgram, devFilteredData, inputSamples);
-            Filter_FFT_Kernel fftKernel(ps, pipeline.context, devFilteredData);
-            DelayAndBandPassKernel delayAndBandPassKernel(ps, pipeline.delayAndBandPassProgram, devCorrectedData, devFilteredData, delaysAtBegin, delaysAfterEnd, phaseOffsets, bandPassCorrectionWeights);
-            BeamFormerKernel beamFormerKernel(ps, pipeline.beamFormerProgram, devComplexVoltages, devCorrectedData, beamFormerWeights);
-            BeamFormerTransposeKernel transposeKernel(ps, pipeline.transposeProgram, transposedComplexVoltages, devComplexVoltages);
-            DedispersionForwardFFTkernel dedispersionForwardFFTkernel(ps, pipeline.context, transposedComplexVoltages);
-            DedispersionBackwardFFTkernel dedispersionBackwardFFTkernel(ps, pipeline.context, transposedComplexVoltages);
-            DedispersionChirpKernel dedispersionChirpKernel(ps, pipeline.dedispersionChirpProgram, queue, transposedComplexVoltages, DMs);
-            double startTime = ps.startTime(), currentTime, stopTime = ps.stopTime(), blockTime = ps.CNintegrationTime();
-
-#pragma omp barrier
-
-            double executionStartTime = omp_get_wtime();
-
-            for (unsigned block = 0; (currentTime = startTime + block * blockTime) < stopTime; block ++) {
-#pragma omp single nowait
-#pragma omp critical (cout)
-                std::cout << "block = " << block << ", time = " << to_simple_string(from_ustime_t(currentTime)) << std::endl;
-
-                memset(delaysAtBegin.origin(), 0, delaysAtBegin.bytesize());
-                memset(delaysAfterEnd.origin(), 0, delaysAfterEnd.bytesize());
-                memset(phaseOffsets.origin(), 0, phaseOffsets.bytesize());
-
-                // FIXME!!!
-                if (ps.nrStations() >= 3)
-                    delaysAtBegin[0][2][0] = 1e-6, delaysAfterEnd[0][2][0] = 1.1e-6;
-
-                delaysAtBegin.hostToDevice(CL_FALSE);
-                delaysAfterEnd.hostToDevice(CL_FALSE);
-                phaseOffsets.hostToDevice(CL_FALSE);
-                beamFormerWeights.hostToDevice(CL_FALSE);
-
-#pragma omp for schedule(dynamic), nowait
-                for (unsigned subband = 0; subband < ps.nrSubbands(); subband ++) {
-                    try {
-#if 1
-                        {
-#if defined USE_B7015
-                            OMP_ScopedLock scopedLock(pipeline.hostToDeviceLock[gpu / 2]);
-#endif
-                            inputSamples.hostToDevice(CL_TRUE);
-                            pipeline.samplesCounter.doOperation(inputSamples.event, 0, 0, inputSamples.bytesize());
-                        }
-#endif
-
-                        //#pragma omp critical (GPU)
-                        {
-                            if (ps.nrChannelsPerSubband() > 1) {
-                                intToFloatKernel.enqueue(queue, pipeline.intToFloatCounter);
-                                fftKernel.enqueue(queue, pipeline.fftCounter);
-                            }
-
-                            delayAndBandPassKernel.enqueue(queue, pipeline.delayAndBandPassCounter, subband);
-                            beamFormerKernel.enqueue(queue, pipeline.beamFormerCounter);
-                            transposeKernel.enqueue(queue, pipeline.transposeCounter);
-                            dedispersionForwardFFTkernel.enqueue(queue, pipeline.dedispersionForwardFFTcounter);
-                            dedispersionChirpKernel.enqueue(queue, pipeline.dedispersionChirpCounter, ps.subbandToFrequencyMapping()[subband]);
-                            dedispersionBackwardFFTkernel.enqueue(queue, pipeline.dedispersionBackwardFFTcounter);
-
-                            queue.finish();
-                        }
-
-                        //queue.enqueueReadBuffer(devComplexVoltages, CL_TRUE, 0, hostComplexVoltages.bytesize(), hostComplexVoltages.origin());
-                        //dedispersedData.deviceToHost(CL_TRUE);
-                    } catch (cl::Error &error) {
-#pragma omp critical (cerr)
-                        std::cerr << "OpenCL error: " << error.what() << ": " << errorMessage(error.err()) << std::endl;
-                        exit(1);
-                    }
-                }
-            }
-
-#pragma omp barrier
-
-#pragma omp master
-            if (!profiling)
-#pragma omp critical (cout)
-                std::cout << "run time = " << omp_get_wtime() - executionStartTime << std::endl;
-        }
-
-
+//
+//        BeamFormerWorkQueue::BeamFormerWorkQueue(BeamFormerPipeline &pipeline, unsigned queueNumber)
+//            :
+//        WorkQueue(pipeline, queueNumber),
+//            pipeline(pipeline),
+//            inputSamples(boost::extents[ps.nrStations()][ps.nrSamplesPerChannel() * ps.nrChannelsPerSubband()][NR_POLARIZATIONS][ps.nrBytesPerComplexSample()], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            devFilteredData(pipeline.context, CL_MEM_READ_WRITE, ps.nrStations() * NR_POLARIZATIONS * ps.nrSamplesPerChannel() * ps.nrChannelsPerSubband() * sizeof(std::complex<float>)),
+//            bandPassCorrectionWeights(boost::extents[ps.nrChannelsPerSubband()], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            delaysAtBegin(boost::extents[ps.nrBeams()][ps.nrStations()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            delaysAfterEnd(boost::extents[ps.nrBeams()][ps.nrStations()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            phaseOffsets(boost::extents[ps.nrBeams()][NR_POLARIZATIONS], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            devCorrectedData(cl::Buffer(pipeline.context, CL_MEM_READ_WRITE, ps.nrStations() * ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * NR_POLARIZATIONS * sizeof(std::complex<float>))),
+//            beamFormerWeights(boost::extents[ps.nrStations()][ps.nrChannelsPerSubband()][ps.nrTABs(0)], queue, CL_MEM_WRITE_ONLY, CL_MEM_READ_ONLY),
+//            devComplexVoltages(cl::Buffer(pipeline.context, CL_MEM_READ_WRITE, ps.nrChannelsPerSubband() * ps.nrSamplesPerChannel() * ps.nrTABs(0) * NR_POLARIZATIONS * sizeof(std::complex<float>))),
+//            //transposedComplexVoltages(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrSamplesPerChannel()][ps.nrChannelsPerSubband()], queue, CL_MEM_READ_ONLY, CL_MEM_READ_WRITE)
+//            transposedComplexVoltages(boost::extents[ps.nrTABs(0)][NR_POLARIZATIONS][ps.nrChannelsPerSubband()][ps.nrSamplesPerChannel()], queue, CL_MEM_READ_ONLY, CL_MEM_READ_WRITE),
+//            DMs(boost::extents[ps.nrTABs(0)], queue, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY)
+//        {
+//            if (ps.correctBandPass()) {
+//                BandPass::computeCorrectionFactors(bandPassCorrectionWeights.origin(), ps.nrChannelsPerSubband());
+//                bandPassCorrectionWeights.hostToDevice(CL_TRUE);
+//            }
+//        }
+//
+//
+//        void BeamFormerWorkQueue::doWork()
+//        {
+//            //queue.enqueueWriteBuffer(devFIRweights, CL_TRUE, 0, firWeightsSize, firFilterWeights);
+//            bandPassCorrectionWeights.hostToDevice(CL_TRUE);
+//            DMs.hostToDevice(CL_TRUE);
+//
+//            IntToFloatKernel intToFloatKernel(ps, queue, pipeline.intToFloatProgram, devFilteredData, inputSamples);
+//            Filter_FFT_Kernel fftKernel(ps, pipeline.context, devFilteredData);
+//            DelayAndBandPassKernel delayAndBandPassKernel(ps, pipeline.delayAndBandPassProgram, devCorrectedData, devFilteredData, delaysAtBegin, delaysAfterEnd, phaseOffsets, bandPassCorrectionWeights);
+//            BeamFormerKernel beamFormerKernel(ps, pipeline.beamFormerProgram, devComplexVoltages, devCorrectedData, beamFormerWeights);
+//            BeamFormerTransposeKernel transposeKernel(ps, pipeline.transposeProgram, transposedComplexVoltages, devComplexVoltages);
+//            DedispersionForwardFFTkernel dedispersionForwardFFTkernel(ps, pipeline.context, transposedComplexVoltages);
+//            DedispersionBackwardFFTkernel dedispersionBackwardFFTkernel(ps, pipeline.context, transposedComplexVoltages);
+//            DedispersionChirpKernel dedispersionChirpKernel(ps, pipeline.dedispersionChirpProgram, queue, transposedComplexVoltages, DMs);
+//            double startTime = ps.startTime(), currentTime, stopTime = ps.stopTime(), blockTime = ps.CNintegrationTime();
+//
+//#pragma omp barrier
+//
+//            double executionStartTime = omp_get_wtime();
+//
+//            for (unsigned block = 0; (currentTime = startTime + block * blockTime) < stopTime; block ++) {
+//#pragma omp single nowait
+//#pragma omp critical (cout)
+//                std::cout << "block = " << block << ", time = " << to_simple_string(from_ustime_t(currentTime)) << std::endl;
+//
+//                memset(delaysAtBegin.origin(), 0, delaysAtBegin.bytesize());
+//                memset(delaysAfterEnd.origin(), 0, delaysAfterEnd.bytesize());
+//                memset(phaseOffsets.origin(), 0, phaseOffsets.bytesize());
+//
+//                // FIXME!!!
+//                if (ps.nrStations() >= 3)
+//                    delaysAtBegin[0][2][0] = 1e-6, delaysAfterEnd[0][2][0] = 1.1e-6;
+//
+//                delaysAtBegin.hostToDevice(CL_FALSE);
+//                delaysAfterEnd.hostToDevice(CL_FALSE);
+//                phaseOffsets.hostToDevice(CL_FALSE);
+//                beamFormerWeights.hostToDevice(CL_FALSE);
+//
+//#pragma omp for schedule(dynamic), nowait
+//                for (unsigned subband = 0; subband < ps.nrSubbands(); subband ++) {
+//                    try {
+//#if 1
+//                        {
+//#if defined USE_B7015
+//                            OMP_ScopedLock scopedLock(pipeline.hostToDeviceLock[gpu / 2]);
+//#endif
+//                            inputSamples.hostToDevice(CL_TRUE);
+//                            pipeline.samplesCounter.doOperation(inputSamples.event, 0, 0, inputSamples.bytesize());
+//                        }
+//#endif
+//
+//                        //#pragma omp critical (GPU)
+//                        {
+//                            if (ps.nrChannelsPerSubband() > 1) {
+//                                intToFloatKernel.enqueue(queue, pipeline.intToFloatCounter);
+//                                fftKernel.enqueue(queue, pipeline.fftCounter);
+//                            }
+//
+//                            delayAndBandPassKernel.enqueue(queue, pipeline.delayAndBandPassCounter, subband);
+//                            beamFormerKernel.enqueue(queue, pipeline.beamFormerCounter);
+//                            transposeKernel.enqueue(queue, pipeline.transposeCounter);
+//                            dedispersionForwardFFTkernel.enqueue(queue, pipeline.dedispersionForwardFFTcounter);
+//                            dedispersionChirpKernel.enqueue(queue, pipeline.dedispersionChirpCounter, ps.subbandToFrequencyMapping()[subband]);
+//                            dedispersionBackwardFFTkernel.enqueue(queue, pipeline.dedispersionBackwardFFTcounter);
+//
+//                            queue.finish();
+//                        }
+//
+//                        //queue.enqueueReadBuffer(devComplexVoltages, CL_TRUE, 0, hostComplexVoltages.bytesize(), hostComplexVoltages.origin());
+//                        //dedispersedData.deviceToHost(CL_TRUE);
+//                    } catch (cl::Error &error) {
+//#pragma omp critical (cerr)
+//                        std::cerr << "OpenCL error: " << error.what() << ": " << errorMessage(error.err()) << std::endl;
+//                        exit(1);
+//                    }
+//                }
+//            }
+//
+//#pragma omp barrier
+//
+//#pragma omp master
+//            if (!profiling)
+//#pragma omp critical (cout)
+//                std::cout << "run time = " << omp_get_wtime() - executionStartTime << std::endl;
+//        }
+//
+//
         UHEP_WorkQueue::UHEP_WorkQueue(UHEP_Pipeline &pipeline, unsigned queueNumber)
             :
         WorkQueue(pipeline, queueNumber),
