e2fffe61992f85b2b47280ed118e5b62a98ce78d
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-19 10:40:20 +0000

    Task #4155: Add VisualStudio solution

---
+++ branches/Cobalt-GPUProc-Task4155/RTCP/GPUProc/VisualStudio/Cobalt/Cobalt/Cobalt.cpp
@@ -0,0 +1,13 @@
+// Cobalt.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    std::cout<<"hello world" << std::endl;
+    return 0;
+}
+
