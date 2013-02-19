d945ac22e45120ccedcef516e2afd12bb025fda6
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-19 10:59:15 +0000

    Task #4155: Add the test executable as a seperate target

---
+++ branches/Cobalt-GPUProc-Task4155/RTCP/GPUProc/VisualStudio/Cobalt/Cobalt_test/Cobalt_test.cpp
@@ -0,0 +1,12 @@
+// Cobalt_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+    std::cout<<"Hello Test" << std::endl;
+}
+
