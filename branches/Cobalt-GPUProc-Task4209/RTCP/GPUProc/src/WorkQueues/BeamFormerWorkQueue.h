5267267dd95e461551dc88de981288d6e813d29f
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-20 14:19:39 +0000

    Task #4209: Created task branch.

---
+++ branches/Cobalt-GPUProc-Task4209/RTCP/GPUProc/src/WorkQueues/BeamFormerWorkQueue.h
@@ -0,0 +1,47 @@
+#ifndef GPUPROC_BEAMFORMERWORKQUEUE_H
+#define GPUPROC_BEAMFORMERWORKQUEUE_H
+
+#include "lofar_config.h"    
+
+#include "Common/LofarLogger.h"
+#include "global_defines.h"
+#include "Interface/Parset.h"
+#include "OpenCL_Support.h"
+#include "OpenMP_Support.h"
+#include <algorithm>
+#include <iostream>
+#include "ApplCommon/PosixTime.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "Pipelines/BeamFormerPipeline.h"
+#include "BandPass.h"
+#include "WorkQueue.h"
+
+namespace LOFAR
+{
+    namespace  RTCP 
+    {     
+                class BeamFormerWorkQueue : public WorkQueue
+        {
+        public:
+            BeamFormerWorkQueue(BeamFormerPipeline &, unsigned queueNumber);
+
+            void doWork();
+
+            BeamFormerPipeline	&pipeline;
+
+            MultiArraySharedBuffer<char, 4>		   inputSamples;
+            cl::Buffer					   devFilteredData;
+            MultiArraySharedBuffer<float, 1>		   bandPassCorrectionWeights;
+            MultiArraySharedBuffer<float, 3>		   delaysAtBegin, delaysAfterEnd;
+            MultiArraySharedBuffer<float, 2>		   phaseOffsets;
+            cl::Buffer					   devCorrectedData;
+            MultiArraySharedBuffer<std::complex<float>, 3> beamFormerWeights;
+            cl::Buffer					   devComplexVoltages;
+            MultiArraySharedBuffer<std::complex<float>, 4> transposedComplexVoltages;
+            MultiArraySharedBuffer<float, 1>		   DMs;
+        };
+
+    }
+}
+#endif
