522e6aeda1df895a156e2d8bff07ea7050fa71ff
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-22 12:38:49 +0000

    Task #4155: Manual move of fixes in 4155 branch to trunk

--- trunk/RTCP/GPUProc/test/RTCP_UnitTest.cc
+++ trunk/RTCP/GPUProc/test/RTCP_UnitTest.cc
@@ -8,21 +8,21 @@
 #include "Interface/Parset.h"
 #include "OpenCL_Support.h"
 
-//#include "UnitTests/IncoherentStokesTest.h"
-//#include "UnitTests/IntToFloatTest.h"
-//#include "UnitTests/BeamFormerTransposeTest.h"
-//#include "UnitTests/DedispersionChirpTest.h"
-//#include "UnitTests/CoherentStokesTest.h"
-//#include "UnitTests/UHEP_BeamFormerTest.h"
-//#include "UnitTests/UHEP_TransposeTest.h"
-//#include "UnitTests/BeamFormerTest.h"
-//#include "UnitTests/CorrelateTriangleTest.h"
-//#include "UnitTests/UHEP_TriggerTest.h"
-//#include "UnitTests/CorrelateRectangleTest.h"
-//#include "UnitTests/CorrelatorTest.h"
-//#include "UnitTests/FFT_Test.h"
+#include "UnitTests/IncoherentStokesTest.h"
+#include "UnitTests/IntToFloatTest.h"
+#include "UnitTests/BeamFormerTransposeTest.h"
+#include "UnitTests/DedispersionChirpTest.h"
+#include "UnitTests/CoherentStokesTest.h"
+#include "UnitTests/UHEP_BeamFormerTest.h"
+#include "UnitTests/UHEP_TransposeTest.h"
+#include "UnitTests/BeamFormerTest.h"
+#include "UnitTests/CorrelateTriangleTest.h"
+#include "UnitTests/UHEP_TriggerTest.h"
+#include "UnitTests/CorrelateRectangleTest.h"
+#include "UnitTests/CorrelatorTest.h"
+#include "UnitTests/FFT_Test.h"
 
-#include  <UnitTest++.h>
+//#include  <UnitTest++.h>  
 
 using namespace LOFAR;
 using namespace LOFAR::RTCP;
@@ -44,31 +44,31 @@ int main(int argc, char **argv)
 
     Parset ps(argv[1]);
 
-    ////(CorrelatorTest)(ps);       //needs parset AARTFAAC!!
-    ////(CorrelateRectangleTest)(ps); //needs parset AARTFAAC!!
+    //(CorrelatorTest)(ps);       //needs parset AARTFAAC!!
+    //(CorrelateRectangleTest)(ps); //needs parset AARTFAAC!!
 
-    ////works with all parsets
-    ////Correlate unittest 
-    //(CorrelateTriangleTest)(ps);
+    //works with all parsets
+    //Correlate unittest 
+    (CorrelateTriangleTest)(ps);
 
-    //////UHEP unittest
-    //(UHEP_BeamFormerTest)(ps);
-    //(UHEP_TransposeTest)(ps);
-    //(UHEP_TriggerTest)(ps);
+    ////UHEP unittest
+    (UHEP_BeamFormerTest)(ps);
+    (UHEP_TransposeTest)(ps);
+    (UHEP_TriggerTest)(ps);
 
-    ////// beamformed unittest 
-    //(IncoherentStokesTest)(ps);
-    //(IntToFloatTest)(ps);
-    //(BeamFormerTest)(ps);
-    //(BeamFormerTransposeTest)(ps);
-    //(DedispersionChirpTest)(ps);
-    //(CoherentStokesTest)(ps);
+    //// beamformed unittest 
+    (IncoherentStokesTest)(ps);
+    (IntToFloatTest)(ps);
+    (BeamFormerTest)(ps);
+    (BeamFormerTransposeTest)(ps);
+    (DedispersionChirpTest)(ps);
+    (CoherentStokesTest)(ps);
 
-    //// dunno what test
-    ////(FFT_Test)(ps);  unknown test
-    ////the actual unittests!
+    // dunno what test
+    //(FFT_Test)(ps);  unknown test
+    //the actual unittests!
 
-    //return 0;
-    return UnitTest::RunAllTests();
+    return 0;
+    //return UnitTest::RunAllTests();
     
 }
