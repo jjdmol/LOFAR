a33dda50f09403e88419a52e717dcf20abb3d5d2
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-25 16:27:43 +0000

    Task #4192: Reindented CorrelatorWorkQueue.h

--- branches/Cobalt-GPUProc-Task4192/RTCP/GPUProc/src/WorkQueues/CorrelatorWorkQueue.h
+++ branches/Cobalt-GPUProc-Task4192/RTCP/GPUProc/src/WorkQueues/CorrelatorWorkQueue.h
@@ -1,5 +1,6 @@
 #ifndef GPUPROC_CORRELATORWORKQUEUE_H
 #define GPUPROC_CORRELATORWORKQUEUE_H
+
 #include "CL/cl.hpp"
 #include "Interface/Parset.h"
 #include "OpenCL_Support.h"
@@ -18,49 +19,49 @@
 
 namespace LOFAR
 {
-    namespace RTCP 
+  namespace RTCP 
+  {
+    class CorrelatorWorkQueue : public WorkQueue
     {
-        class CorrelatorWorkQueue : public WorkQueue
-        {
-        public:
-            CorrelatorWorkQueue(CorrelatorPipeline &, unsigned queueNumber);
+    public:
+      CorrelatorWorkQueue(CorrelatorPipeline &, unsigned queueNumber);
 
-            void doWork();
+      void doWork();
 
 #if defined USE_TEST_DATA
-            void setTestPattern();
-            void printTestOutput();
+      void setTestPattern();
+      void printTestOutput();
 #endif
 
-            //private:
-            void doSubband(unsigned block, unsigned subband);
-            void receiveSubbandSamples(unsigned block, unsigned subband);
-            void sendSubbandVisibilites(unsigned block, unsigned subband);
+      //private:
+      void doSubband(unsigned block, unsigned subband);
+      void receiveSubbandSamples(unsigned block, unsigned subband);
+      void sendSubbandVisibilites(unsigned block, unsigned subband);
 
-            CorrelatorPipeline	&pipeline;
-            cl::Buffer		devFIRweights;
-            cl::Buffer		devBufferA, devBufferB;
-            MultiArraySharedBuffer<float, 1> bandPassCorrectionWeights;
-            MultiArraySharedBuffer<float, 3> delaysAtBegin, delaysAfterEnd;
-            MultiArraySharedBuffer<float, 2> phaseOffsets;
-            MultiArraySharedBuffer<char, 4> inputSamples;
+      CorrelatorPipeline	&pipeline;
+      cl::Buffer		devFIRweights;
+      cl::Buffer		devBufferA, devBufferB;
+      MultiArraySharedBuffer<float, 1> bandPassCorrectionWeights;
+      MultiArraySharedBuffer<float, 3> delaysAtBegin, delaysAfterEnd;
+      MultiArraySharedBuffer<float, 2> phaseOffsets;
+      MultiArraySharedBuffer<char, 4> inputSamples;
 
-            cl::Buffer		devFilteredData;
-            cl::Buffer		devCorrectedData;
+      cl::Buffer		devFilteredData;
+      cl::Buffer		devCorrectedData;
 
-            MultiArraySharedBuffer<std::complex<float>, 4> visibilities;
+      MultiArraySharedBuffer<std::complex<float>, 4> visibilities;
 
-            FIR_FilterKernel		firFilterKernel;
-            Filter_FFT_Kernel		fftKernel;
-            DelayAndBandPassKernel	delayAndBandPassKernel;
+      FIR_FilterKernel		firFilterKernel;
+      Filter_FFT_Kernel		fftKernel;
+      DelayAndBandPassKernel	delayAndBandPassKernel;
 #if defined USE_NEW_CORRELATOR
-            CorrelateTriangleKernel	correlateTriangleKernel;
-            CorrelateRectangleKernel	correlateRectangleKernel;
+      CorrelateTriangleKernel	correlateTriangleKernel;
+      CorrelateRectangleKernel	correlateRectangleKernel;
 #else
-            CorrelatorKernel		correlatorKernel;
+      CorrelatorKernel		correlatorKernel;
 #endif
-        };
+    };
 
-    }
+  }
 }
 #endif
