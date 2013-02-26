fdcf574876b2951a4f1cd874f65f790adde82e24
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-26 08:18:05 +0000

    Task #4197: Split StorageProcess and StorageProcesses into separate files

--- branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.cc
+++ branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.cc
@@ -1,5 +1,7 @@
 #include "lofar_config.h"
 #include <Storage/StorageProcesses.h>
+#include <Storage/SSH.h>
+#include <Interface/Stream.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include <Common/Thread/Thread.h>
@@ -11,126 +13,6 @@ namespace RTCP {
 
 using namespace std;
 using boost::format;
-
-
-StorageProcess::StorageProcess( const Parset &parset, const string &logPrefix, int rank, const string &hostname, FinalMetaData &finalMetaData, Semaphore &finalMetaDataAvailable )
-:
-  itsParset(parset),
-  itsLogPrefix(str(boost::format("%s [StorageWriter rank %2d host %s] ") % logPrefix % rank % hostname)),
-  itsRank(rank),
-  itsHostname(hostname),
-  itsFinalMetaData(finalMetaData),
-  itsFinalMetaDataAvailable(finalMetaDataAvailable)
-{
-}
-
-
-StorageProcess::~StorageProcess()
-{
-  ScopedDelayCancellation dc; // stop() is a cancellation point
-
-  // stop immediately
-  struct timespec immediately = { 0, 0 };
-  stop(immediately);
-}
-
-
-void StorageProcess::start()
-{
-  ASSERTSTR(!itsThread, "StorageProcess has already been started");
-
-  itsThread = new Thread(this, &StorageProcess::controlThread, itsLogPrefix + "[ControlThread] ", 65535);
-}
-
-
-void StorageProcess::stop(struct timespec deadline)
-{
-  if (!itsThread) {
-    // not started
-    return;
-  }
-
-  if (!itsThread->wait(deadline))
-    itsThread->cancel();
-}
-
-
-bool StorageProcess::isDone()
-{
-  return itsThread->isDone();
-}
-
-
-void StorageProcess::controlThread()
-{
-  // Start Storage
-  std::string userName   = itsParset.getString("OLAP.Storage.userName");
-  std::string sshKey     = itsParset.getString("OLAP.Storage.sshIdentityFile");
-  std::string executable = itsParset.getString("OLAP.Storage.msWriter");
-
-  if (userName == "") {
-    // No username given -- use $USER
-    const char *USER = getenv("USER");
-
-    ASSERTSTR(USER, "$USER not set.");
-
-    userName = USER;
-  }
-
-  if (sshKey == "") {
-    // No SSH key given -- try to discover it
-
-    char privkey[1024];
-
-    if (discover_ssh_privkey(privkey, sizeof privkey))
-      sshKey = privkey;
-  }
-
-  char cwd[1024];
-
-  if (getcwd(cwd, sizeof cwd) == 0)
-    throw SystemCallException("getcwd", errno, THROW_ARGS);
-
-  std::string commandLine = str(boost::format("cd %s && %s%s %u %d %u")
-    % cwd
-#if defined USE_VALGRIND
-    % "valgrind --leak-check=full "
-#else
-    % ""
-#endif
-    % executable
-    % itsParset.observationID()
-    % itsRank
-#if defined WORDS_BIGENDIAN
-    % 1
-#else
-    % 0
-#endif
-  );
-
-  SSHconnection sshconn(itsLogPrefix, itsHostname, commandLine, userName, sshKey, 0);
-  sshconn.start();
-
-  // Connect control stream
-  LOG_DEBUG_STR(itsLogPrefix << "[ControlThread] connecting...");
-  std::string resource = getStorageControlDescription(itsParset.observationID(), itsRank);
-  PortBroker::ClientStream stream(itsHostname, storageBrokerPort(itsParset.observationID()), resource, 0);
-
-  // Send parset
-  LOG_DEBUG_STR(itsLogPrefix << "[ControlThread] connected -- sending parset");
-  itsParset.write(&stream);
-  LOG_DEBUG_STR(itsLogPrefix << "[ControlThread] sent parset");
-
-  // Send final meta data once it is available
-  itsFinalMetaDataAvailable.down();
-
-  LOG_DEBUG_STR(itsLogPrefix << "[ControlThread] sending final meta data");
-  itsFinalMetaData.write(stream);
-  LOG_DEBUG_STR(itsLogPrefix << "[ControlThread] sent final meta data");
-
-  // Wait for Storage to finish properly
-  sshconn.wait();
-}
 
 
 StorageProcesses::StorageProcesses( const Parset &parset, const std::string &logPrefix )
--- branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.h
+++ branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.h
@@ -6,62 +6,13 @@
 #include <Common/Thread/Thread.h>
 #include <Interface/Parset.h>
 #include <Interface/SmartPtr.h>
-#include <Interface/Stream.h>
 #include <Interface/FinalMetaData.h>
-#include <Storage/SSH.h>
+#include <Storage/StorageProcess.h>
 #include <string>
 #include <vector>
 
 namespace LOFAR {
 namespace RTCP {
-
-/* A single Storage process.
- *
- * Storage is started as:
- *     Storage_main observationID rank isBigEndian
- *
- * A Storage process is expected to follow the following protocol: 
- *
-  // establish control connection
-  string resource = getStorageControlDescription(observationID, rank);
-  PortBroker::ServerStream stream(resource);
-
-  // read parset
-  Parset parset(&stream);
-
-  ... process observation ...
-
-  // read meta data
-  FinalMetaData finalMetaData;
-  finalMetaData.read(stream);
- */
-
-class StorageProcess {
-    public:
-      // user must call start()
-      StorageProcess( const Parset &parset, const std::string &logPrefix, int rank, const std::string &hostname, FinalMetaData &finalMetaData, Semaphore &finalMetaDataAvailable );
-
-      // calls stop(0)
-      ~StorageProcess();
-
-      void start();
-      void stop( struct timespec deadline );
-      bool isDone();
-
-    private:
-      void                               controlThread();
-
-      const Parset &itsParset;
-      const std::string itsLogPrefix;
-
-      const int itsRank;
-      const std::string itsHostname;
-
-      FinalMetaData                      &itsFinalMetaData;
-      Semaphore                          &itsFinalMetaDataAvailable;
-
-      SmartPtr<Thread> itsThread;
-};
 
 /*
  * Manage a set of StorageProcess objects. The control sequence is as follows:
