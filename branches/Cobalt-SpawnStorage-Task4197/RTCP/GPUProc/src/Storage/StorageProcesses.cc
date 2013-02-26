03c3602be73cc44c7e96c8b381410fda6569210f
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-02-26 10:15:40 +0000

    Task #4197: Replaced StorageProcesses Semaphore by Trigger

--- branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.cc
+++ branches/Cobalt-SpawnStorage-Task4197/RTCP/GPUProc/src/Storage/StorageProcesses.cc
@@ -110,7 +110,7 @@ void StorageProcesses::finalMetaDataThread()
   LOG_DEBUG_STR(itsLogPrefix << "[FinalMetaData] [ControlThread] obtained final meta data");
 
   // Notify clients
-  itsFinalMetaDataAvailable.broadcast();
+  itsFinalMetaDataAvailable.trigger();
 
   // Wait for or end the remote process
   sshconn.wait();
