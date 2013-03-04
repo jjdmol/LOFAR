c6213a52aa16c8bf6aa03d35a26d3bc07bce5221
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-03-04 13:45:59 +0000

    Task #4235: add as input item

---
+++ branches/Cobalt-GPUProc-Task4235/RTCP/GPUProc/src/WorkQueues/WorkQueueInputItem.cc
@@ -0,0 +1,16 @@
+#include "lofar_config.h"    
+
+#include "WorkQueueItem.h"
+
+#include "CL/cl.hpp"
+#include "OpenCL_Support.h"
+#include "global_defines.h"
+
+namespace LOFAR
+{
+  namespace RTCP 
+  {
+
+
+  }
+}
---
+++ branches/Cobalt-GPUProc-Task4235/RTCP/GPUProc/src/WorkQueues/WorkQueueInputItem.h
@@ -0,0 +1,18 @@
+#ifndef GPUPROC_CORRELATORWORKQUEUEINPUTITEM_H
+#define GPUPROC_CORRELATORWORKQUEUEINPUTITEM_H
+
+#include "CL/cl.hpp"
+#include "OpenCL_Support.h"
+#include "global_defines.h"
+
+namespace LOFAR
+{
+  namespace RTCP 
+  {
+    class WorkQueueInputItem
+    {
+
+    };
+  }
+}
+#endif
