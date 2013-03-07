a9047e386212e02d822321af113e5cc7909307b7
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-03-07 16:06:47 +0000

    Task #4195: Allow tested parsets to have a path, and print efficiency statistics

--- branches/Cobalt-perftimers-Task4195/RTCP/GPUProc/test/testParset.sh
+++ branches/Cobalt-perftimers-Task4195/RTCP/GPUProc/test/testParset.sh
@@ -27,7 +27,26 @@ PARSET=$1
 echo "Testing $PARSET"
 
 RUNDIR=`pwd`
-OUTDIR=`basename "${0%.run}.in_output"`/$PARSET
+OUTDIR=`basename "${0%.run}.in_output"`/`basename "$PARSET"`
+
+function parse_logs
+{
+  NORMAL=$1
+  PROFILED=$2
+
+  test -r $NORMAL || return 1
+  test -r $PROFILED || return 1
+
+  # obtain wall time
+  WALLTIME=`<$NORMAL perl -ne 'if (/Wall seconds spent.*?([0-9.]+)$/) { print $1; }'`
+  # obtain GPU cost
+  GPUCOST=`<$PROFILED perl -ne 'if (/GPU  seconds spent computing.*?([0-9.]+)$/) { print $1; }'`
+
+  # log efficiency
+  GPUUSAGE=`echo "scale=0;100*$GPUCOST/$WALLTIME" | bc -l`
+  echo "Total processing time: $WALLTIME s"
+  echo "GPU usage            : $GPUUSAGE %"
+}
 
 (
   # create output dir
@@ -38,13 +57,18 @@ OUTDIR=`basename "${0%.run}.in_output"`/$PARSET
   echo "Global 20" >> RTCP_new.debug &&
   echo "Global 20" >> RTCP.debug &&
 
-  # run correlator
-  $BINDIR/RTCP_new $RUNDIR/$PARSET &&
+  # run correlator -- without profiling
+  $BINDIR/RTCP_new $PARSET > performance_normal.txt 2>&1 &&
+  # run correlator -- with profiling
+  $BINDIR/RTCP_new -p $PARSET > performance_profiled.txt 2>&1 &&
+
+  # check logs
+  parse_logs performance_normal.txt performance_profiled.txt &&
 
   # compare output
   for f in *.MS
   do
-    ${srcdir}/cmpfloat.py $f $RUNDIR/tCorrelateRealData.in_reference/$PARSET/$f || exit 1
+    ${srcdir}/cmpfloat.py $f $RUNDIR/tCorrelateRealData.in_reference/`basename "$PARSET"`/$f || exit 1
   done
 ) || exit 1
 
