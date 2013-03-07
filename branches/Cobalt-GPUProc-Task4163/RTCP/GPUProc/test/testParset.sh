8b599e7699859fa00b65f44ba3ce35d1782fd246
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-03-07 07:55:18 +0000

    Task #4163: Created task branch

---
+++ branches/Cobalt-GPUProc-Task4163/RTCP/GPUProc/test/testParset.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Run a parset and compare the output to that in the reference_output directory.
+
+# Include some useful shell functions
+. $srcdir/testFuncs.sh
+
+BINDIR=`pwd`/../src
+
+# Some host info
+echo "Running as `whoami`"
+echo "Running on `hostname`"
+echo "Working directory is `pwd`"
+
+# Check for GPU
+haveGPU || exit 3
+
+# Check for input files
+if [ ! -e /var/scratch/mol/test_sets ]
+then
+  echo "No input files found -- aborting test."
+  exit 3
+fi
+
+PARSET=$1
+
+echo "Testing $PARSET"
+
+RUNDIR=`pwd`
+OUTDIR=`basename "${0%.run}.in_output"`/$PARSET
+
+(
+  # create output dir
+  mkdir -p $OUTDIR &&
+  cd $OUTDIR &&
+
+  # enable debugging
+  echo "Global 20" >> RTCP_new.debug &&
+  echo "Global 20" >> RTCP.debug &&
+
+  # run correlator
+  $BINDIR/RTCP_new $RUNDIR/$PARSET &&
+
+  # compare output
+  for f in *.MS
+  do
+    ${srcdir}/cmpfloat.py $f $RUNDIR/tCorrelateRealData.in_reference/$PARSET/$f || exit 1
+  done
+) || exit 1
+
+cd $RUNDIR
+
+# toss output
+rm -rf $OUTDIR
+
