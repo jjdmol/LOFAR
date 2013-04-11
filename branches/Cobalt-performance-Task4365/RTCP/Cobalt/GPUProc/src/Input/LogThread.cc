e351a29171c14ed2d572f99c7f03ebe7d0f8bdd6
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-04-11 09:10:32 +0000

    Task #4365: Created task branch

---
+++ branches/Cobalt-performance-Task4365/RTCP/Cobalt/GPUProc/src/Input/LogThread.cc
@@ -0,0 +1,200 @@
+//# LogThread.cc
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+#include "LogThread.h"
+
+#include <unistd.h>
+#include <cstdio>
+#include <algorithm>
+
+#include <Common/LofarLogger.h>
+#include <Common/Thread/Cancellation.h>
+#include <CoInterface/PrintVector.h>
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+
+
+    // log from separate thread, since printing from a signal handler causes deadlocks
+
+    LogThread::LogThread(unsigned nrRspBoards, std::string stationName)
+      :
+      itsCounters(nrRspBoards),
+      itsStationName(stationName)
+    {
+    }
+
+
+    void LogThread::start()
+    {
+      itsThread = new Thread(this, &LogThread::mainLoop, "[LogThread] ", 65536);
+    }
+
+
+    LogThread::~LogThread()
+    {
+      if (itsThread)
+        itsThread->cancel();
+
+      LOG_DEBUG_STR("[LogThread] finished");
+    }
+
+
+#if defined HAVE_BGP_ION
+
+    bool LogThread::readCPUstats(struct CPUload &load)
+    {
+      FILE *file = fopen("/proc/stat", "r");
+      int retval;
+
+      if (file == 0)
+        return false;
+
+      // make sure the file is always closed -- even on cancellation (fscanf CAN be a cancellation point)
+      struct D {
+        ~D()
+        {
+          fclose(file);
+        }
+
+        FILE *file;
+      } onDestruct = { file };
+      (void)onDestruct;
+
+      do
+        retval = fscanf(file, "cpu %llu %*u %llu %llu %*u %*u %llu %*u\n", &load.user, &load.system, &load.idle, &load.interrupt);
+      while (retval != 4 && retval != EOF);
+
+      do
+        retval = fscanf(file, "cpu0 %*u %*u %*u %llu %*u %*u %*u %*u\n", &load.idle0);
+      while (retval != 1 && retval != EOF);
+
+      return retval != EOF;
+    }
+
+
+    void LogThread::writeCPUstats(std::stringstream &str)
+    {
+      struct CPUload load;
+      struct timeval tv;
+
+      static size_t lowIdleCount = 0;
+
+      if (readCPUstats(load)) {
+        gettimeofday( &tv, 0 );
+
+        float timediff = (tv.tv_sec - previousTimeval.tv_sec) + (tv.tv_usec - previousTimeval.tv_usec) / 1.0e6;
+
+        unsigned idle0 = static_cast<unsigned>((load.idle0 - previousLoad.idle0) / timediff);
+        if (idle0 < 10)
+          lowIdleCount++;
+        else
+          lowIdleCount = 0;
+
+        // TODO: Don't print this error in non-realtime mode
+        if (lowIdleCount == 5)
+          LOG_ERROR("CPU load critical on core 0");
+
+        //str << ", us/sy/in/id: ["
+        str << ", us/sy/in/id(0): ["
+            << fixed << setprecision(0)
+            << (unsigned(load.user - previousLoad.user) + 2) / 4 / timediff << '/'
+            << (unsigned(load.system - previousLoad.system) + 2) / 4 / timediff << '/'
+            << (unsigned(load.interrupt - previousLoad.interrupt) + 2) / 4 / timediff << '/'
+            << (unsigned(load.idle - previousLoad.idle) + 2) / 4 / timediff << '('
+            << idle0 << ")]";
+#if 0
+        << "], id: ["
+        << (unsigned(load.idlePerCore[0] - previousLoad.idlePerCore[0]) << '/'
+
+            for (unsigned cpu = 0; cpu < 4; cpu++)
+              str << unsigned(load.idle[cpu] - previousLoad.idle[cpu])
+              << (cpu == 3 ? ']' : ',');
+#endif
+
+        previousLoad = load;
+        previousTimeval = tv;
+      } else {
+        str << ", no CPU load info";
+      }
+    }
+
+#endif
+
+
+    void LogThread::mainLoop()
+    {
+#if defined HAVE_BGP_ION
+      readCPUstats(previousLoad);
+      gettimeofday(&previousTimeval,0);
+#endif
+
+      //LOG_DEBUG("LogThread running");
+
+      // non-atomic updates from other threads cause race conditions, but who cares
+
+      while (true) {
+        std::stringstream logStr;
+        std::vector<unsigned> counts(itsCounters.size());
+
+        for (unsigned rsp = 0; rsp < itsCounters.size(); rsp++) {
+          counts[rsp] = itsCounters[rsp].received;
+          itsCounters[rsp].received = 0;
+        }
+
+        logStr << "[station " << itsStationName << "] ";
+
+        logStr << "received packets = " << counts;
+
+        for (unsigned rsp = 0; rsp < itsCounters.size(); rsp++) {
+          counts[rsp] = itsCounters[rsp].badSize;
+          itsCounters[rsp].badSize = 0;
+        }
+
+        if (static_cast<unsigned>(std::count(counts.begin(), counts.end(), 0U)) != counts.size())
+          logStr << ", bad size = " << counts;
+
+        for (unsigned rsp = 0; rsp < itsCounters.size(); rsp++) {
+          counts[rsp] = itsCounters[rsp].badTimeStamp;
+          itsCounters[rsp].badTimeStamp = 0;
+        }
+
+        if (static_cast<unsigned>(std::count(counts.begin(), counts.end(), 0U)) != counts.size())
+          logStr << ", bad timestamps = " << counts;
+
+#if defined HAVE_BGP_ION
+        writeCPUstats(logStr);
+#endif
+
+        LOG_INFO_STR(logStr.str());
+        sleep(15);
+      }
+
+      //LOG_DEBUG("LogThread stopped");
+    }
+
+  } // namespace Cobalt
+} // namespace LOFAR
+
