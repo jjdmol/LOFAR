aff394968edf3882df19eff8a5801cbe1d569b06
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-04-15 09:03:20 +0000

    Task #4362: Some minor tweaks to get things to compile again on dop282.

--- branches/Cobalt-Task4362/RTCP/Cobalt/GPUProc/test/cuda/t_kernel.cu
+++ branches/Cobalt-Task4362/RTCP/Cobalt/GPUProc/test/cuda/t_kernel.cu
@@ -1,6 +1,7 @@
 #include <GPUProc/complex.h>
 #include <GPUProc/cuda/CUDAException.h>
 #include <iostream>
+#include <complex>
 
 using namespace LOFAR::Cobalt;
 
@@ -23,18 +24,16 @@ __global__ void kernel()
   /* std::cout << cl << std::endl; */
 }
 
-using namespace std;
-
 int main()
 {
   std::cout << "Executing kernel" << std::endl;
   kernel<<<1,1>>>();
   CUDA_CALL(cudaGetLastError());
-  gpu::complex<float> cf(3.14f, 2.72f);
+  std::complex<float> cf(3.14f, 2.72f);
   cf *= 3.0f;
-  gpu::complex<double> cd(1.618, 0.577);
+  std::complex<double> cd(1.618, 0.577);
   cd *= 3.0;
-  gpu::complex<long double> cl(1.41421356237L, 1.73205080757L);
+  std::complex<long double> cl(1.41421356237L, 1.73205080757L);
   cl *= 3.0L;
   std::cout << cf << std::endl;
   std::cout << cd << std::endl;
