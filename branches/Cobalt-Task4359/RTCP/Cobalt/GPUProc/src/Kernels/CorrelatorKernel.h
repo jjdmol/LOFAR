ceaaf62df4380c96ecd354d66150225597ab9f5b
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-04-18 14:12:22 +0000

    Task #4359: create task branch

---
+++ branches/Cobalt-Task4359/RTCP/Cobalt/GPUProc/src/Kernels/CorrelatorKernel.h
@@ -0,0 +1,73 @@
+//# CorrelatorKernel.h
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#ifndef LOFAR_GPUPROC_CORRELATOR_KERNEL_H
+#define LOFAR_GPUPROC_CORRELATOR_KERNEL_H
+
+#include <CoInterface/Parset.h>
+
+#include <GPUProc/Kernel.h>
+#include <GPUProc/global_defines.h>
+#include <GPUProc/opencl-incl.h>
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+#if !defined USE_NEW_CORRELATOR
+
+    class CorrelatorKernel : public Kernel
+    {
+    public:
+      CorrelatorKernel(const Parset &ps, cl::CommandQueue &queue,
+                       cl::Program &program, cl::Buffer &devVisibilities, cl::Buffer &devCorrectedData);
+    };
+
+#else
+
+    class CorrelatorKernel : public Kernel
+    {
+    public:
+      CorrelatorKernel(const Parset &ps, cl::CommandQueue &queue, cl::Program &program,
+                       cl::Buffer &devVisibilities, cl::Buffer &devCorrectedData);
+
+    };
+
+    class CorrelateRectangleKernel : public Kernel
+    {
+    public:
+      CorrelateRectangleKernel(const Parset &ps, cl::CommandQueue &queue, cl::Program &program,
+                               cl::Buffer &devVisibilities, cl::Buffer &devCorrectedData);
+    };
+
+    class CorrelateTriangleKernel : public Kernel
+    {
+    public:
+      CorrelateTriangleKernel(const Parset &ps, cl::CommandQueue &queue, cl::Program &program,
+                              cl::Buffer &devVisibilities, cl::Buffer &devCorrectedData);
+    };
+
+#endif
+
+  }
+}
+
+#endif
+
