ed46635ae20f35ddaa0df71c70bfc5c7c029b4da
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-05-02 07:27:22 +0000

    Task #4429: add updated test and test kernel

--- branches/Cobalt-Task4429/RTCP/Cobalt/GPUProc/test/cuda/.in_tCudaRuntimeCompiler.cu
+++ branches/Cobalt-Task4429/RTCP/Cobalt/GPUProc/test/cuda/.in_tCudaRuntimeCompiler.cu
@@ -1,11 +1,10 @@
 extern "C" {
-__global__ void FIR_filter(void *filteredDataPtr)
-{
-  int *filteredData =(int *)filteredDataPtr;
-  unsigned cpr = blockIdx.x*blockDim.x+threadIdx.x;
-	filteredData[cpr] = NVIDIA_CUDA;
-  int test = 20;
-  return;
-
-}
+  __global__ void FIR_filter(void *filteredDataPtr)
+  {
+    int *filteredData =(int *)filteredDataPtr;
+    unsigned cpr = blockIdx.x*blockDim.x+threadIdx.x;
+    filteredData[cpr] = NVIDIA_CUDA;  // define will be set on the command line as a parameter to the nvcc compiler
+    int test = 20;
+    return;
+  }
 }
