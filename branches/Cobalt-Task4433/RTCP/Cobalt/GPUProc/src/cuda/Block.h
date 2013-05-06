086cdfafdf8311c8fac64f656cc83bdd88c43b73
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-05-06 09:57:42 +0000

    Task #4433: Created task branch

---
+++ branches/Cobalt-Task4433/RTCP/Cobalt/GPUProc/src/cuda/Block.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Block
+{
+  unsigned x;
+  unsigned y;
+  unsigned z;
+};
---
+++ branches/Cobalt-Task4433/RTCP/Cobalt/GPUProc/src/cuda/Event.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cuda.h>  // for CU_EVENT_DEFAULT
+#include <boost/shared_ptr.hpp>
+
+class Event
+{
+public:
+  Event(unsigned flags = CU_EVENT_DEFAULT);
+  float elapsedTime() const;
+  CUevent operator()() const;
+private:
+  friend class Stream;
+  class Impl;
+  boost::shared_ptr<Impl> _impl;
+};
+
