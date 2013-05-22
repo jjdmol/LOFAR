7f769d66f0e7b40ecda13012331d9f01d2eb836d
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-05-22 19:53:45 +0000

    Task #4431: Creating task branch from trunk

---
+++ branches/OTDB-CreateNewOTDB-Task4431/LCS/ApplCommon/test/tObservation.cc
@@ -0,0 +1,198 @@
+//# tObservation.cc
+//#
+//# Copyright (C) 2002-2004
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+//# Includes
+#include <Common/LofarLogger.h>
+#include <Common/ParameterSet.h>
+#include <Common/StreamUtil.h>
+#include <ApplCommon/Observation.h>
+
+using namespace LOFAR;
+
+void showSTS(Observation::StreamToStorage	sts)
+{
+	cout << formatString("DP[%d]:%s, Str[%d]:%s, Pset:%d, Node:%s, Dir: %s, Adder:%d, writer:%d\n", sts.dataProductNr, sts.dataProduct.c_str(), sts.streamNr, sts.filename.c_str(), sts.sourcePset, sts.destStorageNode.c_str(), sts.destDirectory.c_str(), sts.adderNr, sts.writerNr);
+}
+
+int main (int argc, char* argv[]) 
+{
+	INIT_LOGGER(argv[0]);
+	try {
+		if (argc == 2) {
+			ParameterSet	userPS(argv[1]);
+			Observation		someObs(&userPS, true);
+			cout << someObs;
+			cout << "getRCUbitset(96,48,'') = " << someObs.getRCUbitset(96,48,"") << endl;	// Europe
+			cout << "getRCUbitset(96,96,'') = " << someObs.getRCUbitset(96,96,"") << endl;	// Europe
+			cout << "getRCUbitset(96,48,LBA_XXX) = " << someObs.getRCUbitset(96,48,"LBA_XXX") << endl;	// Core
+			cout << "getRCUbitset(96,96,LBA_XXX) = " << someObs.getRCUbitset(96,96,"LBA_XXX") << endl;	// Core
+			cout << "getRCUbitset(96,48,HBA_XXX) = " << someObs.getRCUbitset(96,48,"HBA_XXX") << endl;	// Core
+			cout << "getRCUbitset(96,96,HBA_XXX) = " << someObs.getRCUbitset(96,96,"HBA_XXX") << endl;	// Core
+			vector<int>	b2b = someObs.getBeamAllocation("CS002");
+			cout << "BeamAlloc for CS002 : " << b2b << endl;
+			int	nrStreams = someObs.streamsToStorage.size();
+			for (int i = 0; i < nrStreams; i++) {
+				showSTS(someObs.streamsToStorage[i]);
+			}
+			return (0);
+		}
+
+		cout << ">>>" << endl; // off
+		cout << "### READING AND SHOWING TWO PARSETS ###" << endl;
+		ParameterSet parSet2("tObservation.in_parset2");
+		Observation  dualObs(&parSet2, false);
+		cout << dualObs << endl;
+
+		ParameterSet parSet1("tObservation.in_parset1");
+		Observation  obs1(&parSet1, false);
+		cout << obs1 << endl;
+		cout << "<<<" << endl; // on
+
+		// add an extra beam
+		cout << "### ADDING AN EXTRA BEAM ###" << endl;
+		parSet1.replace("ObsSW.Observation.nrBeams", "2");
+		parSet1.add("ObsSW.Observation.Beam[1].angle1", 		"0.23456789");
+		parSet1.add("ObsSW.Observation.Beam[1].angle2", 		"0.123456789");
+		parSet1.add("ObsSW.Observation.Beam[1].directionType",  "AZEL");
+		parSet1.add("ObsSW.Observation.Beam[1].subbandList", 	"[4,3,102]");
+		parSet1.add("ObsSW.Observation.Beam[1].beamletList", 	"[15,16,18]");
+		Observation  obs2(&parSet1, false);
+		cout << obs2 << endl;
+
+		// test storage node assignment
+		cout << "### ADDING NODE ASSIGNMENT ###" << endl;
+		parSet1.add("ObsSW.OLAP.CNProc.phaseOnePsets", "[]");
+		parSet1.add("ObsSW.OLAP.CNProc.phaseTwoPsets", "[]");
+		parSet1.add("ObsSW.OLAP.CNProc.phaseThreePsets", "[]");
+		parSet1.add("ObsSW.Observation.DataProducts.Output_CoherentStokes.enabled", "true");
+		parSet1.add("ObsSW.Observation.DataProducts.Output_CoherentStokes.filenames", "[beam0.h5,beam1.h5]");
+		parSet1.add("ObsSW.Observation.DataProducts.Output_CoherentStokes.locations", "[/,/]");
+		try {
+			Observation obs4(&parSet1, false);
+			cerr << "Expected a exception because 'locations' where specified wrong" << endl;
+			return (1);
+		}
+		catch (Exception& e) {
+			cout << "Exception on wrong specified locations works OK" << endl;
+		}
+		parSet1.replace("ObsSW.Observation.DataProducts.Output_CoherentStokes.locations", "[a:b,c:d]");
+
+		cout << ">>>" << endl; // off
+		cout << "### TESTING CONFLICT ROUTINE ###" << endl;
+		// test conflicts in clock
+		ParameterSet conflictPS1("tObservation.in_conflict1");
+		Observation  conflictObs1(&conflictPS1, false);
+		ASSERTSTR(obs2.conflicts(conflictObs1), "File 1 should have had a clock conflict");
+	
+		// test conflicts in receivers
+		ParameterSet conflictPS2("tObservation.in_conflict2");
+		Observation  conflictObs2(&conflictPS2, false);
+		ASSERTSTR(obs2.conflicts(conflictObs2), "File 2 should have had a receiver conflict");
+	
+		// test conflicts in beamlets
+//		ParameterSet conflictPS3("tObservation.in_conflict3");
+//		Observation  conflictObs3(&conflictPS3, false);
+//		ASSERTSTR(obs2.conflicts(conflictObs3), "File 3 should have had a beamlet conflict");
+	
+		// test conflicts in nrSlotsPerFrame
+		ParameterSet conflictPS4("tObservation.in_conflict4");
+		Observation  conflictObs4(&conflictPS4, false);
+		ASSERTSTR(obs2.conflicts(conflictObs4), "File 4 should have had a nrSlotInFrame conflict");
+	
+		// everything conflict except the time
+		ParameterSet conflictPS5("tObservation.in_conflict5");
+		Observation  conflictObs5(&conflictPS5, false);
+		ASSERTSTR(!obs2.conflicts(conflictObs5), "File 5 should NOT have had a conflict");
+		cout << "No conflict found in file 5 which is oke." << endl;
+
+		// test conflicts in bit mode
+		ParameterSet conflictPS6("tObservation.in_conflict6");
+		Observation  conflictObs6(&conflictPS6, false);
+		ASSERTSTR(obs2.conflicts(conflictObs6), "File 6 should have had a bit mode conflict");
+
+		cout << "<<<" << endl; // on
+
+		// basic test on RCU bitsets
+		cout << "### SHOWING RCU BITSETS FOR ALL STATIONTYPES ###" << endl;
+		Observation		obs3(&parSet1, false);
+		cout << "getRCUbitset(96,48,'') = " << obs3.getRCUbitset(96,48,"") << endl;	// Europe
+		cout << "getRCUbitset(96,96,'') = " << obs3.getRCUbitset(96,96,"") << endl;	// Europe
+		cout << "getRCUbitset(96,48,LBA_XXX) = " << obs3.getRCUbitset(96,48,"LBA_XXX") << endl;	// Core
+		cout << "getRCUbitset(96,96,LBA_XXX) = " << obs3.getRCUbitset(96,96,"LBA_XXX") << endl;	// Core
+		cout << "getRCUbitset(96,48,HBA_XXX) = " << obs3.getRCUbitset(96,48,"HBA_XXX") << endl;	// Core
+		cout << "getRCUbitset(96,96,HBA_XXX) = " << obs3.getRCUbitset(96,96,"HBA_XXX") << endl;	// Core
+		
+		// test translation of antennaSetname
+		obs3.antennaSet = "HBA_ZERO";
+		cout << "HBA_ZERO(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "HBA_ZERO(true)  = " << obs3.getAntennaFieldName(true) << endl;
+		obs3.antennaSet = "HBA_ONE";
+		cout << "HBA_ONE(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "HBA_ONE(true)  = " << obs3.getAntennaFieldName(true) << endl;
+		obs3.antennaSet = "HBA_DUAL";
+		cout << "HBA_DUAL(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "HBA_DUAL(true)  = " << obs3.getAntennaFieldName(true) << endl;
+		obs3.antennaSet = "HBA_JOINED";
+		cout << "HBA_JOINED(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "HBA_JOINED(true)  = " << obs3.getAntennaFieldName(true) << endl;
+
+		obs3.antennaSet = "LBA_INNER";
+		cout << "LBA_INNER(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "LBA_INNER(true)  = " << obs3.getAntennaFieldName(true) << endl;
+		obs3.antennaSet = "LBA_OUTER";
+		cout << "LBA_OUTER(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "LBA_OUTER(true)  = " << obs3.getAntennaFieldName(true) << endl;
+		obs3.antennaSet = "LBA_X";
+		cout << "LBA_X(false) = " << obs3.getAntennaFieldName(false) << endl;
+		cout << "LBA_X(true)  = " << obs3.getAntennaFieldName(true) << endl;
+
+		// test old syntax agains new syntax
+		cout << ">>>" << endl; // off
+		cout << "### SHOW DIFFERENT BETWEEN OLD AND NEW DATASLOT SYNTAX ###" << endl;
+		ParameterSet oldParset("tObservation.in_oldParset");
+		Observation  oldObs(&oldParset,true);
+		cout << "OLD SYNTAX" << endl;
+		cout << oldObs << endl;
+
+		ParameterSet newParset("tObservation.in_newParset");
+		Observation  newObs(&newParset,true);
+		cout << "NEW SYNTAX" << endl;
+		cout << newObs << endl;
+		cout << "<<<" << endl; // on
+
+    // These observations bugged before:
+		ParameterSet p103821("tObservation.in_parset_obs103821");
+		Observation  o103821(&p103821,true);
+		cout << "OBS 103821" << endl;
+		cout << o103821 << endl;
+		cout << "<<<" << endl;
+
+	}
+	catch (Exception& e) {
+		cout << "Exception: " << e.what() << endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ branches/OTDB-CreateNewOTDB-Task4431/MAC/APL/PIC/TBB_Driver/src/TrigSetupCmd.cc
@@ -0,0 +1,137 @@
+//#  TrigSetupCmd.cc: implementation of the TrigSetupCmd class
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#include <lofar_config.h>
+#include <Common/LofarLogger.h>
+#include <Common/StringUtil.h>
+#include <Common/lofar_bitset.h>
+
+#include "TrigSetupCmd.h"
+
+
+using namespace LOFAR;
+using namespace GCF::TM;
+using namespace TBB_Protocol;
+using namespace TP_Protocol;
+using   namespace TBB;
+
+//--Constructors for a TrigSetupCmd object.----------------------------------------
+TrigSetupCmd::TrigSetupCmd()
+{
+    TS = TbbSettings::instance();
+    setWaitAck(true);       
+}
+      
+//--Destructor for TrigSetupCmd.---------------------------------------------------
+TrigSetupCmd::~TrigSetupCmd() { }
+
+// ----------------------------------------------------------------------------
+bool TrigSetupCmd::isValid(GCFEvent& event)
+{
+    if ((event.signal == TBB_TRIG_SETUP)||(event.signal == TP_TRIG_SETUP_ACK)) {
+        return(true);
+    }
+    return(false);
+}
+
+// ----------------------------------------------------------------------------
+void TrigSetupCmd::saveTbbEvent(GCFEvent& event)
+{
+    TBBTrigSetupEvent tbb_event(event);
+    
+    int32 channel;          // channel 0 .. 191 (= maxboard * max_channels_on_board)
+    for(int rcunr = 0; rcunr < TS->maxChannels(); rcunr++) {
+        channel = TS->convertRcuToChan(rcunr);
+        if ((tbb_event.rcu[rcunr].window > 2) && (tbb_event.rcu[rcunr].level > 80)){
+            TS->setChTriggerLevel(channel, 80);
+        }
+        else {
+            TS->setChTriggerLevel(channel, static_cast<uint32>(tbb_event.rcu[rcunr].level));
+        }
+        TS->setChTriggerStartMode(channel, static_cast<uint32>(tbb_event.rcu[rcunr].start_mode));
+        TS->setChTriggerStopMode(channel, static_cast<uint32>(tbb_event.rcu[rcunr].stop_mode));
+        TS->setChFilterSelect(channel, static_cast<uint32>(tbb_event.rcu[rcunr].filter_select));
+        TS->setChDetectWindow(channel, static_cast<uint32>(tbb_event.rcu[rcunr].window));
+        TS->setChTriggerMode(channel, static_cast<uint32>(tbb_event.rcu[rcunr].trigger_mode));
+        TS->setChOperatingMode(channel, static_cast<uint32>(tbb_event.rcu[rcunr].operating_mode));
+    }
+    
+    bitset<MAX_N_RCUS> channels;
+    channels.set();
+    setChannels(channels);
+    
+    // select firt channel to handle
+    nextChannelNr();
+}
+
+// ----------------------------------------------------------------------------
+void TrigSetupCmd::sendTpEvent()
+{
+    TPTrigSetupEvent tp_event;
+    tp_event.opcode = oc_TRIG_SETUP;
+    tp_event.status = 0;
+
+    tp_event.mp = TS->getChMpNr(getChannelNr());
+    for (int i = 0; i < 4; i++) {
+        tp_event.channel[i].level         = TS->getChTriggerLevel(getChannelNr() + i);
+        tp_event.channel[i].td_mode       =(TS->getChTriggerStartMode(getChannelNr() + i) +
+                                           (TS->getChTriggerStopMode(getChannelNr() + i) << 4));
+        tp_event.channel[i].filter_select = TS->getChFilterSelect(getChannelNr() + i);
+        tp_event.channel[i].window        = TS->getChDetectWindow(getChannelNr() + i);
+        tp_event.channel[i].trigger_mode  = TS->getChTriggerMode(getChannelNr() + i);
+    }
+    
+    TS->boardPort(getBoardNr()).send(tp_event);
+    TS->setBoardUsed(getBoardNr());
+    TS->boardPort(getBoardNr()).setTimer(TS->timeout());    
+}
+
+// ----------------------------------------------------------------------------
+void TrigSetupCmd::saveTpAckEvent(GCFEvent& event)
+{
+    // in case of a time-out, set error mask
+    if (event.signal == F_TIMER) {
+        setStatus(getBoardNr(), TBB_TIME_OUT);
+    }   else {
+        TPTrigSetupAckEvent tp_ack(event);
+        LOG_DEBUG_STR(formatString("Received TrigSetupAck from boardnr[%d]", getBoardNr()));
+        
+        if (tp_ack.status != 0) {
+            setStatus(getBoardNr(), (tp_ack.status << 24));
+        }
+    }
+    // one mp done, go to next mp
+    setChannelNr(TS->getFirstChannelNr(getBoardNr(), TS->getChMpNr(getChannelNr())) + 3);
+    nextChannelNr();
+}
+
+// ----------------------------------------------------------------------------
+void TrigSetupCmd::sendTbbAckEvent(GCFPortInterface* clientport)
+{
+    TBBTrigSetupAckEvent tbb_ack;
+    
+    for (int32 i = 0; i < MAX_N_TBBOARDS; i++) { 
+        tbb_ack.status_mask[i] = getStatus(i);
+    }
+    
+    if (clientport->isConnected()) { clientport->send(tbb_ack); }
+}
