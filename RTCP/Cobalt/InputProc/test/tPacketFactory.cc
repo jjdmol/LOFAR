7f5646f47caa32a785dac835212492d85e6740fb
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-05-22 22:35:17 +0000

    Task #4452: more Cobalt svn props

--- RTCP/Cobalt/InputProc/test/tPacketFactory.cc
+++ RTCP/Cobalt/InputProc/test/tPacketFactory.cc
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along
  * with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: $
+ * $Id$
  */
 
 #include <lofar_config.h>
