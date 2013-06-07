a142817403bd172882a93addfa9c49ab9412caaf
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-06-07 08:39:42 +0000

    Task #4468: Test is complete all corner cases tested.

--- branches/Cobalt-Task4468/RTCP/Cobalt/GPUProc/test/cuda/tIntToFloat.cc
+++ branches/Cobalt-Task4468/RTCP/Cobalt/GPUProc/test/cuda/tIntToFloat.cc
@@ -42,13 +42,9 @@ using namespace LOFAR::Cobalt::gpu;
 using namespace LOFAR::Cobalt;
 
 // 
-float * runTest(float bandPassFactor,
-                float frequency = 0.0,
-                float subbandWidth = 0.0,
-                bool delayCompensation = false, 
-                float delayBegin = 0.0,
-                float delayEnd = 0.0,
-                float PhaseOffset = 0.0)
+float * runTest(unsigned NR_BITS_PER_SAMPLE = 16, 
+                char char_default_value = 0, // will be used depending NR_BITS_PER_SAMPLE
+                short short_default_value = 0)
 {
   // Set up environment
   gpu::Platform pf;
@@ -59,70 +55,59 @@ float * runTest(float bandPassFactor,
   std::stringstream tostrstream("");
 
   string kernelPath = "IntToFloat.cu";  //The test copies the kernel to the current dir (also the complex header, needed for compilation)
- 
+
+  // ****************************************
+  // Compile to ptx  
   // Get an instantiation of the default parameters
   definitions_type definitions = defaultDefinitions();
   flags_type flags = defaultFlags();
 
-  // ****************************************
-  // Compile to ptx
   // Set op string string pairs to be provided to the compiler as defines
-  definitions["NR_CHANNELS"] = "16";
-  unsigned NR_CHANNELS = 16;
   definitions["NR_STATIONS"] = "2";
   unsigned NR_STATIONS = 2;
-  definitions["NR_SAMPLES_PER_CHANNEL"] = "64";
-  unsigned NR_SAMPLES_PER_CHANNEL = 64;
   definitions["NR_SAMPLES_PER_SUBBAND"] = "1024";
   unsigned NR_SAMPLES_PER_SUBBAND = 1024;
-  definitions["NR_BITS_PER_SAMPLE"] = "8";
-  unsigned NR_BITS_PER_SAMPLE = 8;
-
-
+  
+  tostrstream << NR_BITS_PER_SAMPLE;
+  definitions["NR_BITS_PER_SAMPLE"] = tostrstream.str();
   definitions["NR_POLARIZATIONS"] = "2";
   unsigned NR_POLARIZATIONS = 2;
-  definitions["NR_BEAMS"] = "8";
-  unsigned NR_BEAMS = 8;
-  definitions["USE_CUDA"] = "1";
   definitions["COMPLEX"] = "2";
   unsigned COMPLEX = 2;
-  tostrstream << subbandWidth;
-  definitions["SUBBAND_BANDWIDTH"] = tostrstream.str();
-  tostrstream.clear();
-  float SUBBAND_BANDWIDTH = subbandWidth;
-  definitions["BANDPASS_CORRECTION"] = "1";
-  if (delayCompensation)
-    definitions["DELAY_COMPENSATION"] = "1";
   string ptx = createPTX(devices, kernelPath, flags, definitions);
   gpu::Module module(createModule(ctx, kernelPath, ptx));
   Function  hKernel(module, "intToFloat");  // c function this no argument overloading
 
   // *************************************************************
   // Create the data arrays  
-  size_t sizeSampledData = NR_STATIONS * NR_SAMPLES_PER_SUBBAND * NR_POLARIZATIONS * complex * sizeof(short);
-  DeviceMemory DevSampledMemory(ctx, sizeFilteredData);
-  HostMemory rawSampledData = getInitializedArray(ctx, sizeFilteredData, 1.0f);
+  // remember to use the correct size depending on the input type
+  size_t sizeSampledData;
+  if (NR_BITS_PER_SAMPLE==16)
+    sizeSampledData = NR_STATIONS * NR_SAMPLES_PER_SUBBAND * NR_POLARIZATIONS * COMPLEX * sizeof(short);
+  else
+      sizeSampledData = NR_STATIONS * NR_SAMPLES_PER_SUBBAND * NR_POLARIZATIONS * COMPLEX * sizeof(char);
+      
+  DeviceMemory DevSampledMemory(ctx, sizeSampledData);
+  HostMemory rawSampledData = getInitializedArray(ctx, 0, (short)0); // use bogus value: no default constructor..
+  if (NR_BITS_PER_SAMPLE==16)
+    rawSampledData = getInitializedArray(ctx, sizeSampledData, (short)short_default_value);
+  else
+    rawSampledData = getInitializedArray(ctx, sizeSampledData, (char)char_default_value);  
   cuStream.writeBuffer(DevSampledMemory, rawSampledData);
 
-  size_t sizeConvertedData = NR_STATIONS * NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX * sizeof(float);
-  DeviceMemory DevConvertedMemory(ctx, sizeCorrectedData);
+  // Output is always float
+  size_t sizeConvertedData = NR_STATIONS * NR_SAMPLES_PER_SUBBAND * NR_POLARIZATIONS * COMPLEX * sizeof(float);
+  DeviceMemory DevConvertedMemory(ctx, sizeConvertedData);
   HostMemory rawConvertedData = getInitializedArray(ctx, sizeConvertedData, 42.0f); 
   cuStream.writeBuffer(DevConvertedMemory, rawConvertedData);
 
   // ****************************************************************************
   // Run the kernel on the created data
-  hKernel.setArg(0, DevSampledMemory);
-  hKernel.setArg(1, DevConvertedMemory);
+  hKernel.setArg(1, DevSampledMemory);
+  hKernel.setArg(0, DevConvertedMemory);
 
-  // Calculate the number of threads in total and per blovk
-  int nrChannelsPerSubband = NR_CHANNELS;
-  int nrStations = NR_STATIONS; 
-  int MAXNRCUDATHREADS = 1024;//doet moet nog opgevraagt worden en niuet als magish getal
-  size_t maxNrThreads = MAXNRCUDATHREADS;
-  unsigned totalNrThreads = nrChannelsPerSubband * NR_POLARIZATIONS * 2; //ps.nrChannelsPerSubband()
-  unsigned nrPasses = (totalNrThreads + maxNrThreads - 1) / maxNrThreads;
   // assign to gpu_wrapper objects
-  Grid globalWorkSize(1, NR_CHANNELS == 1? 1: NR_CHANNELS/16, NR_STATIONS);  
+  Grid globalWorkSize(1, NR_STATIONS, 1);  
   Block localWorkSize(256, 1,1); 
 
   // Run the kernel
@@ -131,18 +116,18 @@ float * runTest(float bandPassFactor,
   cuStream.synchronize(); // assure that the kernel is finished
   
   // Copy output vector from GPU buffer to host memory.
-  cuStream.readBuffer(rawCorrectedData, DevCorrectedMemory);
+  cuStream.readBuffer(rawConvertedData, DevConvertedMemory);
   cuStream.synchronize(); //assure copy from device is done
   
   // *************************************
   // Create the return values
   float *firstAndLastComplex = new float[4];
   // Return the first complex
-  firstAndLastComplex[0] = rawCorrectedData.get<float>()[0];
-  firstAndLastComplex[1] = rawCorrectedData.get<float>()[1];
+  firstAndLastComplex[0] = rawConvertedData.get<float>()[0];
+  firstAndLastComplex[1] = rawConvertedData.get<float>()[1];
   //return the last complex number
-  firstAndLastComplex[2] = rawCorrectedData.get<float>()[(sizeCorrectedData / sizeof(float)) - 2];
-  firstAndLastComplex[3] = rawCorrectedData.get<float>()[(sizeCorrectedData / sizeof(float)) - 1];
+  firstAndLastComplex[2] = rawConvertedData.get<float>()[(sizeConvertedData / sizeof(float)) - 2];
+  firstAndLastComplex[3] = rawConvertedData.get<float>()[(sizeConvertedData / sizeof(float)) - 1];
 
   // *************************************
   // cleanup memory
@@ -150,131 +135,82 @@ float * runTest(float bandPassFactor,
   return firstAndLastComplex;
 }
 
-TEST(BandPass)
+TEST(CornerCaseMinus128)
 {
   // ***********************************************************
-  // Test if the bandpass correction factor is applied correctly in isolation
-  float bandPassFactor = 2.0;
+  // Test the corner case for 8 bit input, -128 should be clamped to -127
   float * results;
+  results = runTest(8,     // use 8 bits per sample
+                    -128,  // Use -128 as default input
+                    0);    // not used in 8 bit samples
 
-  // The input samples are all ones
-  // After correction, multiply with 2.
-  // The first and the last complex values are retrieved. They should be scaled with the bandPassFactor == 2
-  results = runTest(bandPassFactor);
-
-  CHECK_CLOSE(2.0, results[0], 0.00001);
-  CHECK_CLOSE(2.0, results[1], 0.00001);
-  CHECK_CLOSE(2.0, results[2], 0.00001);
-  CHECK_CLOSE(2.0, results[3], 0.00001);
+  CHECK_CLOSE(-127.0, results[0], 0.00000001);
+  CHECK_CLOSE(-127.0, results[1], 0.00000001);
+  CHECK_CLOSE(-127.0, results[2], 0.00000001);
+  CHECK_CLOSE(-127.0, results[3], 0.00000001);
 
   delete[] results;
 }
 
-TEST(PhaseOffsets)
+TEST(CornerCaseMinus128short)
 {
+  // ***********************************************************
+  // The -128 to -127 clamp should not be applied to 16 bit samples
   float * results;
+  results = runTest(16, // 16 bit samples
+                    0,  // Not used in 16 bit
+                    -128); // use -128 as input
 
-  //**********************************************************************
-  // Delaycompensation but only for the phase ofsets:
-  // All computations the drop except the phase ofset of 1,0 which is fed into a cosisin
-  // cosisin(pi) = -1
-  results = runTest(1.0,   // bandpass factor
-                    1.0,   // frequency
-                    1.0,   
-                    true,  // delayCompensation
-                    0.0,   // delays begin  
-                    0.0,   // delays end
-                    M_PI); // phase offsets
-
-  CHECK_CLOSE(-1.0, results[0], 0.00001);
-  CHECK_CLOSE(-1.0, results[1], 0.00001);
-  CHECK_CLOSE(-1.0, results[2], 0.00001);
-  CHECK_CLOSE(-1.0, results[3], 0.00001);
+  CHECK_CLOSE(-128.0, results[0], 0.00000001);
+  CHECK_CLOSE(-128.0, results[1], 0.00000001);
+  CHECK_CLOSE(-128.0, results[2], 0.00000001);
+  CHECK_CLOSE(-128.0, results[3], 0.00000001);
 
   delete[] results;
 }
 
-SUITE(DelayCompensation)
+
+TEST(CornerCaseMinus127)
 {
-  TEST(ConstantDelay)
-  {
-    float * results;
+  // ***********************************************************
+  // Minus 127 should stay -128
+  float * results;
+  results = runTest(8, -127, 0);
 
-    //****************************************************************************
-    // delays  begin and end both 1 no phase offset frequency 1 width 1
-    // frequency = subbandFrequency - .5f * SUBBAND_BANDWIDTH + (channel + minor) * (SUBBAND_BANDWIDTH / NR_CHANNELS)
-    //  (delaysbegin * - 2 * pi ) * (frequency == 0.5) == -3.14
-    // cosisin(-3.14159+0 i) == -1
-    results = runTest(1.0,   // bandpass factor
-                      1.0,   // frequency
-                      1.0,   
-                      true,  // delayCompensation
-                      1.0,   // delays begin  
-                      1.0,   // delays end
-                      0.0);  // phase offsets
+  CHECK_CLOSE(-127.0, results[0], 0.00000001);
+  CHECK_CLOSE(-127.0, results[1], 0.00000001);
+  CHECK_CLOSE(-127.0, results[2], 0.00000001);
+  CHECK_CLOSE(-127.0, results[3], 0.00000001);
 
-    CHECK_CLOSE(-1.0, results[0], 0.00001);
-    CHECK_CLOSE(-1.0, results[1], 0.00001);
-    CHECK_CLOSE(-1.0, results[2], 0.00001);
-    CHECK_CLOSE(-1.0, results[3], 0.00001);
-
-    delete[] results;
-  }
-
-  TEST(SlopedDelay)
-  {
-    float * results;
-
-    //****************************************************************************
-    // delays  begin 1 and end 0 no phase offset frequency 1 width 1
-    // frequency = subbandFrequency - .5f * SUBBAND_BANDWIDTH + (channel + minor) * (SUBBAND_BANDWIDTH / NR_CHANNELS)
-    //  (delaysbegin * - 2 * pi ) * (frequency == 0.5) == -3.14
-    // cosisin(-3.14159+0 i) == -1
-    // The later sets of samples are calculate as:
-    // vX = vX * dvX;  The delays are multiplied because we are calculating with exponents
-    // Ask john Romein for more details
-    results = runTest(1.0,   // bandpass factor
-                      1.0,   // frequency
-                      1.0,   
-                      true,  // delayCompensation
-                      1.0,   // delays begin  
-                      0.0,   // delays end
-                      0.0);  // phase offsets
-
-    CHECK_CLOSE(-1.0,     results[0], 0.00001);
-    CHECK_CLOSE(-1.0,     results[1], 0.00001);
-    CHECK_CLOSE(1.047860, results[2], 0.00001);
-    CHECK_CLOSE(0.949728, results[3], 0.00001);
-
-    delete[] results;
-  }
+  delete[] results;
 }
 
-TEST(AllAtOnce)
+TEST(IntToFloatSimple)
 {
+  // ***********************************************************
+  // Test if 10 is converted
   float * results;
+  results = runTest(8, 10, 0);
 
-  //****************************************************************************
-  // delays  begin 1 and end 0 no phase offset frequency 1 width 1
-  // frequency = subbandFrequency - .5f * SUBBAND_BANDWIDTH + (channel + minor) * (SUBBAND_BANDWIDTH / NR_CHANNELS)
-  //  (delaysbegin * - 2 * pi ) * (frequency == 0.5) == -3.14
-  // cosisin(-3.14159+0 i) == -1
-  // The later sets of samples are calculate as:
-  // vX = vX * dvX;  The delays are multiplied because we are calculating with exponents
-  // Ask john Romein for more details
-  // In this test the phase offsets are also compensated
-  results = runTest(2.0,   // bandpass factor (weights == 2)
-                    1.0,   // frequency
-                    1.0,   
-                    true,  // delayCompensation
-                    1.0,   // delays begin  
-                    0.0,   // delays end
-                    1.0);  // phase offsets (correct with e = 2.71828)
+  CHECK_CLOSE(10.0, results[0], 0.00000001);
+  CHECK_CLOSE(10.0, results[1], 0.00000001);
+  CHECK_CLOSE(10.0, results[2], 0.00000001);
+  CHECK_CLOSE(10.0, results[3], 0.00000001);
 
-  CHECK_CLOSE( 0.602337, results[0], 0.00001);
-  CHECK_CLOSE(-2.763550, results[1], 0.00001);
-  CHECK_CLOSE(-0.466011, results[2], 0.00001);
-  CHECK_CLOSE( 2.789770, results[3], 0.00001);
+  delete[] results;
+}
+
+TEST(IntToFloatSimpleShort)
+{
+  // ***********************************************************
+  // Test if 10 is converted
+  float * results;
+  results = runTest(16, 0, 2034);
+
+  CHECK_CLOSE(2034.0, results[0], 0.00000001);
+  CHECK_CLOSE(2034.0, results[1], 0.00000001);
+  CHECK_CLOSE(2034.0, results[2], 0.00000001);
+  CHECK_CLOSE(2034.0, results[3], 0.00000001);
 
   delete[] results;
 }
