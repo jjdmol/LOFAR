174c63f10002cc3309b2b9579cf81c9d71fef2aa
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-06-24 16:02:21 +0000

    Task #4405: trivial no-data (Correlator)Pipeline::ProcessObservation() test

--- branches/Cobalt-Task4405/RTCP/Cobalt/GPUProc/test/Pipelines/tCorrelatorPipelineProcessObs.cc
+++ branches/Cobalt-Task4405/RTCP/Cobalt/GPUProc/test/Pipelines/tCorrelatorPipelineProcessObs.cc
@@ -20,15 +20,46 @@
 
 #include <lofar_config.h>
 
+#include <vector>
+
+#ifdef HAVE_MPI
+#  include <mpi.h>
+#endif
+
 #include <Common/LofarLogger.h>
 #include <CoInterface/Parset.h>
+#include <InputProc/OMPThread.h>
 #include <GPUProc/Pipelines/CorrelatorPipeline.h>
 
 using namespace std;
+using namespace LOFAR;
 using namespace LOFAR::Cobalt;
 
-int main() {
+int main(int argc, char *argv[]) {
   INIT_LOGGER("tCorrelatorPipelineProcessObs");
+
+  omp_set_nested(true);
+  OMPThread::init();
+
+  if (setenv("DISPLAY", ":0", 1) < 0)
+  {
+    perror("error setting DISPLAY");
+    exit(1);
+  }
+
+  int rank = 0;
+  int nrHosts = 1;
+#ifdef HAVE_MPI
+  // Initialize and query MPI
+  int mpi_thread_support;
+  if (MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &mpi_thread_support) != MPI_SUCCESS) {
+    cerr << "MPI_Init failed" << endl;
+    exit(1);
+  }
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nrHosts);
+#endif
 
   try {
     gpu::Platform pf;
@@ -42,7 +73,30 @@ int main() {
   vector<gpu::Device> devices(1, device);
   gpu::Context ctx(device);
 
+  // Note: we just need to test the Pipeline part (processObservation()),
+  // not the WorkQueue and other logic below it, as that is already covered
+  // in other tests. (This is not a feature or integration test.)
+  // Testing Pipeline in isolation is next to impossible, but fake it
+  // with a parset where the number of input data blocks is 0. Then we don't
+  // have to start input procs etc. The disadvantage is that some code of
+  // processObservation() remains untested...
   Parset ps("tCorrelatorPipelineProcessObs.parset");
+
+  // "distribute" subbands over 1 node
+  vector<size_t> subbands;
+  for (size_t sb = 0; sb < ps.nrSubbands(); sb++)
+  {
+    subbands.push_back(sb);
+  }
+
+  // no data, so no need to run a sender:
+  // receiver(s) from processObservation() will fwd a end of data NULL pool item immediately.
+  // idem for storage proc: we'll get a failed to connect to storage log msg, but don't care.
+  CorrelatorPipeline(ps, subbands).processObservation();
+
+#ifdef HAVE_MPI
+  MPI_Finalize();
+#endif
 
   return 0;
 }
