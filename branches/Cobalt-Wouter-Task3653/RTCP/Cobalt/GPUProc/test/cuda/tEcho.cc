c5a3e31a2ce9898d5a444a13d26e9518cab54fc4
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-06-24 14:05:18 +0000

    Task #3653: commit of changes

--- branches/Cobalt-Wouter-Task3653/RTCP/Cobalt/GPUProc/test/cuda/tEcho.cc
+++ branches/Cobalt-Wouter-Task3653/RTCP/Cobalt/GPUProc/test/cuda/tEcho.cc
@@ -32,7 +32,9 @@
 #include <GPUProc/gpu_wrapper.h>
 #include <GPUProc/gpu_utils.h>
 #include <GPUProc/cuda/CudaRuntimeCompiler.h>
-
+#include <cuda.h>
+#include <curand.h>
+#include <curand_kernel.h>
 #include "TestUtil.h"
 
 using namespace std;
@@ -41,20 +43,30 @@ using namespace LOFAR::Cobalt::gpu;
 using namespace LOFAR::Cobalt;
 using LOFAR::Exception;
 
-unsigned NR_STATIONS = 4;
-unsigned NR_CHANNELS = 16;
-unsigned NR_SAMPLES_PER_CHANNEL = 64;
-unsigned NR_POLARIZATIONS = 2;
+unsigned NR_INPUTS = 1;
+unsigned NR_NEURONS = 16;  // 16 * 2 == width of thread wave
+unsigned NR_TIMESTEPS = 10;
 unsigned COMPLEX = 2;
-unsigned NR_BASELINES = (NR_STATIONS * (NR_STATIONS + 1) / 2);
+unsigned NR_BASENETWORKS = 1;
+unsigned NR_SOLUTION_PARALEL = 4;
 
-//
+float ACTIVATION_DECAY = 0.9;
+
+
+// Create the data arrays
+size_t lengthInputData = NR_INPUTS * NR_TIMESTEPS * COMPLEX ;
+size_t lengthInputsWeightsData = NR_INPUTS * NR_NEURONS * COMPLEX;
+size_t lengthWeightsData = NR_BASENETWORKS * NR_NEURONS * NR_NEURONS *  COMPLEX;
+size_t lengthActivationsData = NR_BASENETWORKS* NR_TIMESTEPS * NR_SOLUTION_PARALEL * NR_NEURONS * COMPLEX;
+
 HostMemory runTest(gpu::Context ctx,
                    Stream cuStream,
                    float * inputData,
+                   float * inputsWeightsData,
+                   float * weightsData,
                    string function)
 {
-  string kernelFile = "Correlator.cu";
+  string kernelFile = "Echo.cu";
 
   cout << "\n==== runTest: function = " << function << " ====\n" << endl;
 
@@ -66,12 +78,12 @@ HostMemory runTest(gpu::Context ctx,
   // Compile to ptx
   // Set op string string pairs to be provided to the compiler as defines
   definitions["NVIDIA_CUDA"] = "";
-  definitions["NR_STATIONS"] = lexical_cast<string>(NR_STATIONS);
-  definitions["NR_CHANNELS"] = lexical_cast<string>(NR_CHANNELS);
-  definitions["NR_SAMPLES_PER_CHANNEL"] = lexical_cast<string>(NR_SAMPLES_PER_CHANNEL);
-  definitions["NR_POLARIZATIONS"] = lexical_cast<string>(NR_POLARIZATIONS);
-  definitions["COMPLEX"] = lexical_cast<string>(COMPLEX);
-
+  definitions["NR_INPUTS"] = lexical_cast<string>(NR_INPUTS);
+  definitions["NR_NEURONS"] = lexical_cast<string>(NR_NEURONS);
+  definitions["NR_TIMESTEPS"] = lexical_cast<string>(NR_TIMESTEPS);
+  definitions["NR_BASENETWORKS"] = lexical_cast<string>(NR_BASENETWORKS);
+  definitions["NR_SOLUTION_PARALEL"] = lexical_cast<string>(NR_SOLUTION_PARALEL);
+  definitions["ACTIVATION_DECAY"] = lexical_cast<string>(ACTIVATION_DECAY);
   vector<Device> devices(1, ctx.getDevice());
   string ptx = createPTX(devices, kernelFile, flags, definitions);
   gpu::Module module(createModule(ctx, kernelFile, ptx));
@@ -79,34 +91,56 @@ HostMemory runTest(gpu::Context ctx,
 
   // *************************************************************
   // Create the data arrays
-  size_t sizeCorrectedData = NR_STATIONS * NR_CHANNELS * NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX * sizeof(float);
-  DeviceMemory devCorrectedMemory(ctx, sizeCorrectedData);
-  HostMemory rawCorrectedData = getInitializedArray(ctx, sizeCorrectedData, 0.0f);
+  size_t sizeInputData = lengthInputData* sizeof(float);
+  DeviceMemory devInputMemory(ctx, sizeInputData);
+  HostMemory rawInputData = getInitializedArray(ctx, sizeInputData, 1.0f);
+  float *rawInputPtr = rawInputData.get<float>();
+  for (unsigned idx = 0; idx < lengthInputData; ++idx)
+    rawInputPtr[idx] = inputData[idx];
+  cuStream.writeBuffer(devInputMemory, rawInputData);
+
+  size_t sizeInputsWeightsData = lengthInputsWeightsData * sizeof(float);
+  DeviceMemory devInputsWeightsMemory(ctx, sizeInputsWeightsData);
+  HostMemory rawInputsWeightsData = getInitializedArray(ctx, sizeInputsWeightsData, 2.0f);
+  float *rawInputsWeightsPtr = rawInputsWeightsData.get<float>();
+  for (unsigned idx = 0; idx < lengthInputsWeightsData; ++idx)
+    rawInputsWeightsPtr[idx] = inputsWeightsData[idx];
+  cuStream.writeBuffer(devInputsWeightsMemory, rawInputsWeightsData);
 
 
-  size_t sizeVisibilitiesData = NR_BASELINES * NR_CHANNELS * NR_POLARIZATIONS * NR_POLARIZATIONS * COMPLEX * sizeof(float);
-  DeviceMemory devVisibilitiesMemory(ctx, sizeVisibilitiesData);
-  HostMemory rawVisibilitiesData = getInitializedArray(ctx, sizeVisibilitiesData, 42.0f);
-  cuStream.writeBuffer(devVisibilitiesMemory, rawVisibilitiesData);
+  size_t sizeWeightsData = lengthWeightsData * sizeof(float);
+  DeviceMemory devWeightsMemory(ctx, sizeWeightsData);
+  HostMemory rawWeightsData = getInitializedArray(ctx, sizeWeightsData, 3.0f);
+  float *rawWeightsPtr = rawWeightsData.get<float>();
+  for (unsigned idx = 0; idx < lengthWeightsData; ++idx)
+    rawWeightsPtr[idx] = weightsData[idx];
+  cuStream.writeBuffer(devWeightsMemory, rawWeightsData);
 
-  //copy the input received as argument to the input array
-  float *rawDataPtr = rawCorrectedData.get<float>();
-  for (unsigned idx = 0; idx < sizeCorrectedData / sizeof(float); ++idx)
-    rawDataPtr[idx] = inputData[idx];
-  cuStream.writeBuffer(devCorrectedMemory, rawCorrectedData);
+  size_t sizeActivationsData = lengthActivationsData* sizeof(float);
+  DeviceMemory devActivationsMemory(ctx, sizeActivationsData);
+  HostMemory rawActivationsData = getInitializedArray(ctx, sizeActivationsData, 42.0f);
+  cuStream.writeBuffer(devActivationsMemory, rawActivationsData);
+
+  unsigned lengthRandomData = 128;
+  size_t sizeRandomData = lengthRandomData* sizeof(curandState);
+  DeviceMemory devRandomMemory(ctx, sizeRandomData);
+  cuStream.writeBuffer(devActivationsMemory, rawActivationsData);
 
   // ****************************************************************************
   // Run the kernel on the created data
-  hKernel.setArg(0, devVisibilitiesMemory);
-  hKernel.setArg(1, devCorrectedMemory);
+
+  hKernel.setArg(0, devActivationsMemory);
+  hKernel.setArg(1, devInputMemory);
+  hKernel.setArg(2, devInputsWeightsMemory);
+  hKernel.setArg(3, devWeightsMemory);
+  hKernel.setArg(4, devRandomMemory);
 
   // Calculate the number of threads in total and per blovk
-  unsigned nrBlocks = NR_BASELINES;
-  unsigned nrPasses = (nrBlocks + 1024 - 1) / 1024;
-  unsigned nrThreads = (nrBlocks + nrPasses - 1) / nrPasses;
-  unsigned nrUsableChannels = 15;
-  Grid globalWorkSize(nrPasses, nrUsableChannels, 1);
-  Block localWorkSize(nrThreads, 1,1);
+  //Grid globalWorkSize(nrPasses, nrUsableChannels, 1);
+  //Block localWorkSize(nrThreads, 1,1);
+
+  Grid globalWorkSize(1, 1, 1);
+  Block localWorkSize(NR_SOLUTION_PARALEL, 1, 1);
 
   // Run the kernel
   cuStream.synchronize(); // assure memory is copied
@@ -114,10 +148,10 @@ HostMemory runTest(gpu::Context ctx,
   cuStream.synchronize(); // assure that the kernel is finished
 
   // Copy output vector from GPU buffer to host memory.
-  cuStream.readBuffer(rawVisibilitiesData, devVisibilitiesMemory);
+  cuStream.readBuffer(rawActivationsData, devActivationsMemory);
   cuStream.synchronize(); //assure copy from device is done
 
-  return rawVisibilitiesData;
+  return rawActivationsData; //TODO CORRECT THIS OUTPUT!!!
 }
 
 Exception::TerminateHandler t(Exception::terminate);
@@ -126,10 +160,13 @@ int main()
 {
   INIT_LOGGER("tCorrelator");
 
-  try {
+  try 
+  {
     gpu::Platform pf;
     cout << "Detected " << pf.size() << " CUDA devices" << endl;
-  } catch (gpu::CUDAException& e) {
+  } 
+  catch (gpu::CUDAException& e) 
+  {
     cerr << e.what() << endl;
     return 3;
   }
@@ -140,169 +177,75 @@ int main()
   Stream cuStream(ctx);
 
   // Define dependend paramters
-  unsigned lengthInputData = NR_STATIONS * NR_CHANNELS * NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX;
-  unsigned lengthOutputData = NR_BASELINES * NR_CHANNELS * NR_POLARIZATIONS * NR_POLARIZATIONS * COMPLEX;
-
+  // *************************************************************
   // Create data members
   float * inputData = new float[lengthInputData];
-  float * outputData = new float[lengthOutputData];
+  for (unsigned idx = 0; idx < lengthInputData / 2; ++idx)
+  {
+    inputData[idx * 2] = 1.0f;
+    inputData[idx * 2 + 1] = 0.0f;
+  }
+  float * inputsWeightsData = new float[lengthInputsWeightsData];
+  for (unsigned idx = 0; idx < lengthInputsWeightsData / 2; ++idx)
+  {
+    inputsWeightsData[idx * 2] = 0.02f;
+    inputsWeightsData[idx * 2+ 1] = 0.0f;
+  }
+  float * weightsData= new float[lengthWeightsData];
+  for (unsigned idx = 0; idx < lengthWeightsData/2; ++idx)
+  {
+    weightsData[idx * 2] = 5.0f;
+    weightsData[idx * 2 + 1] = 0.0f;
+  }
+  float * activationsData = new float[lengthActivationsData];
+
   float * outputOnHostPtr;
+  
+  const char* function = "echo";
 
-  const char * kernel_functions[] = {
-    "correlate", "correlate_2x2", "correlate_3x3", "correlate_4x4"
-  };
-  unsigned nr_kernel_functions =
-    sizeof(kernel_functions) / sizeof(kernel_functions[0]);
+  
+  // ***********************************************************
+  // Baseline test: If all input data is zero the output should be zero
+  // The output array is initialized with 42s
+  HostMemory outputOnHost = runTest(ctx, cuStream, inputData,
+    inputsWeightsData,weightsData, function);
 
-  for (unsigned func_idx = 0; func_idx < nr_kernel_functions; func_idx++) {
-    const char* function = kernel_functions[func_idx];
+  // Copy the output data to a local array
+  outputOnHostPtr = outputOnHost.get<float>();
+  for (size_t idx = 0; idx < lengthActivationsData; ++idx)
+    activationsData[idx] = outputOnHostPtr[idx];
 
-    // ***********************************************************
-    // Baseline test: If all input data is zero the output should be zero
-    // The output array is initialized with 42s
-    for (unsigned idx = 0; idx < lengthInputData; ++idx)
-      inputData[idx] = 0;
-
-    HostMemory outputOnHost = runTest(ctx, cuStream, inputData, function);
-
-    // Copy the output data to a local array
-    outputOnHostPtr = outputOnHost.get<float>();
-    for (unsigned idx = 0; idx < lengthOutputData; ++idx)
-      outputData[idx] = outputOnHostPtr[idx];
-
-    // Now validate the outputdata
-    for (unsigned idx_baseline = 0; idx_baseline < NR_BASELINES; ++idx_baseline)
+  cout << "Returned output: "  << endl;
+  for (unsigned idx_basenetworks = 0; idx_basenetworks < NR_BASENETWORKS; ++ idx_basenetworks)
+  {
+    unsigned start_basenetwork_idx = idx_basenetworks * NR_TIMESTEPS * NR_SOLUTION_PARALEL * NR_NEURONS * COMPLEX;
+    for (unsigned idx_timestep = 0; idx_timestep < NR_TIMESTEPS; ++ idx_timestep)
     {
-      cerr << "baseline: " << idx_baseline << endl;
-      for (unsigned idx_channels = 0; idx_channels < NR_CHANNELS; ++idx_channels)
+      unsigned start_timestep_idx = idx_timestep * NR_SOLUTION_PARALEL * NR_NEURONS * COMPLEX;
+      cout << "timestep " << idx_timestep << " |";
+      for (unsigned idx_solutions =0; idx_solutions < NR_SOLUTION_PARALEL;++idx_solutions)
       {
-        cerr << idx_channels << " : ";
-        for (unsigned idx = 0; idx < 8; ++idx)
+        unsigned start_solutions_idx = idx_solutions * NR_NEURONS * COMPLEX;
+        
+        for (unsigned idx_neuron = 0; idx_neuron < 2; ++ idx_neuron)
         {
-          unsigned idx_in_output_data =
-            idx_baseline * NR_CHANNELS * NR_POLARIZATIONS * NR_POLARIZATIONS * COMPLEX +
-            idx_channels * NR_POLARIZATIONS * NR_POLARIZATIONS * COMPLEX +
-            idx;
-          cerr << outputData[idx_in_output_data] << ", ";
-          if ( idx_channels != 0)
-            if (outputData[idx_in_output_data] != 0)
-            {
-              cerr << "Non zero number encountered while all input data was zero. Exit -1";
-              delete [] inputData;
-              delete [] outputData;
-              return -1;
-            }
+          cout << "(" << activationsData[start_basenetwork_idx + start_timestep_idx + start_solutions_idx + idx_neuron]
+               << ", " << activationsData[start_basenetwork_idx + start_timestep_idx + start_solutions_idx + idx_neuron + 1]
+               << ")" ;
         }
-        cerr << endl;
+        cout << "|";
       }
+      cout << endl;
     }
+  }
 
-    // ***********************************************************
-    // To test if the kernel is working we try to correlate a number of delayed
-    // random initialize channels.
-    // With zero delay the output should be the highest. A fringe :D
-
-    // 1. First create a random channel with a length that is large enough
-    // It should be length NR_SAMPLES_PER_CHANNEL plus padding at both side to encompass the delay
-    unsigned padding = 7; // We have 15 channels with content 2 * 7 delays + delay 0
-    unsigned lengthRandomData = NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX + 2 * padding * 4;
-    float * randomInputData = new float[lengthRandomData];
-
-    // Create the random signal, seed random generator with zero
-    srand (0);
-    for (unsigned idx = 0; idx < lengthRandomData; ++idx)
-      randomInputData[idx] = ((rand() % 1024 + 1) * 1.0  // make float
-                              - 512.0) / 512.0;          // centre around zero and normalize
-
-    // Fill the input array channels
-    // channel 8 is the non delayed channel and should correlate
-    for (unsigned idx_station = 0; idx_station < 2; ++idx_station)
-    {
-      cerr << "idx_station: " << idx_station << endl;
-      for (unsigned idx_channel = 0; idx_channel < 16; ++idx_channel)
-      {
-        for (unsigned idx_datapoint = 0;
-             idx_datapoint< NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX;
-             ++idx_datapoint)
-        {
-          // In the input array step trough the channel
-          unsigned idx_inputdata =
-            idx_station * 16 * NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX +
-            idx_channel * NR_SAMPLES_PER_CHANNEL * NR_POLARIZATIONS * COMPLEX +
-            idx_datapoint;
-
-          // Pick from the random array the same number of samples
-          // But with an offset depending on the channel number
-          unsigned padding_offset = 32;
-          int padding = padding_offset - idx_channel * 4;       // station 1 is filled with delayed signal
-
-          // Station zero is anchored as the middle and is filled with the baseline signal
-          if (idx_station == 0)
-            padding = padding_offset - 8 * 4;
-
-          // Get the index in the random signal array
-          unsigned idx_randomdata = padding_offset + padding + idx_datapoint;
-
-          //assign the signal;
-          inputData[idx_inputdata] = randomInputData[idx_randomdata];
-          if (idx_datapoint < 16)        // plot first part if the input signal for debugging purpose
-            cerr << inputData[idx_inputdata] << " : ";
-        }
-        cerr << endl;
-      }
-    }
-
-    // Run the kernel
-    outputOnHost = runTest(ctx, cuStream, inputData, function);
-
-    // Copy the output data to a local array
-    outputOnHostPtr = outputOnHost.get<float>();
-    for (unsigned idx = 0; idx < lengthOutputData; ++idx)
-      outputData[idx] = outputOnHostPtr[idx];
-
-    // Target value for correlation channel
-    float targetValues[8] = {36.2332, 0, -7.83033, 3.32368, -7.83033, -3.32368, 42.246, 0};
-
-    // print the contents of the output array for debugging purpose
-    for (unsigned idx_baseline = 0; idx_baseline < NR_BASELINES; ++idx_baseline)
-    {
-      cerr << "baseline: " << idx_baseline << endl;
-      for (unsigned idx_channels = 0; idx_channels < NR_CHANNELS; ++idx_channels)
-      {
-        cerr << idx_channels << " : ";
-        for (unsigned idx = 0; idx < 8; ++idx)
-        {
-          unsigned idx_in_output_data = idx_baseline * NR_CHANNELS * 8 +
-                                        idx_channels * 8 +
-                                        idx;
-          if ( idx_baseline == 1 && idx_channels == 8)
-          {
-
-            //validate that the correct value is found
-            if ( abs(outputData[idx_in_output_data] - targetValues[idx]) > 0.0001)
-            {
-              cerr << "The correlated data found was not within an acceptable delta:" << endl
-                   << "Expected: " << outputData[idx_in_output_data] << endl
-                   << "Found: " << targetValues[idx] << endl
-                   << "Difference: " << outputData[idx_in_output_data] - targetValues[idx]
-                   << "  Delta: " << 0.0001;
-
-              delete [] inputData;
-              delete [] outputData;
-              return -1;
-            }
-          }
-          cerr << outputData[idx_in_output_data] << ", ";
-        }
-        cerr << endl;
-      }
-    }
-    // Validate that the channel 8 had fringes
-
-  } // for func_idx
 
   delete [] inputData;
-  delete [] outputData;
+  delete [] inputsWeightsData;
+  delete [] weightsData;
+  delete [] activationsData;
+
   return 0;
 }
+
 
