b5ed1b62bd6e4bab533304776443f83c44c5f0cf
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-06-30 06:33:54 +0000

    Task #4618: Created task branch

---
+++ branches/Cobalt-Task4618/RTCP/Cobalt/GPUProc/src/cuda/Kernels/FIR_FilterKernel.h
@@ -0,0 +1,71 @@
+//# FIR_FilterKernel.h
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#ifndef LOFAR_GPUPROC_CUDA_FIR_FILTER_KERNEL_H
+#define LOFAR_GPUPROC_CUDA_FIR_FILTER_KERNEL_H
+
+#include <CoInterface/Parset.h>
+
+#include <GPUProc/Kernels/Kernel.h>
+#include <GPUProc/FilterBank.h>
+#include <GPUProc/gpu_wrapper.h>
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+    class FIR_FilterKernel : public Kernel
+    {
+    public:
+      FIR_FilterKernel(const Parset &ps,
+                       gpu::Context &context,
+                       gpu::DeviceMemory &devFilteredData,
+                       gpu::DeviceMemory &devInputSamples,
+                       gpu::Stream &stream);
+
+      FIR_FilterKernel(const Parset &ps,
+                       gpu::Module &module,
+                       gpu::DeviceMemory &devFilteredData,
+                       gpu::DeviceMemory &devInputSamples,
+                       gpu::Stream &stream);
+
+      enum BufferType
+      {
+        INPUT_DATA,
+        OUTPUT_DATA,
+        FILTER_WEIGHTS
+      };
+
+      // Return required buffer size for \a bufferType
+      static size_t bufferSize(const Parset& ps, BufferType bufferType);
+
+    private:
+      void init(gpu::DeviceMemory &devFilteredData,
+                gpu::DeviceMemory &devInputSamples,
+                gpu::Stream &stream);
+
+      gpu::DeviceMemory devFIRweights;
+
+    };
+  }
+}
+
+#endif
+
