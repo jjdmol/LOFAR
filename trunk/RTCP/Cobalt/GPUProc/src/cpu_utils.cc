232087e7f4d37b91d138e563fd97cbe6de851629
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-08-01 07:47:15 +0000

    Task #4666: Backported r25883 from closed branch: derive core list of cpu from /sys

--- trunk/RTCP/Cobalt/GPUProc/src/cpu_utils.cc
+++ trunk/RTCP/Cobalt/GPUProc/src/cpu_utils.cc
@@ -1,42 +1,60 @@
 #include <lofar_config.h>
 
+#include <fstream>
 #include <sched.h>
+#include <boost/format.hpp>
 #include <CoInterface/Parset.h>
+#include <CoInterface/Exceptions.h>
 
 #include <Common/SystemCallException.h>
+#include <CoInterface/PrintVector.h>
 
 namespace LOFAR
 {
   namespace Cobalt
   {
-    // Set the correct processer affinity based on the parset entry
-    // settings.nodes[rank].cpu
+    // Set the correct processer affinity
     void setProcessorAffinity(unsigned cpuId)
     {
-      // Get the number of cpu's (32)
-      unsigned numCPU = sysconf(_SC_NPROCESSORS_ONLN );
+      // Get the number of cores (32)
+      unsigned numCores = sysconf(_SC_NPROCESSORS_ONLN);
 
-      // Get a valid cpu set
-      // sched_getaffinity(pid, size of cpu set, mask to place data in) //pid 0 = current thread
+
+      // Determine the cores local to the specified cpuId
+      vector<unsigned> localCores;
+
+      for (unsigned core = 0; core < numCores; ++core) {
+        // The file below contains an integer indicating the physical CPU
+        // hosting this core.
+        std::ifstream fs(str(boost::format("/sys/devices/system/cpu/cpu%u/topology/physical_package_id") % core).c_str());
+
+        unsigned physical_cpu;
+        fs >> physical_cpu;
+
+        if (!fs.good())
+          continue;
+
+        // Add this core to the mask if it matches the requested CPU
+        if (physical_cpu == cpuId)
+          localCores.push_back(core);
+      }
+
+      if (localCores.empty())
+        THROW(GPUProcException, "Request to bind to non-existing CPU: " << cpuId);
+
+      LOG_DEBUG_STR("Binding to CPU " << cpuId << ": cores " << localCores);
+
+      // put localCores in a cpu_set
       cpu_set_t mask;  
-      bool sched_return = sched_getaffinity(0, sizeof(cpu_set_t), &mask);
-      int localerrno = errno;
-      if (sched_return != 0)
-        throw SystemCallException("sched_getaffinity", localerrno, THROW_ARGS);
 
-
-      // zero the mask 
       CPU_ZERO(&mask); 
 
-      // Set the mask, beginning with cpuId and set the corresponding cpus
-      // use a step of two, this is cobalt specific. The Cobalt procs 
-      // are even/odd for each physical core
-      for (unsigned idx_cpu = cpuId; idx_cpu < numCPU; idx_cpu += 2)  
-        CPU_SET(idx_cpu, &mask);
+      for (vector<unsigned>::const_iterator i = localCores.begin(); i != localCores.end(); ++i)
+        CPU_SET(*i, &mask);
 
       // now assign the mask and set the affinity
-      sched_return = sched_setaffinity(0, sizeof(cpu_set_t), &mask);
-      localerrno = errno;
+      int sched_return = sched_setaffinity(0, sizeof(cpu_set_t), &mask);
+      int localerrno = errno;
       if (sched_return != 0)
         throw SystemCallException("sched_setaffinity", localerrno, THROW_ARGS);
     }
