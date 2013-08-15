29b6925a9d4b40bbd269859045b2ba7fb3a5bd15
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-08-15 07:29:36 +0000

    Task #4704: Created task branch.

---
+++ branches/Cobalt-Task4704/MAC/APL/PIC/TBB_Driver/src/TrigSetupSameCmd.h
@@ -0,0 +1,67 @@
+//#  -*- mode: c++ -*-
+//#
+//#  TrigSetupSameCmd.h: III
+//#
+//#  Copyright (C) 2002-2004
+//#  ASTRON (Netherlands Foundation for Research in Astronomy)
+//#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//#  This program is free software; you can redistribute it and/or modify
+//#  it under the terms of the GNU General Public License as published by
+//#  the Free Software Foundation; either version 2 of the License, or
+//#  (at your option) any later version.
+//#
+//#  This program is distributed in the hope that it will be useful,
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#  GNU General Public License for more details.
+//#
+//#  You should have received a copy of the GNU General Public License
+//#  along with this program; if not, write to the Free Software
+//#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//#  $Id$
+
+#ifndef TRIGSETUPSAMECMD_H_
+#define TRIGSETUPSAMECMD_H_
+
+#include <Common/LofarTypes.h>
+#include <GCF/TM/GCF_Control.h>
+
+#include <APL/TBB_Protocol/TBB_Protocol.ph>
+#include "TP_Protocol.ph"
+
+#include "Command.h"
+#include "DriverSettings.h"
+
+namespace LOFAR {
+	using namespace TBB_Protocol;
+	namespace TBB {
+
+class TrigSetupSameCmd : public Command 
+{
+public:
+	// Constructors for a TrigSetupSameCmd object.
+	TrigSetupSameCmd();
+
+	// Destructor for TrigSetupSameCmd.
+	virtual ~TrigSetupSameCmd();
+	
+	virtual bool isValid(GCFEvent& event);
+	
+	virtual void saveTbbEvent(GCFEvent& event);
+						
+	virtual void sendTpEvent();
+
+	virtual void saveTpAckEvent(GCFEvent& event);
+
+	virtual void sendTbbAckEvent(GCFPortInterface* clientport);
+	
+private:
+	TbbSettings *TS;
+};
+
+	} // end TBB namespace
+} // end LOFAR namespace
+
+#endif /* TRIGSETUPSAMECMD_H_ */
---
+++ branches/Cobalt-Task4704/RTCP/Cobalt/GPUProc/share/gpu/kernels/CoherentStokes.cu
@@ -0,0 +1,2 @@
+// Only needed to have the tCoherentStokesKernel test compile its kernel (this file). Wouter, just replace this file
+
