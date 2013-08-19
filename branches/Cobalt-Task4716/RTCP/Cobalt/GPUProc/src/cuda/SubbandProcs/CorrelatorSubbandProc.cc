59c2c506b9038416366c2e5f61b95c2dda23f230
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-08-19 17:22:33 +0000

    Task #4716: Create a lot more outputPool elements to allow smooth operations with many subbands/subbandproc

--- branches/Cobalt-Task4716/RTCP/Cobalt/GPUProc/src/cuda/SubbandProcs/CorrelatorSubbandProc.cc
+++ branches/Cobalt-Task4716/RTCP/Cobalt/GPUProc/src/cuda/SubbandProcs/CorrelatorSubbandProc.cc
@@ -91,7 +91,7 @@ namespace LOFAR
       correlatorKernel(factories.correlator.create(queue, correlatorBuffers))
     {
       // put enough objects in the outputPool to operate
-      for (size_t i = 0; i < 3; ++i) {
+      for (size_t i = 0; i < std::max(3UL, ps.nrSubbands()); ++i) {
         outputPool.free.append(new CorrelatedDataHostBuffer(
                 ps.nrStations(),
                 ps.nrChannelsPerSubband(),
