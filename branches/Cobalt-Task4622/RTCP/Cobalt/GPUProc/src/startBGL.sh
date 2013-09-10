ede9957ededa7f08b34c0cd48f12a832e0d24915
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-09-10 17:14:57 +0000

    Task #4622: Added initial version of startBGL/stopBGL scripts for MAC

---
+++ branches/Cobalt-Task4622/RTCP/Cobalt/GPUProc/src/startBGL.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# startBGL.sh jobName executable workingDir parset observationID
+#
+# jobName
+# executable      executable file (should be in a place that is readable from BG/L)
+# workingDir      directory for output files (should be readable by BG/L)
+# parset          the parameter file
+# observationID   observation number
+#
+# This script is called by OnlineControl to start an observation.
+
+PARSET_MAC="$4"
+OBSID="$5"
+
+# The name of what will be our parset
+PARSET=$LOFARROOT/var/run/Observation$OBSID.parset
+
+# The file to store the PID in
+PIDFILE=$LOFARROOT/var/run/rtcp-$OBSID.pid
+
+# Prepare environment, typically needed only once
+mkdir -p $LOFARROOT/var/{run,log}
+
+(
+# Always print a header, to match errors to observations
+echo "---------------"
+echo "now:      " `date +"%F %T"`
+echo "called as: $0 $@"
+echo "pwd:       $PWD"
+echo "LOFARROOT: $LOFARROOT"
+echo "obs id:    $OBSID"
+echo "parset:    $PARSET_MAC"
+echo "---------------"
+
+function error {
+  echo "$@"
+  exit 1
+}
+
+# Add static keys
+cat $PARSET_MAC $LOFARROOT/etc/parset-additions.d/*.parset > $PARSET || error "Could not create parset $PARSET"
+
+# Determine start parameters
+HOSTS=`mpi_node_list -n $PARSET | tail -n 1`
+echo "Hosts: $HOSTS"
+
+# Start observation in the background
+COMMAND="nohup mpirun.sh -H $HOSTS rtcp $PARSET > $LOFARROOT/var/log/rtcp-$OBSID.log 2>&1 &"
+echo "Running $COMMAND"
+eval $COMMAND
+PID=$!
+echo "PID: $PID"
+
+# Keep track of PID for stop script
+echo "PID file: $PIDFILE"
+echo $PID > $PIDFILE || error "Could not write PID file: $PIDFILE"
+
+# Done
+echo "Done"
+
+) 2>&1 | tee -a $LOFARROOT/var/log/startBGL.log
+
