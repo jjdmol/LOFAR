66deadedb07f25af5ba27ae5ef8b087a9b6931ce
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-09-11 09:11:34 +0000

    Task #4622: Catch SIGTERM+SIGINT+SIGQUIT+SIGHUP, and explicitly copy the parsets to the remote hosts

--- branches/Cobalt-Task4622/RTCP/Cobalt/GPUProc/src/runObservation.sh
+++ branches/Cobalt-Task4622/RTCP/Cobalt/GPUProc/src/runObservation.sh
@@ -20,12 +20,31 @@ function error {
 HOSTS=`mpi_node_list -n "$PARSET"`
 echo "Hosts: $HOSTS"
 
+# Copy parset to all hosts
+for h in `echo $HOSTS | tr ',' ' '`
+do
+  # Ignore empty hostnames
+  [ -z "$h" ] && continue;
+
+  # Ignore hostnames that point to us
+  [ "$h" == "localhost" ] && continue;
+  [ "$h" == "`hostname`" ] && continue;
+
+  # Ignore hosts that already have the parset
+  # (for example, through NFS).
+  ssh -qn $h [ -e $PWD/$PARSET ] && continue;
+
+  # Copy parset to remote node
+  echo "Copying parset to $h:$PWD"
+  scp -Bq $PARSET $h:$PWD || error "Could not copy parset to $h"
+done
+
 # Run in the background to allow signals to propagate
 mpirun.sh -H "$HOSTS" rtcp "$PARSET" &
 PID=$!
 
 # Propagate SIGTERM
-trap "echo runObservation.sh: killing $PID; kill $PID" SIGTERM
+trap "echo runObservation.sh: killing $PID; kill $PID" SIGTERM SIGINT SIGQUIT SIGHUP
 
 # Wait for $COMMAND to finish. We use 'wait' because it will exit immediately if it
 # receives a signal.
