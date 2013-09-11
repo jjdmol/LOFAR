63245cab9dd053db1abb929a09007cf1dcd5c623
Marcel Loose <gmloose@users.noreply.github.com>
2013-09-11 13:37:07 +0000

    Task #2669: Always check for avaible GPU devices first and bail out with exit status 3 (UNTESTED) if there are none.

--- RTCP/Cobalt/GPUProc/test/Kernels/tCoherentStokesKernel.cc
+++ RTCP/Cobalt/GPUProc/test/Kernels/tCoherentStokesKernel.cc
@@ -59,6 +59,12 @@ TEST_FIXTURE(TestFixture, MustThrow)
 int main()
 {
   INIT_LOGGER("tCoherentStokesKernel");
+  try {
+    gpu::Platform pf;
+  } catch (gpu::GPUException&) {
+    cerr << "No GPU device(s) found. Skipping tests." << endl;
+    return 3;
+  }
   return UnitTest::RunAllTests() == 0 ? 0 : 1;
 }
 
