5f7ce46febfced3ae77592a2eade82c1766d65cc
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-09-12 11:42:48 +0000

    Task #4806: Added LTA feedback and status propagation to OnlineControl

--- branches/Cobalt-Task4806/RTCP/Cobalt/GPUProc/src/runObservation.sh
+++ branches/Cobalt-Task4806/RTCP/Cobalt/GPUProc/src/runObservation.sh
@@ -14,7 +14,20 @@ function error {
   exit 1
 }
 
+function getkey {
+  KEY=$1
+  <$PARSET perl -ne '/^'$KEY'\s*=\s*"?(.*?)"?\s*$/ || next; print "$1";'
+}
+
 [ -n "$PARSET" ] || error "No parset specified"
+[ -r "$PARSET" ] || error "Cannot read parset: $PARSET"
+
+OBSID=`getkey Observation.ObsID`
+echo "Observation ID: $OBSID"
+
+# ******************************
+# Run the observation
+# ******************************
 
 # Determine start parameters
 HOSTS=`mpi_node_list -n "$PARSET"`
@@ -32,11 +45,11 @@ do
 
   # Ignore hosts that already have the parset
   # (for example, through NFS).
-  ssh -qn $h [ -e $PWD/$PARSET ] && continue;
+  timeout 5s ssh -qn $h [ -e $PWD/$PARSET ] && continue;
 
   # Copy parset to remote node
   echo "Copying parset to $h:$PWD"
-  scp -Bq $PARSET $h:$PWD || error "Could not copy parset to $h"
+  timeout 30s scp -Bq $PARSET $h:$PWD || error "Could not copy parset to $h"
 done
 
 # Run in the background to allow signals to propagate
@@ -53,4 +66,41 @@ trap "echo runObservation.sh: killing $PID; kill $PID" SIGTERM SIGINT SIGQUIT SI
 #   signal:    >128
 #   no signal: return code of mpirun.sh
 wait $PID
+OBSRESULT=$?
+
+echo "Result code of observation: $OBSRESULT"
+
+# ******************************
+# Post-process the observation
+# ******************************
+#
+# Note: don't propagate errors here as observation failure,
+#       because that would be too harsh and also makes testing
+#       harder.
+
+ONLINECONTROL_HOST=`getkey Cobalt.Feedback.host`
+ONLINECONTROL_RESULT_PORT=$((21000 + $OBSID % 1000))
+
+if [ $OBSRESULT -eq 0 ]
+then
+  # ***** Observation ran successfully
+
+  # 1. Copy LTA feedback file to ccu001
+  FEEDBACK_DEST=$ONLINECONTROL_HOST:`getkey Cobalt.Feedback.remotePath`
+  echo "Copying feedback to $FEEDBACK_DEST"
+  timeout 30s scp $LOFARROOT/var/run/Observation${OBSID}_feedback $FEEDBACK_DEST
+
+  # 2. Signal success to OnlineControl
+  echo "Signalling success to $ONLINECONTROL_HOST"
+  echo -n "FINISHED" > /dev/tcp/$ONLINECONTROL_HOST/$ONLINECONTROL_RESULT_PORT
+else
+  # ***** Observation failed for some reason
+
+  # 1. Signal failure to OnlineControl
+  echo "Signalling failure to $ONLINECONTROL_HOST"
+  echo -n "ABORT" > /dev/tcp/$ONLINECONTROL_HOST/$ONLINECONTROL_RESULT_PORT
+fi
+
+# Our exit code is that of the observation
+exit $OBSRESULT
 
--- branches/Cobalt-Task4806/RTCP/Cobalt/GPUProc/test/testFuncs.sh
+++ branches/Cobalt-Task4806/RTCP/Cobalt/GPUProc/test/testFuncs.sh
@@ -12,3 +12,43 @@ haveGPU()
   fi
   return 0
 }
+
+function error {
+  echo "$@"
+  exit 1
+}
+
+# set testdir to mirror srcdir
+testdir=$PWD
+
+function mklofarroot {
+  DEST=$1
+
+  # Make a fake LOFARROOT to be able to locate:
+  #   $LOFARROOT/bin:   startBGL.sh, stopBGL.sh, rtcp, mpi_node_list
+  #   $LOFARROOT/etc:   parset-additions.d/
+  #   $LOFARROOT/share: gpu/kernels/*.cu
+  # and the share/ directory for the GPU kernels
+  mkdir -p $DEST || error "Cannot mkdir $DEST"
+  export LOFARROOT=$PWD/$DEST
+  echo "LOFARROOT=$LOFARROOT"
+
+  ln -sfT $PWD/../src $LOFARROOT/bin || error "ln failed for bin"
+  ln -sfT $srcdir/../etc $LOFARROOT/etc || error "ln failed for etc"
+  ln -sfT $srcdir/../share $LOFARROOT/share || error "ln failed for share"
+  mkdir -p $LOFARROOT/var/{run,log} || error "mkdir failed for var/run or var/log"
+  mkdir -p $LOFARROOT/tmp || error "mkdir failed for tmp"
+
+  # Let's work within our temp space
+  cd $LOFARROOT/tmp || error "could not cd $LOFARROOT/tmp"
+
+  # Crude hack for the apps to find their log_prop files. It will mess up
+  # stdout without it.
+  cp $srcdir/../src/Station/mpi_node_list.log_prop . || error "Could not copy mpi_node_list.log_prop"
+  cp $srcdir/../src/rtcp.log_prop . || error "Could not copy rtcp.log_prop"
+
+  # Be able to find all binaries through $PATH
+  export PATH=$LOFARROOT/bin:$srcdir/../src:$srcdir:$PATH
+  echo "PATH=$PATH"
+}
+
