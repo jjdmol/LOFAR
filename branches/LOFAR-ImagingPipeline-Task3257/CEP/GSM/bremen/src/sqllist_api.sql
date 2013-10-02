ad03e2775bb8c09366ef881eb32d20ee6f7f111c
schoenmakers <schoenmakers@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-02 15:05:32 +0000

    Task #3257: Delete of task branch

--- branches/LOFAR-ImagingPipeline-Task3257/CEP/GSM/bremen/src/sqllist_api.sql
+++
@@ -1,7 +0,0 @@
---#APIimage
-select i.imagename, count(*) as source_count, min(f_peak) as min_flux, count(r.runcatid) as new_source_count
-  from images i,
-  inner join extractedsources e on (e.image_id = i.imageid)
-  left outer join runningcatalog r on (r.first_xtrsrc_id = e.xtrsrcid)
- where i.imageid = {0}
-group by imagename;
