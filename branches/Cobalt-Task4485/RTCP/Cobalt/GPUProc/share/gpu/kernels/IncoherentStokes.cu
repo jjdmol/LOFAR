120fe835b8c8609489b9163d12182a69227c784b
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-08 13:50:34 +0000

    Task #4485: Initial version of ported IncoherentStokes kernel.

---
+++ branches/Cobalt-Task4485/RTCP/Cobalt/GPUProc/share/gpu/kernels/IncoherentStokes.cu
@@ -0,0 +1,113 @@
+//# IncoherentStokes.cu
+//#
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+// \file
+// This file contains a CUDA implementation of the GPU kernel for the Incoherent
+// Stokes part of the beam-former pipeline. It calculates station beams by
+// adding the complex voltages of the station antennas without correcting for
+// delays; hence the term \e incoherent.
+
+#if !(INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR >= 1)
+#error Precondition violated: INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR >= 1
+#endif
+
+#if !(NR_CHANNELS >= 1)
+#error Precondition violated: NR_CHANNELS >= 1
+#endif
+
+#if !(NR_INCOHERENT_STOKES == 1 || NR_INCOHERENT_STOKES == 4)
+#error Precondition violated: NR_INCOHERENT_STOKES == 1 || NR_INCOHERENT_STOKES == 4
+#endif
+
+#if !(NR_SAMPLES_PER_CHANNEL && NR_SAMPLES_PER_CHANNEL % INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR == 0)
+#error Precondition violated: NR_SAMPLES_PER_CHANNEL && NR_SAMPLES_PER_CHANNEL % INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR == 0
+#endif
+
+#if !(NR_STATIONS >= 1)
+#error Precondition violated: NR_STATIONS >= 1
+#endif
+
+typedef float (*IncoherentStokesType)[NR_INCOHERENT_STOKES][NR_SAMPLES_PER_CHANNEL / INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR][NR_CHANNELS];
+typedef float4 (*InputType)[NR_STATIONS][NR_CHANNELS][NR_SAMPLES_PER_CHANNEL / INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR][INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR];
+
+
+/*!
+ * Computes the \e incoherent Stokes parameters. The incoherent Stokes
+ * parameters are calculated by adding the complex voltages of the station
+ * antennas without correcting for delays due to (pencil) beam forming.
+ *
+ * Pre-processor input symbols (some are tied to the execution configuration)
+ * Symbol                  | Valid Values            | Description
+ * ----------------------- | ----------------------- | -----------
+ * INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR | >= 1  | number of samples to sum into one output sample
+ * NR_CHANNELS             | >= 1                    | number of frequency channels per subband
+ * NR_INCOHERENT_STOKES    | 1, 4                    | number of Stokes parameters; either 1 (I) or 4 (I,Q,U,V)
+ * NR_SAMPLES_PER_CHANNEL  | multiple of INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR | number of input samples per channel
+ * NR_STATIONS             | >= 1                    | number of antenna fields
+ *
+ */
+
+/* __kernel void incoherentStokes(__global void *restrict stokesPtr, */
+/*                                __global const void *restrict inputPtr) */
+extern "C" __global__ void incoherentStokes(IncoherentStokesType stokes,
+                                            const InputType input)
+{
+  /* uint time = get_global_id(0); */
+  /* uint channel = get_global_id(1); */
+  uint time = blockIdx.x * blockDim.x + threadIdx.x;
+  uint channel = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (time >= NR_SAMPLES_PER_CHANNEL / INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR)
+    return;
+
+  float stokesI = 0;
+#if NR_INCOHERENT_STOKES == 4
+  float stokesQ = 0, halfStokesU = 0, halfStokesV = 0;
+#endif
+
+  for (uint station = 0; station < NR_STATIONS; station++) {
+    for (uint t = 0; t < INCOHERENT_STOKES_TIME_INTEGRATION_FACTOR; t++) {
+      /* float4 sample = (*input)[station][channel][time][t]; */
+      /* float2 X = sample.xy; */
+      /* float2 Y = sample.zw; */
+      float4 sample = (*input)[station][channel][time][t];
+      float2 X = make_float2(sample.x, sample.y);
+      float2 Y = make_float2(sample.z, sample.w);
+      float powerX = X.x * X.x + X.y * X.y;
+      float powerY = Y.x * Y.x + Y.y * Y.y;
+
+      stokesI += powerX + powerY;
+#if NR_INCOHERENT_STOKES == 4
+      stokesQ += powerX - powerY;
+      halfStokesU += X.x * Y.x + X.y * Y.y;
+      halfStokesV += X.y * Y.x - X.x * Y.y;
+#endif
+    }
+  }
+
+  (*stokes)[0][time][channel] = stokesI;
+#if NR_INCOHERENT_STOKES == 4
+  (*stokes)[1][time][channel] = stokesQ;
+  (*stokes)[2][time][channel] = 2 * halfStokesU;
+  (*stokes)[3][time][channel] = 2 * halfStokesV;
+#endif
+}
+
