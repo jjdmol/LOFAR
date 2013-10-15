d597044249a2bf29bcbf66589f723c4e3052cb90
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-15 01:49:18 +0000

    Task #4803: fix and test casacore auto-update measure tables scripts for jenkins@fs5/DAS-4.

--- trunk/RTCP/Cobalt/GPUProc/src/scripts/apply_casacore_measures_data.sh
+++ trunk/RTCP/Cobalt/GPUProc/src/scripts/apply_casacore_measures_data.sh
@@ -1,32 +1,44 @@
 #!/bin/sh
 # apply_casacore_measures_tables.sh
-# Install downloaded casacore measures tables atomically. Written for jenkins@fs5 (DAS-4).
+# Install downloaded casacore measures tables atomically and verify which tables are in use.
+# Written for jenkins@fs5 (DAS-4).
 #
 # $Id$
 
-# Keep these in sync with get_casacore_measures_tables.sh
+# Keep these vars in sync with get_casacore_measures_tables.sh
 working_dir=$HOME/root/share/aips++  
 dir_prefix=measures_data-
 
 
 # find the latest
-cd $working_dir
-if [ ! -d $dir_prefix* ]; then
-  echo "No casacore measures directory recognized"
+if ! cd "$working_dir"; then exit 1; fi
+update_id=`ls -d $dir_prefix* 2> /dev/null | tail -n 1`
+if [ -z "$update_id" ]; then
+  echo "No casacore measures directory recognized. Running findmeastable to see if it has it elsewhere."
   findmeastable
   exit
 fi
-update_id=`ls -d $dir_prefix* | tail -n 1`
 
-# Switch data/ symlink to the latest data/ _atomically_ with a rename. Avoids race with a reader.
-ln -s $update_id/data data_${update_id}_tmp && mv -Tf data_${update_id}_tmp data && sync
+# If not already in use, switch data/ symlink to the latest data/ _atomically_ with a rename. Avoids race with a reader.
+if [ "`readlink data`" != "$update_id/data" ]; then
+  echo "Applying $update_id"
+  ln -s "$update_id/data" "data_${update_id}_tmp" && mv -Tf "data_${update_id}_tmp" data
+else
+  echo "No new table to apply."
+fi
 
-# See if casacore uses the latest tables.
-# rv gets the path in: Measures table Observatories found as /home/jenkins/root/share/aips++/data/geodetic/Observatories
+# See if casacore uses the latest tables by extracting the last token from findmeastable.
+# If ok, it prints: "Measures table Observatories found as /home/jenkins/root/share/aips++/data/geodetic/Observatories"
+if ! findmeastable > /dev/null; then exit 1; fi
 used_dir=`findmeastable | awk -F\  '{print $NF}'`
-status=$?
-echo "findmeastable reports using: $used_dir"
-if [ $status -ne 0 || "$used_dir" != "$working_dir/$update_id/data/geodetic/Observatories" ]; then
+if [ $? -ne 0 ]; then exit 1; fi
+used_path=`readlink -f "$used_dir/../../../data"`
+if [ $? -ne 0 ]; then exit 1; fi
+update_id_path="$working_dir/$update_id/data"
+if [ "$update_id_path" != "$used_path" ]; then
+  echo "It appears that the most recently retrieved measures data is not in use. Most recent is: '$update_id/data'."
+  # potential improvement: revert if applied (and if it used to work) (e.g. empty $update_id/)
   exit 1
 fi
+echo "All cool. The most recently retrieved measures data is (now) in use."
 
