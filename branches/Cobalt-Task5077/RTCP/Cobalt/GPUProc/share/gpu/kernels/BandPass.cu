268dd6e636bec1e3269b42b499f2553ff4afe1ef
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-23 15:10:54 +0000

    Task #5077: New bandpass kernel with first cleanup of the code plus add the test

---
+++ branches/Cobalt-Task5077/RTCP/Cobalt/GPUProc/share/gpu/kernels/BandPass.cu
@@ -0,0 +1,138 @@
+//# BandPass.cu
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+/** @file
+ * This file contains a CUDA implementation of the GPU kernel for the 
+ bandpass correction. It can also transpose the data (pol to dim 0).
+ *
+ * Usually, this kernel will be run after the polyphase filter kernel FIR.cl. In
+ * that case, the input data for this kernel is already in floating point format
+ * (@c NR_CHANNELS > 1). However, if this kernel is the first in row, then the
+ * input data is still in integer format (@c NR_CHANNELS == 1), and this kernel
+ * needs to do the integer-to-float conversion. If we do BANDPASS_CORRECTION
+ * (implies NR_CHANNELS > 1), then we typically also want to transpose the pol
+ * dim to the stride 1 dim (@c DO_TRANSPOSE).
+ *
+ * @attention The following pre-processor variables must be supplied when
+ * compiling this program. Please take the pre-conditions for these variables
+ * into account:
+ * - @c NR_CHANNELS: 1 or a multiple of 16
+ * - if @c NR_CHANNELS == 1 (input data is in integer format):
+ *   - @c NR_BITS_PER_SAMPLE: 8 or 16
+ *   - @c NR_SAMPLES_PER_SUBBAND: a multiple of 16
+ * - if @c NR_CHANNELS > 1 (input data is in floating point format):
+ *   - @c NR_SAMPLES_PER_CHANNEL: a multiple of 16
+ * - @c NR_SAPS: > 0
+ * - @c NR_POLARIZATIONS: 2
+ * - @c SUBBAND_BANDWIDTH: a multiple of @c NR_CHANNELS
+ *
+ * - @c DELAY_COMPENSATION: defined or not
+ * - @c BANDPASS_CORRECTION: defined or not
+ * - @c DO_TRANSPOSE: defined or not
+ */
+
+#include "gpu_math.cuh"
+
+#include "IntToFloat.cuh"
+
+#if defined DO_TRANSPOSE
+typedef  fcomplex (* OutputDataType)[NR_STATIONS][NR_CHANNELS][NR_SAMPLES_PER_CHANNEL][NR_POLARIZATIONS];
+#else
+typedef  fcomplex (* OutputDataType)[NR_STATIONS][NR_POLARIZATIONS][NR_CHANNELS][NR_SAMPLES_PER_CHANNEL];
+#endif
+
+//# TODO: Unify #dims in input type to 4: [NR_SAMPLES_PER_SUBBAND] -> [NR_SAMPLES_PER_CHANNEL][NR_CHANNELS] (see kernel test)
+//#       It is technically incorrect, but different dims for the same input type is a real pain to use/supply.
+//#       Also unify order of #chn, #sampl to [NR_SAMPLES_PER_CHANNEL][NR_CHANNELS]
+typedef  fcomplex (* InputDataType)[NR_STATIONS][NR_POLARIZATIONS][NR_CHANNELS][NR_SAMPLES_PER_CHANNEL][[NR_CHANNELS2]];
+typedef  const float (* BandPassFactorsType)[NR_CHANNELS];
+
+
+/**
+ * This kernel performs (up to) three operations on the input data:
+ * - Apply a fine delay by doing a per channel phase correction.
+ * - Apply a bandpass correction to compensate for the errors introduced by the
+ *   polyphase filter that produced the subbands. This error is deterministic,
+ *   hence it can be fully compensated for.
+ * - Transpose the data so that the time slices for each channel are placed
+ *   consecutively in memory.
+ *
+ * @param[out] correctedDataPtr    pointer to output data of ::OutputDataType,
+ *                                 a 3D array [station][channel][sample][complex]
+ *                                 of ::complex (2 complex polarizations)
+ * @param[in]  filteredDataPtr     pointer to input data; this can either be a
+ *                                 4D array [station][polarization][sample][channel][complex]
+ *                                 of ::fcomplex, or a 2D array [station][subband][complex]
+ *                                 of ::short_complex2 or ::char_complex2,
+ *                                 depending on the value of @c NR_CHANNELS
+ * @param[in]  bandPassFactorsPtr  pointer to bandpass correction data of
+ *                                 ::BandPassFactorsType, a 1D array [channel] of
+ *                                 float, containing bandpass correction factors
+ */
+
+extern "C" {
+ __global__ void correctBandPass( fcomplex * correctedDataPtr,
+                                  const fcomplex * filteredDataPtr,
+                                  const float * bandPassFactorsPtr)
+{
+  OutputDataType outputData = (OutputDataType) correctedDataPtr;
+  InputDataType inputData   = (InputDataType)  filteredDataPtr;
+
+  BandPassFactorsType bandPassFactors = (BandPassFactorsType) bandPassFactorsPtr;
+
+
+  unsigned major       = (blockIdx.x * blockDim.x + threadIdx.x) / 16;
+  unsigned minor       = (blockIdx.x * blockDim.x + threadIdx.x) % 16;
+  unsigned channelBase = (blockIdx.y * blockDim.y + threadIdx.y) * 16;
+
+  unsigned channel = channelBase + minor;
+
+  unsigned station =  blockIdx.z * blockDim.z + threadIdx.z;
+
+  float weight((*bandPassFactors)[channel]);
+
+  for (unsigned timeBase = 0; timeBase < NR_SAMPLES_PER_CHANNEL; timeBase += 16)
+  {
+    unsigned time = timeBase + major;
+
+    fcomplex sampleX = (*inputData)[station][0][time][channel];
+    fcomplex sampleY = (*inputData)[station][1][time][channel];
+
+    sampleX.x *= weight;
+    sampleX.y *= weight;
+    sampleY.x *= weight;
+    sampleY.y *= weight;
+
+// Support all variants of NR_CHANNELS and DO_TRANSPOSE for testing etc.
+// Transpose: data order is [station][channel][time][pol]
+    __shared__ fcomplex tmp[16][17][2]; // one too wide to avoid bank-conflicts on read
+
+    tmp[major][minor][0] = sampleX;
+    tmp[major][minor][1] = sampleY;
+    __syncthreads();
+    (*outputData)[station][channelBase + major][timeBase + minor][0] = tmp[minor][major][0];
+    (*outputData)[station][channelBase + major][timeBase + minor][1] = tmp[minor][major][1];
+    __syncthreads();
+
+
+  }
+}
+}
+
