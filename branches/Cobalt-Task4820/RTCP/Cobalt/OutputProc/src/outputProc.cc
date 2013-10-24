129edfe72a68c92308473b1e79a039a8e0cd3591
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-24 08:12:15 +0000

    Task #4820: Split main() into several functions for readability, and removed obs nr in logging, because that is superfluous in Cobalt

--- branches/Cobalt-Task4820/RTCP/Cobalt/OutputProc/src/outputProc.cc
+++ branches/Cobalt-Task4820/RTCP/Cobalt/OutputProc/src/outputProc.cc
@@ -66,6 +66,74 @@ Exception::TerminateHandler t(Exception::terminate);
 
 char stdoutbuf[1024], stderrbuf[1024];
 
+SubbandWriter *startWriter(const Parset &parset, OutputType outputType, unsigned streamNr)
+{
+  unsigned writerNr = 0;
+
+  // lookup PVSS writer number for this file
+  Observation obs(&parset, false, 64); // FIXME: assume 64 psets, because Observation still deals with BG/P
+
+  for (unsigned i = 0; i < obs.streamsToStorage.size(); i++) 
+  {
+    Observation::StreamToStorage &s = obs.streamsToStorage[i];
+
+    if (s.dataProductNr == static_cast<unsigned>(outputType) && s.streamNr == streamNr) 
+    {
+      writerNr = s.writerNr;
+      break;
+    }
+  }
+
+  string sbLogPrefix = str(boost::format("[obs %u type %u stream %3u writer %3u] ") % parset.observationID() % outputType % streamNr % writerNr);
+
+  try 
+  {
+    return new SubbandWriter(parset, outputType, streamNr, false, sbLogPrefix);
+  } 
+  catch (Exception &ex) 
+  {
+    LOG_WARN_STR(sbLogPrefix << "Could not create writer: " << ex);
+  } 
+  catch (exception &ex) 
+  {
+    LOG_WARN_STR(sbLogPrefix << "Could not create writer: " << ex.what());
+  }
+
+  return NULL;
+}
+
+void readFinalMetaData( Stream &controlStream, vector< SmartPtr<SubbandWriter> > &subbandWriters )
+{
+  // Add final meta data (broken tile information, etc)
+  // that is obtained after the end of an observation.
+  LOG_INFO_STR("Waiting for final meta data");
+  FinalMetaData finalMetaData;
+  finalMetaData.read(controlStream);
+
+  LOG_INFO_STR("Processing final meta data");
+  for (size_t i = 0; i < subbandWriters.size(); ++i)
+    try {
+      subbandWriters[i]->augment(finalMetaData);
+    } catch (Exception &ex) {
+      LOG_WARN_STR("Could not add final meta data: " << ex);
+    }
+}
+
+void writeFeedbackLTA( Stream &controlStream, vector< SmartPtr<SubbandWriter> > &subbandWriters )
+{
+  LOG_INFO_STR("Retrieving LTA feedback");
+  Parset feedbackLTA;
+  for (size_t i = 0; i < subbandWriters.size(); ++i)
+    try {
+      feedbackLTA.adoptCollection(subbandWriters[i]->feedbackLTA());
+    } catch (Exception &ex) {
+      LOG_WARN_STR("Could not obtain feedback for LTA: " << ex);
+    }
+
+  LOG_INFO_STR("Forwarding LTA feedback");
+  feedbackLTA.write(&controlStream);
+}
+
 int main(int argc, char *argv[])
 {
 #if defined HAVE_LOG4CPLUS
@@ -75,8 +143,6 @@ int main(int argc, char *argv[])
 #endif
 
   CasaLogSink::attach();
-
-  string obsLogPrefix = "[obs unknown] ";
 
   try {
     if (argc != 4)
@@ -91,6 +157,9 @@ int main(int argc, char *argv[])
     unsigned myRank = boost::lexical_cast<unsigned>(argv[2]);
     bool isBigEndian = boost::lexical_cast<bool>(argv[3]);
 
+    // We only support little-endian data anymore.
+    ASSERT(isBigEndian == false);
+
     setIOpriority();
     setRTpriority();
     lockInMemory();
@@ -102,13 +171,10 @@ int main(int argc, char *argv[])
     PortBroker::ServerStream controlStream(resource);
 
     Parset parset(&controlStream);
-    Observation obs(&parset, false, 64); // FIXME: assume 64 psets, because Observation still deals with BG/P
 
     const vector<string> &hostnames = parset.settings.outputProcHosts;
     ASSERT(myRank < hostnames.size());
     string myHostName = hostnames[myRank];
-
-    obsLogPrefix = str(boost::format("[obs %u] ") % parset.observationID());
 
     {
       // make sure "parset" stays in scope for the lifetime of the SubbandWriters
@@ -119,80 +185,33 @@ int main(int argc, char *argv[])
       {
         for (unsigned streamNr = 0; streamNr < parset.nrStreams(outputType); streamNr++) 
         {
-          if (parset.getHostName(outputType, streamNr) == myHostName) 
-          {
-            unsigned writerNr = 0;
+          if (parset.getHostName(outputType, streamNr) != myHostName) 
+            continue;
 
-            // lookup PVSS writer number for this file
-            for (unsigned i = 0; i < obs.streamsToStorage.size(); i++) 
-            {
-              Observation::StreamToStorage &s = obs.streamsToStorage[i];
+          SubbandWriter *writer = startWriter(parset, outputType, streamNr);
+          if (writer == NULL)
+            continue;
 
-              if (s.dataProductNr == static_cast<unsigned>(outputType) && s.streamNr == streamNr) 
-              {
-                writerNr = s.writerNr;
-                break;
-              }
-            }
-
-            string sbLogPrefix = str(boost::format("[obs %u type %u stream %3u writer %3u] ") % parset.observationID() % outputType % streamNr % writerNr);
-
-            try 
-            {
-              subbandWriters.push_back(
-                  new SubbandWriter(parset, outputType,
-                                    streamNr, isBigEndian, sbLogPrefix));
-            } 
-            catch (Exception &ex) 
-            {
-              LOG_WARN_STR(sbLogPrefix << "Could not create writer: " << ex);
-            } 
-            catch (exception &ex) 
-            {
-              LOG_WARN_STR(sbLogPrefix << "Could not create writer: " << ex.what());
-            }
-          }
+          subbandWriters.push_back(writer);
         }
       }
 
       /*
        * FINAL META DATA
        */
-      // Add final meta data (broken tile information, etc)
-      // that is obtained after the end of an observation.
-      LOG_INFO_STR(obsLogPrefix << "Waiting for final meta data");
-      FinalMetaData finalMetaData;
-      finalMetaData.read(controlStream);
-
-      LOG_INFO_STR(obsLogPrefix << "Processing final meta data");
-      for (size_t i = 0; i < subbandWriters.size(); ++i)
-        try {
-          subbandWriters[i]->augment(finalMetaData);
-        } catch (Exception &ex) {
-          LOG_WARN_STR(obsLogPrefix << "Could not add final meta data: " << ex);
-        }
+      readFinalMetaData(controlStream, subbandWriters);
 
       /*
        * LTA FEEDBACK
        */
-      LOG_INFO_STR(obsLogPrefix << "Retrieving LTA feedback");
-      Parset feedbackLTA;
-      for (size_t i = 0; i < subbandWriters.size(); ++i)
-        try {
-          feedbackLTA.adoptCollection(subbandWriters[i]->feedbackLTA());
-        } catch (Exception &ex) {
-          LOG_WARN_STR(obsLogPrefix << "Could not obtain feedback for LTA: " << ex);
-        }
-
-      LOG_INFO_STR(obsLogPrefix << "Forwarding LTA feedback");
-      feedbackLTA.write(&controlStream);
+      writeFeedbackLTA(controlStream, subbandWriters);
     }
   } catch (Exception &ex) {
-    LOG_FATAL_STR(obsLogPrefix << "Caught Exception: " << ex);
+    LOG_FATAL_STR("Caught Exception: " << ex);
     return 1;
   }
 
-  LOG_INFO_STR(obsLogPrefix << "Program end");
+  LOG_INFO_STR("Program end");
   return 0;
 }
 
