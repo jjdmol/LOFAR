473e28979580b0187e28166bb8be1d6a0511e9a8
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-10-30 16:58:48 +0000

    Task #5081: First version of the transpose kernel for the incoherent Stokes. This kernel has a naive, poorly performing, implementation, but should be functionally correct.

---
+++ branches/Cobalt-Task5081/RTCP/Cobalt/GPUProc/share/gpu/kernels/IncoherentStokesTranspose.cu
@@ -0,0 +1,94 @@
+//# IncoherentStokesTranspose.cu
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#if !(NR_CHANNELS >= 1)
+#error Precondition violated: NR_CHANNELS >= 1
+#endif
+
+#if !(NR_POLARIZATIONS == 2)
+#error Precondition violated: NR_POLARIZATIONS == 2
+#endif
+
+#if !(NR_SAMPLES_PER_CHANNEL > 0 && NR_SAMPLES_PER_CHANNEL % 16 == 0)
+#error Precondition violated: NR_SAMPLES_PER_CHANNEL > 0 && NR_SAMPLES_PER_CHANNEL % 16 == 0
+#endif
+
+#if !(NR_STATIONS >= 1)
+#error Precondition violated: NR_STATIONS >= 1
+#endif
+
+// 3-D input data array of band-pass corrected data. Note that, actually, the
+// data is 4-D (<tt>[station][channel][time][pol]</tt>), but the 4th dimension
+// has been squashed into a single float4 (i.e., two complex polarizations).
+typedef float4 (*InputDataType)[NR_STATIONS][NR_CHANNELS][NR_SAMPLES_PER_CHANNEL];
+
+// 4-D output data array of band-pass corrected data that can be fed into an
+// inverse FFT (<tt>[station][pol][time][channel]</tt>).
+typedef float2 (*OutputDataType)[NR_STATIONS][NR_POLARIZATIONS][NR_SAMPLES_PER_CHANNEL][NR_CHANNELS];
+
+
+// Performs data transposition from the output of the beamformer kernel to a
+// data order suitable for an inverse FFT. Parallelisation is performed over the
+// TABs and number of samples (time).
+//
+// We have 4 dimensions, but CUDA thread blocks can be up to three. Mangle the
+// TAB and sample dimension in to dim 0 (x).
+//
+// The kernel needs to determine for each thread whether to read and separately
+// whether to write back a sample, because the number of TABs may not divide by
+// the 16x16 thread arrangement (even though we have a 1D thread block).
+//
+// \param[out] OutputDataType 4D output array of samples. For each TAB and pol,
+// a spectrum per time step of complex floats.
+// \param[in] ComplexVoltagesType 3D input array of samples (last dim (pol) is
+// implicit). For each channel, the TABs per time step of two complex floats.
+//
+// Pre-processor input symbols (some are tied to the execution configuration)
+// Symbol                  | Valid Values            | Description
+// ----------------------- | ----------------------- | -----------
+// NR_CHANNELS             | >= 1                    | number of frequency channels per subband
+// NR_POLARIZATIONS        | 2                       | number of polarizations
+// NR_SAMPLES_PER_CHANNEL  | multiple of 16 and > 0  | number of input samples per channel
+// NR_STATIONS             | >= 1                    | number of Tied Array Beams to create
+//
+// Execution configuration:
+//
+// - LocalWorkSize = 1 dimensional; (256, 1, 1) is in use. 
+//   Multiples of (32, 1, 1) may work too.
+// - GlobalWorkSize = 3 dimensional:
+//   + inner dim (x): always 1 block
+//   + middle dim (y): 16 TABs can be processed in a block.
+//     Number of blocks required, rounded-up. eg for 17 tabs we need 2 blocks
+//   + outer dim (z): 16 samples per channel can be processed in a block.
+//     Number of blocks required (fits exactly). 32 channels is 2 blocks
+extern "C"
+__global__ void transpose(OutputDataType output,
+                          const InputDataType input)
+{
+  unsigned time = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned channel = blockIdx.y * blockDim.y + threadIdx.y;
+
+  // Naive approach: directly reading from and writing to global memory.
+  for (int station = 0; station < NR_STATIONS; station++) {
+    float4 sample = (*input)[station][channel][time];
+    (*output)[station][0][time][channel] = make_float2(sample.x, sample.y);
+    (*output)[station][1][time][channel] = make_float2(sample.z, sample.w);
+  }
+}
---
+++ branches/Cobalt-Task5081/RTCP/Cobalt/GPUProc/src/cuda/Kernels/IncoherentStokesTransposeKernel.cc
@@ -0,0 +1,110 @@
+//# IncoherentStokesTransposeKernel.cc
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#include <lofar_config.h>
+
+#include "IncoherentStokesTransposeKernel.h"
+
+#include <GPUProc/global_defines.h>
+#include <Common/lofar_complex.h>
+
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+    using boost::format;
+    using boost::lexical_cast;
+
+    string IncoherentStokesTransposeKernel::theirSourceFile = 
+      "IncoherentStokesTranspose.cu";
+    string IncoherentStokesTransposeKernel::theirFunction = 
+      "transpose";
+
+    IncoherentStokesTransposeKernel::Parameters::Parameters(const Parset& ps) :
+      Kernel::Parameters(ps)
+    {
+      dumpBuffers = 
+        ps.getBool("Cobalt.Kernels.IncoherentStokesTransposeKernel.dumpOutput",
+                   false);
+      dumpFilePattern = 
+        str(format("L%d_SB%%03d_BL%%03d_IncoherentStokesTransposeKernel.dat") % 
+            ps.settings.observationID);
+    }
+
+    IncoherentStokesTransposeKernel::
+    IncoherentStokesTransposeKernel(const gpu::Stream& stream,
+                                    const gpu::Module& module,
+                                    const Buffers& buffers,
+                                    const Parameters& params) :
+      Kernel(stream, gpu::Function(module, theirFunction), buffers, params)
+    {
+      setArg(0, buffers.output);
+      setArg(1, buffers.input);
+
+      unsigned nrBlocks = params.nrSamplesPerChannel;
+      unsigned nrPasses = 
+        (nrBlocks + maxThreadsPerBlock - 1) / maxThreadsPerBlock;
+      unsigned nrThreads = 
+        (nrBlocks + nrPasses - 1) / nrPasses;
+
+      LOG_DEBUG_STR("nrBlocks = " << nrBlocks);
+      LOG_DEBUG_STR("nrPasses = " << nrPasses);
+      LOG_DEBUG_STR("nrThreads = " << nrThreads);
+
+      setEnqueueWorkSizes(
+        // gpu::Grid(nrTimesPerPass * nrPasses, params.nrChannelsPerSubband),
+        // gpu::Block(nrTimesPerPass, 1));
+        gpu::Grid(nrPasses * nrThreads, params.nrChannelsPerSubband),
+        gpu::Block(nrThreads, 1));
+
+    }
+
+    //--------  Template specializations for KernelFactory  --------//
+
+    template<> size_t
+    KernelFactory<IncoherentStokesTransposeKernel>::
+    bufferSize(BufferType bufferType) const
+    {
+      switch (bufferType) {
+      case IncoherentStokesTransposeKernel::INPUT_DATA:
+      case IncoherentStokesTransposeKernel::OUTPUT_DATA:
+        return 
+          itsParameters.nrStations * itsParameters.nrChannelsPerSubband *
+          itsParameters.nrSamplesPerChannel * NR_POLARIZATIONS *
+          sizeof(std::complex<float>);
+      default:
+        THROW(GPUProcException, "Invalid bufferType (" << bufferType << ")");
+      }
+    }
+
+    template<> CompileDefinitions
+    KernelFactory<IncoherentStokesTransposeKernel>::compileDefinitions() const
+    {
+      CompileDefinitions defs =
+        KernelFactoryBase::compileDefinitions(itsParameters);
+      return defs;
+    }
+
+  }
+}
+
