e8f59760e7e66adbaadf50e66087a49d7b3bb044
donker <donker@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-11-05 14:21:25 +0000

    Task #5119: Subband Data Output support in RSPriver for Aartfaac

---
+++ branches/MAC-RSPAartfaacSupport-Task5119/LCS/Common/test/Coordinates.cc
@@ -0,0 +1,75 @@
+//# Coordinates.cc: Implementation of Coordinates used by ObjectFactory test
+//# program.
+//#
+//# Copyright (C) 2006
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+//# Always #include <lofar_config.h> first!
+#include <lofar_config.h>
+
+//# This code can only be compiled if Boost is available.
+#ifdef HAVE_BOOST
+
+//# Includes
+#include "Coordinates.h"
+#include <Common/lofar_iostream.h>
+
+namespace LOFAR
+{
+  // Use an anonymous namespace. This ensures that variables declared inside
+  // it get there own private storage area and are only visible in this
+  // compilation unit.
+  namespace
+  {
+    // Forces the classes to be registered during static initialization.
+    // We don't care about the resturn value; maybe we should ;-)
+    bool dummy = 
+      CoordinateFactory::instance().registerClass<Cartesian>("Cartesian")
+      && CoordinateFactory::instance().registerClass<Cylindrical>("Cylindrical")
+      && CoordinateFactory::instance().registerClass<Spherical>("Spherical"); 
+  }
+
+  void Cartesian::print(ostream& os) const
+  {
+    os << "Cartesian: (x, y, z) = (" 
+       << itsX << ", " << itsY << ", " << itsZ << ")";
+  }
+
+  void Cylindrical::print(ostream& os) const
+  {
+    os << "Cylindrical: (r, theta, z) = ("
+       << itsR << ", " << itsTheta << ", " << itsZ << ")";
+  }
+
+  void Spherical::print(ostream& os) const
+  {
+    os << "Spherical: (r, theta, phi) = ("
+       << itsR << ", " << itsTheta << ", " << itsPhi << ")";
+  }
+
+  ostream& operator<<(ostream& os, const Coordinate& s)
+  {
+    s.print(os);
+    return os;
+  }
+
+}
+
+#endif
