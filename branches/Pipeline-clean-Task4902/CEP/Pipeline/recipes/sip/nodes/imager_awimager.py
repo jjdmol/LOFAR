e988d3cf4e8004a8149f9c75399883e2da638f98
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-11-15 13:02:04 +0000

    Task #4902: rename of parameters

--- branches/Pipeline-clean-Task4902/CEP/Pipeline/recipes/sip/nodes/imager_awimager.py
+++ branches/Pipeline-clean-Task4902/CEP/Pipeline/recipes/sip/nodes/imager_awimager.py
@@ -259,7 +259,7 @@ class imager_awimager(LOFARnodeTCP):
         # keep the wmax and w_proj_planes
         if (not autogenerate_parameters and not specify_fov):
             npix = parset_object.getString('npix')
-            cell_size = parset_object.getString('cellsize')
+            cell_size_formatted = parset_object.getString('cellsize')
         else:
             cell_size_formatted = str(
                         int(round(cell_size))) + 'arcsec'
