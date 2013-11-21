1b7b817de4b73371c4b52d76a58b69a2872f9d71
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-11-21 11:13:30 +0000

    Task #5205; Files are written to temp directory and adapted on the spot. Working space will be cleaned at start of script.

--- branches/Pipeline-Task5205/CEP/Pipeline/test/regression_tests/regression_test_runner.sh
+++ branches/Pipeline-Task5205/CEP/Pipeline/test/regression_tests/regression_test_runner.sh
@@ -16,6 +16,18 @@ if [[ $# = 0 || $1 = "--help" ]]; then
   exit 1
 fi
 
+# test if we started in the correct directory
+# we need to be able to grab and change installed files for full functionality
+if [ -f lofarinit.sh ]
+then
+   :
+else
+    echo "*** Error ***"
+    echo $"Could not find lofarinit at current working directory: $WORKSPACE"
+    echo "script should be started from the base dir, eg: ~/build/gnu_debug "
+    exit 1
+fi
+
 # *******************************************************
 # 1) Check/set environment and set global variables
 
@@ -25,19 +37,8 @@ shopt -s expand_aliases
 
 # Some global named variables
 PIPELINE=$1                                                             # First command line argument
-WORKING_DIR=$"/data/scratch/$USER/regression_test_runner_$PIPELINE"     # Default working space
+WORKING_DIR=$"/data/scratch/$USER/regression_test_runner/$PIPELINE"     # Default working space
 WORKSPACE=$PWD                                                          # Directory script is started from
-
-# test if we are in the correct directory
-# we need to be able to grab and change installed files for full functionality
-if [ -f lofarinit.sh ]
-then
-   :
-else
-    echo $"Could not find lofarinit at current working directory: $WORKSPACE"
-    echo "script should be started from the base dir, eg: ~/build/gnu_debug "
-    exit 1
-fi
 
 # create the var run directory
 mkdir -p $"$WORKSPACE/installed/var/run/pipeline"
@@ -45,12 +46,12 @@ mkdir -p $"$WORKSPACE/installed/var/run/pipeline"
 # set up environment
 use Lofar
 use Pythonlibs
-. lofarinit.sh
+. $"$WORKSPACE/lofarinit.sh"
 
 # *****************************************************
 # 1) Clear old data:
 # Clear var run directory: remove state, map and logfiles
-rm $"$WORKSPACE/installed/var/run/pipeline/*" -rf
+rm $"$WORKSPACE/installed/var/run/pipeline/"* -rf
 
 # Assure working directory exists
 ssh lce068 $"mkdir $WORKING_DIR -p" 
@@ -67,18 +68,20 @@ ssh lce071 $"rm $WORKING_DIR/* -rf"
 ssh lce072 $"rm $WORKING_DIR/* -rf" 
 
 # ******************************************************
-# 2) 
-
+# 2) prepare the config file to run in a pipeline type depending but static location 
 # copy config file to working dir:
-cp installed/share/pipeline/pipeline.cfg  $"$WORKING_DIR/pipeline.cfg"
+cp $"$WORKSPACE/installed/share/pipeline/pipeline.cfg"  $"$WORKING_DIR/pipeline.cfg"
 
 # insert the cluserdesc for test cluster (default install is for cep2)
 sed -i 's/cep2.clusterdesc/cep1_test.clusterdesc/g' $"$WORKING_DIR/pipeline.cfg"
+# specify the new working directory
+sed -i $"s|working_directory = /data/scratch/$USER|working_directory = $WORKING_DIR|g" $"$WORKING_DIR/pipeline.cfg"
+
 
 # copy parset to working dir (to allow output of meta information):
-cp /data/lofar/testdata/CEP/Pipeline/calibrator_pipeline/Observation64405 $"$WORKING_DIR/calibrator.parset"
+cp /data/lofar/testdata/regression_test_runner/msss_calibrator_pipeline/msss_calibrator_pipeline.parset $"$WORKING_DIR/msss_calibrator_pipeline.parset"
 
-python installed/bin/msss_calibrator_pipeline.py $"$WORKING_DIR/calibrator.parset" -c $"$WORKING_DIR/pipeline.cfg" -d
+#python $"$WORKSPACE/installed/bin/msss_calibrator_pipeline.py" $"$WORKING_DIR/msss_calibrator_pipeline.parset" -c $"$WORKING_DIR/pipeline.cfg" -d
 
 
 
