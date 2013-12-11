95b2afd3578b4c422d1b8085fa2dbf8e77e910a7
zwieten <zwieten@b3281354-443f-0410-9bd6-bf5212d75c11>
2013-12-11 09:53:30 +0000

    Task #5098: Added separate Phasors options to Gain and DirectionalGain, e.g. Model.Gain.Phasors = T. Model.Phasors.Enable can still be specified. Its value will be used as the default value for the separate options. If the separate options are specified, their value takes precedence. This ensures backward compatibility.

--- trunk/CEP/Calibration/BBSKernel/include/BBSKernel/MeasurementExprLOFARUtil.h
+++ trunk/CEP/Calibration/BBSKernel/include/BBSKernel/MeasurementExprLOFARUtil.h
@@ -82,7 +82,7 @@ makeClockExpr(Scope &scope, const Station::ConstPtr &station,
 Expr<JonesMatrix>::Ptr
 makeGainExpr(Scope &scope,
     const Station::ConstPtr &station,
-    bool phasors);
+    const GainConfig &config);
 
 Expr<JonesMatrix>::Ptr
 makeTECExpr(Scope &scope,
@@ -101,7 +101,7 @@ Expr<JonesMatrix>::Ptr
 makeDirectionalGainExpr(Scope &scope,
     const Station::ConstPtr &station,
     const string &patch,
-    bool phasors);
+    const DirectionalGainConfig &config);
 
 Expr<JonesMatrix>::Ptr
 makeElevationCutExpr(const Expr<Vector<2> >::Ptr &exprAzEl,
