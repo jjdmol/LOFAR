e710f6bc138aebd15b42f1e8aeb88a42f9d92ca8
Marcel Loose <gmloose@users.noreply.github.com>
2014-01-16 13:45:29 +0000

    Task #5395: Now I'm making a mess of things by committing from the wrong working copy. Blast.

--- LCS/Common/test/tSaxpy.cc
+++ LCS/Common/test/tSaxpy.cc
@@ -82,13 +82,13 @@ int main (int argc, const char* argv[])
   cout<< "float,float " << n << endl;
   saxpy<float,float> (n, 0);
   cout<< "__complex__ float,float " << n << endl;
-  saxpy<__complex__ float,float> (n, 0.);
+  saxpy<__complex__ float,float> (n, 0.+0.i);
   cout<< "std::complex<float>,float " << n << endl;
   saxpy<std::complex<float>,float> (n, std::complex<float>());
   cout<< "double,double " << n << endl;
   saxpy<double,double> (n, 0);
   cout<< "__complex__ double,double " << n << endl;
-  saxpy<__complex__ double,double> (n, 0.);
+  saxpy<__complex__ double,double> (n, 0.+0.i);
   cout<< "std::complex<double>,double " << n << endl;
   saxpy<std::complex<double>,double> (n, std::complex<double>());
   cout<< "std::complex<double>,std::complex<double> " << n << endl;
