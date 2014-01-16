6818a0b5b12d0b152ec85f6720aba2d32424982a
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-01-16 14:59:42 +0000

    Task #1254: Removed variants file for dop282; it's no longer needed. Added a few missing #include's.

--- trunk/RTCP/CNProc/src/InversePPF.h
+++ trunk/RTCP/CNProc/src/InversePPF.h
@@ -67,6 +67,7 @@
 #include <Interface/InverseFilteredData.h>
 #include <Interface/Align.h>
 #include <Interface/AlignedStdAllocator.h>
+#include <boost/noncopyable.hpp>
 #include <vector>
 #include <FilterBank.h>
 #include <FIR.h>
