d3964a3331351683e6883b22a8439dce560c9f1b
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-02-05 08:26:37 +0000

    Task #5490: create task branch

---
+++ branches/Cobalt-Task5490/MAC/Deployment/data/Coordinates/vectors-and-matrices/DE605/de605-lba-solution.lisp
@@ -0,0 +1,13 @@
+(discarded-points
+    NIL)
+(reduced-chi-squared 0.00006292228122027776)
+(normal-vector               (0.6289823 0.0494684 0.7758441))
+(normal-vector-uncertainties (0.0000064 0.0000088 0.0000052))
+(normal-vector-direction-uncertainties-arcsec :max  1.82 :rms  1.44 :avg  1.40)
+(reference-point :etrs (4005718.447 451028.044  4926424.103)
+                 :station-pqr (0.0 0.0 0.0))
+(station-pqr-to-etrs-matrix
+    #2A((-0.10322260722367654 -0.7705364038207051 0.6289823079745582)
+        (0.9944515889182843 -0.09283812525238423 0.049468371688609306)
+        (0.020276357065468757 0.6305987098675971 0.7758441444371902)))
+
