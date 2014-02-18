6de306b34a6a18d0174f2ee448691aa6bb62afca
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-02-18 10:31:15 +0000

    Task #5538: Added script to generate startTime/stopTime parset keys based on the current time

---
+++ trunk/RTCP/Cobalt/GPUProc/src/scripts/startstopkeys.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Generates parset keys for the start and stop times of an observation,
+# based on the current system time.
+#
+
+STARTDELAY=60
+RUNTIME=120
+
+function usage() {
+  echo "Usage: $0 [-s startdelay] [-r runtime]"
+  echo "       $0 -h"
+  echo " "
+  echo "-h              print this help message"
+  echo "-s startdelay   start in 'startdelay' seconds [$STARTDELAY]"
+  echo "-r runtime      stop after 'runtime' seconds [$RUNTIME]"
+
+  exit 1;
+}
+
+while getopts ":s:rh" opt; do
+  case $opt in
+    s) STARTDELAY=$OPTARG
+       ;;
+    r) RUNTIME=$OPTARG
+       ;;
+    h) usage;
+       ;;
+
+    \?) echo "Invalid option: -$OPTARG" >&2
+        exit 1;
+        ;;
+
+    :)  echo "Option requires an argument: -$OPTARG" >&2
+        exit 1;
+        ;;
+  esac
+done
+
+# Calculate start and stop times in seconds since 1970 UTC
+NOW=`date -u +%s`
+START=$(($NOW + $STARTDELAY))
+STOP=$((START + $RUNTIME))
+
+# The date format used in the parset
+FORMAT="%F %T"
+
+# Echo the resulting parset keys
+echo "Observation.startTime = `date -u -d @$START +"$FORMAT"`"
+echo "Observation.stopTime  = `date -u -d @$STOP  +"$FORMAT"`"
+
