91f93e0a0ff5a196b9778d8e07ded13bb949e763
dijkema <dijkema@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-02-21 15:50:03 +0000

    Task #5553: changed testing code

--- branches/LOFAR-BeamNCP-Task5553/CEP/Calibration/pystationresponse/test/tStationBeamNCP.py
+++ branches/LOFAR-BeamNCP-Task5553/CEP/Calibration/pystationresponse/test/tStationBeamNCP.py
@@ -1,23 +1,24 @@
-try:
-    import lofar.stationresponse as st
-    import pyrap.tables as tab
-    import numpy as np
-    import sys
+"Test the Station Beam at the NCP. Rationale: when pointing at the NCP all stations should have (almost) the same beam"
 
-    MSNAME='tStationBeamNCP_tmp.MS'
+import sys
 
-    myt=tab.table(MSNAME,ack=False)
-    mys=st.stationresponse( msname=MSNAME, inverse=False, useElementResponse=True, useArrayFactor=False, useChanFreq=False)
-    times=myt.getcol('TIME')
-    mys.setDirection(0.01,0.5*np.pi)
+import lofar.stationresponse as st
+import pyrap.tables as tab
+import numpy as np
 
-    a=[mys.evaluateStation(time=times[0],station=st) for st in range(20)]
+MSNAME='tStationBeamNCP.in.MS'
 
-    for a1 in a:
-        for a2 in a:
-            if np.linalg.norm(a1-a2)>1.e-6:
-                sys.exit(1)
+myt=tab.table(MSNAME,ack=False)
+mys=st.stationresponse( msname=MSNAME, inverse=False, useElementResponse=True, useArrayFactor=False, useChanFreq=False)
+times=myt.getcol('TIME')
+mys.setDirection(0.01,0.5*np.pi)
 
-    sys.exit(0)
-except:
-    sys.exit(3)
+a=[mys.evaluateStation(time=times[0],station=st) for st in range(20)]
+
+for a1 in a:
+    for a2 in a:
+	if np.linalg.norm(a1-a2)>1.e-3:
+            print "a1=",a1,"\na2=",a2,"\nnorm=",np.linalg.norm(a1-a2)
+	    sys.exit(1)
+
+sys.exit(0)
--- branches/LOFAR-BeamNCP-Task5553/CEP/Calibration/pystationresponse/test/tStationBeamNCP.sh
+++ branches/LOFAR-BeamNCP-Task5553/CEP/Calibration/pystationresponse/test/tStationBeamNCP.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./runctest.sh tApplyCal
+./runctest.sh tStationBeamNCP
