7ce84e9fde5296f896304c2e3949b677ed75db7a
Tammo Jan Dijkema <tammojan@users.noreply.github.com>
2014-03-04 01:07:05 +0000

    Task #5570: re-added current working directory to search path

--- LCS/Common/include/Common/FileLocator.h
+++ LCS/Common/include/Common/FileLocator.h
@@ -44,7 +44,7 @@ namespace LOFAR {
 class FileLocator
 {
 public:
-	#define		BASE_SEARCH_DIR		"$LOFARROOT:/opt/lofar"
+	#define		BASE_SEARCH_DIR		".:$LOFARROOT:/opt/lofar"
 
 	typedef list<string>::iterator		iterator;
 
