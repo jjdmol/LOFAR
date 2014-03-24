23c593ae2f20e38d9277c5afeff8578848d720bc
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-03-24 09:49:28 +0000

    Task #5601: Tagged current version of Cobalt release 1.3 branch as 1.3.0

---
+++ tags/Cobalt-Release-1_3_0/RTCP/Cobalt/GPUProc/src/Station/StationNodeAllocation.cc
@@ -0,0 +1,134 @@
+#include <lofar_config.h>
+#include "StationNodeAllocation.h"
+
+#ifdef HAVE_MPI
+#include <InputProc/Transpose/MPIUtil.h>
+#endif
+#include <boost/format.hpp>
+#include <sstream>
+
+#include <Common/LofarLogger.h>
+#include <Stream/NullStream.h>
+#include <CoInterface/Stream.h>
+
+#include <InputProc/RSPTimeStamp.h>
+#include <InputProc/Buffer/BoardMode.h>
+#include <InputProc/Station/PacketFactory.h>
+#include <InputProc/Station/PacketStream.h>
+
+using namespace std;
+using boost::format;
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+
+StationNodeAllocation::StationNodeAllocation( const StationID &stationID, const Parset &parset )
+:
+  stationID(stationID),
+  parset(parset),
+  stationIdx(parset.settings.stationIndex(stationID.name()))
+{
+  ASSERTSTR(stationIdx >= 0, "Station not found in observation: " << stationID.name());
+}
+
+bool StationNodeAllocation::receivedHere() const
+{
+  int rank = MPI_Rank();
+  int nrHosts = MPI_Size();
+
+  int stationRank = receiverRank();
+
+  if (stationRank == -1) {
+    // allocate stations not mentioned in Cobalt.Hardware round-robin
+    stationRank = parset.settings.stationIndex(stationID.name()) % nrHosts;
+
+    if (stationRank == rank)
+      LOG_WARN_STR("Receiving station " << stationID << " due to round-robin allocation across nodes.");
+  }
+
+  return stationRank == rank;
+}
+
+int StationNodeAllocation::receiverRank() const
+{
+  /*
+   * The parset key
+   *
+   *   Cobalt.Hardware.Node[rank].stations
+   *
+   * contains the station names that will be received
+   * by the specified MPI rank.
+   */
+
+  const string receiver = parset.settings.stations[stationIdx].receiver;
+
+  for (size_t rank = 0; rank < parset.settings.nodes.size(); ++rank) {
+    if (parset.settings.nodes[rank].name == receiver)
+      return rank;
+  }
+
+  return -1;
+}
+
+std::vector< SmartPtr<Stream> > StationNodeAllocation::inputStreams() const
+{
+  const string logPrefix = str(format("[station %s] ") % stationID.name());
+
+  vector<string> inputStreamDescs = parset.settings.stations[stationIdx].inputStreams;
+
+  vector< SmartPtr<Stream> > inputStreams(inputStreamDescs.size());
+
+  // Log all input descriptions
+  stringstream inputDescription;
+
+  for (size_t board = 0; board < inputStreamDescs.size(); ++board) {
+    const string &desc = inputStreamDescs[board];
+
+    if (board > 0)
+      inputDescription << ", ";
+    inputDescription << desc;
+  }
+
+  LOG_INFO_STR(logPrefix << "Input streams: " << inputDescription.str());
+
+  // Connect to specified input stream
+  for (size_t board = 0; board < inputStreamDescs.size(); ++board) {
+    const string &desc = inputStreamDescs[board];
+
+    LOG_DEBUG_STR(logPrefix << "Connecting input stream for board " << board << ": " << desc);
+
+    // Sanity checks
+    if (parset.settings.realTime) {
+      ASSERTSTR(desc.find("udp:") == 0, logPrefix << "Real-time observations should read input from UDP, not " << desc);
+    } else {
+      ASSERTSTR(desc.find("udp:") != 0, logPrefix << "Non-real-time observations should NOT read input from UDP, got " << desc);
+    }
+
+    if (desc == "factory:") {
+      const TimeStamp from(parset.startTime() * parset.subbandBandwidth(), parset.clockSpeed());
+      const TimeStamp to(parset.stopTime() * parset.subbandBandwidth(), parset.clockSpeed());
+
+      const struct BoardMode mode(parset.settings.nrBitsPerSample, parset.settings.clockMHz);
+      PacketFactory factory(mode);
+
+      inputStreams[board] = new PacketStream(factory, from, to, board);
+    } else {
+      try {
+        inputStreams[board] = createStream(desc, true);
+      } catch(Exception &ex) {
+        LOG_ERROR_STR(logPrefix << "Caught exception: " << ex.what());
+
+        inputStreams[board] = new NullStream;
+      }
+    }
+  }
+
+  ASSERTSTR(inputStreams.size() > 0, logPrefix << "No input streams");
+
+  return inputStreams;
+}
+
+  } // namespace Cobalt
+} // namespace LOFAR
---
+++ tags/Cobalt-Release-1_3_0/RTCP/Cobalt/GPUProc/src/cuda/Kernels/FFT_Plan.cc
@@ -0,0 +1,80 @@
+//# FFT_Plan.cc
+//# Copyright (C) 2012-2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#include <lofar_config.h>
+
+#include "FFT_Plan.h"
+#include <GPUProc/gpu_wrapper.h>
+
+namespace LOFAR
+{
+  namespace Cobalt
+  {
+    FFT_Plan::FFT_Plan(gpu::Context &context, unsigned fftSize, unsigned nrFFTs)
+      :
+      context(context)
+    {
+      // prevent cufftPlan1d from crashing
+      if (nrFFTs == 0)
+        nrFFTs = 1;
+
+      gpu::ScopedCurrentContext scc(context);
+
+      cufftResult error;
+      size_t workSize;
+
+      // Allocate plan
+      error = cufftPlan1d(&plan, fftSize, CUFFT_C2C, nrFFTs);
+
+      if (error != CUFFT_SUCCESS) {
+        // Print estimated work size
+        if (cufftEstimate1d(fftSize, CUFFT_C2C, nrFFTs, &workSize) == CUFFT_SUCCESS)
+          LOG_INFO_STR("FFT_Plan size for " << nrFFTs << " x " << fftSize << " points: " << (workSize/1024/1024) << " MByte (estimated)");
+
+        THROW(gpu::CUDAException, "cufftPlan1d: " << gpu::cufftErrorMessage(error));
+      }
+
+      // Print actual work size
+      if (cufftGetSize(plan, &workSize) == CUFFT_SUCCESS) {
+        LOG_DEBUG_STR("FFT_Plan size for " << nrFFTs << " x " << fftSize << " points: " << (workSize/1024/1024) << " MByte (actual)");
+      }
+    }
+
+    FFT_Plan::~FFT_Plan()
+    {
+      gpu::ScopedCurrentContext scc(context);
+
+      cufftDestroy(plan);
+    }
+
+    void FFT_Plan::setStream(const gpu::Stream &stream) const
+    {
+      cufftResult error;
+
+      gpu::ScopedCurrentContext scc(context);
+
+      error = cufftSetStream(plan, stream.get());
+
+      if (error != CUFFT_SUCCESS)
+        THROW(gpu::CUDAException, "cufftSetStream: " << gpu::cufftErrorMessage(error));
+    }
+  }
+}
+
