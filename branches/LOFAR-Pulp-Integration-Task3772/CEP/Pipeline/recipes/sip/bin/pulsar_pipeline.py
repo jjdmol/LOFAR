7283aea619aa70d91a18976b3a29974a05f134ad
jong <jong@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-03-31 14:38:40 +0000

    Task #3772: solve indentation errors

--- branches/LOFAR-Pulp-Integration-Task3772/CEP/Pipeline/recipes/sip/bin/pulsar_pipeline.py
+++ branches/LOFAR-Pulp-Integration-Task3772/CEP/Pipeline/recipes/sip/bin/pulsar_pipeline.py
@@ -31,36 +31,36 @@ class pulsar_pipeline(control):
   integration of the pulsar pipelines as a 'black-box'
   """
 
-  def __init__(self):
-    super(preprocessing_pipeline, self).__init__()
-    self.parset = parameterset()
-    self.input_data = []
-    self.output_data = []
-    self.io_data_mask = []
-    self.parset_feedback_file = None
+    def __init__(self):
+        super(preprocessing_pipeline, self).__init__()
+        self.parset = parameterset()
+        self.input_data = []
+        self.output_data = []
+        self.io_data_mask = []
+        self.parset_feedback_file = None
 
 
-  def go(self):
-    """
-    Read the parset-file that was given as input argument;
-    set jobname, and input/output data products before calling the
-    base-class's `go()` method.
-    """
-    try:
-        parset_file = os.path.abspath(self.inputs['args'][0])
-    except IndexError:
-        return self.usage()
-    self.parset.adoptFile(parset_file)
-    self.parset_feedback_file = parset_file + "_feedback"
+    def go(self):
+        """
+        Read the parset-file that was given as input argument;
+        set jobname, and input/output data products before calling the
+        base-class's `go()` method.
+        """
+        try:
+            parset_file = os.path.abspath(self.inputs['args'][0])
+        except IndexError:
+            return self.usage()
+        self.parset.adoptFile(parset_file)
+        self.parset_feedback_file = parset_file + "_feedback"
 
-    # Set job-name to basename of parset-file w/o extension, if it's not
-    # set on the command-line with '-j' or '--job-name'
-    if not self.inputs.has_key('job_name'):
-        self.inputs['job_name'] = (
-            os.path.splitext(os.path.basename(parset_file))[0])
+        # Set job-name to basename of parset-file w/o extension, if it's not
+        # set on the command-line with '-j' or '--job-name'
+        if not self.inputs.has_key('job_name'):
+            self.inputs['job_name'] = (
+                os.path.splitext(os.path.basename(parset_file))[0])
 
-    # Call the base-class's `go()` method.
-    return super(pulsar_pipeline, self).go()
+        # Call the base-class's `go()` method.
+        return super(pulsar_pipeline, self).go()
   
 
     def _get_io_product_specs(self):
