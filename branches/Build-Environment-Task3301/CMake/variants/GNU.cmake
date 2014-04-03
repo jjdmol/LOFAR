3c01e8e521dede56546dec1c6fe86a2823f2c054
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-04-03 15:33:04 +0000

    Task #3301: Added build variant 'opt3' to the 'gnu' compiler suite. It uses more aggressive optimizations than the 'opt' variant: -O3 -DNDEBUG -DDISABLE_DEBUG_OUTPUT. This latter option removes all LOG_DEBUG messages already at compile time.

--- branches/Build-Environment-Task3301/CMake/variants/GNU.cmake
+++ branches/Build-Environment-Task3301/CMake/variants/GNU.cmake
@@ -7,7 +7,7 @@
 set(LOFAR_COMPILER_SUITES GNU)
 
 # Build variants
-set(LOFAR_BUILD_VARIANTS DEBUG OPT)
+set(LOFAR_BUILD_VARIANTS DEBUG OPT OPT3)
 
 # GNU compiler suite
 set(GNU_COMPILERS GNU_C GNU_CXX GNU_Fortran GNU_ASM)
@@ -19,13 +19,18 @@ set(GNU_ASM       /usr/bin/gcc)      # GNU assembler
 set(GNU_C_FLAGS)
 set(GNU_C_FLAGS_DEBUG    "-g")
 set(GNU_C_FLAGS_OPT      "-g -O2")
+set(GNU_C_FLAGS_OPT3     "-g -O3")
 set(GNU_CXX_FLAGS        "-W -Wall -Woverloaded-virtual -Wno-unknown-pragmas")
 set(GNU_CXX_FLAGS_DEBUG  "-g")
 set(GNU_CXX_FLAGS_OPT    "-g -O2")
+set(GNU_CXX_FLAGS_OPT3   "-g -O3")
 set(GNU_EXE_LINKER_FLAGS)
 set(GNU_EXE_LINKER_FLAGS_DEBUG)
 set(GNU_EXE_LINKER_FLAGS_OPT)
+set(GNU_EXE_LINKER_FLAGS_OPT3)
 set(GNU_COMPILE_DEFINITIONS)
 set(GNU_COMPILE_DEFINITIONS_DEBUG 
                          "-DLOFAR_DEBUG -DENABLE_DBGASSERT -DENABLE_TRACER")
 set(GNU_COMPILE_DEFINITIONS_OPT)
+set(GNU_COMPILE_DEFINITIONS_OPT3
+                         "-DNDEBUG -DDISABLE_DEBUG_OUTPUT")
