973316a9341fe5208c45e2b56d0c3698fedaabca
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-04-04 12:19:12 +0000

    Task #5641: Increased send buffers for BF output, to deal with bursts of subbands

--- branches/Cobalt-Task5641/RTCP/Cobalt/GPUProc/src/cuda/Pipelines/BeamFormerPipeline.cc
+++ branches/Cobalt-Task5641/RTCP/Cobalt/GPUProc/src/cuda/Pipelines/BeamFormerPipeline.cc
@@ -93,7 +93,12 @@ namespace LOFAR
       :
       Pipeline(ps, subbandIndices, devices),
       factories(ps, nrSubbandsPerSubbandProc),
-      multiSender(hostMap(ps, subbandIndices, hostID), 3, ps.realTime())
+      // Each work queue needs an output element for each subband it processes, because the GPU output can
+      // be in bulk: if processing is cheap, all subbands will be output right after they have been received.
+      //
+      // For smooth operations, allocate 2 elements more per work queue, to allow one element to be in transit
+      // on both the receiver and sender side.
+      multiSender(hostMap(ps, subbandIndices, hostID), std::max(3UL, (2 + nrSubbandsPerSubbandProc) * workQueues.size()), ps.realTime())
     {
       ASSERT(ps.settings.beamFormer.enabled);
     }
