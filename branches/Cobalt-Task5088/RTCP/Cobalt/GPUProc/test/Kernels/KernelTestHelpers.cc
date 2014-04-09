843da3909b0c2063c69964d9814f1e2926a59c4e
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-04-09 13:31:13 +0000

    Task #5088: Refactor the test and move the functionality that can be shared to a helper class

---
+++ branches/Cobalt-Task5088/RTCP/Cobalt/GPUProc/test/Kernels/KernelTestHelpers.cc
@@ -0,0 +1,157 @@
+//# KernelTestHelpers.cc: test Kernels/DelayAndBandPassKernel class
+//# Copyright (C) 2013  ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O. Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#include <lofar_config.h>
+
+#include <CoInterface/Parset.h>
+#include <boost/lexical_cast.hpp>
+
+#include "KernelTestHelpers.h"
+
+using namespace std;
+using namespace LOFAR::Cobalt;
+using namespace boost;
+
+void usage(char const *testName)
+{
+  cout << "Usage: " << testName << " [options] " << endl;
+  cout << "" << endl;
+  cout << " Performance measurements: ( no output validation)" << endl;
+  cout << " -t nrtabs          Number of tabs to create, default == 127" << endl;
+  cout << " -c nrchannels      Number of channels to create, default == 64" << endl;
+  cout << " -i IdxGPU          GPU index to run kernel on, default == 0" << endl;
+  cout << " -s nrStations      Number of stations to create, default == 47" << endl;
+  cout << " -b nrSampleBlocks  Number of 64*64 Samples to create, default = 48 (-> 196608 total samples)" << endl;
+  cout << " * The kernels might not actually use all these parameters" << endl;
+  cout << "" << endl;
+  //cout << "If no arguments are provide the kernel with be tested on output validity" << endl;
+  cout << "" << endl;
+}
+
+
+KernelParameters::KernelParameters()
+{
+  nrTabs = 127;
+  nrChannels = 64;
+  idxGPU = 0;
+  nStation = 47;
+  nTimeBlocks = 48;
+}
+
+void KernelParameters::print()
+{
+  cout << "Used Kernel parameters:" << endl
+    << "nrTabs      : " << nrTabs << endl
+    << "nrChannels  : " << nrChannels << endl
+    << "idxGPU      : " << idxGPU << endl
+    << "nStation    : " << nStation << endl
+    << "nTimeBlocks : " << nTimeBlocks << endl;
+}
+
+
+KernelParameters parseCommandlineParameters(int argc, char *argv[], Parset &ps, char *testName)
+{
+  KernelParameters params;
+  int opt;
+
+  // parse all command-line options
+  while ((opt = getopt(argc, argv, "t:c:i:s:b:")) != -1)
+  {
+    switch (opt)
+    {
+    case 't':
+      params.nrTabs = atoi(optarg);
+      break;
+
+    case 'c':
+      params.nrChannels = atoi(optarg);
+
+      break;
+
+    case 'i':
+      params.idxGPU = atoi(optarg);
+      break;
+
+    case 's':
+      params.nStation = atoi(optarg);
+      break;
+
+    case 'b':
+      params.nTimeBlocks = atoi(optarg);
+      break;
+
+    default:
+
+      usage(testName);
+      exit(1);
+    }
+  }
+
+  // we expect no further arguments
+  if (optind != argc) {
+    usage(testName);
+    exit(1);
+  }
+
+  // Create a parset with the correct parameters to run a beamformer kernel
+  ps.add("Observation.Beam[0].TiedArrayBeam[0].directionType", "J2000");
+  ps.add("Observation.Beam[0].TiedArrayBeam[0].absoluteAngle1", "0.0");
+  ps.add("Observation.Beam[0].TiedArrayBeam[0].absoluteAngle2", "0.0");
+  ps.add("Observation.Beam[0].TiedArrayBeam[0].coherent", "true");
+  ps.add("Observation.Beam[0].angle1", "5.0690771926813865");
+  ps.add("Observation.Beam[0].angle2", "0.38194688387907605");
+  ps.add("Observation.Beam[0].directionType", "J2000");
+  ps.add("Observation.Beam[0].nrTabRings", "0");
+  ps.add("Observation.Beam[0].nrTiedArrayBeams", lexical_cast<string>(params.nrTabs));
+  ps.add("Observation.Beam[0].subbandList", "[24..28]");
+  string filenames = "[";
+  filenames.append(lexical_cast<string>(params.nrTabs)).append("*BEAM000.h5]");
+  string hosts = "[";
+  hosts.append(lexical_cast<string>(params.nrTabs)).append("*localhost:.]");
+  ps.add("Observation.DataProducts.Output_CoherentStokes.filenames", filenames);
+  ps.add("Observation.DataProducts.Output_CoherentStokes.locations", hosts);
+  ps.add("Observation.DataProducts.Output_CoherentStokes.enabled", "true");
+
+  ps.add("Observation.DataProducts.Output_Correlated.enabled", "true");
+  ps.add("Observation.DataProducts.Output_Correlated.filenames", "[SB0.MS, SB1.MS, SB2.MS, SB3.MS, SB4.MS]");
+  ps.add("Observation.DataProducts.Output_Correlated.locations", "[5 * :.]");
+  ps.add("Cobalt.BeamFormer.CoherentStokes.nrChannelsPerSubband", lexical_cast<string>(params.nrChannels));
+
+  ps.add("Cobalt.BeamFormer.CoherentStokes.subbandsPerFile", "512");
+  ps.add("Cobalt.BeamFormer.CoherentStokes.timeIntegrationFactor", "16");
+  ps.add("Cobalt.BeamFormer.CoherentStokes.which", "I");
+
+  ps.add("Cobalt.BeamFormer.nrDelayCompensationChannels", lexical_cast<string>(params.nrChannels));
+  ps.add("Cobalt.BeamFormer.nrHighResolutionChannels", lexical_cast<string>(params.nrChannels));
+
+  ps.add("Cobalt.blockSize", lexical_cast<string>(params.nTimeBlocks * 64 * params.nrChannels));
+
+  string stations = "[";
+  stations.append(lexical_cast<string>(params.nStation)).append("*RS106HBA]");
+  ps.add("Observation.VirtualInstrument.stationList", stations);
+  ps.add("Observation.antennaArray", "HBA");
+  ps.add("Observation.nrBeams", "1");
+  ps.add("Observation.beamList", "[5 * 0]");
+  ps.add("Observation.Dataslots.RS106HBA.DataslotList", "[0..4]");
+  ps.add("Observation.Dataslots.RS106HBA.RSPBoardList", "[5 * 0]");
+  ps.updateSettings();
+  params.print();
+  return params;
+}
--- branches/Cobalt-Task5088/RTCP/Cobalt/GPUProc/test/Kernels/tDelayAndBandPassKernel.cc
+++ branches/Cobalt-Task5088/RTCP/Cobalt/GPUProc/test/Kernels/tDelayAndBandPassKernel.cc
@@ -33,69 +33,18 @@
 #include <boost/lexical_cast.hpp>
 
 
+#include "KernelTestHelpers.h"
+
 using namespace std;
 using namespace LOFAR::Cobalt;
 using namespace boost;
 
-
-void usage()
-{
-cout << "Usage: DelayAndBandPAssKernel [options] " << endl;
-cout << "" << endl;
-cout << " Performance measurements: ( no output validation)" << endl;
-cout << "-t nrtabs     Number of tabs to create, default == 1" << endl;
-cout << "-c nrchannels Number of channels to create, default == 1" << endl;
-cout << "-i IdxGPU     GPU index to run kernel on, default == 0" << endl;
-cout << "-s IdxGPU     Number of stations to create" << endl;
-cout << "" << endl;
-//cout << "If no arguments are provide the kernel with be tested on output validity" << endl;
-cout << "" << endl;
-}
-
-
 int main(int argc, char *argv[])
 {
-INIT_LOGGER("tDelayAndBandPassKernel");
-
-
-int opt;
-unsigned nrTabs = 127;
-unsigned nrChannels = 64;
-unsigned idxGPU = 0;
-unsigned nStation = 47;
-// parse all command-line options
-while ((opt = getopt(argc, argv, "t:c:i:s:")) != -1)
-{
-switch (opt)
-{
-case 't':
-nrTabs = atoi(optarg);
-break;
-
-case 'c':
-nrChannels = atoi(optarg);
-break;
-
-case 'i':
-idxGPU = atoi(optarg);
-break;
-
-case 's':
-nStation = atoi(optarg);
-break;
-
-default:
-
-usage();
-exit(1);
-    }
-  }
-
-  // we expect no further arguments
-  if (optind != argc) {
-    usage();
-    exit(1);
-  }
+  char * testName = "tDelayAndBandPassKernel";
+  INIT_LOGGER(testName);
+  Parset ps;
+  KernelParameters params = parseCommandlineParameters(argc, argv, ps, testName);
 
 
   // Set up gpu environment
@@ -112,67 +61,23 @@ exit(1);
   gpu::Context ctx(device);
   gpu::Stream stream(ctx);
 
-  // Create a parset with the correct parameters to run a beamformer kernel
-  Parset ps;
-  ps.add("Observation.Beam[0].TiedArrayBeam[0].directionType", "J2000");
-  ps.add("Observation.Beam[0].TiedArrayBeam[0].absoluteAngle1", "0.0");
-  ps.add("Observation.Beam[0].TiedArrayBeam[0].absoluteAngle2", "0.0");
-  ps.add("Observation.Beam[0].TiedArrayBeam[0].coherent", "true");
-  ps.add("Observation.Beam[0].angle1", "5.0690771926813865");
-  ps.add("Observation.Beam[0].angle2", "0.38194688387907605");
-  ps.add("Observation.Beam[0].directionType", "J2000");
-  ps.add("Observation.Beam[0].nrTabRings", "0");
-  ps.add("Observation.Beam[0].nrTiedArrayBeams", lexical_cast<string>(nrTabs));
-  ps.add("Observation.Beam[0].subbandList", "[24..28]");
-  string filenames = "[";
-  filenames.append(lexical_cast<string>(nrTabs)).append("*BEAM000.h5]");
-  string hosts = "[";
-  hosts.append(lexical_cast<string>(nrTabs)).append("*localhost:.]");
-  ps.add("Observation.DataProducts.Output_CoherentStokes.filenames", filenames);
-  ps.add("Observation.DataProducts.Output_CoherentStokes.locations", hosts);
-  ps.add("Observation.DataProducts.Output_CoherentStokes.enabled", "true");
-
-  ps.add("Observation.DataProducts.Output_Correlated.enabled", "true");
-  ps.add("Observation.DataProducts.Output_Correlated.filenames", "[SB0.MS, SB1.MS, SB2.MS, SB3.MS, SB4.MS]");
-  ps.add("Observation.DataProducts.Output_Correlated.locations", "[5 * :.]");
-
-  //ps.add("Cobalt.BeamFormer.CoherentStokes.nrChannelsPerSubband", "1");
-  ps.add("Cobalt.BeamFormer.CoherentStokes.nrChannelsPerSubband", lexical_cast<string>(nrChannels));
-
-  ps.add("Cobalt.BeamFormer.CoherentStokes.subbandsPerFile", "512");
-  ps.add("Cobalt.BeamFormer.CoherentStokes.timeIntegrationFactor", "16");
-  ps.add("Cobalt.BeamFormer.CoherentStokes.which", "I");
-
-  ps.add("Cobalt.BeamFormer.nrDelayCompensationChannels", "64");
-  ps.add("Cobalt.BeamFormer.nrHighResolutionChannels", "64");
-  ps.add("Cobalt.blockSize", "196608");
-
-  string stations = "[";
-  stations.append(lexical_cast<string>(nStation)).append("*RS106HBA]");
-  ps.add("Observation.VirtualInstrument.stationList", stations);
-  ps.add("Observation.antennaArray", "HBA");
-  ps.add("Observation.nrBeams", "1");
-  ps.add("Observation.beamList", "[5 * 0]");
-  ps.add("Observation.Dataslots.RS106HBA.DataslotList", "[0..4]");
-  ps.add("Observation.Dataslots.RS106HBA.RSPBoardList", "[5 * 0]");
-  ps.updateSettings();
   
-  KernelFactory<DelayAndBandPassKernel> factory(ps);
+  KernelFactory<DelayAndBandPassKernel> factory(BeamFormerFactories::delayCompensationParams(ps));
   stream.synchronize();
 
   gpu::DeviceMemory
-  inputData(ctx, factory.bufferSize(DelayAndBandPassKernel::INPUT_DATA)),
-  filteredData(ctx, factory.bufferSize(DelayAndBandPassKernel::OUTPUT_DATA)),
-  delaysAtBegin(ctx, factory.bufferSize(DelayAndBandPassKernel::DELAYS)),
-  delaysAfterEnd(ctx, factory.bufferSize(DelayAndBandPassKernel::DELAYS)),
-  phase0s(ctx, factory.bufferSize(DelayAndBandPassKernel::PHASE_ZEROS)),
-  bandPassCorrectionWeights(ctx, factory.bufferSize(DelayAndBandPassKernel::BAND_PASS_CORRECTION_WEIGHTS));
+    inputData(ctx, factory.bufferSize(DelayAndBandPassKernel::INPUT_DATA)),
+    filteredData(ctx, factory.bufferSize(DelayAndBandPassKernel::OUTPUT_DATA)),
+    delaysAtBegin(ctx, factory.bufferSize(DelayAndBandPassKernel::DELAYS)),
+    delaysAfterEnd(ctx, factory.bufferSize(DelayAndBandPassKernel::DELAYS)),
+    phase0s(ctx, factory.bufferSize(DelayAndBandPassKernel::PHASE_ZEROS)),
+    bandPassCorrectionWeights(ctx, factory.bufferSize(DelayAndBandPassKernel::BAND_PASS_CORRECTION_WEIGHTS));
 
   DelayAndBandPassKernel::Buffers buffers(inputData, filteredData, delaysAtBegin, delaysAfterEnd, phase0s, bandPassCorrectionWeights);
 
   auto_ptr<DelayAndBandPassKernel> kernel(factory.create(stream, buffers));
-  
-  
+
+
   size_t subbandIdx = 0;
   float centralFrequency = ps.settings.subbands[subbandIdx].centralFrequency;
   size_t SAP = ps.settings.subbands[subbandIdx].SAP;
