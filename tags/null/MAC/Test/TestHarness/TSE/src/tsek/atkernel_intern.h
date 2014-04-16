ef3722e1409ae0dd72455df067e8b09ab690b917
jenkins <jenkins@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-04-16 14:08:33 +0000

    Tagged by Jenkins svn-tag plugin.

---
+++ tags/null/MAC/Test/TestHarness/TSE/src/tsek/atkernel_intern.h
@@ -0,0 +1,41 @@
+/*=========================================================================
+// (c)   Ordina Technical Automation BV
+//
+// The Intellectual Property Rights of this source code file is not transferable.
+// For information on this subject, contact Ordina Technical Automation B.V.
+// Any attempt or participation in deciphering, decoding, reverse engineering or
+// in any way altering the source code is strictly prohibited, unless the prior
+// written consent of Ordina Technical Automation B.V. is obtained.
+//
+// Project       : Script Engine
+// Module/Systeem: Script Engine
+//
+// --- Version Control ---
+//    $RCSfile$
+//    $Revision$
+//    $Date$
+// -----------------------
+//
+// Description   : 
+//
+// Revisions:
+//
+// Date       Author                  Changes
+// 08/08/2000 E.A. Nijboer            Initial release
+// 12/01/2001 E.A. Nijboer            platform test.
+// 09/02/2001 E.A. Nijboer            
+//
+//=========================================================================
+*/
+
+
+/* The only purpose of this file is to provide access to two functions.     */
+/* This is needed, because in the atkernel.c a statemachine is used,        */
+/* keeping track of the internal state of the kernel.                       */
+
+void      internal_ScriptStopped(
+  char *pcReason);
+void      internal_BatchStopped(
+  char *pcReason);
+void      LogLine(
+  char *pcString);
