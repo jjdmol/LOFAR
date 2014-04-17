924f2a8294c3476c2003351f1947561e8106a13a
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-04-17 12:41:45 +0000

    Task #5602: Start dummy outputproc in a seperate thread. Test is green on cobalt9

--- branches/Cobalt-Task5602/RTCP/Cobalt/GPUProc/test/Storage/tStorageProcesses.cc
+++ branches/Cobalt-Task5602/RTCP/Cobalt/GPUProc/test/Storage/tStorageProcesses.cc
@@ -109,21 +109,31 @@ void test_protocol()
   p.updateSettings();
 
   {
-    StorageProcesses sp(p, "");
-
-    // Give Storage time to log its parset
-    sleep(2);
-
-    // Give 10 seconds to exchange final meta data
-    sp.forwardFinalMetaData(time(0) + 10);
-
-    // Give 10 seconds to wrap up
-    sp.stop(time(0) + 10);
-
-    // Obtain LTA feedback
-    ParameterSet feedbackLTA(sp.feedbackLTA());
-
-    ASSERT(feedbackLTA.getString("foo","") == "bar");
+#   pragma omp parallel sections
+    {
+#     pragma omp section
+      {
+        system("./DummyStorage 12345 0");
+      }
+#     pragma omp section
+      {
+	    StorageProcesses sp(p, "");
+	
+  	    // Give Storage time to log its parset
+	    sleep(2);
+	
+	    // Give 10 seconds to exchange final meta data
+	    sp.forwardFinalMetaData(time(0) + 10);
+	
+	    // Give 10 seconds to wrap up
+	    sp.stop(time(0) + 10);
+	
+	    // Obtain LTA feedback
+	    ParameterSet feedbackLTA(sp.feedbackLTA());
+	
+	    ASSERT(feedbackLTA.getString("foo","") == "bar");
+	  }
+    }
   }
 }
 
