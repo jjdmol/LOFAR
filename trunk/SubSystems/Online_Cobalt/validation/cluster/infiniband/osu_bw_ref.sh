7af8232a63bd72a033c6fd93045c1df29f9f9830
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-05-13 14:00:31 +0000

    Task #5714: Updated the osu_bw.test script, the reference output it needs, and the script to generate this reference output.

--- trunk/SubSystems/Online_Cobalt/validation/cluster/infiniband/osu_bw_ref.sh
+++ trunk/SubSystems/Online_Cobalt/validation/cluster/infiniband/osu_bw_ref.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 #
-# Determine reference output for osu_bw test, by running osu_bw 20 times and
-# calculating the averge throughput figures.
+# Determine reference output for osu_bw test, by running osu_bw on each of
+# the Cobalt nodes and calculating the averge throughput figures.
 #
 # $Id$
 
-for i in $(seq 1 20)
-do
-  ./osu_bw.test && \
-  awk '{print $2}' osu_bw.out > $i
-  echo -n "."
-done
-paste $(seq 1 20) | grep '^[0-9]' | \
-  sed -e 's,\t,+,g' -e 's,^,(,' -e 's,$,)/20.,' | bc -l | \
-  sed 's,0\+$,,' > avg
+HOSTS=$(for i in $(seq 1 8); do printf "cbt%03d " $i; done)
 
-{ head -2 osu_bw.out;
-  paste <(awk '{print $1}' osu_bw.out | grep '^[0-9]') <(cat avg);
+rm -f *.bw
+for host in $HOSTS
+do
+  ssh $host $PWD/osu_bw.test
+  for target in $HOSTS
+  do
+    [ "$host" == "$target" ] && continue;
+    f="osu_bw.$target.out"
+    awk '{print $2}' $f > $host.$f.bw
+  done
+done
+
+n=$(echo *.bw | wc -w)
+
+paste *.bw | grep '^[0-9]' | \
+  sed -e 's,\t,+,g' -e 's,^,scale=2; (,' -e 's,$,)/'$n',' | \
+  bc -l > avg
+
+{ head -2 $f;
+  paste <(awk '{print $1}' $f | grep '^[0-9]') <(cat avg);
 } > osu_bw.ref
