0997c01de201bd06ca00df7988b0ebeac3b18be8
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-05-16 13:47:02 +0000

    Task #5607: Rename to allow auto copy to test location

--- branches/Cobalt-Task5607/RTCP/Cobalt/GPUProc/test/tMPISendReceive.cc
+++ branches/Cobalt-Task5607/RTCP/Cobalt/GPUProc/test/tMPISendReceive.cc
@@ -1,59 +1,61 @@
 #include <lofar_config.h>
 
-#include <iostream>
-
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <iostream>
+#include <string>
 
-//using namespace LOFAR;
-//using namespace LOFAR::Cobalt;
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <CoInterface/Parset.h>
+
+using namespace LOFAR;
+using namespace LOFAR::Cobalt;
 using namespace std;
 
-static void usage(const char *argv0)
-{
-  cerr << "RTCP: Real-Time Central Processing of the LOFAR radio telescope." << endl;
-  cerr << "RTCP provides correlation for the Standard Imaging mode and" << endl;
-  cerr << "beam-forming for the Pulsar mode." << endl;
-  // one of the roll-out scripts greps for the version x.y
-
-  cerr << endl;
-  cerr << "Usage: " << argv0 << " parset" << " [-p]" << endl;
-  cerr << endl;
-  cerr << "  -h: print this message" << endl;
-}
 
 main(int argc, char **argv)
 {
-  std::cout << "Hello World!";
+  string testname("tMPISendReceive");
+  std::cout << "testname" << endl;
+
+  string parsetFile = "tMPISendReceive.in_parset";
+
+  // ****************************************************
+  // Set up the mpi environment
+  // Rank in MPI set of hosts, or 0 if no MPI is used
+  int rank = 0;
+
+  // Number of MPI hosts, or 1 if no MPI is used
+  int nrHosts = 1;
 
 
-  /*
-  * Parse command-line options
-  */
+  const char *rankstr, *sizestr;
+  // klijn@cbt009:~/build/5607/gnu_debug/RTCP/Cobalt/GPUProc/test$ mpirun.sh -np 4 ./tMPISendReceive
 
-  int opt;
-  while ((opt = getopt(argc, argv, "ph")) != -1) {
-    switch (opt) {
+  // OpenMPI rank
+  if ((rankstr = getenv("OMPI_COMM_WORLD_RANK")) != NULL)
+    rank = boost::lexical_cast<int>(rankstr);
 
-    case 'h':
-      usage(argv[0]);
-      exit(0);
+  // OpenMPI size
+  if ((sizestr = getenv("OMPI_COMM_WORLD_SIZE")) != NULL)
+    nrHosts = boost::lexical_cast<int>(sizestr);
 
-    default: /* '?' */
-      usage(argv[0]);
-      exit(1);
-    }
-  }
+  // MVAPICH2 rank
+  if ((rankstr = getenv("MV2_COMM_WORLD_RANK")) != NULL)
+    rank = boost::lexical_cast<int>(rankstr);
 
-  // we expect a parset filename as an additional parameter
-  if (optind >= argc) {
-    usage(argv[0]);
-    exit(1);
-  }
+  // MVAPICH2 size
+  if ((sizestr = getenv("MV2_COMM_WORLD_SIZE")) != NULL)
+    nrHosts = boost::lexical_cast<int>(sizestr);
+  // ****************************************************
 
+  cout <<  "MPI rank " << rank << " out of " << nrHosts << " hosts" << endl;
 
+  Parset ps(parsetFile);
   return 0;
 }
