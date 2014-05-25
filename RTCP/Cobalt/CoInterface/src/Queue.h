dcc571e0809a5dfd7f6c27f91cb9c150a67e87c5
Jan David Mol <mol@astron.nl>
2014-05-25 06:50:42 +0000

    Task #5762: Replaced list by deque in Queue, and added speed test

--- RTCP/Cobalt/CoInterface/src/Queue.h
+++ RTCP/Cobalt/CoInterface/src/Queue.h
@@ -30,7 +30,7 @@
 #include <CoInterface/TimeFuncs.h>
 #include <CoInterface/RunningStatistics.h>
 
-#include <list>
+#include <deque>
 #include <time.h>
 #include <string>
 
@@ -98,7 +98,7 @@ template <typename T> class Queue
 
     mutable Mutex              itsMutex;
     Condition	                 itsNewElementAppended;
-    std::list<struct Element>  itsQueue;
+    std::deque<struct Element> itsQueue;
 
     // append() without grabbing itsMutex
     void     unlocked_append(const T&, bool timed);
