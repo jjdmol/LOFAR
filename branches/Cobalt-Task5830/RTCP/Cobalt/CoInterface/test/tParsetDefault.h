e5d5d147eff1f994f34af1ff8716425bcdbfe66e
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-06-10 07:27:32 +0000

    Task #5833 (in branch of Task #5830): move most code from the tParsetDefault header into its own .cc file (prev commit r29466). The two affected tests still PASS.

--- branches/Cobalt-Task5830/RTCP/Cobalt/CoInterface/test/tParsetDefault.h
+++ branches/Cobalt-Task5830/RTCP/Cobalt/CoInterface/test/tParsetDefault.h
@@ -21,73 +21,12 @@
 #ifndef LOFAR_COINTERFACE_TPARSETDEFAULT_H
 #define LOFAR_COINTERFACE_TPARSETDEFAULT_H
 
-// Default RTCP parset creation for test programs.
-// This is useful, because we verify a lot of keys when reading a parset and
-// we don't want to create a passing key/value set for every test case separately.
-//
-// The unmodified default test key/value set created below is tested in tParset.cc.
-
 #include <string>
 #include <CoInterface/Parset.h>
 
 LOFAR::Cobalt::Parset makeDefaultTestParset();
-LOFAR::Cobalt::Parset makeDefaultTestParset(const std::string& key, const std::string& value);
-
-
-LOFAR::Cobalt::Parset makeDefaultTestParset() {
-  LOFAR::Cobalt::Parset ps;
-
-  // Required keys to pass basic parset checks.
-  ps.add("Observation.ObsID", "12345");
-  // Use a valid station name (CS001) that is not in any tParset test,
-  // so we don't have to remove board and slot list keys in some tests.
-  ps.add("Observation.VirtualInstrument.stationList", "[CS001]");
-  ps.add("Observation.antennaSet", "LBA_INNER");
-  ps.add("Observation.bandFilter", "LBA_30_70");
-  ps.add("Observation.nrBeams", "1");
-  ps.add("Observation.Beam[0].subbandList", "[21..23]");
-  ps.add("Observation.Dataslots.CS001LBA.RSPBoardList", "[3*0]");
-  ps.add("Observation.Dataslots.CS001LBA.DataslotList", "[0..2]");
-
-  ps.add("Observation.Beam[0].nrTiedArrayBeams", "1");
-  ps.add("Observation.Beam[0].TiedArrayBeam[0].coherent", "true");
-
-  // for tests that use HBA
-  ps.add("Observation.Dataslots.CS001HBA.RSPBoardList", "[3*0]");
-  ps.add("Observation.Dataslots.CS001HBA.DataslotList", "[0..2]");
-  ps.add("Observation.Dataslots.CS001HBA0.RSPBoardList", "[3*0]");
-  ps.add("Observation.Dataslots.CS001HBA0.DataslotList", "[0..2]");
-  ps.add("Observation.Dataslots.CS001HBA1.RSPBoardList", "[3*0]");
-  ps.add("Observation.Dataslots.CS001HBA1.DataslotList", "[0..2]");
-
-  // basic correlation output keys
-  ps.add("Observation.DataProducts.Output_Correlated.enabled", "true");
-  ps.add("Observation.DataProducts.Output_Correlated.filenames",
-         "[L12345_SAP000_SB000_uv.MS, L12345_SAP000_SB001_uv.MS, L12345_SAP000_SB002_uv.MS]");
-  ps.add("Observation.DataProducts.Output_Correlated.locations", "[3*localhost:tParset-data/]");
-
-  // basic beamforming output keys
-  ps.add("Observation.DataProducts.Output_CoherentStokes.enabled", "true");
-  ps.add("Cobalt.BeamFormer.CoherentStokes.which", "I");
-  ps.add("Observation.DataProducts.Output_CoherentStokes.filenames", "[L12345_SAP000_B000_S000_P000_bf.h5]");
-  ps.add("Observation.DataProducts.Output_CoherentStokes.locations", "[1*localhost:tParset-data/]");
-
-  ps.updateSettings();
-
-  return ps;
-}
-
-// Create a Parset out of one key/value pair in addition to
-// minimally required key/value pairs.
-LOFAR::Cobalt::Parset makeDefaultTestParset(const std::string& key, const std::string& value) {
-  LOFAR::Cobalt::Parset ps = makeDefaultTestParset();
-
-  // use replace() instead of set() in case one of the default keys is specified
-  ps.replace(key, value);
-  ps.updateSettings();
-
-  return ps;
-}
+LOFAR::Cobalt::Parset makeDefaultTestParset(const std::string& key,
+                                            const std::string& value);
 
 #endif
 
