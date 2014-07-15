6c3c027d716ba8edb7f0f4ce32de2859a96cfc23
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-07-15 14:49:12 +0000

    Task #5708: remove task branch

--- branches/Cobalt-Task5708/RTCP/Cobalt/OutputProc/scripts/bf-output-loss.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep ', lost' $1 | awk '{ ( $10 == "subbands," && $13 == "subbands.") ?  sum += $12/($9 + $12) : missed += 1}  END { print "Output loss: " 100*sum/(NR - missed) " %" }'
