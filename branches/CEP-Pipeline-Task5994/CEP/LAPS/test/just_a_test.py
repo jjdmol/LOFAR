c3c622e097e19970e6a82ce99ae53662b57a472e
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-08-12 13:18:50 +0000

    Task #5994: add end of line  and remove imports

--- branches/CEP-Pipeline-Task5994/CEP/LAPS/test/just_a_test.py
+++ branches/CEP-Pipeline-Task5994/CEP/LAPS/test/just_a_test.py
@@ -1,6 +1,8 @@
-import os,sys,time,pg
-from optparse import OptionParser
-import laps.MsgBus
+#import os,sys,time,pg
+#from optparse import OptionParser
+#import laps.MsgBus
 
 if __name__ == '__main__':
   print "test"
+  
+  
