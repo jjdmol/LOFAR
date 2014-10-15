4ca9c09ba08150c6da53b2d97221b84efd798ce4
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-10-15 08:15:02 +0000

    Task #6656: typo

--- branches/CEP-Pipeline-Task6656/CEP/Pipeline/framework/lofarpipe/support/utilities.py
+++ branches/CEP-Pipeline-Task6656/CEP/Pipeline/framework/lofarpipe/support/utilities.py
@@ -264,7 +264,7 @@ def catch_segfaults(cmd, cwd, env, logger, max = 1, cleanup = lambda: None,
             logger.debug("Retrying...")
         logger.debug("Running: %s" % (' '.join(cmd),))
         process = spawn_process(cmd, logger, cwd, env)
-        // add the created process to the usageStat object
+        #add the created process to the usageStat object
         if usageStats:
             usageStats.addPID(process.pid)
 
