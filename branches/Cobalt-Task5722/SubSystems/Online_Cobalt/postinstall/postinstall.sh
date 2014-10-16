8271b57947733d79ce18a84112d2b2610233c614
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-10-16 16:12:12 +0000

    Task #5722: Corrected use of casacore in PATH, and added comments

--- branches/Cobalt-Task5722/SubSystems/Online_Cobalt/postinstall/postinstall.sh
+++ branches/Cobalt-Task5722/SubSystems/Online_Cobalt/postinstall/postinstall.sh
@@ -12,8 +12,9 @@ DAL_SOURCE="https://github.com/nextgen-astrodata/DAL.git"
 
 # Download location for the casacore source.
 #
-# Note: if a new version is used, update CMake/variants/variants.cbm001 as well
-# to compile against the new version.
+# Note: if a new version is used, update:
+#   lofarsys/bashrc                   (to use the new version in $PATH)
+#   /CMake/variants/variants.cbm001   (to compile against the new version)
 CASACORE_SOURCE="ftp://ftp.atnf.csiro.au/pub/software/casacore/casacore-1.7.0.tar.bz2"
 
 function error {
@@ -22,6 +23,7 @@ function error {
 }
 
 function strip_extensions {
+  # strip all extensions that are non-numeric
   sed -r 's/([.][A-Za-z][A-Za-z0-9]*)*$//'
 }
 
@@ -139,7 +141,10 @@ function postinstall_lofarbuild {
   cd build
   # Note that the -DDATA_DIR option does not fully override the measures tables search paths.
   # In fact, some of ., ./data, $HOME/aips++/data, $HOME/data, $HOME/casacore/data are still searched first.
-  cmake -DCMAKE_INSTALL_PREFIX=/localhome/lofar/$CASACOREVERSION -DUSE_HDF5=ON -DUSE_FFTW3=ON -DFFTW3_DISABLE_THREADS=OFF -DUSE_THREADS=ON -DUSE_OPENMP=ON -DDATA_DIR=/localhome/lofar/IERS/current/data .. > cmake.log
+  cmake .. -DCMAKE_INSTALL_PREFIX=/localhome/lofar/$CASACOREVERSION \
+    -DUSE_HDF5=ON -DUSE_FFTW3=ON -DFFTW3_DISABLE_THREADS=OFF \
+    -DUSE_THREADS=ON -DUSE_OPENMP=ON \
+    -DDATA_DIR=/localhome/lofar/IERS/current/data > cmake.log
 
   echo "  Building..."
   make -j 8 > make.log 2>&1 # casacore build is NOT clean
