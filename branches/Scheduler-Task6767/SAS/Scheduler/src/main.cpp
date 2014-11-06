ac0ba5dfc3e8a3559166f40be15bcf084772d7f3
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-11-06 12:52:03 +0000

    Task #6767: Updated and added sources needed for doing Scheduler regression testing.

--- branches/Scheduler-Task6767/SAS/Scheduler/src/main.cpp
+++ branches/Scheduler-Task6767/SAS/Scheduler/src/main.cpp
@@ -11,33 +11,13 @@
  *
  */
 
-#include <QtGui>
-#include <QApplication>
-#include "lofar_scheduler.h"
-#include "Controller.h"
-#include "schedulergui.h"
 
-#undef main
+#include "schedulerLib.h"
 
-QString currentUser;
+//todo:
+//#define SCHEDULER_TEST
 
 int main(int argc, char *argv[])
 {
-	// remember the user running the scheduler, to know if certain functionality like publish and cleaning data should be enabled
-	QStringList env(QProcess::systemEnvironment());
-	env = env.filter("USER");
-	if (!env.isEmpty()) {
-		currentUser = env.front().split("=").back();
-	}
-
-	QApplication app(argc, argv);
-    Controller c(app); // The controller is the intermediary between the SchedulerGUI (the view) and the Scheduler
-    try {
-    c.start(); // controller starts the GUI
-    }
-    catch (...) {
-    	c.quit();
-    }
-    return app.exec();
+    return main_function(argc, argv);
 }
-
