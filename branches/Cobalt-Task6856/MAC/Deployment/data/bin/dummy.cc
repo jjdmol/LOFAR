5495c2eddee63d1d8c8a8a42ef588a65c9d0b6b8
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2014-11-10 14:57:26 +0000

    Task #6856: Deleted working branch

--- branches/Cobalt-Task6856/MAC/Deployment/data/bin/dummy.cc
+++
@@ -1,3 +0,0 @@
-// Dummy file, used as input for dummy library libdeployment,
-// which is only required for CMake in order to collect all
-// required link libraries when linking binaries executables.
