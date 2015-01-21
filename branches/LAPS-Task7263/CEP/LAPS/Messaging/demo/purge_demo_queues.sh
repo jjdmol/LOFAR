735d0148a5054a4fbb5d02e464e0de10b5a2c41c
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-01-21 10:59:52 +0000

    Task #7263: Correct  script call

--- branches/LAPS-Task7263/CEP/LAPS/Messaging/demo/purge_demo_queues.sh
+++ branches/LAPS-Task7263/CEP/LAPS/Messaging/demo/purge_demo_queues.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $( qls | grep laps | awk '{ print $1 }' ) ; do echo $i ; purgequeue $i ; done
+for i in $( qls | grep laps | awk '{ print $1 }' ) ; do echo $i ; purgequeue.py $i ; done
 
 cleanupq.sh laps
 
