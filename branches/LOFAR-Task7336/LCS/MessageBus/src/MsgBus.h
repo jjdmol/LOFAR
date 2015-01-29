c61359fe92b95d8e2a16f4ef101a0fe4dce982db
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-01-29 13:35:55 +0000

    Task #7432: Added the LOFAR license header, and include guards

--- branches/LOFAR-Task7336/LCS/MessageBus/src/MsgBus.h
+++ branches/LOFAR-Task7336/LCS/MessageBus/src/MsgBus.h
@@ -1,23 +1,27 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- */
+//# MsgBus.h: Wrapper for QPID clients to send and receive AMQP messages.
+//#
+//# Copyright (C) 2015
+//# ASTRON (Netherlands Institute for Radio Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+//#
+//# This file is part of the LOFAR software suite.
+//# The LOFAR software suite is free software: you can redistribute it and/or
+//# modify it under the terms of the GNU General Public License as published
+//# by the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The LOFAR software suite is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# $Id$
+
+#ifndef LOFAR_MESSAGEBUS_MSGBUS_H
+#define LOFAR_MESSAGEBUS_MSGBUS_H
 
 #include <qpid/messaging/Connection.h>
 #include <qpid/messaging/Message.h>
@@ -93,5 +97,5 @@ public:
   ~MultiBus();
 };
 
-
+#endif
 
