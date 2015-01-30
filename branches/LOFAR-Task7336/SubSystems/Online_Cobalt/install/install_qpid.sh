285e187f375d9596481430daa8cbfcc89227ede7
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-01-30 16:20:06 +0000

    Task #7336: Added QPID to Cobalt postinstall

---
+++ branches/LOFAR-Task7336/SubSystems/Online_Cobalt/install/install_qpid.sh
@@ -0,0 +1,87 @@
+#!/bin/bash -eu
+
+# We need to be lofarbuild to have the proper writing rights
+#[ "`whoami`" == "lofarbuild" ]
+
+# Download location for the latest QPID source
+PROTON_SOURCE="http://svn.apache.org/repos/asf/qpid/proton/trunk/"
+QPID_SOURCE="http://svn.apache.org/repos/asf/qpid/trunk/qpid/"
+
+QPID_INSTALLDIR=$HOME/src/qpid
+
+# ********************************************
+#  Install latest PROTON & QPID
+#
+#  into $QPID_INSTALLDIR
+# ********************************************
+echo "Configuring PROTON and QPID..."
+mkdir -p $QPID_INSTALLDIR
+
+QPIDDIR=`mktemp -d`
+pushd $QPIDDIR >/dev/null
+
+echo "  Downloading PROTON..."
+svn co http://svn.apache.org/repos/asf/qpid/proton/branches/0.8 proton >/dev/null
+
+echo "  Configuring PROTON..."
+pushd proton >/dev/null
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$QPID_INSTALLDIR .. > cmake.log
+
+echo "  Building PROTON..."
+make -j 8 > make.log
+
+echo "  Installing PROTON..."
+make -j 8 install > make_install.log
+
+# back to QPIDDIR
+popd >/dev/null
+
+echo "  Downloading QPID..."
+svn co http://svn.apache.org/repos/asf/qpid/branches/0.30/qpid/ qpid >/dev/null
+
+echo "  Configuring QPID C bindings..."
+pushd qpid/cpp >/dev/null
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$QPID_INSTALLDIR -DProton_DIR=/localhome/lofar/qpid/lib/cmake/Proton -DBUILD_XML=OFF -DBUILD_SSL=OFF -DBUILD_BINDING_RUBY=OFF .. > cmake.log
+
+echo "  Building QPID C bindings..."
+make -j 8 > make.log
+
+echo "  Installing QPID C bindings..."
+make -j 8 install > make_install.log
+
+# back to QPIDDIR
+popd >/dev/null
+
+echo "  Building and installing QPID Python bindings..."
+pushd qpid/python >/dev/null
+./setup.py build > setup_build.log
+./setup.py install --home=$QPID_INSTALLDIR > setup_install.log
+
+echo "  Building and installing QPID Python QMF..."
+popd >/dev/null
+pushd qpid/extras/qmf >/dev/null
+./setup.py build > setup_build.log
+./setup.py install --home=$QPID_INSTALLDIR > setup_install.log
+
+echo "  Building and installing QPID Python tools..."
+popd >/dev/null
+pushd qpid/tools >/dev/null
+./setup.py build > setup_build.log
+./setup.py install --home=$QPID_INSTALLDIR > setup_install.log
+
+echo "  Creating .profile..."
+PYTHONVERSION=`python -c 'import platform; print "%s.%s" % platform.python_version_tuple()[0:2]'`
+cat > $QPID_INSTALLDIR/.profile << EOF
+export PATH=\$PATH:$QPID_INSTALLDIR/sbin/:$QPID_INSTALLDIR/bin/:$QPID_INSTALLDIR/local/bin/
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$QPID_INSTALLDIR/lib:$QPID_INSTALLDIR/local/lib/
+export PYTHONPATH=\$PYTHONPATH:$QPID_INSTALLDIR/lib/python/:$QPID_INSTALLDIR/lib/python$PYTHONVERSION/site-packages/
+EOF
+
+echo "  Cleaning up..."
+popd >/dev/null
+rm -rf "$QPIDDIR"
+
