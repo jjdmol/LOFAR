dedfa11e4d5b7a966f50fd20e6ebbe79b45ac529
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-02-04 15:07:40 +0000

    Task #7058: Add green test to subprocess: It is now default limited to max 8 concurrent processes.

--- branches/CEP-Pipeline-Task7058/CEP/Pipeline/framework/lofarpipe/support/subprocessgroup.py
+++ branches/CEP-Pipeline-Task7058/CEP/Pipeline/framework/lofarpipe/support/subprocessgroup.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 from lofarpipe.support.lofarexceptions import PipelineException
 
 
@@ -7,8 +8,10 @@ class SubProcessGroup(object):
         A wrapper class for the subprocess module: allows fire and forget
         insertion of commands with a an optional sync/ barrier/ return
         """
-        def __init__(self, logger=None, usageStats = None,
-                     max_concurrent_processes = 8):
+        def __init__(self, logger=None, 
+                     usageStats = None,
+                     max_concurrent_processes = 8,  # Default CEP is max 8 cpu used
+                     polling_interval = 60):        # poll each minute: we have long running processes
             self.process_group = []
             self.logger = logger
             self.usageStats = usageStats
@@ -18,10 +21,11 @@ class SubProcessGroup(object):
             # list of command vdw pairs not started because the maximum
             # number of processes was reached
             self.processes_waiting_for_execution = []
+            self.polling_interval = polling_interval
 
         def _start_process(self, cmd, cwd):
             """
-            Helper function collection all the steps needed to start a process
+            Helper function collection all the coded needed to start a process
             """
             # About to start a process, increase the counter
             self.running_process_count += 1
@@ -64,7 +68,7 @@ class SubProcessGroup(object):
                     "list[string] as arguments suplied: {0}".format(type(cmd)))
 
             # We need to be able to limit the maximum number of processes
-            if running_process_count >= max_concurrent_processes: 
+            if self.running_process_count >= self.max_concurrent_processes: 
                 # Save the command string and cwd
                 self.processes_waiting_for_execution.append((cmd, cwd))
             else:
@@ -80,10 +84,19 @@ class SubProcessGroup(object):
             std out and error will be suplied to the logger
             """
             collected_exit_status = []
-            while (true):
+
+            while (True):
+                # The exit test
+                if (self.running_process_count == 0 and 
+                   len(self.processes_waiting_for_execution) == 0):
+                    break # the while loop
+
+                # start with waiting 
+                time.sleep(self.polling_interval)
+
+                # check all the running processes for completion
                 for idx, (completed, (cmd, process)) in \
                                 enumerate(self.process_group):
-                    # this process is complete continue
                     if completed:
                         continue
 
@@ -95,8 +108,6 @@ class SubProcessGroup(object):
                     # communicate with the process
                     # TODO: This would be the best place to create a
                     # non mem caching interaction with the processes!
-                    # TODO: should a timeout be introduced here to prevent never ending
-                    # runs?
                     (stdoutdata, stderrdata) = process.communicate()
                     exit_status = process.returncode
 
@@ -106,10 +117,12 @@ class SubProcessGroup(object):
 
                     # log the std out and err
                     if self.logger != None:
+                        self.logger.info("Subprocesses group, completed command: ")
                         self.logger.info(cmd)
                         self.logger.debug(stdoutdata)
                         self.logger.warn(stderrdata)
                     else:
+                        print "Subprocesses group, completed command: "
                         print cmd
                         print stdoutdata
                         print stderrdata
@@ -117,14 +130,19 @@ class SubProcessGroup(object):
                     # Now update the state of the internal state
                     self.process_group[idx] = (True, (cmd, process))
                     self.running_process_count -= 1
-                    # Finished process, decrease check if there are pending
-                    # subprocesses, empthy string evaluates to False
-                    if not self.processes_waiting_for_execution:  
-                        # Get the last process
-                        cmd , cwd = self.processes_waiting_for_execution.pop()
-                        # start it
-                        self._start_process(cmd, cwd)
+                                
+                   
+                # if there are less then the allowed processes running and
+                # we have waiting processes start another on
+                if (self.running_process_count < self.max_concurrent_processes 
+                    and
+                    len(self.processes_waiting_for_execution) != 0): 
+                    # Get the last process
+                    cmd , cwd = self.processes_waiting_for_execution.pop()
+                    # start it
+                    self._start_process(cmd, cwd)
 
+            # If none of the processes return with error status
             if len(collected_exit_status) == 0:
                 collected_exit_status = None
             return collected_exit_status
