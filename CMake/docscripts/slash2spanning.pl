68c159951511c5f682fd5fc2b0d3b7fa8d3e9e7d
amesfoort <amesfoort@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-02-05 15:26:22 +0000

    Task #7430: replace in sources seg@astron.nl by softwaresupport@astron.nl
    Jan David agreed it makes sense, although some software covered is not
    currently maintained by the RO software team.
    All replacements happen in comments, so this is safe. (viewed svn diff | grep ... by hand)
    Command used:
    LOFAR$ grep -r -l seg@astron.nl * | xargs sed -i -e 's/seg@astron.nl/softwaresupport@astron.nl/g'

--- CMake/docscripts/slash2spanning.pl
+++ CMake/docscripts/slash2spanning.pl
@@ -6,7 +6,7 @@
 #
 #  Copyright (C) 2002
 #  ASTRON (Netherlands Foundation for Research in Astronomy)
-#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#  P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, softwaresupport@astron.nl
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
