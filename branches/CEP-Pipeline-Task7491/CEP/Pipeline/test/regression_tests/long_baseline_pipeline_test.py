74ccf7419b711895454a42bd4c1131dee84debb5
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-03-11 14:47:26 +0000

    Task #7491: Use correct comparison for longbaseline. Add some comment in the target pipeline test. Why choices were made

--- branches/CEP-Pipeline-Task7491/CEP/Pipeline/test/regression_tests/long_baseline_pipeline_test.py
+++ branches/CEP-Pipeline-Task7491/CEP/Pipeline/test/regression_tests/long_baseline_pipeline_test.py
@@ -8,12 +8,12 @@ def load_and_compare_data_sets(ms1, ms2):
     ms2 = pt.table(ms2)
 
     #get the amount of rows in the dataset
-    n_row = len(ms1.getcol('DATA'))
+    n_row = len(ms1.getcol('CORRECTED_DATA'))
     n_complex_vis = 4
 
     # create a target array with the same length as the datacolumn
     div_array = numpy.zeros((n_row, 1, n_complex_vis), dtype=numpy.complex64)
-    ms1_array = ms1.getcol('DATA')
+    ms1_array = ms1.getcol('CORRECTED_DATA')
     ms2_array = ms2.getcol('CORRECTED_DATA')
 
     div_max = 0
