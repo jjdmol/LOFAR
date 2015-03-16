33a252e31319ce290dd3cbdaf930eee188ac024c
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-03-16 11:52:11 +0000

    Task #7569: Created working branch

---
+++ branches/LOFAR-Task7569/SAS/Scheduler/src/DataTreeWidgetItem.cpp
@@ -0,0 +1,26 @@
+/*
+ * qtreewidget.cpp
+ *
+ * Author         : Alwin de Jong
+ * e-mail         : jong@astron.nl
+ * Revision       : $Revision$
+ * Last change by : $Author$
+ * Change date	  : $Date$
+ * First creation : Aug 23, 2012
+ * URL            : $URL: https://svn.astron.nl/ROD/trunk/LOFAR_Scheduler/DataTreeWidgetItem.cpp $
+ *
+ */
+
+#include "DataTreeWidgetItem.h"
+
+DataTreeWidgetItem::DataTreeWidgetItem(QTreeWidgetItem *base)
+   : QTreeWidgetItem(base)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+DataTreeWidgetItem::~DataTreeWidgetItem() {
+	// TODO Auto-generated destructor stub
+}
+
