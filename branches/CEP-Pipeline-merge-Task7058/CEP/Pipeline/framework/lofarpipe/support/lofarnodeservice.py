d629b38013319199e033a07a1db724ae32497882
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-03-25 13:04:37 +0000

    Task #7058: Add skeleton for the lofarnodeservice

---
+++ branches/CEP-Pipeline-merge-Task7058/CEP/Pipeline/framework/lofarpipe/support/lofarnodeservice.py
@@ -0,0 +1,25 @@
+#                                                       LOFAR PIPELINE FRAMEWORK
+#
+#                                                         Node recipe service
+#                                                         Wouter Klijn
+#                                                      klijn@astron.nl
+# ------------------------------------------------------------------------------
+
+
+#!/usr/bin/python
+from lofar.messagebus.msgbus import FromBus, ToBus
+from lofar.messagebus.message import Message
+
+# Send a message
+tbus = ToBus("test")
+tmsg = Message()
+tmsg.payload = "foo"
+tbus.send(tmsg)
+
+# Receive it
+fbus = FromBus("test")
+fmsg = fbus.get(1)
+assert fmsg != None
+
+# Verify the content
+assert fmsg.payload == "foo"
