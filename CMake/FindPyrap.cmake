671de0009f98e20b1030e950d8111bf603d5b8ca
Tammo Jan Dijkema <tammojan@users.noreply.github.com>
2015-04-16 12:03:15 +0000

    Task #7353: fix FindPyrap.cmake for new python-casacore

--- CMake/FindPyrap.cmake
+++ CMake/FindPyrap.cmake
@@ -36,24 +36,30 @@ if(NOT PYRAP_FOUND)
   # First try to find pyrap
   find_path(PYRAP_INCLUDE_DIR pyrap/Converters.h
     HINTS ${PYRAP_ROOT_DIR} PATH_SUFFIXES include)
-  find_library(PYRAP_LIBRARY pyrap
-    HINTS ${PYRAP_ROOT_DIR} PATH_SUFFIXES lib)
-  mark_as_advanced(PYRAP_INCLUDE_DIR PYRAP_LIBRARY)
 
-  # Pyrap also depends on Casacore
-  include(LofarFindPackage)
-  if(Pyrap_FIND_REQUIRED)
-    lofar_find_package(Casacore REQUIRED COMPONENTS casa)
-  else(Pyrap_FIND_REQUIRED)
-    lofar_find_package(Casacore COMPONENTS casa)
-  endif(Pyrap_FIND_REQUIRED)
+  if (PYRAP_INCLUDE_DIR)
+  
+    # Found old pyrap
+    find_library(PYRAP_LIBRARY pyrap
+      HINTS ${PYRAP_ROOT_DIR} PATH_SUFFIXES lib)
+    mark_as_advanced(PYRAP_INCLUDE_DIR PYRAP_LIBRARY)
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(Pyrap DEFAULT_MSG 
-    PYRAP_LIBRARY PYRAP_INCLUDE_DIR)
+    # Pyrap also depends on Casacore
+    include(LofarFindPackage)
+    if(Pyrap_FIND_REQUIRED)
+      lofar_find_package(Casacore REQUIRED COMPONENTS casa)
+    else(Pyrap_FIND_REQUIRED)
+      lofar_find_package(Casacore COMPONENTS casa)
+    endif(Pyrap_FIND_REQUIRED)
 
-  set(PYRAP_INCLUDE_DIRS ${PYRAP_INCLUDE_DIR} ${CASACORE_INCLUDE_DIRS})
-  set(PYRAP_LIBRARIES ${PYRAP_LIBRARY} ${CASACORE_LIBRARIES})
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Pyrap DEFAULT_MSG 
+      PYRAP_LIBRARY PYRAP_INCLUDE_DIR)
+
+    set(PYRAP_INCLUDE_DIRS ${PYRAP_INCLUDE_DIR} ${CASACORE_INCLUDE_DIRS})
+    set(PYRAP_LIBRARIES ${PYRAP_LIBRARY} ${CASACORE_LIBRARIES})
+
+  endif(PYRAP_INCLUDE_DIR)
 
 endif(NOT PYRAP_FOUND)
 
@@ -62,18 +68,17 @@ if(NOT PYRAP_FOUND)
 
   find_path(PYCASACORE_INCLUDE_DIR casacore/python/Converters.h
     HINTS ${CASACORE_ROOT_DIR} PATH_SUFFIXES include)
-  if (PYCASACORE_INCLUDE_DIR)
 
-    # Found python-casacore.
-    mark_as_advanced(PYCASACORE_INCLUDE_DIR)
-    include(LofarFindPackage)
-    lofar_find_package(Casacore REQUIRED COMPONENTS casa python)
+  # Found python-casacore.
+  mark_as_advanced(PYCASACORE_INCLUDE_DIR)
+  include(LofarFindPackage)
+  lofar_find_package(Casacore REQUIRED COMPONENTS casa python)
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(Pyrap DEFAULT_MSG 
-      PYCASACORE_INCLUDE_DIR)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Pyrap DEFAULT_MSG 
+    PYCASACORE_INCLUDE_DIR)
 
-    set(PYRAP_INCLUDE_DIRS ${CASACORE_INCLUDE_DIRS})
-    set(PYRAP_LIBRARIES ${CASACORE_LIBRARIES})
-  endif()
+  set(PYRAP_INCLUDE_DIRS ${CASACORE_INCLUDE_DIRS})
+  set(PYRAP_LIBRARIES ${CASACORE_LIBRARIES})
+
 endif(NOT PYRAP_FOUND)
