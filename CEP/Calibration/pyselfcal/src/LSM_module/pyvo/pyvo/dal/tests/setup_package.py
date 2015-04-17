04312894742edf38617e95ca475ed0d63d9b648e
Tammo Jan Dijkema <tammojan@users.noreply.github.com>
2015-04-17 07:31:47 +0000

    Task #5678: revert changes (r31500, r31499, r31497) that broke trunk

--- CEP/Calibration/pyselfcal/src/LSM_module/pyvo/pyvo/dal/tests/setup_package.py
+++
@@ -1,6 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-import os
-
-def get_package_data():
-    paths = [ os.path.join('data', '*.xml') ]
-    return { 'pyvo.dal.tests': paths}
