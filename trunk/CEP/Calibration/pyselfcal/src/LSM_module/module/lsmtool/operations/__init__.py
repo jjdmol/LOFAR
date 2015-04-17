db2cee163f954ecec136a882be94836f1e811e92
dijkema <dijkema@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-04-17 07:31:47 +0000

    Task #5678: revert changes (r31500, r31499, r31497) that broke trunk

--- trunk/CEP/Calibration/pyselfcal/src/LSM_module/module/lsmtool/operations/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import glob
-
-__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py") if not f.endswith('__init__.py') and not f.split('/')[-1].startswith('_')]
-for x in __all__: __import__(x, locals(), globals())
