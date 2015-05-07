8d3d61a1820444709d4b2ab653b98128897bd709
loose <loose@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-05-07 10:20:40 +0000

    Task #7855: Created task branch

---
+++ branches/CMake-Task7855/CEP/Calibration/pyselfcal/src/class_obsparMergedData.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+########################################################################
+#                                                                      #
+# Created by N.Vilchez (vilchez@astron.nl)                             #
+# 14/11/2014                                                           #
+#                                                                      #
+########################################################################
+
+
+########################################################################
+# IMPORT general modules
+########################################################################
+
+import sys, glob, os
+import pyrap.tables as pt
+import fpformat
+import numpy as np
+
+########################################################################
+# Define observation directory parameters (NbTimechunk, NbSB etc ...)
+########################################################################
+
+
+
+
+class observationMergedDataParam:
+
+    def __init__(self,obsDir,UVmin):
+    
+	self.obsDir		= obsDir
+	self.UVmin		= UVmin
+	
+
+ 
+ 
+	####################################################################
+	# Observation parameter for merged data
+	####################################################################	
+	
+    def obsParamMergedDataFunc(self):
+    
+
+		# generate the list of files
+		listFiles	= sorted(glob.glob(self.obsDir+'*'))
+		NbFiles		= len(listFiles)
+		
+		if NbFiles == 1:
+			k			= range(NbFiles)
+		else:
+			k			= range(NbFiles-1)			
+		
+		
+		k1			= range(NbFiles)		
+		Files		= range(NbFiles)	
+		 
+		i = 0 
+		  
+		for line in listFiles:
+			
+				splinter1		= self.obsDir
+				linesplit		= line.split(splinter1)
+				Files[i]		= linesplit[1]				
+				i = i+1			
+	
+		
+		
+					
+		# check about the number of channels of the data
+		for i in k: 
+
+			tab00	= pt.table(listFiles[i]+'/SPECTRAL_WINDOW')
+			freq0		= tab00.getcell('REF_FREQUENCY',0)
+			nbchan0		= tab00.getcell('NUM_CHAN',0)
+			
+			if NbFiles == 1:			
+				freq1		= freq0
+				nbchan1		= nbchan0
+			else:
+				tab01	= pt.table(listFiles[i+1]+'/SPECTRAL_WINDOW')			
+				freq1		= tab01.getcell('REF_FREQUENCY',0)
+				nbchan1		= tab01.getcell('NUM_CHAN',0)				
+
+			
+			
+			if freq0 != freq1:
+				
+				print ''
+				print '#################' 
+				print 'Time chunk %s and %s have not the same reference frequency! \n%s MHz and %s MHz  respectively'%(Files[i],Files[i+1],freq0/1E6,freq1/1E6)
+				print '#################'
+				print ''
+				sys.exit(2)
+				
+			if nbchan0 != nbchan1:
+				print ''
+				print '#################' 
+				print 'Time chunk %s and %s have not the same number of channels! \n%s and %s channels respectively'%(Files[i],Files[i+1],nbchan0,nbchan1)
+				print '#################'
+				print ''
+				sys.exit(2)				
+		
+
+		# Determine observation parameters: time for One time chunk, integration time during the observation, UVmin to apply if under +35 degree
+		tabstart0	= pt.table(listFiles[0]+'/OBSERVATION')
+		start0		= tabstart0.getcell('LOFAR_OBSERVATION_START',0)
+		end0		= tabstart0.getcell('LOFAR_OBSERVATION_END',0)
+		trange		= tabstart0.getcell('TIME_RANGE',0)
+		
+		
+		tabtime		= pt.table(listFiles[0])
+		tinterval	= tabtime.getcell('INTERVAL',0)		
+		
+		tabtarget	= pt.table(listFiles[0]+'/FIELD')
+		coords		= tabtarget.getcell('REFERENCE_DIR',0)
+		target		= coords[0]*180./3.14
+		
+		
+		print self.UVmin
+		
+		if self.UVmin == 'none':
+			if target[1] <= 35:
+				self.UVmin=0.1
+			else:
+				self.UVmin=0			
+		    
+		ra_target	= target[0]+360.
+		dec_target	= target[1]
+		
+		
+		# determine number of channels and the reference frequency, integtime for a time chunk and observation integration time
+		frequency				= freq1
+		nbChan					= nbchan1
+		integTimeOnechunk		= end0-start0
+		observationIntegTime	= tinterval
+			
+
+		# Determine the longuest baseline of the data set 
+		dist = range(NbFiles) 			
+		for i in k1: 
+				tab0 	= pt.table(listFiles[i], readonly=False, ack=True)
+				pos0	= tab0.getcol('UVW')
+				flag0	= tab0.getcol('FLAG_ROW')
+				indexes	= np.where(flag0[:] == 0)[0]
+				
+				dist[i] = max(pos0[indexes,0]**2+pos0[indexes,1]**2)**0.5 	
+				
+				
+		maxBaseline	= max(dist)
+
+
+		print 'List Of Files: %s\n'%(listFiles)
+		print ''	
+		print 'Number of subbands (1 channel/subband): %s\n'%(nbChan)
+		print ''
+		print 'Averange Frequency: %s MHz\n'%(fpformat.fix(frequency/1E6,3))
+		print ''
+		print 'Longuest baseline in the data: %s meters\n'%(maxBaseline) 
+		print ''
+		print 'Target coordinates: ra=%s deg, dec=%s deg'%(ra_target,dec_target)
+		print ''
+		
+		
+		
+		
+		
+		return 	listFiles,Files,NbFiles,nbChan,frequency,maxBaseline,integTimeOnechunk,observationIntegTime,self.UVmin,ra_target,dec_target
+					
+								
+	
+	
+	
+	
+	
+	
+
+ 
+ 
