42e0bb390cc2ac6506ee0c063d020d1cf97cfda1
dijkema <dijkema@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-05-12 10:03:03 +0000

    Task #31594: get some patch version system in place

---
+++ branches/LOFAR-AWImager2-Task7767/CEP/Imager/AWImager2/casapatches/newestpatch.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+import glob, os
+import re
+
+def findnewestpatch(rev, branch='trunk', patchdir='.'):
+  os.chdir(patchdir)
+  patches=[]
+  for file in glob.glob("*.patch"):
+    m = re.search('r([0-9]+)', file)
+    if m:
+      patches+=[int(m.group(1))]
+
+  patches=[patchrev for patchrev in patches if patchrev<=rev]
+  patches=sorted(patches)
+
+  return patches[-1]
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description = "Find most recent patch for a casa repository")
+  parser.add_argument("rev", help="Revision you want to patch", type=int)
+  parser.add_argument("-b", "--branch", help="Branch you want to patch (default is trunk)", default="trunk")
+  parser.add_argument("-p", "--patchdir", help="Directory containing patches (default is .)", default=".")
+
+  args = parser.parse_args()
+
+  patchname=findnewestpatch(args.rev, branch=args.branch, patchdir=args.patchdir)
+  print patchname
