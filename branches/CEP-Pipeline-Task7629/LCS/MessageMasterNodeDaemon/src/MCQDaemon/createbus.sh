da7cd55af354c5c422d175c7ce795cb02b784125
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-06-02 13:12:24 +0000

    Task #7629: Refactoring of add bus structure

--- branches/CEP-Pipeline-Task7629/LCS/MessageMasterNodeDaemon/src/MCQDaemon/createbus.sh
+++ branches/CEP-Pipeline-Task7629/LCS/MessageMasterNodeDaemon/src/MCQDaemon/createbus.sh
@@ -1,71 +1,76 @@
-#!/bin/bash
+#!/bin/sh 
 
-
-
-
-
-function createbus {
-   TEMPFILE=`tempfile`
-   HOSTNAME=`hostname`
-   if (( $# >= 2 ))
-   then
-     BUSNAME=$1
-     REMOTEHOST=$2
-     if (( $# >2 ))
-     then 
-       HOSTNAME=$3
-     fi
-     ERRORSEEN=0
-     # setup exchanges
-     echo "qpid-config -b $HOSTNAME add exchange topic $BUSNAME\".proxy.deadletter\" --durable" >> $TEMPFILE
-     qpid-config -b $HOSTNAME add exchange topic $BUSNAME".proxy.deadletter" --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $HOSTNAME add exchange direct $BUSNAME --durable --alternate-exchange=$BUSNAME\".proxy.deadletter\"" >> $TEMPFILE
-     qpid-config -b $HOSTNAME add exchange direct $BUSNAME --durable --alternate-exchange=$BUSNAME".proxy.deadletter" >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $REMOTEHOST add exchange topic $BUSNAME\".proxy.deadletter\" --durable" >> $TEMPFILE
-     qpid-config -b $REMOTEHOST add exchange topic $BUSNAME".proxy.deadletter" --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $REMOTEHOST add exchange direct $BUSNAME --durable --alternate-exchange=$BUSNAME\".proxy.deadletter\"" >> $TEMPFILE
-     qpid-config -b $REMOTEHOST add exchange direct $BUSNAME --durable --alternate-exchange=$BUSNAME".proxy.deadletter" >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     #setup deadletter queues
-     echo "qpid-config -b $HOSTNAME add queue $BUSNAME\".deadletter\" --durable" >> $TEMPFILE
-     qpid-config -b $HOSTNAME add queue $BUSNAME".deadletter" --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $HOSTNAME bind $BUSNAME\".proxy.deadletter\" $BUSNAME\".deadletter\" '*' --durable" >> $TEMPFILE
-     qpid-config -b $HOSTNAME bind $BUSNAME".proxy.deadletter" $BUSNAME".deadletter" '*' --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $REMOTEHOST add queue $BUSNAME\".deadletter\" --durable" >> $TEMPFILE
-     qpid-config -b $REMOTEHOST add queue $BUSNAME".deadletter" --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-config -b $REMOTEHOST bind $BUSNAME\".proxy.deadletter\" $BUSNAME\".deadletter\" '*' --durable" >> $TEMPFILE
-     qpid-config -b $REMOTEHOST bind $BUSNAME".proxy.deadletter" $BUSNAME".deadletter" '*' --durable >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     #setup routing
-     echo "qpid-route -d dynamic add $REMOTEHOST $HOSTNAME $BUSNAME" >> $TEMPFILE
-     qpid-route -d dynamic add $REMOTEHOST $HOSTNAME $BUSNAME >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     echo "qpid-route -d dynamic add $HOSTNAME $REMOTEHOST $BUSNAME" >> $TEMPFILE
-     qpid-route -d dynamic add $HOSTNAME $REMOTEHOST $BUSNAME >> $TEMPFILE 2>&1
-     (( $? >0 )) && ERRORSEEN=1
-
-     (( $ERRORSEEN >0 )) && echo "ERROR during setup, check "$TEMPFILE" for errors." 
-     (( $ERRORSEEN == 0 )) && rm $TEMPFILE
-   else
-     echo " 'createbus' creates a dynamic routing bus between two nodes." 
-     echo "Usage: createbus <busname> <remotenode>" 
-     echo "   or: createbus <busname> <node1> <node2>" 
-     echo " If only one hostname is given the second hostname is presumed to be the local node." 
-     echo " to ensure one-time delivery of messages avoid loops in the bus topology." 
-   fi
+function usage {
+    echo "'createbus' creates a dynamic routing bus between two nodes." 
+    echo "   createbus <busname> <node1> <node2>" 
+	echo ""
+    echo "If only one hostname is given the second hostname is presumed to be the local node." 
+    echo "to ensure one-time delivery of messages avoid loops in the bus topology." 
+	echo ""
 }
+
+function add_deadletter_topic {
+    local hostname=$1
+	local busname=$2
+    echo "qpid-config -b $hostname add exchange topic $busname\".proxy.deadletter\" --durable" 
+    qpid-config -b $hostname add exchange topic $busname".proxy.deadletter" --durable 
+}
+
+function add_deadletter_alternate_exchange {
+    local hostname=$1
+    local busname=$2
+    echo "qpid-config -b $hostname add exchange direct $busname --durable --alternate-exchange=$busname\".proxy.deadletter\"" 
+    qpid-config -b $hostname add exchange direct $busname --durable --alternate-exchange=$busname".proxy.deadletter" 
+}
+
+function add_deadletter_queue {
+    local hostname=$1
+    local busname=$2
+    echo "qpid-config -b $hostname add queue $busname\".deadletter\" --durable" 
+    qpid-config -b $hostname add queue $busname".deadletter" --durable 
+}
+
+function bind_deadletter_topic_to_queue {
+    local hostname=$1
+    local busname=$2
+    echo "qpid-config -b $hostname bind $busname\".proxy.deadletter\" $busname\".deadletter\" '*' --durable" 
+    qpid-config -b $hostname bind $busname".proxy.deadletter" $busname".deadletter" '*' --durable 
+}
+
+function add_routing {
+    local hostname=$1
+	local remotehost=$2
+    local busname=$3
+    echo "qpid-route -d dynamic add $remotehost $hostname $busname" 
+    qpid-route -d dynamic add $remotehost $hostname $busname 
+}
+
+# createbus attempts to create a full instantiated bus structure between two nodes
+function createbus {
+    busname=$1
+    remotehost=$2
+    hostname=$3
+		
+	add_deadletter_topic $hostname $busname 
+	add_deadletter_topic $remotehost $busname 
+
+    add_deadletter_alternate_exchange $hostname $busname 
+	add_deadletter_alternate_exchange $remotehost $busname 
+
+    add_deadletter_queue $hostname $busname 
+	add_deadletter_queue $remotehost $busname 
+
+	bind_deadletter_topic_to_queue $hostname $busname 
+	bind_deadletter_topic_to_queue $remotehost $busname 
+
+	add_routing $remotehost $hostname $busname
+	add_routing $hostname $remotehost $busname 
+}
+set -e
+if [ $# != 3 ]
+then
+    usage
+    return 1
+fi
+
+createbus $1 $2 $3
