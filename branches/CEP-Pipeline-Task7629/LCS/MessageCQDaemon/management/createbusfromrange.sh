71dbe4aa70e575c5908d50e5f16e75f69df117bf
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-07-28 11:14:45 +0000

    Task #7629: Helper script that creates a bus structure from a range

--- branches/CEP-Pipeline-Task7629/LCS/MessageCQDaemon/management/createbusfromrange.sh
+++ branches/CEP-Pipeline-Task7629/LCS/MessageCQDaemon/management/createbusfromrange.sh
@@ -1,20 +1,46 @@
 function create_named_numbered_node_list {
-    if (( $# == 4 ))
+    if (( $# == 3 ))
     then
-        return_value=$1
-        node_root=$2
-        index_start=$3
-        index_end=$4
+        node_root=$1
+        index_start=$2
+        index_end=$3
         #
         #           Use space seperate the instances
-        list=`printf ' locus%03d' {98..99}`      
-        # For bash higher then 4.0 use: locus{001..094}
-        echo $list
+        for i in `seq $index_start $index_end`
+        do
+          printf " %s%03d" "$node_root" "$i"
+        done
     else
-        echo "Usage $FUNCNAME node_name_root index_start index_end (inclusive)"
+        echo "Usage $FUNCNAME <node_name_root> <index_start> <index_end> (ranges are inclusive)"
         exit 1
     fi
 }
 
+function usage {
+    echo "'createbusfromrange' creates or deletes a dynamic routing bus between a range of named nodes."
+    echo " It uses the createbus.sh to perform the actual creation of the bus" 
+    echo ""
+    echo "   createbusfromrange <add|del> <busname> <hubnode> <rootname> <range min> <range max>[ignore]" 
+    echo " eg:"
+    echo "  createbusfromrange add testbus lhn001 locus 1 94  ignore"
+    echo ""
+    echo "If only one hostname is given the second hostname is presumed to be the local node." 
+    echo "to ensure one-time delivery of messages avoid loops in the bus topology." 
+    echo "If ignore is provided errors will be ignored, this could be used to clean partly created"
+    echo "bus structures/setups"
+}
 
-return_value2=`create_named_numbered_node_list return_value2 $1 $2 $3`
+if (( $# < 7  ))
+# validate the number of arguments
+then
+    usage
+    exit 1
+fi
+
+node_list=`create_named_numbered_node_list $4 $5 $6`
+
+my_dir="$(dirname "$0")"
+
+"$my_dir/createbus.sh" $1 $2 $3 $node_list $7
+
+
