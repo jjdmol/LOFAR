1f9cca7c5c799f4c87a852903f7a8ca7af075b99
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-07-29 12:02:57 +0000

    Task #8174: Only test products that are in output list.

--- branches/LOFAR-Release-2_11/SAS/Scheduler/src/taskstorage.cpp
+++ branches/LOFAR-Release-2_11/SAS/Scheduler/src/taskstorage.cpp
@@ -303,28 +303,33 @@ QDataStream& operator>> (QDataStream &in, TaskStorage &storage) {
 bool TaskStorage::getEqualityInputOutputProducts()const
 {
     // Check we have the same number of dataproduct types
-    if (itsInputDataProducts.size() != itsOutputDataProducts.size())
-        return false;
+    //if (itsInputDataProducts.size() != itsOutputDataProducts.size())
+    //    return false;
 
     //loop over the input and output data types
     std::map<dataProductTypes, inputDataProduct >::const_iterator inputTypePair;
     std::map<dataProductTypes, outputDataProduct >::const_iterator outputTypePair;
-    for (inputTypePair = itsInputDataProducts.begin(),
-         outputTypePair = itsOutputDataProducts.begin();
-         inputTypePair != itsInputDataProducts.end();  // length is the same
-         ++inputTypePair, ++outputTypePair )
+    for (outputTypePair = itsOutputDataProducts.begin();
+         outputTypePair != itsOutputDataProducts.end();  // length is the same
+          ++outputTypePair )
     {
+        if(itsInputDataProducts.find(outputTypePair->first) !=
+                itsInputDataProducts.end())
+            continue;
+        inputDataProduct input = itsInputDataProducts.at(outputTypePair->first);
+
+
         // Check if we have the same number of input and output entries
-        if (inputTypePair->second.locations.size() !=
+        if (input.locations.size() !=
             outputTypePair->second.locations.size())
             return false;
 
         // Loop over all the input and output locations
         QStringList::const_iterator inputLoc;
         QStringList::const_iterator outputLoc;
-        for (inputLoc = inputTypePair->second.locations.begin(),
+        for (inputLoc = input.locations.begin(),
              outputLoc = outputTypePair->second.locations.begin();
-             inputLoc != inputTypePair->second.locations.end();
+             inputLoc != input.locations.end();
              ++inputLoc , ++outputLoc)
         {
             //return false if the nodes are not the same
@@ -334,6 +339,8 @@ bool TaskStorage::getEqualityInputOutputProducts()const
     }
     return true;
 }
+
+
 
 void TaskStorage::setInputFileSizes(dataProductTypes dpType, const std::pair<double, unsigned> &inputFileSizes) {
     itsInputDataFiles[dpType] = inputFileSizes;
