9b3d2f77e055c2aa41bd8d45d456dd0313121ac1
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-07-31 08:13:52 +0000

    Task #7629: Green light on the pipelineSCQDaemon test

--- branches/CEP-Pipeline-Task7629/CEP/Pipeline/test/daemons/testPipelineSCQDaemon.sh
+++ branches/CEP-Pipeline-Task7629/CEP/Pipeline/test/daemons/testPipelineSCQDaemon.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 # Create the a bus structure to be used by the test program
-pwd
-
-printenv
-createbus.sh add testPipelineSCQDaemon $(hostname) 
-
-
-
 ./runctest.sh testPipelineSCQDaemon
-
-# ate the a bus structure to be used by the test program
-createbus.sh del testPipelineSCQDaemon $(hostname) ignore
