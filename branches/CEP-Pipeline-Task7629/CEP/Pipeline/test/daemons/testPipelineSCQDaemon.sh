5089db0199fabd105f0bdb395371b6fb50c05256
klijn <klijn@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-07-31 08:06:16 +0000

    Task #7629: Attempt to fix the test on cep2

--- branches/CEP-Pipeline-Task7629/CEP/Pipeline/test/daemons/testPipelineSCQDaemon.sh
+++ branches/CEP-Pipeline-Task7629/CEP/Pipeline/test/daemons/testPipelineSCQDaemon.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Create the a bus structure to be used by the test program
+pwd
+
+printenv
 createbus.sh add testPipelineSCQDaemon $(hostname) 
+
+
 
 ./runctest.sh testPipelineSCQDaemon
 
