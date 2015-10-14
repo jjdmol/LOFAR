3c2acafe70febe6fb3c41ebd0973d1adc659636e
peterzon <peterzon@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-10-14 09:06:34 +0000

    Task #8571: Initial import of messaging python libraries based on work from Marcel Loose for APERTIF.

---
+++ branches/LOFAR-Messaging-Task8571/LCS/MessageBus/python/messaging/RPC.py
@@ -0,0 +1,37 @@
+
+#  RPC invocation with possible timeout
+from messaging.messagebus import ToBus,FromBus
+from messaging.messages import ServiceMessage
+
+import uuid
+
+
+class RPC():
+  def __init__(self,bus,service,timeout=None):
+     self.timeout=timeout
+     self.BusName=bus
+     self.ServiceName=service
+     self.Request = ToBus(self.BusName+"/"+self.ServiceName,
+            options={"create":"always", "link":"{x-declare: {arguments:{ \"qpid.default_mandatory_topic\": True}}}"})
+     self.ReplyAddress="reply."+str(uuid.uuid4())
+     self.Reply = FromBus(self.BusName+"/"+self.ReplyAddress)
+
+  def __enter__(self):
+     self.Request.open()
+     self.Reply.open()
+     return self
+  
+  def __exit__(self, exc_type, exc_val, exc_tb):
+     self.Request.close()
+     self.Reply.close()
+
+  def __call__(self,msg,timeout=-1):
+     if (timeout==-1):
+       timeout=self.timeout
+     MyMsg=ServiceMessage(msg)
+     MyMsg.reply_to=self.ReplyAddress
+     self.Request.send(MyMsg) #ServiceMessage(MyMsg) #msg,reply_to=self.ReplyAddress))
+     answer=self.Reply.receive(timeout)
+     if (answer!=None):
+        answer=answer.content
+     return answer
---
+++ branches/LOFAR-Messaging-Task8571/LCS/MessageBus/python/messaging/Service.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+
+from messaging.messagebus import ToBus,FromBus
+from messaging.messages import EventMessage
+import threading
+import time
+import uuid
+
+# create service:
+class Service():
+
+   def __init__(self,BusName,ServiceName,ServiceHandler,exclusive=True,numthreads=1):
+      self.BusName=BusName
+      self.ServiceName=ServiceName
+      self.ServiceHandler=ServiceHandler
+      self.connected=False
+      self.running=False
+      self.exclusive=exclusive
+      self.link_uuid=str(uuid.uuid4())
+      self._numthreads=numthreads
+      if (self.BusName!=None):
+         self.Listen=FromBus(self.BusName+"/"+self.ServiceName,options={"link":"{name:two, x-bindings:[{key:" + self.ServiceName + ", arguments: {\"qpid.exclusive-binding\":True}}]}","capacity":numthreads*20})
+         self.Reply=ToBus(self.BusName)
+      else:
+         self.Listen=FromBus(self.ServiceName)
+         self.Reply=self.replyto
+ 
+   def StartListening(self,numthreads=None):
+      if (numthreads!=None):
+         self._numthreads=numthreads
+      numthreads=self._numthreads
+      self.connected=True
+      self.running=True
+      self._tr=[]
+      self.counter=[]
+     #with self.Listen:
+      #  with self.Reply:
+      for i in range(numthreads):
+             self._tr.append(threading.Thread(target=self.loop,args=[i]))
+             self.counter.append(0)
+             self._tr[i].start()
+   
+   def __enter__(self):
+      self.Listen.open()
+      self.Reply.open()
+
+   def __exit__(self, exc_type, exc_val, exc_tb):
+      self.Listen.close()
+      self.Reply.close()
+
+   def loop(self,index):
+      print( "Listening for messages on Bus %s and service name %s." %(self.BusName,self.ServiceName)) 
+      #with self.Listen,self.Reply:
+      while self.running:
+         msg=None
+         try:
+           # get the next message
+           msg=self.Listen.receive(1)
+         except Exception as e:
+           print e
+
+ 
+         if (msg!=None):
+             self.counter[index]+=1
+             #print "got a message"
+             # create a reply message using the ToUpper conversion
+             replymessage=""
+             try:
+                replymessage=self.ServiceHandler(msg.content)
+             except Exception as e:
+                print e
+                replymessage=str(e)
+             # ensure to deliver at the destination in the reply_to field
+             # send the result to the RPC client
+             ToSend=EventMessage(replymessage)
+             ToSend.subject=msg.reply_to
+             msg.show()
+             ToSend.show()
+             self.Reply.send(ToSend)
+             self.Listen.ack(msg)
+         
+      print( "STOPPED Listening for messages on Bus %s and service name %s and %d processed." %(self.BusName,self.ServiceName,self.counter[index]))
+
+   def StopListening(self):
+      if (self.running):
+           self.running=False
+           for i in range(self._numthreads):
+             self._tr[i].join()
+      if (self.connected):
+           self.connected=False
+           self.Listen.close()
+           self.Reply.close()
+
+   def WaitForInterrupt(self):
+      looping=True
+      while looping:
+         try:
+           time.sleep(100)
+         except (KeyboardInterrupt): 
+           looping=False
+           print("Keyboard interrupt received.")
+
