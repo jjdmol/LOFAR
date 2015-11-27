998cf57dd67181c433dfe1fbd9c54d2e77d8c601
renting <renting@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-11-27 12:39:29 +0000

    Task #8550: adding XML Generator to LOFAR repo

---
+++ branches/CEP4-DevelopClusterModel-Story-Task8432/SAS/XML_generator/test/test_regression.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+import sys, os, subprocess
+
+# diff should only be something like:
+# 3,5c3,5
+# <           <version>2.10.3</version>
+# <           <template version="2.10.3" author="Alwin de Jong" changedBy="Alwin de Jong">
+# <           <description>XML Template generator version 2.10.3</description>
+# ---
+# >   <version>2.12.0</version>
+# >   <template version="2.12.0" author="Alwin de Jong,Adriaan Renting" changedBy="Adriaan Renting">
+# >   <description>XML Template generator version 2.12.0</description>
+def checkDiff(diff):
+  if len(diff) > 8:
+    return False
+  return True
+
+def main():
+  infiles = os.listdir("txt")
+  results = []
+  for infile in infiles:
+    if infile.startswith("old"):
+      continue # pre 2.6 files that no longer have valid syntax
+    name, ext = os.path.splitext(infile)
+    outfile = name + ".xml"
+    print "\n"
+    print "*** Processing %s ***" % infile
+    cmd  = ["../src/xmlgen.py", "-i", "./txt/%s" % infile, "-o", "test.xml"]
+    p    = subprocess.Popen(cmd, stdin=open('/dev/null'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logs = p.communicate()[0].splitlines()  #stdout
+    print "xmlgen ran with return code: %s" % p.returncode
+    xmlgen = p.returncode
+    if p.returncode:
+      for l in logs: print l
+      results.append((infile, xmlgen, -1, False))
+      continue
+    else:
+      cmd  = ["diff", "-w", "xml/%s.xml" % name, "test.xml"] ## -w ignores differences in whitespace
+      p    = subprocess.Popen(cmd, stdin=open('/dev/null'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      logs = p.communicate()[0].splitlines() #stdout
+      print "diff reply was %i lines long" % len(logs)
+      check = checkDiff(logs)
+      if not check:
+        for l in logs: print l
+      results.append((infile, xmlgen, p.returncode, check))
+    os.remove("test.xml")
+  print "\nResults:"
+  success = True
+  for r in results:
+    print "%s: xmlgen: %i diff: %i, %s" % r
+    success = success and r[3]
+  if success:
+    print "success"
+    return 0
+  else:
+    print "failure"
+    return 1
+
+if __name__ == "__main__":
+  sys.exit(main())
