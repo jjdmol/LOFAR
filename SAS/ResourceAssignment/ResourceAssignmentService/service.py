c68e6120c65c81a35b3de1e84cc260c3b4405c63
schaap <schaap@astron.nl>
2015-12-01 15:21:22 +0000

    Task #8582: implemented service as wrapper around radb. Use one MessageHandlerInterface which maps all service calls to handler methods

--- SAS/ResourceAssignment/ResourceAssignmentService/service.py
+++ SAS/ResourceAssignment/ResourceAssignmentService/service.py
@@ -19,15 +19,17 @@ with RPC(busname, 'GetProjectDetails') as getProjectDetails:
 
 '''
 from os import stat
-import sys
 import logging
 from lofar.messaging import Service
 from lofar.messaging.Service import MessageHandlerInterface
 from lofar.common.util import waitForInterrupt
 from lofar.sas.resourceassignment.resourceassignmentservice import radb
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
-logger=logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 class _RADBHandler(MessageHandlerInterface):
     def __init__(self, **kwargs):
@@ -35,23 +37,79 @@ class _RADBHandler(MessageHandlerInterface):
         self.username = kwargs.pop("username", 'resourceassignment')
         self.password = kwargs.pop("password", '')
 
+        self.service2MethodMap = {
+            'RAS.GetResourceClaimStatuses': self._getResourceClaimStatuses,
+            'RAS.GetResourceClaims': self._getResourceClaims,
+            'RAS.GetResourceGroupTypes': self._getResourceGroupTypes,
+            'RAS.GetResourceGroupTypes': self._getResourceGroupTypes,
+            'RAS.GetResourceGroups': self._getResourceGroups,
+            'RAS.GetResourceTypes': self._getResourceTypes,
+            'RAS.GetResources': self._getResources,
+            'RAS.GetTask': self._getTask,
+            'RAS.GetTaskStatuses': self._getTaskStatuses,
+            'RAS.GetTaskTypes': self._getTaskTypes,
+            'RAS.GetTaskTypes': self._getTaskTypes,
+            'RAS.GetTasks': self._getTasks,
+            'RAS.GetUnits': self._getUnits}
+
     def prepare_loop(self):
         self.radb = radb.RADatabase(username=self.username,
                                     password=self.password)
 
-class _GetTaskStatusesHandler(_RADBHandler):
-    def handle_message(self, text):
+    def _getTaskStatuses(self):
         return self.radb.getTaskStatuses()
 
+    def _getTaskTypes(self):
+        return self.radb.getTaskTypes()
+
+    def _getResourceClaimStatuses(self):
+        return self.radb.getResourceClaimStatuses()
+
+    def _getResourceClaims(self):
+        return self.radb.getResourceClaims()
+
+    def _getResourceGroupTypes(self):
+        return self.radb.getResourceGroupTypes()
+
+    def _getResourceGroups(self):
+        return self.radb.getResourceGroups()
+
+    def _getResourceTypes(self):
+        return self.radb.getResourceTypes()
+
+    def _getResources(self):
+        return self.radb.getResources()
+
+    def _getTasks(self):
+        result = self.radb.getTasks()
+        for task in result:
+            print type(task['starttime']), task['starttime']
+        return result
+
+    def _getTask(self, **kwargs):
+        task = self.radb.getTask(kwargs['id'])
+        print type(task), task
+        return task
+
+    def _getUnits(self):
+        return self.radb.getUnits()
 
 
 def main():
-    getTaskStatusesService = Service('GetTaskStatuses',
-                   _GetTaskStatusesHandler,
-                   busname='raservice',
-                   handler_args={'password':'123456'})
+    # make sure config.py is mode 600 to hide passwords
+    if oct(stat('config.py').st_mode & 0777) != '0600':
+        print 'Please change permissions of config.py to 600'
+        exit(-1)
 
-    with getTaskStatusesService:
+    # safely import radb_password
+    from lofar.sas.resourceassignment.resourceassignmentservice.config import radb_password
+
+    service = Service('RAS.*',
+                      _RADBHandler,
+                      busname='raservice',
+                      handler_args={'password': radb_password})
+
+    with service:
         waitForInterrupt()
 
 if __name__ == '__main__':
