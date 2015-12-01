7a00c765a04be5dd89d717a4b6d23f9dd6ce0d7a
schaap <schaap@astron.nl>
2015-12-01 15:22:22 +0000

    Task #8582: implemented rpc module which hides the calls to the service

--- SAS/ResourceAssignment/ResourceAssignmentService/rpc.py
+++ SAS/ResourceAssignment/ResourceAssignmentService/rpc.py
@@ -10,26 +10,97 @@ from lofar.messaging.RPC import RPC
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class RPCException(Exception):
+    def __init__(self, message):
+        self.message = message
 
-def getTaskStatuses(busname='raservice'):
-    with RPC('GetTaskStatuses', busname=busname, timeout=10) as rpc:
-        res, status = rpc()
+    def __str__(self):
+        return "RPCException: " + str(self.message)
 
-        if status != 'OK':
-            logger.error("%s %s" % (status, res))
-            return status
+busname = 'raservice'
 
-        return res
+def _rpc(service, **kwargs):
+    try:
+        rpckwargs = {'timeout': 10}
+        with RPC(service, busname=busname, **rpckwargs) as rpc:
+            if kwargs:
+                res, status = rpc(**kwargs)
+            else:
+                res, status = rpc()
+
+            if status != 'OK':
+                message = "%s %s" % (status, res)
+                logger.error(message)
+                raise RPCException(message)
+
+            return res
+    except Exception as e:
+        logger.error(str(e))
+        raise RPCException(str(e))
+
+def getResourceClaimStatuses():
+    return _rpc('RAS.GetResourceClaimStatuses')
+
+def getResourceClaims():
+    return _rpc('RAS.GetResourceClaims')
+
+def getResourceGroupTypes():
+    return _rpc('RAS.GetResourceGroupTypes')
+
+def getResourceGroups():
+    return _rpc('RAS.GetResourceGroups')
+
+def getResourceTypes():
+    return _rpc('RAS.GetResourceTypes')
+
+def getResources():
+    return _rpc('RAS.GetResources')
+
+def getTask(id):
+    task = _rpc('RAS.GetTask', id=id)
+    if task:
+        task['starttime'] = task['starttime'].datetime()
+        task['endtime'] = task['endtime'].datetime()
+    return task
+
+def getTasks():
+    tasks = _rpc('RAS.GetTasks')
+    for task in tasks:
+        task['starttime'] = task['starttime'].datetime()
+        task['endtime'] = task['endtime'].datetime()
+    return tasks
+
+def getTaskTypes():
+    return _rpc('RAS.GetTaskTypes')
+
+def getTaskStatuses():
+    return _rpc('RAS.GetTaskStatuses')
+
+def getUnits():
+    return _rpc('RAS.GetUnits')
 
 
-def main(busname='raservice'):
-    result = getTaskStatuses(busname)
+def main():
+    def resultPrint(method):
+        print '\n-- ' + str(method.__name__) + ' --'
 
-    for obj in result:
-        id = obj['id']
-        logger.info('Object %s' % (id))
-        for k, v in obj.items():
-            logger.info('  %s: %s' % (k, v))
+        for obj in method():
+            print '  Object %s' % (obj['id'] if 'id' in obj else '')
+
+            for k, v in obj.items():
+                print '    %s: %s' % (k, v)
+
+    resultPrint(getTaskStatuses)
+    resultPrint(getTaskTypes)
+    resultPrint(getResourceClaimStatuses)
+    resultPrint(getUnits)
+    resultPrint(getResourceTypes)
+    resultPrint(getResourceGroupTypes)
+    resultPrint(getResources)
+    resultPrint(getResourceGroups)
+    resultPrint(getTasks)
+    resultPrint(getResourceClaims)
+
 
 
 if __name__ == '__main__':
