01a64a9baf9be9aae975871cccd1f0bc57fd2469
schaap <schaap@astron.nl>
2015-12-03 08:22:42 +0000

    Task #8582: moved methods into RARPC class. fixed type of qpid.timestamps to datetimes

--- SAS/ResourceAssignment/ResourceAssignmentService/rpc.py
+++ SAS/ResourceAssignment/ResourceAssignmentService/rpc.py
@@ -1,107 +1,87 @@
 #!/usr/bin/python
 
-import sys
 import logging
 from lofar.messaging.RPC import RPC
 
-''' Simple RPC client for Service momqueryservice.GetProjectDetails
+''' Simple RPC client for Service lofarbus.RAS.*Z
 '''
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-class RPCException(Exception):
+class RARPCException(Exception):
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
-        return "RPCException: " + str(self.message)
-
-busname = 'raservice'
-
-def _rpc(service, **kwargs):
-    try:
-        rpckwargs = {'timeout': 10}
-        with RPC(service, busname=busname, **rpckwargs) as rpc:
-            if kwargs:
-                res, status = rpc(**kwargs)
-            else:
-                res, status = rpc()
-
-            if status != 'OK':
-                message = "%s %s" % (status, res)
-                logger.error(message)
-                raise RPCException(message)
-
-            return res
-    except Exception as e:
-        logger.error(str(e))
-        raise RPCException(str(e))
-
-def getResourceClaimStatuses():
-    return _rpc('RAS.GetResourceClaimStatuses')
-
-def getResourceClaims():
-    return _rpc('RAS.GetResourceClaims')
-
-def getResourceGroupTypes():
-    return _rpc('RAS.GetResourceGroupTypes')
-
-def getResourceGroups():
-    return _rpc('RAS.GetResourceGroups')
-
-def getResourceTypes():
-    return _rpc('RAS.GetResourceTypes')
-
-def getResources():
-    return _rpc('RAS.GetResources')
-
-def getTask(id):
-    task = _rpc('RAS.GetTask', id=id)
-    if task:
-        task['starttime'] = task['starttime'].datetime()
-        task['endtime'] = task['endtime'].datetime()
-    return task
-
-def getTasks():
-    tasks = _rpc('RAS.GetTasks')
-    for task in tasks:
-        task['starttime'] = task['starttime'].datetime()
-        task['endtime'] = task['endtime'].datetime()
-    return tasks
-
-def getTaskTypes():
-    return _rpc('RAS.GetTaskTypes')
-
-def getTaskStatuses():
-    return _rpc('RAS.GetTaskStatuses')
-
-def getUnits():
-    return _rpc('RAS.GetUnits')
+        return "RARPCException: " + str(self.message)
 
 
-def main():
-    def resultPrint(method):
-        print '\n-- ' + str(method.__name__) + ' --'
+class RARPC:
+    def __init__(self, busname='lofarbus'):
+        self.busname = busname
 
-        for obj in method():
-            print '  Object %s' % (obj['id'] if 'id' in obj else '')
+    def _rpc(self, service, timeout=10, **kwargs):
+        try:
+            rpckwargs = {'timeout': timeout}
+            with RPC(service, busname=self.busname, **rpckwargs) as rpc:
+                if kwargs:
+                    res, status = rpc(**kwargs)
+                else:
+                    res, status = rpc()
 
-            for k, v in obj.items():
-                print '    %s: %s' % (k, v)
+                if status != 'OK':
+                    message = "%s %s" % (status, res)
+                    logger.error(message)
+                    raise RARPCException(message)
 
-    resultPrint(getTaskStatuses)
-    resultPrint(getTaskTypes)
-    resultPrint(getResourceClaimStatuses)
-    resultPrint(getUnits)
-    resultPrint(getResourceTypes)
-    resultPrint(getResourceGroupTypes)
-    resultPrint(getResources)
-    resultPrint(getResourceGroups)
-    resultPrint(getTasks)
-    resultPrint(getResourceClaims)
+                return res
+        except Exception as e:
+            logger.error(str(e))
+            raise RARPCException(str(e))
 
+    def getResourceClaimStatuses(self):
+        return self._rpc('RAS.GetResourceClaimStatuses')
 
+    def getResourceClaims(self):
+        claims = self._rpc('RAS.GetResourceClaims')
+        for claim in claims:
+            claim['starttime'] = claim['starttime'].datetime()
+            claim['endtime'] = claim['endtime'].datetime()
+        return claims
 
-if __name__ == '__main__':
-    main()
+    def getResourceGroupTypes(self):
+        return self._rpc('RAS.GetResourceGroupTypes')
+
+    def getResourceGroups(self):
+        return self._rpc('RAS.GetResourceGroups')
+
+    def getResourceTypes(self):
+        return self._rpc('RAS.GetResourceTypes')
+
+    def getResources(self):
+        return self._rpc('RAS.GetResources')
+
+    def getTask(self, id):
+        task = self._rpc('RAS.GetTask', id=id)
+        if task:
+            task['starttime'] = task['starttime'].datetime()
+            task['endtime'] = task['endtime'].datetime()
+        return task
+
+    def getTasks(self):
+        tasks = self._rpc('RAS.GetTasks')
+        for task in tasks:
+            task['starttime'] = task['starttime'].datetime()
+            task['endtime'] = task['endtime'].datetime()
+        return tasks
+
+    def getTaskTypes(self):
+        return self._rpc('RAS.GetTaskTypes')
+
+    def getTaskStatuses(self):
+        return self._rpc('RAS.GetTaskStatuses')
+
+    def getUnits(self):
+        return self._rpc('RAS.GetUnits')
+
