3469c2c4d4fa38856f60d84493be66817b534382
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2015-12-11 14:11:56 +0000

    Task #8870: Created task branch

---
+++ branches/LOFAR-Task8870/MAC/Deployment/data/bin/dummy.cc
@@ -0,0 +1,3 @@
+// Dummy file, used as input for dummy library libdeployment,
+// which is only required for CMake in order to collect all
+// required link libraries when linking binaries executables.
