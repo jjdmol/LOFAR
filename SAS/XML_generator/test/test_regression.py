2c78300d4800ad42052045ea46a0b8ce9ae6cad6
renting <renting@astron.nl>
2016-01-14 17:59:27 +0000

    Task #8432: trying to fix my regression test, might be OSX diff bug

--- SAS/XML_generator/test/test_regression.py
+++ SAS/XML_generator/test/test_regression.py
@@ -11,9 +11,9 @@ import sys, os, subprocess
 # >   <template version="2.12.0" author="Alwin de Jong,Adriaan Renting" changedBy="Adriaan Renting">
 # >   <description>XML Template generator version 2.12.0</description>
 def checkDiff(diff):
-  if len(diff) > 8:
-    return False
-  return True
+  if len(diff) == 8:
+    return True
+  return False
 
 def main():
   infiles = os.listdir("txt")
@@ -35,13 +35,17 @@ def main():
       results.append((infile, xmlgen, -1, False))
       continue
     else:
-      cmd  = ["diff", "-w -B", "xml/%s.xml" % name, "test.xml"] ## -w ignores differences in whitespace and blank lines
-      p    = subprocess.Popen(cmd, stdin=open('/dev/null'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-      logs = p.communicate()[0].splitlines() #stdout
-      print "diff reply was %i lines long" % len(logs)
-      check = checkDiff(logs)
+      cmd   = ["diff", "-w", "-B", "./xml/%s.xml" % name, "test.xml"]
+      ## -w ignores differences in whitespace
+      ## -I '^[[:space:]]*$' because -B doesn't work for blank lines
+      p     = subprocess.Popen(cmd, stdin=open('/dev/null'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      logs  = p.communicate()
+      diffs = logs[0].splitlines() #stdout
+      print "diff reply was %i lines long" % len(diffs)
+      check = checkDiff(diffs)
       if not check:
-        for l in logs: print l
+        for l in diffs: print l
+        print logs[1]
       results.append((infile, xmlgen, p.returncode, check))
     os.remove("test.xml")
   print "\nResults:"
