74a496acc74cfaeaaea8d45277a119ed60d9e187
donker <donker@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-01-20 12:28:21 +0000

    Task #8578: backup

---
+++ branches/ResourceEstimator-Task8578/SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/__init__.py
@@ -0,0 +1,10 @@
+""" resource_estimators __init__()
+"""
+
+from parameterset import Parameterset
+from observation import ObservationResourceEstimator
+from longbaseline_pipeline import LongBaselinePipelineResourceEstimator
+from calibration_pipeline import CalibrationPipelineResourceEstimator
+from pulsar_pipeline import PulsarPipelineResourceEstimator
+from image_pipeline import ImagePipelineResourceEstimator
+
---
+++ branches/ResourceEstimator-Task8578/SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/longbaseline_pipeline.py
@@ -0,0 +1,50 @@
+""" L
+"""
+
+import logging
+from math import ceil
+from base_resource_estimator import BaseResourceEstimator
+
+logger = logging.getLogger(__name__)
+
+
+class LongBaselinePipelineResourceEstimator(BaseResourceEstimator):
+    """ LongBaselinePipelineResourceEstimator
+    """
+    def __init__(self, kwargs, input_files):
+        BaseResourceEstimator.__init__(self, name='longbaseline_pipeline')
+        logger.debug("init LongBaselinePipelineResourceEstimator")
+        self.parset = ParameterSet(kwargs).make_subset('dp.output')
+        self.duration = int(kwargs.get('observation.duration', 1))
+        self.input_files = input_files
+        self.used_keys = ('correlated.enabled', 'longbaseline.subband_groups_per_ms',
+                          'longbaseline.subbands_per_subband_group')
+        if self.check_parset():
+            self.estimate()
+        return
+
+    def estimate(self):
+        """ Estimate for calibration pipeline
+        calculates: datasize (number of files, file size), bandwidth
+        """
+        logger.debug("start estimate '{}'".format(self.name))
+        if 'dp_correlated_uv' in self.input_files:
+            if self.parset['correlated']['enabled'] == 'true':
+                logger.debug("calculate long baseline data size")
+
+                nr_output_files = 0
+                subband_groups_per_ms = int(self.parset['longbaseline']['subband_groups_per_ms'])
+                subbands_per_subband_group = int(self.parset['longbaseline']['subbands_per_subband_group'])
+                if subband_groups_per_ms and subbands_per_subband_group:
+                    nr_input_files = int(self.input_files['dp_correlated_uv']['nr_files'])
+                    if (nr_input_files % (subbands_per_subband_group * subband_groups_per_ms)) == 0:
+                        nr_output_files = nr_input_files / (subbands_per_subband_group * subband_groups_per_ms)
+                        self.output_files['dp_correlated_uv'] = {'nr_files': nr_output_files, 'file_size': 1000}
+                        logger.debug("dp_correlated_uv: {} files {} bytes each".format(nr_output_files, 1000))
+
+            # count total data size
+            for values in self.output_files.itervalues():
+                self.total_data_size += values['nr_files'] * values['file_size']
+            self.total_bandwidth = ceil((self.total_data_size * 8) / self.duration)  # bits/second
+        return
+
