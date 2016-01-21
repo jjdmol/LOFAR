ca08b8432399633e41e417977551e3df5b3aa2ab
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-01-21 20:05:51 +0000

    Task #8888: Added full service test, fixed reveiled bugs. Added correlator parset test.

--- branches/LOFAR-Task8888/SAS/ResourceAssignment/Services/test/tJobsToSchedule.py
+++ branches/LOFAR-Task8888/SAS/ResourceAssignment/Services/test/tJobsToSchedule.py
@@ -3,11 +3,17 @@
 # Be able to find service python file
 import sys, os
 sys.path.insert(0, "{srcdir}/../src".format(**os.environ))
+
 from JobsToSchedule import *
 from lofar.parameterset import PyParameterSet
+from lofar.messaging import EventMessage, Service
 
 import unittest
 from glob import glob
+import uuid
+
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def setUpModule():
   pass
@@ -43,11 +49,79 @@ def parset_as_dict(filename):
 
 
 class TestResourceIndicators(unittest.TestCase):
-  def test_foo(self):
-    parset = parset_as_dict("tJobsToSchedule.in_426942")
+  def test_preprocessing_pipeline(self):
+    parset = parset_as_dict("tJobsToSchedule.in_preprocessing")
+    r = resourceIndicatorsFromParset(parset)
 
-    print resourceIndicatorsFromParset(parset)
+  def test_correlator_observation(self):
+    parset = parset_as_dict("tJobsToSchedule.in_correlator")
+    r = resourceIndicatorsFromParset(parset)
 
+
+class TestService(unittest.TestCase):
+  def test(self):
+    """
+      Request the resources for a simulated obsid 1, with the following predecessor tree:
+
+        1 requires 2, 3
+        2 requires 3
+        3 requires nothing
+    """
+
+    # setup mock parset service
+    def TaskSpecificationService( input_dict ):
+      obsid = input_dict["OtdbID"]
+      print obsid
+
+      if obsid == 1:
+        predecessors = "[2,3]"
+      elif obsid == 2:
+        predecessors = "[3]"
+      elif obsid == 3:
+        predecessors = "[]"
+      else:
+        raise Exception("Invalid obsID")
+
+      return {
+        "Version.number":                                     "1",
+        PARSET_PREFIX + "Observation.ObsID":                  str(obsid),
+        PARSET_PREFIX + "Observation.Scheduler.predecessors": predecessors,
+      }
+
+    # Create a random bus
+    busname = "%s-%s" % (sys.argv[0], uuid.uuid4())
+    with ToBus(busname, options={ "create": "always", "delete": "always", "node": { "type": "topic" }}):
+      status_service = "%s/TaskStatus" % (busname,)
+      parset_service = "%s/TaskSpecification" % (busname,)
+      jts_service    = "%s/TaskSpecified" % (busname,)
+
+      # Setup our fake TaskSpecification server, and start our JobsToSchedule service to test
+      with Service("TaskSpecification", TaskSpecificationService, busname=busname):
+        with JobsToSchedule("TaskSpecified", otdb_busname=busname, my_busname=busname) as jts:
+          # Start listening for answer before we trigger it
+          with FromBus(jts_service) as fb:
+
+            # Send fake status update
+            with ToBus(status_service) as tb:
+              msg = EventMessage(content={
+                "treeID": 1,
+                "state": "prescheduled",
+                "time_of_change": "2016-01-01 00:00:00.00",
+              })
+              tb.send(msg)
+
+            # Wait for answer from service
+            result = fb.receive(1.0)
+            self.assertIsNotNone(result)
+
+            # Verify result
+            self.assertIn("sasID", result.content)
+            self.assertIn("resource_indicators", result.content)
+
+            self.assertEqual(result.content["sasID"], 1)
+            self.assertIn("1", result.content["resource_indicators"])
+            self.assertIn("2", result.content["resource_indicators"])
+            self.assertIn("3", result.content["resource_indicators"])
 
 def main(argv):
   unittest.main(verbosity=2)
