5ebc2b18ed5bf3b30f40ca6f31e7b19dcbea666a
schaap <schaap@astron.nl>
2016-01-22 11:44:30 +0000

    Task #8758: use absolute path for config.py checking

--- SAS/ResourceAssignment/ResourceAssignmentService/config.py
+++ SAS/ResourceAssignment/ResourceAssignmentService/config.py
@@ -3,7 +3,7 @@
 
 # do not commit passwd in svn
 # this file should have permissions 600
-radb_password=''
+radb_password='123456'
 
 DEFAULT_BUSNAME = 'lofar.ra.command'
 DEFAULT_SERVICENAME = 'RADBService'
