1d78d6fe6e0ce66c7a5a94628daf4705da19a8e2
Jan David Mol <mol@astron.nl>
2016-01-27 10:04:34 +0000

    Task #8888: Created BusListener class for JobsToSchedule service

---
+++ SAS/ResourceAssignment/Services/src/RABusListener.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# RABusListener.py: RABusListener listens on the lofar ra message bus and calls (empty) on<SomeMessage> methods when such a message is received.
+#
+# Copyright (C) 2015
+# ASTRON (Netherlands Institute for Radio Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This file is part of the LOFAR software suite.
+# The LOFAR software suite is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# The LOFAR software suite is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
+"""
+RABusListener listens on the lofar otdb message bus and calls (empty) on<SomeMessage> methods when such a message is received.
+Typical usage is to derive your own subclass from RABusListener and implement the specific on<SomeMessage> methods that you are interested in.
+"""
+
+from lofar.messaging.messagebus import AbstractBusListener
+
+import qpid.messaging
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+
+class JobsToScheduleBusListener(AbstractBusListener):
+    def __init__(self, busname='lofar.ra', subject='TaskSpecified', broker=None, **kwargs):
+        """
+        JobsToScheduleBusListener listens on the lofar ra message bus and calls (empty) on<SomeMessage> methods when such a message is received.
+        Typical usage is to derive your own subclass from JobsToScheduleBusListener and implement the specific on<SomeMessage> methods that you are interested in.
+        :param address: valid Qpid address (default: lofar.otdb.status)
+        :param broker: valid Qpid broker host (default: None, which means localhost)
+        additional parameters in kwargs:
+            options=   <dict>  Dictionary of options passed to QPID
+            exclusive= <bool>  Create an exclusive binding so no other services can consume duplicate messages (default: False)
+            numthreads= <int>  Number of parallel threads processing messages (default: 1)
+            verbose=   <bool>  Output extra logging over stdout (default: False)
+        """
+        address = "%s/%s" % (busname, subject)
+        super(JobsToScheduleBusListener, self).__init__(address, broker, **kwargs)
+
+    def _handleMessage(self, msg):
+        logger.debug("RABusListener.handleMessage: %s" %str(msg))
+
+        sasId = msg.content['sasID']
+        modificationTime = datetime.utcnow()
+        if 'time_of_change' in msg.content:
+            try:
+                modificationTime = datetime.strptime(msg.content['time_of_change'], '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError as e:
+                logger.error('could not parse time_of_change %s : %s' % (msg.content['time_of_change'], e))
+
+        resource_indicators = msg.content['resource_indicators']
+
+        self.onMessage(sasId, modificationTime, resource_indicators)
+
+    def onMessage(self, sasId, modificationTime, resourceIndicators):
+        pass
+
+__all__ = ["JobsToScheduleBusListener"]
