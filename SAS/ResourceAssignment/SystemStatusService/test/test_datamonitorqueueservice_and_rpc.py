753ade65cebbe9c22eb4958529c93c28560441a6
peterzon <peterzon@astron.nl>
2016-02-03 15:52:42 +0000

    Task #9026: wrong input parameter hangs ctest

--- SAS/ResourceAssignment/SystemStatusService/test/test_datamonitorqueueservice_and_rpc.py
+++ SAS/ResourceAssignment/SystemStatusService/test/test_datamonitorqueueservice_and_rpc.py
@@ -77,7 +77,7 @@ try:
                 rpc = SSDBrpc(busname=busname)
                 self.assertEqual((retvalues_getstatenames,'OK')      , rpc.getstatenames())
                 self.assertEqual((retvalues_getactivegroupnames,'OK'), rpc.getactivegroupnames())
-                self.assertEqual((retvalues_gethostsforgid,'OK')     , rpc.gethostsforgid(4))
+                self.assertEqual((retvalues_gethostsforgid,'OK')     , rpc.gethostsforgid("4"))
                 self.assertEqual((retvalues_listall,'OK')            , rpc.listall())
                 self.assertEqual((retvalues_countactivehosts,'OK')   , rpc.countactivehosts())
                 self.assertEqual((retvalues_getArchivingStatus,'OK') , rpc.getArchivingStatus())
