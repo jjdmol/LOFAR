49740bd3796c01e2fc0287c33655a1fd6f7d47b6
schaap <schaap@astron.nl>
2016-02-08 13:48:01 +0000

    Task #8887: added config. added optionparser for commandline args. resourceassigner can now actually start

---
+++ SAS/ResourceAssignment/ResourceAssigner/lib/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# $Id$
+
+DEFAULT_BUSNAME = 'lofar.ra.command'
+DEFAULT_SERVICENAME = 'RAService'
+
+# TODO: should try to import config from RATaskSpecified (which is currently non-existent)
+RATASKSPECIFIED_SERVICENAME = 'OTDB.TaskSpecified'
--- SAS/ResourceAssignment/ResourceAssigner/lib/raservice.py
+++ SAS/ResourceAssignment/ResourceAssigner/lib/raservice.py
@@ -14,7 +14,7 @@
 #
 # The LOFAR software suite is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along
@@ -27,114 +27,133 @@ TaskSpecifiedListener listens to a bus on which specified tasks get published. I
 to assign resources to these tasks.
 """
 
-from lofar.messaging.messagebus import AbstractBusListener
-from lofar.messaging.RPC import RPC
-
 import qpid.messaging
 import logging
 from datetime import datetime
 
-import lofar.sas.resourceassignment.rarpc as rarpc
+from lofar.messaging.messagebus import AbstractBusListener
+from lofar.messaging.RPC import RPC
+
+import lofar.sas.resourceassignment.resourceassignmentservice.rpc as rarpc
+from lofar.sas.resourceassignment.resourceassigner.config import DEFAULT_BUSNAME, DEFAULT_SERVICENAME
 
 logger = logging.getLogger(__name__)
 
 
 class SpecifiedTaskListener(AbstractBusListener):
-  def __init__(self, busname='lofar.?.?', subject='?.?', broker=None, **kwargs):
-    """
-    SpecifiedTaskListener listens on the lofar ?? bus and calls onTaskSpecified
-    :param address: valid Qpid address (default: lofar.otdb.status)
-    :param broker: valid Qpid broker host (default: None, which means localhost)
-    additional parameters in kwargs:
-        options=   <dict>  Dictionary of options passed to QPID
-        exclusive= <bool>  Create an exclusive binding so no other services can consume duplicate messages (default: False)
-        numthreads= <int>  Number of parallel threads processing messages (default: 1)
-        verbose=   <bool>  Output extra logging over stdout (default: False)
-    """
-    address = "%s/%s" % (busname, subject)
-    super(OTDBBusListener, self).__init__(address, broker, **kwargs)
+    def __init__(self, busname='lofar.?.?', subject='?.?', broker=None, **kwargs):
+        """
+        SpecifiedTaskListener listens on the lofar ?? bus and calls onTaskSpecified
+        :param address: valid Qpid address (default: lofar.otdb.status)
+        :param broker: valid Qpid broker host (default: None, which means localhost)
+        additional parameters in kwargs:
+                options=     <dict>    Dictionary of options passed to QPID
+                exclusive= <bool>    Create an exclusive binding so no other services can consume duplicate messages (default: False)
+                numthreads= <int>    Number of parallel threads processing messages (default: 1)
+                verbose=     <bool>    Output extra logging over stdout (default: False)
+        """
+        address = "%s/%s" % (busname, subject)
+        super(SpecifiedTaskListener, self).__init__(address, broker, **kwargs)
 
-  def _handleMessage(self, msg):
-    logger.debug("SpecifiedTaskListener.handleMessage: %s" %str(msg))
+    def _handleMessage(self, msg):
+        logger.debug("SpecifiedTaskListener.handleMessage: %s" %str(msg))
 
-    taskId =  msg.content['treeID']
-    modificationTime = datetime.utcnow()
-    if 'time_of_change' in msg.content:
-      try:
-        modificationTime = datetime.strptime(msg.content['time_of_change'], '%Y-%m-%d %H:%M:%S.%f')
-      except ValueError as e:
-        logger.error('could not parse time_of_change %s : %s' % (msg.content['time_of_change'], e))
+        taskId =    msg.content['treeID']
+        modificationTime = datetime.utcnow()
+        if 'time_of_change' in msg.content:
+            try:
+                modificationTime = datetime.strptime(msg.content['time_of_change'], '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError as e:
+                logger.error('could not parse time_of_change %s : %s' % (msg.content['time_of_change'], e))
 
-    if msg.content['specification']:
-      self.onTaskSpecified(treeId, modificationTime, msg.content['specification'])
-    else:
-      logger.error("Task %s not properly received:" % (taskId, ))
+        if msg.content['specification']:
+            self.onTaskSpecified(treeId, modificationTime, msg.content['specification'])
+        else:
+            logger.error("Task %s not properly received:" % (taskId, ))
 
 __all__ = ["SpecifiedTaskListener"]
 
 def parseSpecification(specification):
-  default = "CEP2"
-  cluster ="CEP4"
-  return cluster
+    default = "CEP2"
+    cluster ="CEP4"
+    return cluster
 
 def getNeededResouces(specification):
-  # Used settings
-  ServiceName = "ToUpper"
-  BusName     = "simpletest"
+    # Used settings
+    ServiceName = "ToUpper"
+    BusName         = "simpletest"
  
-  # Initialize a Remote Procedure Call object
-  with RPC(BusName,ServiceName) as remote_fn:
-    replymessage, status = remote_fn("Hello World ToUpper.")
-    print replymessage
-    
+    # Initialize a Remote Procedure Call object
+    with RPC(BusName,ServiceName) as remote_fn:
+        replymessage, status = remote_fn("Hello World ToUpper.")
+        print replymessage
+
 def getAvailableResources(cluster):
-  # Used settings
-  ServiceName = "GetServerState"
-  BusName     = "simpletest"
+    # Used settings
+    ServiceName = "GetServerState"
+    BusName         = "simpletest"
  
-  groupnames = []
-  available  = []
-  with RPC(BusName, ServiceName) as GetServerState:
-    replymessage, status = GetServerState.getactivegroupnames()
-    if not status:
-      groupnames = replymessage
-    else:
-      logger.error("T")
-  if cluster in groupnames.keys():
+    groupnames = []
+    available    = []
     with RPC(BusName, ServiceName) as GetServerState:
-      replymessage, status = GetServerState.gethostsforgid(groupnames[cluster])
-      if not status:
-        available = replymessage
-      else:
+        replymessage, status = GetServerState.getactivegroupnames()
+        if not status:
+            groupnames = replymessage
+        else:
+            logger.error("T")
+    if cluster in groupnames.keys():
+        with RPC(BusName, ServiceName) as GetServerState:
+            replymessage, status = GetServerState.gethostsforgid(groupnames[cluster])
+            if not status:
+                available = replymessage
+            else:
+                logger.error("T")
+    else:
         logger.error("T")
-  else:
-    logger.error("T")
-  return available
+    return available
 
 def checkResources(needed, available):
-  return True
+    return True
 
 def claimResources(needed):
-  rarpc.InsertTask()
+    rarpc.InsertTask()
 
-def commitResources(result.id):
-  pass
+def commitResources(result_id):
+    pass
 
 def onTaskSpecified(treeId, modificationTime, specification):
-  cluster = parseSpecification(specification)
-  needed = getNeededResouces(specification)
-  available = getAvailableResources(cluster)
-  if checkResources(needed, available):
-    result = claimResources(needed)
-    if result.success:
-      commitResources(result.id)
-      SetTaskToSCHEDULED(Task.)
-    else:
-      SetTaskToCONFLICT(Task.)
+    cluster = parseSpecification(specification)
+    needed = getNeededResouces(specification)
+    available = getAvailableResources(cluster)
+    if checkResources(needed, available):
+        result = claimResources(needed)
+        if result.success:
+            commitResources(result.id)
+            #SetTaskToSCHEDULED(Task.)
+        else:
+            #SetTaskToCONFLICT(Task.)
+            pass
 
 def main():
-  print "SpecifiedTaskListener started"
-  
-  service = SpecifiedTaskListener()
-  service.startListening()
+    from optparse import OptionParser
+    from lofar.messaging import setQpidLogLevel
+    from lofar.common.util import waitForInterrupt
 
+    # Check the invocation arguments
+    parser = OptionParser("%prog [options]",
+                          description='runs the resourceassigner service')
+    parser.add_option('-q', '--broker', dest='broker', type='string', default=None, help='Address of the qpid broker, default: localhost')
+    parser.add_option("-b", "--busname", dest="busname", type="string", default=DEFAULT_BUSNAME, help="Name of the bus exchange on the qpid broker, default: %s" % DEFAULT_BUSNAME)
+    parser.add_option("-s", "--servicename", dest="servicename", type="string", default=DEFAULT_SERVICENAME, help="Name for this service, default: %s" % DEFAULT_SERVICENAME)
+    parser.add_option('-V', '--verbose', dest='verbose', action='store_true', help='verbose logging')
+    (options, args) = parser.parse_args()
+
+    setQpidLogLevel(logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.DEBUG if options.verbose else logging.INFO)
+
+    with SpecifiedTaskListener() as service:
+        waitForInterrupt()
+
+if __name__ == '__main__':
+    main()
