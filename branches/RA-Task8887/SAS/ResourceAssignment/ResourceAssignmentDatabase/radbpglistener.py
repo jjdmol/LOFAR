c623cedfb2d21bf8f08e7dbe280412026134b489
schaap <schaap@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-02-17 14:33:47 +0000

    Task #8887: use dbcredentials in radbpglistener. added cmdline options

--- branches/RA-Task8887/SAS/ResourceAssignment/ResourceAssignmentDatabase/radbpglistener.py
+++ branches/RA-Task8887/SAS/ResourceAssignment/ResourceAssignmentDatabase/radbpglistener.py
@@ -28,23 +28,24 @@ import psycopg2.extras
 from datetime import datetime
 import time
 import json
+from optparse import OptionParser
 
 from lofar.common.postgres import PostgresListener
 from lofar.messaging import EventMessage, ToBus
-from lofar.sas.resourceassignment.database.config import radb_password, DEFAULT_BUSNAME
+from lofar.sas.resourceassignment.database.config import DEFAULT_BUSNAME, DEFAULT_NOTIFICATION_SUBJECT_PREFIX
+from lofar.common import dbcredentials
 
 logger = logging.getLogger(__name__)
 
 class RADBPGListener(PostgresListener):
     def __init__(self,
-                 host='10.149.96.6', #mcu005.control.lofar
-                 database='resourceassignment',
-                 username='resourceassignment',
-                 password=radb_password,
                  busname=DEFAULT_BUSNAME,
+                 notification_prefix=DEFAULT_NOTIFICATION_SUBJECT_PREFIX,
+                 dbcreds=None,
                  broker=None):
-        super(RADBPGListener, self).__init__(host, database, username, password)
+        super(RADBPGListener, self).__init__(dbcreds.host, dbcreds.database, dbcreds.user, dbcreds.password)
 
+        self.notification_prefix = notification_prefix
         self.event_bus = ToBus(busname, broker=broker)
 
         self.setupPostgresNotifications('resource_allocation', 'task', view_for_row='task_view')
@@ -58,22 +59,22 @@ class RADBPGListener(PostgresListener):
         self.subscribe('resource_claim_delete', self.onResourceClaimDeleted)
 
     def onTaskUpdated(self, payload = None):
-        self._sendNotification('RADB.TaskUpdated', payload, ['starttime', 'endtime'])
+        self._sendNotification('TaskUpdated', payload, ['starttime', 'endtime'])
 
     def onTaskInserted(self, payload = None):
-        self._sendNotification('RADB.TaskInserted', payload, ['starttime', 'endtime'])
+        self._sendNotification('TaskInserted', payload, ['starttime', 'endtime'])
 
     def onTaskDeleted(self, payload = None):
-        self._sendNotification('RADB.TaskDeleted', payload)
+        self._sendNotification('TaskDeleted', payload)
 
     def onResourceClaimUpdated(self, payload = None):
-        self._sendNotification('RADB.ResourceClaimUpdated', payload, ['starttime', 'endtime'])
+        self._sendNotification('ResourceClaimUpdated', payload, ['starttime', 'endtime'])
 
     def onResourceClaimInserted(self, payload = None):
-        self._sendNotification('RADB.ResourceClaimInserted', payload, ['starttime', 'endtime'])
+        self._sendNotification('ResourceClaimInserted', payload, ['starttime', 'endtime'])
 
     def onResourceClaimDeleted(self, payload = None):
-        self._sendNotification('RADB.ResourceClaimDeleted', payload)
+        self._sendNotification('ResourceClaimDeleted', payload)
 
     def __enter__(self):
         super(RADBPGListener, self).__enter__()
@@ -119,20 +120,35 @@ class RADBPGListener(PostgresListener):
             content=None
 
         try:
-            msg = EventMessage(context=subject, content=content)
+            msg = EventMessage(context=self.notification_prefix + subject, content=content)
             logger.info('Sending notification: ' + str(msg).replace('\n', ' '))
             self.event_bus.send(msg)
         except Exception as e:
             logger.error(str(e))
 
-def main(busname=DEFAULT_BUSNAME):
-    with RADBPGListener(busname=busname, password=radb_password) as listener:
+def main():
+    # Check the invocation arguments
+    parser = OptionParser("%prog [options]",
+                          description='runs the radb postgres listener which listens to changes on some tables in the radb and publishes the changes as notifications on the bus.')
+    parser.add_option('-q', '--broker', dest='broker', type='string', default=None, help='Address of the qpid broker, default: localhost')
+    parser.add_option("-b", "--busname", dest="busname", type="string", default=DEFAULT_BUSNAME, help="Name of the publication bus on the qpid broker, [default: %default]")
+    parser.add_option("-n", "--notification_prefix", dest="notification_prefix", type="string", default=DEFAULT_NOTIFICATION_SUBJECT_PREFIX, help="The prefix for all notifications of this publisher, [default: %default]")
+    parser.add_option_group(dbcredentials.options_group(parser))
+    parser.set_defaults(dbcredentials="RADB")
+    (options, args) = parser.parse_args()
+
+    dbcreds = dbcredentials.parse_options(options)
+
+    with RADBPGListener(busname=options.busname,
+                        notification_prefix=options.notification_prefix,
+                        dbcreds=dbcreds,
+                        broker=options.broker) as listener:
         listener.waitWhileListening()
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                         level=logging.INFO)
-    main(busname=DEFAULT_BUSNAME)
+    main()
 
 
 
