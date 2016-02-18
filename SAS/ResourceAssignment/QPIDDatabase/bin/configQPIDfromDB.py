a10446f11b5932ffeec0719127ca82008a6aad64
peterzon <peterzon@astron.nl>
2016-02-18 11:46:26 +0000

    Task #8900: initial export tool added

---
+++ SAS/ResourceAssignment/QPIDDatabase/bin/configQPIDfromDB.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# setup PYTHONPATH while testing: export PYTHONPATH=../lib:$PYTHONPATH
+
+from QPIDDB import qpidinfra
+
+
+def qpidconfig_add_queue(host,queue):
+    print ("qpid-config -b %s add queue %s --durable" %(host,queue))
+
+def qpidconfig_add_topic(host,exchange):
+    print ("qpid-config -b %s add exchange topic %s --durable" %(host,exchange))
+
+def qpidroute_add(fromhost,tohost,exchange,routingkey):
+    print ("qpid-route -d route add %s %s %s \'%s\' " %(tohost,fromhost,exchange,routingkey))
+
+QPIDinfra = qpidinfra()
+QPIDinfra.perqueue(qpidconfig_add_queue)
+QPIDinfra.perexchange(qpidconfig_add_topic)
+QPIDinfra.perfederationexchange(qpidroute_add)
+
