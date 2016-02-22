7c5e01832ed5459e6b24fe4dce7df9f77bb0b58b
schaap <schaap@astron.nl>
2016-02-22 08:10:48 +0000

    Task #8725: comments

--- LTA/LTAIngest/md5adler/md5.h
+++ LTA/LTAIngest/md5adler/md5.h
@@ -42,7 +42,9 @@
 /* typedef unsigned long int UINT4; */
 typedef unsigned int UINT4;
 
-/* Data structure for MD5 (Message Digest) computation */
+/* Data structure for MD5 (Message Digest) computation
+ * plus additional adler32 and byte_count variables
+ */
 typedef struct {
   unsigned int adler32;         /* adler 32 crc */
   unsigned long byte_count;     /* total number of bytes processed */
