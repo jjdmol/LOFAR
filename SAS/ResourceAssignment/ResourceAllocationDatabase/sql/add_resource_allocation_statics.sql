a966edeebf4fd79c0b0ddc78db0a485454f21227
schaap <schaap@astron.nl>
2016-02-25 15:05:28 +0000

    Task #8570: case

--- SAS/ResourceAssignment/ResourceAllocationDatabase/sql/add_resource_allocation_statics.sql
+++ SAS/ResourceAssignment/ResourceAllocationDatabase/sql/add_resource_allocation_statics.sql
@@ -5,7 +5,7 @@ BEGIN;
 INSERT INTO resource_allocation.task_status VALUES (200, 'prepared'), (300, 'approved'), (320, 'on_hold'), (335, 'conflict'),
 (350, 'prescheduled'), (400, 'scheduled'), (500, 'queued'), (600, 'active'), (900, 'completing'), (1000, 'finished'), (1100, 'aborted'),
 (1150, 'error'), (1200, 'obsolete'); -- This is the list from OTDB, we'll need to merge it with the list from MoM in the future, might use different indexes?
-INSERT INTO resource_allocation.task_type VALUES (0, 'OBSERVATION'),(1, 'PIPELINE'); -- We'll need more types
-INSERT INTO resource_allocation.resource_claim_status VALUES (0, 'CLAIMED'), (1, 'ALLOCATED'), (2, 'CONFLICT');
+INSERT INTO resource_allocation.task_type VALUES (0, 'observation'),(1, 'pipeline'); -- We'll need more types
+INSERT INTO resource_allocation.resource_claim_status VALUES (0, 'claimed'), (1, 'allocated'), (2, 'conflict');
 INSERT INTO resource_allocation.config VALUES (0, 'max_fill_percentage_cep4', '85.00'), (1, 'claim_timeout', '172800'); -- Just some values 172800 is two days in seconds
 COMMIT;
