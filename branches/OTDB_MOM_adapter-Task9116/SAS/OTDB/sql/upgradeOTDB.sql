27cddeb8b3e32ada5d995c8914c6b56902825674
overeem <overeem@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-02-29 13:36:01 +0000

    Task #9116: Added functions: getDefaultTemplates, getStations, setProjects.

--- branches/OTDB_MOM_adapter-Task9116/SAS/OTDB/sql/upgradeOTDB.sql
+++ branches/OTDB_MOM_adapter-Task9116/SAS/OTDB/sql/upgradeOTDB.sql
@@ -1,17 +1,5 @@
--- add new columns to the tree metadata table
-ALTER TABLE statehistory 
-	ADD COLUMN creation	timestamp(6) DEFAULT now();
-
-CREATE INDEX statehist_creation_idx ON statehistory(creation);
-
-CREATE TABLE otdb_admin (
-    treestatusevent timestamp(6)
-) WITHOUT OIDS;
-INSERT INTO otdb_admin VALUES(now());
 
 -- Load new functions
-\i getStateChanges_func.sql
+\i getStations_func.sql
 
 -- Load modified/improved functions
-\i exportTree_func.sql
-\i getTreeGroup_func.sql
