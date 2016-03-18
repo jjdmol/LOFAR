f65df4b0d7eccd0be47d53b56d1dc74255ec7cde
renting <renting@astron.nl>
2016-03-18 06:21:45 +0000

    Task #8886: checkin of incomplete changes to estimator to be able to continue editing offline

--- SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/base_resource_estimator.py
+++ SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/base_resource_estimator.py
@@ -1,6 +1,29 @@
+# base_resource_estimator.py
+#
+# Copyright (C) 2016
+# ASTRON (Netherlands Institute for Radio Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This file is part of the LOFAR software suite.
+# The LOFAR software suite is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# The LOFAR software suite is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id: base_resource_estimator.py 33534 2016-02-08 14:28:26Z schaap $
+
 """ Base class for Resource Estimators
 """
 import logging
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -10,36 +33,47 @@ class BaseResourceEstimator(object):
     """
     def __init__(self, name):
         self.name = name
-        self.error = ""
-        self.parset = {}
+        #self.error = ""
+        #self.parset = {}
         self.required_keys = ()
-        self.input_files = {}
-        self.output_files = {}
-        self.duration = 0  # in seconds
-        self.total_data_size = 0  # size in bytes
-        self.total_bandwidth = 0  # in bytes/second
+        #self.input_files = {}
+        #self.output_files = {}
+        #self.duration = 0  # in seconds
+        #self.total_data_size = 0  # size in bytes
+        #self.total_bandwidth = 0  # in bytes/second
 
-    def checkParsetForRequiredKeys(self):
-        """ check if all required keys needed are available"""
+    def _checkParsetForRequiredKeys(self, parset):
+        """ Check if all required keys needed are available """
         logger.debug("required keys: %s" % ', '.join(self.required_keys))
-        logger.debug("parset   keys: %s" % ', '.join(self.parset.keys()))
-
-        missing_keys = set(self.required_keys) - set(self.parset.keys())
-
+        logger.debug("parset   keys: %s" % ', '.join(parset.keys()))
+        missing_keys = set(self.required_keys) - set(parset.keys())
         if missing_keys:
             logger.error("missing keys: %s" % ', '.join(missing_keys))
             return False
-
         return True
+    
+    def _getDateTime(date_time):
+        return datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+
+    def _calculate(self, parset, input_files={}):
+        raise NotImplementedError('estimate() in base class is called. Please implement estimate() in your subclass')
+
+    def estimate(self, parset, input_files={}):
+        """ Create estimates for a single process based on its parset and input files"""
+        if self._checkParsetForRequiredKeys(parset):
+            estimates = self._calculate(parset, input_files)
+        else:
+            raise ValueError('The parset is incomplete')
+        result = {}
+        result[self.name] = {}
+        result[self.name]['total_data_size'] = estimates['total_data_size']
+        result[self.name]['total_bandwidth'] = estimates['total_bandwidth']
+        result[self.name]['output_files']    = estimates['output_files']
+        return result
 
     def estimate(self):
         raise NotImplementedError('estimate() in base class is called. Please implement estimate() in your subclass')
 
-    def result_as_dict(self):
-        """ return estimated values as dict """
-        result = {}
-        result[self.name] = {}
-        result[self.name]['total_data_size'] = int(self.total_data_size)
-        result[self.name]['total_bandwidth'] = int(self.total_bandwidth)
-        result[self.name]['output_files'] = self.output_files
-        return result
+#    def result_as_dict(self):
+#        """ return estimated values as dict """
+#        return result
--- SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/calibration_pipeline.py
+++ SAS/ResourceAssignment/ResourceAssignmentEstimator/resource_estimators/calibration_pipeline.py
@@ -1,13 +1,31 @@
-""" C
-"""
+# base_resource_estimator.py
+#
+# Copyright (C) 2016
+# ASTRON (Netherlands Institute for Radio Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This file is part of the LOFAR software suite.
+# The LOFAR software suite is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# The LOFAR software suite is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id: base_resource_estimator.py 33534 2016-02-08 14:28:26Z schaap $
 
 import logging
 from math import ceil
 from base_resource_estimator import BaseResourceEstimator
-from .parameterset import ParameterSet
+from lofar.parameterset import parameterset
 
 logger = logging.getLogger(__name__)
-
 
 class CalibrationPipelineResourceEstimator(BaseResourceEstimator):
     """ CalibrationPipelineResourceEstimator
@@ -15,42 +33,40 @@ class CalibrationPipelineResourceEstimator(BaseResourceEstimator):
     """
     def __init__(self, kwargs, input_files):
         BaseResourceEstimator.__init__(self, name='calibration_pipeline')
-        self.parset = ParameterSet(kwargs).make_subset('dp.output')
-        self.duration = int(kwargs.get('observation.duration', 1))
-        self.input_files = input_files
         self.required_keys = ('correlated.enabled', 'correlated.demixing_settings.freq_step',
                           'correlated.demixing_settings.time_step', 'instrument_model.enabled')
-        if self.checkParsetForRequiredKeys():
-            self.estimate()
-        return
 
-    def estimate(self):
+    def _calculate(self, parset, input_files):
         """ Estimate for calibration pipeline
         calculates: datasize (number of files, file size), bandwidth
         """
         logger.debug("start estimate '{}'".format(self.name))
-        if 'dp_correlated_uv' in self.input_files:
-            if self.parset['correlated']['enabled'] == 'true':
+        parset = parameterset(parset).makeSubset('dp.output')
+        output_files = {}
+        duration = int(kwargs.get('observation.duration', 1))
+        if 'dp_correlated_uv' in input_files:
+            if parset['correlated']['enabled'] == 'true':
                 logger.debug("calculate correlated data size")
-                freq_step = int(self.parset['correlated']['demixing_settings']['freq_step'])
-                time_step = int(self.parset['correlated']['demixing_settings']['time_step'])
+                freq_step = int(parset['correlated']['demixing_settings']['freq_step'])
+                time_step = int(parset['correlated']['demixing_settings']['time_step'])
                 reduction_factor = freq_step * time_step
-                input_file_size = int(self.input_files['dp_correlated_uv']['file_size'])
+                input_file_size = int(input_files['dp_correlated_uv']['file_size'])
                 output_file_size = 0.0
                 if reduction_factor > 0:
                     new_size = input_file_size / reduction_factor
                     output_file_size = new_size + new_size / 64.0 * (1.0 + reduction_factor) + new_size / 2.0
-                self.output_files['dp_correlated_uv'] = {'nr_files': int(self.input_files['dp_correlated_uv']['nr_files']), 'file_size': int(output_file_size)}
-                logger.debug("dp_correlated_uv: {} files {} bytes each".format(int(self.input_files['dp_correlated_uv']['nr_files']), int(output_file_size)))
+                output_files['dp_correlated_uv'] = {'nr_files': int(input_files['dp_correlated_uv']['nr_files']), 'file_size': int(output_file_size)}
+                logger.debug("dp_correlated_uv: {} files {} bytes each".format(int(input_files['dp_correlated_uv']['nr_files']), int(output_file_size)))
 
-            if self.parset['instrument_model']['enabled'] == 'true':
+            if parset['instrument_model']['enabled'] == 'true':
                 logger.debug("calculate instrument-model data size")
-                self.output_files['dp_instrument_model'] = {'nr_files': int(self.input_files['dp_correlated_uv']['nr_files']), 'file_size': 1000}
-                logger.debug("dp_pulsar: {} files {} bytes each".format(int(self.input_files['dp_correlated_uv']['nr_files']), 1000))
+                output_files['dp_instrument_model'] = {'nr_files': int(input_files['dp_correlated_uv']['nr_files']), 'file_size': 1000}
+                logger.debug("dp_instrument_model: {} files {} bytes each".format(int(input_files['dp_correlated_uv']['nr_files']), 1000))
 
             # count total data size
-            for values in self.output_files.itervalues():
-                self.total_data_size += values['nr_files'] * values['file_size']
-            self.total_bandwidth = ceil((self.total_data_size * 8) / self.duration)  # bits/second
-        return
+            total_data_size = 0
+            for values in output_files.itervalues():
+                total_data_size += values['nr_files'] * values['file_size']
+            total_bandwidth = ceil((self.total_data_size * 8) / duration)  # bits/second
+        return {"total_data_size":total_data_size, "total_bandwidth":total_bandwidth, "output_files":output_files}
 
