f44581dfc05e3784cdbc90db63934b9aafd5b436
schaap <schaap@astron.nl>
2016-03-18 14:05:11 +0000

    Task #8887: removed obsolete rapublisher, now that everybody can listen with teh radbbuslistener.

--- SAS/ResourceAssignment/ResourceAssigner/lib/config.py
+++ SAS/ResourceAssignment/ResourceAssigner/lib/config.py
@@ -4,10 +4,6 @@
 DEFAULT_BUSNAME = 'lofar.ra.command'
 DEFAULT_SERVICENAME = 'RAService'
 
-RA_NOTIFICATION_BUSNAME='lofar.ra.notification'
-RA_NOTIFICATION_PREFIX='RA.'
-RA_NOTIFICATION_SUBJECTS=RA_NOTIFICATION_PREFIX+'*'
-
 try:
     from lofar.sas.resourceassignment.rataskspecified.config import DEFAULT_NOTIFICATION_BUSNAME as RATASKSPECIFIED_NOTIFICATION_BUSNAME
     from lofar.sas.resourceassignment.rataskspecified.config import RATASKSPECIFIED_NOTIFICATIONNAME as RATASKSPECIFIED_NOTIFICATIONNAME
