896828e892f326d4580ec113122dba3ed60f5ddc
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-04-08 15:01:18 +0000

    Task #8887: Added "old" message routes

--- branches/RA-Task8887/SAS/ResourceAssignment/QPIDInfrastructure/bin/populateDB.sh
+++ branches/RA-Task8887/SAS/ResourceAssignment/QPIDInfrastructure/bin/populateDB.sh
@@ -11,30 +11,58 @@ PROD=false
 
 # Host names to use
 if $PROD; then
-  CCU=ccu001.control.lofar
-  MCU=mcu001.control.lofar
-  SCU=scu001.control.lofar
+  CCU=ccu001
+  MCU=mcu001
+  SCU=scu001
+  SAS=sas001
+
+  LCS=lcs023
+
+  COBALT="cbt001 cbt002 cbt003 cbt004 cbt005 cbt006 cbt007 cbt008"
+
+  CEP2="`seq -f locus%03.0f 1 94`"
+  CEP2HEAD=lhn001
+
+  CEP4="`seq -f cpu%02.0f.cep4 1 50`"
+  CEP4HEAD="head01.cep4 head02.cep4"
 else
-  CCU=ccu099.control.lofar
-  MCU=mcu099.control.lofar
-  SCU=scu099.control.lofar
+  CCU=ccu099
+  MCU=mcu099
+  SCU=scu099
+  SAS=sas099
+
+  LCS=lcs028
+
+  COBALT="cbt009"
+
+  CEP2="locus098 locus099"
+  CEP2HEAD=locus102
+
+  CEP4="`seq -f cpu%02.0f.cep4 1 50`"
+  CEP4HEAD="head01.cep4 head02.cep4"
 fi
 
 # -----------------------------------------
-#   Queues for processing feedback
-#
-# Queues:
-#    lofar.task.feedback.dataproducts
-#    lofar.task.feedback.processing
-#    lofar.task.feedback.state
-#
-# Route (all queues):
-#    cpuXX.cep4 -> headXX.cep4 -> ccu001
+#   Cobalt & Pipelines -> MessageRouter
 # -----------------------------------------
 
-for tnode in head{01..02}.cep4.control.lofar
+for tnode in $CEP4HEAD
 do
-  for fnode in cpu{01..50}.cep4.control.lofar
+  for fnode in $CEP4
+  do
+    addtoQPIDDB.py --broker $fnode --queue lofar.task.feedback.dataproducts --federation $tnode
+    addtoQPIDDB.py --broker $fnode --queue lofar.task.feedback.processing --federation $tnode
+    addtoQPIDDB.py --broker $fnode --queue lofar.task.feedback.state --federation $tnode
+  done
+
+  addtoQPIDDB.py --broker $tnode --queue lofar.task.feedback.dataproducts --federation $CCU
+  addtoQPIDDB.py --broker $tnode --queue lofar.task.feedback.processing --federation $CCU
+  addtoQPIDDB.py --broker $tnode --queue lofar.task.feedback.state --federation $CCU
+done
+
+for tnode in $CEP2HEAD
+do
+  for fnode in $CEP2
   do
     addtoQPIDDB.py --broker $fnode --queue lofar.task.feedback.dataproducts --federation $tnode
     addtoQPIDDB.py --broker $fnode --queue lofar.task.feedback.processing --federation $tnode
@@ -47,19 +75,45 @@ do
 done
 
 # -----------------------------------------
-#   Exchanges for ResourceAssignment
-#
-# Exchanges:
-#    lofar.ra.command
-#    lofar.ra.notification
-#    lofar.otdb.command
-#    lofar.otdb.notification
-#    lofar.ssdb.command
-#    lofar.ssdb.notification
-#
-# Route (lofar.ra.command):
-#    cpuXX.cep4 -> headXX.cep4 -> ccu001
+#   MessageRouter -> MoM
 # -----------------------------------------
+
+addtoQPIDDB.py --broker $CCU --queue mom.task.feedback.dataproducts --federation $LCS
+addtoQPIDDB.py --broker $CCU --queue mom.task.feedback.processing --federation $LCS
+addtoQPIDDB.py --broker $CCU --queue mom.task.feedback.state --federation $LCS
+
+# -----------------------------------------
+#   MessageRouter -> OTDB
+# -----------------------------------------
+
+addtoQPIDDB.py --broker $CCU --queue otdb.task.feedback.dataproducts --federation $MCU
+addtoQPIDDB.py --broker $CCU --queue otdb.task.feedback.processing --federation $MCU
+
+# -----------------------------------------
+#   MessageRouter -> MAC
+# -----------------------------------------
+
+addtoQPIDDB.py --broker $CCU --exchange mac.task.feedback.state
+
+# -----------------------------------------
+#   MACScheduler -> MessageRouter -> MoM
+# -----------------------------------------
+
+addtoQPIDDB.py --broker $MCU --queue lofar.task.specification.system --federation $CCU
+addtoQPIDDB.py --broker $CCU --queue mom.task.specification.system --federation $LCS
+
+# -----------------------------------------
+#   MoM <-> MoM-OTDB-Adapter
+# -----------------------------------------
+
+addtoQPIDDB.py --broker $SAS --queue mom.command --federation $LCS
+addtoQPIDDB.py --broker $SAS --queue mom.importxml --federation $LCS
+addtoQPIDDB.py --broker $LCS --queue mom-otdb-adapter.importxml --federation $SAS
+
+# -----------------------------------------
+#   ResourceAssignment
+# -----------------------------------------
+
 addtoQPIDDB.py --broker $SCU --exchange lofar.ra.command
 addtoQPIDDB.py --broker $SCU --exchange lofar.ra.notification
 addtoQPIDDB.py --broker $SCU --exchange lofar.otdb.command
@@ -68,9 +122,9 @@ addtoQPIDDB.py --broker $SCU --exchange lofar.ssdb.command
 addtoQPIDDB.py --broker $SCU --exchange lofar.ssdb.notification
 
 # TODO: messages will end up at $SCU twice?
-for tnode in head{01..02}.cep4.control.lofar
+for tnode in head{01..02}.cep4
 do
-  for fnode in cpu{01..50}.cep4.control.lofar
+  for fnode in cpu{01..50}.cep4
   do
     addtoQPIDDB.py --broker $fnode --exchange lofar.otdb.command --federation $tnode
   done
