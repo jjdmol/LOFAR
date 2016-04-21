709febc6ee4c5493ad38514e759a6f5c79d53122
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-04-21 14:48:39 +0000

    Task #8887: Parse LOFARENV using the latest conventions

--- branches/RA-Task8887/SAS/ResourceAssignment/QPIDInfrastructure/bin/populateDB.sh
+++ branches/RA-Task8887/SAS/ResourceAssignment/QPIDInfrastructure/bin/populateDB.sh
@@ -7,8 +7,13 @@
 # Whether to modify production (true) or test (false)
 if [ "$LOFARENV" == "PRODUCTION" ]; then
   PROD=true
+  PREFIX=
+elif [ "$LOFARENV" == "TEST" ]; then
+  PROD=false
+  PREFIX=".test"
 else
   PROD=false
+  PREFIX=".devel"
 fi
 
 
@@ -20,8 +25,6 @@ if $PROD; then
   echo "Press ENTER to continue, or ^C to abort"
   echo "----------------------------------------------"
   read
-
-  PREFIX=
 
   CCU=ccu001.control.lofar
   MCU=mcu001.control.lofar
@@ -39,8 +42,6 @@ if $PROD; then
   CEP4="`seq -f cpu%02.0f.cep4.control.lofar 1 50`"
   CEP4HEAD="head01.cep4.control.lofar head02.cep4.control.lofar"
 else
-  PREFIX=test.
-
   CCU=ccu099.control.lofar
   MCU=mcu099.control.lofar
   SCU=scu099.control.lofar
