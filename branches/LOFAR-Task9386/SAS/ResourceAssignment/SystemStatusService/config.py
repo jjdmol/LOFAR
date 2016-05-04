a3053e27eda24c721c71972ee588a72367f12dfa
mol <mol@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-05-04 10:03:21 +0000

    Task #9386: Deleted working branch

--- branches/LOFAR-Task9386/SAS/ResourceAssignment/SystemStatusService/config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# $Id$
-
-from lofar.messaging import adaptNameToEnvironment
-
-DEFAULT_SSDB_BUSNAME = adaptNameToEnvironment('lofar.ssdb.command')
-DEFAULT_SSDB_SERVICENAME = 'SSDBService'
