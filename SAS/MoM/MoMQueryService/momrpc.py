c33249e539aeabce54a5f10c25da299b08453685
schaap <schaap@astron.nl>
2016-05-20 10:29:06 +0000

    Task #9349: added little cmdline script to copy a task in mom (for testing and convenience)

--- SAS/MoM/MoMQueryService/momrpc.py
+++ SAS/MoM/MoMQueryService/momrpc.py
@@ -4,11 +4,15 @@ import sys
 import logging
 from optparse import OptionParser
 from lofar.messaging.RPC import RPC, RPCException, RPCWrapper
+from lofar.messaging import setQpidLogLevel
 from lofar.mom.momqueryservice.config import DEFAULT_MOM_BUSNAME, DEFAULT_MOM_SERVICENAME
 
 logger = logging.getLogger(__file__)
 
 class MoMRPC(RPCWrapper):
+    def __init__(self, busname=DEFAULT_MOM_BUSNAME, broker=None, timeout=10, verbose=False):
+        super(MoMRPC, self).__init__(busname=busname, servicename='', broker=broker, timeout=timeout, verbose=verbose)
+
     def copyTask(self, mom2id, newTaskName=None, newTaskDescription=None):
         logger.info("calling copyTask rpc for mom2id %s" % (mom2id))
         new_task_mom2id = self.rpc('TaskCopy', mom2Id=mom2id) #, newTaskName=newTaskName, newTaskDescription=newTaskDescription)
@@ -21,7 +25,6 @@ def main():
                           description='do rpc calls to the momservice from the commandline')
     parser.add_option('-q', '--broker', dest='broker', type='string', default=None, help='Address of the qpid broker, default: localhost')
     parser.add_option('-b', '--busname', dest='busname', type='string', default=DEFAULT_MOM_BUSNAME, help='Name of the bus exchange on the qpid broker [default: %default]')
-    parser.add_option('-s', '--servicename', dest='servicename', type='string', default=DEFAULT_MOM_SERVICENAME, help='Name for this service [default: %default]')
     parser.add_option('--mom2id', dest='mom2id_to_copy', type='int', help='[REQUIRED] mom2id of the task to copy.')
     parser.add_option('-V', '--verbose', dest='verbose', action='store_true', help='verbose logging')
     (options, args) = parser.parse_args()
@@ -34,8 +37,9 @@ def main():
 
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                         level=logging.DEBUG if verbose else logging.INFO)
+    setQpidLogLevel(logging.WARN)
 
-    with MoMRPC(busname=options.busname, servicename=options.servicename, broker=options.broker, verbose=verbose) as rpc:
+    with MoMRPC(busname=options.busname, broker=options.broker, verbose=verbose) as rpc:
         print rpc.copyTask(options.mom2id_to_copy)
 
 if __name__ == '__main__':
