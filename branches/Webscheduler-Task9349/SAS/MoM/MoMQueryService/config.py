7b4ea0910b315e5c76099dbc8fc985d441af8893
schaap <schaap@b3281354-443f-0410-9bd6-bf5212d75c11>
2016-05-20 10:29:06 +0000

    Task #9349: added little cmdline script to copy a task in mom (for testing and convenience)

--- branches/Webscheduler-Task9349/SAS/MoM/MoMQueryService/config.py
+++ branches/Webscheduler-Task9349/SAS/MoM/MoMQueryService/config.py
@@ -6,5 +6,5 @@ from lofar.messaging import adaptNameToEnvironment
 DEFAULT_MOMQUERY_BUSNAME = adaptNameToEnvironment('lofar.ra.command')
 DEFAULT_MOMQUERY_SERVICENAME = 'momqueryservice'
 
-DEFAULT_MOM_BUSNAME = adaptNameToEnvironment('lofar.mom.bus')
+DEFAULT_MOM_BUSNAME = adaptNameToEnvironment('lofar.mom.command')
 DEFAULT_MOM_SERVICENAME = ''
